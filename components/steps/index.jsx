943fbd3d3455797398e526d8ef8396fb58efa4e2
羽航 <geyuhang.pt@alibaba-inc.com>
2015-07-08 15:53:51 +0800

    调整steps的样式

--- components/steps/index.jsx
+++ components/steps/index.jsx
@@ -7,13 +7,15 @@ var AntSteps = React.createClass({
   getDefaultProps() {
     return {
       prefixCls: 'ant-steps',
+      iconPrefix: 'ant',
       size: 'default',
       maxDescriptionWidth: 100
     };
   },
   render() {
     return (<Steps size={this.props.size}
-                   maxDescriptionWidth ={this.props.maxDescriptionWidth}
+                   iconPrefix={this.props.iconPrefix}
+                   maxDescriptionWidth={this.props.maxDescriptionWidth}
                    prefixCls={this.props.prefixCls}>
       {this.props.children}
     </Steps>);
