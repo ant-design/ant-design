d48b80299be01f7e6ce654cdeb8abb778572c154
afc163 <afc163@gmail.com>
2015-07-15 17:32:32 +0800

    code style

--- components/pagination/index.jsx
+++ components/pagination/index.jsx
@@ -8,7 +8,10 @@ let prefixCls = 'ant-pagination';
 
 class AntPagination extends React.Component {
   render() {
-    return <Pagination selectComponentClass={Select} selectPrefixCls="ant-select" prefixCls={prefixCls} {...this.props} />;
+    return <Pagination selectComponentClass={Select}
+      selectPrefixCls="ant-select"
+      prefixCls={prefixCls}
+      {...this.props} />;
   }
 }
 
