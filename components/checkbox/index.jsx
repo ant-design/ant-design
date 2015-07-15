40595178cefcb316a78e44f9477bace42df6a570
蔡伦 <liuqin.sheng@alipay.com>
2015-07-15 16:01:24 +0800

    demo update

--- components/checkbox/index.jsx
+++ components/checkbox/index.jsx
@@ -1,15 +1,11 @@
 var Checkbox = require('rc-checkbox');
 var React = require('react');
-
 var AntCheckbox = React.createClass({
-    getDefaultProps() {
-      return {
-            prefixCls: 'ant-checkbox'
-        };
-    },
-    render() {
-      return <Checkbox {...this.props}/>;
-    }
+  getDefaultProps() {
+    return {prefixCls: 'ant-checkbox'};
+  }, render() {
+    return <Checkbox {...this.props}/>;
+  }
 });
 
 module.exports = AntCheckbox;
