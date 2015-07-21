1108336633d76182329e9ae0d734cfda73eef8bf
afc163 <afc163@gmail.com>
2015-07-21 18:06:42 +0800

    fix prepublish script

--- scripts/prenpm.js
+++ scripts/prenpm.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 var cwd = process.cwd();
 var path = require('path');
+var pkg = require('../package');
+
 var originalIndex = fs.readFileSync(path.join(cwd, 'index.js'), 'utf-8');
 var newIndex = originalIndex
   .replace(/\/components\//g, '/')
   .replace("require('./package.json').version", "'" + require('../package.json').version + "'")
   .replace("require('./style/index.less')", "require('./index.css')");
 fs.writeFileSync(path.join(cwd, 'lib/index.js'), newIndex, 'utf-8');
-fs.createReadStream(path.join(cwd, 'dist/antd.css')).pipe(fs.createWriteStream(path.join(cwd, 'lib/index.css')));
+
+var antdCss = path.join(cwd, 'dist/' + pkg.name + '-' + pkg.version + '.css');
+fs.createReadStream(antdCss)
+  .pipe(fs.createWriteStream(path.join(cwd, 'lib/index.css')));
 console.log('prenpm done');
