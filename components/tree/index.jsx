0065270e7f2515a2a2865ae11ab71c763d3d8e3b
ioldfish <fish.wangl@gmail.com>
2015-08-05 17:38:02 +0800

    update ant-tree style

--- components/tree/index.jsx
+++ components/tree/index.jsx
@@ -3,9 +3,14 @@ import Tree from 'rc-tree';
 var TreeNode = Tree.TreeNode;
 
 var antDTree = React.createClass({
+  getDefaultProps() {
+    return {
+      prefixCls: 'ant-tree'
+    };
+  },
 
   render() {
-    return <Tree {...this.props} >
+    return <Tree {...this.props} showIcon={false} showLine={false}>
       {this.props.children}
     </Tree>;
   }
