6b32f18f79289f28241a1309e710325d7556bfc9
afc163 <afc163@gmail.com>
2015-08-17 18:14:39 +0800

    improve table filter dropdown

--- components/table/filterDropdown.jsx
+++ components/table/filterDropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Menu from 'rc-menu';
+import Dropdown from '../dropdown';
 
 var FilterMenu = React.createClass({
   getInitialState() {
@@ -29,8 +30,16 @@ var FilterMenu = React.createClass({
       selectedKeys: []
     }, this.handleConfirm);
   },
-  handleConfirm(){
+  handleConfirm() {
+    this.setState({
+      visible: false
+    });
     this.props.confirmFilter(this.props.column, this.state.selectedKeys);
+  },
+  onVisibleChange(visible) {
+    this.setState({
+      visible: visible
+    });
   },
   renderMenus(items) {
     let menuItems = items.map((item) => {
@@ -40,7 +49,7 @@ var FilterMenu = React.createClass({
   },
   render() {
     let column = this.props.column;
-    return <Menu multiple={true}
+    let menus = <Menu multiple={true}
                  prefixCls="ant-dropdown-menu"
                  className="ant-table-filter-dropdown"
                  onSelect={this.setSelectedKeys}
@@ -67,6 +76,15 @@ var FilterMenu = React.createClass({
         </a>
       </Menu.Item>
     </Menu>;
+
+    let dropdownSelectedClass = '';
+    if (this.props.selectedKeys.length > 0) {
+      dropdownSelectedClass = 'ant-table-filter-selected';
+    }
+
+    return <Dropdown trigger="click" overlay={menus} visible={this.state.visible} onVisibleChange={this.onVisibleChange}>
+      <i title="筛选" className={'anticon anticon-bars ' + dropdownSelectedClass}></i>
+    </Dropdown>;
   }
 });
 
