e95e5570770e930cd07162f95659ff783c18f6d4
afc163 <afc163@gmail.com>
2015-08-18 18:36:31 +0800

    detect mousePosition in click event

--- components/modal/index.jsx
+++ components/modal/index.jsx
@@ -5,12 +5,7 @@ function noop() {
 }
 
 let mousePosition;
-Dom.addEventListener(document, 'mousemove', function onDocumentMousemove(e) {
-  mousePosition = {
-    x: e.pageX,
-    y: e.pageY
-  };
-});
+let mousePositionEventBinded;
 
 export default React.createClass({
   getDefaultProps() {
@@ -53,6 +48,26 @@ export default React.createClass({
       }
       this.setState(newState);
     }
+  },
+
+  componentDidMount() {
+    if (mousePositionEventBinded) {
+      return;
+    }
+    // 只有点击事件支持从鼠标位置动画展开
+    Dom.addEventListener(document.body, 'click', function onDocumentMousemove(e) {
+      mousePosition = {
+        x: e.pageX,
+        y: e.pageY
+      };
+      // 100ms 内发生过点击事件，则从点击位置动画展示
+      // 否则直接 zoom 展示
+      // 这样可以兼容非点击方式展开
+      setTimeout(function() {
+        mousePosition = null;
+      }, 100);
+    });
+    mousePositionEventBinded = true;
   },
 
   render() {
