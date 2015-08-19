ac27a1814d25aefbfaa7658b7ee56acb239240b0
jljsj <liangjun.jiang@alipay.com>
2015-08-19 17:26:55 +0800

    add tag animation

--- components/tag/index.jsx
+++ components/tag/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { animationEndEvent, addEventListenerOnce } from '../util/index';
 const prefixCls = 'ant-tag';
 
 class AntTag extends React.Component {
@@ -10,17 +11,26 @@ class AntTag extends React.Component {
     };
   }
   close(e) {
+    var dom = React.findDOMNode(this);
+    dom.style.width = dom.offsetWidth + 'px';
+    // Magic code
+    // 重复是去除浏览器渲染bug；
+    dom.style.width = dom.offsetWidth + 'px';
     this.setState({
       closed: true
     });
     this.props.onClose.call(this, e);
   }
+
   render() {
     let close = this.props.closable ?
       <i className="anticon anticon-cross" onClick={this.close.bind(this)}></i> : '';
     let colorClass = this.props.prefixCls + '-' + this.props.color;
 
-    return this.state.closed ? null : <div className={this.props.prefixCls + ' ' + colorClass}>
+    let className = this.props.prefixCls + ' ' + colorClass;
+    className = this.state.closed ? className +' '+ this.props.prefixCls + '-close' : className;
+
+    return <div className={className}>
       <a className={this.props.prefixCls + '-text'} {...this.props} />
       {close}
     </div>;
@@ -30,7 +40,8 @@ class AntTag extends React.Component {
 AntTag.defaultProps = {
   prefixCls: prefixCls,
   closable: false,
-  onClose: function() {}
+  onClose: function () {
+  }
 };
 
 export default AntTag;
