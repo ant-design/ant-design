6538c5d862da8766ac4cf904c8eb8b3bccc4ab66
jljsj <liangjun.jiang@alipay.com>
2015-08-24 17:50:12 +0800

    updata menuAnimation

--- components/menu/index.jsx
+++ components/menu/index.jsx
@@ -1,6 +1,57 @@
 import React from 'react';
 import Menu from 'rc-menu';
-import { transitionEndEvent, animationEndEvent, addEventListenerOnce } from '../util/index';
+import velocity from 'velocity-animate';
+import EnterAnimation from 'enter-animation';
+
+const animation = {
+  enter(node, done) {
+    var ok = false;
+
+    function complete() {
+      if (!ok) {
+        ok = 1;
+        done();
+      }
+    }
+    node.style.display = 'none';
+    velocity(node, 'slideDown', {
+      duration: 300,
+      complete: complete,
+      easing: 'easeInOutQuad'
+    });
+    return {
+      stop: function () {
+        velocity(node, 'finish');
+        complete();
+      }
+    };
+  },
+  appear() {
+    return this.enter.apply(this, arguments);
+  },
+  leave(node, done) {
+    var ok = false;
+
+    function complete() {
+      if (!ok) {
+        ok = 1;
+        done();
+      }
+    }
+
+    velocity(node, 'slideUp', {
+      duration: 300,
+      complete: complete,
+      easing: 'easeInOutQuad'
+    });
+    return {
+      stop: function () {
+        velocity(node, 'finish');
+        complete();
+      }
+    };
+  }
+};
 
 const AntMenu = React.createClass({
   getDefaultProps() {
@@ -8,57 +59,29 @@ const AntMenu = React.createClass({
       prefixCls: 'ant-menu'
     };
   },
-
   toggle(info) {
     let dom = React.findDOMNode(info.item || info);
-    let ul = dom.getElementsByTagName('ul')[0] || dom;
-    ul.style.display = 'block';
-
-    let animStr = this.props.mode === 'horizontal' || !this.props.mode ? ' slide-up' : ' zoom', childAnimStr = ' zoom';
-    let enterAndleave = info.open ? '-enter' : '-leave';
-
-    if (this.props.mode === 'inline') {
-      ul.style.height='';
-      addEventListenerOnce(ul, transitionEndEvent, function (e) {
-        ul.style.height = '';
-        ul.style.display = '';
-      });
-      let offsetHeight = ul.offsetHeight;
-      if (info.open) {
-        ul.style.height = 0;
-      } else {
-        ul.style.height = offsetHeight + 'px';
-        offsetHeight = 0;
+    let ul = dom.getElementsByTagName('ul')[0];
+    function TweenAnimate(_dom) {
+      let num=Number((.12/_dom.children.length).toFixed(3));
+      if(info.open){
+        EnterAnimation.to(_dom,{duration:.15,data:'scale',interval:num});
+      }else{
+        EnterAnimation.to(_dom,{duration:.15,data:'scale',interval:num,direction:'leave',upend:true,ease:'cubic-bezier(0.6, 0.04, 0.98, 0.335)'});
       }
-      // Magic code
-      //跟tag的渲染bug一样,需要再去计算一次dom的height;
-      let a=ul.offsetHeight;
-      ul.style.height = offsetHeight + 'px';
+    }
+    if (this.props.mode && info.item && !ul) {
+      setTimeout(()=> {
+        ul = dom.getElementsByTagName('ul')[0];
+        TweenAnimate(ul);
+      }, 1);
     } else {
-      ul.className += animStr + enterAndleave + animStr + enterAndleave + '-active';
-      addEventListenerOnce(ul, animationEndEvent, function (e) {
-        ul.style.display = '';
-        ul.className = ul.className.replace(/(slide-up-|zoom-|enter|-active|leave)/g, '').trim();
-      });
+      if (!ul) {
+        ul = dom;
+      }
+      TweenAnimate(ul);
     }
-
-    let ulChild = ul.children, len = ulChild.length;
-    for (let i = 0; i < len; i++) {
-      let li = ulChild[i];
-      let delayNum = info.open ? i : (len - 1 - i);
-      let delay = 0.2 / len * delayNum;
-      li.style.animationDelay = delay + 's';
-      li.style.animationDuration = (0.2 - delay + 0.1) + 's';
-      li.className += childAnimStr + enterAndleave + childAnimStr + enterAndleave + '-active';
-      addEventListenerOnce(li, animationEndEvent, function (e) {
-        li.style.animationDelay = '';
-        li.style.animationDuration = '';
-        li.className = li.className.replace(/(slide-up-|zoom-|enter|-active|leave)/g, '').trim();
-      });
-    }
-
   },
-
   componentDidMount() {
     this.componentDidUpdate(this.props);
   },
@@ -70,9 +93,26 @@ const AntMenu = React.createClass({
       this.toggle(this);
     }
   },
-
   render() {
-    return <Menu {...this.props} onOpen={this.toggle} onClose={this.toggle}/>;
+    let openAnimation = '';
+    switch (this.props.mode) {
+      case 'horizontal':
+        openAnimation = 'slide-up';
+        break;
+      case 'vertical':
+        openAnimation = 'zoom';
+        break;
+      case 'inline':
+        openAnimation = animation;
+        break;
+    }
+    let html = '';
+    if (this.props.mode === 'inline') {
+      html = <Menu {...this.props} openAnimation={openAnimation} onOpen={this.toggle} onClose={this.toggle}/>;
+    } else {
+      html = <Menu {...this.props} openTransitionName={openAnimation} onOpen={this.toggle} onClose={this.toggle}/>;
+    }
+    return html;
   }
 });
 
