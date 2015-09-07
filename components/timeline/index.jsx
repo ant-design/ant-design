f3bbf778f84b8b7a85b343be23cc2cdafe6fd1bb
zhujun24 <zhujun87654321@gmail.com>
2015-09-07 15:13:59 +0800

    continue api position

--- components/timeline/index.jsx
+++ components/timeline/index.jsx
@@ -8,11 +8,13 @@ let Timeline = React.createClass({
   },
   render() {
     let children = this.props.children;
+    let continuee = this.props.continue;
     return (
       <ul className={this.props.prefixCls}>
         {React.Children.map(children, function (ele, idx) {
           let np = {
-            timelineLast: idx === children.length - 1
+            timelineLast: idx === children.length - 1,
+            continuee: continuee
           };
           return React.cloneElement(ele, np);
         }, this)}
@@ -26,21 +28,22 @@ Timeline.Item = React.createClass({
     return {
       prefixCls: 'ant-timeline',
       color: 'blue',
-      end: false
+      continuee: false
     };
   },
   render() {
     let props = this.props;
     let prefixCls = props.prefixCls;
     let color = props.color;
-    let end = props.end;
-    let endCls = end ? prefixCls + '-item-last' : '';
-    let last = end ? <div className={prefixCls + '-item-head ' + prefixCls + '-item-head-end'}></div> : null;
-    let lastLineShow = (props.timelineLast && !end) ? 'none' : 'block';
+    let continuee = props.continuee;
+    let timelineLast = props.timelineLast;
+    let endCls = continuee && timelineLast ? prefixCls + '-item-last' : '';
+    let last = continuee && timelineLast ? <div className={prefixCls + '-item-head ' + prefixCls + '-item-head-end'}></div> : null;
+    let lastTailShow = (timelineLast && !continuee) ? 'none' : 'block';
 
     return (
       <li className={prefixCls + '-item ' + endCls}>
-        <div style={{display:lastLineShow}} className={prefixCls + '-item-tail'}></div>
+        <div style={{display:lastTailShow}} className={prefixCls + '-item-tail'}></div>
         <div className={prefixCls + '-item-head ' + prefixCls + '-item-head-' + color}></div>
         <div className={prefixCls + '-item-content'}>{props.children}</div>
         {last}
