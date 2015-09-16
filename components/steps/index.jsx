78d15aabbf928a65f495aa963b4ef2666aaf91fb
afc163 <afc163@gmail.com>
2015-09-16 18:23:34 +0800

    Fix initial maxDescriptionWidth for vertical steps

--- components/steps/index.jsx
+++ components/steps/index.jsx
@@ -12,12 +12,16 @@ const AntSteps = React.createClass({
     };
   },
   render() {
+    let maxDescriptionWidth = this.props.maxDescriptionWidth;
+    if (this.props.direction === 'vertical') {
+      maxDescriptionWidth = 'auto';
+    }
     return (
       <Steps size={this.props.size}
              current={this.props.current}
              direction={this.props.direction}
              iconPrefix={this.props.iconPrefix}
-             maxDescriptionWidth={this.props.maxDescriptionWidth}
+             maxDescriptionWidth={maxDescriptionWidth}
              prefixCls={this.props.prefixCls}>
         {this.props.children}
       </Steps>
