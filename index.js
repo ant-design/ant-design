27ec5610598ce51d792f9adadde8eab727481e7c
SimaQ <sima.zhang1990@gmail.com>
2015-10-09 13:54:02 +0800

    feat: add form to antd.

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ const antd = {
   Icon: require('./components/iconfont'),
   Row: require('./components/layout').Row,
   Col: require('./components/layout').Col,
-  Spin: require('./components/spin')
+  Spin: require('./components/spin'),
+  Form: require('./components/form'),
 };
 
 antd.version = require('./package.json').version;
