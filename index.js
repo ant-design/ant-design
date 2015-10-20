59c36965ea837476d348229a681976067c5e83c3
SimaQ <sima.zhang1990@gmail.com>
2015-10-20 19:39:43 +0800

    fix: fix slider disabled style bug.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ require('./style/index.less');
 // matchMedia polyfill for
 // https://github.com/WickyNilliams/enquire.js/issues/82
 window.matchMedia = window.matchMedia || function () {
-    return {
-      matches: false,
-      addListener: function () {
-      },
-      removeListener: function () {
-      }
-    };
+  return {
+    matches: false,
+    addListener: function () {
+    },
+    removeListener: function () {
+    }
   };
+};
 
 const antd = {
   Affix: require('./components/affix'),
