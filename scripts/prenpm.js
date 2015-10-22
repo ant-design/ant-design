a687c32b1d11c98df36a279b5fc9d5c1ae70ab26
yiminghe <yiminghe@gmail.com>
2015-10-22 19:09:23 +0800

    add compiled css

--- scripts/prenpm.js
+++ scripts/prenpm.js
@@ -7,7 +7,10 @@ var originalIndex = fs.readFileSync(path.join(cwd, 'index.js'), 'utf-8');
 var newIndex = originalIndex
   .replace(/\/components\//g, '/')
   .replace("require('./package.json').version", "require('./version')")
+  .replace("require('./style/index.less')", "require('../style/index.less')");
 fs.writeFileSync(path.join(cwd, 'lib/index.js'), newIndex, 'utf-8');
 fs.writeFileSync(path.join(cwd, 'lib/version.js'), "module.exports = '" + require('../package.json').version + "';", 'utf-8');
-
+var antdCss = path.join(cwd, 'dist/index.css');
+fs.createReadStream(antdCss)
+  .pipe(fs.createWriteStream(path.join(cwd, 'lib/index.css')));
 console.log('prenpm done');
