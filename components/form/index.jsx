5bb37f7033a619932eed2bff9498c81e51abc14f
SimaQ <sima.zhang1990@gmail.com>
2015-10-25 11:34:31 +0800

    form 组建

--- components/form/index.jsx
+++ components/form/index.jsx
@@ -1,7 +1,10 @@
 import Form from './Form';
 import FormItem from './FormItem';
 import Input from './Input';
+import ValueMixin from './ValueMixin';
 
 Form.Item = FormItem;
 Form.Input = Input;
+Form.ValueMixin = ValueMixin;
+Form.InputGroup = Input.Group;
 export default Form;
