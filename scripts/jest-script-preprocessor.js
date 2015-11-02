2fa7f95cce78d39b056cf5eba5b19ae96f1caa14
afc163 <afc163@gmail.com>
2015-11-02 22:36:21 +0800

    basic test

--- scripts/jest-script-preprocessor.js
+++
@@ -1,9 +0,0 @@
-// jest-script-preprocessor.js
-var babelJest = require("babel-jest");
-
-module.exports = {
-  process: function(src, filename) {
-    return babelJest.process(src, filename)
-      .replace(/^require.*\.less.*;$/gm, '');
-  }
-};
--- tests/exports.js
+++
@@ -1,8 +0,0 @@
-jest.dontMock('../index');
-var antd = require('../index');
-
-describe('antd', function() {
-  it('antd should be existd', function() {
-     expect(antd).toBeTruthy();
-  });
-});
