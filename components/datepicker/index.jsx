c10e7e2a49d730e6d008b673c1311c18609d2db3
afc163 <afc163@gmail.com>
2015-11-19 17:20:00 +0800

    keep placeholder can be set

--- components/datepicker/index.jsx
+++ components/datepicker/index.jsx
@@ -13,6 +13,7 @@ function createPicker(TheCalendar) {
     getDefaultProps() {
       return {
         format: 'yyyy-MM-dd',
+        placeholder: '',
         transitionName: 'slide-up',
         popupStyle: {},
         onSelect: null, // 向前兼容
@@ -79,7 +80,10 @@ function createPicker(TheCalendar) {
       const timeValue = value ? new Date(value.getTime()) : null;
       // onSelect 为向前兼容.
       if (this.props.onSelect) {
-        require('util-deprecate')(this.props.onSelect, 'onSelect property of Datepicker is deprecated, use onChange instead')(timeValue);
+        require('util-deprecate')(
+          this.props.onSelect,
+          'onSelect property of Datepicker is deprecated, use onChange instead'
+        )(timeValue);
       }
       this.props.onChange(timeValue);
     },
@@ -89,12 +93,14 @@ function createPicker(TheCalendar) {
       // 否则会以周日开始排
       let defaultCalendarValue = new GregorianCalendar(this.getLocale());
       defaultCalendarValue.setTime(Date.now());
+
+      const placeholder = this.props.placeholder || this.getLocale().lang.placeholder;
       const calendar = (
         <TheCalendar
           disabledDate={this.props.disabledDate}
           locale={this.getLocale().lang}
           defaultValue={defaultCalendarValue}
-          dateInputPlaceholder={this.getLocale().lang.placeholder}
+          dateInputPlaceholder={placeholder}
           showTime={this.props.showTime}
           prefixCls="ant-calendar"
           showOk={this.props.showTime}
@@ -132,7 +138,7 @@ function createPicker(TheCalendar) {
                   <input disabled={this.props.disabled}
                          onChange={this.handleInputChange}
                          value={value && this.getFormatter().format(value)}
-                         placeholder={this.getLocale().lang.placeholder}
+                         placeholder={placeholder}
                          className={'ant-calendar-picker-input ant-input' + sizeClass}/>
                   <span className="ant-calendar-picker-icon"/>
                 </span>
