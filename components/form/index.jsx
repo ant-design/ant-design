7eb2f5b58a1c404ed7c3d78d5f8b386f6b075e64
afc163 <afc163@gmail.com>
2015-11-20 18:51:12 +0800

    Add support for degrade usage of form

--- components/form/index.jsx
+++ components/form/index.jsx
@@ -1,7 +1,15 @@
 import Form from './Form';
 import FormItem from './FormItem';
 import ValueMixin from './ValueMixin';
+import Input from '../input';
 
 Form.Item = FormItem;
 Form.ValueMixin = ValueMixin;
+
+// 对于 import { Form, Input } from 'antd/lib/form/';
+// 的方式做向下兼容
+// https://github.com/ant-design/ant-design/pull/566
+Form.Form = Form;
+Form.Input = Input;
+
 export default Form;
