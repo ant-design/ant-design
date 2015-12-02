ad1a97d0aa22143efd6581149c49a69887b4feee
jljsj <liangjun.jiang@alipay.com>
2015-12-02 18:06:16 +0800

    add tabs default animation,remove Api animation

--- components/tabs/index.jsx
+++ components/tabs/index.jsx
@@ -5,19 +5,22 @@ const prefixCls = 'ant-tabs';
 class AntTabs extends React.Component {
   render() {
     let className = (this.props.className || '');
+    let animation = this.props.animation;
     if (this.props.size === 'small' || this.props.size === 'mini') {
       className += ' ' + prefixCls + '-mini';
     }
     if (this.props.tabPosition === 'left' || this.props.tabPosition === 'right') {
       className += ' ' + prefixCls + '-vertical';
+      animation = null;
     }
-    return <Tabs {...this.props} className={className} />;
+    return <Tabs {...this.props} className={className} animation={animation}/>;
   }
 }
 
 AntTabs.defaultProps = {
   prefixCls: prefixCls,
-  size: 'default'
+  size: 'default',
+  animation: 'slide-horizontal',
 };
 
 AntTabs.TabPane = Tabs.TabPane;
--- components/tabs/index.md
+++ components/tabs/index.md
@@ -20,7 +20,6 @@
 | 参数             | 说明                                         | 类型     | 默认值                          |
 |------------------|----------------------------------------------|----------|---------------------------------|
 | activeKey        | 当前激活 tab 面板的 key                      | String   | 无                              |
-| animation        | 是否启用面板切换动画                         | String   | 空。目前只支持 slide-horizontal |
 | defaultActiveKey | 初始化选中面板的 key，如果没有设置 activeKey | String   | 第一个面板                      |
 | onChange         | 切换面板的回调                               | Function | 无                              |
 | onTabClick       | tab 被点击的回调                             | Function | 无                              |
