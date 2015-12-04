948120e224b56bfbd8e618ff780c567354f62267
Yuwei Ba <i@xiaoba.me>
2015-12-04 14:19:06 +0800

    update simple pagination style

--- components/pagination/index.jsx
+++ components/pagination/index.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import Pagination from 'rc-pagination';
-import Select from 'rc-select';
+import Select from '../select';
 import zhCN from './locale/zh_CN';
 
 const prefixCls = 'ant-pagination';
 
+const MiniSelect = React.createClass({
+  render() {
+    return <Select size="small" {...this.props} />;
+  }
+});
+
+MiniSelect.Option = Select.Option;
+
 class AntPagination extends React.Component {
   render() {
     let className = this.props.className;
+
+    if (this.props.simple) {
+      // hijiking simple pagination
+      let newProps = {};
+      for (let key in this.props) {
+        if (this.props.hasOwnProperty(key)) {
+          newProps[key] = this.props[key];
+        }
+      }
+
+      delete newProps.simple;
+
+      className += ' mini';
+
+      return <Pagination selectComponentClass={MiniSelect}
+        selectPrefixCls="ant-select"
+        prefixCls={prefixCls}
+        showSizeChanger
+        showQuickJumper
+        {...newProps} className={className}/>;
+    }
+
     if (this.props.size === 'small') {
       className += ' mini';
     }
+
     return <Pagination selectComponentClass={Select}
                        selectPrefixCls="ant-select"
                        prefixCls={prefixCls}
@@ -20,6 +51,7 @@ class AntPagination extends React.Component {
 
 AntPagination.defaultProps = {
   locale: zhCN,
+  className: '',
 };
 
 export default AntPagination;
