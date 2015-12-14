6f04d7c1a7199895b0607537128ff1eb54bfb445
afc163 <afc163@gmail.com>
2015-12-14 16:30:15 +0800

    Add type=card tabs

--- components/tabs/index.jsx
+++ components/tabs/index.jsx
@@ -1,26 +1,29 @@
 import Tabs from 'rc-tabs';
 import React from 'react';
-const prefixCls = 'ant-tabs';
+import classNames from 'classnames';
 
 class AntTabs extends React.Component {
   render() {
-    let className = (this.props.className || '');
-    let animation = this.props.animation;
-    if (this.props.size === 'small' || this.props.size === 'mini') {
-      className += ' ' + prefixCls + '-mini';
-    }
-    if (this.props.tabPosition === 'left' || this.props.tabPosition === 'right') {
-      className += ' ' + prefixCls + '-vertical';
+    let { prefixCls, size, tabPosition, animation, type } = this.props;
+    let className = classNames({
+      [this.props.className]: !!this. props.className,
+      [prefixCls + '-mini']: size === 'small' || size === 'mini',
+      [prefixCls + '-vertical']: tabPosition === 'left' || tabPosition === 'right',
+      [prefixCls + '-' + type]: true,
+    });
+    if (tabPosition === 'left' || tabPosition === 'right' || type === 'card') {
       animation = null;
     }
-    return <Tabs {...this.props} className={className} animation={animation}/>;
+    return <Tabs {...this.props} className={className} animation={animation} />;
   }
 }
 
 AntTabs.defaultProps = {
-  prefixCls: prefixCls,
+  prefixCls: 'ant-tabs',
   size: 'default',
   animation: 'slide-horizontal',
+  type: 'line', // or 'card',
+  closable: false,
 };
 
 AntTabs.TabPane = Tabs.TabPane;
