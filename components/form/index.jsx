e586b78705bd35261537f5dd1ec08ee5de28013a
Benjy Cui <benjytrys@gmail.com>
2016-02-04 11:40:40 +0800

    feat: scroll to first invalid field automatically #993

--- components/form/index.jsx
+++ components/form/index.jsx
@@ -1,14 +1,54 @@
+import ReactDOM from 'react-dom';
 import Form from './Form';
 import FormItem from './FormItem';
 import ValueMixin from './ValueMixin';
 import Input from '../input';
 import { createForm } from 'rc-form';
+import scrollIntoView from 'dom-scroll-into-view';
+
+function wrapValidateFields(validateFields) {
+  return (...args) => {
+    const originalCallback = args[args.length - 1];
+
+    function newCb(error, values) {
+      if (error) {
+        for (const name in error) {
+          if (error.hasOwnProperty(name) && error[name].instance) {
+            scrollIntoView(ReactDOM.findDOMNode(error[name].instance), window, {
+              onlyScrollIfNeeded: true,
+            });
+            break;
+          }
+        }
+      }
+      if (typeof originalCallback === 'function') {
+        originalCallback(error, values);
+      }
+    }
+
+    if (typeof originalCallback === 'function') {
+      args[args.length - 1] = newCb;
+    } else {
+      args.push(newCb);
+    }
+    return validateFields.apply(null, args);
+  };
+}
 
 Form.create = (o = {}) => {
   const options = {
     ...o,
     fieldNameProp: 'id',
     fieldMetaProp: '__meta',
+    refComponent: true,
+    mapProps(props) {
+      const form = props.form;
+      form.validateFields = wrapValidateFields(form.validateFields);
+      if (typeof o.mapProps === 'function') {
+        return o.mapProps(props);
+      }
+      return props;
+    },
   };
 
   return createForm(options);
