2e839a42fa3e88bbb69bf6bc6ab63721389231e4
afc163 <afc163@gmail.com>
2016-02-11 21:53:54 +0800

    Add react-transform-hmre

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
       loader: 'babel',
       query: {
         cacheDirectory: true,
-        presets: ['es2015', 'react', 'stage-0'],
+        presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
         plugins: ['add-module-exports']
       }
     }, {
