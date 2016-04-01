2ed55a490007cebe58da483de967fe50af7a3bc6
afc163 <afc163@gmail.com>
2016-04-01 14:29:11 +0800

    move deperated warning into componentDidMount

--- components/queue-anim/index.jsx
+++ components/queue-anim/index.jsx
@@ -3,10 +3,12 @@ import RcQueueAnim from 'rc-queue-anim';
 import warning from 'warning';
 
 export default class QueueAnim extends React.Component {
-  render() {
+  componentDidMount() {
     warning(false, '`QueueAnim` is deperated, ' +
       'you can import QueueAnim from \'rc-queue-anim\' directly.' +
       'The Demo will be moved to http://motion.ant.design/#/component/queue-anim');
+  }
+  render() {
     return <RcQueueAnim {...this.props} />;
   }
 }
