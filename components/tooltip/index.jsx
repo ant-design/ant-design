9be3a6e26cb2a88621baa00c4ea334c4892e2b3d
jljsj <liangjun.jiang@alipay.com>
2016-04-07 15:02:22 +0800

    add tooltip anim origin

--- components/tooltip/index.jsx
+++ components/tooltip/index.jsx
@@ -10,46 +10,61 @@ export default class Tooltip extends React.Component {
   static defaultProps = {
     prefixCls: 'ant-tooltip',
     placement: 'top',
+    transitionName: 'zoom-big',
     mouseEnterDelay: 0.1,
     mouseLeaveDelay: 0.1
   }
+
   constructor(props) {
     super(props);
     this.state = {
       visible: false
     };
   }
+
   onVisibleChange = (visible) => {
     this.setState({ visible });
   }
-  render() {
-    let transitionName = ({
-      top: 'zoom-down',
-      bottom: 'zoom-up',
-      left: 'zoom-right',
-      right: 'zoom-left',
-      topLeft: 'zoom-down',
-      bottomLeft: 'zoom-up',
-      leftTop: 'zoom-right',
-      rightTop: 'zoom-left',
-      topRight: 'zoom-down',
-      bottomRight: 'zoom-up',
-      leftBottom: 'zoom-right',
-      rightBottom: 'zoom-left',
-    })[this.props.placement];
 
+  onPopupAlign = (domNode, align) => {
+    // 当前返回的位置;
+    let placement;
+    Object.keys(placements).forEach(key => {
+      const item = placements[key];
+      if (item.points[0] === align.points[0] && item.points[1] === align.points[1]) {
+        placement = key;
+      }
+    });
+    // 当前坐标;
+    const rect = domNode.getBoundingClientRect();
+    let top = '50%';
+    let left = '50%';
+    if (placement.indexOf('top') >= 0 || placement.indexOf('Bottom') >= 0) {
+      top = `${rect.height - align.offset[1]}px`;
+    } else if (placement.indexOf('Top') >= 0 || placement.indexOf('bottom') >= 0) {
+      top = `${-align.offset[1]}px`;
+    }
+    if (placement.indexOf('left') >= 0 || placement.indexOf('Right') >= 0) {
+      left = `${rect.width - align.offset[0]}px`;
+    } else if (placement.indexOf('right') >= 0 || placement.indexOf('Left') >= 0) {
+      left = `${-align.offset[0]}px`;
+    }
+    domNode.style.transformOrigin = `${left} ${top}`;
+  };
+
+  render() {
     // Hide tooltip when there is no title
     let visible = this.state.visible;
-    if (!this.props.title) {
+    if (!this.props.title && !this.props.overlay) {
       visible = false;
     }
-
     return (
-      <RcTooltip transitionName={transitionName}
+      <RcTooltip transitionName={this.props.transitionName}
         builtinPlacements={placements}
         overlay={this.props.title}
         visible={visible}
         onVisibleChange={this.onVisibleChange}
+        onPopupAlign={this.onPopupAlign}
         {...this.props}>
         {this.props.children}
       </RcTooltip>
