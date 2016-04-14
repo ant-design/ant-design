9af3e4fc554072ef297ebae3b107334636c07dd5
plandem <plandem@gmail.com>
2016-04-14 07:57:59 +0400

    ru locale (#1411)

---
+++ components/locale-provider/ru_RU.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Andrey Gayvoronsky on 13/04/16.
+ */
+
+export default {
+  Pagination: require('rc-pagination/lib/locale/ru_RU'),
+  DatePicker: require('../date-picker/locale/ru_RU'),
+  TimePicker: require('../time-picker/locale/ru_RU'),
+  Calendar: require('../calendar/locale/ru_RU'),
+  Table: {
+    filterTitle: 'Фильтр',
+    filterConfirm: 'OK',
+    filterReset: 'Сбросить',
+    emptyText: 'Нет данных',
+  },
+  Modal: {
+    okText: 'OK',
+    cancelText: 'Отмена',
+    justOkText: 'OK',
+  },
+  Popconfirm: {
+    okText: 'OK',
+    cancelText: 'Отмена',
+  },
+  Transfer: {
+    notFoundContent: 'Not Found',
+    searchPlaceholder: 'Search here',
+    itemUnit: 'item',
+    itemsUnit: 'items',
+  },
+};
+
