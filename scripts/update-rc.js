e75355e860b4983d3e11b08b3325e2e0f9856c61
yiminghe <yiminghe@gmail.com>
2016-04-27 16:38:48 +0800

    update scripts

--- scripts/update-rc.js
+++ scripts/update-rc.js
@@ -1,21 +1,8 @@
 #!/usr/bin/env node
+
 /* eslint strict:0, camelcase:0 */
 'use strict';
 
-const child_process = require('child_process');
-const execSync = child_process.execSync;
-const runCmd = require('antd-tools/lib/runCmd');
-execSync('rm -rf node_modules/rc-*');
-const packageJson = require('../package.json');
-const deps = packageJson.dependencies;
-const savePrefix = execSync('tnpm config get save-prefix').toString().trim();
-execSync('tnpm config set save-prefix \'~\'');
-let cmd = [];
-for (const name in deps) {
-  if (name.match(/^rc-/)) {
-    cmd.push(`${name}@latest`);
-  }
-}
-runCmd('tnpm', ['i'].concat(cmd).concat('--save'), () => {
-  execSync(`tnpm config set save-prefix '${savePrefix}'`);
+require('antd-tools/lib/updateComponents')((name) => {
+  return !!name.match(/^rc-/);
 });
