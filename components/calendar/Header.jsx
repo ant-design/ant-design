48f74fd5bda4e73d1bb3a6766eb417aa7410c540
afc163 <afc163@gmail.com>
2016-04-27 11:23:18 +0800

    fix missing Option (#1521)

--- components/calendar/Header.jsx
+++ components/calendar/Header.jsx
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { PREFIX_CLS } from './Constants';
 import Select from '../select';
 import { Group, Button } from '../radio';
+const Option = Select.Option;
 
 function noop() {}
 
