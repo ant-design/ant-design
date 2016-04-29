54045fc7c2db0ed6ebe5524f704acedb5811c55e
afc163 <afc163@gmail.com>
2016-04-29 15:31:07 +0800

    components.less for custom less theme

--- scripts/prepub.js
+++ scripts/prepub.js
@@ -6,15 +6,22 @@
 // Build a entry less file to dist/antd.less
 var fs = require('fs');
 var path = require('path');
-var componentsPath = path.join(process.cwd(), 'components');
-var entryLessContent = `@import "../lib/style/index.less";`;
 console.log('Building a entry less file to dist/antd.less');
+var componentsPath = path.join(process.cwd(), 'components');
+var componentsLessContent = '';
 
+// Build components in one file: lib/style/components.less
 fs.readdir(componentsPath, function(err, files) {
   files.forEach(function(file) {
     if (fs.existsSync(path.join(componentsPath, file, 'style', 'index.less'))) {
-      entryLessContent += `\n@import "../lib/${path.join(file, 'style', 'index.less')}";`
+      componentsLessContent += `@import "../${path.join(file, 'style', 'index.less')}";\n`
     }
   });
-  fs.writeFileSync(path.join(process.cwd(), 'dist', 'antd.less'), entryLessContent);
+  fs.writeFileSync(path.join(process.cwd(), 'lib', 'style', 'components.less'), componentsLessContent);
+
+  // Build less entry file: dist/antd.less
+  fs.writeFileSync(
+    path.join(process.cwd(), 'dist', 'antd.less'),
+    '@import "../lib/style/index.less";\n@import "../lib/style/components.less";'
+  );
 });
