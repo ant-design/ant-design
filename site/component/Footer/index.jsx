ef8852bc2741b3ac9992224e3bd4a4ee328a786c
afc163 <afc163@gmail.com>
2016-05-12 17:44:36 +0800

    Fix footer links

--- site/component/Footer/index.jsx
+++ site/component/Footer/index.jsx
@@ -26,35 +26,42 @@ export default class Footer extends React.Component {
         <ul>
           <li>
             <h2>GitHub</h2>
-            <a target="_blank " href="https://github.com/ant-design/ant-design">
-              仓库
-            </a>
-            <a target="_blank" href="https://github.com/ant-design/ant-design/tree/master/style">
-              样式
-            </a>
-            <a target="_blank" href="https://github.com/ant-design/antd-bin">
-              开发工具
-            </a>
+            <div>
+              <a target="_blank " href="https://github.com/ant-design/ant-design">仓库</a>
+            </div>
+            <div>
+              <a target="_blank" href="https://github.com/ant-design/antd-init">antd-init</a> - 脚手架
+            </div>
+            <div>
+              <a target="_blank" href="http://ant-tool.github.io">ant-tool</a> - 开发工具
+            </div>
           </li>
           <li>
-            <h2>关于我们</h2>
-            <a href="https://github.com/ant-ued/blog/issues">团队博客 - Ant UED</a>
+            <h2>相关站点</h2>
+            <div><a href="http://motion.ant.design">Ant Motion</a> - 设计动效</div>
+            <div><a href="http://motion.ant.design">Ant UX</a> - 页面逻辑素材</div>
           </li>
           <li>
             <h2>联系我们</h2>
-            <a target="_blank" href="https://github.com/ant-design/ant-design/issues">
-              反馈和建议
-            </a>
-            <a target="_blank" href="https://gitter.im/ant-design/ant-design">
-              讨论
-            </a>
-            <a target="_blank" href="https://github.com/ant-design/ant-design/issues/new">
-              报告 Bug
-            </a>
+            <div>
+              <a target="_blank" href="https://github.com/ant-design/ant-design/issues">
+                反馈和建议
+              </a>
+            </div>
+            <div>
+              <a target="_blank" href="https://gitter.im/ant-design/ant-design">
+                讨论
+              </a>
+            </div>
+            <div>
+              <a target="_blank" href="https://github.com/ant-design/ant-design/issues/new">
+                报告 Bug
+              </a>
+            </div>
           </li>
           <li>
-            <div>©2015 蚂蚁金服体验技术部出品</div>
-            <div>
+            <div>©2016 蚂蚁金服体验技术部出品</div>
+            <div style={{ marginTop: 10 }}>
               文档版本：
               <Select
                 size="small"
