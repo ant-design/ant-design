41df1a4ae8e098ffd396f1c6d2b2070a7de8d1f6
Benjy Cui <benjytrys@gmail.com>
2016-05-17 11:35:24 +0800

    site: fix

--- site/entry/index.jsx
+++ site/entry/index.jsx
@@ -52,9 +52,32 @@ if (!location.port) {
   /* eslint-enable */
 }
 
-const isZhCN =
-        (typeof localStorage !== 'undefined' && localStorage.getItem('locale') === 'zh-CN') ||
-        (navigator.language === 'zh-CN');
+// Polyfill
+const areIntlLocalesSupported = require('intl-locales-supported');
+const localesMyAppSupports = ['zh-CN', 'en-US'];
+
+if (global.Intl) {
+    // Determine if the built-in `Intl` has the locale data we need.
+  if (!areIntlLocalesSupported(localesMyAppSupports)) {
+    // `Intl` exists, but it doesn't have the data we need, so load the
+    // polyfill and patch the constructors we need with the polyfill's.
+    /* eslint-disable global-require */
+    const IntlPolyfill = require('intl');
+    /* eslint-enable global-require */
+    Intl.NumberFormat = IntlPolyfill.NumberFormat;
+    Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+  }
+} else {
+  // No `Intl`, so use and load the polyfill.
+  /* eslint-disable global-require */
+  global.Intl = require('intl');
+  /* eslint-enable global-require */
+}
+
+
+const isZhCN = (typeof localStorage !== 'undefined' && localStorage.getItem('locale') !== 'en-US');
+        // (typeof localStorage !== 'undefined' && localStorage.getItem('locale') === 'zh-CN') ||
+        // (navigator.language === 'zh-CN');
 const appLocale = isZhCN ? cnLocale : enLocale;
 addLocaleData(appLocale.data);
 
