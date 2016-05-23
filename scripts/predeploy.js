e185441b1366ecf78faa45b5d891ae52f5e2a2b9
afc163 <afc163@gmail.com>
2016-05-23 19:32:28 +0800

    fix #1685

---
+++ scripts/predeploy.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+'use strict';
+
+// Copy CNAME and components into _site before site deploy
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+if(fs.existsSync(path.join(process.cwd(), '_site'))) {
+  execSync('cp -rf CNAME components _site');
+}
