8dff727a2773c502e3867f6772257b3d993a30f7
yiminghe <yiminghe@gmail.com>
2016-05-31 15:08:21 +0800

    simplify index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import {
 } from 'antd';
 
 // copy from above
-const antd = {
+export {
   Rate, Affix, DatePicker, Tooltip, Tag, Carousel, Tabs, Modal, Dropdown, Progress, Popover, Select,
   Breadcrumb, Popconfirm, Pagination, Steps, InputNumber, Switch, Checkbox, Table, Collapse, message,
   Slider, QueueAnim, Radio, notification, Alert, Validation, Tree, TreeSelect, Upload,
@@ -24,5 +24,3 @@ if (process.env.NODE_ENV !== 'production') {
 please use https://github.com/ant-design/babel-plugin-antd to reduce app bundle size.`);
   }
 }
-
-export default antd;
