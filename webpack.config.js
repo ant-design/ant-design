e051e8ae32a61e6174d76795fedb7cc3bc21b145
ddcat1115 <ddcat1115@gmail.com>
2016-06-06 15:12:38 +0800

    format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
 module.exports = function (webpackConfig) {
   webpackConfig = getWebpackConfig(webpackConfig);
   if (process.env.RUN_ENV === 'PRODUCTION') {
-	// Fix ie8 compatibility
+    // Fix ie8 compatibility
     webpackConfig[0].module.loaders.unshift({
       test: /\.jsx?$/,
       loader: 'es3ify-loader',
