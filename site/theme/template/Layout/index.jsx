6392c9f705b1905f6f82ae3a5fa21a083a34afc0
afc163 <afc163@gmail.com>
2016-06-12 21:34:37 +0800

    fix ga

--- site/theme/template/Layout/index.jsx
+++ site/theme/template/Layout/index.jsx
@@ -13,18 +13,6 @@ window.react = React;
 window['react-dom'] = ReactDOM;
 window.antd = antd;
 
-// Enable Google Analytics
-if (!location.port) {
-  /* eslint-disable */
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-72788897-1', 'auto');
-  ga('send', 'pageview');
-  /* eslint-enable */
-}
-
 // Polyfill
 const areIntlLocalesSupported = require('intl-locales-supported');
 const localesMyAppSupports = ['zh-CN', 'en-US'];
