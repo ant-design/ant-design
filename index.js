f844bc32ee237d16070f16954664cf473a5a5d54
yiminghe <yiminghe@gmail.com>
2016-06-22 13:31:04 +0800

    fix dist

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function camelCase(name) {
 const req = require.context('./components', true, /^\.\/[^_][\w-]+\/(style\/)?index\.tsx?$/);
 
 req.keys().forEach((mod) => {
-  const v = req(mod);
+  let v = req(mod);
+  if (v && v.default) {
+    v = v.default;
+  }
   const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
   if (match && match[1]) {
     if (match[1] === 'message' || match[1] === 'notification') {
