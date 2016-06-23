c2942fa5308e82b3a2778af5d794752ec19681e9
afc163 <afc163@gmail.com>
2016-06-23 16:00:30 +0800

    Fix stopImmediatePropagation in IE8, close #2154

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -14,7 +14,9 @@ function noop() {
 
 function stopPropagation(e) {
   e.stopPropagation();
-  e.nativeEvent.stopImmediatePropagation();
+  if (e.nativeEvent.stopImmediatePropagation) {
+    e.nativeEvent.stopImmediatePropagation();
+  }
 }
 
 const defaultLocale = {
