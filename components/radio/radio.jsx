0eaf9406721a2df1a1f69cb1fb7ce7e1d333623b
afc163 <afc163@gmail.com>
2016-06-27 17:22:03 +0800

    Fix Radio & Checkbox margin again

--- components/radio/radio.jsx
+++ components/radio/radio.jsx
@@ -12,15 +12,20 @@ export default class Radio extends React.Component {
   }
   render() {
     const { prefixCls, children, checked, disabled, className, style } = this.props;
+    const wrapperClassString = classNames({
+      [`${prefixCls}-wrapper`]: true,
+      [`${prefixCls}-wrapper-checked`]: checked,
+      [`${prefixCls}-wrapper-disabled`]: disabled,
+      [className]: !!className,
+    });
     const classString = classNames({
       [`${prefixCls}`]: true,
       [`${prefixCls}-checked`]: checked,
       [`${prefixCls}-disabled`]: disabled,
-      [className]: !!className,
     });
     return (
-      <label className={classString} style={style}>
-        <RcRadio {...this.props} style={null} children={null} />
+      <label className={wrapperClassString} style={style}>
+        <RcRadio {...this.props} className={classString} style={null} children={null} />
         {children ? <span>{children}</span> : null}
       </label>
     );
