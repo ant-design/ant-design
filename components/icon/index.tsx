9bf8c81ec870d62dfed213af88dc1b70ab9ef027
afc163 <afc163@gmail.com>
2016-06-28 22:22:42 +0800

    fix icon.tsx

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import splitObject from '../_util/splitObject';
+import * as React from 'react';
 
 export default props => {
-  const [{type, className = ''}, others] = splitObject(props,
-    ['type','className']);
-
-  let className2 = `${className} anticon anticon-${type}`.trim();
-  return <i className={className2} {...others} />;
+  const { type, className = '' } = props;
+  return <i {...props} className={`${className} anticon anticon-${type}`.trim()} />
 };
