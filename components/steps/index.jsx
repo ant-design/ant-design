4b4a55b00385fbc7066eaa8b5105c39ce443a0b2
afc163 <afc163@gmail.com>
2016-07-07 15:42:00 +0800

    remove unused code

--- components/steps/index.jsx
+++ components/steps/index.jsx
@@ -7,17 +7,12 @@ export default class Steps extends React.Component {
   static defaultProps = {
     prefixCls: 'ant-steps',
     iconPrefix: 'ant',
-    maxDescriptionWidth: 100,
     current: 0,
   }
 
   render() {
-    let maxDescriptionWidth = this.props.maxDescriptionWidth;
-    if (this.props.direction === 'vertical') {
-      maxDescriptionWidth = 'auto';
-    }
     return (
-      <RcSteps {...this.props} maxDescriptionWidth={maxDescriptionWidth} />
+      <RcSteps {...this.props} />
     );
   }
 }
