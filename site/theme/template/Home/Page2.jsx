2a64384852a55e4f8ba47ba55e9623a51b1ed898
jiang <155259966@qq.com>
2016-07-08 17:33:25 +0800

    update rc-scroll-anim (#2322)

--- site/theme/template/Home/Page2.jsx
+++ site/theme/template/Home/Page2.jsx
@@ -7,10 +7,12 @@ import QueueAnim from 'rc-queue-anim';
 
 export default function Page2() {
   return (
-    <ScrollOverPack scrollName="page2" className="content-wrapper page" playScale={1} replay>
-      <QueueAnim className="text-wrapper left-text" delay={300} key="text" duration={550} type="bottom"
-        leaveReverse
-        hideProps={{ child: null }}
+    <ScrollOverPack scrollName="page2"
+      className="content-wrapper page" playScale={1} replay
+      hideProps={{ image: { reverse: true } }}
+    >
+      <QueueAnim className="text-wrapper left-text" delay={300} key="text"
+        duration={550} type="bottom" leaveReverse
       >
         <h2 key="h2">设计模式</h2>
         <p key="p" style={{ maxWidth: 260 }}>总结中后台设计中反复出现的问题，并提供相应的解决方案。</p>
@@ -23,8 +25,9 @@ export default function Page2() {
           </Link>
         </div>
       </QueueAnim>
-      <TweenOne key="image" className="image2 image-wrapper" animation={{ x: 0, opacity: 1, delay: 300, duration: 550 }}
-        style={{ transform: 'translateX(100px)', opacity: 0 }} hideProps={{ reverse: true }}
+      <TweenOne key="image" className="image2 image-wrapper"
+        animation={{ x: 0, opacity: 1, delay: 300, duration: 550 }}
+        style={{ transform: 'translateX(100px)', opacity: 0 }}
       />
     </ScrollOverPack>
   );
