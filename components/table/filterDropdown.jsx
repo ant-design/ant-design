626607f7fd8a6e9ac2ef7512eb71da9356bc2249
Benjy Cui <benjycui@users.noreply.github.com>
2016-07-27 13:39:51 +0800

    Revert "Should not hide filter dropdown when select item"

--- components/table/filterDropdown.jsx
+++ components/table/filterDropdown.jsx
@@ -48,13 +48,10 @@ export default class FilterMenu extends React.Component {
     this.confirmFilter();
   }
 
-  onVisibleChange = (visible, e) => {
-    // Don't close menu when select menu item
-    if (e && e.type === 'click' &&
-        e.currentTarget.className === 'ant-table-filter-dropdown') {
-      return;
-    }
-    this.setState({ visible });
+  onVisibleChange = (visible) => {
+    this.setState({
+      visible,
+    });
     if (!visible) {
       this.confirmFilter();
     }
