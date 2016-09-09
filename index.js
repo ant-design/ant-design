bb2d4f6b927c1d0afeb6b67c4c164945bafce34e
yiminghe <yiminghe@gmail.com>
2016-09-09 15:41:15 +0800

    fix index

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 import warn from 'rc-util/lib/warn';
 import * as antd from './components';
 
+const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
+
+req.keys().forEach(req);
+
 warn('You are using prebuilt antd, ' +
      'please use https://github.com/ant-design/babel-plugin-antd to reduce app bundle size.');
 
