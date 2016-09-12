59834377a6fd169075a630a3178d868012be0193
Benjy Cui <benjytrys@gmail.com>
2016-09-12 17:53:17 +0800

    fix: backward compatibility

--- components/style/index.tsx
+++ components/style/index.tsx
@@ -1,1 +1,17 @@
 import './index.less';
+
+function isFlexSupported(style) {
+  return 'flex' in style ||
+    'webkitFlex' in style ||
+    'MozFlex' in style;
+}
+
+if (typeof window !== undefined && window.document && window.document.documentElement) {
+  const { documentElement } = window.document;
+  const NO_FLEX = 'no-flex';
+
+  if (!isFlexSupported(documentElement.style) &&
+      documentElement.className.indexOf(NO_FLEX) === -1) {
+    documentElement.className += ` ${NO_FLEX}`;
+  }
+}
