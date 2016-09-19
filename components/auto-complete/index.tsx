370a6bde528daa7afa17d5b678df63eac56af7f8
Benjy Cui <benjytrys@gmail.com>
2016-09-19 10:22:52 +0800

    fix: should export type

--- components/auto-complete/index.tsx
+++ components/auto-complete/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Select, { Option, OptGroup } from '../select';
 import classNames from 'classnames';
 
-interface SelectedValue {
+export interface SelectedValue {
   key: string;
   label: React.ReactNode;
 }
