2c45e8ab35ca34d58f75642db3a7a0cc7576330e
Benjy Cui <benjytrys@gmail.com>
2016-09-21 17:29:04 +0800

    chore: add 1x.ant.design

--- site/theme/index.js
+++ site/theme/index.js
@@ -19,6 +19,7 @@ module.exports = {
     '0.10.x': 'http://010x.ant.design/',
     '0.11.x': 'http://011x.ant.design/',
     '0.12.x': 'http://012x.ant.design/',
+    '1.x': 'http://1x.ant.design/',
   },
   routes: {
     path: '/',
