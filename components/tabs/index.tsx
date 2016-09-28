37d5c24da8c8d141526d1f172ff3d14e7ce166d5
infeng <fzhihao@outlook.com>
2016-09-28 14:47:36 +0800

    complete declaration in Tabs and Select

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -29,12 +29,12 @@ export interface TabsProps {
 // Tabs
 export interface TabPaneProps {
   /** 选项卡头显示文字 */
-  tab: React.ReactNode | string;
+  tab?: React.ReactNode | string;
   style?: React.CSSProperties;
 }
 
 export default class Tabs extends React.Component<TabsProps, any> {
-  static TabPane: TabPaneProps = TabPane;
+  static TabPane = TabPane as React.ClassicComponentClass<TabPaneProps>;
 
   static defaultProps = {
     prefixCls: 'ant-tabs',
