6edac76abbd2e6d7fdee7a8e63c6e935367e1074
afc163 <afc163@gmail.com>
2016-10-07 18:00:40 +0800

    prolong delay animation click time

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -103,10 +103,10 @@ export default class Modal extends React.Component<ModalProps, any> {
         x: e.pageX,
         y: e.pageY,
       };
-      // 20ms 内发生过点击事件，则从点击位置动画展示
+      // 100ms 内发生过点击事件，则从点击位置动画展示
       // 否则直接 zoom 展示
       // 这样可以兼容非点击方式展开
-      setTimeout(() => mousePosition = null, 20);
+      setTimeout(() => mousePosition = null, 100);
     });
     mousePositionEventBinded = true;
   }
