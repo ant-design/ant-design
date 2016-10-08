d063ed4dca501efcd0c313eab385d84015e83cd2
afc163 <afc163@gmail.com>
2016-10-08 16:12:19 +0800

    not leave require locale in dist files, close #3204

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const webpack = require('webpack');
 const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
+
+function ignoreMomentLocale(webpackConfig) {
+  delete webpackConfig.module.noParse;
+  webpackConfig.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+}
 
 module.exports = function (webpackConfig) {
   webpackConfig = getWebpackConfig(webpackConfig);
@@ -8,6 +14,11 @@ module.exports = function (webpackConfig) {
       test: /\.(tsx|jsx?)$/,
       loader: 'es3ify-loader',
     });
+    // noParse still leave `require('./locale' + name)` in dist files
+    // ignore is better
+    // http://stackoverflow.com/q/25384360
+    ignoreMomentLocale(webpackConfig[0]);
+    ignoreMomentLocale(webpackConfig[1]);
   }
   return webpackConfig;
 };
