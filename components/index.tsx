47a1ec4efe9317904e166bd19f66079d85930545
afc163 <afc163@gmail.com>
2016-10-12 15:56:43 +0800

    keep index.js as entry

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,12 @@
+/* eslint no-console:0 */
+// this file is not used if use https://github.com/ant-design/babel-plugin-import
+if (process.env.NODE_ENV !== 'production') {
+  if (typeof console !== 'undefined' && console.warn) {
+    console.warn(`You are using prebuilt antd,
+please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.`);
+  }
+}
+
 export { default as Affix } from './affix';
 
 export { default as AutoComplete } from './auto-complete';
