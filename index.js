e02d77b3df954416d900a4f49d7a4912cd89af30
Meck <yesmeck@gmail.com>
2016-11-07 11:48:28 +0800

    Styles should be exported as well.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+/* eslint no-console:0 */
+// this file is not used if use https://github.com/ant-design/babel-plugin-import
+
+function camelCase(name) {
+  return name.charAt(0).toUpperCase() +
+    name.slice(1).replace(/-(\w)/g, (m, n) => {
+      return n.toUpperCase();
+    });
+}
+
+const req = require.context('./components', true, /^\.\/[^_][\w-]+\/(style\/)index\.tsx?$/);
+
+console.log(req.keys());
+
+req.keys().forEach((mod) => {
+  let v = req(mod);
+  if (v && v.default) {
+    v = v.default;
+  }
+  const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
+  if (match && match[1]) {
+    if (match[1] === 'message' || match[1] === 'notification') {
+      // message & notification should not be capitalized
+      exports[match[1]] = v;
+    } else {
+      exports[camelCase(match[1])] = v;
+    }
+  }
+});
+
 module.exports = require('./components');
