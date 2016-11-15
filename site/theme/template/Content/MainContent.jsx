dde040e3e8349606a43d0f4808e0a8cac7b9bf31
afc163 <afc163@gmail.com>
2016-11-15 15:10:01 +0800

    fix lints

--- site/theme/template/Content/MainContent.jsx
+++ site/theme/template/Content/MainContent.jsx
@@ -83,13 +83,15 @@ export default class MainContent extends React.Component {
             ];
     const disabled = item.disabled;
     const url = item.filename.replace(/(\/index)?((\.zh-CN)|(\.en-US))?\.md$/i, '').toLowerCase();
-    const child = !item.link ?
+    const child = !item.link ? (
       <Link to={{ query: this.props.location.query, pathname: /^components/.test(url) ? `${url}/` : url }} disabled={disabled}>
         {text}
-      </Link> :
+      </Link>
+    ) : (
       <a href={item.link} target="_blank" rel="noopener noreferrer" disabled={disabled}>
         {text}
-      </a>;
+      </a>
+    );
 
     return (
       <Menu.Item key={key.toLowerCase()} disabled={disabled}>
