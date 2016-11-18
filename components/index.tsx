08bbe78c08b398e4ed74d4d0cebb2fa51d752fbc
jiangmu <jiangmu@mogujie.com>
2016-11-18 23:34:35 +0800

    refactor: 调整env的判断方式，避免用户手动构建时将console提示打包进去

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,6 @@
 /* eslint no-console:0 */
 // this file is not used if use https://github.com/ant-design/babel-plugin-import
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   if (typeof console !== 'undefined' && console.warn) {
     console.warn(`You are using prebuilt antd,
 please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.`);
