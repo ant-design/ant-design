c5d9205a2b20cc1201d824dc97aae1269cacad46
afc163 <afc163@gmail.com>
2016-11-22 15:42:09 +0800

    fix node snap

--- tests/node/demo.test.js
+++ tests/node/demo.test.js
@@ -1,4 +1,4 @@
-import glob from 'glob'
+import glob from 'glob';
 import { render } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
 
@@ -10,13 +10,13 @@ const skip = [
   'breadcrumb/demo/router', // https://github.com/ReactTraining/react-router/blob/master/docs/guides/ServerRendering.md#history-singletons,
 ];
 
-files.forEach(file => {
+files.forEach((file) => {
   let testMethod = test;
   if (skip.some(c => file.includes(c))) {
     testMethod = test.skip;
   }
   testMethod(`renders ${file} correctly`, () => {
-    const demo = require('../.' + file);
+    const demo = require(`../.${file}`); // eslint-disable-line
     const wrapper = render(demo);
     expect(renderToJson(wrapper)).toMatchSnapshot();
   });
