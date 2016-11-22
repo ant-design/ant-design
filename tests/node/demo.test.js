57f5d299af033c3813e68414d949ba3012dd6615
Wei Zhu <yesmeck@gmail.com>
2016-11-22 14:28:05 +0800

    Check in snapshots

---
+++ tests/node/demo.test.js
@@ -0,0 +1,23 @@
+import glob from 'glob'
+import { render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+
+const files = glob.sync('./components/**/demo/*.md');
+
+const skip = [
+  'mention', // https://github.com/facebook/draft-js/issues/385
+  'menu', // https://github.com/react-component/menu/issues/63
+  'breadcrumb/demo/router', // https://github.com/ReactTraining/react-router/blob/master/docs/guides/ServerRendering.md#history-singletons,
+];
+
+files.forEach(file => {
+  let testMethod = test;
+  if (skip.some(c => file.includes(c))) {
+    testMethod = test.skip;
+  }
+  testMethod(`renders ${file} correctly`, () => {
+    const demo = require('../.' + file);
+    const wrapper = render(demo);
+    expect(renderToJson(wrapper)).toMatchSnapshot();
+  });
+});
