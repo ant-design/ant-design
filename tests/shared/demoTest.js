e089f3904883dd20044423ef404833757eaba3a5
afc163 <afc163@gmail.com>
2016-11-23 14:30:21 +0800

    fix ci timezone

--- tests/shared/demoTest.js
+++ tests/shared/demoTest.js
@@ -9,7 +9,7 @@ export default function demoTest(component, options = {}) {
 
   files.forEach(file => {
     testMethod(`renders ${file} correctly`, () => {
-      MockDate.set(1479772800000); // 2016-11-22
+      MockDate.set(1479772800000, new Date().getTimezoneOffset()); // 2016-11-22
       const demo = require('../.' + file);
       const wrapper = render(demo);
       expect(renderToJson(wrapper)).toMatchSnapshot();
