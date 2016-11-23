f6f63606eaf8f209cd56c42af452dd17eff46d5d
afc163 <afc163@gmail.com>
2016-11-23 12:13:36 +0800

    fix test snap via mocking Date.now

--- tests/shared/demoTest.js
+++ tests/shared/demoTest.js
@@ -1,6 +1,7 @@
 import glob from 'glob'
 import { render } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
+import MockDate from 'mockdate';
 
 export default function demoTest(component, options = {}) {
   const testMethod = options.skip ? test.skip : test;
@@ -8,9 +9,11 @@ export default function demoTest(component, options = {}) {
 
   files.forEach(file => {
     testMethod(`renders ${file} correctly`, () => {
+      MockDate.set(1479772800000); // 2016-11-22
       const demo = require('../.' + file);
       const wrapper = render(demo);
       expect(renderToJson(wrapper)).toMatchSnapshot();
+      MockDate.reset();
     });
   });
 }
