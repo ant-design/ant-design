0acc7d592cf5ed3f4608eff6ea556455e1309aa3
Wei Zhu <yesmeck@gmail.com>
2016-11-23 16:37:58 +0800

    Breadcrumb router demo test

--- tests/breadcrumb/demo.test.js
+++ tests/breadcrumb/demo.test.js
@@ -1,3 +1,12 @@
+import { render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
 import demoTest from '../shared/demoTest';
+import routerDemo from '../../components/breadcrumb/demo/router.md';
 
 demoTest('breadcrumb', { skip: [ 'router.md' ] });
+
+const testMethod = typeof window !== 'undefined' ? test : test.skip;
+testMethod('renders ./components/breadcrumb/demo/router.md correctly', () => {
+  const wrapper = render(routerDemo);
+  expect(renderToJson(wrapper)).toMatchSnapshot();
+});
