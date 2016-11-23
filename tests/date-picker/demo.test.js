5bd8977d75ba0de9eeb66a506904d7ed02364738
Wei Zhu <yesmeck@gmail.com>
2016-11-23 16:31:54 +0800

    Datepicker locale demo test

--- tests/date-picker/demo.test.js
+++ tests/date-picker/demo.test.js
@@ -1,3 +1,14 @@
+import { render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import MockDate from 'mockdate';
 import demoTest from '../shared/demoTest';
 
-demoTest('date-picker');
+demoTest('date-picker', { skip: [ 'locale.md' ] });
+
+test('renders ./components/date-picker/demo/locale.md correctly', () => {
+  MockDate.set(new Date('2016-11-22').getTime());
+  const LocaleDemo = require('../../components/date-picker/demo/locale');
+  const wrapper = render(LocaleDemo);
+  expect(renderToJson(wrapper)).toMatchSnapshot();
+  MockDate.reset();
+});
