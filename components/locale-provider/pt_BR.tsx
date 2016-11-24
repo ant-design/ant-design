db5c9f71e4e771a29ae37749909ece3ba33e00fb
Nathan Tavares Nascimento <nathan.tnascimento@gmail.com>
2016-11-24 19:56:02 -0200

    feature: add pt_BR locale support

---
+++ components/locale-provider/pt_BR.tsx
@@ -0,0 +1,39 @@
+import moment from 'moment';
+import 'moment/locale/pt-br';
+moment.locale('pt-br');
+
+import Pagination from '../pagination/locale/pt_BR';
+import DatePicker from '../date-picker/locale/pt_BR';
+import TimePicker from '../time-picker/locale/pt_BR';
+import Calendar from '../calendar/locale/pt_BR';
+
+export default {
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  Table: {
+    filterTitle: 'Filtro',
+    filterConfirm: 'OK',
+    filterReset: 'Resetar',
+    emptyText: 'Não há dados',
+  },
+  Modal: {
+    okText: 'OK',
+    cancelText: 'Cancelar',
+    justOkText: 'OK',
+  },
+  Popconfirm: {
+    okText: 'OK',
+    cancelText: 'Cancelar',
+  },
+  Transfer: {
+    notFoundContent: 'Não encontrado',
+    searchPlaceholder: 'Procurar',
+    itemUnit: 'item',
+    itemsUnit: 'items',
+  },
+  Select: {
+    notFoundContent: 'Não encontrado',
+  },
+};
---
+++ components/pagination/locale/pt_BR.tsx
@@ -0,0 +1,12 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ páginas',
+  jump_to: 'Vá até',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Página anterior',
+  next_page: 'Próxima página',
+  prev_5: '5 páginas anteriores',
+  next_5: '5 próximas páginas',
+};
