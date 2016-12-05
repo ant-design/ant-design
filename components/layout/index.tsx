33a7677429abf0eb8106f6c6c2e6b6931b89009b
偏右 <afc163@gmail.com>
2016-12-05 16:07:24 +0800

    Revert "new Layout Component" (#4131)

--- components/layout/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from './basic';
-import Sider from './Sider';
-
-Layout.Sider = Sider;
-export default Layout;
---
+++ tests/layout/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+let { Col, Row } = require('../../components/grid/index');
+
+describe('Grid', function() {
+  it('should render Col', () => {
+    const col = TestUtils.renderIntoDocument(
+      <Col span="2"></Col>
+    );
+    const colNode = TestUtils.findRenderedDOMComponentWithTag(col, 'DIV');
+    expect(colNode.className).toBe('ant-col-2');
+  });
+  it('should render Row', () => {
+    const row = TestUtils.renderIntoDocument(
+      <Row></Row>
+    );
+    const rowNode = TestUtils.findRenderedDOMComponentWithTag(row, 'DIV');
+    expect(rowNode.className).toBe('ant-row');
+  });
+});
