bcbdb43fdb4960c7f6dc7fbc6f4993f91ce15da5
Benjy Cui <benjytrys@gmail.com>
2017-01-04 10:08:35 +0800

    chore: update link to locale format

--- components/date-picker/locale/zh_CN.tsx
+++ components/date-picker/locale/zh_CN.tsx
@@ -20,6 +20,6 @@ const locale = {
 locale.lang.ok = '确 定';
 
 // All settings at:
-// https://github.com/ant-design/ant-design/issues/424
+// https://github.com/ant-design/ant-design/blob/master/components/date-picker/locale/example.json
 
 export default locale;
