0c39b958678fe4f82daac89e6561319ca422412d
Wei Zhu <yesmeck@gmail.com>
2017-01-06 12:57:52 +0800

    Cleanup npm scripts

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,31 +1,23 @@
-// Test dist files
 import fs from 'fs';
 import path from 'path';
 
 describe('antd dist files', () => {
-  const distFilesExisted = fs.existsSync(path.join(process.cwd(), 'dist', 'antd.js'));
-  if (!distFilesExisted) {
-    it('empty test case placeholder', () => {});
-    return;
-  }
-
-  const antd = require('../dist/antd'); // eslint-disable-line global-require
-  const antdInComponents = require('../components/'); // eslint-disable-line global-require
-
   // https://github.com/ant-design/ant-design/issues/1638
   // https://github.com/ant-design/ant-design/issues/1968
   it('exports modules correctly', () => {
+    const antd = process.env.CI ? require('../dist/antd') : require('../components'); // eslint-disable-line global-require
     expect(Object.keys(antd)).toMatchSnapshot();
-    expect(Object.keys(antdInComponents)).toMatchSnapshot();
   });
 
   // https://github.com/ant-design/ant-design/issues/1970
   // https://github.com/ant-design/ant-design/issues/1804
-  it('should be compatible in IE8', () => {
-    const antdJsContent = fs.readFileSync(path.join(process.cwd(), 'dist', 'antd.js'));
-    expect(
-      antdJsContent.toString()
-       .indexOf('function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }')
-    ).toBe(-1);
-  });
+  if (process.env.CI) {
+    it('should be compatible in IE8', () => {
+      const antdJsContent = fs.readFileSync(path.join(process.cwd(), 'dist', 'antd.js'));
+      expect(
+        antdJsContent.toString()
+        .indexOf('function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }')
+      ).toBe(-1);
+    });
+  }
 });
