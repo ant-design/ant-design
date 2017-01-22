ace9863f3d7a549a1474338ba1e2a1477ba1dea2
paranoidjk <hust2012jiangkai@gmail.com>
2017-01-22 17:46:08 +0800

    fix(transfer): checkall should response to filter result; fix #4654

--- components/transfer/__tests__/index.test.js
+++ components/transfer/__tests__/index.test.js
@@ -1,3 +1,4 @@
+/* eslint no-use-before-define: "off" */
 import React from 'react';
 import { render, mount } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
@@ -23,6 +24,50 @@ const listCommonProps = {
   }],
   selectedKeys: ['a'],
   targetKeys: ['b'],
+  lazy: false,
+};
+
+const searchTransferProps = {
+  dataSource: [
+    {
+      key: '0',
+      title: 'content1',
+      description: 'description of content1',
+      chosen: false,
+    },
+    {
+      key: '1',
+      title: 'content2',
+      description: 'description of content2',
+      chosen: false,
+    },
+    {
+      key: '2',
+      title: 'content3',
+      description: 'description of content3',
+      chosen: false,
+    },
+    {
+      key: '3',
+      title: 'content4',
+      description: 'description of content4',
+      chosen: false,
+    },
+    {
+      key: '4',
+      title: 'content5',
+      description: 'description of content5',
+      chosen: false,
+    },
+    {
+      key: '5',
+      title: 'content6',
+      description: 'description of content6',
+      chosen: false,
+    },
+  ],
+  selectedKeys: [],
+  targetKeys: ['3', '4'],
   lazy: false,
 };
 
@@ -81,5 +126,70 @@ describe('Transfer', () => {
     const wrapper = mount(<Transfer {...listCommonProps} showSearch filterOption={filterOption} />);
     wrapper.find(TransferSearch).at(0).find('input').simulate('change', { target: { value: 'a' } });
     expect(wrapper.find(TransferList).at(0).find(TransferItem).find(Checkbox)).toHaveLength(1);
+  });
+
+  it('should display the correct count of items when filter by input', () => {
+    const filterOption = (inputValue, option) => option.description.indexOf(inputValue) > -1;
+    const renderFunc = item => item.title;
+    const wrapper = mount(
+      <Transfer
+        {...searchTransferProps}
+        showSearch
+        filterOption={filterOption}
+        render={renderFunc}
+      />
+    );
+    wrapper.find(TransferSearch).at(0).find('input').simulate('change', { target: { value: 'content2' } });
+    expect(wrapper.find(TransferList).at(0).find('.ant-transfer-list-header-selected > span').at(0)
+      .first()
+      .text()
+      .trim()).toEqual('1');
+  });
+
+  it('should just check the filtered item when click on check all after search by input', () => {
+    const filterOption = (inputValue, option) => option.description.indexOf(inputValue) > -1;
+    const renderFunc = item => item.title;
+    const handleSelectChange = jest.fn();
+    const wrapper = mount(
+      <Transfer
+        {...searchTransferProps}
+        showSearch
+        filterOption={filterOption}
+        render={renderFunc}
+        onSelectChange={handleSelectChange}
+      />
+    );
+    wrapper.find(TransferSearch).at(0).find('input')
+      .simulate('change', { target: { value: 'content2' } });
+    wrapper.find(TransferList).at(0).find('.ant-transfer-list-header input[type="checkbox"]').filterWhere(n => !n.prop('checked'))
+      .simulate('change');
+    expect(handleSelectChange).toHaveBeenCalledWith(['1'], []);
+  });
+
+  it('should transfer just the filtered item after search by input', () => {
+    const filterOption = (inputValue, option) => option.description.indexOf(inputValue) > -1;
+    const renderFunc = item => item.title;
+    const handleChange = jest.fn();
+    const handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
+      wrapper.setProps({
+        selectedKeys: [...sourceSelectedKeys, ...targetSelectedKeys],
+      });
+    };
+    const wrapper = mount(
+      <Transfer
+        {...searchTransferProps}
+        showSearch
+        filterOption={filterOption}
+        render={renderFunc}
+        onSelectChange={handleSelectChange}
+        onChange={handleChange}
+      />
+    );
+    wrapper.find(TransferSearch).at(0).find('input')
+      .simulate('change', { target: { value: 'content2' } });
+    wrapper.find(TransferList).at(0).find('.ant-transfer-list-header input[type="checkbox"]').filterWhere(n => !n.prop('checked'))
+      .simulate('change');
+    wrapper.find(TransferOperation).find(Button).at(1).simulate('click');
+    expect(handleChange).toHaveBeenCalledWith(['1', '3', '4'], 'right', ['1']);
   });
 });
--- components/transfer/item.tsx
+++ components/transfer/item.tsx
@@ -5,38 +5,12 @@ import assign from 'object-assign';
 import Lazyload from 'react-lazy-load';
 import Checkbox from '../checkbox';
 
-function isRenderResultPlainObject(result) {
-  return result && !React.isValidElement(result) &&
-    Object.prototype.toString.call(result) === '[object Object]';
-}
-
 export default class Item extends React.Component<any, any> {
   shouldComponentUpdate(...args) {
     return PureRenderMixin.shouldComponentUpdate.apply(this, args);
   }
-  matchFilter = (text) => {
-    const { filter, filterOption, item } = this.props;
-    if (filterOption) {
-      return filterOption(filter, item);
-    }
-    return text.indexOf(filter) >= 0;
-  }
   render() {
-    const { render, filter, item, lazy, checked, prefixCls, onClick } = this.props;
-    const renderResult = render(item);
-    let renderedText;
-    let renderedEl;
-    if (isRenderResultPlainObject(renderResult)) {
-      renderedText = renderResult.value;
-      renderedEl = renderResult.label;
-    } else {
-      renderedText = renderResult;
-      renderedEl = renderResult;
-    }
-
-    if (filter && filter.trim() && !this.matchFilter(renderedText)) {
-      return null;
-    }
+    const { renderedText, renderedEl, item, lazy, checked, prefixCls, onClick } = this.props;
 
     const className = classNames({
       [`${prefixCls}-content-item`]: true,
