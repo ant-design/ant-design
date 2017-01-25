fd233cf430eb47c1940927beeb5ddaa94f108599
Simon Waloschek <sonovice@users.noreply.github.com>
2017-01-25 14:12:27 +0100

    Added German locale (#4686)
    * Added German locale
    
    * Added German locale for pagination

---
+++ components/locale-provider/de_DE.tsx
@@ -0,0 +1,39 @@
+import moment from 'moment';
+moment.locale('de');
+
+import Pagination from 'rc-pagination/lib/locale/de_DE';
+import DatePicker from '../date-picker/locale/de_DE';
+import TimePicker from '../time-picker/locale/de_DE';
+import Calendar from '../calendar/locale/de_DE';
+
+export default {
+  locale: 'de',
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  Table: {
+    filterTitle: 'Filter-Menü',
+    filterConfirm: 'OK',
+    filterReset: 'Zurücksetzen',
+    emptyText: 'Keine Daten',
+  },
+  Modal: {
+    okText: 'OK',
+    cancelText: 'Abbrechen',
+    justOkText: 'OK',
+  },
+  Popconfirm: {
+    okText: 'OK',
+    cancelText: 'Abbrechen',
+  },
+  Transfer: {
+    notFoundContent: 'Nicht gefunden',
+    searchPlaceholder: 'Suchen',
+    itemUnit: 'Eintrag',
+    itemsUnit: 'Einträge',
+  },
+  Select: {
+    notFoundContent: 'Nicht gefunden',
+  },
+};
