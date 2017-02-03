e1777a7b6b5038f3e6d36be23567ef67172a191b
yiminghe <yiminghe@gmail.com>
2017-02-03 19:53:04 +0800

    add version to dist

--- scripts/prepub.js
+++ scripts/prepub.js
@@ -12,6 +12,32 @@ if (fs.existsSync(path.join(__dirname, '../dist'))) {
   fs.writeFileSync(path.join(process.cwd(), 'lib', 'version.js'), `
 module.exports = '${packageInfo.version}';
 `);
+
+  const antdV = `
+  (function(root) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports.version = '${packageInfo.version}';
+	else
+		root["antd"].version = '${packageInfo.version}';
+})(this);
+`;
+
+  const antdPath = path.join(process.cwd(), 'dist', 'antd.js');
+  const antdMinPath = path.join(process.cwd(), 'dist', 'antd.min.js');
+
+  if (fs.existsSync(antdPath)) {
+    const content = fs.readFileSync(antdPath, 'utf-8');
+    const minContent = fs.readFileSync(antdMinPath, 'utf-8');
+    fs.writeFileSync(antdPath, `
+${content}
+${antdV}
+`);
+    fs.writeFileSync(antdMinPath, `
+${minContent}
+${antdV}
+`);
+  }
+
   console.log('Building a entry less file to dist/antd.less');
   const componentsPath = path.join(process.cwd(), 'components');
   let componentsLessContent = '';
