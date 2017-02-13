28961856aab21d26fac3f0f1fd9cecf702a1a2a6
corneyl <cornieljoosse@gmail.com>
2017-02-13 08:46:13 +0100

    feat: Added Dutch locale and bumped dep versions so they include Dutch locales (#4785)
    * Added Dutch locale and bumped dep versions so they include Dutch locales
    
    Bumped versions so they include Dutch locales
    - rc-calendar to ~7.6.2
    - rc-pagination to ~1.6.6
    
    * Added dutch locale to LocaleProvider test

--- components/locale-provider/__tests__/index.test.js
+++ components/locale-provider/__tests__/index.test.js
@@ -10,6 +10,7 @@ import esES from '../es_ES';
 import svSE from '../sv_SE';
 import frBE from '../fr_BE';
 import deDE from '../de_DE';
+import nlNL from '../nl_NL';
 
 const Option = Select.Option;
 const RangePicker = DatePicker.RangePicker;
@@ -55,7 +56,7 @@ const App = () => (
 
 describe('Locale Provider', () => {
   it('should display the text as locale changed', () => {
-    [enUS, ptBR, ruRU, esES, svSE, frBE, deDE].forEach((locale) => {
+    [enUS, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL].forEach((locale) => {
       const wrapper = mount(
         <LocaleProvider locale={locale}>
           <App />
