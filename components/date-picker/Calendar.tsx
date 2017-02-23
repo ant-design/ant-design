1c837963e2eda0166fc2c7b59e59d2169811d7a1
Benjy Cui <benjytrys@gmail.com>
2017-02-23 16:04:27 +0800

    chore: add warning for deprecated API

--- components/date-picker/Calendar.tsx
+++ components/date-picker/Calendar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CalendarLocale from 'rc-calendar/lib/locale/zh_CN';
 import RcCalendar from 'rc-calendar';
+import warning from 'warning';
 
 export default class Calendar extends React.Component<any, any> {
   static defaultProps = {
@@ -9,6 +10,7 @@ export default class Calendar extends React.Component<any, any> {
   };
 
   render() {
+    warning(false, 'DatePicker.Calendar is deprecated, use Calendar instead.');
     return <RcCalendar {...this.props} />;
   }
 }
