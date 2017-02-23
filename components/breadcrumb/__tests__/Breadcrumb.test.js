9c6a6d61758696e5f971fa19fc8f182005faa1dd
afc163 <afc163@gmail.com>
2017-02-23 13:43:50 +0800

    Fix Breadcrumb falsy children, close #5015

--- components/breadcrumb/__tests__/Breadcrumb.test.js
+++ components/breadcrumb/__tests__/Breadcrumb.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import Breadcrumb from '../Breadcrumb';
+import { mount, render } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
+import Breadcrumb from '../index';
 
 describe('Breadcrumb', () => {
   it('warns on non-Breadcrumb.Item children', () => {
@@ -17,5 +18,19 @@ describe('Breadcrumb', () => {
     expect(console.error.calls.argsFor(0)[0]).toContain(
       'Breadcrumb only accetps Breadcrumb.Item as it\'s children'
     );
+  });
+
+  // https://github.com/ant-design/ant-design/issues/5015
+  it('should allow Breadcrumb.Item is null or undefined', () => {
+    const wrapper = render(
+      <Breadcrumb>
+        {null}
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        {undefined}
+      </Breadcrumb>
+    );
+    // eslint-disable-next-line
+    expect(console.error.calls).toBe(undefined);
+    expect(renderToJson(wrapper)).toMatchSnapshot();
   });
 });
