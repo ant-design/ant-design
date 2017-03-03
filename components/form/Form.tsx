ab0f6f0441aae2e5866605228777b51bbfe3da2e
Guzhongren <Guzhongren@users.noreply.github.com>
2017-03-03 14:05:30 +0800

    from's value has changed, without event to process
    from's value has changed, without event to process, there is this prop in  previous version.

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -70,6 +70,8 @@ export type WrappedFormUtils = {
     rules?: Array<any>;
     /** 是否和其他控件互斥，特别用于 Radio 单选控件 */
     exclusive?: boolean;
+    /** 表单输入值改变事件 */
+    onChange?: (value: any) => void;
   }): (node: React.ReactNode) => React.ReactNode;
 }
 
