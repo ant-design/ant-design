189f8f5bea6940eab9ab8c6d137b147c6b0e436e
Madis Väin <madisvain@gmail.com>
2017-03-11 13:43:58 +0200

    add Estonian translations

---
+++ components/locale-provider/et_EE.tsx
@@ -0,0 +1,38 @@
+import moment from 'moment';
+moment.locale('et');
+
+export default {
+  locale: 'et',
+  Table: {
+    filterTitle: 'Filtri menüü',
+    filterConfirm: 'OK',
+    filterReset: 'Nulli',
+    emptyText: 'Andmed puuduvad',
+    selectAll: 'Vali kõik',
+    selectInvert: 'Inverteeri valik',
+  },
+  Modal: {
+    okText: 'OK',
+    cancelText: 'Tühista',
+    justOkText: 'OK',
+  },
+  Popconfirm: {
+    okText: 'OK',
+    cancelText: 'Tühista',
+  },
+  Transfer: {
+    notFoundContent: 'Ei leitud',
+    searchPlaceholder: 'Otsi siit',
+    itemUnit: 'kogus',
+    itemsUnit: 'kogus',
+  },
+  Select: {
+    notFoundContent: 'Ei leitud',
+  },
+  Upload: {
+    uploading: 'Üleslaadimine...',
+    removeFile: 'Eemalda fail',
+    uploadError: 'Üleslaadimise tõrge',
+    previewFile: 'Faili eelvaade',
+  },
+};
