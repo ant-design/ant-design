4273deb92d0c95a6e929d8e29cc1de408ce3c089
Benjy Cui <benjytrys@gmail.com>
2017-03-20 15:18:03 +0800

    fix: suppress warning while testing, close: #5345

--- components/index.tsx
+++ components/index.tsx
@@ -1,10 +1,13 @@
-/* eslint no-console:0 */
 // this file is not used if use https://github.com/ant-design/babel-plugin-import
-if (process.env.NODE_ENV !== 'production') {
-  if (typeof console !== 'undefined' && console.warn && typeof window !== 'undefined') {
-    console.warn(`You are using a whole package of antd,
-please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.`);
-  }
+import warning from './_util/warning';
+
+const ENV = process.env.NODE_ENV;
+if (ENV !== 'production' && ENV !== 'test') {
+  warning(
+    false,
+    'You are using a whole package of antd,' +
+    'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.'
+  );
 }
 
 export { default as Affix } from './affix';
