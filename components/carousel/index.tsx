3499dbc88efb62d6186ae04ad71c5a35e2b9c2b6
afc163 <afc163@gmail.com>
2017-03-31 14:35:25 +0800

    Update Carousel ts definitions
    close #5543
    
    http://npmjs.com/@types/react-slick

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -23,23 +23,45 @@ import React from 'react';
 export type CarouselEffect = 'scrollx' | 'fade';
 // Carousel
 export interface CarouselProps {
-  /** 动画效果函数，可取 scrollx, fade */
   effect?: CarouselEffect;
-  /** 是否显示面板指示点 */
   dots?: boolean;
-  /** 垂直显示 */
   vertical?: boolean;
-  /** 是否自动切换 */
   autoplay?: boolean;
-  /** 动画效果 */
   easing?: string;
-  /** 切换面板的回调 */
   beforeChange?: (from: number, to: number) => void;
-  /** 切换面板的回调 */
   afterChange?: (current: number) => void;
-  /** 行内样式 */
   style?: React.CSSProperties;
   prefixCls?: string;
+  accessibility?: boolean;
+  nextArrow?: HTMLElement | any;
+  prevArrow?: HTMLElement | any;
+  pauseOnHover?: boolean;
+  className?: string;
+  adaptiveHeight?: boolean;
+  arrows?: boolean;
+  autoplaySpeed?: number;
+  centerMode?: boolean;
+  centerPadding?: string | any;
+  cssEase?: string | any;
+  dotsClass?: string;
+  draggable?: boolean;
+  fade?: boolean;
+  focusOnSelect?: boolean;
+  infinite?: boolean;
+  initialSlide?: number;
+  lazyLoad?: boolean;
+  rtl?: boolean;
+  slide?: string;
+  slidesToShow?: number;
+  slidesToScroll?: number;
+  speed?: number;
+  swipe?: boolean;
+  swipeToSlide?: boolean;
+  touchMove?: boolean;
+  touchThreshold?: number;
+  variableWidth?: boolean;
+  useCSS?: boolean;
+  slickGoTo?: number;
 }
 
 export default class Carousel extends React.Component<CarouselProps, any> {
