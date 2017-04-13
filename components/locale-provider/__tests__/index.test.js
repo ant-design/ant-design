c7261f5fb6956245c4e8236ce3a9a2878e697ea9
afc163 <afc163@gmail.com>
2017-04-13 11:12:30 +0800

    update locale-provider test case

--- components/locale-provider/__tests__/index.test.js
+++ components/locale-provider/__tests__/index.test.js
@@ -19,6 +19,9 @@ import skSK from '../sk_SK';
 import jaJP from '../ja_JP';
 import trTR from '../tr_TR';
 import zhTW from '../zh_TW';
+import fiFI from '../fi_FI';
+
+const locales = [enUS, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL, caES, csCZ, koKR, etEE, skSK, jaJP, trTR, zhTW, fiFI];
 
 const Option = Select.Option;
 const RangePicker = DatePicker.RangePicker;
@@ -64,7 +67,7 @@ const App = () => (
 
 describe('Locale Provider', () => {
   it('should display the text as locale changed', () => {
-    [enUS, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL, caES, csCZ, koKR, etEE, skSK, jaJP, trTR, zhTW].forEach((locale) => {
+    locales.forEach((locale) => {
       const wrapper = mount(
         <LocaleProvider locale={locale}>
           <App />
@@ -86,7 +89,7 @@ describe('Locale Provider', () => {
         return null;
       }
     }
-    [enUS, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL, caES, csCZ, koKR, trTR, zhTW].forEach((locale) => {
+    locales.forEach((locale) => {
       mount(
         <LocaleProvider locale={locale}>
           <ModalDemo />
