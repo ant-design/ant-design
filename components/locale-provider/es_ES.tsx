58fc2b89cb7944e5d387aeccd7f766e97eff3fc9
Tino Diaz <ginodeis@gmail.com>
2017-04-27 10:19:39 +0100

    Update locale es_ES (Spanish)

--- components/locale-provider/es_ES.tsx
+++ components/locale-provider/es_ES.tsx
@@ -1,31 +1,44 @@
-import Pagination from 'rc-pagination/lib/locale/en_US';
-import DatePicker from '../date-picker/locale/en_US';
-import TimePicker from '../time-picker/locale/en_US';
+import Pagination from 'rc-pagination/lib/locale/es_ES';
+import DatePicker from '../date-picker/locale/es_ES';
+import TimePicker from '../time-picker/locale/es_ES';
+import Calendar from '../calendar/locale/es_ES';
 
 export default {
-  locale: 'en',
+  locale: 'es',
   Pagination,
   DatePicker,
   TimePicker,
+  Calendar,
   Table: {
-    filterTitle: 'Filtrar Menu',
-    filterConfirm: 'OK',
-    filterReset: 'Resetear',
-    emptyText: 'No Hay Datos',
+      filterTitle: 'Filtrar Menu',
+      filterConfirm: 'OK',
+      filterReset: 'Resetear',
+      emptyText: 'No Hay Datos',
+      selectAll: 'Seleccionar Todo',
+      selectInvert: 'Invertir Selección',
   },
   Modal: {
-    okText: 'OK',
-    cancelText: 'Cancelar',
-    justOkText: 'OK',
+      okText: 'OK',
+      cancelText: 'Cancelar',
+      justOkText: 'OK',
   },
   Popconfirm: {
-    okText: 'OK',
-    cancelText: 'Cancelar',
+      okText: 'OK',
+      cancelText: 'Cancelar',
   },
   Transfer: {
-    notFoundContent: 'No Encontrado',
-    searchPlaceholder: 'Buscar Aquí',
-    itemUnit: 'item',
-    itemsUnit: 'items',
+      notFoundContent: 'No Encontrado',
+      searchPlaceholder: 'Buscar aquí',
+      itemUnit: 'item',
+      itemsUnit: 'items',
+  },
+  Select: {
+      notFoundContent: 'No Encontrado',
+  },
+  Upload: {
+      uploading: 'Subiendo...',
+      removeFile: 'Eliminar archivo',
+      uploadError: 'Error de subida',
+      previewFile: 'Vista previa',
   },
 };
