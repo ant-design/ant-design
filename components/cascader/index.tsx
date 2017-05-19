88a89ba871447c5050d708d18ae283a69ed70cdb
Benjy Cui <benjytrys@gmail.com>
2017-05-19 11:47:36 +0800

    fix: broken hover style of Cascader, close: #6202

--- components/cascader/index.tsx
+++ components/cascader/index.tsx
@@ -330,6 +330,9 @@ export default class Cascader extends React.Component<CascaderProps, any> {
         style={style}
         className={pickerCls}
       >
+        <span className={`${prefixCls}-picker-label`}>
+          {this.getLabel()}
+        </span>
         <Input
           {...inputProps}
           ref="input"
@@ -344,9 +347,6 @@ export default class Cascader extends React.Component<CascaderProps, any> {
           onKeyDown={this.handleKeyDown}
           onChange={showSearch ? this.handleInputChange : undefined}
         />
-        <span className={`${prefixCls}-picker-label`}>
-          {this.getLabel()}
-        </span>
         {clearIcon}
         <Icon type="down" className={arrowCls} />
       </span>
