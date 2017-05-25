99048fa9bfe13ae5333c1ec103ca93324e88bba4
Benjy Cui <benjytrys@gmail.com>
2017-05-25 17:09:19 +0800

    chore: antd-tools will handle this

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,6 @@ function ignoreMomentLocale(webpackConfig) {
   webpackConfig.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
 }
 
-// Fix ie8 compatibility
-function es3ify(webpackConfig) {
-  webpackConfig.module.loaders.unshift({
-    test: /\.(tsx|jsx?)$/,
-    loader: 'es3ify-loader',
-  });
-}
-
 function addLocales(webpackConfig) {
   let packageName = 'antd-with-locales';
   if (webpackConfig.entry['antd.min']) {
@@ -31,7 +23,6 @@ module.exports = function (webpackConfig) {
   webpackConfig = getWebpackConfig(webpackConfig);
   if (process.env.RUN_ENV === 'PRODUCTION') {
     webpackConfig.forEach((config) => {
-      es3ify(config);
       ignoreMomentLocale(config);
       addLocales(config);
     });
