94901de2e8c0a2ed6f62f5aa50c98994aad71844
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2017-05-27 01:21:09 +0700

    add name props to input-related interfaces

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -16,6 +16,7 @@ export interface AbstractCheckboxProps {
   onMouseEnter?: React.MouseEventHandler<any>;
   onMouseLeave?: React.MouseEventHandler<any>;
   value?: any;
+  name?: string;
 }
 
 export interface CheckboxProps extends AbstractCheckboxProps {
