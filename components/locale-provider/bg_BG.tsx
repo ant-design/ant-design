cdeae5980e871b6454034d081e1603b1bc0e36f2
Adrian Dimitrov <dimitrov.adrian@gmail.com>
2017-05-30 15:50:01 +0300

    Added Bulgarian translation

---
+++ components/locale-provider/bg_BG.tsx
@@ -0,0 +1,47 @@
+import moment from 'moment';
+moment.locale('bg');
+
+import Pagination from 'rc-pagination/lib/locale/bg_BG';
+import DatePicker from '../date-picker/locale/bg_BG';
+import TimePicker from '../time-picker/locale/bg_BG';
+import Calendar from '../calendar/locale/bg_BG';
+
+export default {
+  locale: 'bg',
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  Table: {
+    filterTitle: 'Филтриране',
+    filterConfirm: 'Добре',
+    filterReset: 'Нулриане',
+    emptyText: 'Няма данни',
+    selectAll: 'Избор на текуща страница',
+    selectInvert: 'Обръщане',
+  },
+  Modal: {
+    okText: 'Дибре',
+    cancelText: 'Отказ',
+    justOkText: 'Дибре',
+  },
+  Popconfirm: {
+    okText: 'Дибре',
+    cancelText: 'Отказ',
+  },
+  Transfer: {
+    notFoundContent: 'Няма намерени',
+    searchPlaceholder: 'Търсене',
+    itemUnit: 'избор',
+    itemsUnit: 'избори',
+  },
+  Select: {
+    notFoundContent: 'Няма намерени',
+  },
+  Upload: {
+    uploading: 'Качване...',
+    removeFile: 'Премахване',
+    uploadError: 'Грешка при качването',
+    previewFile: 'Преглед',
+  },
+};
