1f69de8da48ab1a881555962df1492975f06e57b
Wei Zhu <yesmeck@gmail.com>
2017-06-30 15:29:28 +0800

    Run demo test against dist/lib/es dir

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -3,7 +3,10 @@
 npm run lint && \
 npm run dist && \
 node ./tests/dekko/dist.test.js && \
+LIB_DIR=dist npm test && \
 npm run compile && \
 node ./tests/dekko/lib.test.js && \
-npm test -- --coverage -w $MAX_WORKERS && \
-npm test -- --config .jest.node.json -w $MAX_WORKERS
+LIB_DIR=es npm test && \
+LIB_DIR=lib npm test && \
+npm test && \
+npm run test-node
