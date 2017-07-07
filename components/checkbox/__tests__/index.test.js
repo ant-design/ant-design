9eee9f5dcda547cdbe4e51a92d6939a6fbe32abd
afc163 <afc163@gmail.com>
2017-07-07 20:38:52 +0800

    Add test case for Checkbox

--- components/checkbox/__tests__/index.test.js
+++ components/checkbox/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Checkbox from '..';
 
 describe('Checkbox', () => {
@@ -19,5 +19,20 @@ describe('Checkbox', () => {
 
     wrapper.simulate('mouseleave');
     expect(onMouseLeave).toHaveBeenCalled();
+  });
+
+  it('CheckGroup should work', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(
+      <Checkbox.Group options={['Apple', 'Pear', 'Orange']} onChange={onChange} />
+    );
+    wrapper.find('.ant-checkbox-input').at(0).simulate('change');
+    expect(onChange).toBeCalledWith(['Apple']);
+    wrapper.find('.ant-checkbox-input').at(1).simulate('change');
+    expect(onChange).toBeCalledWith(['Apple', 'Pear']);
+    wrapper.find('.ant-checkbox-input').at(2).simulate('change');
+    expect(onChange).toBeCalledWith(['Apple', 'Pear', 'Orange']);
+    wrapper.find('.ant-checkbox-input').at(1).simulate('change');
+    expect(onChange).toBeCalledWith(['Apple', 'Orange']);
   });
 });
