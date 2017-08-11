e633f91c27c8a613c5aa52183e0df4acd942533a
Eden Wang <yociduo@vip.qq.com>
2017-08-11 23:55:36 +0800

    Disabled Dropdown is showing Menu on hover (#7102)
    * disabled dropdown is showing menu on hover
    
    * fix cr error
    
    * update snap
    
    * update document

--- components/dropdown/dropdown.tsx
+++ components/dropdown/dropdown.tsx
@@ -10,6 +10,7 @@ export interface DropDownProps {
   style?: React.CSSProperties;
   onVisibleChange?: (visible?: boolean) => void;
   visible?: boolean;
+  disabled?: boolean;
   align?: Object;
   getPopupContainer?: (triggerNode: Element) => HTMLElement;
   prefixCls?: string;
@@ -44,9 +45,10 @@ export default class Dropdown extends React.Component<DropDownProps, any> {
   }
 
   render() {
-    const { children, prefixCls, overlay } = this.props;
+    const { children, prefixCls, overlay, trigger, disabled } = this.props;
     const dropdownTrigger = cloneElement(children as any, {
       className: classNames((children as any).props.className, `${prefixCls}-trigger`),
+      disabled,
     });
     const fixedModeOverlay = cloneElement(overlay as any, {
       mode: 'vertical',
@@ -54,8 +56,9 @@ export default class Dropdown extends React.Component<DropDownProps, any> {
     return (
       <RcDropdown
         transitionName={this.getTransitionName()}
-        {...this.props}
+        trigger={disabled ? [] : trigger}
         overlay={fixedModeOverlay}
+        {...this.props}
       >
         {dropdownTrigger}
       </RcDropdown>
