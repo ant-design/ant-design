d281b0b7f949fb597bb527bb79daff90858b638c
Ramsés Moreno <ramses@cuatromedios.com>
2017-08-16 18:05:06 -0500

    Corrections in the Spanish locale. In Spanish, the only uppercase letter in a sentece is the first letter and not in every word. Some othe minor changes.

--- components/locale-provider/es_ES.tsx
+++ components/locale-provider/es_ES.tsx
@@ -4,41 +4,41 @@ import TimePicker from '../time-picker/locale/es_ES';
 import Calendar from '../calendar/locale/es_ES';
 
 export default {
-  locale: 'es',
-  Pagination,
-  DatePicker,
-  TimePicker,
-  Calendar,
-  Table: {
-    filterTitle: 'Filtrar Menu',
-    filterConfirm: 'OK',
-    filterReset: 'Resetear',
-    emptyText: 'No Hay Datos',
-    selectAll: 'Seleccionar Todo',
-    selectInvert: 'Invertir Selección',
-  },
-  Modal: {
-    okText: 'OK',
-    cancelText: 'Cancelar',
-    justOkText: 'OK',
-  },
-  Popconfirm: {
-    okText: 'OK',
-    cancelText: 'Cancelar',
-  },
-  Transfer: {
-    notFoundContent: 'No Encontrado',
-    searchPlaceholder: 'Buscar aquí',
-    itemUnit: 'item',
-    itemsUnit: 'items',
-  },
-  Select: {
-    notFoundContent: 'No Encontrado',
-  },
-  Upload: {
-    uploading: 'Subiendo...',
-    removeFile: 'Eliminar archivo',
-    uploadError: 'Error de subida',
-    previewFile: 'Vista previa',
-  },
+    locale: 'es',
+    Pagination,
+    DatePicker,
+    TimePicker,
+    Calendar,
+    Table: {
+        filterTitle: 'Filtrar menú',
+        filterConfirm: 'Aceptar',
+        filterReset: 'Reiniciar',
+        emptyText: 'No hay datos',
+        selectAll: 'Seleccionar todo',
+        selectInvert: 'Invertir selección',
+    },
+    Modal: {
+        okText: 'Aceptar',
+        cancelText: 'Cancelar',
+        justOkText: 'Aceptar',
+    },
+    Popconfirm: {
+        okText: 'Aceptar',
+        cancelText: 'Cancelar',
+    },
+    Transfer: {
+        notFoundContent: 'No encontrado',
+        searchPlaceholder: 'Buscar aquí',
+        itemUnit: 'elemento',
+        itemsUnit: 'elementos',
+    },
+    Select: {
+        notFoundContent: 'No encontrado',
+    },
+    Upload: {
+        uploading: 'Subiendo...',
+        removeFile: 'Eliminar archivo',
+        uploadError: 'Error al subir el archivo',
+        previewFile: 'Vista previa',
+    },
 };
