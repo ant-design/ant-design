742999ff286ba5c22f713b41728ea8260f7fa964
偏右 <afc163@gmail.com>
2017-08-27 16:53:36 +0800

    Fix matchMedia not present in jest test (#7344)
    close #6560
    
    close #3308
    
    close #7341

--- tests/setup.js
+++ tests/setup.js
@@ -2,17 +2,6 @@ import { jsdom } from 'jsdom';
 
 // fixed jsdom miss
 if (typeof window !== 'undefined') {
-  const matchMediaPolyfill = function matchMediaPolyfill() {
-    return {
-      matches: false,
-      addListener() {
-      },
-      removeListener() {
-      },
-    };
-  };
-  window.matchMedia = window.matchMedia || matchMediaPolyfill;
-
   const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
   global.document = jsdom(documentHTML);
   global.window = document.parentWindow;
