2437ca419ed2617544da449fc106436a08ed564d
Wei Zhu <yesmeck@gmail.com>
2017-09-15 10:50:45 +0800

    Use snapshot test

--- components/icon/__tests__/index.test.js
+++ components/icon/__tests__/index.test.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
+import { render } from 'enzyme';
 import Icon from '..';
 
-class Wrapper extends React.Component {
-  render() {
-    return this.props.children;
-  }
-}
-
 describe('Icon', () => {
-  let icon;
-  let iconNode;
-
-  beforeEach(() => {
-    icon = TestUtils.renderIntoDocument(
-      <Wrapper><Icon type="appstore" className="my-icon-classname" /></Wrapper>
-    );
-    iconNode = TestUtils.findRenderedDOMComponentWithTag(icon, 'I');
-  });
-
   it('should render to a <i class="xxx"></i>', () => {
-    expect(iconNode.tagName).toBe('I');
-    expect(iconNode.className).toContain('my-icon-classname');
-    expect(iconNode.className).toContain('anticon');
-    expect(iconNode.className).toContain('anticon-appstore');
+    const wrapper = render(
+      <Icon type="appstore" className="my-icon-classname" />
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
