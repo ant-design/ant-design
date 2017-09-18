ed2303a5da7d38234ebebddd8141bf8fd02be8c5
Wei Zhu <yesmeck@gmail.com>
2017-09-18 04:31:48 -0500

    Change primary color in browser (#7516)

--- site/theme/template/utils.jsx
+++ site/theme/template/utils.jsx
@@ -64,3 +64,14 @@ export function isLocalStorageNameSupported() {
     return false;
   }
 }
+
+export function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
