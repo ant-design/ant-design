e8d249cb328ad27ae630175fc3927527ddc79791
Marco Afonso <mafonso333@gmail.com>
2017-09-20 23:25:35 +0100

    Add pt_PT to tests

--- components/locale-provider/__tests__/index.test.js
+++ components/locale-provider/__tests__/index.test.js
@@ -34,7 +34,7 @@ import elGR from '../el_GR';
 import nbNO from '../nb_NO';
 import srRS from '../sr_RS';
 
-const locales = [enUS, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL, caES, csCZ, koKR, etEE, skSK, jaJP, trTR, zhTW, fiFI, plPL, bgBG, enGB, frFR, nlBE, itIT, viVN, thTH, faIR, elGR, nbNO, srRS];
+const locales = [enUS, ptPT, ptBR, ruRU, esES, svSE, frBE, deDE, nlNL, caES, csCZ, koKR, etEE, skSK, jaJP, trTR, zhTW, fiFI, plPL, bgBG, enGB, frFR, nlBE, itIT, viVN, thTH, faIR, elGR, nbNO, srRS];
 
 const Option = Select.Option;
 const RangePicker = DatePicker.RangePicker;
