aa593a56afb6618282ea39ede450a69f3ba911a5
Mohamed Seada <mohamed.seada.1994@gmail.com>
2017-10-09 13:20:06 +0200

    Create ar_EG.js

---
+++ components/pagination/custom_local/ar_EG.js
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ الصفحة',
+  jump_to: 'الذهاب إلى',
+  jump_to_confirm: 'تأكيد',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'الصفحة السابقة',
+  next_page: 'الصفحة التالية',
+  prev_5: 'خمس صفحات سابقة',
+  next_5: 'خمس صفحات تالية',
+  prev_3: 'ثلاث صفحات سابقة',
+  next_3: 'ثلاث صفحات تالية',
+};
