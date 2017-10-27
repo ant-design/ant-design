8bde0c0feecaa7be01f0edbb9d4e2d3f994f8007
vgeyi <vgeyiz@126.com>
2017-10-27 20:21:34 +0800

    Rewrite the MenuItem to handle ref for react 16 (#8037)
    * ReWrite MenuItem to remove ref warning for react 16
    
    * unified coding style
    
    * add removing semicolon

--- components/menu/MenuItem.tsx
+++ components/menu/MenuItem.tsx
@@ -3,22 +3,22 @@ import { Item } from 'rc-menu';
 import PropTypes from 'prop-types';
 import Tooltip from '../tooltip';
 
-const MenuItem: any = (props, { inlineCollapsed }) => {
-  return (
-    <Tooltip
+class MenuItem extends React.Component<any, any> {
+  static contextTypes = {
+    inlineCollapsed: PropTypes.bool,
+  };
+  static isMenuItem = 1;
+  render() {
+    const { inlineCollapsed } = this.context;
+    const props = this.props;
+    return <Tooltip
       title={inlineCollapsed && props.level === 1 ? props.children : ''}
       placement="right"
       overlayClassName={`${props.rootPrefixCls}-inline-collapsed-tooltip`}
     >
       <Item {...props} />
-    </Tooltip>
-  );
-};
-
-MenuItem.contextTypes = {
-  inlineCollapsed: PropTypes.bool,
-};
-
-MenuItem.isMenuItem = 1;
+    </Tooltip>;
+  }
+}
 
 export default MenuItem;
