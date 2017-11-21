499b81f79dd23d199d10e3675f0de7b2b6ca2b7c
slientcloud <rjmuqiang@gmail.com>
2017-11-21 15:53:03 +0800

    fix(tooltip): support defaultVisible for tooltip, close #8257 (#8258)

--- components/tooltip/index.tsx
+++ components/tooltip/index.tsx
@@ -16,6 +16,7 @@ export interface AbstractTooltipProps {
   overlayStyle?: React.CSSProperties;
   placement?: TooltipPlacement;
   builtinPlacements?: Object;
+  defaultVisible?: boolean;
   visible?: boolean;
   onVisibleChange?: (visible: boolean) => void;
   mouseEnterDelay?: number;
@@ -67,7 +68,7 @@ export default class Tooltip extends React.Component<TooltipProps, any> {
     super(props);
 
     this.state = {
-      visible: !!props.visible,
+      visible: !!props.visible || !!props.defaultVisible,
     };
   }
 
