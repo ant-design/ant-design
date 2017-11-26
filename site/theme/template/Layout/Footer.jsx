52f230cb26ace27ac3016d88e8bc02ab8658c4a6
ddcat1115 <ddcat1115@gmail.com>
2017-11-26 18:33:48 +0800

    3.0 site update

--- site/theme/template/Layout/Footer.jsx
+++ site/theme/template/Layout/Footer.jsx
@@ -85,160 +85,162 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <footer id="footer">
-        <ul>
-          <li>
-            <h2><Icon type="github" /> Ant Design</h2>
-            <div>
-              <a target="_blank " href="https://github.com/ant-design/ant-design">
-                GitHub
-              </a>
-            </div>
-            <div>
-              <a href="http://pro.ant.design">Ant Design Pro</a>
-            </div>
-            <div>
-              <a href="http://mobile.ant.design">Ant Design Mobile</a>
-            </div>
-            <div>
-              <a href="http://ng.ant.design">NG-ZORRO</a>
-              <span> - </span>
-              Ant Design of Angular
-            </div>
-            <div>
-              <a target="_blank " href="https://github.com/websemantics/awesome-ant-design">
-                <FormattedMessage id="app.footer.awesome" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank " href="http://ant-design.gitee.io/">
-                <FormattedMessage id="app.footer.chinamirror" />
-              </a>
-            </div>
-            <div style={{ marginTop: 12 }}>
-              <ColorPicker
-                type="sketch"
-                small
-                color={this.state.color}
-                position="top"
-                presetColors={[
-                  '#F5222D',
-                  '#FA541C',
-                  '#FA8C16',
-                  '#FAAD14',
-                  '#FADB14',
-                  '#A0D911',
-                  '#52C41A',
-                  '#13C2C2',
-                  '#1890FF',
-                  '#2F54EB',
-                  '#722ED1',
-                  '#EB2F96',
-                ]}
-                onChangeComplete={this.handleColorChange}
-              />
-            </div>
-          </li>
-          <li>
-            <h2><Icon type="link" /> <FormattedMessage id="app.footer.resources" /></h2>
-            <div>
-              <a href="http://scaffold.ant.design">Scaffolds</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.scaffolds" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva">dva</a> - <FormattedMessage id="app.footer.dva" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva-cli">dva-cli</a> -
-              <FormattedMessage id="app.footer.dev-tools" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://eggjs.org/">Egg</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.eggjs" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://antv.alipay.com/">AntV</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.data-vis" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://motion.ant.design">Ant Motion</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.motion" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://library.ant.design/">AntD Library</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.antd-library" />
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://ux.ant.design">Ant UX</a>
-              <span> - </span>
-              <FormattedMessage id="app.footer.antux" />
-            </div>
-          </li>
-          <li>
-            <h2><Icon type="customer-service" /> <FormattedMessage id="app.footer.community" /></h2>
-            <div>
-              <a href="/changelog">
-                <FormattedMessage id="app.footer.change-log" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/wiki/FAQ">
-                <FormattedMessage id="app.footer.faq" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design">
-                <FormattedMessage id="app.footer.discuss-cn" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design-english">
-                <FormattedMessage id="app.footer.discuss-en" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://new-issue.ant.design/">
-                <FormattedMessage id="app.footer.bug-report" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/issues">
-                <FormattedMessage id="app.footer.issues" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="http://stackoverflow.com/questions/tagged/antd">
-                <FormattedMessage id="app.footer.stackoverflow" />
-              </a>
-            </div>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://segmentfault.com/t/antd">
-                <FormattedMessage id="app.footer.segmentfault" />
-              </a>
-            </div>
-          </li>
-          <li>
-            <h2>Copyright © {new Date().getFullYear()}</h2>
-            <div>
-              <FormattedMessage id="app.footer.author" />
-              &nbsp;
-              <Link to="/docs/resource/work-with-us">
-                <FormattedMessage id="app.footer.work_with_us" />
-              </Link>
-            </div>
-            <div>
-              Built with&nbsp;
-              <a target="_blank" rel="noopener noreferrer" href="https://github.com/benjycui/bisheng">
-                BiSheng
-              </a>
-            </div>
-          </li>
-        </ul>
+      <footer id="footer" className="dark">
+        <div className="footer-wrap">
+          <ul>
+            <li>
+              <h2><Icon type="github" /> Ant Design</h2>
+              <div>
+                <a target="_blank " href="https://github.com/ant-design/ant-design">
+                  GitHub
+                </a>
+              </div>
+              <div>
+                <a href="http://pro.ant.design">Ant Design Pro</a>
+              </div>
+              <div>
+                <a href="http://mobile.ant.design">Ant Design Mobile</a>
+              </div>
+              <div>
+                <a href="http://ng.ant.design">NG-ZORRO</a>
+                <span> - </span>
+                Ant Design of Angular
+              </div>
+              <div>
+                <a target="_blank " href="https://github.com/websemantics/awesome-ant-design">
+                  <FormattedMessage id="app.footer.awesome" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank " href="http://ant-design.gitee.io/">
+                  <FormattedMessage id="app.footer.chinamirror" />
+                </a>
+              </div>
+              <div style={{ marginTop: 12 }}>
+                <ColorPicker
+                  type="sketch"
+                  small
+                  color={this.state.color}
+                  position="top"
+                  presetColors={[
+                    '#F5222D',
+                    '#FA541C',
+                    '#FA8C16',
+                    '#FAAD14',
+                    '#FADB14',
+                    '#A0D911',
+                    '#52C41A',
+                    '#13C2C2',
+                    '#1890FF',
+                    '#2F54EB',
+                    '#722ED1',
+                    '#EB2F96',
+                  ]}
+                  onChangeComplete={this.handleColorChange}
+                />
+              </div>
+            </li>
+            <li>
+              <h2><Icon type="link" /> <FormattedMessage id="app.footer.resources" /></h2>
+              <div>
+                <a href="http://scaffold.ant.design">Scaffolds</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.scaffolds" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva">dva</a> - <FormattedMessage id="app.footer.dva" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/dvajs/dva-cli">dva-cli</a> -
+                <FormattedMessage id="app.footer.dev-tools" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://eggjs.org/">Egg</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.eggjs" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://antv.alipay.com/">AntV</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.data-vis" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://motion.ant.design">Ant Motion</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.motion" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://library.ant.design/">AntD Library</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.antd-library" />
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://ux.ant.design">Ant UX</a>
+                <span> - </span>
+                <FormattedMessage id="app.footer.antux" />
+              </div>
+            </li>
+            <li>
+              <h2><Icon type="customer-service" /> <FormattedMessage id="app.footer.community" /></h2>
+              <div>
+                <a href="/changelog">
+                  <FormattedMessage id="app.footer.change-log" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/wiki/FAQ">
+                  <FormattedMessage id="app.footer.faq" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design">
+                  <FormattedMessage id="app.footer.discuss-cn" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://gitter.im/ant-design/ant-design-english">
+                  <FormattedMessage id="app.footer.discuss-en" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://new-issue.ant.design/">
+                  <FormattedMessage id="app.footer.bug-report" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ant-design/ant-design/issues">
+                  <FormattedMessage id="app.footer.issues" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="http://stackoverflow.com/questions/tagged/antd">
+                  <FormattedMessage id="app.footer.stackoverflow" />
+                </a>
+              </div>
+              <div>
+                <a target="_blank" rel="noopener noreferrer" href="https://segmentfault.com/t/antd">
+                  <FormattedMessage id="app.footer.segmentfault" />
+                </a>
+              </div>
+            </li>
+            <li>
+              <h2>Copyright © {new Date().getFullYear()}</h2>
+              <div>
+                <FormattedMessage id="app.footer.author" />
+                &nbsp;
+                <Link to="/docs/resource/work-with-us">
+                  <FormattedMessage id="app.footer.work_with_us" />
+                </Link>
+              </div>
+              <div>
+                Built with&nbsp;
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/benjycui/bisheng">
+                  BiSheng
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
       </footer>
     );
   }
