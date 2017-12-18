3f3fbfbe673fc003f2a19481312d22710a438d9c
afc163 <afc163@gmail.com>
2017-12-18 17:58:45 +0800

    Add promote link for seeconf

--- site/theme/template/Home/index.jsx
+++ site/theme/template/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import DocumentTitle from 'react-document-title';
+import { Icon } from 'antd';
 import Banner from './Banner';
 import Page1 from './Page1';
 import Page2 from './Page2';
@@ -25,19 +26,54 @@ function getStyle() {
   `;
 }
 
-function Home(props) {
-  return (
-    <DocumentTitle title={`Ant Design - ${props.intl.formatMessage({ id: 'app.home.slogan' })}`}>
-      <div className="main-wrapper">
-        <Banner {...props} />
-        <Page1 {...props} />
-        <Page2 {...props} />
-        <Page3 {...props} />
-        <Page4 {...props} />
-        <style dangerouslySetInnerHTML={{ __html: getStyle() }} />
-      </div>
-    </DocumentTitle>
-  );
+const promoteBannerImageUrl = 'https://gw.alipayobjects.com/zos/rmsportal/qVVhewfLyIYZnqrBvfhy.png';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      adBannerClosed: (
+        window.localStorage &&
+        window.localStorage.getItem(`adBannerClosed-${promoteBannerImageUrl}`) === 'true'
+      ),
+    };
+  }
+  closePromoteBanner = (e) => {
+    e.preventDefault();
+    this.makeAdBannerClosed();
+  }
+  makeAdBannerClosed = () => {
+    this.setState({
+      adBannerClosed: true,
+    });
+    if (window.localStorage) {
+      window.localStorage.setItem(`adBannerClosed-${promoteBannerImageUrl}`, 'true');
+    }
+  }
+  render() {
+    const promoteBanner = this.state.adBannerClosed ? null : (
+      <a href="http://seeconf.alipay.com/" className="promote-banner" onClick={this.makeAdBannerClosed}>
+        <img
+          src={promoteBannerImageUrl}
+          alt="seeconf"
+        />
+        <Icon type="cross" title="close ad" onClick={this.closePromoteBanner} />
+      </a>
+    );
+    return (
+      <DocumentTitle title={`Ant Design - ${this.props.intl.formatMessage({ id: 'app.home.slogan' })}`}>
+        <div className="main-wrapper">
+          {promoteBanner}
+          <Banner {...this.props} />
+          <Page1 {...this.props} />
+          <Page2 {...this.props} />
+          <Page3 {...this.props} />
+          <Page4 {...this.props} />
+          <style dangerouslySetInnerHTML={{ __html: getStyle() }} />
+        </div>
+      </DocumentTitle>
+    );
+  }
 }
 
 export default injectIntl(Home);
