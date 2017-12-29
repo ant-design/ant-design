642f8b20ecb5cffe0ffa060b14552b2d08381736
Wei Zhu <yesmeck@gmail.com>
2017-12-29 03:02:45 -0600

    Add open in CodeSandbox (#8747)

--- site/theme/zh-CN.js
+++ site/theme/zh-CN.js
@@ -20,6 +20,7 @@ module.exports = {
     'app.demo.copy': '复制代码',
     'app.demo.copied': '复制成功',
     'app.demo.codepen': '在 CodePen 中打开',
+    'app.demo.codesandbox': '在 CodeSandbox 中打开',
     'app.demo.riddle': '在 Riddle 中打开',
     'app.home.slogan': '一个 UI 设计语言',
     'app.home.introduce': '设计规范',
