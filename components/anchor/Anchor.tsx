d8c300246ad38db810ae415942ac21f70f309196
jjzhangg <zhangjunjing@gmail.com>
2018-01-03 11:34:47 +0800

    anchor scroll supports complete href link
    anchor scroll supports complete href link(http://example.com/somepath#id), not just #id format

--- components/anchor/Anchor.tsx
+++ components/anchor/Anchor.tsx
@@ -42,6 +42,7 @@ function easeInOutCubic(t: number, b: number, c: number, d: number) {
 }
 
 const reqAnimFrame = getRequestAnimationFrame();
+const sharpMatcherRegx = /#([^#]+)$/;
 function scrollTo(href: string, offsetTop = 0, target: () => Window | HTMLElement, callback = () => { }) {
   const scrollTop = getScroll(target(), true);
   const targetElement = document.getElementById(href.substring(1));
@@ -172,7 +173,9 @@ export default class Anchor extends React.Component<AnchorProps, any> {
 
     const linkSections: Array<Section> = [];
     this.links.forEach(link => {
-      const target = document.getElementById(link.substring(1));
+      const sharpLinkMatch = sharpMatcherRegx.exec(link);
+      if(!sharpLinkMatch) return;
+      const target = document.getElementById(sharpLinkMatch[1]);
       if (target && getOffsetTop(target) < offsetTop + bounds) {
         const top = getOffsetTop(target);
         linkSections.push({
