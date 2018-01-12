7dc4ee6d7d56061b08555a10eec361b4a49933c1
afc163 <afc163@gmail.com>
2018-01-12 21:05:48 +0800

    site: remove home page video

--- site/theme/template/Home/index.jsx
+++ site/theme/template/Home/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import DocumentTitle from 'react-document-title';
 import PropTypes from 'prop-types';
-import Animate from 'rc-animate';
 import Banner from './Banner';
 import Page1 from './Page1';
 import Page2 from './Page2';
@@ -45,42 +44,15 @@ function getStyle() {
   `;
 }
 
-const versionName = 'antd-noShowNewVersionVideo-3.0';
-const vidoeSrc = 'https://gw.alipayobjects.com/os/rmsportal/DNDyihnvkHUuANuumKck.mp4';
-// window.localStorage.setItem(versionName, 'false');
+/* eslint-disable react/prefer-stateless-function */
 class Home extends React.Component {
   static contextTypes = {
     intl: PropTypes.object.isRequired,
     isMobile: PropTypes.bool.isRequired,
   }
-  constructor(props) {
-    super(props);
-    const noShowNewVersionVideo = typeof window === 'undefined' ? true : (
-      window.localStorage && window.localStorage.getItem(versionName) === 'true'
-    );
-    this.state = { noShowNewVersionVideo };
-  }
-  onVideoEnd = () => {
-    this.setState({
-      noShowNewVersionVideo: true,
-    });
-    if (window.localStorage) {
-      window.localStorage.setItem(versionName, 'true');
-    }
-  }
   render() {
     const { isMobile, intl } = this.context;
     const childProps = { ...this.props, isMobile, locale: intl.locale };
-    const noShowNewVersionVideo = this.state.noShowNewVersionVideo ? null : (
-      <div className="new-version-video" key="video">
-        <div className="vidoe-wrap">
-          <video width="100%" autoPlay onEnded={this.onVideoEnd}>
-            <source src={vidoeSrc} autoPlay type="video/mp4" />
-            <track kind="captions" />
-          </video>
-        </div>
-      </div>
-    );
     return (
       <DocumentTitle title={`Ant Design - ${this.props.intl.formatMessage({ id: 'app.home.slogan' })}`}>
         <div className="main-wrapper">
@@ -89,11 +61,6 @@ class Home extends React.Component {
           <Page2 {...childProps} />
           <Page3 {...childProps} />
           <style dangerouslySetInnerHTML={{ __html: getStyle() }} />
-          {!isMobile && (
-            <Animate transitionName="fade">
-              {noShowNewVersionVideo}
-            </Animate>
-          )}
         </div>
       </DocumentTitle>
     );
