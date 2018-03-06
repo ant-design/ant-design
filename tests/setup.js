8226ede38ec11e3eb314d7514abfc65dd596578c
偏右 <afc163@gmail.com>
2018-03-06 21:21:01 +0800

    Fix test case for new jsdom (#9527)
    * Fix test case for new jsdom
    
    * use setTimeout as raf in jest jsdom
    
    * Fix cancelAnimationFrame
    
    * Add comment for facebook/jest#5147
    
    * longer timeout
    
    * fix snap
    
    * upgrade antd-tools

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,5 @@
 /* eslint-disable global-require */
-import { jsdom } from 'jsdom';
-
-// fixed jsdom miss
 if (typeof window !== 'undefined') {
-  const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-  global.document = jsdom(documentHTML);
-  global.window = document.parentWindow;
-
   global.window.resizeTo = (width, height) => {
     global.window.innerWidth = width || global.window.innerWidth;
     global.window.innerHeight = height || global.window.innerHeight;
@@ -14,8 +7,14 @@ if (typeof window !== 'undefined') {
   };
 }
 
-global.requestAnimationFrame = global.requestAnimationFrame || function (cb) {
+// The built-in requestAnimationFrame and cancelAnimationFrame not working with jest.runFakeTimes()
+// https://github.com/facebook/jest/issues/5147
+global.requestAnimationFrame = function (cb) {
   return setTimeout(cb, 0);
+};
+
+global.cancelAnimationFrame = function (cb) {
+  return clearTimeout(cb, 0);
 };
 
 const Enzyme = require('enzyme');
