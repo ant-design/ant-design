ccf8c15b82e383bf462a704726c0e925334c2e18
zswang <wangjihu@sensorsdata.cn>
2018-03-11 00:13:26 +0800

    Compatible with non-image format file preview in UploadList

--- components/upload/UploadList.tsx
+++ components/upload/UploadList.tsx
@@ -13,6 +13,10 @@ const previewFile = (file: File, callback: Function) => {
   reader.readAsDataURL(file);
 };
 
+const isImageUrl = (url: string): boolean => {
+  return /^data:image\//.test(url) || /\.(webp|svg|png|gif|jpg|jpeg)$/.test(url);
+};
+
 export default class UploadList extends React.Component<UploadListProps, any> {
   static defaultProps = {
     listType: 'text',  // or picture
@@ -77,6 +81,14 @@ export default class UploadList extends React.Component<UploadListProps, any> {
         } else if (!file.thumbUrl && !file.url) {
           icon = <Icon className={`${prefixCls}-list-item-thumbnail`} type="picture" />;
         } else {
+          let thumbnail = isImageUrl((file.thumbUrl || file.url) as string) ? (
+            <img src={file.thumbUrl || file.url} alt={file.name} />
+          ) : (
+            <Icon
+              type="file"
+              style={{ fontSize: 48, color: 'rgba(0,0,0,0.5)' }}
+            />
+          );
           icon = (
             <a
               className={`${prefixCls}-list-item-thumbnail`}
@@ -85,7 +97,7 @@ export default class UploadList extends React.Component<UploadListProps, any> {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src={file.thumbUrl || file.url} alt={file.name} />
+              {thumbnail}
             </a>
           );
         }
