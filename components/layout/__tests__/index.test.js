1e33ba1a3dd79b6e79f32dc706241be374ddfd4e
zhuyue <fuping.dfp@antfin.com>
2018-03-13 15:05:05 +0800

    Add test for Sider[position]

--- components/layout/__tests__/index.test.js
+++ components/layout/__tests__/index.test.js
@@ -34,4 +34,12 @@ describe('Layout', () => {
     );
     expect(wrapper.find('.ant-layout-sider').hasClass('ant-layout-sider-has-trigger')).toBe(true);
   });
+
+  it('detect ant-layout-sider-aero-width-trigger-right in sider when position is right', () => {
+    const wrapper = mount(<Sider collapsedWidth="0" collapsible>Sider</Sider>);
+    expect(wrapper.find('.ant-layout-sider-zero-width-trigger-left')).toHaveLength(1);
+
+    wrapper.setProps({ position: 'right' });
+    expect(wrapper.find('.ant-layout-sider-zero-width-trigger-right')).toHaveLength(1);
+  });
 });
