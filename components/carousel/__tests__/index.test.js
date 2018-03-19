714cdb6e0555b2429447a56d0f559b1070416d56
Oleg Kuzava <olegkuzava@gmail.com>
2018-03-19 22:12:25 +0200

    Add test coverage for exposed slick methods

--- components/carousel/__tests__/index.test.js
+++ components/carousel/__tests__/index.test.js
@@ -3,11 +3,19 @@ import { mount } from 'enzyme';
 import Carousel from '..';
 
 describe('Carousel', () => {
-  it('should has innerSlider', () => {
+  it('should have innerSlider', () => {
     const wrapper = mount(<Carousel><div /></Carousel>);
     const { innerSlider } = wrapper.instance();
     const innerSliderFromRefs = wrapper.instance().slick.innerSlider;
     expect(innerSlider).toBe(innerSliderFromRefs);
     expect(typeof innerSlider.slickNext).toBe('function');
+  });
+  
+  it('should have exposed slick methods', () => {
+    const wrapper = mount(<Carousel><div /></Carousel>);
+    const { next, prev, goTo } = wrapper.instance();
+    expect(typeof next).toBe('function');
+    expect(typeof prev).toBe('function');
+    expect(typeof goTo).toBe('function');
   });
 });
