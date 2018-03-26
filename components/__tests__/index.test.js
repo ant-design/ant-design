4537a8d0fbc270b790780c2889851bcbf247750f
afc163 <afc163@gmail.com>
2018-03-26 12:01:08 +0800

    Add test case

--- components/__tests__/index.test.js
+++ components/__tests__/index.test.js
@@ -1,7 +1,16 @@
-import * as antd from '..';
+const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+const antd = require('..');
 
 describe('antd', () => {
   it('exports modules correctly', () => {
     expect(Object.keys(antd)).toMatchSnapshot();
+  });
+
+  it('should hint when import all components', () => {
+    expect(warnSpy.mock.calls).toHaveLength(1);
+    expect(warnSpy.mock.calls[0][0]).toMatch(
+      'You are using a whole package of antd, please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.'
+    );
+    warnSpy.mockRestore();
   });
 });
