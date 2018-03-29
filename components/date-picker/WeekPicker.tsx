c526e175c724cca651af8069407c1c0b480e7e46
afc163 <afc163@gmail.com>
2018-03-29 11:26:38 +0800

    Fix WeekPick style prop, close #9848

--- components/date-picker/WeekPicker.tsx
+++ components/date-picker/WeekPicker.tsx
@@ -84,6 +84,7 @@ export default class WeekPicker extends React.Component<any, any> {
     const {
       prefixCls, className, disabled, pickerClass, popupStyle,
       pickerInputClass, format, allowClear, locale, localeCode, disabledDate,
+      style, onFocus, onBlur,
     } = this.props;
 
     const pickerValue = this.state.value;
@@ -123,8 +124,9 @@ export default class WeekPicker extends React.Component<any, any> {
             value={(value && value.format(format)) || ''}
             placeholder={placeholder}
             className={pickerInputClass}
-            onFocus={this.props.onFocus}
-            onBlur={this.props.onBlur}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            style={style}
           />
           {clearIcon}
           <span className={`${prefixCls}-picker-icon`} />
