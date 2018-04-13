92eeb7f8c9ae54372440db71c719ee6d9ff84d28
afc163 <afc163@gmail.com>
2018-04-13 15:28:31 +0800

    Tweak list grid margin

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -170,7 +170,7 @@ export default class List extends React.Component<ListProps> {
     );
 
     let childrenContent;
-    childrenContent = isLoading && (<div style={{ minHeight: 53 }} />);
+    childrenContent = isLoading && <div style={{ minHeight: 53 }} />;
     if (dataSource.length > 0) {
       const items = dataSource.map((item: any, index: number) => this.renderItem(item, index));
       const childrenList = React.Children.map(items, (child: any, index) => React.cloneElement(child, {
