66ba6dd20168296b9582bdceec056f0fd8497c5d
paranoidjk <hust2012jiangkai@gmail.com>
2018-05-02 04:38:22 +0000

    fix: merge

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -115,14 +115,14 @@ export interface FormComponentProps {
   form: WrappedFormUtils;
 }
 
-interface RcBaseFromProps {
+export interface RcBaseFormProps {
    wrappedComponentRef?: any;
 }
 
 export interface ComponentDecorator {
   <P extends FormComponentProps>(
     component: React.ComponentClass<P> | React.SFC<P>,
-  ): React.ComponentClass<Omit<P extends RcBaseFromProps, keyof FormComponentProps>>;
+  ): React.ComponentClass<RcBaseFormProps & Omit<P, keyof FormComponentProps>>;
 }
 
 export default class Form extends React.Component<FormProps, any> {
