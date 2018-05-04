66260e48453617888ab4e1711e89a18ea063b60c
Karol Majewski <karol-majewski@users.noreply.github.com>
2018-05-04 18:27:30 +0200

    Use exact type for Breadcrumb.Item
    Using "any" breaks the build for TSLint users having the "no-unsafe-any" rule enabled.
    
    See https://palantir.github.io/tslint/rules/no-unsafe-any/

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -41,7 +41,7 @@ function defaultItemRender(route: Route, params: any, routes: Route[], paths: st
 }
 
 export default class Breadcrumb extends React.Component<BreadcrumbProps, any> {
-  static Item: any;
+  static Item: typeof BreadcrumbItem;
 
   static defaultProps = {
     prefixCls: 'ant-breadcrumb',
