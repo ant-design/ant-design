f4b8a58480e2a603b469f769e8cd0db72a7a20d2
Christian <chr.vadala@gmail.com>
2018-05-04 08:51:15 +0200

    fixes #10289 and #10209: lost of user selection on table data updates (#10332)
    * fixes #10289 and #10209: lost of user selection on table data updates
    
    * adds shallowequal check

--- components/table/__tests__/Table.filter.test.js
+++ components/table/__tests__/Table.filter.test.js
@@ -106,6 +106,22 @@ describe('Table.filter', () => {
     expect(dropdown.props().visible).toBe(false);
   });
 
+  it('if the filter is visible it should ignore the selectedKeys changes', () => {
+    const wrapper = mount(createTable({
+      columns: [{
+        ...column,
+        filterDropdownVisible: true,
+      }],
+    }));
+
+    const filterMenu = wrapper.find('FilterMenu').instance();
+    expect(filterMenu.state.selectedKeys).toEqual([]);
+    wrapper.find('FilterMenu').find('input[type="checkbox"]').first().simulate('click');
+    expect(filterMenu.state.selectedKeys).toEqual(['boy']);
+    wrapper.setProps({ dataSource: [...data, { key: 999, name: 'Chris' }] });
+    expect(filterMenu.state.selectedKeys).toEqual(['boy']);
+  });
+
   it('fires change event when visible change', () => {
     const handleChange = jest.fn();
     const wrapper = mount(createTable({
