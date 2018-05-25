042c014a380cb8bffabe0c83b1ea035fff927983
afc163 <afc163@gmail.com>
2018-05-25 16:04:38 +0800

    test => it

--- components/badge/__tests__/index.test.js
+++ components/badge/__tests__/index.test.js
@@ -12,12 +12,12 @@ describe('Badge', () => {
     jest.useRealTimers();
   });
 
-  test('badge dot not scaling count > 9', () => {
+  it('badge dot not scaling count > 9', () => {
     const badge = mount(<Badge count={10} dot />);
     expect(badge.find('.ant-card-multiple-words').length).toBe(0);
   });
 
-  test('badge dot not showing count == 0', () => {
+  it('badge dot not showing count == 0', () => {
     const badge = mount(<Badge count={0} dot />);
     expect(badge.find('.ant-badge-dot').length).toBe(0);
   });
