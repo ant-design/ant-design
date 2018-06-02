f5f9d78bd2e1c285b37f82edf6ea382ae95e1675
Wei Zhu <yesmeck@gmail.com>
2018-06-02 12:19:20 +0800

    use reduce

--- site/getExternalResources.js
+++ site/getExternalResources.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const { uniq } = require('lodash');
 
 function getExternalResources() {
-  let resources = [];
   const files = glob.sync('{./{components,docs}/**/*.md,./site/**/*.{less,js,jsx}}');
-  files.forEach((file) => {
+  const resources = files.reduce((acc, file) => {
     const content = fs.readFileSync(file, 'utf-8');
     const pattern = new RegExp('(https://.+\\.alipayobjects\\.com/.+\\.(png|jpg|svg)|)', 'mg');
     const matches = content.match(pattern);
     if (matches) {
-      resources = resources.concat(matches);
+      acc = acc.concat(matches);
     }
-  });
+    return acc;
+  }, []);
   return uniq(resources);
 }
 
