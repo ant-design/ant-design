ef459d22849f050eeebc473e2da96df1b11f4bf3
陈帅 <qixian.cs@outlook.com>
2018-06-25 23:25:38 +0800

    edit test script

--- components/drawer/__tests__/Drawer.test.js
+++ components/drawer/__tests__/Drawer.test.js
@@ -29,28 +29,21 @@ class DrawerTester extends React.Component {
 describe('Drawer', () => {
   it('render correctly', () => {
     const wrapper = mount(<DrawerTester width={400} />);
-    const content = wrapper.find('.ant-drawer-body').getDOMNode().innerHTML;
-    expect(content).toBe('Here is content of Drawer');
     expect(wrapper.render()).toMatchSnapshot();
   });
 
   it('have a title', () => {
     const wrapper = mount(<DrawerTester title="Test Title" />);
-    const title = wrapper.find('.ant-drawer-title').getDOMNode().innerHTML;
-    expect(title).toBe('Test Title');
-    const closable = wrapper.find('.ant-drawer-close').exists();
-    expect(closable).toBe(true);
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
   it('closable is false', () => {
     const wrapper = mount(<DrawerTester closable={false} />);
-    const closable = wrapper.find('.ant-drawer-close').exists();
-    expect(closable).toBe(false);
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
   it('destroyOnClose is true', () => {
     const wrapper = mount(<DrawerTester destroyOnClose visible={false} />);
-    const body = wrapper.find('.ant-drawer-body').exists();
-    expect(body).toBe(false);
+    expect(wrapper.render()).toMatchSnapshot();
   });
 });
