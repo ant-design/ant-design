88981fef2285e1ba481201e2ca7d46df1a171385
陈帅 <qixian.cs@outlook.com>
2018-07-20 13:31:30 +0800

    style: Adjust the edge shadow

--- components/drawer/__tests__/Drawer.test.js
+++ components/drawer/__tests__/Drawer.test.js
@@ -3,45 +3,6 @@ import { mount } from 'enzyme';
 import Drawer from '..';
 
 describe('Drawer', () => {
-  it('render correctly', () => {
-    const wrapper = mount(
-      <Drawer
-        visible
-        width={400}
-        getContainer={false}
-      >
-        Here is content of Drawer
-      </Drawer>
-    );
-    expect(wrapper.render()).toMatchSnapshot();
-  });
-
-  it('have a title', () => {
-    const wrapper = mount(
-      <Drawer
-        visible
-        title="Test Title"
-        getContainer={false}
-      >
-        Here is content of Drawer
-      </Drawer>
-    );
-    expect(wrapper.render()).toMatchSnapshot();
-  });
-
-  it('closable is false', () => {
-    const wrapper = mount(
-      <Drawer
-        visible
-        closable={false}
-        getContainer={false}
-      >
-        Here is content of Drawer
-      </Drawer>
-    );
-    expect(wrapper.render()).toMatchSnapshot();
-  });
-
   it('destroyOnClose is true', () => {
     const wrapper = mount(
       <Drawer
