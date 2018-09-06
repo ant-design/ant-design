a3ec487229c518fc5901ebc04cce6fe1afb9f488
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-09-06 21:42:37 +0800

    remove fields.js

--- components/icon/utils.ts
+++ components/icon/utils.ts
@@ -1,4 +1,5 @@
 import { ThemeType } from "./index";
+import warning from "../_util/warning";
 
 // These props make sure that the SVG behaviours like general text.
 // Reference: https://blog.prototypr.io/align-svg-icons-to-text-and-say-goodbye-to-font-icons-d44b3d7b26b4
