03b198775a9c19fc81d70e2754dc95c37f54ac3b
zombiej <smith3816@gmail.com>
2018-09-13 16:32:00 +0800

    close #12221. Adjust form validateFields type

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -100,15 +100,21 @@ export type WrappedFormUtils = {
   setFields(obj: Object): void;
   /** 校验并获取一组输入域的值与 Error */
   validateFields(fieldNames: Array<string>, options: Object, callback: ValidateCallback): void;
-  validateFields(fieldNames: Array<string>, callback: ValidateCallback): void;
   validateFields(options: Object, callback: ValidateCallback): void;
+  validateFields(fieldNames: Array<string>, callback: ValidateCallback): void;
+  validateFields(fieldNames: Array<string>, options: Object): void;
+  validateFields(fieldNames: Array<string>): void;
+  validateFields(options: Object): void;
   validateFields(callback: ValidateCallback): void;
   validateFields(): void;
   /** 与 `validateFields` 相似，但校验完后，如果校验不通过的菜单域不在可见范围内，则自动滚动进可见范围 */
-  validateFieldsAndScroll(fieldNames?: Array<string>, options?: Object, callback?: ValidateCallback): void;
-  validateFieldsAndScroll(fieldNames?: Array<string>, callback?: ValidateCallback): void;
-  validateFieldsAndScroll(options?: Object, callback?: ValidateCallback): void;
-  validateFieldsAndScroll(callback?: ValidateCallback): void;
+  validateFieldsAndScroll(fieldNames: Array<string>, options: Object, callback: ValidateCallback): void;
+  validateFieldsAndScroll(options: Object, callback: ValidateCallback): void;
+  validateFieldsAndScroll(fieldNames: Array<string>, callback: ValidateCallback): void;
+  validateFieldsAndScroll(fieldNames: Array<string>, options: Object): void;
+  validateFieldsAndScroll(fieldNames: Array<string>): void;
+  validateFieldsAndScroll(options: Object): void;
+  validateFieldsAndScroll(callback: ValidateCallback): void;
   validateFieldsAndScroll(): void;
   /** 获取某个输入控件的 Error */
   getFieldError(name: string): Object[];
