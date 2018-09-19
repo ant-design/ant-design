93583f3fe0f2bcaf8846b83bedcb5da7a48eefa1
Oren Kosto <oren@panda-os.com>
2018-09-19 15:03:08 +0300

    Added Hebrew translations

--- components/date-picker/locale/he_IL.tsx
+++ components/date-picker/locale/he_IL.tsx
@@ -1,4 +1,4 @@
-import CalendarLocale from 'rc-calendar/lib/locale/he_IL';
+import CalendarLocale from 'rc-calendar/lib/locale/en_US';
 import TimePickerLocale from '../../time-picker/locale/he_IL';
 
 // Merge into a locale object
