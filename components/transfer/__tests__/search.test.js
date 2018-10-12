8b224d1ea0849928988a38ef82f83ce3b75f2af8
zombiej <smith3816@gmail.com>
2018-10-12 22:25:50 +0800

    add test case

--- components/transfer/__tests__/search.test.js
+++ components/transfer/__tests__/search.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Search from '../search';
+import Transfer from '../index';
 
 describe('Search', () => {
   it('should show cross icon when input value exists', () => {
@@ -11,5 +12,41 @@ describe('Search', () => {
     wrapper.setProps({ value: 'a' });
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('onSearch', () => {
+    const dataSource = [{
+      key: 'a',
+      title: 'a',
+      description: 'a',
+    }, {
+      key: 'b',
+      title: 'b',
+      description: 'b',
+    }, {
+      key: 'c',
+      title: 'c',
+      description: 'c',
+    }];
+
+    const onSearch = jest.fn();
+    const wrapper = mount(
+      <Transfer
+        dataSource={dataSource}
+        selectedKeys={[]}
+        targetKeys={[]}
+        render={item => item.title}
+        onSearch={onSearch}
+        showSearch
+      />
+    );
+
+    wrapper.find('.ant-input').at(0).simulate('change', { target: { value: 'a' } });
+    expect(onSearch).toBeCalledWith('left', 'a');
+
+    onSearch.mockReset();
+
+    wrapper.find('.ant-transfer-list-search-action').at(0).simulate('click');
+    expect(onSearch).toBeCalledWith('left', '');
   });
 });
