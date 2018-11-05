c2184d6334a716f48266cbf86c252d5ec5e87f5b
afc163 <afc163@gmail.com>
2018-11-05 11:59:15 +0800

    site: update header responsive ui

--- site/theme/template/Layout/Header.jsx
+++ site/theme/template/Layout/Header.jsx
@@ -200,13 +200,13 @@ export default class Header extends React.Component {
           </Popover>
         )}
         <Row>
-          <Col xxl={4} xl={5} lg={5} md={6} sm={24} xs={24}>
+          <Col xxl={4} xl={5} lg={5} md={5} sm={24} xs={24}>
             <Link to={utils.getLocalizedPathname('/', isZhCN)} id="logo">
               <img alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
               <img alt="Ant Design" src="https://gw.alipayobjects.com/zos/rmsportal/DkKNubTaaVsKURhcVGkh.svg" />
             </Link>
           </Col>
-          <Col xxl={20} xl={19} lg={19} md={18} sm={0} xs={0}>
+          <Col xxl={20} xl={19} lg={19} md={19} sm={0} xs={0}>
             <div id="search-box">
               <Icon type="search" />
               <Input ref={ref => this.searchInput = ref} placeholder={searchPlaceholder} />
