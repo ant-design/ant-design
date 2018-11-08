1dfe22293382097daa99d8b7e87db02f43c6f4b7
ilanus <hasanovtk@gmail.com>
2018-11-08 15:00:33 +0100

    🌐 adding hungarian localisations

---
+++ components/locale-provider/hu_HU.tsx
@@ -0,0 +1,45 @@
+import Pagination from 'rc-pagination/lib/locale/hu_HU';
+import DatePicker from '../date-picker/locale/hu_HU';
+import TimePicker from '../time-picker/locale/hu_HU';
+import Calendar from '../calendar/locale/hu_HU';
+
+export default {
+  locale: 'hu',
+  Pagination,
+  DatePicker,
+  TimePicker,
+  Calendar,
+  Table: {
+    filterTitle: 'Szűrők',
+    filterConfirm: 'Alkalmazás',
+    filterReset: 'Visszaállítás',
+    emptyText: 'Nincs adat',
+    selectAll: 'Jelenlegi oldal kiválasztása',
+    selectInvert: 'Jelenlegi oldal inverze',
+    sortTitle: 'Rendezés',
+  },
+  Modal: {
+    okText: 'Alkalmazás',
+    cancelText: 'Visszavonás',
+    justOkText: 'Alkalmazás',
+  },
+  Popconfirm: {
+    okText: 'Alkalmazás',
+    cancelText: 'Visszavonás',
+  },
+  Transfer: {
+    notFoundContent: 'Nem található',
+    searchPlaceholder: 'Keresés',
+    itemUnit: 'elem',
+    itemsUnit: 'elemek',
+  },
+  Select: {
+    notFoundContent: 'Nem található',
+  },
+  Upload: {
+    uploading: 'Feltöltés...',
+    removeFile: 'Fájl eltávolítása',
+    uploadError: 'Feltöltési hiba',
+    previewFile: 'Fájl előnézet',
+  },
+ };
