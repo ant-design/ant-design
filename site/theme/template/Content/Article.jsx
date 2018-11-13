c1908e78e53a71115eeba2e5a0ba1acf2fe2f375
afc163 <afc163@gmail.com>
2018-11-13 15:24:12 +0800

    :chart_with_upwards_trend: site: fix ga event

--- site/theme/template/Content/Article.jsx
+++ site/theme/template/Content/Article.jsx
@@ -4,7 +4,6 @@ import { FormattedMessage } from 'react-intl';
 import DocumentTitle from 'react-document-title';
 import { getChildren } from 'jsonml.js/lib/utils';
 import { Timeline, Alert, Affix } from 'antd';
-import delegate from 'delegate';
 import EditButton from './EditButton';
 import { ping } from '../utils';
 
@@ -14,12 +13,6 @@ export default class Article extends React.Component {
   }
 
   componentDidMount() {
-    // Add ga event click
-    this.delegation = delegate(this.node, '.resource-card', 'click', (e) => {
-      if (window.ga) {
-        window.ga('send', 'event', 'Download', 'resource', e.delegateTarget.href);
-      }
-    }, false);
     this.componentDidUpdate();
   }
 
@@ -39,9 +32,16 @@ export default class Article extends React.Component {
 
   componentWillUnmount() {
     clearTimeout(this.pingTimer);
-    if (this.delegation) {
-      this.delegation.destroy();
+  }
+
+  onResourceClick = (e) => {
+    if (!window.gtag && e.target.className !== 'resource-card') {
+      return;
     }
+    window.gtag('event', 'resource', {
+      event_category: 'Download',
+      event_label: e.target.href,
+    });
   }
 
   getArticle(article) {
@@ -78,7 +78,8 @@ export default class Article extends React.Component {
     const isNotTranslated = locale === 'en-US' && typeof title === 'object';
     return (
       <DocumentTitle title={`${title[locale] || title} - Ant Design`}>
-        <article className="markdown" ref={(node) => { this.node = node; }}>
+        { /* eslint-disable-next-line */ }
+        <article className="markdown" onClick={this.onResourceClick}>
           {isNotTranslated && (
             <Alert
               type="warning"
