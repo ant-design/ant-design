6f98969dc0272665c0bd38e3f20f8c73fe153326
afc163 <afc163@gmail.com>
2018-11-15 14:43:54 +0800

    :bug: Fix case sensitive module path

--- components/select/__tests__/index.test.js
+++ components/select/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Select from '..';
-import Icon from '../../Icon';
+import Icon from '../../icon';
 import focusTest from '../../../tests/shared/focusTest';
 
 const { Option } = Select;
