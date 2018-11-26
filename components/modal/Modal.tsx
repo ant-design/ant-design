4abc2824e4c022179c96b8e57e192ea2e360b823
afc163 <afc163@gmail.com>
2018-11-26 19:32:33 +0800

    :memo: upgrade ts comment

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -26,7 +26,7 @@ export interface ModalProps {
   /** 点击模态框右上角叉、取消按钮、Props.maskClosable 值为 true 时的遮罩层或键盘按下 Esc 时的回调*/
   onCancel?: (e: React.MouseEvent<any>) => void;
   afterClose?: () => void;
-  /** 居中 */
+  /** 垂直居中 */
   centered?: boolean;
   /** 宽度*/
   width?: string | number;
