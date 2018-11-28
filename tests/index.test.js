647ba193cddae02d211c821a2575af3527379162
afc163 <afc163@gmail.com>
2018-11-28 12:39:10 +0800

    ⚙️  upgrade code style

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 import pkg from '../package.json';
 
 const testDist = process.env.LIB_DIR === 'dist';
@@ -6,7 +7,7 @@ describe('antd dist files', () => {
   // https://github.com/ant-design/ant-design/issues/1638
   // https://github.com/ant-design/ant-design/issues/1968
   it('exports modules correctly', () => {
-    const antd = testDist ? require('../dist/antd') : require('../components'); // eslint-disable-line global-require
+    const antd = testDist ? require('../dist/antd') : require('../components');
     expect(Object.keys(antd)).toMatchSnapshot();
   });
 
@@ -14,7 +15,7 @@ describe('antd dist files', () => {
   // https://github.com/ant-design/ant-design/issues/1804
   if (testDist) {
     it('should have antd.version', () => {
-      const antd = require('../dist/antd'); // eslint-disable-line global-require
+      const antd = require('../dist/antd');
       expect(antd.version).toBe(pkg.version);
     });
   }
