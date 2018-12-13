839d0dcb09980088cd4e9611e9d3dee9b5599d50
yibu.wang <yibu.wang@orion.co.com>
2018-12-13 14:20:02 +0800

    fix

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -776,7 +776,7 @@ export default class Table<T> extends React.Component<TableProps<T>, TableState<
   }
 
   getColumnKey(column: ColumnProps<T>, index?: number) {
-    return column.key || column.dataIndex || index;
+    return column.key || (column.dataIndex as string) || index;
   }
 
   getMaxCurrent(total: number) {
