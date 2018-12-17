8b8c133984b34e26eb22a04a758a3c0b7a4a9c7e
陈帅 <qixian.cs@outlook.com>
2018-12-17 16:10:58 +0800

    fix test ci (#13659)

--- .jest.node.js
+++ .jest.node.js
@@ -1,19 +1,18 @@
-const libDir = process.env.LIB_DIR;
-
+// jest config for server render environment
 module.exports = {
   setupFiles: ['./tests/setup.js'],
-  moduleFileExtensions: ['js', 'md', 'ts', 'tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'md'],
   transform: {
     '\\.tsx?$': './node_modules/antd-tools/lib/jest/codePreprocessor',
     '\\.js$': './node_modules/antd-tools/lib/jest/codePreprocessor',
     '\\.md$': './node_modules/antd-tools/lib/jest/demoPreprocessor',
   },
-  testRegex: `${libDir === 'dist' ? 'demo' : '.*'}\\.test\\.js$`,
+  testRegex: 'demo\\.test\\.js$',
   testEnvironment: 'node',
   snapshotSerializers: ['enzyme-to-json/serializer'],
   globals: {
     'ts-jest': {
-      tsConfig: './tsconfig.test.json',
+      tsConfigFile: './tsconfig.test.json',
     },
   },
 };
