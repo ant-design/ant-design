3c4097fe7207fc000aa7de09a528a7ba77cb9b05
HarlanLuo <luoxwen@gmail.com>
2018-12-24 17:37:19 +0800

    fix doc error and add test case for WeekPicker

--- components/date-picker/__tests__/WeekPicker.test.js
+++ components/date-picker/__tests__/WeekPicker.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import DatePicker from '..';
 import focusTest from '../../../tests/shared/focusTest';
+import { openPanel } from './utils';
 
 const { WeekPicker } = DatePicker;
 
@@ -11,5 +12,40 @@ describe('WeekPicker', () => {
   it('should support style prop', () => {
     const wrapper = mount(<WeekPicker style={{ width: 400 }} />);
     expect(wrapper.render()).toMatchSnapshot();
+  });
+
+  it('extra footer works', () => {
+    const wrapper = mount(
+      <WeekPicker renderExtraFooter={mode => <span className="extra-node">{mode}</span>} />,
+    );
+    openPanel(wrapper);
+
+    let extraNode = wrapper.find('.extra-node');
+    expect(extraNode.length).toBe(1);
+    expect(extraNode.text()).toBe('date');
+
+    wrapper
+      .find('.ant-calendar-month-select')
+      .hostNodes()
+      .simulate('click');
+    extraNode = wrapper.find('.ant-calendar-month-panel .extra-node');
+    expect(extraNode.length).toBe(1);
+    expect(extraNode.text()).toBe('month');
+
+    wrapper
+      .find('.ant-calendar-year-select')
+      .hostNodes()
+      .simulate('click');
+    extraNode = wrapper.find('.ant-calendar-year-panel .extra-node');
+    expect(extraNode.length).toBe(1);
+    expect(extraNode.text()).toBe('year');
+
+    wrapper
+      .find('.ant-calendar-year-panel-decade-select')
+      .hostNodes()
+      .simulate('click');
+    extraNode = wrapper.find('.ant-calendar-decade-panel .extra-node');
+    expect(extraNode.length).toBe(1);
+    expect(extraNode.text()).toBe('decade');
   });
 });
