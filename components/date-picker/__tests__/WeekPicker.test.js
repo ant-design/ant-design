465b85f773177d600b80116a815786d47f45e709
afc163 <afc163@gmail.com>
2018-12-27 14:32:23 +0800

    Fix dateRender not supported at WeekPicker
    close #13957

--- components/date-picker/__tests__/WeekPicker.test.js
+++ components/date-picker/__tests__/WeekPicker.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, render } from 'enzyme';
 import DatePicker from '..';
 import focusTest from '../../../tests/shared/focusTest';
 
@@ -11,5 +11,19 @@ describe('WeekPicker', () => {
   it('should support style prop', () => {
     const wrapper = mount(<WeekPicker style={{ width: 400 }} />);
     expect(wrapper.render()).toMatchSnapshot();
+  });
+
+  it('should support dateRender', () => {
+    const wrapper = mount(
+      <WeekPicker open dateRender={current => <span>{current.format('YYYY-MM-DD')}</span>} />,
+    );
+    expect(
+      render(
+        wrapper
+          .find('Trigger')
+          .instance()
+          .getComponent(),
+      ),
+    ).toMatchSnapshot();
   });
 });
