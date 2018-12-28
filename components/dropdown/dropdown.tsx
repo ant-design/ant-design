9ed455f55d9554bbaf6f772e5e3f1fde09db6f16
zombiej <smith3816@gmail.com>
2018-12-28 18:29:00 +0800

    update overlay

--- components/dropdown/dropdown.tsx
+++ components/dropdown/dropdown.tsx
@@ -16,9 +16,12 @@ const Placements = tuple(
   'bottomRight',
 );
 type Placement = (typeof Placements)[number];
+
+type OverlayFunc = () => React.ReactNode;
+
 export interface DropDownProps {
   trigger?: ('click' | 'hover' | 'contextMenu')[];
-  overlay: React.ReactNode;
+  overlay: React.ReactNode | OverlayFunc;
   onVisibleChange?: (visible?: boolean) => void;
   visible?: boolean;
   disabled?: boolean;
@@ -55,41 +58,30 @@ export default class Dropdown extends React.Component<DropDownProps, any> {
     return 'slide-up';
   }
 
-  componentDidMount() {
+  renderOverlay = (prefixCls: string) => {
+    // rc-dropdown already can process the function of overlay, but we have check logic here.
+    // So we need render the element to check and pass back to rc-dropdown.
     const { overlay } = this.props;
-    if (overlay) {
-      const overlayProps = (overlay as React.ReactElement<any>).props;
-      warning(
-        !overlayProps.mode || overlayProps.mode === 'vertical',
-        `mode="${overlayProps.mode}" is not supported for Dropdown\'s Menu.`,
-      );
+
+    let overlayNode;
+    if (typeof overlay === 'function') {
+      overlayNode = (overlay as OverlayFunc)();
+    } else {
+      overlayNode = overlay;
     }
-  }
+    overlayNode = React.Children.only(overlayNode);
 
-  renderDropDown = ({
-    getPopupContainer: getContextPopupContainer,
-    getPrefixCls,
-  }: ConfigConsumerProps) => {
-    const {
-      prefixCls: customizePrefixCls,
-      children,
-      overlay: overlayElements,
-      trigger,
-      disabled,
-      getPopupContainer,
-    } = this.props;
+    const overlayProps = overlayNode.props;
 
-    const prefixCls = getPrefixCls('dropdown', customizePrefixCls);
-    const child = React.Children.only(children);
-    const overlay = React.Children.only(overlayElements);
+    // Warning if use other mode
+    warning(
+      !overlayProps.mode || overlayProps.mode === 'vertical',
+      `mode="${overlayProps.mode}" is not supported for Dropdown\'s Menu.`,
+    );
 
-    const dropdownTrigger = React.cloneElement(child, {
-      className: classNames(child.props.className, `${prefixCls}-trigger`),
-      disabled,
-    });
     // menu cannot be selectable in dropdown defaultly
     // menu should be focusable in dropdown defaultly
-    const { selectable = false, focusable = true } = overlay.props;
+    const { selectable = false, focusable = true } = overlayProps;
 
     const expandIcon = (
       <span className={`${prefixCls}-menu-submenu-arrow`}>
@@ -98,14 +90,37 @@ export default class Dropdown extends React.Component<DropDownProps, any> {
     );
 
     const fixedModeOverlay =
-      typeof overlay.type === 'string'
+      typeof overlayNode.type === 'string'
         ? overlay
-        : React.cloneElement(overlay, {
+        : React.cloneElement(overlayNode, {
             mode: 'vertical',
             selectable,
             focusable,
             expandIcon,
           });
+
+    return fixedModeOverlay;
+  };
+
+  renderDropDown = ({
+    getPopupContainer: getContextPopupContainer,
+    getPrefixCls,
+  }: ConfigConsumerProps) => {
+    const {
+      prefixCls: customizePrefixCls,
+      children,
+      trigger,
+      disabled,
+      getPopupContainer,
+    } = this.props;
+
+    const prefixCls = getPrefixCls('dropdown', customizePrefixCls);
+    const child = React.Children.only(children);
+
+    const dropdownTrigger = React.cloneElement(child, {
+      className: classNames(child.props.className, `${prefixCls}-trigger`),
+      disabled,
+    });
 
     const triggerActions = disabled ? [] : trigger;
     let alignPoint;
@@ -121,7 +136,7 @@ export default class Dropdown extends React.Component<DropDownProps, any> {
         getPopupContainer={getPopupContainer || getContextPopupContainer}
         transitionName={this.getTransitionName()}
         trigger={triggerActions}
-        overlay={fixedModeOverlay}
+        overlay={() => this.renderOverlay(prefixCls)}
       >
         {dropdownTrigger}
       </RcDropdown>
