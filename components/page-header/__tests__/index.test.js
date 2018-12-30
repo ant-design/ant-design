900f64b94281fa00cd37fa1b1b93ffeff2bb8289
Shuai Chen <wasd2144@hotmail.com>
2018-12-30 08:08:28 +0800

    update snapshots

--- components/page-header/__tests__/index.test.js
+++ components/page-header/__tests__/index.test.js
@@ -19,22 +19,22 @@ describe('PageHeader', () => {
       },
     ];
     const wrapper = mount(<PageHeader title="页面标题" breadcrumb={{ routes }} />);
-    expect(wrapper.find('.ant-pageheader-back-icon')).toHaveLength(0);
+    expect(wrapper.find('.ant-page-header-back-icon')).toHaveLength(0);
   });
   it('pageHeader should  no contain back it back', async () => {
     const wrapper = mount(<PageHeader title="页面标题" backIcon={false} />);
-    expect(wrapper.find('.ant-pageheader-back-icon')).not.toHaveLength(1);
+    expect(wrapper.find('.ant-page-header-back-icon')).not.toHaveLength(1);
   });
 
   it('pageHeader should contain back it back', async () => {
     const wrapper = mount(<PageHeader title="页面标题" />);
-    expect(wrapper.find('.ant-pageheader-back-icon')).toHaveLength(1);
+    expect(wrapper.find('.ant-page-header-back-icon')).toHaveLength(1);
   });
 
   it('pageHeader onBack transfer', async () => {
     const callback = jest.fn(() => true);
     const wrapper = mount(<PageHeader title="页面标题" onBack={callback} />);
-    wrapper.find('.ant-pageheader-back-icon').simulate('click');
+    wrapper.find('.ant-page-header-back-icon').simulate('click');
     expect(callback).toBeCalled();
   });
 
@@ -42,7 +42,7 @@ describe('PageHeader', () => {
     const callback = jest.fn(() => true);
     window.history.back = callback;
     const wrapper = mount(<PageHeader title="页面标题" />);
-    wrapper.find('.ant-pageheader-back-icon').simulate('click');
+    wrapper.find('.ant-page-header-back-icon').simulate('click');
     expect(callback).toBeCalled();
   });
 });
