80f4100c081501352ed3eaed2ebe73b1afd60e5d
zombiej <smith3816@gmail.com>
2019-01-10 18:19:54 +0800

    add edit style

---
+++ components/text/demo/interactive.md
@@ -0,0 +1,25 @@
+---
+order: 1
+title:
+  zh-CN: 可交互
+  en-US: Interactive
+---
+
+## zh-CN
+
+提供额外的交互能力。
+
+## en-US
+
+Provide additional interactive capacity.
+
+```jsx
+import { Text } from 'antd';
+
+ReactDOM.render(
+  <div>
+    <Text editable>editable text</Text>
+  </div>,
+  mountNode
+);
+```
--- components/text/index.tsx
+++ components/text/index.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
 import classNames from 'classnames';
+import omit from 'omit.js';
 import { withConfigConsumer, ConfigConsumerProps } from '../config-provider';
-// import LocaleReceiver from '../locale-provider/LocaleReceiver';
+import Icon from '../icon';
+import Tooltip from '../tooltip';
 
 export interface TextProps {
   prefixCls?: string;
   className?: string;
   style?: React.CSSProperties;
   children?: React.ReactNode;
+  editable?: boolean;
 }
 
 class Text extends React.Component<TextProps & ConfigConsumerProps, {}> {
-  render() {
-    const { children, className, prefixCls, ...restProps } = this.props;
+  renderEdit() {
+    const { editable, prefixCls } = this.props;
+    if (!editable) return;
 
     return (
-      <p className={classNames(prefixCls, className)} {...restProps}>
+      <Tooltip title="edit">
+        <Icon className={`${prefixCls}-edit`} tabIndex={-1} type="edit" />
+      </Tooltip>
+    );
+  }
+
+  render() {
+    const { children, className, prefixCls, editable, ...restProps } = this.props;
+
+    const textProps = omit(restProps, ['prefixCls', 'editable']);
+
+    return (
+      <p className={classNames(prefixCls, className)} {...textProps}>
         {children}
+        {this.renderEdit()}
       </p>
     );
   }
