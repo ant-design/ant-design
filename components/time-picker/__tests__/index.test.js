656342a2bf482435856753dc4c08554bbbc73b78
zombiej <smith3816@gmail.com>
2019-01-22 22:54:36 +0800

    add test case of allowClear

--- components/time-picker/__tests__/index.test.js
+++ components/time-picker/__tests__/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, render } from 'enzyme';
 import RcTimePicker from 'rc-time-picker/lib/TimePicker';
+import moment from 'moment';
 import TimePicker from '..';
 import focusTest from '../../../tests/shared/focusTest';
 
@@ -31,5 +32,12 @@ describe('TimePicker', () => {
     expect(errorSpy).toBeCalledWith(
       'Warning: `allowEmpty` in TimePicker is deprecated. Please use `allowClear` instead.',
     );
+  });
+
+  it('not render clean icon when allowClear is false', () => {
+    const wrapper = mount(
+      <TimePicker defaultValue={moment('2000-01-01 00:00:00')} allowClear={false} />,
+    );
+    expect(wrapper.render()).toMatchSnapshot();
   });
 });
