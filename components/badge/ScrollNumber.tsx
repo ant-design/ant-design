d30fdf11d705d99b4f58114fad147392ea8bdbb8
ztplz <mysticzt@gmail.com>
2019-01-24 12:17:38 +0800

    Fix changing the count of a badge with border-color raises an error (#14525)
    fix #14516

--- components/badge/ScrollNumber.tsx
+++ components/badge/ScrollNumber.tsx
@@ -169,7 +169,10 @@ export default class ScrollNumber extends Component<ScrollNumberProps, ScrollNum
     // mock border-color by box-shadow for compatible with old usage:
     // <Badge count={4} style={{ backgroundColor: '#fff', color: '#999', borderColor: '#d9d9d9' }} />
     if (style && style.borderColor) {
-      newProps.style.boxShadow = `0 0 0 1px ${style.borderColor} inset`;
+      newProps.style = {
+        ...style,
+        boxShadow: `0 0 0 1px ${style.borderColor} inset`,
+      };
     }
     if (displayComponent) {
       return React.cloneElement(displayComponent, {
