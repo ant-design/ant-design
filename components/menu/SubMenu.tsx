4ca822a0746637ab9a8c9dbad370b581140f9c98
zombiej <smith3816@gmail.com>
2019-01-28 12:32:23 +0800

    update

--- components/menu/SubMenu.tsx
+++ components/menu/SubMenu.tsx
@@ -3,12 +3,17 @@ import * as PropTypes from 'prop-types';
 import { SubMenu as RcSubMenu } from 'rc-menu';
 import classNames from 'classnames';
 
+interface TitleClickEntity {
+  key: string;
+  domEvent: Event;
+}
+
 export interface SubMenuProps {
   rootPrefixCls?: string;
   className?: string;
   disabled?: boolean;
   title?: React.ReactNode;
-  onTitleClick?: (key: string, domEvent?: Event) => void;
+  onTitleClick?: (clickEntity: TitleClickEntity) => void;
 }
 
 class SubMenu extends React.Component<SubMenuProps, any> {
