4a5d46acbff6539191126c4545a80bf494a98e8f
Shuai Chen <wasd2144@hotmail.com>
2019-01-29 15:31:15 +0800

    fix ci

--- components/result/__tests__/index.test.js
+++ components/result/__tests__/index.test.js
@@ -7,7 +7,7 @@ describe('Progress', () => {
   it('successPercent should decide the progress status when it exists', () => {
     const wrapper = mount(
       <Result
-        icon="success"
+        status="success"
         title="Successfully Purchased Cloud Server ECS!"
         subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
         extra={[
