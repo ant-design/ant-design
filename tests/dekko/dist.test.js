74eb7bd40eb017b166721dd313f9cbeceab59f02
zy410419243 <zy410419243@gmail.com>
2019-02-04 15:10:21 +0800

    fix: throw error correctly by dekko

--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -1,12 +1,14 @@
 const $ = require('dekko');
 const chalk = require('chalk');
 
-$('dist')
-  .isDirectory()
-  .hasFile('antd.css')
-  .hasFile('antd.min.css')
-  .hasFile('antd.js')
-  .hasFile('antd.min.js');
-
-// eslint-disable-next-line
-console.log(chalk.green('✨ `dist` directory is valid.'));
+try {
+  $('dist')
+    .isDirectory()
+    .hasFile('antd.css')
+    .hasFile('antd.min.css')
+    .hasFile('antd.js')
+    .hasFile('antd.min.js');
+} catch (error) {
+  // eslint-disable-next-line
+  console.log(chalk.red('✨ ' + error));
+}
--- tests/dekko/lib.test.js
+++ tests/dekko/lib.test.js
@@ -1,24 +1,26 @@
 const $ = require('dekko');
 const chalk = require('chalk');
 
-$('lib')
-  .isDirectory()
-  .hasFile('index.js')
-  .hasFile('index.d.ts');
+try {
+  $('lib')
+    .isDirectory()
+    .hasFile('index.js')
+    .hasFile('index.d.ts');
 
-$('lib/*')
-  .filter(filename => !filename.endsWith('index.js') && !filename.endsWith('index.d.ts'))
-  .isDirectory()
-  .filter(filename => !filename.endsWith('style') && !filename.endsWith('_util'))
-  .hasFile('index.js')
-  .hasFile('index.d.ts')
-  .hasDirectory('style');
+  $('lib/*')
+    .filter(filename => !filename.endsWith('index.js') && !filename.endsWith('index.d.ts'))
+    .isDirectory()
+    .filter(filename => !filename.endsWith('style') && !filename.endsWith('_util'))
+    .hasFile('index.js')
+    .hasFile('index.d.ts')
+    .hasDirectory('style');
 
-$('lib/*/style')
-  .hasFile('css.js')
-  .hasFile('index.js');
+  $('lib/*/style')
+    .hasFile('css.js')
+    .hasFile('index.js');
 
-$('lib/style').hasFile('v2-compatible-reset.css');
-
-// eslint-disable-next-line
-console.log(chalk.green('✨ `lib` directory is valid.'));
+  $('lib/style').hasFile('v2-compatible-reset.css');
+} catch (error) {
+  // eslint-disable-next-line
+  console.log(chalk.red('✨ ' + error));
+}
