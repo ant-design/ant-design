4ce7868e9e7f288763e62f1065e8276103984d65
zombiej <smith3816@gmail.com>
2019-02-13 20:56:50 +0800

    use tagName of article

--- components/typography/Typography.tsx
+++ components/typography/Typography.tsx
@@ -18,7 +18,7 @@ interface InternalTypographyProps extends TypographyProps {
 
 const Typography: React.SFC<InternalTypographyProps> = ({
   prefixCls: customizePrefixCls,
-  component = 'div',
+  component = 'article',
   className,
   ['aria-label']: ariaLabel,
   setContentRef,
