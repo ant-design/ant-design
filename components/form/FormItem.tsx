be9439e748adab1adb4ff7c6fdf8a129819553e9
zombiej <smith3816@gmail.com>
2019-02-19 23:00:40 +0800

    Revert "Adjust style to be similar with origin (#14886)"
    This reverts commit 735175e3cf51870fef8151ad2c381f69f5073fd8.

--- components/form/FormItem.tsx
+++ components/form/FormItem.tsx
@@ -157,36 +157,25 @@ export default class FormItem extends React.Component<FormItemProps, any> {
   renderHelp(prefixCls: string) {
     const help = this.getHelpMessage();
     const children = help ? (
-      <div className={`${prefixCls}-explain`} key="help">
+      <span className={`${prefixCls}-explain`} key="help">
         {help}
-      </div>
+      </span>
     ) : null;
     if (children) {
       this.helpShow = !!children;
     }
 
     return (
-      <Animate
-        transitionName="show-help"
-        component=""
-        transitionAppear
-        key="help"
-        onEnd={this.onHelpAnimEnd}
-      >
-        {children}
-      </Animate>
-    );
-  }
-
-  renderHelpHolder(prefixCls: string) {
-    const { style } = this.props;
-    if (this.helpShow || (style && ('margin' in style || 'marginBottom' in style))) {
-      return null;
-    }
-
-    return (
-      <div className={`${prefixCls}-explain-holder`} aria-hidden="true">
-        {'\u00A0'}
+      <div className={`${prefixCls}-explain-holder`}>
+        <Animate
+          transitionName="show-help"
+          component=""
+          transitionAppear
+          key="help"
+          onEnd={this.onHelpAnimEnd}
+        >
+          {children}
+        </Animate>
       </div>
     );
   }
@@ -220,7 +209,6 @@ export default class FormItem extends React.Component<FormItemProps, any> {
     c1: React.ReactNode,
     c2: React.ReactNode,
     c3: React.ReactNode,
-    helperHolder: React.ReactNode,
   ) {
     const props = this.props;
     const onlyControl = this.getOnlyControl;
@@ -274,7 +262,6 @@ export default class FormItem extends React.Component<FormItemProps, any> {
         </span>
         {c2}
         {c3}
-        {helperHolder}
       </div>
     );
   }
@@ -378,7 +365,6 @@ export default class FormItem extends React.Component<FormItemProps, any> {
           children,
           this.renderHelp(prefixCls),
           this.renderExtra(prefixCls),
-          this.renderHelpHolder(prefixCls),
         ),
       ),
     ];
--- components/form/demo/form-in-modal.md
+++ components/form/demo/form-in-modal.md
@@ -45,7 +45,7 @@ const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
             <Form.Item label="Description">
               {getFieldDecorator('description')(<Input type="textarea" />)}
             </Form.Item>
-            <Form.Item style={{ marginBottom: 0 }}>
+            <Form.Item className="collection-create-form_last-form-item">
               {getFieldDecorator('modifier', {
                 initialValue: 'public',
               })(
@@ -110,3 +110,8 @@ class CollectionsPage extends React.Component {
 ReactDOM.render(<CollectionsPage />, mountNode);
 ````
 
+````css
+.collection-create-form_last-form-item {
+  margin-bottom: 0;
+}
+````
