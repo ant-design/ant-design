8221cec9f3ae44fc04d4de8550a514f770780c1a
yoyo837 <yoyo837@hotmail.com>
2019-03-04 12:39:57 +0800

    fix Warning: Each child in a list should have a unique "key" prop.

--- components/form/FormItem.tsx
+++ components/form/FormItem.tsx
@@ -259,7 +259,7 @@ export default class FormItem extends React.Component<FormItemProps, any> {
 
   renderWrapper(prefixCls: string, children: React.ReactNode) {
     return (
-      <FormContext.Consumer>
+      <FormContext.Consumer key="wrapper">
         {({ wrapperCol: contextWrapperCol, vertical }: FormContextProps) => {
           const { wrapperCol } = this.props;
           const mergedWrapperCol: ColProps =
@@ -328,7 +328,7 @@ export default class FormItem extends React.Component<FormItemProps, any> {
 
   renderLabel(prefixCls: string) {
     return (
-      <FormContext.Consumer>
+      <FormContext.Consumer key="label">
         {({ vertical, labelCol: contextLabelCol }: FormContextProps) => {
           const { label, labelCol, colon, id } = this.props;
           const required = this.isRequired();
