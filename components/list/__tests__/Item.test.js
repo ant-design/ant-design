ac86e38a2598fd9407b3a4d7e11828a15398b1ac
afc163 <afc163@gmail.com>
2019-03-11 19:39:42 +0800

    :white_check_mark: Add test cases

---
+++ components/list/__tests__/Item.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import List from '..';
+
+describe('List Item Layout', () => {
+  it('horizontal itemLayout List which contains string nodes should not be flex container', async () => {
+  });
+
+  it('horizontal itemLayout List should accept extra node', async () => {
+  });
+
+  it('horizontal itemLayout List should be flex container defaultly', async () => {
+  });
+
+  it('vertical itemLayout List should', async () => {
+  });
+});
