f9d8e50a1ff37c54f1cfa0ab63648abdc665e433
zombiej <smith3816@gmail.com>
2019-03-13 16:04:42 +0800

    rm input from datePicker

--- components/date-picker/style/index.tsx
+++ components/date-picker/style/index.tsx
@@ -2,7 +2,5 @@ import '../../style/index.less';
 import './index.less';
 
 // style dependencies
-// deps-lint-skip: input
-import '../../input/style';
 import '../../time-picker/style';
 import '../../tag/style';
