f8baa3787c402278c673de6b56c59b4586984249
zombiej <smith3816@gmail.com>
2019-03-13 11:33:42 +0800

    update tree-select

--- components/tree-select/style/index.tsx
+++ components/tree-select/style/index.tsx
@@ -2,6 +2,7 @@ import '../../style/index.less';
 import './index.less';
 
 // style dependencies
-// deps-lint-skip: select, empty, checkbox
+// deps-lint-skip: select, checkbox
 import '../../select/style';
+import '../../empty/style';
 import '../../checkbox/style';
