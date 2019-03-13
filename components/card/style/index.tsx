04226e36eb1beab80ea21c257ace478e53213bf1
zombiej <smith3816@gmail.com>
2019-03-13 12:01:44 +0800

    add missing style of card

--- components/card/style/index.tsx
+++ components/card/style/index.tsx
@@ -2,5 +2,6 @@ import '../../style/index.less';
 import './index.less';
 
 // style dependencies
-// deps-lint-skip: row, col
 import '../../tabs/style';
+import '../../row/style';
+import '../../col/style';
