6bea64b7833c2448d704d4e455ec5f473c8380c7
Michal Pekar <pekac45@gmail.com>
2019-03-18 10:58:08 +0000

    Update readme, typo - space after comma. (#15489)
    Sentence: ... applications,including the official React implementation ...

--- site/theme/en-US.js
+++ site/theme/en-US.js
@@ -31,7 +31,7 @@ module.exports = {
     'app.home.design-language': 'Design Language',
     'app.home.solution': 'Solution',
     'app.home.components-explain':
-      'Based on the Ant Design language, we have provided a suite of out-of-the-box with high quality for developing and serving enterprise background applications,including the official React implementation and Angular, Vue implementations',
+      'Based on the Ant Design language, we have provided a suite of out-of-the-box with high quality for developing and serving enterprise background applications, including the official React implementation and Angular, Vue implementations',
     'app.home.product-pro-slogan': 'Out-of-the-box front-end / Design solution',
     'app.home.product-mobile-slogan':
       "antd-mobile is the implementation of Ant Design's mobile specification",
