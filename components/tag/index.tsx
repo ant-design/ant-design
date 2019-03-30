e2495fafe0293672f71d01fe5e67cc2fa6419fbd
jiang <155259966@qq.com>
2019-03-30 20:36:35 +0800

    Rm tag anim (#15233)
    * rm tag default anim, add animate demo
    
    * rm InnerTag
    
    * rm animate
    
    * rm afterClose test
    
    * update warning
    
    * update animation demo
    
    * fix typo
    
    * update wraning
    
    * fix tag test
    
    * rebase master, test tag
    
    * update tag to Tag
    
    * update animation explanation
    
    * update en-us

--- components/tag/index.tsx
+++ components/tag/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Animate from 'rc-animate';
 import classNames from 'classnames';
 import omit from 'omit.js';
 import { polyfill } from 'react-lifecycles-compat';
@@ -7,6 +6,7 @@ import Icon from '../icon';
 import CheckableTag from './CheckableTag';
 import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
 import Wave from '../_util/wave';
+import warning from '../_util/warning';
 
 export { CheckableTagProps } from './CheckableTag';
 
@@ -24,15 +24,6 @@ export interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
 interface TagState {
   visible: boolean;
 }
-
-interface InnterTagProps extends TagProps {
-  show: boolean;
-}
-
-const InnerTag = ({ show, ...restProps }: InnterTagProps) => {
-  const divProps = omit(restProps, ['onClose', 'afterClose', 'color', 'visible', 'closable']);
-  return <div {...divProps} />;
-};
 
 class Tag extends React.Component<TagProps, TagState> {
   static CheckableTag = CheckableTag;
@@ -53,10 +44,23 @@ class Tag extends React.Component<TagProps, TagState> {
     visible: true,
   };
 
+  constructor(props: TagProps) {
+    super(props);
+    warning(
+      !('afterClose' in props),
+      'Tag',
+      "'afterClose' will be deprecated, please use 'onClose', we will remove this in the next version.",
+    );
+  }
+
   setVisible(visible: boolean, e: React.MouseEvent<HTMLElement>) {
-    const { onClose } = this.props;
+    const { onClose, afterClose } = this.props;
     if (onClose) {
       onClose(e);
+    }
+    if (afterClose && !onClose) {
+      // next version remove.
+      afterClose();
     }
     if (e.defaultPrevented) {
       return;
@@ -68,15 +72,6 @@ class Tag extends React.Component<TagProps, TagState> {
 
   handleIconClick = (e: React.MouseEvent<HTMLElement>) => {
     this.setVisible(false, e);
-  };
-
-  animationEnd = (_: string, existed: boolean) => {
-    if (!existed) {
-      const { afterClose } = this.props;
-      if (afterClose) {
-        afterClose();
-      }
-    }
   };
 
   isPresetColor(color?: string): boolean {
@@ -119,28 +114,14 @@ class Tag extends React.Component<TagProps, TagState> {
   }
 
   renderTag = (configProps: ConfigConsumerProps) => {
-    const { getPrefixCls } = configProps;
     const { prefixCls: customizePrefixCls, children, ...otherProps } = this.props;
-    const { visible } = this.state;
-    const prefixCls = getPrefixCls('tag', customizePrefixCls);
+    const divProps = omit(otherProps, ['onClose', 'afterClose', 'color', 'visible', 'closable']);
     return (
       <Wave>
-        <Animate
-          component=""
-          showProp="show"
-          transitionName={`${prefixCls}-zoom`}
-          onEnd={this.animationEnd}
-        >
-          <InnerTag
-            show={visible}
-            {...otherProps}
-            className={this.getTagClassName(configProps)}
-            style={this.getTagStyle()}
-          >
-            {children}
-            {this.renderCloseIcon()}
-          </InnerTag>
-        </Animate>
+        <div {...divProps} className={this.getTagClassName(configProps)} style={this.getTagStyle()}>
+          {children}
+          {this.renderCloseIcon()}
+        </div>
       </Wave>
     );
   };
