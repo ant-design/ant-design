65e4c290dce28c2bf8da15a6f694a46e293063e0
zombiej <smith3816@gmail.com>
2019-04-03 18:06:36 +0800

    fix deps

--- components/transfer/style/index.tsx
+++ components/transfer/style/index.tsx
@@ -1,9 +1,9 @@
 import '../../style/index.less';
 import './index.less';
-import './table.less';
 
 // style dependencies
 import '../../empty/style';
 import '../../checkbox/style';
 import '../../button/style';
 import '../../input/style';
+import '../../table/style';
