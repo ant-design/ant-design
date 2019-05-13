7cdcc7cc2fdc221dfeff6418a28d6992fb22d6aa
Gin-X <xcbwyys@hotmail.com>
2019-05-13 17:54:18 +0800

    fix children's type of interface Route (#16550)

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -11,7 +11,7 @@ import { Omit } from '../_util/type';
 export interface Route {
   path: string;
   breadcrumbName: string;
-  children: Omit<Route, 'children'>[];
+  children?: Omit<Route, 'children'>[];
 }
 
 export interface BreadcrumbProps {
