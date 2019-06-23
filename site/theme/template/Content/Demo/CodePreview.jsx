313c3cc84569d7ed1acdf0485906e51ce740f308
zombiej <smith3816@gmail.com>
2019-06-23 12:03:38 +0800

    support lang panel

--- site/theme/template/Content/Demo/CodePreview.jsx
+++ site/theme/template/Content/Demo/CodePreview.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
 import { Radio, Tabs } from 'antd';
 
+const { TabPane } = Tabs;
+
 const LANGS = {
   tsx: 'TypeScript',
   jsx: 'JavaScript',
 };
 
 const CodePreview = ({ toReactComponent, codes }) => {
-  const langList = Object.keys(codes);
-  const [lang, setLang] = React.useState('jsx');
-
-  React.useEffect(() => {
-    if ('tsx' in codes) {
-      setLang('tsx');
-    }
-  }, [langList]);
+  const langList = Object.keys(codes).sort();
 
   return (
     <div className="highlight">
-      <div className="lang-list">
-        <Radio.Group>
-          {langList.map(val => (
-            <Radio key={val} value={val}>
-              {LANGS[val]}
-            </Radio>
-          ))}
-        </Radio.Group>
-      </div>
-
-      {toReactComponent([
-        'pre',
-        {
-          lang,
-          highlighted: codes[lang],
-        },
-      ])}
+      <Tabs>
+        {langList.map(lang => (
+          <TabPane tab={LANGS[lang]} key={lang}>
+            {toReactComponent([
+              'pre',
+              {
+                lang,
+                highlighted: codes[lang],
+              },
+            ])}
+          </TabPane>
+        ))}
+      </Tabs>
     </div>
   );
 };
