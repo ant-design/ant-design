20fc91ac99e08b034b5b4fe2877f6fc2e23d1eee
zombiej <smith3816@gmail.com>
2019-06-23 14:43:20 +0800

    init form

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -1,3 +1,8 @@
-import Form from 'rc-field-form';
+import * as React from 'react';
+import FieldForm, { FormInstance } from 'rc-field-form';
+
+const Form = React.forwardRef<FormInstance>((props, ref) => {
+  return <FieldForm {...props} ref={ref} />;
+});
 
 export default Form;
