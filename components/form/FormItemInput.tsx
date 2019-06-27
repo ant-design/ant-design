eed3ad2a4abdac3f40e877a76769f0ee9a7ef886
zombiej <smith3816@gmail.com>
2019-06-27 17:49:50 +0800

    fix ts

--- components/form/FormItemInput.tsx
+++ components/form/FormItemInput.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import classNames from 'classnames';
 import Icon from '../icon';
 import CSSMotion from 'rc-animate/lib/CSSMotion';
--- components/form/FormItemLabel.tsx
+++ components/form/FormItemLabel.tsx
@@ -1,4 +1,4 @@
-import * as React from 'React';
+import * as React from 'react';
 import classNames from 'classnames';
 import Col, { ColProps } from '../grid/col';
 import { FormLabelAlign } from './interface';
