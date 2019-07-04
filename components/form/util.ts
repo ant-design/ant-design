21788966a7185e70a60b17bf2f7b6b9c1d8078b2
zombiej <smith3816@gmail.com>
2019-07-04 12:13:43 +0800

    adjust logic of label id

--- components/form/util.ts
+++ components/form/util.ts
@@ -63,7 +63,9 @@ export function toArray<T>(candidate?: T | T[] | false): T[] {
   return Array.isArray(candidate) ? candidate : [candidate];
 }
 
-export function getFieldId(namePath: InternalNamePath, formName?: string) {
+export function getFieldId(namePath: InternalNamePath, formName?: string): string | undefined {
+  if (!namePath.length) return undefined;
+
   const mergedId = namePath.join('_');
   return formName ? `${formName}_${mergedId}` : mergedId;
 }
