97b9f25a3ecea8c1bf0538f69c1c164e0e0bc403
zombiej <smith3816@gmail.com>
2019-07-22 15:15:02 +0800

    clean up

--- components/transfer/__tests__/list.test.js
+++ components/transfer/__tests__/list.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 import List from '../list';
 import Checkbox from '../../checkbox';
-import raf from '../../_util/raf';
 
 const listCommonProps = {
   prefixCls: 'ant-transfer-list',
