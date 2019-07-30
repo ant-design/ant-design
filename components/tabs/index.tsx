0c9752c4f9d5bde8fa5f98e5e9cfc7017f86b602
wang <wangchao@treefinance.com.cn>
2019-07-30 14:16:47 +0800

    bugfix

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -125,7 +125,6 @@ export default class Tabs extends React.Component<TabsProps, any> {
       [`${prefixCls}-${type}`]: true,
       [`${prefixCls}-no-animation`]: !tabPaneAnimated,
     });
-
     // only card type tabs can be added and closed
     let childrenWithClose: React.ReactElement<any>[] = [];
     if (type === 'editable-card') {
