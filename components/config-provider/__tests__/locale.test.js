f6566571ac67e797501032dc2c0595c1a2e20721
zombieJ <smith3816@gmail.com>
2019-08-06 00:27:47 +0800

    fix: ConfigProvider should not block LocaleProvider (#18105)

--- components/config-provider/__tests__/locale.test.js
+++ components/config-provider/__tests__/locale.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import ConfigProvider from '..';
+import LocaleProvider from '../../locale-provider';
+import locale from '../../locale/zh_CN';
+import TimePicker from '../../time-picker';
 
 describe('ConfigProvider.Locale', () => {
   it('not throw', () => {
@@ -14,5 +17,45 @@ describe('ConfigProvider.Locale', () => {
         <span />
       </ConfigProvider>,
     );
+  });
+
+  describe('support legacy LocaleProvider', () => {
+    function testLocale(wrapper) {
+      expect(wrapper.find('input').props().placeholder).toBe(locale.TimePicker.placeholder);
+    }
+
+    it('LocaleProvider', () => {
+      const wrapper = mount(
+        <LocaleProvider locale={locale}>
+          <TimePicker />
+        </LocaleProvider>,
+      );
+
+      testLocale(wrapper);
+    });
+
+    it('LocaleProvider > ConfigProvider', () => {
+      const wrapper = mount(
+        <LocaleProvider locale={locale}>
+          <ConfigProvider>
+            <TimePicker />
+          </ConfigProvider>
+        </LocaleProvider>,
+      );
+
+      testLocale(wrapper);
+    });
+
+    it('ConfigProvider > ConfigProvider', () => {
+      const wrapper = mount(
+        <ConfigProvider locale={locale}>
+          <ConfigProvider>
+            <TimePicker />
+          </ConfigProvider>
+        </ConfigProvider>,
+      );
+
+      testLocale(wrapper);
+    });
   });
 });
