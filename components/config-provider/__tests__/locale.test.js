7cad1b20052a59850b9fb8ce9c1a22d3dbfe7bb5
zombiej <smith3816@gmail.com>
2019-08-05 11:11:08 +0800

    skip react 15

--- components/config-provider/__tests__/locale.test.js
+++ components/config-provider/__tests__/locale.test.js
@@ -4,6 +4,10 @@ import ConfigProvider from '..';
 
 describe('ConfigProvider.Locale', () => {
   it('not throw', () => {
+    if (process.env.REACT === '15') {
+      return;
+    }
+
     mount(
       <ConfigProvider locale={{}}>
         <span />
