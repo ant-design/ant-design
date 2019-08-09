3ef5661a1f7c094917b890994f3dbe3de5cbcd3d
afc163 <afc163@gmail.com>
2019-08-09 15:25:10 +0800

    :white_check_mark: Add test case for AutoComplete

--- components/auto-complete/__tests__/index.test.js
+++ components/auto-complete/__tests__/index.test.js
@@ -22,4 +22,36 @@ describe('AutoComplete with Custom Input Element Render', () => {
     // should not filter data source defaultly
     expect(dropdownWrapper.find('MenuItem').length).toBe(3);
   });
+
+  it('AutoComplete should work when dataSource is object array', () => {
+    const wrapper = mount(
+      <AutoComplete dataSource={[{ text: 'text', value: 'value' }, { text: 'abc', value: 'xxx' }]}>
+        <input />
+      </AutoComplete>,
+    );
+    expect(wrapper.find('input').length).toBe(1);
+    wrapper.find('input').simulate('change', { target: { value: 'a' } });
+    const dropdownWrapper = mount(
+      wrapper
+        .find('Trigger')
+        .instance()
+        .getComponent(),
+    );
+
+    // should not filter data source defaultly
+    expect(dropdownWrapper.find('MenuItem').length).toBe(2);
+  });
+
+  it('AutoComplete throws error when contains invalid dataSource', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => undefined);
+    expect(() => {
+      mount(
+        <AutoComplete dataSource={[() => {}]}>
+          <textarea />
+        </AutoComplete>,
+      );
+    }).toThrow();
+    // eslint-disable-next-line no-console
+    console.error.mockRestore();
+  });
 });
