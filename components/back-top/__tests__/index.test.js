78556b6ff44403254c2daadd29dff199682884d1
偏右 <afc163@gmail.com>
2019-08-22 18:04:10 +0800

    :white_check_mark: increase BackTop code coverage (#18406)

--- components/back-top/__tests__/index.test.js
+++ components/back-top/__tests__/index.test.js
@@ -19,4 +19,27 @@ describe('BackTop', () => {
     expect(window.pageYOffset).toBe(0);
     scrollToSpy.mockRestore();
   });
+
+  it('could be unmount without error', async () => {
+    const wrapper = mount(<BackTop />);
+    expect(() => {
+      wrapper.unmount();
+    }).not.toThrow();
+  });
+
+  it('support onClick', async () => {
+    const onClick = jest.fn();
+    const wrapper = mount(<BackTop onClick={onClick} visibilityHeight={-1} />);
+    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((x, y) => {
+      window.scrollY = y;
+      window.pageYOffset = y;
+    });
+    window.scrollTo(0, 400);
+    // trigger scroll manually
+    wrapper.instance().handleScroll();
+    await sleep();
+    wrapper.find('.ant-back-top').simulate('click');
+    expect(onClick).toHaveBeenCalled();
+    scrollToSpy.mockRestore();
+  });
 });
