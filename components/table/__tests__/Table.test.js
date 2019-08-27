d11978f43ab970322b4ec59c2e58819ce3bf1aec
kristof0425 <dombi.kristof@gmail.com>
2019-08-27 20:35:39 +0200

    fix(Table): move rowSelection logic to getDerivedStateFromProps

--- components/table/__tests__/Table.test.js
+++ components/table/__tests__/Table.test.js
@@ -61,7 +61,7 @@ describe('Table', () => {
     ];
     wrapper.setProps({ columns: newColumns });
 
-    expect(wrapper.instance().columns).toBe(newColumns);
+    expect(wrapper.dive().instance().columns).toBe(newColumns);
   });
 
   it('loading with Spin', async () => {
