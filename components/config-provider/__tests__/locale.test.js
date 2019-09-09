7743967bf6bf54f63cc3a540c739c05a8c9c1982
afc163 <afc163@gmail.com>
2019-09-09 11:44:54 +0800

    :bug: Fix ConfigProvider locale not working for confirm
    close #18731

--- components/config-provider/__tests__/locale.test.js
+++ components/config-provider/__tests__/locale.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { mount } from 'enzyme';
 import ConfigProvider from '..';
 import LocaleProvider from '../../locale-provider';
-import locale from '../../locale/zh_CN';
+import zhCN from '../../locale/zh_CN';
+import enUS from '../../locale/en_US';
 import TimePicker from '../../time-picker';
+import Modal from '../../modal';
 
 describe('ConfigProvider.Locale', () => {
+  function $$(className) {
+    return document.body.querySelectorAll(className);
+  }
+
   it('not throw', () => {
     if (process.env.REACT === '15') {
       return;
@@ -19,14 +25,54 @@ describe('ConfigProvider.Locale', () => {
     );
   });
 
+  // https://github.com/ant-design/ant-design/issues/18731
+  it('should not reset locale for Modal', () => {
+    class App extends React.Component {
+      state = {
+        showButton: false,
+      };
+
+      componentDidMount() {
+        this.setState({
+          showButton: true,
+        });
+      }
+
+      openConfirm = () => {
+        Modal.confirm({
+          title: 'title',
+          content: 'Some descriptions',
+        });
+      };
+
+      render() {
+        return (
+          <ConfigProvider locale={zhCN}>
+            {this.state.showButton ? (
+              <ConfigProvider locale={enUS}>
+                <button type="button" onClick={this.openConfirm}>
+                  open
+                </button>
+              </ConfigProvider>
+            ) : null}
+          </ConfigProvider>
+        );
+      }
+    }
+
+    const wrapper = mount(<App />);
+    wrapper.find('button').simulate('click');
+    expect($$('.ant-btn-primary')[0].textContent).toBe('OK');
+  });
+
   describe('support legacy LocaleProvider', () => {
     function testLocale(wrapper) {
-      expect(wrapper.find('input').props().placeholder).toBe(locale.TimePicker.placeholder);
+      expect(wrapper.find('input').props().placeholder).toBe(zhCN.TimePicker.placeholder);
     }
 
     it('LocaleProvider', () => {
       const wrapper = mount(
-        <LocaleProvider locale={locale}>
+        <LocaleProvider locale={zhCN}>
           <TimePicker />
         </LocaleProvider>,
       );
@@ -36,7 +82,7 @@ describe('ConfigProvider.Locale', () => {
 
     it('LocaleProvider > ConfigProvider', () => {
       const wrapper = mount(
-        <LocaleProvider locale={locale}>
+        <LocaleProvider locale={zhCN}>
           <ConfigProvider>
             <TimePicker />
           </ConfigProvider>
@@ -48,7 +94,7 @@ describe('ConfigProvider.Locale', () => {
 
     it('ConfigProvider > ConfigProvider', () => {
       const wrapper = mount(
-        <ConfigProvider locale={locale}>
+        <ConfigProvider locale={zhCN}>
           <ConfigProvider>
             <TimePicker />
           </ConfigProvider>
