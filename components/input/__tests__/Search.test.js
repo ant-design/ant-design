6f695b050ed1e61251b191dead5fea7f248a3f64
afc163 <afc163@gmail.com>
2019-09-12 15:47:02 +0800

    :bug: Input.Search should trigger onSearch when clear
    close #18729

--- components/input/__tests__/Search.test.js
+++ components/input/__tests__/Search.test.js
@@ -137,4 +137,18 @@ describe('Input.Search', () => {
     expect(wrapper.render()).toMatchSnapshot();
     expect(wrapperWithEnterButton.render()).toMatchSnapshot();
   });
+
+  // https://github.com/ant-design/ant-design/issues/18729
+  it('should trigger onSearch when click clear icon', () => {
+    const onSearch = jest.fn();
+    const wrapper = mount(<Search allowClear defaultValue="value" onSearch={onSearch} />);
+    wrapper
+      .find('.ant-input-clear-icon')
+      .at(0)
+      .simulate('click');
+    expect(onSearch).toHaveBeenLastCalledWith(
+      '',
+      expect.anything(),
+    );
+  });
 });
