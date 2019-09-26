2c864b9bc637d0bf4028ef142c33be16712afabc
swillis12 <35236833+swillis12@users.noreply.github.com>
2019-09-25 23:02:08 -0700

    Add sortColumn to title render (#19012)
    Update docs
    
    Update TS interface
    
    Add null
    
    Fix lint error

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -1203,12 +1203,13 @@ export default class Table<T> extends React.Component<TableProps<T>, TableState<
   }
 
   renderColumnTitle(title: ColumnProps<T>['title']) {
-    const { filters, sortOrder } = this.state;
+    const { filters, sortOrder, sortColumn } = this.state;
     // https://github.com/ant-design/ant-design/issues/11246#issuecomment-405009167
     if (title instanceof Function) {
       return title({
         filters,
         sortOrder,
+        sortColumn,
       });
     }
     return title;
