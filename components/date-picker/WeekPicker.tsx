49ecf9b738a1562e114439f369ab0c1882a66211
afc163 <afc163@gmail.com>
2019-09-27 14:41:55 +0800

    :white_check_mark: add more test cases for DatePicker

--- components/date-picker/WeekPicker.tsx
+++ components/date-picker/WeekPicker.tsx
@@ -47,7 +47,7 @@ class WeekPicker extends React.Component<any, WeekPickerState> {
     const value = props.value || props.defaultValue;
     if (value && !interopDefault(moment).isMoment(value)) {
       throw new Error(
-        'The value/defaultValue of DatePicker or MonthPicker must be ' +
+        'The value/defaultValue of WeekPicker must be ' +
           'a moment object after `antd@2.0`, see: https://u.ant.design/date-picker-value',
       );
     }
