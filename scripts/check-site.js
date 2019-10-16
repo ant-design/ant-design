dba1cba1834e15964df6c711a167c56415311727
ycjcl868 <45808948@qq.com>
2019-10-16 23:23:11 +0800

    feat: check site files

--- scripts/check-site.js
+++ scripts/check-site.js
@@ -1,34 +1,63 @@
 const path = require('path');
 const glob = require('glob');
 const chalk = require('chalk');
+const uniq = require('lodash/uniq');
+const difference = require('lodash/difference');
+const fetch = require('node-fetch');
 
-function checkKeyFilesExist() {
-  const keyfiles = [
-    '/index.html',
-    '/index-cn.html',
-    '/404.html',
-    '/CNAME',
-    '/color.less',
-    '/config.yml',
-    '/changelog.html',
-    '/changelog-cn.html',
-  ];
-  const sitePath = path.join(process.cwd(), '_site');
+const sitePath = path.join(process.cwd(), '_site');
 
+const throwError = keyfile => {
+  console.log(chalk.red(`❌ Check site file ${keyfile} failed`));
+  process.exit(1);
+};
+
+const removeNameHash = filename => {
+  return (
+    filename
+      .replace(/(?<=\.).*?(?:\.)/g, '')
+      // eslint-disable-next-line no-useless-escape
+      .replace(/(?<=\-).*?(?:\-)/g, '')
+      // eslint-disable-next-line no-useless-escape
+      .replace(/(?<=\-).*?(?:\.)/g, '')
+  );
+};
+
+const getPrevfiles = async () => {
+  const { tree } = await fetch(
+    'https://api.github.com/repos/ant-design/ant-design/git/trees/gh-pages?recursive=1',
+  )
+    .then(res => res.json())
+    .catch(e => {
+      console.log(chalk.red('❌ fetch api github failed', e));
+    });
+  const files = uniq(
+    tree
+      .filter(item => item && Number(item.mode) === 100644)
+      .map(item => removeNameHash(item.path)),
+  );
+  return files;
+};
+
+const startCheck = async () => {
+  const globOpts = {
+    nodir: true,
+  };
   console.log(chalk.cyan('Checking site file...'));
-  const nextSiteFiles = glob
-    .sync(path.join(sitePath, '**'))
-    .map(filepath => filepath.replace(sitePath, ''));
+  const prevFiles = await getPrevfiles();
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const keyfile of keyfiles) {
-    if (!nextSiteFiles.includes(keyfile)) {
-      console.log(chalk.red(`❌ Check site file ${keyfile} failed...`));
-      throw new Error(`keyfile ${keyfile} not existed`);
-    }
+  const nextFiles = uniq(
+    glob
+      .sync(path.join(process.cwd(), '_site', '**'), globOpts)
+      .map(file => removeNameHash(file.replace(`${sitePath}/`, ''))),
+  );
+
+  const diff = difference(prevFiles, nextFiles);
+  // create files
+  if (nextFiles.length >= prevFiles.length && diff.length > 0) {
+    throwError(JSON.stringify(diff));
   }
+  console.log(chalk.green('✅ Check site files success'));
+};
 
-  console.log(chalk.green('✅ Check site files success...'));
-}
-
-checkKeyFilesExist();
+startCheck();
