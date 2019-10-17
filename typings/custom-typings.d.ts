f2e541ade5ad62e74842202c3473165363a24e6e
骗你是小猫咪 <darryshaw@gmail.com>
2019-10-17 10:24:34 +0800

    refactor: improve typescript module declare (#19254)
    * refactor: improve typescript declare
    
    * remove rc-progress  declare
    
    * remove rc-drawer  declare
    
    * improve site antd module resolve path

--- typings/custom-typings.d.ts
+++ typings/custom-typings.d.ts
@@ -20,15 +20,9 @@ declare module 'rc-cascader';
 
 declare module 'rc-checkbox';
 
-declare module 'rc-radio';
-
 declare module 'rc-dropdown';
 
 declare module 'rc-editor-mention';
-
-declare module 'rc-progress';
-
-declare module 'rc-drawer';
 
 declare module 'rc-tabs*';
 
@@ -80,7 +74,5 @@ declare module '*.json' {
   export const version: string;
   export default value;
 }
-
-declare module 'react-lifecycles-compat';
 
 declare module 'react-copy-to-clipboard';
