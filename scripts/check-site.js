d3f19dda48f38c7f746a93206734df9996ec63da
ycjcl868 <45808948@qq.com>
2019-10-17 15:54:09 +0800

    fix: test

--- scripts/check-site.js
+++ scripts/check-site.js
@@ -36,7 +36,7 @@ describe('site test', () => {
 
   it('Home Page', async () => {
     const { status, $ } = await render('/');
-    expect($('title').text()).toEqual(`Ant Design - ${enUS.messages['app.home.sloga1n']}`);
+    expect($('title').text()).toEqual(`Ant Design - ${enUS.messages['app.home.slogan']}`);
     expect(status).toBe(200);
   });
 });
