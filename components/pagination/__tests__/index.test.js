360937cad6990f2fef2ce693de9d997b9161bb00
Saeed Rahimi <saeedr.com@gmail.com>
2019-10-30 14:04:45 +0330

    add pagination rtl test

--- components/pagination/__tests__/index.test.js
+++ components/pagination/__tests__/index.test.js
@@ -1,6 +1,18 @@
+import React from 'react';
 import Pagination from '..';
+import { render, mount } from 'enzyme';
+import ConfigProvider from '../../config-provider';
 import mountTest from '../../../tests/shared/mountTest';
 
 describe('Pagination', () => {
   mountTest(Pagination);
+
+  it('should be rendered correctly in RTL', () => {
+    const wrapper = mount(
+      <ConfigProvider direction="rtl">
+        <Pagination defaultCurrent={1} total={50} />
+      </ConfigProvider>,
+    );
+    expect(render(wrapper)).toMatchSnapshot();
+  });
 });
