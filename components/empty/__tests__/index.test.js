019425afc28657e9ede61543fd05294f418580fc
Saeed Rahimi <saeedr.com@gmail.com>
2019-11-10 11:55:28 +0330

    RTL:  Empty component

--- components/empty/__tests__/index.test.js
+++ components/empty/__tests__/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, mount } from 'enzyme';
 import Empty from '..';
+import ConfigProvider from '../../config-provider';
 import mountTest from '../../../tests/shared/mountTest';
 
 describe('Empty', () => {
@@ -14,5 +15,14 @@ describe('Empty', () => {
   it('description can be false', () => {
     const wrapper = mount(<Empty description={false} />);
     expect(wrapper.find('.ant-empty-description').length).toBe(0);
+  });
+
+  it('should render in RTL direction', () => {
+    const wrapper = mount(
+      <ConfigProvider direction="rtl">
+        <Empty />
+      </ConfigProvider>,
+    );
+    expect(render(wrapper)).toMatchSnapshot();
   });
 });
