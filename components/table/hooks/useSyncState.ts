3e87c2fb0d78603cf3d2ab4705f147f0a5fee3e1
jeessy2 <454207171@qq.com>
2019-11-22 17:03:55 +0800

    new line

--- components/table/hooks/useSyncState.ts
+++ components/table/hooks/useSyncState.ts
@@ -5,5 +5,8 @@ type UseSyncStateProps<T> = [() => T, (newValue: T) => void];
 export default function useSyncState<T>(filteredKeys: T): UseSyncStateProps<T> {
   const filteredKeysRef = React.useRef<T>(filteredKeys);
 
-  return [() => filteredKeysRef.current, (newValue: T) => (filteredKeysRef.current = newValue)];
+  return [
+    () => filteredKeysRef.current,
+    (newValue: T) => (filteredKeysRef.current = newValue)
+  ];
 }
