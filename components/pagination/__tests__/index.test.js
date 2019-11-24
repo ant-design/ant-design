6bd48f29a4f968e34dcd562c3dbc3ef3a68b8918
Saeed Rahimi <saeedr.com@gmail.com>
2019-11-24 08:43:04 +0330

    test: add pagination component rtl tests

--- components/pagination/__tests__/index.test.js
+++ components/pagination/__tests__/index.test.js
@@ -3,9 +3,11 @@ import { render, mount } from 'enzyme';
 import Pagination from '..';
 import ConfigProvider from '../../config-provider';
 import mountTest from '../../../tests/shared/mountTest';
+import rtlTest from '../../../tests/shared/rtlTest';
 
 describe('Pagination', () => {
   mountTest(Pagination);
+  rtlTest(Pagination);
 
   it('should be rendered correctly in RTL', () => {
     const wrapper = mount(
