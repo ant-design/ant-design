bca51115bd820eaa2391e0f63bcb07b8317339a7
zombiej <smith3816@gmail.com>
2019-12-30 17:56:00 +0800

    auto width

--- site/theme/template/Layout/Header.jsx
+++ site/theme/template/Layout/Header.jsx
@@ -158,7 +158,6 @@ class Header extends React.Component {
         key="version"
         className="version"
         size="small"
-        style={{ width: 110 }}
         dropdownMatchSelectWidth={false}
         defaultValue={antdVersion}
         onChange={this.handleVersionChange}
