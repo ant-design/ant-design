fb48ea497a6959f283aa5350c8aa427003d3cfc0
zombiej <smith3816@gmail.com>
2019-12-30 11:55:58 +0800

    chore: Add warning when use remove function

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -2,6 +2,7 @@ import InternalForm, { useForm, FormInstance } from './Form';
 import Item from './FormItem';
 import List from './FormList';
 import { FormProvider } from './context';
+import warning from '../_util/warning';
 
 type InternalForm = typeof InternalForm;
 interface Form extends InternalForm {
@@ -9,6 +10,9 @@ interface Form extends InternalForm {
   Item: typeof Item;
   List: typeof List;
   Provider: typeof FormProvider;
+
+  /** @deprecated Only for warning usage. Do not use. */
+  create: () => void;
 }
 
 const Form: Form = InternalForm as Form;
@@ -17,6 +21,13 @@ Form.Item = Item;
 Form.List = List;
 Form.useForm = useForm;
 Form.Provider = FormProvider;
+Form.create = () => {
+  warning(
+    false,
+    'Form',
+    'antd v4 removed `Form.create`. Please remove or use `@ant-design/compatible` instead.',
+  );
+};
 
 export { FormInstance };
 
