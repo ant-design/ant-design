3294a2e8f106845ddc613e1301beedd6cd07c1a9
zombiej <smith3816@gmail.com>
2020-01-14 19:33:04 +0800

    chore: Add DatePicker related interface ts export

--- components/date-picker/index.tsx
+++ components/date-picker/index.tsx
@@ -1,6 +1,15 @@
 import { Moment } from 'moment';
 import momentGenerateConfig from 'rc-picker/lib/generate/moment';
-import generatePicker from './generatePicker';
+import generatePicker, {
+  PickerProps,
+  PickerDateProps,
+  RangePickerProps as BaseRangePickerProps,
+} from './generatePicker';
+
+export type DatePickerProps = PickerProps<Moment>;
+export type MonthPickerProps = Omit<PickerDateProps<Moment>, 'picker'>;
+export type WeekPickerProps = Omit<PickerDateProps<Moment>, 'picker'>;
+export type RangePickerProps = BaseRangePickerProps<Moment>;
 
 const DatePicker = generatePicker<Moment>(momentGenerateConfig);
 
