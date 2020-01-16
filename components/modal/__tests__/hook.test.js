0c711c4fd32992f8265cdef8833964277f106865
zombiej <smith3816@gmail.com>
2020-01-16 21:08:17 +0800

    update test case

--- components/modal/__tests__/hook.test.js
+++ components/modal/__tests__/hook.test.js
@@ -7,7 +7,9 @@ jest.mock('rc-util/lib/Portal');
 
 describe('Modal.hook', () => {
   it('hooks support context', () => {
+    jest.useFakeTimers();
     const Context = React.createContext('light');
+    let instance;
 
     const Demo = () => {
       const [modal, contextHolder] = Modal.useModal();
@@ -15,7 +17,7 @@ describe('Modal.hook', () => {
         <Context.Provider value="bamboo">
           <Button
             onClick={() => {
-              modal.confirm({
+              instance = modal.confirm({
                 content: (
                   <Context.Consumer>
                     {name => <div className="test-hook">{name}</div>}
@@ -33,5 +35,20 @@ describe('Modal.hook', () => {
     wrapper.find('button').simulate('click');
 
     expect(wrapper.find('.test-hook').text()).toEqual('bamboo');
+
+    // Update instance
+    instance.update({
+      content: <div className="updated-content" />,
+    });
+    wrapper.update();
+    expect(wrapper.find('.updated-content')).toHaveLength(1);
+
+    // Destroy
+    instance.destroy();
+    jest.runAllTimers();
+    wrapper.update();
+    expect(wrapper.find('Modal')).toHaveLength(0);
+
+    jest.useRealTimers();
   });
 });
