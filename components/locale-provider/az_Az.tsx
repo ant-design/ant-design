bf18ab25053ce6a3902ef8f0d6af377e63059e14
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-02-11 17:35:16 +0400

    add Azerbaijani translation

---
+++ components/locale-provider/az_Az.tsx
@@ -0,0 +1,117 @@
+export default {
+  locale: 'az',
+  Pagination: {
+    // Options.jsx
+    items_per_page: '/ səhifə',
+    jump_to: 'Get',
+    jump_to_confirm: 'təsdiqlə',
+    page: '',
+
+    // Pagination.jsx
+    prev_page: 'Əvvəlki Səhifə',
+    next_page: 'Növbəti Səhifə',
+    prev_5: 'Əvvəlki 5 Səhifə',
+    next_5: 'Növbəti 5 Səhifə',
+    prev_3: 'Əvvəlki 3 Səhifə',
+    next_3: 'Növbəti 3 Səhifə'
+  },
+  DatePicker: {
+    lang: {
+      placeholder: 'Tarix seçin',
+      rangePlaceholder: ['Başlama tarixi', 'Bitmə tarixi'],
+      today: 'Bugün',
+      now: 'İndi',
+      backToToday: 'Bugünə qayıt',
+      ok: 'Təsdiq',
+      clear: 'Təmizlə',
+      month: 'Ay',
+      year: 'İl',
+      timeSelect: 'vaxtı seç',
+      dateSelect: 'tarixi seç',
+      weekSelect: 'Həftə seç',
+      monthSelect: 'Ay seç',
+      yearSelect: 'il seç',
+      decadeSelect: 'Onillik seçin',
+      yearFormat: 'YYYY',
+      dateFormat: 'D.M.YYYY',
+      dayFormat: 'D',
+      dateTimeFormat: 'D.M.YYYY HH:mm:ss',
+      monthBeforeYear: true,
+      previousMonth: 'Əvvəlki ay (PageUp)',
+      nextMonth: 'Növbəti ay (PageDown)',
+      previousYear: 'Sonuncu il (Control + left)',
+      nextYear: 'Növbəti il (Control + right)',
+      previousDecade: 'Sonuncu onillik',
+      nextDecade: 'Növbəti onillik',
+      previousCentury: 'Sonuncu əsr',
+      nextCentury: 'Növbəti əsr'
+    },
+    timePickerLocale: {
+      placeholder: 'Vaxtı seç'
+    }
+  },
+  TimePicker: {
+    placeholder: 'Vaxtı seç'
+  },
+  Calendar: {
+    today: 'Bugün',
+    now: 'İndi',
+    backToToday: 'Bugünə qayıt',
+    ok: 'Bəli',
+    clear: 'Təmizlə',
+    month: 'Ay',
+    year: 'İl',
+    timeSelect: 'vaxtı seç',
+    dateSelect: 'tarixi seç',
+    weekSelect: 'Həftə seç',
+    monthSelect: 'Ay seç',
+    yearSelect: 'il seç',
+    decadeSelect: 'Onillik seçin',
+    yearFormat: 'İİİİ',
+    dateFormat: 'gg.aa.iiii',
+    dayFormat: 'g',
+    dateTimeFormat: 'gg.aa.iiii ss:dd:ss',
+    monthBeforeYear: true,
+    previousMonth: 'Əvvəlki ay (PageUp)',
+    nextMonth: 'Növbəti ay (PageDown)',
+    previousYear: 'Sonuncu il (Control + left)',
+    nextYear: 'Növbəti il (Control + right)',
+    previousDecade: 'Sonuncu onillik',
+    nextDecade: 'Növbəti onillik',
+    previousCentury: 'Sonuncu əsr',
+    nextCentury: 'Növbəti əsr'
+  },
+  Table: {
+    filterTitle: 'Filter menyu',
+    filterConfirm: 'Axtar',
+    filterReset: 'Sıfırla',
+    emptyText: 'Məlumat yoxdur',
+    selectAll: 'Cari səhifəni seç',
+    selectInvert: 'Invert current page'
+  },
+  Modal: {
+    okText: 'Bəli',
+    cancelText: 'Ləğv et',
+    justOkText: 'Bəli'
+  },
+  Popconfirm: {
+    okText: 'Bəli',
+    cancelText: 'Ləğv et'
+  },
+  Transfer: {
+    titles: ['', ''],
+    notFoundContent: 'Tapılmadı',
+    searchPlaceholder: 'Burda axtar',
+    itemUnit: 'item',
+    itemsUnit: 'items'
+  },
+  Select: {
+    notFoundContent: 'Tapılmadı'
+  },
+  Upload: {
+    uploading: 'Yüklənir...',
+    removeFile: 'Faylı sil',
+    uploadError: 'Yükləmə xətası',
+    previewFile: 'Fayla önbaxış'
+  }
+};
