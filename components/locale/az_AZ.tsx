e48fc712b6d1d783c5936be3183a17e2f02388be
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-02-12 08:53:54 +0400

    fix: wrong import from tr_TR locales into az_AZ

--- components/locale/az_AZ.tsx
+++ components/locale/az_AZ.tsx
@@ -1,7 +1,7 @@
-import Pagination from 'rc-pagination/lib/locale/tr_TR';
-import DatePicker from '../date-picker/locale/tr_TR';
-import TimePicker from '../time-picker/locale/tr_TR';
-import Calendar from '../calendar/locale/tr_TR';
+import Pagination from 'rc-pagination/lib/locale/az_AZ';
+import DatePicker from '../date-picker/locale/az_AZ';
+import TimePicker from '../time-picker/locale/az_AZ';
+import Calendar from '../calendar/locale/az_AZ';
 
 export default {
   locale: 'az',
