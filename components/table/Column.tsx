d56f3bd7165f7bf2e64c08cb39cfbf61deea603a
jeessy2 <454207171@qq.com>
2020-02-17 18:14:43 +0800

    fix: Table ColumnProps ts define

--- components/table/Column.tsx
+++ components/table/Column.tsx
@@ -1,7 +1,7 @@
 import { ColumnType } from './interface';
 
 export interface ColumnProps<RecordType> extends ColumnType<RecordType> {
-  children?: ColumnTitle<RecordType>;
+  children?: ColumnTitle<RecordType>[];
 }
 
 /* istanbul ignore next */
