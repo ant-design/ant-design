bb335bcc78c0e48d547a4eda3f8dfa91afa3e893
Amumu <yoyo837@hotmail.com>
2020-02-22 14:16:44 +0800

    uniq (#21522)

--- site/theme/template/Color/ColorPatterns.jsx
+++ site/theme/template/Color/ColorPatterns.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import uniq from 'lodash/uniq';
 import { generate } from '@ant-design/colors';
 import ColorBlock from './ColorBlock';
 
 export default function ColorPatterns({ color }) {
-  return generate(color).map((colorString, i) => (
+  return uniq(generate(color)).map((colorString, i) => (
     <ColorBlock color={colorString} index={i + 1} key={colorString} />
   ));
 }
