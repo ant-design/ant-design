85806cf9d7893ef43c75e77572f01c18b3cdf70b
nossbigg <chenggibson@gmail.com>
2020-03-11 22:38:58 +0800

    feat: Define PickerLocale typedef

--- components/date-picker/locale/pt_BR.tsx
+++ components/date-picker/locale/pt_BR.tsx
@@ -1,8 +1,9 @@
 import CalendarLocale from 'rc-picker/lib/locale/pt_BR';
 import TimePickerLocale from '../../time-picker/locale/pt_BR';
+import { PickerLocale } from '../generatePicker';
 
 // Merge into a locale object
-const locale = {
+const locale: PickerLocale = {
   lang: {
     placeholder: 'Selecionar data',
     rangePlaceholder: ['Data inicial', 'Data final'],
