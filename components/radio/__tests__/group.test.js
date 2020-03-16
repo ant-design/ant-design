c86c3cf5a1f8673ab68700efa93522b7c8cc741c
二货机器人 <smith3816@gmail.com>
2020-03-16 12:47:24 +0800

    refactor: Adjust Radio.Group logic that value is undefined should be uncontrolled mode. (#22245)
    * test driven
    
    * adjust raido group logic

--- components/radio/__tests__/group.test.js
+++ components/radio/__tests__/group.test.js
@@ -170,10 +170,35 @@ describe('Radio Group', () => {
   });
 
   it('passes prefixCls down to radio', () => {
-    const options = [{ label: 'Apple', value: 'Apple' }, { label: 'Orange', value: 'Orange', style: { fontSize: 12 } }];
+    const options = [
+      { label: 'Apple', value: 'Apple' },
+      { label: 'Orange', value: 'Orange', style: { fontSize: 12 } },
+    ];
 
     const wrapper = render(<RadioGroup prefixCls="my-radio" options={options} />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('value is null or undefined', () => {
+    it('use `defaultValue` when `value` is undefined', () => {
+      const options = [{ label: 'Bamboo', value: 'bamboo' }];
+      const wrapper = mount(
+        <RadioGroup defaultValue="bamboo" value={undefined} options={options} />,
+      );
+
+      expect(wrapper.state().value).toEqual('bamboo');
+    });
+
+    [undefined, null].forEach(newValue => {
+      it(`should set value back when value change back to ${newValue}`, () => {
+        const options = [{ label: 'Bamboo', value: 'bamboo' }];
+        const wrapper = mount(<RadioGroup value="bamboo" options={options} />);
+        expect(wrapper.state().value).toEqual('bamboo');
+
+        wrapper.setProps({ value: newValue });
+        expect(wrapper.state().value).toEqual(newValue);
+      });
+    });
   });
 });
