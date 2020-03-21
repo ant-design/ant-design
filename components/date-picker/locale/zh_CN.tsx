1e95c6cef03d656b0b72de358c68aa816b7d8cc0
二货机器人 <smith3816@gmail.com>
2020-03-21 22:43:04 +0800

    feat: Quarter picker (#22468)
    * support quarter picker
    
    * update snapshot
    
    * fix lint

--- components/date-picker/locale/zh_CN.tsx
+++ components/date-picker/locale/zh_CN.tsx
@@ -7,6 +7,7 @@ const locale: PickerLocale = {
   lang: {
     placeholder: '请选择日期',
     yearPlaceholder: '请选择年份',
+    quarterPlaceholder: '请选择季度',
     monthPlaceholder: '请选择月份',
     weekPlaceholder: '请选择周',
     rangePlaceholder: ['开始日期', '结束日期'],
