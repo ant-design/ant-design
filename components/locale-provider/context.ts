3c8977b50e8aa96af907d3fb926feff4c8f7fbb6
二货机器人 <smith3816@gmail.com>
2020-03-30 14:23:03 +0000

    fix lint

--- components/locale-provider/context.ts
+++ components/locale-provider/context.ts
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { Locale } from './';
+import { Locale } from '.';
 
 const LocaleContext = createContext<(Partial<Locale> & { exist?: boolean }) | undefined>(undefined);
 
