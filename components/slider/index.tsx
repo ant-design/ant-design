ant-design/ant-design

commit a6c207d9dd9ea14c251d57b186b710b3066a7a64

fix: fix vertical handle position (#22135)
* fix: fix vertical handle position

* :lipstick: make tooltip right for vertical slider

* fix: fix style and bump rc-slider

components/slider/index.tsx
@@ -91,7 +91,13 @@ export default class Slider extends React.Component<SliderProps, SliderState> {
     prefixCls,
     info: { value, dragging, index, ...restProps },
   }) => {
-    const { tipFormatter, tooltipVisible, tooltipPlacement, getTooltipPopupContainer } = this.props;
+    const {
+      tipFormatter,
+      tooltipVisible,
+      tooltipPlacement,
+      getTooltipPopupContainer,
+      vertical,
+    } = this.props;
     const { visibles } = this.state;
     const isTipFormatter = tipFormatter ? visibles[index] || dragging : false;
     const visible = tooltipVisible || (tooltipVisible === undefined && isTipFormatter);
@@ -100,7 +106,7 @@ export default class Slider extends React.Component<SliderProps, SliderState> {
         prefixCls={tooltipPrefixCls}
         title={tipFormatter ? tipFormatter(value) : ''}
         visible={visible}
-        placement={tooltipPlacement || 'top'}
+        placement={tooltipPlacement || vertical ? 'right' : 'top'}
         transitionName="zoom-down"
         key={index}
         overlayClassName={`${prefixCls}-tooltip`}

components/slider/style/index.less
@@ -189,7 +189,7 @@
     }
 
     .@{slider-prefix-cls}-handle {
-      margin-bottom: -7px;
+      margin-top: -6px; // we chould consider border width as well: (10 + 2 ) / 2
       margin-left: -5px;
 
       .@{slider-prefix-cls}-rtl& {

commit a392930aa22377259304a859e70a8d71930f228f

Fix order of operations for tooltipPlacement
tooltipPlacement was broken by https://github.com/ant-design/ant-design/commit/a6c207d9dd9ea14c251d57b186b710b3066a7a64.
The grouping of this term was getting parsed as `(tooltipPlacement || vertical) ? 'right' : 'top'`, which was incorrect.

components/slider/index.tsx
@@ -106,7 +106,7 @@ export default class Slider extends React.Component<SliderProps, SliderState> {
         prefixCls={tooltipPrefixCls}
         title={tipFormatter ? tipFormatter(value) : ''}
         visible={visible}
-        placement={tooltipPlacement || vertical ? 'right' : 'top'}
+        placement={tooltipPlacement || (vertical ? 'right' : 'top')}
         transitionName="zoom-down"
         key={index}
         overlayClassName={`${prefixCls}-tooltip`}