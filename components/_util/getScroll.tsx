a80db7d20e14bb743832388f3e4faab57a9ebe23
afc163 <afc163@gmail.com>
2020-03-31 19:10:33 +0800

    fix ci

--- components/_util/getScroll.tsx
+++ components/_util/getScroll.tsx
@@ -1,18 +1,27 @@
-export default function getScroll(target: HTMLElement | Window | Document | null, top: boolean): number {
+function isWindow(obj: any) {
+  return obj !== null && obj !== undefined && obj === obj.window;
+}
+
+export default function getScroll(
+  target: HTMLElement | Window | Document | null,
+  top: boolean,
+): number {
   if (typeof window === 'undefined') {
     return 0;
   }
   const method = top ? 'scrollTop' : 'scrollLeft';
   let result = 0;
-  if (target instanceof Window) {
-    result = target[top ? 'pageYOffset' : 'pageXOffset'];
+  if (isWindow(target)) {
+    result = (target as Window)[top ? 'pageYOffset' : 'pageXOffset'];
   } else if (target instanceof Document) {
     result = target.documentElement[method];
   } else if (target) {
-    result = target[method];
+    result = (target as HTMLElement)[method];
   }
-  if (target && !(target instanceof Window) && typeof result !== 'number') {
-    result = (target.ownerDocument || (target as Document)).documentElement[method];
+  if (target && !isWindow(target) && typeof result !== 'number') {
+    result = ((target as HTMLElement).ownerDocument || (target as Document)).documentElement[
+      method
+    ];
   }
   return result;
 }
