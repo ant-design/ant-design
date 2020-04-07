1380850a34cb7822e1e92eda8da768f97afbb243
xrkffgg <xrkffgg@gmail.com>
2020-04-07 13:44:25 +0800

    docs: optimize icon doc in rtl (#22982)

--- site/theme/template/IconDisplay/index.tsx
+++ site/theme/template/IconDisplay/index.tsx
@@ -110,7 +110,7 @@ class IconDisplay extends React.Component<IconDisplayProps, IconDisplayState> {
           </Radio.Group>
           <Input.Search
             placeholder={messages['app.docs.components.icon.search.placeholder']}
-            style={{ marginLeft: 10, flex: 1 }}
+            style={{ margin: '0 10px', flex: 1 }}
             allowClear
             onChange={e => this.handleSearchIcon(e.currentTarget.value)}
             size="large"
