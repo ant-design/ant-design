b25bcc00f55c66436c1b9851f5cfaa7297f5f4df
hengkx <ycxzhkx@gmail.com>
2020-04-24 01:07:28 +0800

    fix lint

--- components/list/style/index.tsx
+++ components/list/style/index.tsx
@@ -2,7 +2,6 @@ import '../../style/index.less';
 import './index.less';
 
 // style dependencies
-import '../../empty/style';
 import '../../spin/style';
 import '../../pagination/style';
 import '../../grid/style';
