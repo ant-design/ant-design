d50cc55d8787efec1c8336223eda16ea0c53f3be
afc163 <afc163@gmail.com>
2020-04-24 18:48:05 +0800

    :book: fix site description and rtl className

--- scripts/check-site.js
+++ scripts/check-site.js
@@ -43,11 +43,7 @@ describe('site test', () => {
   const expectComponent = async component => {
     const { status, $ } = await render(`/${component}/`);
     expect(status).toBe(200);
-    expect(
-      $('.markdown > h1')
-        .text()
-        .toLowerCase(),
-    ).toMatch(handleComponentName(component));
+    expect($('.markdown > h1').text().toLowerCase()).toMatch(handleComponentName(component));
   };
 
   beforeAll(() => {
@@ -67,13 +63,15 @@ describe('site test', () => {
 
   it('Basic Pages en', async () => {
     const { status, $ } = await render('/');
-    expect($('title').text()).toEqual(`Ant Design - ${enUS.messages['app.home.slogan']}`);
+    expect($('title').text()).toEqual(
+      `Ant Design - The world's second most popular React UI framework`,
+    );
     expect(status).toBe(200);
   });
 
   it('Basic Pages zh', async () => {
     const { status, $ } = await render('/index-cn');
-    expect($('title').text()).toEqual(`Ant Design - ${zhCN.messages['app.home.slogan']}`);
+    expect($('title').text()).toEqual(`Ant Design - 一套企业级 UI 设计语言和 React 组件库`);
     expect(status).toBe(200);
   });
 
