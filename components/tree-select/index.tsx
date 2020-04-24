9fe93ea724dd89843f2874892a1ec57312cede80
hengkx <ycxzhkx@gmail.com>
2020-04-24 22:24:51 +0800

    refactor(tree-select): rewrite with hook

--- components/tree-select/index.tsx
+++ components/tree-select/index.tsx
@@ -8,7 +8,7 @@ import RcTreeSelect, {
 } from 'rc-tree-select';
 import classNames from 'classnames';
 import omit from 'omit.js';
-import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
+import { ConfigContext } from '../config-provider';
 import warning from '../_util/warning';
 import { AntTreeNodeProps } from '../tree';
 import getIcons from '../select/utils/iconUtil';
@@ -35,162 +35,137 @@ export interface TreeSelectProps<T>
   bordered?: boolean;
 }
 
-class TreeSelect<T> extends React.Component<TreeSelectProps<T>, {}> {
-  static TreeNode = TreeNode;
+function InternalTreeSelect<T>(
+  props: TreeSelectProps<T>,
+  ref: ((instance: T | null) => void) | React.MutableRefObject<T | null> | null,
+) {
+  warning(
+    props.multiple !== false || !props.treeCheckable,
+    'TreeSelect',
+    '`multiple` will alway be `true` when `treeCheckable` is true',
+  );
 
-  static SHOW_ALL: typeof SHOW_ALL = SHOW_ALL;
-
-  static SHOW_PARENT: typeof SHOW_PARENT = SHOW_PARENT;
-
-  static SHOW_CHILD: typeof SHOW_CHILD = SHOW_CHILD;
-
-  static defaultProps = {
-    transitionName: 'slide-up',
-    choiceTransitionName: 'zoom',
-    bordered: true,
-  };
-
-  selectRef = React.createRef<RcTreeSelect>();
-
-  constructor(props: TreeSelectProps<T>) {
-    super(props);
-
-    warning(
-      props.multiple !== false || !props.treeCheckable,
-      'TreeSelect',
-      '`multiple` will alway be `true` when `treeCheckable` is true',
-    );
-  }
-
-  focus() {
-    if (this.selectRef.current) {
-      this.selectRef.current.focus();
-    }
-  }
-
-  blur() {
-    if (this.selectRef.current) {
-      this.selectRef.current.blur();
-    }
-  }
-
-  renderTreeSelect = ({
+  const {
     getPopupContainer: getContextPopupContainer,
     getPrefixCls,
     renderEmpty,
     direction,
-  }: ConfigConsumerProps) => {
-    const {
-      prefixCls: customizePrefixCls,
-      size: customizeSize,
-      className,
-      treeCheckable,
-      multiple,
-      listHeight = 256,
-      listItemHeight = 26,
-      notFoundContent,
-      switcherIcon,
-      treeLine,
-      getPopupContainer,
-      dropdownClassName,
-      bordered,
-      treeIcon = false,
-    } = this.props;
+  } = React.useContext(ConfigContext);
+  const size = React.useContext(SizeContext);
 
-    const prefixCls = getPrefixCls('select', customizePrefixCls);
-    const treePrefixCls = getPrefixCls('select-tree', customizePrefixCls);
-    const treeSelectPrefixCls = getPrefixCls('tree-select', customizePrefixCls);
+  const {
+    prefixCls: customizePrefixCls,
+    size: customizeSize,
+    className,
+    treeCheckable,
+    multiple,
+    listHeight = 256,
+    listItemHeight = 26,
+    notFoundContent,
+    switcherIcon,
+    treeLine,
+    getPopupContainer,
+    dropdownClassName,
+    bordered,
+    treeIcon = false,
+  } = props;
 
-    const mergedDropdownClassName = classNames(
-      dropdownClassName,
-      `${treeSelectPrefixCls}-dropdown`,
-      {
-        [`${treeSelectPrefixCls}-dropdown-rtl`]: direction === 'rtl',
-      },
-    );
+  const prefixCls = getPrefixCls('select', customizePrefixCls);
+  const treePrefixCls = getPrefixCls('select-tree', customizePrefixCls);
+  const treeSelectPrefixCls = getPrefixCls('tree-select', customizePrefixCls);
 
-    const isMultiple = !!(treeCheckable || multiple);
+  const mergedDropdownClassName = classNames(dropdownClassName, `${treeSelectPrefixCls}-dropdown`, {
+    [`${treeSelectPrefixCls}-dropdown-rtl`]: direction === 'rtl',
+  });
 
-    // ===================== Icons =====================
-    const { suffixIcon, itemIcon, removeIcon, clearIcon } = getIcons({
-      ...this.props,
-      multiple: isMultiple,
-    });
+  const isMultiple = !!(treeCheckable || multiple);
 
-    // ===================== Empty =====================
-    let mergedNotFound: React.ReactNode;
-    if (notFoundContent !== undefined) {
-      mergedNotFound = notFoundContent;
-    } else {
-      mergedNotFound = renderEmpty('Select');
-    }
+  // ===================== Icons =====================
+  const { suffixIcon, itemIcon, removeIcon, clearIcon } = getIcons({
+    ...props,
+    multiple: isMultiple,
+  });
 
-    // ==================== Render =====================
-    const selectProps = omit(this.props, [
-      'prefixCls',
-      'suffixIcon',
-      'itemIcon',
-      'removeIcon',
-      'clearIcon',
-      'switcherIcon',
-      'size',
-      'bordered',
-    ]);
-
-    return (
-      <SizeContext.Consumer>
-        {size => {
-          const mergedSize = customizeSize || size;
-          const mergedClassName = classNames(
-            !customizePrefixCls && treeSelectPrefixCls,
-            {
-              [`${prefixCls}-lg`]: mergedSize === 'large',
-              [`${prefixCls}-sm`]: mergedSize === 'small',
-              [`${prefixCls}-rtl`]: direction === 'rtl',
-              [`${prefixCls}-borderless`]: !bordered,
-            },
-            className,
-          );
-
-          return (
-            <RcTreeSelect
-              {...selectProps}
-              ref={this.selectRef}
-              prefixCls={prefixCls}
-              className={mergedClassName}
-              listHeight={listHeight}
-              listItemHeight={listItemHeight}
-              treeCheckable={
-                treeCheckable ? (
-                  <span className={`${prefixCls}-tree-checkbox-inner`} />
-                ) : (
-                  treeCheckable
-                )
-              }
-              inputIcon={suffixIcon}
-              menuItemSelectedIcon={itemIcon}
-              removeIcon={removeIcon}
-              clearIcon={clearIcon}
-              switcherIcon={(nodeProps: AntTreeNodeProps) =>
-                renderSwitcherIcon(treePrefixCls, switcherIcon, treeLine, nodeProps)
-              }
-              showTreeIcon={treeIcon}
-              notFoundContent={mergedNotFound}
-              getPopupContainer={getPopupContainer || getContextPopupContainer}
-              treeMotion={null}
-              dropdownClassName={mergedDropdownClassName}
-            />
-          );
-        }}
-      </SizeContext.Consumer>
-    );
-  };
-
-  render() {
-    return <ConfigConsumer>{this.renderTreeSelect}</ConfigConsumer>;
+  // ===================== Empty =====================
+  let mergedNotFound: React.ReactNode;
+  if (notFoundContent !== undefined) {
+    mergedNotFound = notFoundContent;
+  } else {
+    mergedNotFound = renderEmpty('Select');
   }
+
+  // ==================== Render =====================
+  const selectProps = omit(props, [
+    'prefixCls',
+    'suffixIcon',
+    'itemIcon',
+    'removeIcon',
+    'clearIcon',
+    'switcherIcon',
+    'size',
+    'bordered',
+  ]);
+
+  const mergedSize = customizeSize || size;
+  const mergedClassName = classNames(
+    !customizePrefixCls && treeSelectPrefixCls,
+    {
+      [`${prefixCls}-lg`]: mergedSize === 'large',
+      [`${prefixCls}-sm`]: mergedSize === 'small',
+      [`${prefixCls}-rtl`]: direction === 'rtl',
+      [`${prefixCls}-borderless`]: !bordered,
+    },
+    className,
+  );
+
+  return (
+    <RcTreeSelect
+      {...selectProps}
+      ref={ref}
+      prefixCls={prefixCls}
+      className={mergedClassName}
+      listHeight={listHeight}
+      listItemHeight={listItemHeight}
+      treeCheckable={
+        treeCheckable ? <span className={`${prefixCls}-tree-checkbox-inner`} /> : treeCheckable
+      }
+      inputIcon={suffixIcon}
+      menuItemSelectedIcon={itemIcon}
+      removeIcon={removeIcon}
+      clearIcon={clearIcon}
+      switcherIcon={(nodeProps: AntTreeNodeProps) =>
+        renderSwitcherIcon(treePrefixCls, switcherIcon, treeLine, nodeProps)
+      }
+      showTreeIcon={treeIcon}
+      notFoundContent={mergedNotFound}
+      getPopupContainer={getPopupContainer || getContextPopupContainer}
+      treeMotion={null}
+      dropdownClassName={mergedDropdownClassName}
+    />
+  );
 }
 
 export { TreeNode };
+
+interface CompoundedComponent<T>
+  extends React.ForwardRefExoticComponent<TreeSelectProps<T> & React.RefAttributes<HTMLElement>> {
+  TreeNode: typeof TreeNode;
+  SHOW_ALL: typeof SHOW_ALL;
+  SHOW_PARENT: typeof SHOW_PARENT;
+  SHOW_CHILD: typeof SHOW_CHILD;
+}
+
+const TreeSelect = React.forwardRef(InternalTreeSelect) as CompoundedComponent<unknown>;
+
+TreeSelect.defaultProps = {
+  transitionName: 'slide-up',
+  choiceTransitionName: 'zoom',
+  bordered: true,
+};
+
+TreeSelect.TreeNode = TreeNode;
+TreeSelect.SHOW_ALL = SHOW_ALL;
+TreeSelect.SHOW_PARENT = SHOW_PARENT;
+TreeSelect.SHOW_CHILD = SHOW_CHILD;
 
 export default TreeSelect;
