e544d1337b750bec5d9caaa361ab98c4057d8260
afc163 <afc163@gmail.com>
2020-04-26 19:57:08 +0800

    fix Empty style dep

--- components/list/style/index.tsx
+++ components/list/style/index.tsx
@@ -2,6 +2,7 @@ import '../../style/index.less';
 import './index.less';
 
 // style dependencies
+import '../../empty/style';
 import '../../spin/style';
 import '../../pagination/style';
 import '../../grid/style';
