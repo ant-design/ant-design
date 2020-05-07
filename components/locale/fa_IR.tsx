9a99ebd48d52e23add0dea2a6dccb2538f3d5813
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-05-07 09:20:02 +0430

    Update fa_IR based on zh_CN

--- components/locale/fa_IR.tsx
+++ components/locale/fa_IR.tsx
@@ -61,6 +61,55 @@ const localeValues: Locale = {
   PageHeader: {
     back: 'برگشت',
   },
+  Form: {
+    defaultValidateMessages: {
+      default: 'خطا در ${label}',
+      required: 'فیلد ${label} اجباریست',
+      enum: '${label} باید یکی از [${enum}] باشد',
+      whitespace: '${label} نمیتواند خالی باشد',
+      date: {
+        format: 'ساختار تاریخ در ${label} نامعتبر است',
+        parse: '${label} قابل تبدیل به تاریخ نیست',
+        invalid: '${label} تاریخی نا معتبر است',
+      },
+      types: {
+        string: typeTemplate,
+        method: typeTemplate,
+        array: typeTemplate,
+        object: typeTemplate,
+        number: typeTemplate,
+        date: typeTemplate,
+        boolean: typeTemplate,
+        integer: typeTemplate,
+        float: typeTemplate,
+        regexp: typeTemplate,
+        email: typeTemplate,
+        url: typeTemplate,
+        hex: typeTemplate,
+      },
+      string: {
+        len: '${label} باید ${len} کاراکتر باشد',
+        min: '${label} باید حداقل ${min} کاراکتر باشد',
+        max: '${label} باید حداکثر ${max} کاراکتر باشد',
+        range: '${label} باید بین ${min}-${max} کاراکتر باشد',
+      },
+      number: {
+        len: '${label} باید برابر ${len}',
+        min: '${label} حداقل میتواند ${min} باشد',
+        max: '${label} حداکثر میتواند ${max} باشد',
+        range: '${label} باید بین ${min}-${max} باشد',
+      },
+      array: {
+        len: 'تعداد ${label} باید ${len} باشد.',
+        min: 'تعداد ${lable} حداقل باید ${min} باشد',
+        max: 'تعداد ${lable} حداکثر باید ${max} باشد',
+        range: 'مقدار ${label} باید بین ${min}-${max} باشد',
+      },
+      pattern: {
+        mismatch: 'الگوی ${label} با ${pattern} برابری نمی‌کند',
+      },
+    },
+  },
 };
 
 export default localeValues;
