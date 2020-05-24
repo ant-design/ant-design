e8dfdba82a424091b548f73634cd6e0e4e6884c8
afc163 <afc163@gmail.com>
2020-05-24 21:35:03 +0800

    test: increase test case of Grid and Tree

--- components/cascader/index.tsx
+++ components/cascader/index.tsx
@@ -3,7 +3,6 @@ import RcCascader from 'rc-cascader';
 import arrayTreeFilter from 'array-tree-filter';
 import classNames from 'classnames';
 import omit from 'omit.js';
-import isEqual from 'lodash/isEqual';
 import KeyCode from 'rc-util/lib/KeyCode';
 import CloseCircleFilled from '@ant-design/icons/CloseCircleFilled';
 import DownOutlined from '@ant-design/icons/DownOutlined';
