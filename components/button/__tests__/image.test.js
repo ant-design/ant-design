ebd24928f89204a94aa8859b33f2cd5762db5c31
shaodahong <darryshaw@gmail.com>
2020-05-24 10:53:01 +0800

    test faild condition

--- components/button/__tests__/image.test.js
+++ components/button/__tests__/image.test.js
@@ -5,7 +5,7 @@ import imageTest from '../../../tests/shared/imageTest';
 describe('Button image', () => {
   imageTest(
     <>
-      <Button type="primary">Primary</Button>
+      <Button type="primary">Primary Test</Button>
       <Button>Default</Button>
       <Button type="dashed">Dashed</Button>
       <Button type="link">Link</Button>
