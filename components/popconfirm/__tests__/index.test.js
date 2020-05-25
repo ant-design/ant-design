c93400d375cb3a3ac188093d4d92e75d71b2800c
偏右 <afc163@gmail.com>
2020-05-25 17:43:59 +0800

    feat: ⌨️ Popconfirm can be closed by pressing ESC (#24420)
    * feat: support ESC to close Popconfirm
    
    close #15203
    
    * add test case
    
    * fix focus in other element inside inner-content
    
    * no fit
    
    * fix snapshot
    
    * refactor it
    
    * use util cloneElement
    
    * fix test case

--- components/popconfirm/__tests__/index.test.js
+++ components/popconfirm/__tests__/index.test.js
@@ -176,4 +176,23 @@ describe('Popconfirm', () => {
     triggerNode.simulate('click');
     expect(ref.current.getPopupDomNode()).toBeFalsy();
   });
+
+  it('should be closed by pressing ESC', () => {
+    const onVisibleChange = jest.fn();
+    const wrapper = mount(
+      <Popconfirm
+        title="title"
+        mouseEnterDelay={0}
+        mouseLeaveDelay={0}
+        onVisibleChange={onVisibleChange}
+      >
+        <span>Delete</span>
+      </Popconfirm>,
+    );
+    const triggerNode = wrapper.find('span').at(0);
+    triggerNode.simulate('click');
+    expect(onVisibleChange).toHaveBeenLastCalledWith(true, undefined);
+    triggerNode.simulate('keydown', { key: 'Escape', keyCode: 27 });
+    expect(onVisibleChange).toHaveBeenLastCalledWith(false, eventObject);
+  });
 });
