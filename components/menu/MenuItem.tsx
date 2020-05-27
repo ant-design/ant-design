820ae110396617599e459d574315c59a43966b88
xrkffgg <xrkffgg@vip.qq.com>
2020-05-27 18:02:27 +0800

    fix snap

--- components/menu/MenuItem.tsx
+++ components/menu/MenuItem.tsx
@@ -43,12 +43,11 @@ export default class MenuItem extends React.Component<MenuItemProps> {
     const { icon, children, level } = this.props;
     // inline-collapsed.md demo 依赖 span 来隐藏文字,有 icon 属性，则内部包裹一个 span
     // ref: https://github.com/ant-design/ant-design/pull/23456
-    if ((!icon && children) || (isValidElement(children) && children.type === 'span')) {
-      return inlineCollapsed && level === 1 && typeof children === 'string' ? (
-        <p>{children.charAt(0)}</p>
-      ) : (
-        children
-      );
+    if (!icon || (isValidElement(children) && children.type === 'span')) {
+      if (children && inlineCollapsed && level === 1 && typeof children === 'string') {
+        return <p>{children.charAt(0)}</p>;
+      }
+      return children;
     }
     return <span>{children}</span>;
   }
