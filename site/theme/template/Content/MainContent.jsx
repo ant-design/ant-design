896d65900a685feef4f8bbe341bb44c7ee4c5846
afc163 <afc163@gmail.com>
2020-05-27 20:29:28 +0800

    fix lint

--- site/theme/template/Content/MainContent.jsx
+++ site/theme/template/Content/MainContent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'bisheng/router';
 import { Row, Col, Menu, Affix, Tooltip, Avatar, Dropdown } from 'antd';
+import { injectIntl } from 'react-intl';
 import { LeftOutlined, RightOutlined, ExportOutlined } from '@ant-design/icons';
 import ContributorsList from '@qixian.cs/github-contributors-list';
 import classNames from 'classnames';
