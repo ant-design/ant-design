813772b564361a24b690dfcb8016cc9be2b60d81
zombiej <smith3816@gmail.com>
2020-05-29 00:21:07 +0800

    editable

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import RcTabs, { TabPane, TabsProps as RcTabsProps } from 'rc-tabs';
+import { EditableConfig } from 'rc-tabs/lib/interface';
 import classNames from 'classnames';
 import omit from 'omit.js';
 import EllipsisOutlined from '@ant-design/icons/EllipsisOutlined';
 import PlusOutlined from '@ant-design/icons/PlusOutlined';
+import CloseOutlined from '@ant-design/icons/CloseOutlined';
 
 import { ConfigConsumer, ConfigConsumerProps, ConfigContext } from '../config-provider';
 import { cloneElement, isValidElement } from '../_util/reactNode';
@@ -12,21 +14,39 @@ import { SizeType } from '../config-provider/SizeContext';
 export type TabsType = 'line' | 'card' | 'editable-card';
 export type TabsPosition = 'top' | 'right' | 'bottom' | 'left';
 
-export interface TabsProps extends RcTabsProps {
+export interface TabsProps extends Omit<RcTabsProps, 'editable'> {
   type?: TabsType;
   size?: SizeType;
+  hideAdd?: boolean;
+  onEdit?: (e: React.MouseEvent | React.KeyboardEvent | string, action: 'add' | 'remove') => void;
 }
 
-function Tabs({ type, className, size, ...props }: TabsProps) {
+function Tabs({ type, className, size, onEdit, hideAdd, ...props }: TabsProps) {
   const { prefixCls: customizePrefixCls } = props;
   const { getPrefixCls } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('tabs', customizePrefixCls);
+
+  let editable: EditableConfig | undefined;
+  if (type === 'editable-card') {
+    editable = {
+      onEdit: (editType, { key, event }) => {
+        onEdit?.(editType === 'add' ? event : key!, editType);
+      },
+      removeIcon: <CloseOutlined />,
+      addIcon: <PlusOutlined />,
+      showAdd: hideAdd !== false,
+    };
+  }
+
   return (
     <RcTabs
       {...props}
       className={classNames(className, {
         [`${prefixCls}-${size}`]: size,
+        [`${prefixCls}-card`]: ['card', 'editable-card'].includes(type as string),
+        [`${prefixCls}-editable-card`]: type === 'editable-card',
       })}
+      editable={editable}
       moreIcon={<EllipsisOutlined />}
       prefixCls={prefixCls}
     />
