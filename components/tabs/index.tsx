70ffb1da9b35d88e75389bf785fa952426f7b8af
zombiej <smith3816@gmail.com>
2020-05-28 23:18:01 +0800

    support size

--- components/tabs/index.tsx
+++ components/tabs/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import RcTabs, { TabPane, TabsProps as RcTabsProps } from 'rc-tabs';
 import classNames from 'classnames';
 import omit from 'omit.js';
@@ -8,19 +7,30 @@ import PlusOutlined from '@ant-design/icons/PlusOutlined';
 
 import { ConfigConsumer, ConfigConsumerProps, ConfigContext } from '../config-provider';
 import { cloneElement, isValidElement } from '../_util/reactNode';
+import { SizeType } from '../config-provider/SizeContext';
 
 export type TabsType = 'line' | 'card' | 'editable-card';
 export type TabsPosition = 'top' | 'right' | 'bottom' | 'left';
 
 export interface TabsProps extends RcTabsProps {
   type?: TabsType;
+  size?: SizeType;
 }
 
-function Tabs({ type, ...props }: TabsProps) {
+function Tabs({ type, className, size, ...props }: TabsProps) {
   const { prefixCls: customizePrefixCls } = props;
   const { getPrefixCls } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('tabs', customizePrefixCls);
-  return <RcTabs {...props} moreIcon={<EllipsisOutlined />} prefixCls={prefixCls} />;
+  return (
+    <RcTabs
+      {...props}
+      className={classNames(className, {
+        [`${prefixCls}-${size}`]: size,
+      })}
+      moreIcon={<EllipsisOutlined />}
+      prefixCls={prefixCls}
+    />
+  );
 }
 
 Tabs.TabPane = TabPane;
