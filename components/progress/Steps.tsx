8e3bae4d651ca094151f2424ac55ed6641ab0139
afc163 <afc163@gmail.com>
2020-05-29 12:02:48 +0800

    add default color for Progress steps

--- components/progress/Steps.tsx
+++ components/progress/Steps.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import { ProgressProps, ProgressSize } from './progress';
 
 interface StepsProps extends ProgressProps {
@@ -9,23 +10,27 @@ interface StepsProps extends ProgressProps {
 
 const Steps: React.FC<StepsProps> = props => {
   const { size, steps, percent = 0, strokeWidth = 8, strokeColor, prefixCls, children } = props;
-  const getStyledSteps = () => {
-    const current = Math.floor(steps * (percent / 100));
-    const stepWidth = size === 'small' ? 2 : 14;
-    const styleSteps = [];
-    for (let i = 0; i < steps; i++) {
-      const stepStyle = {
-        backgroundColor: i <= current - 1 ? strokeColor : undefined,
-        width: stepWidth,
-        height: strokeWidth,
-      };
-      styleSteps.push(<div key={i} className={`${prefixCls}-steps-item`} style={stepStyle} />);
-    }
-    return styleSteps;
-  };
+  const current = Math.floor(steps * (percent / 100));
+  const stepWidth = size === 'small' ? 2 : 14;
+  const styledSteps = [];
+  for (let i = 0; i < steps; i += 1) {
+    styledSteps.push(
+      <div
+        key={i}
+        className={classNames(`${prefixCls}-steps-item`, {
+          [`${prefixCls}-steps-item-active`]: i <= current - 1,
+        })}
+        style={{
+          backgroundColor: i <= current - 1 ? strokeColor : undefined,
+          width: stepWidth,
+          height: strokeWidth,
+        }}
+      />,
+    );
+  }
   return (
     <div className={`${prefixCls}-steps-outer`}>
-      {getStyledSteps()}
+      {styledSteps}
       {children}
     </div>
   );
