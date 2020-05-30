3e531741e7c5525ce7c0c0b8b814f18419faa967
afc163 <afc163@gmail.com>
2020-05-30 18:54:24 +0800

    fix url

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,14 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './_site',
-      url: [
-        'http://localhost/',
-        'http://localhost/index-cn',
-        'http://localhost/docs/react/intruduce',
-        'http://localhost/docs/react/intruduce-cn',
-        'http://localhost/components/button/',
-        'http://localhost/components/button-cn/',
-      ],
+      url: ['http://localhost/', 'http://localhost/components/button/'],
     },
     upload: {
       target: 'temporary-public-storage',
