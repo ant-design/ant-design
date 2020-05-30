faf18d47b27bd8650747866114f306b6c596e59c
afc163 <afc163@gmail.com>
2020-05-30 17:47:38 +0800

    Add url

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,9 +2,20 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './_site',
+      url: [
+        'http://localhost/',
+        'http://localhost/index-cn',
+        'http://localhost/docs/react/intruduce',
+        'http://localhost/docs/react/intruduce-cn',
+        'http://localhost/components/button/',
+        'http://localhost/components/button-cn/',
+      ],
     },
     upload: {
       target: 'temporary-public-storage',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
     },
   },
 };
