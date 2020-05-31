449e4b1632a82411af5a2a3e60ca64f86548ffee
hengkx <ycxzhkx@gmail.com>
2020-05-31 12:59:22 +0800

    refactor(LocaleReceiver): improve code

--- components/locale-provider/LocaleReceiver.tsx
+++ components/locale-provider/LocaleReceiver.tsx
@@ -15,37 +15,33 @@ interface LocaleInterface {
 export interface LocaleReceiverContext {
   antLocale?: LocaleInterface;
 }
-
-export default class LocaleReceiver extends React.Component<LocaleReceiverProps> {
-  static defaultProps = {
-    componentName: 'global',
-  };
-
-  static contextType = LocaleContext;
-
-  getLocale() {
-    const { componentName, defaultLocale } = this.props;
+const LocaleReceiver: React.FC<LocaleReceiverProps> = ({
+  componentName = 'global',
+  defaultLocale,
+  children,
+}) => {
+  const antLocale = React.useContext(LocaleContext);
+  const getLocale = () => {
     const locale: object | Function =
       defaultLocale || (defaultLocaleData as LocaleInterface)[componentName || 'global'];
-    const antLocale = this.context;
-    const localeFromContext = componentName && antLocale ? antLocale[componentName] : {};
+    const localeFromContext =
+      componentName && antLocale ? (antLocale as LocaleInterface)[componentName] : {};
     return {
       ...(typeof locale === 'function' ? locale() : locale),
       ...(localeFromContext || {}),
     };
-  }
+  };
 
-  getLocaleCode() {
-    const antLocale = this.context;
+  const getLocaleCode = () => {
     const localeCode = antLocale && antLocale.locale;
     // Had use LocaleProvide but didn't set locale
     if (antLocale && antLocale.exist && !localeCode) {
       return defaultLocaleData.locale;
     }
     return localeCode;
-  }
+  };
 
-  render() {
-    return this.props.children(this.getLocale(), this.getLocaleCode(), this.context);
-  }
-}
+  return children(getLocale(), getLocaleCode(), antLocale) as React.ReactElement;
+};
+
+export default LocaleReceiver;
