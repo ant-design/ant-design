f488964ed42c4b1f8e9dca339087a9594898233f
xrkffgg <xrkffgg@vip.qq.com>
2020-06-01 15:37:19 +0800

    chore: test lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './_site',
-      url: ['http://localhost/', 'http://localhost/components/button/'],
+      url: ['http://localhost:8001/', 'http://localhost:8001/components/button/'],
     },
     upload: {
       target: 'temporary-public-storage',
