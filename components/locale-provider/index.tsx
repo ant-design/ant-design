23a8567d0eca63c3b97bb2f6ec5ed78e6fdf9c0e
hengkx <ycxzhkx@gmail.com>
2020-06-01 14:57:43 +0800

    Update index.tsx

--- components/locale-provider/index.tsx
+++ components/locale-provider/index.tsx
@@ -42,38 +42,21 @@ export interface LocaleProviderProps {
   _ANT_MARK__?: string;
 }
 
-export default class LocaleProvider extends React.Component<LocaleProviderProps, any> {
-  static defaultProps = {
-    locale: {},
-  };
+const LocaleProvider: React.FC<LocaleProviderProps> = ({ locale = {}, children, _ANT_MARK__ }) => {
+  devWarning(
+    _ANT_MARK__ === ANT_MARK,
+    'LocaleProvider',
+    '`LocaleProvider` is deprecated. Please use `locale` with `ConfigProvider` instead: http://u.ant.design/locale',
+  );
 
-  constructor(props: LocaleProviderProps) {
-    super(props);
-    changeConfirmLocale(props.locale && props.locale.Modal);
+  React.useEffect(() => {
+    changeConfirmLocale(locale && locale.Modal);
+    return () => changeConfirmLocale();
+  }, [locale]);
 
-    devWarning(
-      props._ANT_MARK__ === ANT_MARK,
-      'LocaleProvider',
-      '`LocaleProvider` is deprecated. Please use `locale` with `ConfigProvider` instead: http://u.ant.design/locale',
-    );
-  }
+  return (
+    <LocaleContext.Provider value={{ ...locale, exist: true }}>{children}</LocaleContext.Provider>
+  );
+};
 
-  componentDidUpdate(prevProps: LocaleProviderProps) {
-    const { locale } = this.props;
-    if (prevProps.locale !== locale) {
-      changeConfirmLocale(locale && locale.Modal);
-    }
-  }
-
-  componentWillUnmount() {
-    changeConfirmLocale();
-  }
-
-  render() {
-    const { locale, children } = this.props;
-
-    return (
-      <LocaleContext.Provider value={{ ...locale, exist: true }}>{children}</LocaleContext.Provider>
-    );
-  }
-}
+export default LocaleProvider;
