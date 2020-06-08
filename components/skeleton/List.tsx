1c9da44ee2af4536b3de4c67d3046447d8f7c7a3
Vikas Prashar <vikasprashar99@gmail.com>
2020-06-08 11:24:17 +0530

    Added Skeleton.List feature

---
+++ components/skeleton/List.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import omit from 'omit.js';
+import classNames from 'classnames';
+import Element, { SkeletonElementProps } from './Element';
+import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
+
+export interface SkeletonListProps extends Omit<SkeletonElementProps, 'size'> {
+  size?: 'large' | 'small' | 'default';
+}
+
+const SkeletonList = (props: SkeletonListProps) => {
+  const renderSkeletonList = ({ getPrefixCls }: ConfigConsumerProps) => {
+    const { prefixCls: customizePrefixCls, className, active } = props;
+    const prefixCls = getPrefixCls('skeleton', customizePrefixCls);
+    const otherProps = omit(props, ['prefixCls']);
+    const cls = classNames(prefixCls, className, `${prefixCls}-element`, {
+      [`${prefixCls}-active`]: active,
+    });
+    return (
+      <div className={cls}>
+        <Element prefixCls={`${prefixCls}-list`} {...otherProps} />
+      </div>
+    );
+  };
+  return <ConfigConsumer>{renderSkeletonList}</ConfigConsumer>;
+};
+
+SkeletonList.defaultProps = {
+  size: 'default',
+};
+
+export default SkeletonList;
