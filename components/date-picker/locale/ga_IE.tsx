b30ebb78b9ae0f056b2d0bda1693996a2ac81634
Amumu <yoyo837@hotmail.com>
2020-06-09 13:49:38 +0800

    feat: add ga_IE locale (#24849)
    * add ga_IE locale
    
    * add test case

---
+++ components/date-picker/locale/ga_IE.tsx
@@ -0,0 +1,19 @@
+import CalendarLocale from 'rc-calendar/lib/locale/ga_IE';
+import TimePickerLocale from '../../time-picker/locale/ga_IE';
+
+// Merge into a locale object
+const locale = {
+  lang: {
+    placeholder: 'Select date',
+    rangePlaceholder: ['Start date', 'End date'],
+    ...CalendarLocale,
+  },
+  timePickerLocale: {
+    ...TimePickerLocale,
+  },
+};
+
+// All settings at:
+// https://github.com/ant-design/ant-design/blob/master/components/date-picker/locale/example.json
+
+export default locale;
