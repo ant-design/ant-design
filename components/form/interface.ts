4ebd13ae00724289afed84f7e50cb70b608ced58
kimyongkuk <k0603156@gmail.com>
2020-06-24 23:09:14 +0900

    chore: export form value type

--- components/form/interface.ts
+++ components/form/interface.ts
@@ -1,3 +1,8 @@
 export { Options as ScrollOptions } from 'scroll-into-view-if-needed';
 export type FormLabelAlign = 'left' | 'right';
-export { Store, StoreValue } from 'rc-field-form/lib/interface';
+export {
+  InternalNamePath,
+  Store,
+  StoreValue,
+  ValidateErrorEntity,
+} from 'rc-field-form/lib/interface';
