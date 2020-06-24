5f966158837db3054b1e2b502afa6dc6e6f29c77
xrkffgg <xrkffgg@vip.qq.com>
2020-06-24 18:06:27 +0800

    add maxlength

--- components/avatar/demo/group.md
+++ components/avatar/demo/group.md
@@ -14,7 +14,7 @@ title:
 Avatar group display.
 
 ```tsx
-import { Avatar, Switch } from 'antd';
+import { Avatar, Switch, Divider } from 'antd';
 import { UserOutlined, AntDesignOutlined } from '@ant-design/icons';
 
 class Demo extends React.Component {
@@ -29,9 +29,20 @@ class Demo extends React.Component {
       <>
         <div>
           Hovered：
-          <Switch checked={this.state.loading} onChange={this.change} />
+          <Switch checked={this.state.hovered} onChange={this.change} />
         </div>
         <Avatar.Group hovered={this.state.hovered} style={{ marginTop: 16 }}>
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+          <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+          <Avatar style={{ backgroundColor: '#1890ff' }} icon={<AntDesignOutlined />} />
+        </Avatar.Group>
+        <Divider />
+        <Avatar.Group
+          maxLength={2}
+          hovered={this.state.hovered}
+          excessItemsStyle={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
+        >
           <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
           <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
           <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
--- components/avatar/group.tsx
+++ components/avatar/group.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
+import Avatar from './avatar';
 
 export interface GroupProps {
   className?: string;
@@ -8,12 +9,20 @@ export interface GroupProps {
   style?: React.CSSProperties;
   prefixCls?: string;
   hovered?: boolean;
+  maxLength?: number;
+  excessItemsStyle?: React.CSSProperties;
 }
 
 const Group: React.FC<GroupProps> = props => (
   <ConfigConsumer>
     {({ getPrefixCls, direction }: ConfigConsumerProps) => {
-      const { prefixCls: customizePrefixCls, className = '', hovered } = props;
+      const {
+        prefixCls: customizePrefixCls,
+        className = '',
+        hovered,
+        maxLength,
+        excessItemsStyle,
+      } = props;
       const prefixCls = getPrefixCls('avatar-group', customizePrefixCls);
       const cls = classNames(
         prefixCls,
@@ -23,9 +32,23 @@ const Group: React.FC<GroupProps> = props => (
         },
         className,
       );
+
+      const renderChildren = () => {
+        const { children } = props;
+        const numOfChildren = React.Children.count(children);
+        if (maxLength && maxLength < numOfChildren) {
+          const childrenWithProps = React.Children.toArray(children).slice(0, maxLength);
+          childrenWithProps.push(
+            <Avatar style={excessItemsStyle}>{`+${numOfChildren - maxLength}`}</Avatar>,
+          );
+          return childrenWithProps;
+        }
+        return props.children;
+      };
+
       return (
         <div className={cls} style={props.style}>
-          {props.children}
+          {renderChildren()}
         </div>
       );
     }}
