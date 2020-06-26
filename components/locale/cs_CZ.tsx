3acc028d8a829bac46c0f81bf30aaf4e887ea51f
Karel Šopua <karel.soupa@emz-hanauer.com>
2020-06-26 14:22:48 +0200

    feat: Updated Czech, German and Italian translations for Table locale

--- components/locale/cs_CZ.tsx
+++ components/locale/cs_CZ.tsx
@@ -10,10 +10,21 @@ const localeValues: Locale = {
   DatePicker,
   TimePicker,
   Calendar,
+
   Table: {
     filterTitle: 'Filtr',
     filterConfirm: 'Potvrdit',
     filterReset: 'Obnovit',
+    filterEmptyText: 'Žádné filtry',
+    selectAll: 'Vybrat všehchny řádky na současné stránce',
+    selectInvert: 'Invertovat výběr na současné stránce',
+    selectionAll: 'Vybrat všechny řádky',
+    sortTitle: 'Řadit',
+    expand: 'Rozbalit řádek',
+    collapse: 'Zabalit řádek',
+    triggerDesc: 'Klikni pro sestupné řazení',
+    triggerAsc: 'Klikni pro vzestupné řazení',
+    cancelSort: 'Klikni pro zrušení řazení',
   },
   Modal: {
     okText: 'Ok',
