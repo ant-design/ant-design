bb3b562f5b9ca782b04b84a35c332d1652114ff8
Johannes Andersen <johannes.andersen@signifyd.com>
2020-07-01 23:59:48 +0100

    feat: improve nb_NO translations

--- components/time-picker/locale/nb_NO.tsx
+++ components/time-picker/locale/nb_NO.tsx
@@ -2,6 +2,7 @@ import { TimePickerLocale } from '../index';
 
 const locale: TimePickerLocale = {
   placeholder: 'Velg tid',
+  rangePlaceholder: ['Starttid', 'Sluttid'],
 };
 
 export default locale;
