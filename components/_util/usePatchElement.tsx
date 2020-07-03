3a2f5cf5c951dfb5d811062f8cc11e24b72560d0
07akioni <07akioni2@gmail.com>
2020-07-03 15:42:25 +0800

    chore: comment on usePatchElement

--- components/_util/usePatchElement.tsx
+++ components/_util/usePatchElement.tsx
@@ -7,8 +7,11 @@ export default function usePatchElement(): [
   const [elements, setElements] = React.useState<React.ReactElement[]>([]);
 
   function patchElement(element: React.ReactElement) {
+    // append a new element to elements (and create a new ref)
     setElements(originElements => [...originElements, element]);
 
+    // return a function that removes the new element out of elements (and create a new ref)
+    // it works a little like useEffect
     return () => {
       setElements(originElements => originElements.filter(ele => ele !== element));
     };
