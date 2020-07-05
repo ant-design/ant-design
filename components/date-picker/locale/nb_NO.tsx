54756242ffd1584f8931f68463c94aae3d7de3fb
Johannes Andersen <johannes.andersen@signifyd.com>
2020-07-05 22:47:16 +0100

    Updated rc-picker 1.10.0 -> 1.10.6

--- components/date-picker/locale/nb_NO.tsx
+++ components/date-picker/locale/nb_NO.tsx
@@ -1,10 +1,10 @@
+import CalendarLocale from 'rc-picker/lib/locale/nb_NO';
 import TimePickerLocale from '../../time-picker/locale/nb_NO';
 import { PickerLocale } from '../generatePicker';
 
 // Merge into a locale object
 const locale: PickerLocale = {
   lang: {
-    locale: 'nb_NO',
     placeholder: 'Velg dato',
     yearPlaceholder: 'Velg år',
     quarterPlaceholder: 'Velg kvartal',
@@ -14,40 +14,11 @@ const locale: PickerLocale = {
     rangeYearPlaceholder: ['Startår', 'Sluttår'],
     rangeMonthPlaceholder: ['Startmåned', 'Sluttmåned'],
     rangeWeekPlaceholder: ['Start uke', 'Sluttuke'],
-    today: 'I dag',
-    now: 'Nå',
-    backToToday: 'Tilbake til i dag',
-    ok: 'Ok',
-    clear: 'Fjern',
-    month: 'Måned',
-    year: 'År',
-    timeSelect: 'Velg tid',
-    dateSelect: 'Velg dato',
-    monthSelect: 'Velg en måned',
-    yearSelect: 'Velg et år',
-    decadeSelect: 'Velg et tiår',
-    monthBeforeYear: true,
-    previousMonth: 'Forrige måned (PageUp)',
-    nextMonth: 'Neste måned (PageDown)',
-    previousYear: 'I fjor (Kontroll + venstre)',
-    nextYear: 'Neste år (Kontroll + høyre)',
-    previousDecade: 'Siste tiår',
-    nextDecade: 'Neste tiår',
-    previousCentury: 'Siste århundre',
-    nextCentury: 'Neste århundre',
-    yearFormat: 'YYYY',
-    dateFormat: 'DD.MM.YYYY',
-    dayFormat: 'DD',
-    dateTimeFormat: 'DD.MM.YYYY HH:mm:ss',
+    ...CalendarLocale,
   },
   timePickerLocale: {
-    placeholder: 'Select time',
     ...TimePickerLocale,
   },
-  dateFormat: 'YYYY-MM-DD',
-  dateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
-  weekFormat: 'YYYY-wo',
-  monthFormat: 'YYYY-MM',
 };
 
 // All settings at:
