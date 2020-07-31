c68f4f2d453c267865f45765f6413c1c961eb29d
zombiej <smith3816@gmail.com>
2020-07-31 16:28:15 +0800

    test: add test case of compile

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Rule, RuleObject, RuleRender } from 'rc-field-form/lib/interface';
 import InternalForm, { useForm, FormInstance, FormProps } from './Form';
 import Item, { FormItemProps } from './FormItem';
