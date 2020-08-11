08b82e3dfc0aafa6ac2ebbf99c99b2fbe5401fdd
07akioni <07akioni2@gmail.com>
2020-08-11 12:22:57 +0800

    test: remove afterClose case

--- components/alert/__tests__/index.test.tsx
+++ components/alert/__tests__/index.test.tsx
@@ -21,20 +21,17 @@ describe('Alert', () => {
 
   it('could be closed', () => {
     const onClose = jest.fn();
-    const afterClose = jest.fn();
     const wrapper = mount(
       <Alert
         message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
         type="warning"
         closable
         onClose={onClose}
-        afterClose={afterClose}
       />,
     );
     wrapper.find('.ant-alert-close-icon').simulate('click');
-    expect(onClose).toHaveBeenCalled();
     jest.runAllTimers();
-    expect(afterClose).toHaveBeenCalled();
+    expect(onClose).toHaveBeenCalled();
   });
 
   describe('data and aria props', () => {
