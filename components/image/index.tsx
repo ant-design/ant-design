a6dd4af6895fee50a85c1fc31b1552143f72c9ae
Richard Freling <4776235+rfreling@users.noreply.github.com>
2020-09-11 20:26:09 -0700

    Image | getPreviewContainer

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import RcImage, { ImageProps } from 'rc-image';
 import { ConfigConsumerProps, ConfigContext } from '../config-provider';
 
-const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, ...otherProps }) => {
-  const { getPrefixCls }: ConfigConsumerProps = React.useContext(ConfigContext);
+const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, getPreviewContainer, ...otherProps }) => {
+  const { getPrefixCls, getPopupContainer: getContextPopupContainer }: ConfigConsumerProps = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('image', customizePrefixCls);
 
-  return <RcImage prefixCls={prefixCls} {...otherProps} />;
+  return <RcImage 
+           prefixCls={prefixCls} 
+           getPreviewContainer={getPreviewContainer === undefined ? getContextPopupContainer : getPreviewContainer}
+           {...otherProps} 
+         />;
 };
 
 export default Image;
