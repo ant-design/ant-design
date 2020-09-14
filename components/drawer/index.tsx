6ce8c5c9c5d91c0e8fe8b2df5bccc3b23daa1fc7
nuintun <1730277+nuintun@users.noreply.github.com>
2020-09-14 15:12:48 +0800

    public rc-drawer onChange props

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -45,7 +45,7 @@ export interface DrawerProps {
   prefixCls?: string;
   push?: boolean | PushState;
   placement?: placementType;
-  onChange?: ((visible?: boolean) => void);
+  onChange?: (visible?: boolean) => void;
   onClose?: (e: EventType) => void;
   afterVisibleChange?: (visible: boolean) => void;
   className?: string;
