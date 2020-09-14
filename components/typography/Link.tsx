ab4c3e7c4a0336a1f5597e919428689d9a9e91d8
LeoB-O <liubobotb@126.com>
2020-09-14 17:47:54 +0800

    fix: don't refresh page when use Typography.Link in router(#26624)

--- components/typography/Link.tsx
+++ components/typography/Link.tsx
@@ -6,10 +6,15 @@ export interface LinkProps
   extends BlockProps,
     Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'type'> {
   ellipsis?: boolean;
+  navigate?: Function;
+}
+
+function isModifiedEvent(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+  return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
 }
 
 const Link: React.ForwardRefRenderFunction<HTMLElement, LinkProps> = (
-  { ellipsis, rel, ...restProps },
+  { ellipsis, rel, navigate, onClick, ...restProps },
   ref,
 ) => {
   devWarning(
@@ -22,9 +27,32 @@ const Link: React.ForwardRefRenderFunction<HTMLElement, LinkProps> = (
 
   React.useImperativeHandle(ref, () => baseRef.current?.contentRef.current!);
 
+  const { target } = restProps;
+
   const mergedProps = {
     ...restProps,
     rel: rel === undefined && restProps.target === '_blank' ? 'noopener noreferrer' : rel,
+    onClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      try {
+        if (onClick) {
+          onClick(event);
+        }
+      } catch (ex) {
+        event.preventDefault();
+        throw ex;
+      }
+
+      if (
+        navigate && // react-router-dom Link
+        !event.defaultPrevented && // onClick prevented default
+        event.button === 0 && // ignore everything but left clicks
+        (!target || target === '_self') && // let browser handle "target=_blank" etc.
+        !isModifiedEvent(event) // ignore clicks with modifier keys
+      ) {
+        event.preventDefault();
+        navigate();
+      }
+    },
   };
 
   // https://github.com/ant-design/ant-design/issues/26622
