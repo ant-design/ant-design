d9bd2586717ae068ada5629668aac325a56b5497
Richard Freling <4776235+rfreling@users.noreply.github.com>
2020-09-22 09:50:18 -0700

    getPreviewContainer -> getPopupContainer
    https://github.com/react-component/image/pull/29/commits/e04ceb6d9fe9cdf2d058be05c300407505e3a217

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import RcImage, { ImageProps } from 'rc-image';
 import { ConfigConsumerProps, ConfigContext } from '../config-provider';
 
-const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, getPreviewContainer, ...otherProps }) => {
+const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, getPopupContainer, ...otherProps }) => {
   const { getPrefixCls, getPopupContainer: getContextPopupContainer }: ConfigConsumerProps = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('image', customizePrefixCls);
 
   return <RcImage 
            prefixCls={prefixCls} 
-           getPreviewContainer={getPreviewContainer === undefined ? getContextPopupContainer : getPreviewContainer}
+           getPopupContainer={getPopupContainer === undefined ? getContextPopupContainer : getPopupContainer}
            {...otherProps} 
          />;
 };
