97ffe7044f7f4ce502a3d819e6e0184289a771be
lilywang <wangxiaoying@supermonkey.com.cn>
2020-09-24 17:34:11 +0800

    feat: export ImageProps in ImageProps.tsx

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import RcImage, { ImageProps } from 'rc-image';
+import RcImage, { ImageProps as RcImageProps } from 'rc-image';
 import { ConfigConsumerProps, ConfigContext } from '../config-provider';
+
+export interface ImageProps extends RcImageProps {}
 
 const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, ...otherProps }) => {
   const { getPrefixCls }: ConfigConsumerProps = React.useContext(ConfigContext);
