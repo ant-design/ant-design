1acf5bed5b3ee074dfbd4bdf6e1940a04814156f
zombiej <smith3816@gmail.com>
2020-09-27 23:54:40 +0800

    fix test

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -280,4 +280,9 @@ export interface NotificationApi extends NotificationInstance {
   useNotification: () => [NotificationInstance, React.ReactElement];
 }
 
+/** @private test only function. Not work on production */
+export const getInstance = async (cacheKey: string) => {
+  return process.env.NODE_ENV === 'test' ? notificationInstance[cacheKey] : null;
+};
+
 export default api as NotificationApi;
