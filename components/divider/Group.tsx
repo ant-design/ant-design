30a7fa73889fa99ff7c0784609ddd9097b03ce5b
Tom Xu <ycxzhkx@gmail.com>
2020-09-29 20:22:06 +0800

    rtl

--- components/divider/Group.tsx
+++ components/divider/Group.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import toArray from 'rc-util/lib/Children/toArray';
 import Divider, { DividerProps } from './Divider';
+import { ConfigContext } from '../config-provider';
 
 export interface DividerGroupProps extends Omit<DividerProps, 'plain' | 'orientation'> {}
 
 const Group: React.FC<DividerGroupProps> = ({ children, ...props }) => {
+  const { direction } = React.useContext(ConfigContext);
+
   const childNodes = toArray(children);
+
   if (childNodes.length === 0) {
     return null;
   }
 
-  const nodes = childNodes.map((child, i) => {
+  const nodes = (direction === 'rtl' ? childNodes.reverse() : childNodes).map((child, i) => {
     /* eslint-disable react/no-array-index-key */
     return (
       <React.Fragment key={i}>
