0010f3b18a9428997f217cf204bc7de2d4407d0e
Tom Xu <ycxzhkx@gmail.com>
2020-10-03 19:22:33 +0800

    Update TimelineItem.tsx

--- components/timeline/TimelineItem.tsx
+++ components/timeline/TimelineItem.tsx
@@ -17,7 +17,7 @@ export interface TimelineItemProps {
 // for compatibililty
 // https://github.com/ant-design/ant-design/pull/26832
 export interface TimeLineItemProps extends TimelineItemProps {
-  __deprecated_do_not_use_it__?: any; // eslint-disable-line @typescript-eslint/camelcase
+  __deprecated_do_not_use_it__?: any; // eslint-disable-line camelcase
 }
 
 const TimelineItem: React.FC<TimelineItemProps> = props => {
