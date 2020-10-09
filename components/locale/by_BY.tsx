e5b250f799cc7004f1c7e2be0fc056af744f38bf
Ivan Stsilba <stivan1717@gmail.com>
2020-10-09 10:53:47 +0300

    add Belarusian locale, fix bugs

--- components/locale/by_BY.tsx
+++ components/locale/by_BY.tsx
@@ -1,93 +1,17 @@
-const localeValues = {
+import Pagination from 'rc-pagination/lib/locale/by_BY';
+import DatePicker from '../date-picker/locale/by_BY';
+import TimePicker from '../time-picker/locale/by_BY';
+import Calendar from '../calendar/locale/by_BY';
+import { Locale } from '../locale-provider';
+
+var typeTemplate: string = "${label} не з'яўляецца тыпам ${type}";
+
+const localeValues: Locale = {
   locale: 'by',
-  Pagination: {
-    // Options.jsx
-    items_per_page: '/ стр.',
-    jump_to: 'Перайсці',
-    jump_to_confirm: 'Пацвердзіць',
-    page: '',
-    // Pagination.jsx
-    prev_page: 'Назад',
-    next_page: 'Наперад',
-    prev_5: 'Папярэднія 5',
-    next_5: 'Наступныя 5',
-    prev_3: 'Папярэднія 3',
-    next_3: 'Наступныя 3',
-  },
-  DatePicker: {
-    lang: {
-      placeholder: 'Выберыце дату',
-      yearPlaceholder: 'Выберыце год',
-      quarterPlaceholder: 'Выберыце квартал',
-      monthPlaceholder: 'Выберыце месяц',
-      weekPlaceholder: 'Выберыце тыдзень',
-      rangePlaceholder: ['Пачатковая дата', 'Канчатковая дата'],
-      rangeYearPlaceholder: ['Пачатковы год', 'Год заканчэння'],
-      rangeMonthPlaceholder: ['Пачатковы месяц', 'Канчатковы месяц'],
-      rangeWeekPlaceholder: ['Пачатковы тыдзень', 'Канчатковы тыдзень'],
-      today: 'Сёння',
-      now: 'Зараз',
-      backToToday: 'Дадзеная дата',
-      ok: 'Ok',
-      clear: 'Ачысціць',
-      month: 'Месяц',
-      year: 'Год',
-      timeSelect: 'Выбраць час',
-      dateSelect: 'Выбраць дату',
-      monthSelect: 'Выбраць месяц',
-      yearSelect: 'Выбраць год',
-      decadeSelect: 'Выбраць дзесяцігоддзе',
-      yearFormat: 'YYYY',
-      dateFormat: 'D-M-YYYY',
-      dayFormat: 'D',
-      dateTimeFormat: 'D-M-YYYY HH:mm:ss',
-      monthBeforeYear: true,
-      previousMonth: 'Папярэдні месяц (PageUp)',
-      nextMonth: 'Наступны месяц (PageDown)',
-      previousYear: 'Папярэдні год (Control + left)',
-      nextYear: 'Наступны год (Control + right)',
-      previousDecade: 'Папярэдняе дзесяцігоддзе',
-      nextDecade: 'Наступнае дзесяцігоддзе',
-      previousCentury: 'Папярэдні век',
-      nextCentury: 'Наступны век',
-    },
-    timePickerLocale: {
-      placeholder: 'Выберыце час',
-      rangePlaceholder: ['Час пачатку', 'Час заканчэння'],
-    },
-  },
-  TimePicker: {
-    placeholder: 'Выберыце час',
-    rangePlaceholder: ['Час пачатку', 'Час заканчэння'],
-  },
-  Calendar: {
-    locale: 'by_BY',
-    today: 'Сёння',
-    now: 'Зараз',
-    backToToday: 'Дадзеная дата',
-    ok: 'Ok',
-    clear: 'Ачысціць',
-    month: 'Месяц',
-    year: 'Год',
-    timeSelect: 'Выбраць час',
-    dateSelect: 'Выбраць дату',
-    monthSelect: 'Выбраць месяц',
-    yearSelect: 'Выбраць год',
-    decadeSelect: 'Выбраць дзесяцігоддзе',
-    yearFormat: 'YYYY',
-    dateFormat: 'D-M-YYYY',
-    dayFormat: 'D',
-    dateTimeFormat: 'D-M-YYYY HH:mm:ss',
-    monthBeforeYear: true,
-    previousMonth: 'Папярэдні месяц (PageUp)',
-    nextMonth: 'Наступны месяц (PageDown)',
-    previousYear: 'Папярэдні год (Control + left)',
-    nextYear: 'Наступны год (Control + right)',
-    previousDecade: 'Папярэдняе дзесяцігоддзе',
-    nextDecade: 'Наступнае дзесяцігоддзе',
-    previousCentury: 'Папярэдні век',
-    nextCentury: 'Наступны век',
-  },
+  Pagination: Pagination,
+  DatePicker: DatePicker,
+  TimePicker: TimePicker,
+  Calendar: Calendar,
   global: {
     placeholder: 'Калі ласка выберыце',
   },
@@ -145,10 +69,59 @@ const localeValues = {
     edit: 'Рэдагаваць',
     copy: 'Капіяваць',
     copied: 'Капіяванне завершана',
-    expand: 'Раскрыць',
+    expand: 'Разгарнуць',
   },
   PageHeader: {
     back: 'Назад',
   },
+  Form: {
+    defaultValidateMessages: {
+      default: 'Памылка праверкі поля ${label}',
+      required: 'Калі ласка, увядзіце ${label}',
+      enum: '${label} павінен быць адным з [${enum}]',
+      whitespace: '${label} не можа быць пустым',
+      date: {
+        format: '${label} няправільны фармат даты',
+        parse: '${label} не можа быць пераўтворана ў дату',
+        invalid: "${label} не з'яўляецца карэктнай датай",
+      },
+      types: {
+        string: typeTemplate,
+        method: typeTemplate,
+        array: typeTemplate,
+        object: typeTemplate,
+        number: typeTemplate,
+        date: typeTemplate,
+        boolean: typeTemplate,
+        integer: typeTemplate,
+        float: typeTemplate,
+        regexp: typeTemplate,
+        email: typeTemplate,
+        url: typeTemplate,
+        hex: typeTemplate,
+      },
+      string: {
+        len: '${label} павінна быць ${len} сімвалаў',
+        min: '${label} павінна быць больш або роўная ${min} сімвалаў',
+        max: '${label} павінна быць менш або роўная ${max} сімвалаў',
+        range: 'Даўжыня ${label} павінна быць паміж ${min}-${max} сімвалаў',
+      },
+      number: {
+        len: '${label} павінна быць роўная ${len}',
+        min: '${label} павінна быць больш або роўная ${min}',
+        max: '${label} павінна быць больш або роўная ${max}',
+      },
+      array: {
+        len: 'Колькасць элементаў ${label} павінна быць роўная ${len}',
+        min: 'Колькасць элементаў ${label} павінна быць больш або роўная ${min}',
+        max: 'Колькасць элементаў ${label} павінна быць менш або роўная ${max}',
+        range: 'Колькасць элементаў ${label} павінна быць паміж ${min} і ${max}',
+      },
+      pattern: {
+        mismatch: '${label} не адпавядае шаблону ${pattern}',
+      },
+    },
+  },
 };
+
 export default localeValues;
