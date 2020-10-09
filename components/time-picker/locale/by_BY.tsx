4107c8323433730b9bfe2f3cb84d59e9c792c7b4
StIvan8 <61523186+StIvan8@users.noreply.github.com>
2020-10-09 16:37:07 +0300

    feat: add Belarusian translation (#27028)
    * add Belarusian language translation
    
    * add Belarusian locale, fix bugs
    
    * fix: slider and select version

---
+++ components/time-picker/locale/by_BY.tsx
@@ -0,0 +1,8 @@
+import { TimePickerLocale } from '../index';
+
+const locale: TimePickerLocale = {
+  placeholder: 'Выберыце час',
+  rangePlaceholder: ['Час пачатку', 'Час заканчэння'],
+};
+
+export default locale;
