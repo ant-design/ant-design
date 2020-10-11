eaaebc1e5edaca5c584408c2f3150998e8fb618d
Tom Xu <ycxzhkx@gmail.com>
2020-10-11 22:44:12 +0800

    refactor(image): improve code

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import RcImage, { ImageProps } from 'rc-image';
-import { ConfigConsumerProps, ConfigContext } from '../config-provider';
+import { ConfigContext } from '../config-provider';
 
 const Image: React.FC<ImageProps> = ({ prefixCls: customizePrefixCls, ...otherProps }) => {
-  const { getPrefixCls }: ConfigConsumerProps = React.useContext(ConfigContext);
+  const { getPrefixCls } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('image', customizePrefixCls);
 
   return <RcImage prefixCls={prefixCls} {...otherProps} />;
