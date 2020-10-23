1b4c255f2b30ac7d0aa2f2b87bd4421cd8b20ea4
shaodahong <darryshaw@gmail.com>
2020-10-23 16:29:19 +0800

    fix test coverage

--- .jest.js
+++ .jest.js
@@ -42,7 +42,7 @@ module.exports = {
     '!components/*/locale/index.tsx',
     '!components/*/__tests__/type.test.tsx',
     '!components/**/*/interface.{ts,tsx}',
-    '!components/*/__tests__/image.test.tsx',
+    '!components/*/__tests__/image.test.{ts,tsx}',
   ],
   transformIgnorePatterns,
   snapshotSerializers: ['enzyme-to-json/serializer'],
