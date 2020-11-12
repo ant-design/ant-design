9d7a72f1aa823c3421710a70bc6bbda282b0942a
Tom Xu <ycxzhkx@gmail.com>
2020-11-12 18:01:47 +0800

    Update layout.tsx

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { ConfigContext } from '../config-provider';
-import useForceUpdate from '../_util/hooks/useForceUpdate';
 
 export interface GeneratorProps {
   suffixCls: string;
@@ -53,16 +52,13 @@ const Basic = (props: BasicPropsWithTagName) => {
 const BasicLayout: React.FC<BasicPropsWithTagName> = props => {
   const { direction } = React.useContext(ConfigContext);
 
-  const forceUpdate = useForceUpdate();
-
-  const siders = React.useRef<string[]>([]);
+  const [siders, setSiders] = React.useState<string[]>([]);
 
   const { prefixCls, className, children, hasSider, tagName: Tag, ...others } = props;
   const classString = classNames(
     prefixCls,
     {
-      [`${prefixCls}-has-sider`]:
-        typeof hasSider === 'boolean' ? hasSider : siders.current.length > 0,
+      [`${prefixCls}-has-sider`]: typeof hasSider === 'boolean' ? hasSider : siders.length > 0,
       [`${prefixCls}-rtl`]: direction === 'rtl',
     },
     className,
@@ -73,12 +69,10 @@ const BasicLayout: React.FC<BasicPropsWithTagName> = props => {
       value={{
         siderHook: {
           addSider: (id: string) => {
-            siders.current = [...siders.current, id];
-            forceUpdate();
+            setSiders(prev => [...prev, id]);
           },
           removeSider: (id: string) => {
-            siders.current = siders.current.filter(currentId => currentId !== id);
-            forceUpdate();
+            setSiders(prev => prev.filter(currentId => currentId !== id));
           },
         },
       }}
