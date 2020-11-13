bbe148d01601f560589d9ebe1d068fa7536cec75
wf-erikslokharts <74410386+wf-erikslokharts@users.noreply.github.com>
2020-11-13 14:04:52 +0000

    Update LoadingIcon.tsx

--- components/button/LoadingIcon.tsx
+++ components/button/LoadingIcon.tsx
@@ -16,7 +16,7 @@ const getRealWidth = (node: HTMLElement) => ({
   transform: 'scale(1)',
 });
 
-export default function LoadingIcon({ prefixCls, loading, existIcon }: LoadingIconProps) {
+const LoadingIcon: React.FC<LoadingIconProps> = ({ prefixCls, loading, existIcon }) => {
   const visible = !!loading;
 
   if (existIcon) {
@@ -50,3 +50,5 @@ export default function LoadingIcon({ prefixCls, loading, existIcon }: LoadingIc
     </CSSMotion>
   );
 }
+
+export default LoadingIcon;
