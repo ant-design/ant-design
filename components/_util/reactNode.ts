65b1c2fccb12067f9f3c8f2fe80107a11bd3eb24
zombiej <smith3816@gmail.com>
2020-11-17 21:49:43 +0800

    refactor: Replace BackTop with rc-motion

--- components/_util/reactNode.ts
+++ components/_util/reactNode.ts
@@ -2,16 +2,20 @@ import * as React from 'react';
 
 export const { isValidElement } = React;
 
+type AnyObject = Record<any, any>;
+
+type RenderProps = undefined | AnyObject | ((originProps: AnyObject) => AnyObject | undefined);
+
 export function replaceElement(
   element: React.ReactNode,
   replacement: React.ReactNode,
-  props: any,
+  props: RenderProps,
 ): React.ReactNode {
   if (!isValidElement(element)) return replacement;
 
-  return React.cloneElement(element, typeof props === 'function' ? props() : props);
+  return React.cloneElement(element, typeof props === 'function' ? props(element.props) : props);
 }
 
-export function cloneElement(element: React.ReactNode, props?: any): React.ReactElement {
+export function cloneElement(element: React.ReactNode, props?: RenderProps): React.ReactElement {
   return replaceElement(element, element, props) as React.ReactElement;
 }
