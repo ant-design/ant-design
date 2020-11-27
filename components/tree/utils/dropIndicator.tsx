fc4e29b32a4dbb8cc9bf0d2826a450586dfff8c3
07akioni <07akioni2@gmail.com>
2020-11-28 00:55:02 +0800

    refactor: Tree drag behavior

---
+++ components/tree/utils/dropIndicator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const offset = 4;
+
+export default function dropIndicatorRender(props: {
+  dropPosition: -1 | 0 | 1;
+  dropLevelOffset: number;
+  indent: number;
+  prefixCls: string;
+  direction: 'ltr' | 'rtl';
+}) {
+  const { dropPosition, dropLevelOffset, prefixCls, indent, direction } = props;
+  const startPosition = direction === 'ltr' ? 'left' : 'right';
+  const endPosition = direction === 'ltr' ? 'right' : 'left';
+  const style: React.CSSProperties = {
+    [startPosition]: -dropLevelOffset * indent + offset,
+    [endPosition]: 0,
+  };
+  switch (dropPosition) {
+    case -1:
+      style.top = -3;
+      break;
+    case 1:
+      style.bottom = -3;
+      break;
+    case 0:
+      style.bottom = -3;
+      style[startPosition] = indent + offset;
+      break;
+    default:
+      break;
+  }
+  return <div style={style} className={`${prefixCls}-drop-indicator`} />;
+}
