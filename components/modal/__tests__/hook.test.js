59a33b0f3d70490c41be9d3b287a96256863bb95
juniortourr <juniortour@qq.com>
2020-11-29 19:30:36 +0800

    fix: hook modal not trigger onCancel

--- components/modal/__tests__/hook.test.js
+++ components/modal/__tests__/hook.test.js
@@ -62,4 +62,42 @@ describe('Modal.hook', () => {
 
     jest.useRealTimers();
   });
+
+  it('hooks modal should trigger onCancel', () => {
+    let cancelCount = 0;
+    const Demo = () => {
+      const [modal, contextHolder] = Modal.useModal();
+
+      const openBrokenModal = React.useCallback(() => {
+        modal.info({
+          okType: 'default',
+          maskClosable: true,
+          okCancel: true,
+          onCancel: () => {
+            cancelCount += 1;
+          },
+          content: 'Hello!',
+        });
+      }, [modal]);
+
+      return (
+        <div className="App">
+          {contextHolder}
+          <div className="open-hook-modal-btn" onClick={openBrokenModal}>
+            Test hook modal
+          </div>
+        </div>
+      );
+    };
+
+    const wrapper = mount(<Demo />);
+
+    wrapper.find('.open-hook-modal-btn').simulate('click');
+    wrapper.find('.ant-modal-confirm-btns .ant-btn').first().simulate('click');
+    expect(cancelCount).toEqual(1); // click cancel btn, trigger onCancel
+
+    wrapper.find('.open-hook-modal-btn').simulate('click');
+    wrapper.find('.ant-modal-wrap').simulate('click');
+    expect(cancelCount).toEqual(2); // click modal wrapper, trigger onCancel
+  });
 });
