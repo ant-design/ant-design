74515aa3099e51f781b377c97341969d95bc82a7
afc163 <afc163@gmail.com>
2020-12-01 16:44:14 +0800

    fix react 16 ci

--- tests/setup.js
+++ tests/setup.js
@@ -33,7 +33,7 @@ const Enzyme = require('enzyme');
 
 const Adapter =
   process.env.REACT === '16'
-    ? require('enzyme-adapter-react-16') // eslint-disable-line import/no-extraneous-dependencies
+    ? require('enzyme-adapter-react-16') // eslint-disable-line import/no-extraneous-dependencies,import/no-unresolved
     : require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
