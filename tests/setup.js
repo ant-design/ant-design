32a9ba52b87513760206a61bab9af02c8874e8c1
Tom Xu <ycxzhkx@gmail.com>
2020-12-01 18:28:35 +0800

    chore: 🆙 upgrade react 17 (#27268)
    #26136

--- tests/setup.js
+++ tests/setup.js
@@ -31,7 +31,10 @@ if (typeof window !== 'undefined') {
 
 const Enzyme = require('enzyme');
 
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter =
+  process.env.REACT === '16'
+    ? require('enzyme-adapter-react-16') // eslint-disable-line import/no-extraneous-dependencies,import/no-unresolved
+    : require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
 
