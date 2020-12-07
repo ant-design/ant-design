9cd4fab604450c8d10327c75a7df22691c2f275b
二货机器人 <smith3816@gmail.com>
2020-12-07 17:08:48 +0800

    feat: Image add preview style (#28235)
    * feat: Add preview mask
    
    * feat: Preview support locale
    
    * test: Update snapshot
    
    * fix lint
    
    * fix lint

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
+import { useContext } from 'react';
+import EyeOutlined from '@ant-design/icons/EyeOutlined';
 import RcImage, { ImageProps } from 'rc-image';
+import defaultLocale from '../locale/en_US';
 import PreviewGroup from './PreviewGroup';
 import { ConfigContext } from '../config-provider';
 
@@ -7,11 +10,34 @@ export interface CompositionImage<P> extends React.FC<P> {
   PreviewGroup: typeof PreviewGroup;
 }
 
-const Image: CompositionImage<ImageProps> = ({ prefixCls: customizePrefixCls, ...otherProps }) => {
-  const { getPrefixCls } = React.useContext(ConfigContext);
+const Image: CompositionImage<ImageProps> = ({
+  prefixCls: customizePrefixCls,
+  preview,
+  ...otherProps
+}) => {
+  const { getPrefixCls } = useContext(ConfigContext);
   const prefixCls = getPrefixCls('image', customizePrefixCls);
 
-  return <RcImage prefixCls={prefixCls} {...otherProps} />;
+  const { locale: contextLocale = defaultLocale } = useContext(ConfigContext);
+  const imageLocale = contextLocale.Image || defaultLocale.Image;
+
+  const mergedPreview = React.useMemo(() => {
+    if (preview === false) {
+      return preview;
+    }
+
+    return {
+      mask: (
+        <div className={`${prefixCls}-mask-info`}>
+          <EyeOutlined />
+          {imageLocale?.preview}
+        </div>
+      ),
+      ...(typeof preview === 'object' ? preview : null),
+    };
+  }, [preview, imageLocale]);
+
+  return <RcImage prefixCls={prefixCls} preview={mergedPreview} {...otherProps} />;
 };
 
 export { ImageProps };
