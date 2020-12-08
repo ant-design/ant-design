8559e138aa4a4a59fa4d07ce9d16e0a876f329d8
偏右 <afc163@gmail.com>
2020-12-08 09:42:52 +0800

    Update index.tsx

--- components/affix/index.tsx
+++ components/affix/index.tsx
@@ -1,10 +1,3 @@
-import * as React from 'react';
-import classNames from 'classnames';
-import omit from 'omit.js';
-import ResizeObserver from 'rc-resize-observer';
-import { ConfigContext, ConfigConsumerProps } from '../config-provider';
-import { throttleByAnimationFrameDecorator } from '../_util/throttleByAnimationFrame';
-
 import {
   addObserveTarget,
   removeObserveTarget,
