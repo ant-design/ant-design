11baf92e66a6030499c67c8221c87e675ed7928d
二货机器人 <smith3816@gmail.com>
2020-12-08 11:16:12 +0800

    Update index.tsx

--- components/affix/index.tsx
+++ components/affix/index.tsx
@@ -1,17 +1,3 @@
-import * as React from 'react';
-import classNames from 'classnames';
-import omit from 'omit.js';
-import ResizeObserver from 'rc-resize-observer';
-import { ConfigContext, ConfigConsumerProps } from '../config-provider';
-import { throttleByAnimationFrameDecorator } from '../_util/throttleByAnimationFrame';
-
-import {
-  addObserveTarget,
-  removeObserveTarget,
-  getTargetRect,
-  getFixedTop,
-  getFixedBottom,
-} from './utils';
 
 function getDefaultTarget() {
   return typeof window !== 'undefined' ? window : null;
