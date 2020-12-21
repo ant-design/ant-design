fb5d092a10a5495052f38fd5cd75a07509a04275
Filip Kukovec <filip.kukovec1@gmail.com>
2020-12-21 09:31:13 +0100

    Update hr_HR.tsx
    Disable no-template-curly-in-string lint error

--- components/locale/hr_HR.tsx
+++ components/locale/hr_HR.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 import Pagination from 'rc-pagination/lib/locale/hr_HR';
 import DatePicker from '../date-picker/locale/hr_HR';
 import TimePicker from '../time-picker/locale/hr_HR';
