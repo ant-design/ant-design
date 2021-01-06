46855d3e17a745ba2b9a7b0c8d0b99820c07703c
afc163 <afc163@gmail.com>
2021-01-06 21:57:29 +0800

    fix image preview icon missing

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import EyeOutlined from '@ant-design/icons/EyeOutlined';
+import RotateLeftOutlined from '@ant-design/icons/RotateLeftOutlined';
+import RotateRightOutlined from '@ant-design/icons/RotateRightOutlined';
+import ZoomInOutlined from '@ant-design/icons/ZoomInOutlined';
+import ZoomOutOutlined from '@ant-design/icons/ZoomOutOutlined';
+import CloseOutlined from '@ant-design/icons/CloseOutlined';
+import LeftOutlined from '@ant-design/icons/LeftOutlined';
+import RightOutlined from '@ant-design/icons/RightOutlined';
 import RcImage, { ImageProps } from 'rc-image';
 import defaultLocale from '../locale/en_US';
 import PreviewGroup from './PreviewGroup';
@@ -33,6 +40,15 @@ const Image: CompositionImage<ImageProps> = ({
           {imageLocale?.preview}
         </div>
       ),
+      icons: {
+        rotateLeft: <RotateLeftOutlined />,
+        rotateRight: <RotateRightOutlined />,
+        zoomIn: <ZoomInOutlined />,
+        zoomOut: <ZoomOutOutlined />,
+        close: <CloseOutlined />,
+        left: <LeftOutlined />,
+        right: <RightOutlined />,
+      },
       ...(typeof preview === 'object' ? preview : null),
     };
   }, [preview, imageLocale]);
