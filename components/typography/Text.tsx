8a3464c2f2499defdbdeb261df8bcedfd6aceafb
zombiej <smith3816@gmail.com>
2021-01-12 11:42:01 +0800

    feat: ellipsis support tooltip

--- components/typography/Text.tsx
+++ components/typography/Text.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
+import omit from 'omit.js';
 import devWarning from '../_util/devWarning';
-import Base, { BlockProps } from './Base';
+import Base, { BlockProps, EllipsisConfig } from './Base';
 
 export interface TextProps extends BlockProps {
-  ellipsis?: boolean;
+  ellipsis?: Omit<EllipsisConfig, 'expandable' | 'rows' | 'onExpand'>;
 }
 
 const Text: React.FC<TextProps> = ({ ellipsis, ...restProps }) => {
-  devWarning(
-    typeof ellipsis !== 'object',
-    'Typography.Text',
-    '`ellipsis` only supports boolean value.',
-  );
-  return <Base {...restProps} ellipsis={!!ellipsis} component="span" />;
+  const mergedEllipsis = React.useMemo(() => {
+    if (ellipsis && typeof ellipsis === 'object') {
+      return omit(ellipsis, ['expandable', 'rows']);
+    }
+
+    return ellipsis;
+  }, [ellipsis]);
+
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    typeof ellipsis === 'object' &&
+    ellipsis &&
+    ('expandable' in ellipsis || 'rows' in ellipsis)
+  ) {
+    devWarning(false, 'Typography.Text', '`ellipsis` do not support `expandable` or `rows` props.');
+  }
+
+  return <Base {...restProps} ellipsis={mergedEllipsis} component="span" />;
 };
 
 export default Text;
