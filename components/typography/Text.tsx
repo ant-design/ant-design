a5bdf5aedd66feef14626718a23ce9a7284cf233
afc163 <afc163@gmail.com>
2021-01-14 14:42:01 +0800

    chore: fix tsc error

--- components/typography/Text.tsx
+++ components/typography/Text.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import omit from 'omit.js';
+import omit from 'rc-util/lib/omit';
 import devWarning from '../_util/devWarning';
 import Base, { BlockProps, EllipsisConfig } from './Base';
 
