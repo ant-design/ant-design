7e513fb50b9940babdb9e03908c740ef30da2cd2
xrkffgg <xrkffgg@vip.qq.com>
2021-01-19 17:55:43 +0800

    up

--- scripts/make-release.js
+++ scripts/make-release.js
@@ -33,51 +33,47 @@ function getChangelog(content, version) {
 }
 
 async function main() {
+  const { owner, repo } = github.context.repo;
+  const { ref_type: refType, ref: version } = github.context.payload;
+
+  if (owner !== 'ant-design' || repo !== 'ant-design') {
+    return false;
+  }
+
+  if (refType !== 'tag') {
+    return false;
+  }
+
+  let enChangelog = '';
+  let cnChangelog = '';
+
+  for (let i = 0; i < branches.length; i += 1) {
+    const url = `https://raw.githubusercontent.com/ant-design/ant-design/${branches[i]}/`;
+    const enChangelogContent = await axios.get(`${url}/CHANGELOG.en-US.md`);
+    enChangelog = enChangelog || getChangelog(enChangelogContent.data, version);
+    const cnChangelogContent = await axios.get(`${url}/CHANGELOG.zh-CN.md`);
+    cnChangelog = cnChangelog || getChangelog(cnChangelogContent.data, version);
+  }
+
+  const changelog = [enChangelog, '\n', '---', '\n', cnChangelog].join('\n');
+
   try {
-    const { owner, repo } = github.context.repo;
-    const { ref_type: refType, ref: version } = github.context.payload;
+    await octokit.repos.createRelease({
+      owner,
+      repo,
+      tag_name: version,
+      name: version,
+      body: changelog,
+    });
 
-    if (owner !== 'ant-design' || repo !== 'ant-design') {
-      return false;
-    }
-
-    if (refType !== 'tag') {
-      return false;
-    }
-
-    let enChangelog = '';
-    let cnChangelog = '';
-
-    for (let i = 0; i < branches.length; i += 1) {
-      const url = `https://raw.githubusercontent.com/ant-design/ant-design/${branches[i]}/`;
-      const enChangelogContent = await axios.get(`${url}/CHANGELOG.en-US.md`);
-      enChangelog = enChangelog || getChangelog(enChangelogContent.data, version);
-      const cnChangelogContent = await axios.get(`${url}/CHANGELOG.zh-CN.md`);
-      cnChangelog = cnChangelog || getChangelog(cnChangelogContent.data, version);
-    }
-
-    const changelog = [enChangelog, '\n', '---', '\n', cnChangelog].join('\n');
-
-    try {
-      await octokit.repos.createRelease({
-        owner,
-        repo,
-        tag_name: version,
-        name: version,
-        body: changelog,
+    if (cnChangelog) {
+      axios.post(`https://oapi.dingtalk.com/robot/send?access_token=${ddToken}`, {
+        msgtype: 'markdown',
+        markdown: {
+          title: `${version} 发布日志`,
+          text: `# ${version} 发布日志 \n\n ${cnChangelog}`,
+        },
       });
-
-      if (cnChangelog) {
-        axios.post(`https://oapi.dingtalk.com/robot/send?access_token=${ddToken}`, {
-          msgtype: 'markdown',
-          markdown: {
-            title: `${version} 发布日志`,
-            text: `# ${version} 发布日志 \n\n ${cnChangelog}`,
-          },
-        });
-      }
-    } catch (error) {
-      console.log(error);
     }
   } catch (error) {
     console.log(error);
