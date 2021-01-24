1d1f5e5ec1e22f84b56ff0c9af4625f44ff88bd5
chenshuai2144 <qixian.cs@outlook.com>
2021-01-24 20:55:07 +0800

    fix test

--- components/grid/index.tsx
+++ components/grid/index.tsx
@@ -2,6 +2,10 @@ import Row from './row';
 import Col from './col';
 import useBreakpoint from './hooks/useBreakpoint';
 
+export { RowProps } from './row';
+
+export { ColProps, ColSize } from './col';
+
 export { Row, Col };
 
 export default { useBreakpoint };
