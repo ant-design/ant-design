fcb88de4816ddc0a63f1581e68a5831b65510de5
amiralitaheri <amiralitaheri64@gmail.com>
2021-02-05 13:43:50 +0330

    feat: Add missing locale value for time-picker/locale/fa_IR.tsx
    Signed-off-by: amiralitaheri <amiralitaheri64@gmail.com>

--- components/time-picker/locale/fa_IR.tsx
+++ components/time-picker/locale/fa_IR.tsx
@@ -2,6 +2,7 @@ import { TimePickerLocale } from '../index';
 
 const locale: TimePickerLocale = {
   placeholder: 'انتخاب زمان',
+  rangePlaceholder: ['زمان شروع', 'زمان پایان'],
 };
 
 export default locale;
