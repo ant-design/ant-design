1de4d84486511d3498f455781057ee87407b32b3
Rui H Banki <ruihbanki@gmail.com>
2021-02-09 10:23:59 -0300

    fix: Change role prop place to work with screen readers

--- components/form/ErrorList.tsx
+++ components/form/ErrorList.tsx
@@ -83,6 +83,7 @@ export default function ErrorList({
           <div
             className={classNames(baseClassName, holderClassName, rootClassName)}
             style={holderStyle}
+            role="alert"
             id={`${fieldId}_help`}
           >
             <CSSMotionList
@@ -103,7 +104,6 @@ export default function ErrorList({
                 return (
                   <div
                     key={key}
-                    role="alert"
                     className={classNames(itemClassName, {
                       [`${baseClassName}-${errorStatus}`]: errorStatus,
                     })}
