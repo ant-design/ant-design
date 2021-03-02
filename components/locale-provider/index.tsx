7a7a8c1ced01eaf4187827fb9c57d597fc61a0bf
黄建峰 <645381995@qq.com>
2021-03-02 09:42:55 +0800

    fix: 修复切换页面 changeConfirmLocale 被清空问题

--- components/locale-provider/index.tsx
+++ components/locale-provider/index.tsx
@@ -53,13 +53,15 @@ export default class LocaleProvider extends React.Component<LocaleProviderProps,
 
   constructor(props: LocaleProviderProps) {
     super(props);
-    changeConfirmLocale(props.locale && props.locale.Modal);
-
     devWarning(
       props._ANT_MARK__ === ANT_MARK,
       'LocaleProvider',
       '`LocaleProvider` is deprecated. Please use `locale` with `ConfigProvider` instead: http://u.ant.design/locale',
     );
+  }
+
+  componentDidMount() {
+    changeConfirmLocale(this.props.locale && this.props.locale.Modal);
   }
 
   componentDidUpdate(prevProps: LocaleProviderProps) {
