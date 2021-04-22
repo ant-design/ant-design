a86e0434c11263b233204b05425fd89ee7bbbf47
afc163 <afc163@gmail.com>
2021-04-23 00:20:48 +0800

    Revert "fix(Badge): update cached isDotRef when isHidden (#30090)"
    This reverts commit d87901e9a58474e6459221efe3e9908d60ad8e91.

--- components/badge/index.tsx
+++ components/badge/index.tsx
@@ -94,8 +94,6 @@ const Badge: CompoundedComponent = ({
   const isDotRef = useRef(showAsDot);
   if (!isHidden) {
     isDotRef.current = showAsDot;
-  } else {
-    isDotRef.current = false;
   }
 
   // =============================== Styles ===============================
@@ -188,8 +186,8 @@ const Badge: CompoundedComponent = ({
 
           const scrollNumberCls = classNames({
             [`${prefixCls}-dot`]: isDot,
-            [`${prefixCls}-count`]: !isDot && !isHidden,
-            [`${prefixCls}-count-sm`]: !isDot && !isHidden && size === 'small',
+            [`${prefixCls}-count`]: !isDot,
+            [`${prefixCls}-count-sm`]: size === 'small',
             [`${prefixCls}-multiple-words`]:
               !isDot && displayCount && displayCount.toString().length > 1,
             [`${prefixCls}-status-${status}`]: !!status,
