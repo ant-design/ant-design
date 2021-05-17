5890df8549f8a60299938796e645633782903e7a
JuFeng Zhang <zjffun@gmail.com>
2021-05-17 20:34:42 +0800

    fix(Collapse): end motion when set activeKey while hiding

--- components/_util/motion.tsx
+++ components/_util/motion.tsx
@@ -1,11 +1,12 @@
 import { CSSMotionProps, MotionEventHandler, MotionEndEventHandler } from 'rc-motion';
+import { MotionEvent } from 'rc-motion/lib/interface';
 
 // ================== Collapse Motion ==================
 const getCollapsedHeight: MotionEventHandler = () => ({ height: 0, opacity: 0 });
 const getRealHeight: MotionEventHandler = node => ({ height: node.scrollHeight, opacity: 1 });
 const getCurrentHeight: MotionEventHandler = node => ({ height: node.offsetHeight });
-const skipOpacityTransition: MotionEndEventHandler = (_, event) =>
-  (event as TransitionEvent).propertyName === 'height';
+const skipOpacityTransition: MotionEndEventHandler = (_, event: MotionEvent) =>
+  event?.deadline === true || (event as TransitionEvent).propertyName === 'height';
 
 const collapseMotion: CSSMotionProps = {
   motionName: 'ant-motion-collapse',
