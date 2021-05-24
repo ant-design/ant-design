47efa3e4a4a2462bee3e00a50a3a53a199fa1401
二货机器人 <smith3816@gmail.com>
2021-05-24 17:36:18 +0800

    fix: Menu SubMenu (#30638)
    * chore: fix level issue
    
    * test: coverage
    
    * chore: clean up
    
    * chore: miss children defition

--- components/menu/SubMenu.tsx
+++ components/menu/SubMenu.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { SubMenu as RcSubMenu } from 'rc-menu';
+import { useFullPath } from 'rc-menu/lib/context/PathContext';
 import classNames from 'classnames';
 import omit from 'rc-util/lib/omit';
-import MenuContext, { MenuContextProps } from './MenuContext';
+import MenuContext from './MenuContext';
 import { isValidElement } from '../_util/reactNode';
 
 interface TitleEventEntity {
@@ -22,28 +23,30 @@ export interface SubMenuProps {
   onTitleMouseLeave?: (e: TitleEventEntity) => void;
   popupOffset?: [number, number];
   popupClassName?: string;
+  children?: React.ReactNode;
 }
 
-class SubMenu extends React.Component<SubMenuProps, any> {
-  static contextType = MenuContext;
+function SubMenu(props: SubMenuProps) {
+  const { popupClassName, icon, title } = props;
+  const context = React.useContext(MenuContext);
+  const { prefixCls, inlineCollapsed, antdMenuTheme } = context;
 
-  context: MenuContextProps;
+  const parentPath = useFullPath();
 
-  renderTitle(inlineCollapsed: boolean) {
-    const { icon, title, level } = this.props;
-    const { prefixCls } = this.context;
+  let titleNode: React.ReactNode;
 
-    if (!icon) {
-      return inlineCollapsed && level === 1 && title && typeof title === 'string' ? (
+  if (!icon) {
+    titleNode =
+      inlineCollapsed && !parentPath.length && title && typeof title === 'string' ? (
         <div className={`${prefixCls}-inline-collapsed-noicon`}>{title.charAt(0)}</div>
       ) : (
         title
       );
-    }
+  } else {
     // inline-collapsed.md demo 依赖 span 来隐藏文字,有 icon 属性，则内部包裹一个 span
     // ref: https://github.com/ant-design/ant-design/pull/23456
     const titleIsSpan = isValidElement(title) && title.type === 'span';
-    return (
+    titleNode = (
       <>
         {icon}
         {titleIsSpan ? title : <span className={`${prefixCls}-title-content`}>{title}</span>}
@@ -51,24 +54,20 @@ class SubMenu extends React.Component<SubMenuProps, any> {
     );
   }
 
-  render() {
-    const { popupClassName } = this.props;
-    const { prefixCls, inlineCollapsed, antdMenuTheme } = this.context;
-    return (
-      <MenuContext.Provider
-        value={{
-          ...this.context,
-          firstLevel: false,
-        }}
-      >
-        <RcSubMenu
-          {...omit(this.props, ['icon'])}
-          title={this.renderTitle(inlineCollapsed)}
-          popupClassName={classNames(prefixCls, `${prefixCls}-${antdMenuTheme}`, popupClassName)}
-        />
-      </MenuContext.Provider>
-    );
-  }
+  return (
+    <MenuContext.Provider
+      value={{
+        ...context,
+        firstLevel: false,
+      }}
+    >
+      <RcSubMenu
+        {...omit(props, ['icon'])}
+        title={titleNode}
+        popupClassName={classNames(prefixCls, `${prefixCls}-${antdMenuTheme}`, popupClassName)}
+      />
+    </MenuContext.Provider>
+  );
 }
 
 export default SubMenu;
