5d8e93b6c1e23076eaf18318b63af2db46056a83
Jehu <z_zhihao@foxmail.com>
2021-07-12 19:57:05 +0800

    fix: add Table.SELECTION_NONE for Russian localization (#31361)

--- components/locale/ru_RU.tsx
+++ components/locale/ru_RU.tsx
@@ -25,6 +25,7 @@ const localeValues: Locale = {
     emptyText: 'Нет данных',
     selectAll: 'Выбрать всё',
     selectInvert: 'Инвертировать выбор',
+    selectNone: 'Очистить все данные',
     selectionAll: 'Выбрать все данные',
     sortTitle: 'Сортировка',
     expand: 'Развернуть строку',
