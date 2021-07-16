7c8a7aa6fa02e7809ea8f37fadb3597158fb119c
austaras <austaras@outlook.com>
2021-07-16 18:31:02 +0800

    feat: InputNumber support addon

--- components/input-number/index.tsx
+++ components/input-number/index.tsx
@@ -6,12 +6,15 @@ import DownOutlined from '@ant-design/icons/DownOutlined';
 
 import { ConfigContext } from '../config-provider';
 import SizeContext, { SizeType } from '../config-provider/SizeContext';
+import { cloneElement } from '../_util/reactNode';
 
 type ValueType = string | number;
 
 export interface InputNumberProps<T extends ValueType = ValueType>
   extends Omit<RcInputNumberProps<T>, 'size'> {
   prefixCls?: string;
+  addonBefore?: React.ReactNode;
+  addonAfter?: React.ReactNode;
   size?: SizeType;
   bordered?: boolean;
 }
@@ -24,6 +27,8 @@ const InputNumber = React.forwardRef<HTMLInputElement, InputNumberProps>((props,
     className,
     size: customizeSize,
     prefixCls: customizePrefixCls,
+    addonBefore,
+    addonAfter,
     bordered = true,
     readOnly,
     ...others
@@ -45,7 +50,7 @@ const InputNumber = React.forwardRef<HTMLInputElement, InputNumberProps>((props,
     className,
   );
 
-  return (
+  const element = (
     <RcInputNumber
       ref={ref}
       className={inputNumberClass}
@@ -56,6 +61,40 @@ const InputNumber = React.forwardRef<HTMLInputElement, InputNumberProps>((props,
       {...others}
     />
   );
+
+  if (addonBefore != null || addonAfter != null) {
+    const wrapperClassName = `${prefixCls}-group`;
+    const addonClassName = `${wrapperClassName}-addon`;
+    const addonBeforeNode = addonBefore ? (
+      <div className={addonClassName}>{addonBefore}</div>
+    ) : null;
+    const addonAfterNode = addonAfter ? <div className={addonClassName}>{addonAfter}</div> : null;
+
+    const mergedWrapperClassName = classNames(`${prefixCls}-wrapper`, wrapperClassName, {
+      [`${wrapperClassName}-rtl`]: direction === 'rtl',
+    });
+
+    const mergedGroupClassName = classNames(
+      `${prefixCls}-group-wrapper`,
+      {
+        [`${prefixCls}-group-wrapper-sm`]: size === 'small',
+        [`${prefixCls}-group-wrapper-lg`]: size === 'large',
+        [`${prefixCls}-group-wrapper-rtl`]: direction === 'rtl',
+      },
+      className,
+    );
+    return (
+      <div className={mergedGroupClassName} style={props.style}>
+        <div className={mergedWrapperClassName}>
+          {addonBeforeNode}
+          {cloneElement(element, { style: null })}
+          {addonAfterNode}
+        </div>
+      </div>
+    );
+  }
+
+  return element;
 });
 
 export default InputNumber as (<T extends ValueType = ValueType>(
