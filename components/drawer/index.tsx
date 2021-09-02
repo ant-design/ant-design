699f3f162c06c4e2082080fa4615c266efccfb13
baxtergu <baxtergu@gmail.com>
2021-09-02 15:21:35 +0800

    feat: DrawerProps interface add autoFocus prop

--- components/drawer/index.tsx
+++ components/drawer/index.tsx
@@ -27,6 +27,7 @@ export interface PushState {
   distance: string | number;
 }
 export interface DrawerProps {
+  autoFocus?: boolean;
   closable?: boolean;
   closeIcon?: React.ReactNode;
   destroyOnClose?: boolean;
