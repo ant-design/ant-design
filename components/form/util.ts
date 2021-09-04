8fca961157a5260f5feeb5fd59fdcd7d1df35ddb
James <daoxingyue@live.cn>
2021-09-04 09:59:15 +0800

    change: add black list for form item name

--- components/form/util.ts
+++ components/form/util.ts
@@ -1,5 +1,12 @@
 import { InternalNamePath } from './interface';
 
+// form item black list.  in forom ,you can use form.id get the form item.
+// use object hasOwnProperty will get better performance if black list is longer.
+const formItemNameBlackList: { [key: string]: number } = { parentNode: 1 };
+
+// default form item id prefix.
+const defaultItemNamePrefixCls: string = 'form_item';
+
 export function toArray<T>(candidate?: T | T[] | false): T[] {
   if (candidate === undefined || candidate === false) return [];
 
@@ -10,5 +17,12 @@ export function getFieldId(namePath: InternalNamePath, formName?: string): strin
   if (!namePath.length) return undefined;
 
   const mergedId = namePath.join('_');
-  return formName ? `${formName}_${mergedId}` : mergedId;
+
+  if (formName) {
+    return `${formName}_${mergedId}`;
+  }
+
+  const isIllegalName = Object.prototype.hasOwnProperty.call(formItemNameBlackList, mergedId);
+
+  return isIllegalName ? `${defaultItemNamePrefixCls}_${mergedId}` : mergedId;
 }
