8397efc5ec4b726a9102045436d5b34507b77d5f
Federica Oltolina <federica.oltolina@gmail.com>
2021-09-16 17:30:24 +0200

    feat: add support to it_IT locale in forms, images and page headers blocks

--- components/locale/it_IT.tsx
+++ components/locale/it_IT.tsx
@@ -4,6 +4,9 @@ import TimePicker from '../time-picker/locale/it_IT';
 import Calendar from '../calendar/locale/it_IT';
 import { Locale } from '../locale-provider';
 
+/* eslint-disable no-template-curly-in-string */
+var typeTemplate = "Il valore del campo ${label} non è valido per il tipo: ${type}";
+
 const localeValues: Locale = {
   locale: 'it',
   Pagination,
@@ -37,6 +40,12 @@ const localeValues: Locale = {
     searchPlaceholder: 'Cerca qui',
     itemUnit: 'elemento',
     itemsUnit: 'elementi',
+    remove: 'Deseleziona',
+    selectCurrent: 'Seleziona la pagina corrente',
+    removeCurrent: 'Deseleziona la pagina corrente',
+    selectAll: 'Seleziona tutto',
+    removeAll: 'Deseleziona tutto',
+    selectInvert: 'Inverti selezione'
   },
   Upload: {
     uploading: 'Caricamento...',
@@ -57,6 +66,62 @@ const localeValues: Locale = {
     copied: 'copia effettuata',
     expand: 'espandi',
   },
+  PageHeader: {
+    back: 'Indietro'
+  },
+  Form: {
+  optional: '(opzionale)',
+  defaultValidateMessages: {
+    "default": 'Errore nel campo ${label}',
+    required: 'Il campo ${label} è obbligatorio',
+    "enum": 'Il valore del campo ${label} deve essere uno dei seguenti: [${enum}]',
+    whitespace: 'Il valore del campo ${label} non può essere vuoto',
+    date: {
+      format: "Il valore del campo ${label} non è nel formato corretto",
+      parse: 'Il valore del campo ${label} non può essere convertito in data',
+      invalid: "Il valore del campo ${label} non è una data valida"
+    },
+    types: {
+      string: typeTemplate,
+      method: typeTemplate,
+      array: typeTemplate,
+      object: typeTemplate,
+      number: typeTemplate,
+      date: typeTemplate,
+      "boolean": typeTemplate,
+      integer: typeTemplate,
+      "float": typeTemplate,
+      regexp: typeTemplate,
+      email: typeTemplate,
+      url: typeTemplate,
+      hex: typeTemplate
+    },
+    string: {
+      len: 'La lunghezza del campo ${label} deve essere di ${len} caratteri',
+      min: 'La lunghezza del campo ${label} deve essere minimo ${min} caratteri',
+      max: 'La lunghezza del campo ${label} deve essere massimo ${max} caratteri',
+      range: 'La lunghezza del campo ${label} deve essere fra ${min} e ${max} caratteri'
+    },
+    number: {
+      len: 'Il valore del campo ${label} deve essere ${len}',
+      min: 'Il valore del campo ${label} deve essere maggiore di ${min}',
+      max: 'Il valore del campo ${label} deve essere minore di ${max}',
+      range: 'Il valore del campo ${label} deve essere compreso fra ${min} e ${max}'
+    },
+    array: {
+      len: 'La lunghezza della lista ${label} deve essere ${len}',
+      min: 'La lunghezza della lista ${label} deve essere almeno ${min}',
+      max: 'La lunghezza della lista ${label} deve essere al massimo ${max}',
+      range: 'La lunghezza della lista ${label} deve essere ${min}-${max}'
+    },
+    pattern: {
+      mismatch: 'Il valore del campo ${label} non corrisponde al formato: ${pattern}'
+    }
+  }
+},
+Image: {
+  preview: 'Anteprima'
+}
 };
 
 export default localeValues;
