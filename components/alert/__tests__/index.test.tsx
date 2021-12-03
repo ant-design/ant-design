9dab76fd1267b37e56374290d426addf972e496b
afc163 <afc163@gmail.com>
2021-12-04 00:28:25 +0800

    test: fix ci warnings

--- components/alert/__tests__/index.test.tsx
+++ components/alert/__tests__/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { mount, render } from 'enzyme';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import Alert from '..';
 import Button from '../../button';
 import Tooltip from '../../tooltip';
@@ -32,14 +33,18 @@ describe('Alert', () => {
         onClose={onClose}
       />,
     );
-    wrapper.find('.ant-alert-close-icon').simulate('click');
-    jest.runAllTimers();
+    act(() => {
+      jest.useFakeTimers();
+      wrapper.find('.ant-alert-close-icon').simulate('click');
+      jest.runAllTimers();
+      jest.useRealTimers();
+    });
     expect(onClose).toHaveBeenCalled();
   });
 
   describe('action of Alert', () => {
     it('custom action', () => {
-      const wrapper = render(
+      const wrapper = mount(
         <Alert
           message="Success Tips"
           type="success"
@@ -131,7 +136,7 @@ describe('Alert', () => {
 
   it('could accept none react element icon', () => {
     const wrapper = mount(<Alert message="Success Tips" type="success" showIcon icon="icon" />);
-    expect(wrapper).toMatchRenderedSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
   it('should not render message div when no message', () => {
--- tests/setupAfterEnv.ts
+++ tests/setupAfterEnv.ts
@@ -1,7 +1,3 @@
 import { toHaveNoViolations } from 'jest-axe';
-import toMatchRenderedSnapshot from './matchers/rendered-snapshot';
 
 expect.extend(toHaveNoViolations);
-expect.extend({
-  toMatchRenderedSnapshot,
-});
