6af0644bf4335c41b126b2d1833c943d8af6bc01
Michael Schramm <michael.schramm@gmail.com>
2021-12-12 13:36:59 +0100

    update card grid prop type to extend div
    fixed #33264

--- components/card/Grid.tsx
+++ components/card/Grid.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { ConfigConsumer, ConfigConsumerProps } from '../config-provider';
 
-export interface CardGridProps {
+export interface CardGridProps extends React.HTMLAttributes<HTMLDivElement> {
   prefixCls?: string;
-  className?: string;
   hoverable?: boolean;
-  style?: React.CSSProperties;
 }
 
 const Grid: React.FC<CardGridProps> = ({ prefixCls, className, hoverable = true, ...props }) => (
