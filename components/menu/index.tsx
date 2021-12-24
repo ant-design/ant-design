f1e0c9c1d21978f2f2bbd698769f3661f73f045a
二货机器人 <smith3816@gmail.com>
2021-12-24 10:57:35 +0800

    chore: Internal Menu interface for ProLayout usage (#33399)
    * test: Update snapshot
    
    * chore: disable tooltip
    
    * chore: rename

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -23,6 +23,13 @@ export type MenuMode = 'vertical' | 'vertical-left' | 'vertical-right' | 'horizo
 export interface MenuProps extends RcMenuProps {
   theme?: MenuTheme;
   inlineIndent?: number;
+
+  // >>>>> Private
+  /**
+   * @private Internal Usage. Not promise crash if used in production. Connect with chenshuai2144
+   *   for removing.
+   */
+  _internalDisableMenuItemTitleTooltip?: boolean;
 }
 
 type InternalMenuProps = MenuProps &
@@ -67,6 +74,7 @@ class InternalMenu extends React.Component<InternalMenuProps> {
       className,
       theme,
       expandIcon,
+      _internalDisableMenuItemTitleTooltip,
       ...restProps
     } = this.props;
 
@@ -83,13 +91,14 @@ class InternalMenu extends React.Component<InternalMenuProps> {
     const menuClassName = classNames(`${prefixCls}-${theme}`, className);
 
     // TODO: refactor menu with function component
-    const contextValue = memoize((cls, collapsed, the, dir) => ({
+    const contextValue = memoize((cls, collapsed, the, dir, disableMenuItemTitleTooltip) => ({
       prefixCls: cls,
       inlineCollapsed: collapsed || false,
       antdMenuTheme: the,
       direction: dir,
       firstLevel: true,
-    }))(prefixCls, inlineCollapsed, theme, direction);
+      disableMenuItemTitleTooltip,
+    }))(prefixCls, inlineCollapsed, theme, direction, _internalDisableMenuItemTitleTooltip);
 
     return (
       <MenuContext.Provider value={contextValue}>
