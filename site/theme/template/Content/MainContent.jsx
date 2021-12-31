8c25dfd9009f96c6dcef3c9feaf14637818f5968
期贤 <qixian.cs@antgroup.com>
2021-12-31 14:26:26 +0800

    merge

--- site/theme/template/Content/MainContent.jsx
+++ site/theme/template/Content/MainContent.jsx
@@ -373,6 +373,7 @@ class MainContent extends Component {
   renderMainContent({ theme, setIframeTheme }) {
     const { localizedPageData, demos, location } = this.props;
     if (location.pathname.includes('components/overview')) {
+      const type = utils.isZhCN(location.pathname) ? '重型组件' : 'ProComponents';
       return (
         <ComponentOverview
           {...this.props}
@@ -383,41 +384,48 @@ class MainContent extends Component {
               {
                 meta: {
                   category: 'Components',
-                  cover: 'https://gw.alipayobjects.com/zos/alicdn/tX6-md4H6/Affix.svg',
-                  filename: 'https://procomponents.ant.design/components/layout/',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/4n5H%24UX%24j/bianzu%2525204.svg',
+                  filename: 'https://procomponents.ant.design/components/layout',
                   subtitle: '高级布局',
                   title: 'ProLayout',
-                  type: '重型组件',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
                 },
               },
               {
                 meta: {
                   category: 'Components',
-                  cover: 'https://gw.alipayobjects.com/zos/alicdn/tX6-md4H6/Affix.svg',
-                  filename: 'https://procomponents.ant.design/components/layout/',
+                  cover: 'https://gw.alipayobjects.com/zos/antfincdn/mStei5BFC/bianzu%2525207.svg',
+                  filename: 'https://procomponents.ant.design/components/form',
                   subtitle: '高级表单',
                   title: 'ProForm',
-                  type: '重型组件',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
                 },
               },
               {
                 meta: {
                   category: 'Components',
-                  cover: 'https://gw.alipayobjects.com/zos/alicdn/tX6-md4H6/Affix.svg',
-                  filename: 'https://procomponents.ant.design/components/layout/',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/AwU0Cv%26Ju/bianzu%2525208.svg',
+                  filename: 'https://procomponents.ant.design/components/table',
                   subtitle: '高级表格',
                   title: 'ProTable',
-                  type: '重型组件',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
                 },
               },
               {
                 meta: {
                   category: 'Components',
-                  cover: 'https://gw.alipayobjects.com/zos/alicdn/tX6-md4H6/Affix.svg',
-                  filename: 'https://procomponents.ant.design/components/layout/',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/H0%26LSYYfh/bianzu%2525209.svg',
+                  filename: 'https://procomponents.ant.design/components/descriptions',
                   subtitle: '高级定义列表',
                   title: 'ProDescriptions',
-                  type: '重型组件',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
                 },
               },
             ])}
