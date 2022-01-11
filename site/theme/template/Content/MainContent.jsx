e84fba5ae165bae2611caf302e6133dc404bfd94
陈帅 <qixian.cs@outlook.com>
2022-01-11 10:51:45 +0800

    docs: add ProComponents cards (#33647)
    * merge
    
    * merge
    
    * docs: add ProComponents cards
    
    Co-authored-by: 期贤 <qixian.cs@antgroup.com>

--- site/theme/template/Content/MainContent.jsx
+++ site/theme/template/Content/MainContent.jsx
@@ -373,13 +373,84 @@ class MainContent extends Component {
   renderMainContent({ theme, setIframeTheme }) {
     const { localizedPageData, demos, location } = this.props;
     if (location.pathname.includes('components/overview')) {
+      const type = utils.isZhCN(location.pathname) ? '重型组件' : 'ProComponents';
       return (
         <ComponentOverview
           {...this.props}
           doc={localizedPageData}
-          componentsData={getModuleData(this.props).filter(
-            ({ meta }) => meta.category === 'Components',
-          )}
+          componentsData={getModuleData(this.props)
+            .filter(({ meta }) => meta.category === 'Components')
+            .concat([
+              {
+                meta: {
+                  category: 'Components',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/4n5H%24UX%24j/bianzu%2525204.svg',
+                  filename: 'https://procomponents.ant.design/components/layout',
+                  subtitle: '高级布局',
+                  title: 'ProLayout',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+              {
+                meta: {
+                  category: 'Components',
+                  cover: 'https://gw.alipayobjects.com/zos/antfincdn/mStei5BFC/bianzu%2525207.svg',
+                  filename: 'https://procomponents.ant.design/components/form',
+                  subtitle: '高级表单',
+                  title: 'ProForm',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+              {
+                meta: {
+                  category: 'Components',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/AwU0Cv%26Ju/bianzu%2525208.svg',
+                  filename: 'https://procomponents.ant.design/components/table',
+                  subtitle: '高级表格',
+                  title: 'ProTable',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+              {
+                meta: {
+                  category: 'Components',
+                  cover:
+                    'https://gw.alipayobjects.com/zos/antfincdn/H0%26LSYYfh/bianzu%2525209.svg',
+                  filename: 'https://procomponents.ant.design/components/descriptions',
+                  subtitle: '高级定义列表',
+                  title: 'ProDescriptions',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+              {
+                meta: {
+                  category: 'Components',
+                  cover: 'https://gw.alipayobjects.com/zos/antfincdn/uZUmLtne5/bianzu%2525209.svg',
+                  filename: 'https://procomponents.ant.design/components/list',
+                  subtitle: '高级列表',
+                  title: 'ProList',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+              {
+                meta: {
+                  category: 'Components',
+                  cover: 'https://gw.alipayobjects.com/zos/antfincdn/N3eU432oA/bianzu%2525209.svg',
+                  filename: 'https://procomponents.ant.design/components/list',
+                  subtitle: '可编辑表格',
+                  title: 'EditableProTable',
+                  type,
+                  tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+                },
+              },
+            ])}
         />
       );
     }
