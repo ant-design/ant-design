54381397b2a6424845c00ccf22b461688c19115d
zombiej <smith3816@gmail.com>
2022-01-14 14:23:06 +0800

    chore: clean up

--- components/typography/__tests__/ellipsis.test.js
+++ components/typography/__tests__/ellipsis.test.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { SmileOutlined, LikeOutlined, HighlightOutlined, CheckOutlined } from '@ant-design/icons';
-import KeyCode from 'rc-util/lib/KeyCode';
-import { resetWarned } from 'rc-util/lib/warning';
-import { spyElementPrototype, spyElementPrototypes } from 'rc-util/lib/test/domHook';
-import copy from 'copy-to-clipboard';
-import Title from '../Title';
-import Link from '../Link';
-import Paragraph from '../Paragraph';
-import Text from '../Text';
 import Base from '../Base';
-import mountTest from '../../../tests/shared/mountTest';
-import rtlTest from '../../../tests/shared/rtlTest';
-import Typography from '../Typography';
 import { sleep } from '../../../tests/utils';
 
 jest.mock('copy-to-clipboard');
