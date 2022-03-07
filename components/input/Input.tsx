77960c3f756d4e4607c42b1586b44bb0b136d5ce
MadCcc <1075746765@qq.com>
2022-03-07 14:02:37 +0800

    feat: Input support clearIcon (#34325)
    * feat: Input support clearIcon
    
    * docs: update Input api

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -113,7 +113,7 @@ export function triggerFocus(
 export interface InputProps
   extends Omit<
     RcInputProps,
-    'wrapperClassName' | 'groupClassName' | 'inputClassName' | 'affixWrapperClassName' | 'clearIcon'
+    'wrapperClassName' | 'groupClassName' | 'inputClassName' | 'affixWrapperClassName'
   > {
   size?: SizeType;
   status?: InputStatus;
@@ -129,6 +129,7 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
     onBlur,
     onFocus,
     suffix,
+    clearIcon,
     ...rest
   } = props;
   const { getPrefixCls, direction, input } = React.useContext(ConfigContext);
@@ -207,7 +208,7 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
       onBlur={handleBlur}
       onFocus={handleFocus}
       suffix={suffixNode}
-      clearIcon={<CloseCircleFilled />}
+      clearIcon={clearIcon || <CloseCircleFilled />}
       inputClassName={classNames(
         !withPrefixSuffix && {
           [`${prefixCls}-sm`]: mergedSize === 'small',
--- components/input/index.zh-CN.md
+++ components/input/index.zh-CN.md
@@ -23,6 +23,7 @@ cover: https://gw.alipayobjects.com/zos/alicdn/xS9YEJhfe/Input.svg
 | addonBefore | 带标签的 input，设置前置标签 | ReactNode | - |  |
 | allowClear | 可以点击清除图标删除内容 | boolean | - |  |
 | bordered | 是否有边框 | boolean | true | 4.5.0 |
+| clearIcon | 清除按钮，与 `allowClear` 一同使用 | ReactNode | <CloseCircleFilled /> | 4.19.0 |
 | defaultValue | 输入框默认内容 | string | - |  |
 | disabled | 是否禁用状态，默认为 false | boolean | false |  |
 | id | 输入框的 id | string | - |  |
