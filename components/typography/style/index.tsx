deae9d126c9cca2bb3560a3ae540b97d3f398094
afc163 <afc163@gmail.com>
2022-03-08 19:58:09 +0800

    add some style

--- components/typography/style/index.tsx
+++ components/typography/style/index.tsx
@@ -3,9 +3,37 @@ import { CSSInterpolation, CSSObject } from '@ant-design/cssinjs';
 import { TinyColor } from '@ctrl/tinycolor';
 import { DerivativeToken, useStyleRegister, useToken, withPrefix } from '../../_util/theme';
 
+const genTypographyStyle = ({
+  prefixCls,
+  token,
+}: {
+  prefixCls: string;
+  token: DerivativeToken;
+}): CSSObject => ({
+  color: token.textColor,
+  overflowWrap: 'break-word',
+
+  [`&.${prefixCls}-secondary`]: {
+    color: token.textColorSecondary,
+  },
+
+  [`&.${prefixCls}-success`]: {
+    color: token.successColor,
+  },
+
+  [`&.${prefixCls}-warning`]: {
+    color: token.warningColor,
+  },
+});
+
 // ============================== Export ==============================
 export default function useStyle(prefixCls: string) {
   const [theme, token, hashId] = useToken();
 
-  return useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => []);
+  return [
+    useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => [
+      withPrefix(genTypographyStyle({ prefixCls, token }), prefixCls),
+    ]),
+    hashId,
+  ];
 }
