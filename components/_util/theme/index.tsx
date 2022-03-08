2755cfdd128a812f08b60e6794fca1c3cc10cc8d
afc163 <afc163@gmail.com>
2022-03-08 21:52:54 +0800

    update code style

--- components/_util/theme/index.tsx
+++ components/_util/theme/index.tsx
@@ -63,6 +63,8 @@ export interface DesignToken {
   textColorInverse: string;
   placeholderColor: string;
 
+  disabledColor: string;
+
   iconColorHover: string;
 
   headingColor: string;
--- components/typography/style/index.tsx
+++ components/typography/style/index.tsx
@@ -1,6 +1,7 @@
 // deps-lint-skip-all
 import { CSSInterpolation, CSSObject } from '@ant-design/cssinjs';
 import { TinyColor } from '@ctrl/tinycolor';
+import { generate } from '@ant-design/colors';
 import { DerivativeToken, useStyleRegister, useToken, withPrefix } from '../../_util/theme';
 
 const genTypographyStyle = ({
@@ -9,22 +10,43 @@ const genTypographyStyle = ({
 }: {
   prefixCls: string;
   token: DerivativeToken;
-}): CSSObject => ({
-  color: token.textColor,
-  overflowWrap: 'break-word',
+}): CSSObject => {
+  const errorColors = generate(token.errorColor);
+  return {
+    [`.${prefixCls}`]: {
+      color: token.textColor,
+      overflowWrap: 'break-word',
+      '&&-secondary': {
+        color: token.textColorSecondary,
+      },
 
-  [`&.${prefixCls}-secondary`]: {
-    color: token.textColorSecondary,
-  },
+      '&&-success': {
+        color: token.successColor,
+      },
 
-  [`&.${prefixCls}-success`]: {
-    color: token.successColor,
-  },
+      '&&-warning': {
+        color: token.warningColor,
+      },
 
-  [`&.${prefixCls}-warning`]: {
-    color: token.warningColor,
-  },
-});
+      '&&-danger': {
+        color: token.errorColor,
+        'a&:active, a&:focus, a&:hover': {
+          color: errorColors[4],
+        },
+      },
+
+      '&&-disabled': {
+        color: token.disabledColor,
+        cursor: 'not-allowed',
+        userSelect: 'none',
+      },
+
+      'div, p': {
+        marginBottom: '1em',
+      },
+    },
+  };
+};
 
 // ============================== Export ==============================
 export default function useStyle(prefixCls: string) {
@@ -32,7 +54,7 @@ export default function useStyle(prefixCls: string) {
 
   return [
     useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => [
-      withPrefix(genTypographyStyle({ prefixCls, token }), prefixCls),
+      genTypographyStyle({ prefixCls, token }),
     ]),
     hashId,
   ];
