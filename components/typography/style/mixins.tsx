db1422b9efdf8dda7ec76a51f97b28c0c12f28d8
afc163 <afc163@gmail.com>
2022-03-09 19:59:11 +0800

    add reset styles

---
+++ components/typography/style/mixins.tsx
@@ -0,0 +1,155 @@
+/*
+.typography-title(@fontSize; @fontWeight; @lineHeight; @headingColor; @headingMarginBottom;) {
+  margin-bottom: @headingMarginBottom;
+  color: @headingColor;
+  font-weight: @fontWeight;
+  fontSize: @fontSize;
+  line-height: @lineHeight;
+}
+*/
+import { gold } from '@ant-design/colors';
+import type { CSSObject } from '@ant-design/cssinjs';
+import type { DerivativeToken } from '../../_util/theme';
+
+// Typography
+// ---
+/*
+@typography-title-font-weight: 600;
+@typography-title-margin-top: 1.2em;
+@typography-title-margin-bottom: 0.5em;
+*/
+const typographyToken = {
+  titleFontWeight: 600,
+  titleMarginBottom: '0.5em',
+};
+
+// eslint-disable-next-line import/prefer-default-export
+const getTitleStyle = ({
+  fontSize,
+  lineHeight,
+  color,
+}: {
+  fontSize: number;
+  lineHeight: number;
+  color: string;
+}) => ({
+  marginBottom: typographyToken.titleMarginBottom,
+  color,
+  fontWeight: typographyToken.titleFontWeight,
+  fontSize,
+  lineHeight,
+});
+
+// eslint-disable-next-line import/prefer-default-export
+export const getTitleStyles = (token: DerivativeToken) => {
+  const lineHeights = [1.23, 1.35, 1.35, 1.4, 1.5];
+  const styles = {} as CSSObject;
+  lineHeights.forEach((lineHeight, i) => {
+    styles[
+      `
+      h${i + 1}&,
+      div&-h${i + 1},
+      div&-h${i + 1} > textarea,
+      h${i + 1}
+    `
+    ] = getTitleStyle({
+      fontSize: (token as any)[`heading${i + 1}Size`],
+      lineHeight,
+      color: token.headingColor,
+    });
+  });
+  return styles;
+};
+
+export const getResetStyles = (): CSSObject => ({
+  code: {
+    margin: '0 0.2em',
+    padding: '0.2em 0.4em 0.1em',
+    fontSize: '85%',
+    background: 'rgba(150, 150, 150, 0.1)',
+    border: '1px solid rgba(100, 100, 100, 0.2)',
+    borderRadius: 3,
+  },
+
+  kbd: {
+    margin: '0 0.2em',
+    padding: '0.15em 0.4em 0.1em',
+    fontSize: '90%',
+    background: 'rgba(150, 150, 150, 0.06)',
+    border: '1px solid rgba(100, 100, 100, 0.2)',
+    borderBottomWidth: 2,
+    borderRadius: 3,
+  },
+
+  mark: {
+    padding: 0,
+    backgroundColor: gold[2],
+  },
+
+  'u, ins': {
+    textDecoration: 'underline',
+    textDecorationSkipInk: 'auto',
+  },
+
+  's, del': {
+    textDecoration: 'line-through',
+  },
+
+  strong: {
+    fontWeight: 600,
+  },
+
+  // list
+  'ul, ol': {
+    margin: '0 0 1em',
+    padding: 0,
+
+    li: {
+      margin: '0 0 0 20px',
+      padding: '0 0 0 4px',
+    },
+  },
+
+  ul: {
+    listStyleType: 'circle',
+
+    ul: {
+      listStyleType: 'disc',
+    },
+  },
+
+  ol: {
+    listStyleType: 'decimal',
+  },
+
+  // pre & block
+  'pre, blockquote': {
+    margin: '1em 0',
+  },
+
+  pre: {
+    padding: '0.4em 0.6em',
+    whiteSpace: 'pre-wrap',
+    wordWrap: 'break-word',
+    background: 'rgba(150, 150, 150, 0.1)',
+    border: '1px solid rgba(100, 100, 100, 0.2)',
+    borderRadius: '3px',
+
+    // Compatible for marked
+    code: {
+      display: 'inline',
+      margin: 0,
+      padding: 0,
+      fontSize: 'inherit',
+      fontFamily: 'inherit',
+      background: 'transparent',
+      border: 0,
+    },
+  },
+
+  blockquote: {
+    padding: '0 0 0 0.6em',
+    borderLeft: '4px solid rgba(100, 100, 100, 0.2)',
+    opacity: '0.85',
+  },
+});
