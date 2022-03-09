6c9cbe0bb2733b89528e50fe7d38a0d616cef609
PeachScript <scdzwyxst@gmail.com>
2022-03-09 23:24:01 +0800

    refactor: better cssinjs practice for Rate

--- components/rate/style/index.tsx
+++ components/rate/style/index.tsx
@@ -1,129 +1,129 @@
 // deps-lint-skip-all
-import { CSSObject } from '@ant-design/cssinjs';
+import { CSSInterpolation, CSSObject } from '@ant-design/cssinjs';
 import {
   DerivativeToken,
   resetComponent,
   UseComponentStyleResult,
   useStyleRegister,
   useToken,
-  withPrefix,
 } from '../../_util/theme';
 
 interface RateToken extends DerivativeToken {
   rateStarColor: string;
   rateStarSize: number;
   rateStarHoverScale: CSSObject['transform'];
-}
-
-interface GenRateArgs {
-  prefixCls: string;
+  rateCls: string;
   iconPrefixCls: string;
-  token: RateToken;
 }
 
-const genRateStarStyle = ({ prefixCls, iconPrefixCls, token }: GenRateArgs): CSSObject => ({
-  [`.${prefixCls}-star`]: {
-    position: 'relative',
-    display: 'inline-block',
-    color: 'inherit',
-    cursor: 'pointer',
+const genRateStarStyle = (token: RateToken): CSSObject => {
+  const { rateCls } = token;
 
-    '&:not(:last-child)': {
-      marginInlineEnd: 8,
-    },
-
-    '> div': {
-      transition: 'all 0.3s, outline 0s',
-
-      '&:hover': {
-        transform: token.rateStarHoverScale,
-      },
-
-      '&:focus': {
-        outline: 0,
-      },
-
-      '&:focus-visible': {
-        outline: `1px dashed ${token.borderColorSplit}`,
-        transform: token.rateStarHoverScale,
-      },
-    },
-
-    '&-first, &-second': {
-      color: token.borderColorSplit,
-      transition: 'all 0.3s',
-      userSelect: 'none',
-
-      [`.${iconPrefixCls}`]: {
-        verticalAlign: 'middle',
-      },
-    },
-
-    '&-first': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '50%',
-      height: '100%',
-      overflow: 'hidden',
-      opacity: 0,
-    },
-
-    [`&-half .${prefixCls}-star-first, &-half .${prefixCls}-star-second`]: {
-      opacity: 1,
-    },
-
-    [`&-half .${prefixCls}-star-first, &-full .${prefixCls}-star-second`]: {
+  return {
+    [`${rateCls}-star`]: {
+      position: 'relative',
+      display: 'inline-block',
       color: 'inherit',
-    },
-  },
-});
+      cursor: 'pointer',
 
-const genRateRtlStyle = (prefixCls: string): CSSObject => ({
-  [`&-rtl.${prefixCls}`]: {
+      '&:not(:last-child)': {
+        marginInlineEnd: token.marginXS,
+      },
+
+      '> div': {
+        transition: `all ${token.duration}, outline 0s`,
+
+        '&:hover': {
+          transform: token.rateStarHoverScale,
+        },
+
+        '&:focus': {
+          outline: 0,
+        },
+
+        '&:focus-visible': {
+          outline: `1px dashed ${token.borderColorSplit}`,
+          transform: token.rateStarHoverScale,
+        },
+      },
+
+      '&-first, &-second': {
+        color: token.borderColorSplit,
+        transition: `all ${token.duration}`,
+        userSelect: 'none',
+
+        [token.iconPrefixCls]: {
+          verticalAlign: 'middle',
+        },
+      },
+
+      '&-first': {
+        position: 'absolute',
+        top: 0,
+        insetInlineStart: 0,
+        width: '50%',
+        height: '100%',
+        overflow: 'hidden',
+        opacity: 0,
+      },
+
+      [`&-half ${rateCls}-star-first, &-half ${rateCls}-star-second`]: {
+        opacity: 1,
+      },
+
+      [`&-half ${rateCls}-star-first, &-full ${rateCls}-star-second`]: {
+        color: 'inherit',
+      },
+    },
+  };
+};
+
+const genRateRtlStyle = (token: RateToken): CSSObject => ({
+  [`&-rtl${token.rateCls}`]: {
     direction: 'rtl',
-
-    [`.${prefixCls}-star-first`]: {
-      right: 0,
-      left: 'auto',
-    },
   },
 });
 
-const genRateStyle = ({ prefixCls, iconPrefixCls, token }: GenRateArgs): CSSObject => ({
-  ...resetComponent(token),
+const genRateStyle = (token: RateToken): CSSInterpolation => {
+  const { rateCls } = token;
 
-  display: 'inline-block',
-  margin: 0,
-  padding: 0,
-  color: token.rateStarColor,
-  fontSize: token.rateStarSize,
-  lineHeight: 'unset',
-  listStyle: 'none',
-  outline: 'none',
+  return {
+    [rateCls]: {
+      ...resetComponent(token),
 
-  // disable styles
-  [`&-disabled.${prefixCls} .${prefixCls}-star`]: {
-    cursor: 'default',
+      display: 'inline-block',
+      margin: 0,
+      padding: 0,
+      color: token.rateStarColor,
+      fontSize: token.rateStarSize,
+      lineHeight: 'unset',
+      listStyle: 'none',
+      outline: 'none',
 
-    '&:hover': {
-      transform: 'scale(1)',
+      // disable styles
+      [`&-disabled${rateCls} ${rateCls}-star`]: {
+        cursor: 'default',
+
+        '&:hover': {
+          transform: 'scale(1)',
+        },
+      },
+
+      // star styles
+      ...genRateStarStyle(token),
+
+      // text styles
+      [`+ ${rateCls}-text`]: {
+        display: 'inline-block',
+        marginInlineStart: token.marginXS,
+        fontSize: token.fontSize,
+      },
+
+      // rtl styles
+      ...genRateRtlStyle(token),
     },
-  },
-
-  // star styles
-  ...genRateStarStyle({ prefixCls, iconPrefixCls, token }),
-
-  // text styles
-  [`+ .${prefixCls}-text`]: {
-    display: 'inline-block',
-    marginInlineStart: 8,
-    fontSize: token.fontSize,
-  },
-
-  // rtl styles
-  ...genRateRtlStyle(prefixCls),
-});
+  };
+};
 
 // ============================== Export ==============================
 export default function useStyle(
@@ -138,12 +138,12 @@ export default function useStyle(
     rateStarColor: '#fadb14', // @yellow-6
     rateStarSize: 20, // fixed-value
     rateStarHoverScale: 'scale(1.1)', // fixed-value
+    rateCls: `.${prefixCls}`,
+    iconPrefixCls: `.${iconPrefixCls}`,
   };
 
   return [
-    useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => [
-      withPrefix(genRateStyle({ prefixCls, iconPrefixCls, token: rateToken }), prefixCls),
-    ]),
+    useStyleRegister({ theme, token, hashId, path: [prefixCls] }, () => [genRateStyle(rateToken)]),
     hashId,
   ];
 }
