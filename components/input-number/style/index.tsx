c8516555e7872810d6c1e20690c291a0060e58f6
MadCcc <1075746765@qq.com>
2022-03-11 11:34:24 +0800

    chore: code style

--- components/input-number/style/index.tsx
+++ components/input-number/style/index.tsx
@@ -20,322 +20,353 @@ import {
 } from '../../input/style';
 
 interface InputNumberToken extends InputToken {
+  prefixCls: string;
   inputNumberCls: string;
   inputNumberHandlerActiveBgColor: string;
 }
 
-const genInputNumberStyles = (token: InputNumberToken): CSSObject => ({
-  [`.${token.inputNumberCls}`]: {
-    ...resetComponent(token),
-    ...genBasicInputStyle(token.inputNumberCls, token),
-    ...genStatusStyle(token.inputNumberCls, token),
+const genInputNumberStyles = (token: InputNumberToken): CSSObject => {
+  const {
+    inputNumberCls,
+    borderWidth,
+    borderStyle,
+    borderColor,
+    borderRadius,
+    fontSizeLG,
+    controlHeightLG,
+    controlHeightSM,
+    errorColor,
+    paddingXS,
+    textColorSecondary,
+    durationFast,
+    inputNumberHandlerActiveBgColor,
+    primaryColor,
+    marginXXS,
+    controlHeight,
+    inputPaddingHorizontal,
+    duration,
+    componentBackground,
+    durationMid,
+    textColorDisabled,
+    prefixCls,
+  } = token;
 
-    display: 'inline-block',
-    width: 90, // FIXME: magic number
-    margin: 0,
-    padding: 0,
-    border: `${token.borderWidth}px ${token.borderStyle} ${token.borderColor}`,
-    borderRadius: token.borderRadius,
+  return {
+    [inputNumberCls]: {
+      ...resetComponent(token),
+      ...genBasicInputStyle(prefixCls, token),
+      ...genStatusStyle(prefixCls, token),
 
-    '&-rtl': {
-      direction: 'rtl',
+      display: 'inline-block',
+      width: 90, // FIXME: magic number
+      margin: 0,
+      padding: 0,
+      border: `${borderWidth}px ${borderStyle} ${borderColor}`,
+      borderRadius,
 
-      [`.${token.inputNumberCls}-input`]: {
+      '&-rtl': {
         direction: 'rtl',
-      },
-    },
 
-    [`&:hover .${token.inputNumberCls}-handler-wrap, &-focused .${token.inputNumberCls}-handler-wrap`]:
-      {
+        [`${inputNumberCls}-input`]: {
+          direction: 'rtl',
+        },
+      },
+
+      [`&:hover ${inputNumberCls}-handler-wrap, &-focused ${inputNumberCls}-handler-wrap`]: {
         opacity: 1,
       },
 
-    '&-lg': {
-      padding: 0,
-      fontSize: token.fontSizeLG,
+      '&-lg': {
+        padding: 0,
+        fontSize: fontSizeLG,
 
-      [`input.${token.inputNumberCls}-input`]: {
-        height: token.controlHeightLG - 2 * token.borderWidth,
+        [`input${inputNumberCls}-input`]: {
+          height: controlHeightLG - 2 * borderWidth,
+        },
       },
-    },
 
-    '&-sm': {
-      padding: 0,
+      '&-sm': {
+        padding: 0,
 
-      [`input.${token.inputNumberCls}-input`]: {
-        height: token.controlHeightSM - 2 * token.borderWidth,
-        padding: `0 ${token.paddingXS - token.borderWidth}px`,
+        [`input${inputNumberCls}-input`]: {
+          height: controlHeightSM - 2 * borderWidth,
+          padding: `0 ${paddingXS - borderWidth}px`,
+        },
       },
-    },
 
-    '&:hover': {
-      ...genHoverStyle(token),
-    },
-
-    '&-focused': {
-      ...genActiveStyle(token),
-    },
-
-    '&-disabled': {
-      ...genDisabledStyle(token),
-      [`.${token.inputNumberCls}-input`]: {
-        cursor: 'not-allowed',
+      '&:hover': {
+        ...genHoverStyle(token),
       },
-      [`.${token.inputNumberCls}-handler-wrap`]: {
-        display: 'none',
+
+      '&-focused': {
+        ...genActiveStyle(token),
       },
-    },
 
-    '&-readonly': {
-      [`.${token.inputNumberCls}-handler-wrap`]: {
-        display: 'none',
+      '&-disabled': {
+        ...genDisabledStyle(token),
+        [`${inputNumberCls}-input`]: {
+          cursor: 'not-allowed',
+        },
+        [`${inputNumberCls}-handler-wrap`]: {
+          display: 'none',
+        },
       },
-    },
 
-    '&-borderless': {
-      boxShadow: 'none',
-    },
-
-    // ===================== Out Of Range =====================
-    '&-out-of-range': {
-      input: {
-        color: token.errorColor,
+      '&-readonly': {
+        [`${inputNumberCls}-handler-wrap`]: {
+          display: 'none',
+        },
       },
-    },
 
-    // Style for input-group: input with label, with button or dropdown...
-    '&-group': {
-      ...resetComponent(token),
-      ...genInputGroupStyle(token.inputNumberCls, token),
+      '&-borderless': {
+        boxShadow: 'none',
+      },
 
-      '&-wrapper': {
-        display: 'inline-block',
-        textAlign: 'start',
-        verticalAlign: 'top', // https://github.com/ant-design/ant-design/issues/6403
+      // ===================== Out Of Range =====================
+      '&-out-of-range': {
+        input: {
+          color: errorColor,
+        },
+      },
 
-        [`.${token.inputNumberCls}-affix-wrapper`]: {
+      // Style for input-group: input with label, with button or dropdown...
+      '&-group': {
+        ...resetComponent(token),
+        ...genInputGroupStyle(prefixCls, token),
+
+        '&-wrapper': {
+          display: 'inline-block',
+          textAlign: 'start',
+          verticalAlign: 'top', // https://github.com/ant-design/ant-design/issues/6403
+
+          [`${inputNumberCls}-affix-wrapper`]: {
+            width: '100%',
+          },
+        },
+      },
+
+      [inputNumberCls]: {
+        '&-handler': {
+          position: 'relative',
+          display: 'block',
           width: '100%',
-        },
-      },
-    },
+          height: '50%',
+          overflow: 'hidden',
+          color: textColorSecondary,
+          fontWeight: 'bold',
+          lineHeight: 0,
+          textAlign: 'center',
+          borderInlineStart: `${borderWidth}px ${borderStyle} ${borderColor}`,
+          transition: `all ${durationFast} linear`,
 
-    [`.${token.inputNumberCls}`]: {
-      '&-handler': {
-        position: 'relative',
-        display: 'block',
-        width: '100%',
-        height: '50%',
-        overflow: 'hidden',
-        color: token.textColorSecondary,
-        fontWeight: 'bold',
-        lineHeight: 0,
-        textAlign: 'center',
-        borderInlineStart: `${token.borderWidth}px ${token.borderStyle} ${token.borderColor}`,
-        transition: `all ${token.durationFast} linear`,
-
-        '&:active': {
-          background: token.inputNumberHandlerActiveBgColor,
-        },
-
-        '&:hover &-up-inner, &:hover &-down-inner': {
-          color: token.primaryColor,
-        },
-      },
-
-      '&-handler-up-inner, &-handler-down-inner': {
-        ...resetIcon(),
-
-        position: 'absolute',
-        insetInlineEnd: token.marginXXS,
-        width: token.controlHeightSM / 2,
-        height: token.controlHeightSM / 2,
-        color: token.textColorSecondary,
-        lineHeight: token.controlHeightSM / 2,
-        transition: `all ${token.durationFast} linear`,
-        userSelect: 'none',
-      },
-
-      '&-input': {
-        width: '100%',
-        height: token.controlHeight - 2 * token.borderWidth,
-        padding: `0 ${token.inputPaddingHorizontal - token.borderWidth}px`,
-        textAlign: 'start',
-        backgroundColor: 'transparent',
-        border: 0,
-        borderRadius: token.borderRadius,
-        outline: 0,
-        transition: `all ${token.duration} linear`,
-        appearance: 'textfield', // FIXME: important
-        ...genPlaceholderStyle(),
-
-        '&[type="number"]::-webkit-inner-spin-button, &[type="number"]::-webkit-outer-spin-button':
-          {
-            margin: 0,
-            /* stylelint-disable-next-line property-no-vendor-prefix */
-            webkitAppearance: 'none',
-            appearance: 'none',
+          '&:active': {
+            background: inputNumberHandlerActiveBgColor,
           },
-      },
 
-      '&-handler-wrap': {
-        position: 'absolute',
-        insetBlockStart: 0,
-        insetInlineEnd: 0,
-        width: 22, // FIXME: magic number
-        height: '100%',
-        background: token.componentBackground,
-        borderStartStartRadius: 0,
-        borderStartEndRadius: token.borderRadius,
-        borderEndEndRadius: token.borderRadius,
-        borderEndStartRadius: 0,
-        opacity: 0,
-        transition: `opacity ${token.durationMid} linear ${token.durationFast}`,
-
-        // Fix input number inside Menu makes icon too large
-        // We arise the selector priority by nest selector here
-        // https://github.com/ant-design/ant-design/issues/14367
-        [`.${token.inputNumberCls}-handler`]: {
-          [`.${token.inputNumberCls}-handler-up-inner,
-          .${token.inputNumberCls}-handler-down-inner`]: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minWidth: 'auto',
-            marginRight: 0,
-            fontSize: '7px', // FIXME: magic
+          '&:hover &-up-inner, &:hover &-down-inner': {
+            color: primaryColor,
           },
         },
 
-        [`.${token.inputNumberCls}-borderless &`]: {
-          borderInlineStartWidth: 0,
+        '&-handler-up-inner, &-handler-down-inner': {
+          ...resetIcon(),
+
+          position: 'absolute',
+          insetInlineEnd: marginXXS,
+          width: controlHeightSM / 2,
+          height: controlHeightSM / 2,
+          color: textColorSecondary,
+          lineHeight: controlHeightSM / 2,
+          transition: `all ${durationFast} linear`,
+          userSelect: 'none',
         },
 
-        [`&:hover .${token.inputNumberCls}-handler`]: {
-          height: '40%',
-        },
-      },
+        '&-input': {
+          width: '100%',
+          height: controlHeight - 2 * borderWidth,
+          padding: `0 ${inputPaddingHorizontal - borderWidth}px`,
+          textAlign: 'start',
+          backgroundColor: 'transparent',
+          border: 0,
+          borderRadius,
+          outline: 0,
+          transition: `all ${duration} linear`,
+          appearance: 'textfield', // FIXME: important
+          ...genPlaceholderStyle(),
 
-      '&-handler-up': {
-        borderStartEndRadius: token.borderRadius,
-        cursor: 'pointer',
-
-        '&-inner': {
-          top: '50%',
-          marginTop: -5, // FIXME: magic
-          textAlign: 'center',
-        },
-
-        '&:hover': {
-          height: '60% !important',
-        },
-      },
-
-      '&-handler-down': {
-        top: 0,
-        borderBlockStart: `${token.borderWidth}px ${token.borderStyle} ${token.borderColor}`,
-        borderEndEndRadius: token.borderRadius,
-        cursor: 'pointer',
-
-        '&-inner': {
-          top: '50%',
-          textAlign: 'center',
-          transform: 'translateY(-50%)',
+          '&[type="number"]::-webkit-inner-spin-button, &[type="number"]::-webkit-outer-spin-button':
+            {
+              margin: 0,
+              /* stylelint-disable-next-line property-no-vendor-prefix */
+              webkitAppearance: 'none',
+              appearance: 'none',
+            },
         },
 
-        '&:hover': {
-          height: '60% !important',
+        '&-handler-wrap': {
+          position: 'absolute',
+          insetBlockStart: 0,
+          insetInlineEnd: 0,
+          width: 22, // FIXME: magic number
+          height: '100%',
+          background: componentBackground,
+          borderStartStartRadius: 0,
+          borderStartEndRadius: borderRadius,
+          borderEndEndRadius: borderRadius,
+          borderEndStartRadius: 0,
+          opacity: 0,
+          transition: `opacity ${durationMid} linear ${durationFast}`,
+
+          // Fix input number inside Menu makes icon too large
+          // We arise the selector priority by nest selector here
+          // https://github.com/ant-design/ant-design/issues/14367
+          [`${inputNumberCls}-handler`]: {
+            [`${inputNumberCls}-handler-up-inner,
+          ${inputNumberCls}-handler-down-inner`]: {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              minWidth: 'auto',
+              marginRight: 0,
+              fontSize: '7px', // FIXME: magic
+            },
+          },
+
+          [`${inputNumberCls}-borderless &`]: {
+            borderInlineStartWidth: 0,
+          },
+
+          [`&:hover ${inputNumberCls}-handler`]: {
+            height: '40%',
+          },
         },
 
-        [`.${token.inputNumberCls}-borderless &`]: {
-          borderBlockStartWidth: 0,
-        },
-      },
-      '&-handler-up-disabled, &-handler-down-disabled': {
-        cursor: 'not-allowed',
-      },
+        '&-handler-up': {
+          borderStartEndRadius: borderRadius,
+          cursor: 'pointer',
 
-      [`&-handler-up-disabled:hover &-handler-up-inner,
+          '&-inner': {
+            top: '50%',
+            marginTop: -5, // FIXME: magic
+            textAlign: 'center',
+          },
+
+          '&:hover': {
+            height: '60% !important',
+          },
+        },
+
+        '&-handler-down': {
+          top: 0,
+          borderBlockStart: `${borderWidth}px ${borderStyle} ${borderColor}`,
+          borderEndEndRadius: borderRadius,
+          cursor: 'pointer',
+
+          '&-inner': {
+            top: '50%',
+            textAlign: 'center',
+            transform: 'translateY(-50%)',
+          },
+
+          '&:hover': {
+            height: '60% !important',
+          },
+
+          [`${inputNumberCls}-borderless &`]: {
+            borderBlockStartWidth: 0,
+          },
+        },
+        '&-handler-up-disabled, &-handler-down-disabled': {
+          cursor: 'not-allowed',
+        },
+
+        [`&-handler-up-disabled:hover &-handler-up-inner,
       &-handler-down-disabled:hover &-handler-down-inner`]: {
-        color: token.textColorDisabled,
+          color: textColorDisabled,
+        },
       },
     },
-  },
-});
+  };
+};
 
-const genAffixWrapperStyles = (token: InputNumberToken): CSSObject => ({
-  [`.${token.inputNumberCls}-affix-wrapper`]: {
-    ...genBasicInputStyle(token.inputNumberCls, token),
-    ...genStatusStyle(token.inputNumberCls, token),
-    // or number handler will cover form status
-    position: 'relative',
-    display: 'inline-flex',
-    width: 90, // FIXME: magic
-    padding: 0,
-    paddingInlineStart: token.inputPaddingHorizontal,
+const genAffixWrapperStyles = (token: InputNumberToken): CSSObject => {
+  const { inputNumberCls, inputPaddingHorizontal, inputAffixMargin, prefixCls } = token;
 
-    '&:not(&-disabled):hover': {
-      ...genHoverStyle(token),
-      zIndex: 1,
-    },
-
-    '&-focused, &:focus': {
-      zIndex: 1,
-    },
-
-    '&-disabled': {
-      [`.${token.inputNumberCls}[disabled]`]: {
-        background: 'transparent',
-      },
-    },
-
-    [`> div.${token.inputNumberCls}`]: {
-      width: '100%',
-      border: 'none',
-      outline: 'none',
-
-      [`&.${token.inputNumberCls}-focused`]: {
-        boxShadow: 'none !important',
-      },
-    },
-
-    [`input.${token.inputNumberCls}-input`]: {
+  return {
+    [`${inputNumberCls}-affix-wrapper`]: {
+      ...genBasicInputStyle(prefixCls, token),
+      ...genStatusStyle(prefixCls, token),
+      // or number handler will cover form status
+      position: 'relative',
+      display: 'inline-flex',
+      width: 90, // FIXME: magic
       padding: 0,
-    },
+      paddingInlineStart: inputPaddingHorizontal,
 
-    '&::before': {
-      width: 0,
-      visibility: 'hidden',
-      content: '"\\a0"',
-    },
-
-    [`.${token.inputNumberCls}-handler-wrap`]: {
-      zIndex: 2,
-    },
-
-    [`.${token.inputNumberCls}`]: {
-      '&-prefix, &-suffix': {
-        display: 'flex',
-        flex: 'none',
-        alignItems: 'center',
-        pointerEvents: 'none',
-      },
-
-      '&-prefix': {
-        marginInlineEnd: token.inputAffixMargin,
-      },
-
-      '&-suffix': {
-        position: 'absolute',
-        insetBlockStart: 0,
-        insetInlineEnd: 0,
+      '&:not(&-disabled):hover': {
+        ...genHoverStyle(token),
         zIndex: 1,
-        height: '100%',
-        marginInlineEnd: token.inputPaddingHorizontal,
-        marginInlineStart: token.inputAffixMargin,
+      },
+
+      '&-focused, &:focus': {
+        zIndex: 1,
+      },
+
+      '&-disabled': {
+        [`${inputNumberCls}[disabled]`]: {
+          background: 'transparent',
+        },
+      },
+
+      [`> div${inputNumberCls}`]: {
+        width: '100%',
+        border: 'none',
+        outline: 'none',
+
+        [`&${inputNumberCls}-focused`]: {
+          boxShadow: 'none !important',
+        },
+      },
+
+      [`input${inputNumberCls}-input`]: {
+        padding: 0,
+      },
+
+      '&::before': {
+        width: 0,
+        visibility: 'hidden',
+        content: '"\\a0"',
+      },
+
+      [`${inputNumberCls}-handler-wrap`]: {
+        zIndex: 2,
+      },
+
+      [inputNumberCls]: {
+        '&-prefix, &-suffix': {
+          display: 'flex',
+          flex: 'none',
+          alignItems: 'center',
+          pointerEvents: 'none',
+        },
+
+        '&-prefix': {
+          marginInlineEnd: inputAffixMargin,
+        },
+
+        '&-suffix': {
+          position: 'absolute',
+          insetBlockStart: 0,
+          insetInlineEnd: 0,
+          zIndex: 1,
+          height: '100%',
+          marginInlineEnd: inputPaddingHorizontal,
+          marginInlineStart: inputAffixMargin,
+        },
       },
     },
-  },
-});
+  };
+};
 
 // ============================== Export ==============================
 export default function useStyle(prefixCls: string): UseComponentStyleResult {
@@ -343,7 +374,8 @@ export default function useStyle(prefixCls: string): UseComponentStyleResult {
 
   const inputNumberToken: InputNumberToken = {
     ...initInputToken(token),
-    inputNumberCls: prefixCls,
+    prefixCls,
+    inputNumberCls: `.${prefixCls}`,
     inputNumberHandlerActiveBgColor: '#f4f4f4', // FIXME: magic number
   };
 
