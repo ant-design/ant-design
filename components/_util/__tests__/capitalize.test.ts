ad46ba2f678bd13c968a44622caeba68cbcd8d49
vagusX <vagusxl@gmail.com>
2022-03-13 20:49:10 +0800

    test: improve coverage

--- components/_util/__tests__/capitalize.test.ts
+++ components/_util/__tests__/capitalize.test.ts
@@ -5,5 +5,13 @@ describe('capitalize', () => {
     expect(capitalize('antd')).toBe('Antd');
     expect(capitalize('Antd')).toBe('Antd');
     expect(capitalize(' antd')).toBe(' antd');
+    expect(capitalize('')).toBe('');
+  });
+
+  it('should return the original value when is not a string', () => {
+    expect(capitalize(1 as any)).toBe(1);
+    expect(capitalize(true as any)).toBe(true);
+    expect(capitalize(undefined as any)).toBe(undefined);
+    expect(capitalize(null as any)).toBe(null);
   });
 });
