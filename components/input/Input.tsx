c9b6a24978c6a93cb336506ec687c4048a5b3e57
MadCcc <1075746765@qq.com>
2022-03-14 15:37:02 +0800

    chore: add focus warning for Input (#34475)
    * chore: add focus warning for Input
    
    * chore: code clean
    
    * chore: remove duplicate variables

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -149,7 +149,7 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
   const mergedStatus = getMergedStatus(contextStatus, customStatus);
 
   // ===================== Focus warning =====================
-  const inputHasPrefixSuffix = hasPrefixSuffix(props);
+  const inputHasPrefixSuffix = hasPrefixSuffix(props) || !!hasFeedback;
   const prevHasPrefixSuffix = useRef<boolean>(inputHasPrefixSuffix);
   useEffect(() => {
     if (inputHasPrefixSuffix && !prevHasPrefixSuffix.current) {
@@ -200,8 +200,6 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
     </>
   );
 
-  const withPrefixSuffix = hasPrefixSuffix(props) || hasFeedback;
-
   // Allow clear
   let mergedAllowClear;
   if (typeof allowClear === 'object' && allowClear?.clearIcon) {
@@ -229,7 +227,7 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
           [`${prefixCls}-rtl`]: direction === 'rtl',
           [`${prefixCls}-borderless`]: !bordered,
         },
-        !withPrefixSuffix && getStatusClassNames(prefixCls, mergedStatus),
+        !inputHasPrefixSuffix && getStatusClassNames(prefixCls, mergedStatus),
       )}
       affixWrapperClassName={classNames(
         {
