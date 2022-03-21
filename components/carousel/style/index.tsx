5033e05ae40e233267e9bdfc946b247401153d55
afc163 <afc163@gmail.com>
2022-03-21 22:25:32 +0800

    move some styles

--- components/carousel/style/index.tsx
+++ components/carousel/style/index.tsx
@@ -19,6 +19,55 @@ const genCarouselStyle: GenerateStyle<CarouselToken, CSSObject> = token => {
   return {
     [carouselPrefixCls]: {
       ...resetComponent(token),
+
+      'slick-slider': {
+        position: 'relative',
+        display: 'block',
+        boxSizing: 'border-box',
+        touchAction: 'pan-y',
+        WebkitTouchCallout: 'none',
+        WebkitTapHighlightColor: 'transparent',
+      },
+
+      '.slick-list': {
+        position: 'relative',
+        display: 'block',
+        margin: '0',
+        padding: '0',
+        overflow: 'hidden',
+
+        '&:focus': {
+          outline: 'none',
+        },
+
+        '&.dragging': {
+          cursor: 'pointer',
+        },
+      },
+
+      '.slick-slide': {
+        pointerEvents: 'none',
+
+        // https://github.com/ant-design/ant-design/issues/23294
+        'input.@{ant-prefix}-radio-input, input.@{ant-prefix}-checkbox-input': {
+          visibility: 'hidden',
+        },
+
+        '&.slick-active': {
+          pointerEvents: 'auto',
+
+          'input.@{ant-prefix}-radio-input, input.@{ant-prefix}-checkbox-input': {
+            visibility: 'visible',
+          },
+        },
+
+        // fix Carousel content height not match parent node
+        // when children is empty node
+        // https://github.com/ant-design/ant-design/issues/25878
+        '> div > div': {
+          verticalAlign: 'bottom',
+        },
+      },
     },
   };
 };
