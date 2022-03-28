fd6100423f4c2296f398ee752726536ffc7c93c3
MadCcc <1075746765@qq.com>
2022-03-28 10:37:07 +0800

    chore: code clean

--- components/menu/__tests__/index.test.js
+++ components/menu/__tests__/index.test.js
@@ -14,7 +14,6 @@ import Tooltip from '../../tooltip';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import collapseMotion from '../../_util/motion';
-import { sleep } from '../../../tests/utils';
 
 const { SubMenu } = Menu;
 
