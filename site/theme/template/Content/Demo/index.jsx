869bcd962e22fb5ef5a8d1c78031c0217e6e828b
MadCcc <1075746765@qq.com>
2022-04-03 23:08:06 +0800

    docs: fix riddle demo

--- site/theme/template/Content/Demo/index.jsx
+++ site/theme/template/Content/Demo/index.jsx
@@ -249,10 +249,11 @@ class Demo extends React.Component {
 
     const riddlePrefillConfig = {
       title: `${localizedTitle} - antd@${dependencies.antd}`,
-      js: `${react18 ? `import { createRoot } from 'react-dom/client';\n` : ''}${sourceCode.replace(
-        /export default/,
-        'const Demo =',
-      )}\n\n${
+      js: `${
+        react18
+          ? `import React from 'react';\nimport { createRoot } from 'react-dom/client';\n`
+          : ''
+      }${sourceCode.replace(/export default/, 'const Demo =')}\n\n${
         react18 ? 'createRoot(mountNode).render(<Demo />)' : 'ReactDOM.render(<Demo />, mountNode)'
       };\n`,
       css: prefillStyle,
