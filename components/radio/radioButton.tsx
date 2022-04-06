326ad64620eef24fffb0d8b756892221f038f0f7
vagusx <vagusxl@gmail.com>
2022-04-06 16:15:50 +0800

    refactor: add optionType for Radio internally

--- components/radio/radioButton.tsx
+++ components/radio/radioButton.tsx
@@ -3,7 +3,7 @@ import Radio from './radio';
 import { RadioChangeEvent } from './interface';
 import { AbstractCheckboxProps } from '../checkbox/Checkbox';
 import { ConfigContext } from '../config-provider';
-import RadioGroupContext from './context';
+import RadioGroupContext, { RadioOptionTypeContextProvider } from './context';
 
 export type RadioButtonProps = AbstractCheckboxProps<RadioChangeEvent>;
 
@@ -12,12 +12,17 @@ const RadioButton = (props: RadioButtonProps, ref: React.Ref<any>) => {
   const { getPrefixCls } = React.useContext(ConfigContext);
 
   const { prefixCls: customizePrefixCls, ...radioProps } = props;
-  const prefixCls = getPrefixCls('radio-button', customizePrefixCls);
-  if (radioGroupContext) {
-    radioProps.checked = props.value === radioGroupContext.value;
-    radioProps.disabled = props.disabled || radioGroupContext.disabled;
+  const prefixCls = getPrefixCls('radio', customizePrefixCls);
+
+  if (radioGroupContext?.optionType) {
+    return <Radio prefixCls={prefixCls} {...radioProps} type="radio" ref={ref} />;
   }
-  return <Radio prefixCls={prefixCls} {...radioProps} type="radio" ref={ref} />;
+
+  return (
+    <RadioOptionTypeContextProvider value="button">
+      <Radio prefixCls={prefixCls} {...radioProps} type="radio" ref={ref} />
+    </RadioOptionTypeContextProvider>
+  );
 };
 
 export default React.forwardRef(RadioButton);
