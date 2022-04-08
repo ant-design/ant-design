3e3036b231254e74c464c99e8388e077cc408cb7
vagusx <vagusxl@gmail.com>
2022-04-08 13:27:56 +0800

    feat: remove useless condition for RadioGroupContext

--- components/radio/radioButton.tsx
+++ components/radio/radioButton.tsx
@@ -3,20 +3,15 @@ import Radio from './radio';
 import { RadioChangeEvent } from './interface';
 import { AbstractCheckboxProps } from '../checkbox/Checkbox';
 import { ConfigContext } from '../config-provider';
-import RadioGroupContext, { RadioOptionTypeContextProvider } from './context';
+import { RadioOptionTypeContextProvider } from './context';
 
 export type RadioButtonProps = AbstractCheckboxProps<RadioChangeEvent>;
 
 const RadioButton = (props: RadioButtonProps, ref: React.Ref<any>) => {
-  const radioGroupContext = React.useContext(RadioGroupContext);
   const { getPrefixCls } = React.useContext(ConfigContext);
 
   const { prefixCls: customizePrefixCls, ...radioProps } = props;
   const prefixCls = getPrefixCls('radio', customizePrefixCls);
-
-  if (radioGroupContext?.optionType) {
-    return <Radio prefixCls={prefixCls} {...radioProps} type="radio" ref={ref} />;
-  }
 
   return (
     <RadioOptionTypeContextProvider value="button">
