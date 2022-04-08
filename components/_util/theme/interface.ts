be8f7f2b14477931ca1705603ee978f00ea23d8d
MadCcc <1075746765@qq.com>
2022-04-08 14:52:57 +0800

    refactor: Grid migrate to genComponentHook

--- components/_util/theme/interface.ts
+++ components/_util/theme/interface.ts
@@ -50,6 +50,7 @@ export interface OverrideToken {
   Divider?: DividerComponentToken;
   Empty?: EmptyComponentToken;
   Form?: {};
+  Grid?: {};
   Input?: {};
   InputNumber?: InputNumberComponentToken;
   Mentions?: MentionsComponentToken;
