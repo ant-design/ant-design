7746374c724222449223d78a87547a779797010a
MadCcc <1075746765@qq.com>
2022-04-20 11:54:42 +0800

    chore: code clean

--- components/spin/__tests__/index.test.js
+++ components/spin/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, mount } from 'enzyme';
+import { mount } from 'enzyme';
 // eslint-disable-next-line import/no-named-as-default
-import { render as testingLibRender } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Spin from '..';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
@@ -22,12 +22,12 @@ describe('Spin', () => {
 
   it("should render custom indicator when it's set", () => {
     const customIndicator = <div className="custom-indicator" />;
-    const wrapper = render(<Spin indicator={customIndicator} />);
-    expect(wrapper).toMatchSnapshot();
+    const wrapper = mount(<Spin indicator={customIndicator} />);
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
   it('should be controlled by spinning', () => {
-    const { container, rerender } = testingLibRender(<Spin spinning={false} />);
+    const { container, rerender } = render(<Spin spinning={false} />);
     expect(container.querySelector('.ant-spin-spinning')).toBeFalsy();
     rerender(<Spin spinning />);
     expect(container.querySelector('.ant-spin-spinning')).toBeTruthy();
