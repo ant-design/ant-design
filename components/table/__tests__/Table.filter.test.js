a9806a2be07918d09a0ff96520a64b82c872ebda
Yuyao Nie <nieyuyao0826@hotmail.com>
2022-04-23 17:11:52 +0800

    perf: optimize calculation of filteredKeysIsAllControlled (#35064)
    * perf: optimize calculation of filteredKeysIsAllControlled
    
    * fix: ci failure
    
    * fix: modify codes by suggestion

--- components/table/__tests__/Table.filter.test.js
+++ components/table/__tests__/Table.filter.test.js
@@ -2252,4 +2252,57 @@ describe('Table.filter', () => {
     expect(wrapper.find('.ant-tree-checkbox-checked').length).toBe(1);
     expect(wrapper.find('.ant-tree-checkbox-checked+span').text()).toBe('Girl');
   });
+
+  it('filteredKeys should all be controlled or not controlled', () => {
+    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    errorSpy.mockReset();
+    const tableData = [
+      {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+      },
+    ];
+    const columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        key: 'name',
+        filters: [],
+      },
+      {
+        title: 'age',
+        dataIndex: 'age',
+        key: 'age',
+        filters: [],
+      },
+    ];
+    render(
+      createTable({
+        columns,
+        data: tableData,
+      }),
+    );
+    expect(errorSpy).not.toBeCalled();
+    errorSpy.mockReset();
+    columns[0].filteredValue = [];
+    render(
+      createTable({
+        columns,
+        data: tableData,
+      }),
+    );
+    expect(errorSpy).toBeCalledWith(
+      'Warning: [antd: Table] Columns should all contain `filteredValue` or not contain `filteredValue`.',
+    );
+    errorSpy.mockReset();
+    columns[1].filteredValue = [];
+    render(
+      createTable({
+        columns,
+        data: tableData,
+      }),
+    );
+    expect(errorSpy).not.toBeCalled();
+  });
 });
