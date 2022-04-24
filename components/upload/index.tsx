b81a8f9c2a9571856fad1e5a5130134bc284c37c
kanweiwei <kanweiwei@nutstore.net>
2022-04-24 19:15:19 +0800

    fix: upload circle ref

--- components/upload/index.tsx
+++ components/upload/index.tsx
@@ -1,8 +1,20 @@
-import Upload from './Upload';
 import Dragger from './Dragger';
+import InternalUpload, { LIST_IGNORE, UploadProps } from './Upload';
 
 export { UploadProps, UploadListProps, UploadChangeParam, RcFile } from './interface';
 export { DraggerProps } from './Dragger';
 
+type InternalUploadType = typeof InternalUpload;
+interface UploadInterface<T = any> extends InternalUploadType {
+  <U extends T>(
+    props: React.PropsWithChildren<UploadProps<U>> & React.RefAttributes<any>,
+  ): React.ReactElement;
+  Dragger: typeof Dragger;
+  LIST_IGNORE: string;
+}
+
+const Upload = InternalUpload as UploadInterface;
 Upload.Dragger = Dragger;
+Upload.LIST_IGNORE = LIST_IGNORE;
+
 export default Upload;
