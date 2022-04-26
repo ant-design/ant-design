810e0b52bc42aa02339855e4f97757be1cfcc904
谭博尹 <740514874@qq.com>
2022-04-26 11:22:34 +0800

    fix: RTL-model

--- components/progress/style/index.tsx
+++ components/progress/style/index.tsx
@@ -122,7 +122,7 @@ const genBaseStyle: GenerateStyle<ProgressToken> = (token:ProgressToken) => {
           borderRadius: '10px', // FIXME: hard code in v4
           opacity: 0,
           animation: `${antProgressActive} 2.4s cubic-bezier(0.23, 1, 0.32, 1) infinite`,
-          content: '\"\"',
+          content: "",
         },
       },
 
