74c299080569c0ee1caccddc54741869ee737169
vagusX <vagusxl@gmail.com>
2022-04-28 11:10:01 +0000

    fix: lint issue

--- components/segmented/__tests__/index.test.tsx
+++ components/segmented/__tests__/index.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons';
 
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
