51f6a3f2b67c7e06449cac07e13745a1ca46d211
黄建峰 <645381995@qq.com>
2022-05-09 19:36:48 +0800

    feat: test

--- components/drawer/__tests__/DrawerEvent.test.js
+++ components/drawer/__tests__/DrawerEvent.test.js
@@ -1,86 +1,78 @@
 import React from 'react';
 import Drawer from '..';
-import { render, fireEvent, cleanup } from '../../../tests/utils';
+import { render, fireEvent } from '../../../tests/utils';
 
 describe('Drawer', () => {
-  afterEach(() => {
-    cleanup();
-  });
-
   const getDrawer = props => (
-    <Drawer visible {...props}>
+    <Drawer visible getContainer={false} {...props}>
       Here is content of Drawer
     </Drawer>
   );
 
   it('render correctly', () => {
-    render(getDrawer());
-    expect(document.body).toMatchSnapshot();
-  });
-
-  it('render correctly when set visible false', () => {
-    const { rerender } = render(getDrawer());
-    expect(document.querySelector('.ant-drawer-body')).toBeTruthy();
+    const { container, asFragment, rerender } = render(getDrawer());
+    expect(container.querySelector('.ant-drawer-body')).toBeTruthy();
 
     rerender(getDrawer({ visible: false }));
 
-    expect(document.querySelector('.ant-drawer-body').textContent).toEqual(
+    expect(container.querySelector('.ant-drawer-body').textContent).toEqual(
       'Here is content of Drawer',
     );
-    expect(document.body).toMatchSnapshot();
+
+    expect(asFragment().firstChild).toMatchSnapshot();
   });
 
   it('mask trigger onClose', () => {
     const onClose = jest.fn();
-    render(getDrawer({ onClose }));
+    const { container } = render(getDrawer({ onClose }));
 
-    fireEvent.click(document.querySelector('.ant-drawer-mask'));
+    fireEvent.click(container.querySelector('.ant-drawer-mask'));
     expect(onClose).toHaveBeenCalled();
   });
 
   it('close button trigger onClose', () => {
     const onClose = jest.fn();
-    render(getDrawer({ onClose }));
+    const { container } = render(getDrawer({ onClose }));
 
-    fireEvent.click(document.querySelector('.ant-drawer-close'));
+    fireEvent.click(container.querySelector('.ant-drawer-close'));
     expect(onClose).toHaveBeenCalled();
   });
 
   it('maskClosable no trigger onClose', () => {
     const onClose = jest.fn();
-    render(getDrawer({ onClose, maskClosable: false }));
+    const { container } = render(getDrawer({ onClose, maskClosable: false }));
 
-    fireEvent.click(document.querySelector('.ant-drawer-mask'));
+    fireEvent.click(container.querySelector('.ant-drawer-mask'));
     expect(onClose).not.toHaveBeenCalled();
   });
 
   it('dom should be removed after close when destroyOnClose is true', () => {
-    const { rerender } = render(getDrawer({ destroyOnClose: true }));
+    const { container, rerender } = render(getDrawer({ destroyOnClose: true }));
 
     rerender(getDrawer({ destroyOnClose: true, visible: false }));
-    const ev = new Event('transitionend', { bubbles: true });
+    const ev = new TransitionEvent('transitionend', { bubbles: true });
     ev.propertyName = 'transform';
     fireEvent(document.querySelector('.ant-drawer-content-wrapper'), ev);
 
-    expect(document.querySelector('.ant-drawer-wrapper-body')).toBeFalsy();
+    expect(container.querySelector('.ant-drawer-wrapper-body')).toBeFalsy();
   });
 
   it('dom should be existed after close when destroyOnClose is false', () => {
-    const { rerender } = render(getDrawer());
-    expect(document.querySelector('.ant-drawer-wrapper-body')).toBeTruthy();
+    const { container, rerender } = render(getDrawer());
+    expect(container.querySelector('.ant-drawer-wrapper-body')).toBeTruthy();
 
     rerender(getDrawer({ visible: false }));
-    const ev = new Event('transitionend', { bubbles: true });
+    const ev = new TransitionEvent('transitionend', { bubbles: true });
     ev.propertyName = 'transform';
     fireEvent(document.querySelector('.ant-drawer-content-wrapper'), ev);
 
-    expect(document.querySelector('.ant-drawer-wrapper-body')).toBeTruthy();
+    expect(container.querySelector('.ant-drawer-wrapper-body')).toBeTruthy();
   });
   it('test afterVisibleChange', async () => {
     const afterVisibleChange = jest.fn();
     const { rerender } = render(getDrawer({ afterVisibleChange, visible: true }));
     rerender(getDrawer({ afterVisibleChange, visible: false }));
-    const ev = new Event('transitionend', { bubbles: true });
+    const ev = new TransitionEvent('transitionend', { bubbles: true });
     ev.propertyName = 'transform';
     fireEvent(document.querySelector('.ant-drawer-content-wrapper'), ev);
     expect(afterVisibleChange).toBeCalledTimes(1);
