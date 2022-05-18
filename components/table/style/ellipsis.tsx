9630d7ceea23a325d3c0632330907977be04245d
afc163 <afc163@gmail.com>
2022-05-18 11:57:10 +0800

    style: add cell-ellipsis styles

--- components/table/style/ellipsis.tsx
+++ components/table/style/ellipsis.tsx
@@ -5,7 +5,33 @@ import type { TableToken } from './index';
 const genStyle: GenerateStyle<TableToken, CSSObject> = token => {
   const { componentCls } = token;
   return {
-    [`${componentCls}-wrapper`]: {},
+    [`${componentCls}-wrapper`]: {
+      [`${componentCls}-cell-ellipsis`]: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        wordBreak: 'keep-all',
+
+        // Fixed first or last should special process
+        [`
+          &${componentCls}-cell-fix-left-last,
+          &${componentCls}-cell-fix-right-first
+        `]: {
+          overflow: 'visible',
+          [`${componentCls}-cell-content`]: {
+            display: 'block',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          },
+        },
+
+        [`${componentCls}-column-title`]: {
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          wordBreak: 'keep-all',
+        },
+      },
+    },
   };
 };
 
