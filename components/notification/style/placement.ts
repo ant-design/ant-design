3a388d24b75f343f2405dfd5d6838e565428c485
MadCcc <1075746765@qq.com>
2022-05-20 17:40:03 +0800

    refactor: notification full token (#35656)
    * refactor: notification full token
    
    * chore: code clean
    
    * test: fix lint

--- components/notification/style/placement.ts
+++ components/notification/style/placement.ts
@@ -4,29 +4,29 @@ import type { GenerateStyle } from '../../_util/theme';
 import type { NotificationToken } from '.';
 
 const genNotificationPlacementStyle: GenerateStyle<NotificationToken, CSSObject> = token => {
-  const { componentCls, notificationWidth, notificationMarginEdge } = token;
+  const { componentCls, width, notificationMarginEdge } = token;
 
   const notificationTopFadeIn = new Keyframes('antNotificationTopFadeIn', {
     '0%': {
-      marginTop: '-100%', // FIXME: hardcode in v4
-      opacity: 0, // FIXME: hardcode in v4
+      marginTop: '-100%',
+      opacity: 0,
     },
 
     '100%': {
-      marginTop: 0, // FIXME: hardcode in v4
-      opacity: 1, // FIXME: hardcode in v4
+      marginTop: 0,
+      opacity: 1,
     },
   });
 
   const notificationBottomFadeIn = new Keyframes('antNotificationBottomFadeIn', {
     '0%': {
-      marginBottom: '-100%', // FIXME: hardcode in v4
-      opacity: 0, // FIXME: hardcode in v4
+      marginBottom: '-100%',
+      opacity: 0,
     },
 
     '100%': {
-      marginBottom: 0, // FIXME: hardcode in v4
-      opacity: 1, // FIXME: hardcode in v4
+      marginBottom: 0,
+      opacity: 1,
     },
   });
 
@@ -34,25 +34,23 @@ const genNotificationPlacementStyle: GenerateStyle<NotificationToken, CSSObject>
     '0%': {
       right: {
         _skip_check_: true,
-        value: notificationWidth,
+        value: width,
       },
-      opacity: 0, // FIXME: hardcode in v4
+      opacity: 0,
     },
 
     '100%': {
       right: {
         _skip_check_: true,
-        value: 0, // FIXME: hardcode in v4
+        value: 0,
       },
-      opacity: 1, // FIXME: hardcode in v4
+      opacity: 1,
     },
   });
 
   return {
     [`&${componentCls}-top, &${componentCls}-bottom`]: {
-      // marginRight: 0,
-      // marginLeft: 0,
-      marginInline: '0 0', // FIXME: hardcode in v4
+      marginInline: 0,
     },
 
     [`&${componentCls}-top`]: {
@@ -70,9 +68,7 @@ const genNotificationPlacementStyle: GenerateStyle<NotificationToken, CSSObject>
     },
 
     [`&${componentCls}-topLeft, &${componentCls}-bottomLeft`]: {
-      // marginRight: 0,
-      marginInlineEnd: 0, // FIXME: hardcode in v4
-      // marginLeft: notificationMarginEdge,
+      marginInlineEnd: 0,
       marginInlineStart: notificationMarginEdge,
 
       [`${componentCls}-fade-enter${componentCls}-fade-enter-active, ${componentCls}-fade-appear${componentCls}-fade-appear-active`]:
