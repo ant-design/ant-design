b90d435dd3ccfedf8e8a168f675c97d10e4220eb
afc163 <afc163@gmail.com>
2022-05-26 18:14:33 +0800

    style: add expand code

--- components/table/style/expand.tsx
+++ components/table/style/expand.tsx
@@ -4,10 +4,10 @@ import { operationUnit } from '../../_util/theme';
 import type { TableToken } from './index';
 
 const genExpandStyle: GenerateStyle<TableToken, CSSObject> = token => {
-  const { componentCls, Checkbox: { checkboxSize } = { checkboxSize: 0 } } = token;
-  const expandIconSize =
-    Math.ceil((token.fontSizeSM * 1.4 - token.controlLineWidth * 3) / 2) * 2 +
-    token.controlLineWidth * 3;
+  const { componentCls } = token;
+  const checkboxSize = token.fontSizeLG;
+  const halfInnerSize = Math.ceil((token.fontSizeSM * 1.4 - token.controlLineWidth * 3) / 2);
+  const expandIconSize = halfInnerSize * 2 + token.controlLineWidth * 3;
   const tableBorder = `${token.controlLineWidth}px ${token.controlLineType} ${token.tableBorderColor}`;
   return {
     [`${componentCls}-wrapper`]: {
@@ -42,6 +42,58 @@ const genExpandStyle: GenerateStyle<TableToken, CSSObject> = token => {
         transform: `scale(${checkboxSize / expandIconSize})`,
         transition: 'all 0.3s',
         userSelect: 'none',
+
+        [`&:focus, &:hover, &:active`]: {
+          borderColor: 'currentcolor',
+        },
+
+        [`&::before, &::after`]: {
+          position: 'absolute',
+          background: 'currentcolor',
+          transition: 'transform 0.3s ease-out',
+          content: '""',
+        },
+
+        '&::before': {
+          top: halfInnerSize,
+          right: '3px',
+          left: '3px',
+          height: token.controlLineWidth,
+        },
+
+        '&::after': {
+          top: '3px',
+          bottom: '3px',
+          left: halfInnerSize,
+          width: token.controlLineWidth,
+          transform: 'rotate(90deg)',
+        },
+
+        // Motion effect
+        '&-collapsed::before': {
+          transform: 'rotate(-180deg)',
+        },
+
+        '&-collapsed::after': {
+          transform: 'rotate(0deg)',
+        },
+
+        '&-spaced': {
+          '&::before, &::after': {
+            display: 'none',
+            content: 'none',
+          },
+          background: 'transparent',
+          border: '0',
+          visibility: 'hidden',
+        },
+
+        [`${componentCls}-row-indent + &`]: {
+          marginTop:
+            (token.fontSizeBase * token.lineHeight - token.controlLineWidth * 3) / 2 -
+            halfInnerSize,
+          marginRight: token.paddingXS,
+        },
       },
     },
   };
