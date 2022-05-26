eed02fc64b7c6a05d0c05eb61227542ae7d5141d
afc163 <afc163@gmail.com>
2022-05-26 19:01:05 +0800

    style: add fixed code

--- components/table/style/fixed.tsx
+++ components/table/style/fixed.tsx
@@ -5,7 +5,46 @@ import type { TableToken } from './index';
 const genFixedStyle: GenerateStyle<TableToken, CSSObject> = token => {
   const { componentCls } = token;
   return {
-    [`${componentCls}-wrapper`]: {},
+    [`${componentCls}-wrapper`]: {
+      [`
+        ${componentCls}-cell-fix-left,
+        ${componentCls}-cell-fix-right
+      `]: {
+        position: 'sticky !important',
+        zIndex: token.zIndexTableFixed,
+        background: token.tableBg,
+      },
+
+      [`
+        ${componentCls}-cell-fix-left-first::after,
+        ${componentCls}-cell-fix-left-last::after
+      `]: {
+        position: 'absolute',
+        top: '0',
+        right: '0',
+        bottom: '-1px',
+        width: '30px',
+        transform: 'translateX(100%)',
+        transition: 'box-shadow 0.3s',
+        content: '""',
+        pointerEvents: 'none',
+      },
+
+      [`
+    ${componentCls}-cell-fix-right-first::after,
+    ${componentCls}-cell-fix-right-last::after
+  `]: {
+        position: 'absolute',
+        top: '0',
+        bottom: '-1px',
+        left: '0',
+        width: '30px',
+        transform: 'translateX(-100%)',
+        transition: 'box-shadow 0.3s',
+        content: '""',
+        pointerEvents: 'none',
+      },
+    },
   };
 };
 
