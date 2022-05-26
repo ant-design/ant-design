23dea4ec4078ead93aefa94f142ba38059bcc4a4
afc163 <afc163@gmail.com>
2022-05-26 11:49:30 +0800

    style: fix bordered style

--- components/table/style/bordered.tsx
+++ components/table/style/bordered.tsx
@@ -4,8 +4,89 @@ import type { TableToken } from './index';
 
 const genStyle: GenerateStyle<TableToken, CSSObject> = token => {
   const { componentCls } = token;
+  const tableBorder = `${token.controlLineWidth}px ${token.controlLineType} ${token.tableBorderColor}`;
   return {
-    [`${componentCls}-wrapper`]: {},
+    [`${componentCls}-wrapper`]: {
+      [`${componentCls}${componentCls}-bordered`]: {
+        // ============================ Title =============================
+        [`> ${componentCls}-title`]: {
+          border: tableBorder,
+          borderBottom: '0',
+        },
+
+        [`> ${componentCls}-container`]: {
+          // ============================ Content ============================
+          borderLeft: tableBorder,
+
+          [`
+            > ${componentCls}-content,
+            > ${componentCls}-header,
+            > ${componentCls}-body,
+            > ${componentCls}-summary
+          `]: {
+            '> table': {
+              // ============================= Cell =============================
+              [`
+                > thead > tr > th,
+                > tbody > tr > td,
+                > tfoot > tr > th,
+                > tfoot > tr > td
+              `]: {
+                borderRight: tableBorder,
+              },
+
+              // ============================ Header ============================
+              '> thead': {
+                '> tr:not(:last-child) > th': {
+                  borderBottom: tableBorder,
+                },
+
+                '> tr > th::before': {
+                  backgroundColor: 'transparent !important',
+                },
+              },
+
+              // Fixed right should provides additional border
+              [`
+                > thead > tr,
+                > tbody > tr,
+                > tfoot > tr
+              `]: {
+                [`> ${componentCls}-cell-fix-right-first::after`]: {
+                  borderRight: tableBorder,
+                },
+              },
+
+              // ========================== Expandable ==========================
+              '> table > tbody > tr > td': {
+                [`> ${componentCls}-expanded-row-fixed`]: {
+                  margin:
+                    '-@table-padding-vertical (-@table-padding-horizontal - @border-width-base)',
+
+                  '&::after': {
+                    position: 'absolute',
+                    top: '0',
+                    right: token.controlLineWidth,
+                    bottom: '0',
+                    borderRight: tableBorder,
+                    content: '""',
+                  },
+                },
+              },
+            },
+          },
+
+          [`
+            > ${componentCls}-content,
+            > ${componentCls}-header
+          `]: {
+            '> table': {
+              borderTop: tableBorder,
+            },
+          },
+        },
+      },
+    },
   };
 };
 
