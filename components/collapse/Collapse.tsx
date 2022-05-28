49575dde716ad49b9b907681e3c36eaee5917500
zombiej <smith3816@gmail.com>
2022-05-28 18:09:58 +0800

    chore: revert empty line

--- components/collapse/Collapse.tsx
+++ components/collapse/Collapse.tsx
@@ -3,6 +3,7 @@ import RcCollapse from 'rc-collapse';
 import type { CSSMotionProps } from 'rc-motion';
 import classNames from 'classnames';
 import RightOutlined from '@ant-design/icons/RightOutlined';
+
 import toArray from 'rc-util/lib/Children/toArray';
 import omit from 'rc-util/lib/omit';
 import type { CollapsibleType } from './CollapsePanel';
@@ -81,7 +82,6 @@ const Collapse: CollapseInterface = props => {
 
   const renderExpandIcon = (panelProps: PanelProps = {}) => {
     const { expandIcon } = props;
-
     const icon = (
       expandIcon ? (
         expandIcon(panelProps)
