83d64524866556a440a501cffe973c32db18d343
MadCcc <1075746765@qq.com>
2022-05-30 19:42:32 +0800

    test: fix lint

--- scripts/collect-token-statistic.js
+++ scripts/collect-token-statistic.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const fs = require('fs-extra');
@@ -43,11 +45,9 @@ const Notification = () => {
 };
 ReactDOMServer.renderToString(React.createElement(Notification));
 
-const fillStatistic = async () => {
+(async () => {
   const libPath = `${process.cwd()}/lib/_util/theme/util/statistic.js`;
   const libContent = await fs.readFile(libPath, 'utf8');
   const newLibContent = `${libContent}\nexports._statistic_build_ = ${JSON.stringify(statistic)}`;
   await fs.writeFile(libPath, newLibContent, 'utf8');
-};
-
-fillStatistic();
+})();
