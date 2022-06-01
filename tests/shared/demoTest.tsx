276f9018d27b59401d17a416bcc3a0f42d4d3b09
vagusx <vagusxl@gmail.com>
2022-06-01 10:33:30 +0800

    fix: use test-lib for demoTest

--- tests/shared/demoTest.tsx
+++ tests/shared/demoTest.tsx
@@ -1,53 +1,55 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import * as React from 'react';
 import glob from 'glob';
-import { render } from 'enzyme';
 import MockDate from 'mockdate';
 import moment from 'moment';
 import type { TriggerProps } from 'rc-trigger';
 import { excludeWarning } from './excludeWarning';
+import { render } from '../utils';
 
 export const TriggerMockContext = React.createContext<Partial<TriggerProps> | undefined>(undefined);
 
-type CheerIO = ReturnType<typeof render>;
-type CheerIOElement = CheerIO[0];
 // We should avoid use it in 4.0. Reopen if can not handle this.
 const USE_REPLACEMENT = false;
 const testDist = process.env.LIB_DIR === 'dist';
+
+function isValidElement(element: ChildNode): element is HTMLElement {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType#value
+  return element.nodeType === 3 || element.nodeType === 8;
+}
 
 /**
  * Rc component will generate id for aria usage. It's created as `test-uuid` when env === 'test'. Or
  * `f7fa7a3c-a675-47bc-912e-0c45fb6a74d9`(randomly) when not test env. So we need hack of this to
  * modify the `aria-controls`.
  */
-function ariaConvert(wrapper: CheerIO) {
-  if (!testDist || !USE_REPLACEMENT) return wrapper;
+function ariaConvert(container: HTMLElement) {
+  if (!testDist || !USE_REPLACEMENT) return container;
 
   const matches = new Map();
 
-  function process(entry: CheerIOElement) {
-    if (entry.type === 'text' || entry.type === 'comment') {
-      return;
-    }
-    const { attribs, children } = entry;
-    if (matches.has(entry)) return;
-    matches.set(entry, true);
+  function process(entry: ChildNode) {
+    if (isValidElement(entry)) {
+      const { childNodes } = entry;
+      if (matches.has(entry)) return;
+      matches.set(entry, true);
 
-    // Change aria
-    if (attribs && attribs['aria-controls']) {
-      attribs['aria-controls'] = ''; // Remove all the aria to keep render sync in jest & jest node
-    }
+      // Change aria
+      if (entry.getAttribute('aria-controls')) {
+        entry.setAttribute('aria-controls', ''); // Remove all the aria to keep render sync in jest & jest node
+      }
 
-    // Loop children
-    if (!children) {
-      return;
+      // Loop children
+      if (!childNodes.length) {
+        return;
+      }
+      childNodes.forEach(process);
     }
-    (Array.isArray(children) ? children : [children]).forEach(process);
   }
 
-  wrapper.each((_, entry) => process(entry));
+  container.childNodes.forEach(entry => process(entry));
 
-  return wrapper;
+  return container;
 }
 
 type Options = {
@@ -85,12 +87,19 @@ function baseText(doInject: boolean, component: string, options: Options = {}) {
           );
         }
 
-        const wrapper = render(Demo);
+        const { container, asFragment } = render(Demo);
+
+        const childNodes = Array.from(asFragment().childNodes);
 
         // Convert aria related content
-        ariaConvert(wrapper);
+        ariaConvert(container);
 
-        expect(wrapper).toMatchSnapshot();
+        if (childNodes.length === 1) {
+          expect(childNodes[0]).toMatchSnapshot();
+        } else {
+          expect(childNodes).toMatchSnapshot();
+        }
+
         MockDate.reset();
 
         errSpy();
