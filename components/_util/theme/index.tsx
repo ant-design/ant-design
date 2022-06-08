865a987418a6dcbb6355d5e1cace9f962fc54aee
MadCcc <1075746765@qq.com>
2022-06-09 01:54:45 +0800

    feat: format token correctly

--- components/_util/theme/index.tsx
+++ components/_util/theme/index.tsx
@@ -79,7 +79,7 @@ export function useToken(): [Theme<SeedToken, DerivativeToken>, GlobalToken, str
     [defaultSeedToken, rootDesignToken],
     {
       salt,
-      override: override?.derivative,
+      override,
       formatToken,
     },
   );
