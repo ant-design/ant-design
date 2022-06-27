491cfc07b4b2699fa809a6c123d43e45e78fddfc
MadCcc <1075746765@qq.com>
2022-06-27 20:11:58 +0800

    test: add override component token test

--- components/config-provider/__tests__/theme.test.tsx
+++ components/config-provider/__tests__/theme.test.tsx
@@ -2,6 +2,7 @@ import { kebabCase } from 'lodash';
 import canUseDom from 'rc-util/lib/Dom/canUseDom';
 import React from 'react';
 import ConfigProvider from '..';
+import { InputNumber } from '../..';
 import { render } from '../../../tests/utils';
 import { useToken } from '../../theme';
 import darkDerivative from '../../theme/themes/dark';
@@ -67,5 +68,21 @@ describe('ConfigProvider.Theme', () => {
       </ConfigProvider>,
     );
     expect(tokenRef?.colorBgComponent).toBe('#141414');
+  });
+
+  it('overriding component token should work', () => {
+    render(
+      <ConfigProvider theme={{ override: { InputNumber: { handleWidth: 50.1234 } } }}>
+        <InputNumber />
+      </ConfigProvider>,
+    );
+    const dynamicStyles = Array.from(document.querySelectorAll('style[data-css-hash]')).map(
+      item => item?.innerHTML ?? '',
+    );
+    expect(
+      dynamicStyles.some(
+        style => style.includes('.ant-input-number') && style.includes('width:50.1234px'),
+      ),
+    ).toBeTruthy();
   });
 });
