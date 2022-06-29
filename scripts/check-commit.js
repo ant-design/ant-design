39b5d580b1104f19190cbfa506387bef5467ecd3
MadCcc <1075746765@qq.com>
2022-06-29 22:03:04 +0800

    chore: update check-commit

--- scripts/check-commit.js
+++ scripts/check-commit.js
@@ -24,7 +24,7 @@ async function checkVersion() {
 }
 
 async function checkBranch({ current }) {
-  if (version.includes('-alpha.')) {
+  if (version.includes('-alpha.') || version.includes('-experimental.')) {
     console.log(chalk.cyan('😃 Alpha version. Skip branch check.'));
   } else if (current !== 'master' && current !== '4.0-prepare') {
     console.log(chalk.yellow('🤔 You are not in the master branch!'));
