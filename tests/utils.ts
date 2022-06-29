9542704fb31bef0a2c435af9c7eaf6a37cc3fe75
zombiej <smith3816@gmail.com>
2022-06-29 11:44:19 +0800

    test: Fix table empty test

--- tests/utils.ts
+++ tests/utils.ts
@@ -4,8 +4,8 @@ import { StrictMode } from 'react';
 import { act } from 'react-dom/test-utils';
 import type { RenderOptions } from '@testing-library/react';
 import { render } from '@testing-library/react';
-import { _rs as onLibResize } from 'rc-resize-observer/lib/utils/observerUtil'
-import { _rs as onEsResize } from 'rc-resize-observer/es/utils/observerUtil'
+import { _rs as onLibResize } from 'rc-resize-observer/lib/utils/observerUtil';
+import { _rs as onEsResize } from 'rc-resize-observer/es/utils/observerUtil';
 
 export function setMockDate(dateString = '2017-09-18T03:30:07.795') {
   MockDate.set(dateString);
@@ -33,11 +33,14 @@ export { customRender as render };
 export const triggerResize = (target: Element) => {
   const originGetBoundingClientRect = target.getBoundingClientRect;
 
-  target.getBoundingClientRect = () => ({ width: 510, height: 903 }) as DOMRect;
-  onLibResize([{ target } as ResizeObserverEntry]);
-  onEsResize([{ target } as ResizeObserverEntry]);
+  target.getBoundingClientRect = () => ({ width: 510, height: 903 } as DOMRect);
+
+  act(() => {
+    onLibResize([{ target } as ResizeObserverEntry]);
+    onEsResize([{ target } as ResizeObserverEntry]);
+  });
 
   target.getBoundingClientRect = originGetBoundingClientRect;
-}
+};
 
 export * from '@testing-library/react';
