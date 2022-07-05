892654601e45ef7790e34b9c45509db22c6d438a
MadCcc <1075746765@qq.com>
2022-07-05 10:35:09 +0800

    chore: update dist config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,9 @@
 /* eslint no-param-reassign: 0 */
 // This config is for building dist files
-const chalk = require('chalk');
 const getWebpackConfig = require('@ant-design/tools/lib/getWebpackConfig');
-const RemovePlugin = require('remove-files-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
-const darkVars = require('./scripts/dark-vars');
-const compactVars = require('./scripts/compact-vars');
-
-function injectLessVariables(config, variables) {
-  (Array.isArray(config) ? config : [config]).forEach(conf => {
-    conf.module.rules.forEach(rule => {
-      // filter less rule
-      if (rule.test instanceof RegExp && rule.test.test('.less')) {
-        const lessRule = rule.use[rule.use.length - 1];
-        if (lessRule.options.lessOptions) {
-          lessRule.options.lessOptions.modifyVars = {
-            ...lessRule.options.lessOptions.modifyVars,
-            ...variables,
-          };
-        } else {
-          lessRule.options.modifyVars = {
-            ...lessRule.options.modifyVars,
-            ...variables,
-          };
-        }
-      }
-    });
-  });
-
-  return config;
-}
 
 function addLocales(webpackConfig) {
   let packageName = 'antd-with-locales';
@@ -51,56 +23,7 @@ function externalDayjs(config) {
   };
 }
 
-function processWebpackThemeConfig(themeConfig, theme, vars) {
-  themeConfig.forEach(config => {
-    // rename default entry to ${theme} entry
-    Object.keys(config.entry).forEach(entryName => {
-      const originPath = config.entry[entryName];
-      let replacedPath = [...originPath];
-
-      // We will replace `./index` to `./index-style-only` since theme dist only use style file
-      if (originPath.length === 1 && originPath[0] === './index') {
-        replacedPath = ['./index-style-only'];
-      } else {
-        // eslint-disable-next-line no-console
-        console.log(chalk.red('🆘 Seems entry has changed! It should be `./index`'));
-      }
-
-      config.entry[entryName.replace('antd', `antd.${theme}`)] = replacedPath;
-      delete config.entry[entryName];
-    });
-
-    // apply ${theme} less variables
-    injectLessVariables(config, vars);
-
-    // ignore emit ${theme} entry js & js.map file
-    config.plugins.push(
-      new RemovePlugin({
-        after: {
-          root: './dist',
-          include: [
-            `antd.${theme}.js`,
-            `antd.${theme}.js.map`,
-            `antd.${theme}.min.js`,
-            `antd.${theme}.min.js.map`,
-          ],
-          log: false,
-          logWarning: false,
-        },
-      }),
-    );
-  });
-}
-
-const legacyEntryVars = {
-  'root-entry-name': 'default',
-};
-const webpackConfig = injectLessVariables(getWebpackConfig(false), legacyEntryVars);
-const webpackDarkConfig = injectLessVariables(getWebpackConfig(false), legacyEntryVars);
-const webpackCompactConfig = injectLessVariables(getWebpackConfig(false), legacyEntryVars);
-const webpackVariableConfig = injectLessVariables(getWebpackConfig(false), {
-  'root-entry-name': 'variable',
-});
+const webpackConfig = getWebpackConfig(false);
 
 if (process.env.RUN_ENV === 'PRODUCTION') {
   webpackConfig.forEach(config => {
@@ -135,15 +58,6 @@ if (process.env.RUN_ENV === 'PRODUCTION') {
       );
     }
   });
-
-  processWebpackThemeConfig(webpackDarkConfig, 'dark', darkVars);
-  processWebpackThemeConfig(webpackCompactConfig, 'compact', compactVars);
-  processWebpackThemeConfig(webpackVariableConfig, 'variable', {});
 }
 
-module.exports = [
-  ...webpackConfig,
-  ...webpackDarkConfig,
-  ...webpackCompactConfig,
-  ...webpackVariableConfig,
-];
+module.exports = [...webpackConfig];
