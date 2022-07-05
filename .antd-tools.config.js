8dfc0ea97653f4e521133c40b5d7f8dad0400ead
MadCcc <1075746765@qq.com>
2022-07-05 19:02:23 +0800

    chore: copy reset.css to es

--- .antd-tools.config.js
+++ .antd-tools.config.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+function finalizeCompile() {
+  if (fs.existsSync(path.join(__dirname, './es'))) {
+    // Build less entry file: dist/antd.less
+    fs.copyFileSync(
+      path.join(process.cwd(), 'components', 'style', 'reset.css'),
+      path.join(process.cwd(), 'es', 'style', 'reset.css'),
+    );
+  }
+}
+
 function finalizeDist() {
   if (fs.existsSync(path.join(__dirname, './dist'))) {
     // Build less entry file: dist/antd.less
@@ -12,6 +22,9 @@ function finalizeDist() {
 }
 
 module.exports = {
+  compile: {
+    finalize: finalizeCompile,
+  },
   dist: {
     finalize: finalizeDist,
   },
