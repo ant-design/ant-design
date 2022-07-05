e81fd0418c1977e0dbf14186d480122103fca0d8
MadCcc <1075746765@qq.com>
2022-07-05 18:42:24 +0800

    chore: copy reset.css

--- .antd-tools.config.js
+++ .antd-tools.config.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+function finalizeDist() {
+  if (fs.existsSync(path.join(__dirname, './dist'))) {
+    // Build less entry file: dist/antd.less
+    fs.copyFileSync(
+      path.join(process.cwd(), 'components', 'style', 'reset.css'),
+      path.join(process.cwd(), 'dist', 'reset.css'),
+    );
+  }
+}
+
 module.exports = {
+  dist: {
+    finalize: finalizeDist,
+  },
   bail: true,
 };
--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -6,7 +6,8 @@ $('dist')
   .hasFile('antd-with-locales.js')
   .hasFile('antd-with-locales.min.js')
   .hasFile('antd.js')
-  .hasFile('antd.min.js');
+  .hasFile('antd.min.js')
+  .hasFile('reset.css');
 
 // eslint-disable-next-line no-console
 console.log(chalk.green('✨ `dist` directory is valid.'));
