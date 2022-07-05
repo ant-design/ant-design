f643de20d3ad94132d16534fa332ea5c959f335c
MadCcc <1075746765@qq.com>
2022-07-05 11:11:16 +0800

    chore: update dist dekko

--- tests/dekko/dist.test.js
+++ tests/dekko/dist.test.js
@@ -6,8 +6,7 @@ $('dist')
   .hasFile('antd-with-locales.js')
   .hasFile('antd-with-locales.min.js')
   .hasFile('antd.js')
-  .hasFile('antd.min.js')
-  .hasFile('dark-theme.js');
+  .hasFile('antd.min.js');
 
 // eslint-disable-next-line no-console
 console.log(chalk.green('✨ `dist` directory is valid.'));
