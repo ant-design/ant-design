dbbf1e50a2ce237269718c7df8b104061c79638d
二货机器人 <smith3816@gmail.com>
2022-07-07 23:41:20 +0800

    chore: rm useless warning in alpha (#36419)
    * chore: rm useless warning in alpha
    
    * chore: no need for test

--- components/_util/alphaWarning.tsx
+++ components/_util/alphaWarning.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 import Notification from '../notification';
 
 // Alpha version warning, should remove when published
-if (process.env.NODE_ENV !== 'production' && canUseDom()) {
+/* istanbul ignore next */
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test' && canUseDom()) {
   const icon: React.ReactNode = (
     <img
       alt="antd"
