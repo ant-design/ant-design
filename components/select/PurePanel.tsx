2bf43fbdfa2dea174e8ee12964069786687935c1
zombiej <smith3816@gmail.com>
2022-07-07 14:50:16 +0800

    feat: support select pure render

---
+++ components/select/PurePanel.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { ConfigContext } from '../config-provider';
+
+interface BaseProps {
+  prefixCls?: string;
+  style?: React.CSSProperties;
+}
+
+/* istanbul ignore next */
+export default function genPurePanel<ComponentProps extends BaseProps>(Component: any) {
+  return function PurePanel(props: ComponentProps) {
+    const { prefixCls: customizePrefixCls, style } = props;
+    const holderRef = React.useRef<HTMLDivElement>(null);
+    const [popupHeight, setPopupHeight] = React.useState(0);
+
+    const { getPrefixCls } = React.useContext(ConfigContext);
+    const prefixCls = getPrefixCls('select', customizePrefixCls);
+
+    React.useEffect(() => {
+      if (typeof ResizeObserver !== 'undefined') {
+        const resizeObserver = new ResizeObserver(entries => {
+          const element: HTMLDivElement = entries[0].target as any;
+          setPopupHeight(element.offsetHeight + 8);
+        });
+
+        const interval = setInterval(() => {
+          const popup = holderRef.current?.querySelector(`.${prefixCls}-dropdown`);
+
+          if (popup) {
+            clearInterval(interval);
+            resizeObserver.observe(popup);
+          }
+        }, 10);
+
+        return () => {
+          clearInterval(interval);
+          resizeObserver.disconnect();
+        };
+      }
+    }, []);
+
+    return (
+      <div ref={holderRef} style={{ paddingBottom: popupHeight }}>
+        <Component
+          {...props}
+          style={{
+            ...style,
+            margin: 0,
+          }}
+          open
+          getPopupContainer={() => holderRef.current!}
+        />
+      </div>
+    );
+  };
+}
---
+++ components/select/demo/render-panel.md
@@ -0,0 +1,37 @@
+---
+order: 999
+title:
+  zh-CN: _InternalPanelDoNotUseOrYouWillBeFired
+  en-US: _InternalPanelDoNotUseOrYouWillBeFired
+debug: true
+---
+
+## zh-CN
+
+调试用组件，请勿直接使用。
+
+## en-US
+
+Debug usage. Do not use in your production.
+
+```tsx
+import { Select } from 'antd';
+import React from 'react';
+
+const { _InternalPanelDoNotUseOrYouWillBeFired: InternalSelect } = Select;
+
+const App: React.FC = () => (
+  <InternalSelect
+    defaultValue="lucy"
+    style={{ width: 120 }}
+    options={[
+      { label: 'Jack', value: 'jack' },
+      { label: 'Lucy', value: 'lucy' },
+      { label: 'Disabled', value: 'disabled' },
+      { label: 'Bamboo', value: 'bamboo' },
+    ]}
+  />
+);
+
+export default App;
+```
