ce603d0332532e2abe3ae9d078d5e31ef8200aad
zombiej <smith3816@gmail.com>
2022-07-07 22:24:18 +0800

    chore: no need for test

--- components/_util/alphaWarning.tsx
+++ components/_util/alphaWarning.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Notification from '../notification';
 
 // Alpha version warning, should remove when published
+/* istanbul ignore next */
 if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test' && canUseDom()) {
   const icon: React.ReactNode = (
     <img
