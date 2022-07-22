12672e8d76f05336063d8df1fbc0772c24341a26
zhangtianyuan <tianyuan233.zhang@gmail.com>
2022-07-22 08:57:29 +0800

    chore: add blank line

--- components/tree/Tree.tsx
+++ components/tree/Tree.tsx
@@ -96,6 +96,7 @@ export interface AntTreeNodeDropEvent {
 export type TreeNodeNormal = DataNode;
 
 type DraggableFn = (node: DataNode) => boolean;
+
 interface DraggableConfig {
   icon?: React.ReactNode | false;
   nodeDraggable?: DraggableFn;
