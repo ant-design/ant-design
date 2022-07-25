d719a21cc0c51e802d8db4a5e794736ef64703bb
zombiej <smith3816@gmail.com>
2022-07-25 16:50:57 +0800

    test: drawer demo extends test

--- components/drawer/__tests__/demo-extend.test.tsx
+++ components/drawer/__tests__/demo-extend.test.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { extendTest } from '../../../tests/shared/demoTest';
 
-// jest.mock('rc-drawer', () => {
-//   const Drawer = jest.requireActual('rc-drawer');
-//   const MockDrawer = Drawer.default;
-//   return (props: any) => {
-//     const newProps = {
-//       ...props,
-//       open: true,
-//     };
-//     return <MockDrawer {...newProps} />;
-//   };
-// });
+jest.mock('rc-drawer', () => {
+  const Drawer = jest.requireActual('rc-drawer');
+  const MockDrawer = Drawer.default;
+  return (props: any) => {
+    const newProps = {
+      ...props,
+      open: true,
+    };
+    return <MockDrawer {...newProps} />;
+  };
+});
 
 extendTest('drawer', {
   testingLib: true,
