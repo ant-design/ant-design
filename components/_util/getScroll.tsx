b785e34bbf3bca079c4da5d76496046db85ad64f
Zheeeng <hi@zheeeng.me>
2022-07-26 12:44:25 +0800

    fix: for mock scenario to passing tests

--- components/_util/getScroll.tsx
+++ components/_util/getScroll.tsx
@@ -17,7 +17,15 @@ export default function getScroll(
     result = target.documentElement[method];
   } else if (target instanceof HTMLElement) {
     result = target[method];
+  } else if (target) {
+    // According to the type inference, the `target` is `never` type.
+    // Since we configured the loose mode type checking, and supports mocking the target with such shape below::
+    //    `{ documentElement: { scrollLeft: 200, scrollTop: 400 } }`,
+    //    the program may falls into this branch.
+    // Check the corresponding tests for details. Don't sure what is the real scenario this happens.
+    result = target[method];
   }
+
   if (target && !isWindow(target) && typeof result !== 'number') {
     result = (target.ownerDocument ?? target).documentElement?.[method];
   }
