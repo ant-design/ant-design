d285d2535e6bd1e9b9afd1a1863d6e2840e6581f
陈帅 <qixian.cs@outlook.com>
2022-07-29 18:36:03 +0800

    Update components/menu/style/index.tsx
    Co-authored-by: MadCcc <1075746765@qq.com>

--- components/menu/style/index.tsx
+++ components/menu/style/index.tsx
@@ -405,8 +405,8 @@ export default (prefixCls: string, injectStyle: boolean): UseComponentStyleResul
       // Menu Token
       const menuToken = mergeToken<MenuToken>(token, {
         menuItemHeight: controlHeightLG,
-        menuItemPaddingInline: 16,
-        menuItemMarginInline: 4,
+        menuItemPaddingInline: token.margin
+        menuItemMarginInline: token.marginXXS,
         menuArrowSize,
         menuHorizontalHeight: controlHeightLG * 1.15,
         menuArrowOffset: `${menuArrowSize * 0.25}px`,
