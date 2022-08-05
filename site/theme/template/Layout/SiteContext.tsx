73e541911bd720fb32a078687e38c044911ef91c
lijianan <574980606@qq.com>
2022-08-05 13:45:46 +0800

    refactor: use typescript refactor javascript (#36907)
    * refactor: rewrite header by FunctionComponent
    
    * refactor: rewrite footer by FunctionComponent
    
    * fix: strong type
    
    * refactor: rewrite IconDisplay & PicSearcher & Category by FunctionComponent
    
    * style: formatting
    
    * pref: add useCallback/useMemo
    
    * refactor: use typescript refactor javascript
    
    * fix: remove string
    
    * fix: fix eslint

--- site/theme/template/Layout/SiteContext.tsx
+++ site/theme/template/Layout/SiteContext.tsx
@@ -4,6 +4,9 @@ import type { DirectionType } from 'antd/es/config-provider';
 export interface SiteContextProps {
   isMobile: boolean;
   direction: DirectionType;
+  theme?: string;
+  setTheme?: (theme: string, persist?: boolean) => void;
+  setIframeTheme?: (iframeNode: HTMLIFrameElement, theme: string) => void;
 }
 
 const SiteContext = React.createContext<SiteContextProps>({
