c09ab9af60f8f4f717816590897a4295199316d4
二货机器人 <smith3816@gmail.com>
2022-08-08 14:55:40 +0800

    chore: clean up

--- components/tabs/hooks/useAnmiateConfig.ts
+++ components/tabs/hooks/useAnmiateConfig.ts
@@ -41,7 +41,6 @@ export default function useAnimateConfig(
       motionName: getTransitionName(prefixCls, 'switch'),
     };
   }
-  console.log('>>>>>', animated, mergedAnimated);
 
   return mergedAnimated;
 }
