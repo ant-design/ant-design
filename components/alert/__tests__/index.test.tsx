b309639725fad9e5d998d6bca2e68a960879c912
ian.xufeng <ian.xufeng@shopee.com>
2022-08-08 18:52:22 +0800

    Alert(test): remove useless descible for perventing nesting

--- components/alert/__tests__/index.test.tsx
+++ components/alert/__tests__/index.test.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import Alert from '..';
 import accessibilityTest from '../../../tests/shared/accessibilityTest';
 import rtlTest from '../../../tests/shared/rtlTest';
-import { fireEvent, render, sleep, act, screen } from '../../../tests/utils';
+import { render, act, screen } from '../../../tests/utils';
 import Button from '../../button';
 import Popconfirm from '../../popconfirm';
 import Tooltip from '../../tooltip';
@@ -42,41 +42,37 @@ describe('Alert', () => {
     expect(onClose).toBeCalledTimes(1);
   });
 
-  describe('action of Alert', () => {
-    it('custom action', () => {
-      const { container } = render(
-        <Alert
-          message="Success Tips"
-          type="success"
-          showIcon
-          action={
-            <Button size="small" type="text">
-              UNDO
-            </Button>
-          }
-          closable
-        />,
-      );
-      expect(container.firstChild).toMatchSnapshot();
-    });
+  it('custom action', () => {
+    const { container } = render(
+      <Alert
+        message="Success Tips"
+        type="success"
+        showIcon
+        action={
+          <Button size="small" type="text">
+            UNDO
+          </Button>
+        }
+        closable
+      />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 
-  describe('data and aria props', () => {
-    it('should sets data attributes on alert when pass attributes to props', () => {
-      render(
-        <Alert data-test="test-id" data-id="12345" aria-describedby="some-label" message={null} />,
-      );
-      const alert = screen.getByRole('alert');
-      expect(alert).toHaveAttribute('data-test', 'test-id');
-      expect(alert).toHaveAttribute('data-id', '12345');
-      expect(alert).toHaveAttribute('aria-describedby', 'some-label');
-    });
+  it('should sets data attributes on alert when pass attributes to props', () => {
+    render(
+      <Alert data-test="test-id" data-id="12345" aria-describedby="some-label" message={null} />,
+    );
+    const alert = screen.getByRole('alert');
+    expect(alert).toHaveAttribute('data-test', 'test-id');
+    expect(alert).toHaveAttribute('data-id', '12345');
+    expect(alert).toHaveAttribute('aria-describedby', 'some-label');
+  });
 
-    it('sets role attribute on input', () => {
-      render(<Alert role="status" message={null} />);
+  it('sets role attribute on input', () => {
+    render(<Alert role="status" message={null} />);
 
-      expect(screen.getByRole('status')).toBeInTheDocument();
-    });
+    expect(screen.getByRole('status')).toBeInTheDocument();
   });
 
   it('should show error as ErrorBoundary when children have error', () => {
