7dc38e6f96f7a4dfc96c3ec2be4d85dcf0a13012
lijianan <574980606@qq.com>
2022-08-09 21:44:39 +0800

    fix: eslint error fix

--- components/locale/az_AZ.tsx
+++ components/locale/az_AZ.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 import Pagination from 'rc-pagination/lib/locale/az_AZ';
 import Calendar from '../calendar/locale/az_AZ';
 import DatePicker from '../date-picker/locale/az_AZ';
