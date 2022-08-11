4c0e625b53340b6355f829ce1771d37962712ae8
foryuki <foryuki@outlook.com>
2022-08-11 08:57:52 +0800

    chore: remove console

--- components/dropdown/dropdown.tsx
+++ components/dropdown/dropdown.tsx
@@ -123,7 +123,6 @@ const Dropdown: DropdownInterface = props => {
   } = props;
 
   const prefixCls = getPrefixCls('dropdown', customizePrefixCls);
-  console.log(children, 'children');
   const child = React.Children.only(children) as React.ReactElement<any>;
 
   const dropdownTrigger = cloneElement(child, {
