969925653cd13964f7a1816691b2ec0c7b8a41e1
Yuki Zhang <foryuki@outlook.com>
2022-08-15 17:47:10 +0800

    test: move test cases to testing-library for DropDown (#37003)
    * temp
    
    * fix: mock esmodule default for Dropdown
    
    * test: move test cases to testing-library for DropDown
    
    * chore: remove console
    
    * fix: snapshot test
    
    * refactor: jest mock improvement

--- components/dropdown/__tests__/index.test.tsx
+++ components/dropdown/__tests__/index.test.tsx
@@ -1,44 +1,61 @@
-import { mount } from 'enzyme';
 import React from 'react';
+import type { TriggerProps } from 'rc-trigger';
 import Dropdown from '..';
+import type { DropDownProps } from '..';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import { act, fireEvent, render, sleep } from '../../../tests/utils';
 import Menu from '../../menu';
 
+let triggerProps: TriggerProps;
+
+jest.mock('rc-trigger', () => {
+  let Trigger = jest.requireActual('rc-trigger/lib/mock');
+  Trigger = Trigger.default || Trigger;
+  const h: typeof React = jest.requireActual('react');
+
+  return {
+    default: h.forwardRef<unknown, TriggerProps>((props, ref) => {
+      triggerProps = props;
+      return h.createElement(Trigger, { ref, ...props });
+    }),
+    __esModule: true,
+  };
+});
+
 describe('Dropdown', () => {
   mountTest(() => (
-    <Dropdown menu={<Menu />}>
+    <Dropdown overlay={<Menu />}>
       <span />
     </Dropdown>
   ));
 
   rtlTest(() => (
-    <Dropdown menu={<Menu />}>
+    <Dropdown overlay={<Menu />}>
       <span />
     </Dropdown>
   ));
 
   it('overlay is function and has custom transitionName', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <Dropdown overlay={() => <div>menu</div>} transitionName="move-up" visible>
         <button type="button">button</button>
       </Dropdown>,
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(Array.from(asFragment().childNodes)).toMatchSnapshot();
   });
 
   it('overlay is string', () => {
-    const wrapper = mount(
-      <Dropdown overlay="string" visible>
+    const { asFragment } = render(
+      <Dropdown overlay={'string' as any} visible>
         <button type="button">button</button>
       </Dropdown>,
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(Array.from(asFragment().childNodes)).toMatchSnapshot();
   });
 
   it('support Menu expandIcon', async () => {
-    const props = {
+    const props: DropDownProps = {
       overlay: (
         <Menu expandIcon={<span id="customExpandIcon" />}>
           <Menu.Item key="1">foo</Menu.Item>
@@ -51,23 +68,23 @@ describe('Dropdown', () => {
       getPopupContainer: node => node,
     };
 
-    const wrapper = mount(
+    const { container } = render(
       <Dropdown {...props}>
         <button type="button">button</button>
       </Dropdown>,
     );
     await sleep(500);
-    expect(wrapper.find(Dropdown).find('#customExpandIcon').length).toBe(1);
+    expect(container.querySelectorAll('#customExpandIcon').length).toBe(1);
   });
 
   it('should warn if use topCenter or bottomCenter', () => {
     const error = jest.spyOn(console, 'error');
-    mount(
+    render(
       <div>
-        <Dropdown overlay="123" placement="bottomCenter">
+        <Dropdown overlay={'123' as any} placement="bottomCenter">
           <button type="button">bottomCenter</button>
         </Dropdown>
-        <Dropdown overlay="123" placement="topCenter">
+        <Dropdown overlay={'123' as any} placement="topCenter">
           <button type="button">topCenter</button>
         </Dropdown>
       </div>,
@@ -82,13 +99,13 @@ describe('Dropdown', () => {
 
   // zombieJ: when replaced with react test lib, it may be mock fully content
   it('dropdown should support auto adjust placement', () => {
-    const wrapper = mount(
+    render(
       <Dropdown overlay={<div>menu</div>} visible>
         <button type="button">button</button>
       </Dropdown>,
     );
 
-    expect(wrapper.find('Trigger').prop('builtinPlacements')).toEqual(
+    expect(triggerProps.builtinPlacements).toEqual(
       expect.objectContaining({
         bottomLeft: expect.objectContaining({
           overflow: {
@@ -104,7 +121,7 @@ describe('Dropdown', () => {
     jest.useFakeTimers();
     const { container } = render(
       <Dropdown
-        trigger="click"
+        trigger={['click']}
         overlay={
           <Menu
             items={[
@@ -127,13 +144,13 @@ describe('Dropdown', () => {
     );
 
     // Open
-    fireEvent.click(container.querySelector('a'));
+    fireEvent.click(container.querySelector('a')!);
     act(() => {
       jest.runAllTimers();
     });
 
     // Close
-    fireEvent.click(container.querySelector('.ant-dropdown-menu-item'));
+    fireEvent.click(container.querySelector('.ant-dropdown-menu-item')!);
 
     // Force Motion move on
     for (let i = 0; i < 10; i += 1) {
@@ -143,7 +160,7 @@ describe('Dropdown', () => {
     }
 
     // Motion End
-    fireEvent.animationEnd(container.querySelector('.ant-slide-up-leave-active'));
+    fireEvent.animationEnd(container.querySelector('.ant-slide-up-leave-active')!);
 
     expect(container.querySelector('.ant-dropdown-hidden')).toBeTruthy();
 
