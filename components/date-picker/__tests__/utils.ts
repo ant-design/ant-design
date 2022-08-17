a0027978efb1f881154cd0ee88285fb19bc4ade6
“konghai” <574980606@qq.com>
2022-08-17 19:14:27 +0800

    test: migrate part of DatePicker tests

--- components/date-picker/__tests__/utils.ts
+++ components/date-picker/__tests__/utils.ts
@@ -2,66 +2,41 @@ import { fireEvent } from '../../../tests/utils';
 import type { render } from '../../../tests/utils';
 
 export function openPanel(wrapper: ReturnType<typeof render>) {
-  const target = wrapper.container?.querySelector('.ant-calendar-picker-input');
-  if (target) {
-    fireEvent.click(target);
-  }
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-picker-input')!);
 }
 
 export function clearInput(wrapper: ReturnType<typeof render>) {
-  const target = wrapper.container?.querySelector('.ant-calendar-picker-clear');
-  if (target) {
-    fireEvent.click(target);
-  }
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-picker-clear')!);
 }
 
 export function nextYear(wrapper: ReturnType<typeof render>) {
-  const target = wrapper.container?.querySelector('.ant-calendar-next-year-btn');
-  if (target) {
-    fireEvent.click(target);
-  }
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-next-year-btn')!);
 }
 
 export function nextMonth(wrapper: ReturnType<typeof render>) {
-  const target = wrapper.container?.querySelector('.ant-calendar-next-month-btn');
-  if (target) {
-    fireEvent.click(target);
-  }
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-next-month-btn')!);
 }
 
 export function openPicker(wrapper: ReturnType<typeof render>, index = 0) {
-  const target = wrapper.container?.querySelectorAll('input')[index];
-  if (target) {
-    fireEvent.mouseDown(target);
-    fireEvent.focus(target);
-  }
+  fireEvent.mouseDown(wrapper.container?.querySelectorAll('input')?.[index]!);
+  fireEvent.focus(wrapper.container?.querySelectorAll('input')?.[index]!);
 }
 
 export function closePicker(wrapper: ReturnType<typeof render>, index = 0) {
-  const target = wrapper.container?.querySelectorAll('input')[index];
-  if (target) {
-    fireEvent.blur(target);
-  }
+  fireEvent.blur(wrapper.container?.querySelectorAll('input')[index]!);
 }
 
 export function selectCell(wrapper: ReturnType<typeof render>, text: string | number, index = 0) {
   let matchCell: HTMLTableCellElement | null = null;
-  const target = wrapper.container?.querySelectorAll('table')?.[index];
-  if (target) {
-    const tds = target?.querySelectorAll('td');
-    tds.forEach(td => {
-      if (
-        td.querySelector('div')?.innerHTML === String(text) &&
-        td.className.includes('-in-view')
-      ) {
-        matchCell = td;
-        fireEvent.click(td);
-      }
-    });
-    if (!matchCell) {
-      throw new Error('Cell not match in picker panel.');
+  const tds = wrapper.container?.querySelectorAll('table')?.[index]?.querySelectorAll('td');
+  tds.forEach(td => {
+    if (td.querySelector('div')?.innerHTML === String(text) && td.className.includes('-in-view')) {
+      matchCell = td;
+      fireEvent.click(td);
     }
+  });
+  if (!matchCell) {
+    throw new Error('Cell not match in picker panel.');
   }
-
   return matchCell;
 }
