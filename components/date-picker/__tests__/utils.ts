ffa87e0e72c3c1bfa7a338eb1a2e4cea8d1e3e5e
“konghai” <574980606@qq.com>
2022-08-17 16:30:18 +0800

    test: migrate part of DatePicker tests

--- components/date-picker/__tests__/utils.ts
+++ components/date-picker/__tests__/utils.ts
@@ -2,28 +2,28 @@ import { fireEvent } from '../../../tests/utils';
 import type { render } from '../../../tests/utils';
 
 export function openPanel(wrapper: ReturnType<typeof render>) {
-  fireEvent.click(wrapper.container.querySelector('.ant-calendar-picker-input')!);
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-picker-input')!);
 }
 
 export function clearInput(wrapper: ReturnType<typeof render>) {
-  fireEvent.click(wrapper.container.querySelector('.ant-calendar-picker-clear')!);
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-picker-clear')!);
 }
 
 export function nextYear(wrapper: ReturnType<typeof render>) {
-  fireEvent.click(wrapper.container.querySelector('.ant-calendar-next-year-btn')!);
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-next-year-btn')!);
 }
 
 export function nextMonth(wrapper: ReturnType<typeof render>) {
-  fireEvent.click(wrapper.container.querySelector('.ant-calendar-next-month-btn')!);
+  fireEvent.click(wrapper.container?.querySelector('.ant-calendar-next-month-btn')!);
 }
 
 export function openPicker(wrapper: ReturnType<typeof render>, index = 0) {
-  fireEvent.mouseDown(wrapper.container.querySelectorAll('input')[index]!);
-  fireEvent.focus(wrapper.container.querySelectorAll('input')[index]!);
+  fireEvent.mouseDown(wrapper.container?.querySelectorAll('input')[index]!);
+  fireEvent.focus(wrapper.container?.querySelectorAll('input')[index]!);
 }
 
 export function closePicker(wrapper: ReturnType<typeof render>, index = 0) {
-  fireEvent.blur(wrapper.container.querySelectorAll('input')[index]!);
+  fireEvent.blur(wrapper.container?.querySelectorAll('input')[index]!);
 }
 
 export function selectCell(wrapper: ReturnType<typeof render>, text: string | number, index = 0) {
