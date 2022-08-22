c0f5e8c7c73ec4ef0bd42ebaf42a2cc91771fdad
二货机器人 <smith3816@gmail.com>
2022-08-22 18:12:06 +0800

    test: Update ser logic

--- tests/setupAfterEnv.ts
+++ tests/setupAfterEnv.ts
@@ -1,4 +1,38 @@
 import { toHaveNoViolations } from 'jest-axe';
 import '@testing-library/jest-dom';
+import format, { plugins } from 'pretty-format';
+
+/**
+ * React 17 & 18 will have different behavior in some special cases:
+ *
+ * React 17:
+ *
+ * ```html
+ * <span> Hello World </span>
+ * ```
+ *
+ * React 18:
+ *
+ * ```html
+ * <span> Hello World </span>
+ * ```
+ *
+ * These diff is nothing important in front end but will break in snapshot diff.
+ */
+expect.addSnapshotSerializer({
+  test: element => element instanceof HTMLElement,
+  print: element => {
+    const htmlContent = format(element, {
+      plugins: [plugins.DOMCollection, plugins.DOMElement],
+    });
+
+    const filtered = htmlContent
+      .split(/[\n\r]+/)
+      .filter(line => line.trim())
+      .join('\n');
+
+    return filtered;
+  },
+});
 
 expect.extend(toHaveNoViolations);
