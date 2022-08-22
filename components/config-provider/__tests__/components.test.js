db0344c85fd1c87a0fde2f386deea25009546d6e
lijianan <574980606@qq.com>
2022-08-22 22:54:38 +0800

    test: migrate part of ConfigProvider tests (#37142)
    * test: migrate part of ConfigProvider tests
    
    * test: migrate part of ConfigProvider tests
    
    * test: update snap
    
    * test: update snap
    
    * test: migrate part of ConfigProvider tests
    
    * fix: commented error case
    
    * chore: update snapshot name
    
    * test: csp test case
    
    * test: revert test case
    
    * test: Update snapshot
    
    * test: Update ser logic
    
    * test: more cov
    
    * test: cascader snapshit
    
    * test: update match
    
    * test: more detact
    
    * test: more handler
    
    * chore: Update snapshot
    
    * test: Table test case
    
    Co-authored-by: 二货机器人 <smith3816@gmail.com>

--- components/config-provider/__tests__/components.test.js
+++ components/config-provider/__tests__/components.test.js
@@ -1,4 +1,3 @@
-import { render } from 'enzyme';
 import moment from 'moment';
 import React from 'react';
 import ConfigProvider from '..';
@@ -54,72 +53,71 @@ import Transfer from '../../transfer';
 import Tree from '../../tree';
 import TreeSelect from '../../tree-select';
 import Upload from '../../upload';
+import { render } from '../../../tests/utils';
 
 jest.mock('rc-util/lib/Portal');
 
 describe('ConfigProvider', () => {
   describe('components', () => {
     function testPair(name, renderComponent) {
+      const isArray = ['Menu', 'TimePicker', 'Tooltip'].includes(name);
       describe(`${name}`, () => {
         // normal
         it('normal', () => {
-          expect(render(renderComponent({}))).toMatchSnapshot();
+          const { container } = render(renderComponent({}));
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         // prefixCls
         it('prefixCls', () => {
-          expect(render(renderComponent({ prefixCls: `prefix-${name}` }))).toMatchSnapshot();
+          const { container } = render(renderComponent({ prefixCls: `prefix-${name}` }));
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         // configProvider
         it('configProvider', () => {
-          expect(
-            render(
-              <ConfigProvider pageHeader={{ ghost: false }} prefixCls="config">
-                {renderComponent({})}
-              </ConfigProvider>,
-            ),
-          ).toMatchSnapshot();
+          const { container } = render(
+            <ConfigProvider pageHeader={{ ghost: false }} prefixCls="config">
+              {renderComponent({})}
+            </ConfigProvider>,
+          );
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         it('configProvider componentSize large', () => {
-          expect(
-            render(
-              <ConfigProvider componentSize="large" prefixCls="config">
-                {renderComponent({})}
-              </ConfigProvider>,
-            ),
-          ).toMatchSnapshot();
+          const { container } = render(
+            <ConfigProvider componentSize="large" prefixCls="config">
+              {renderComponent({})}
+            </ConfigProvider>,
+          );
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         it('configProvider componentSize middle', () => {
-          expect(
-            render(
-              <ConfigProvider componentSize="middle" prefixCls="config">
-                {renderComponent({})}
-              </ConfigProvider>,
-            ),
-          ).toMatchSnapshot();
+          const { container } = render(
+            <ConfigProvider componentSize="middle" prefixCls="config">
+              {renderComponent({})}
+            </ConfigProvider>,
+          );
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         it('configProvider componentDisabled', () => {
-          expect(
-            render(
-              <ConfigProvider componentDisabled prefixCls="config">
-                {renderComponent({})}
-              </ConfigProvider>,
-            ),
-          ).toMatchSnapshot();
+          const { container } = render(
+            <ConfigProvider componentDisabled prefixCls="config">
+              {renderComponent({})}
+            </ConfigProvider>,
+          );
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
 
         it('configProvider virtual and dropdownMatchSelectWidth', () => {
-          expect(
-            render(
-              <ConfigProvider virtual={false} dropdownMatchSelectWidth={false}>
-                {renderComponent({})}
-              </ConfigProvider>,
-            ),
-          ).toMatchSnapshot();
+          const { container } = render(
+            <ConfigProvider virtual={false} dropdownMatchSelectWidth={false}>
+              {renderComponent({})}
+            </ConfigProvider>,
+          );
+          expect(isArray ? Array.from(container.children) : container.firstChild).toMatchSnapshot();
         });
       });
     }
@@ -147,9 +145,7 @@ describe('ConfigProvider', () => {
 
     // Badge
     testPair('Badge', props => {
-      const newProps = {
-        ...props,
-      };
+      const newProps = { ...props };
 
       // Hook for additional `scrollNumberPrefixCls` prop
       if (props.prefixCls) {
@@ -161,7 +157,6 @@ describe('ConfigProvider', () => {
           <Badge {...newProps} count={5}>
             <span />
           </Badge>
-
           <Badge {...newProps} dot>
             <span />
           </Badge>
@@ -230,7 +225,7 @@ describe('ConfigProvider', () => {
     // Collapse
     testPair('Collapse', props => (
       <Collapse {...props}>
-        <Collapse.Panel header="Bamboo">
+        <Collapse.Panel key="Collapse" header="Bamboo">
           <p>Light</p>
         </Collapse.Panel>
       </Collapse>
@@ -504,19 +499,11 @@ describe('ConfigProvider', () => {
           title: 'Name',
           dataIndex: 'name',
           filters: [
-            {
-              text: 'Joe',
-              value: 'Joe',
-            },
+            { text: 'Joe', value: 'Joe' },
             {
               text: 'Submenu',
               value: 'Submenu',
-              children: [
-                {
-                  text: 'Green',
-                  value: 'Green',
-                },
-              ],
+              children: [{ text: 'Green', value: 'Green' }],
             },
           ],
           filterDropdownVisible: true,
@@ -576,7 +563,6 @@ describe('ConfigProvider', () => {
         <Tree {...props}>
           <Tree.TreeNode title="bamboo" />
         </Tree>
-
         <Tree.DirectoryTree {...props}>
           <Tree.TreeNode title="bamboo" />
         </Tree.DirectoryTree>
@@ -592,16 +578,7 @@ describe('ConfigProvider', () => {
 
     // Upload
     testPair('Upload', props => (
-      <Upload
-        {...props}
-        defaultFileList={[
-          {
-            uid: '1',
-            name: 'xxx.png',
-            status: 'done',
-          },
-        ]}
-      >
+      <Upload {...props} defaultFileList={[{ uid: '1', name: 'xxx.png', status: 'done' }]}>
         <span />
       </Upload>
     ));
--- tests/setupAfterEnv.ts
+++ tests/setupAfterEnv.ts
@@ -1,4 +1,43 @@
 import { toHaveNoViolations } from 'jest-axe';
 import '@testing-library/jest-dom';
+import format, { plugins } from 'pretty-format';
+
+/**
+ * React 17 & 18 will have different behavior in some special cases:
+ *
+ * React 17:
+ *
+ * ```html
+ * <span> Hello World </span>
+ * ```
+ *
+ * React 18:
+ *
+ * ```html
+ * <span> Hello World </span>
+ * ```
+ *
+ * These diff is nothing important in front end but will break in snapshot diff.
+ */
+expect.addSnapshotSerializer({
+  test: element =>
+    typeof HTMLElement !== 'undefined' &&
+    (element instanceof HTMLElement ||
+      element instanceof DocumentFragment ||
+      element instanceof HTMLCollection ||
+      (Array.isArray(element) && element[0] instanceof HTMLElement)),
+  print: element => {
+    const htmlContent = format(element, {
+      plugins: [plugins.DOMCollection, plugins.DOMElement],
+    });
+
+    const filtered = htmlContent
+      .split(/[\n\r]+/)
+      .filter(line => line.trim())
+      .join('\n');
+
+    return filtered;
+  },
+});
 
 expect.extend(toHaveNoViolations);
