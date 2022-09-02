c23a28014858e318cea185c88245200c4bb0ea96
“konghai” <574980606@qq.com>
2022-09-02 13:31:36 +0800

    test: BackTop js => ts

--- components/back-top/__tests__/index.test.tsx
+++ components/back-top/__tests__/index.test.tsx
@@ -10,14 +10,14 @@ describe('BackTop', () => {
 
   it('should scroll to top after click it', async () => {
     const { container } = render(<BackTop visibilityHeight={-1} />);
-    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((x, y) => {
+    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((_, y) => {
       window.scrollY = y;
       window.pageYOffset = y;
       document.documentElement.scrollTop = y;
     });
     window.scrollTo(0, 400);
     expect(document.documentElement.scrollTop).toBe(400);
-    fireEvent.click(container.querySelector('.ant-back-top'));
+    fireEvent.click(container.querySelector('.ant-back-top')!);
     await sleep(500);
     expect(document.documentElement.scrollTop).toBe(0);
     scrollToSpy.mockRestore();
@@ -26,13 +26,13 @@ describe('BackTop', () => {
   it('support onClick', async () => {
     const onClick = jest.fn();
     const { container } = render(<BackTop onClick={onClick} visibilityHeight={-1} />);
-    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((x, y) => {
+    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((_, y) => {
       window.scrollY = y;
       window.pageYOffset = y;
     });
     document.dispatchEvent(new Event('scroll'));
     window.scrollTo(0, 400);
-    fireEvent.click(container.querySelector('.ant-back-top'));
+    fireEvent.click(container.querySelector('.ant-back-top')!);
     expect(onClick).toHaveBeenCalled();
     scrollToSpy.mockRestore();
   });
@@ -40,9 +40,13 @@ describe('BackTop', () => {
   it('invalid target', async () => {
     const onClick = jest.fn();
     const { container } = render(
-      <BackTop onClick={onClick} visible target={() => ({ documentElement: {} })} />,
+      <BackTop
+        onClick={onClick}
+        visible
+        target={() => ({ documentElement: {} } as unknown as HTMLElement)}
+      />,
     );
-    fireEvent.click(container.querySelector('.ant-back-top'));
+    fireEvent.click(container.querySelector('.ant-back-top')!);
     expect(onClick).toHaveBeenCalled();
   });
 });
