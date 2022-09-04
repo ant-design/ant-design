5f5bea6faedc479a2ec478e399a97267524d4403
lijianan <574980606@qq.com>
2022-09-04 14:20:15 +0800

    fix: eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -81,7 +81,6 @@ module.exports = {
         'import/no-extraneous-dependencies': 0,
         'react/jsx-no-constructed-context-values': 0,
         'react/no-unstable-nested-components': 0,
-        'react/no-unknown-property': 0,
       },
     },
   ],
