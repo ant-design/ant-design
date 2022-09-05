37331b8f1bd9e2ce1a6278bc9453a606de039452
wangning <wangning4567@163.com>
2022-09-05 21:43:31 +0800

    test: update snap

--- components/card/__tests__/index.test.tsx
+++ components/card/__tests__/index.test.tsx
@@ -5,6 +5,7 @@ import { fireEvent, render } from '../../../tests/utils';
 import Button from '../../button/index';
 import Card from '../index';
 import '@testing-library/jest-dom';
+
 console.log('fireEvent');
 
 describe('Card', () => {
