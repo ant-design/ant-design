00b4b6c8188c808925b889014e06a587af0bc12b
黑雨 <wangning4567@163.com>
2022-09-06 17:32:10 +0800

    test: testing-lib

--- tests/shared/demoTest.tsx
+++ tests/shared/demoTest.tsx
@@ -7,6 +7,47 @@ import { TriggerMockContext } from './demoTestContext';
 
 require('isomorphic-fetch');
 
+type CheerIO = ReturnType<any>;
+type CheerIOElement = CheerIO[0];
+// We should avoid use it in 4.0. Reopen if can not handle this.
+const USE_REPLACEMENT = false;
+const testDist = process.env.LIB_DIR === 'dist';
+
+/**
+ * Rc component will generate id for aria usage. It's created as `test-uuid` when env === 'test'. Or
+ * `f7fa7a3c-a675-47bc-912e-0c45fb6a74d9`(randomly) when not test env. So we need hack of this to
+ * modify the `aria-controls`.
+ */
+function ariaConvert(wrapper: CheerIO) {
+  if (!testDist || !USE_REPLACEMENT) return wrapper;
+
+  const matches = new Map();
+
+  function process(entry: CheerIOElement) {
+    if (entry.type === 'text' || entry.type === 'comment') {
+      return;
+    }
+    const { attribs, children } = entry;
+    if (matches.has(entry)) return;
+    matches.set(entry, true);
+
+    // Change aria
+    if (attribs && attribs['aria-controls']) {
+      attribs['aria-controls'] = ''; // Remove all the aria to keep render sync in jest & jest node
+    }
+
+    // Loop children
+    if (!children) {
+      return;
+    }
+    (Array.isArray(children) ? children : [children]).forEach(process);
+  }
+
+  wrapper.each((_: any, entry: any) => process(entry));
+
+  return wrapper;
+}
+
 type Options = {
   skip?: boolean | string[];
   testingLib?: boolean;
@@ -21,6 +62,7 @@ function baseText(doInject: boolean, component: string, options: Options = {}) {
       testMethod = test.skip;
     }
     Date.now = jest.fn(() => new Date('2016-11-22').getTime());
+    jest.useFakeTimers().setSystemTime(new Date('2016-11-22'));
     // function doTest(name: string, openTrigger = false) {
     testMethod(
       doInject ? `renders ${file} extend context correctly` : `renders ${file} correctly`,
@@ -43,10 +85,12 @@ function baseText(doInject: boolean, component: string, options: Options = {}) {
         }
 
         const { container } = render(Demo);
+        ariaConvert(container);
         expect(container.firstChild).toMatchSnapshot();
         errSpy();
       },
     );
+    jest.useRealTimers();
   });
 }
 
