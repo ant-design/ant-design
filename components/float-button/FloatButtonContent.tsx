579da0fc7eb34143c59842b495f086711b566b5f
lijianan <574980606@qq.com>
2022-09-12 23:18:43 +0800

    feat: FloatButton

--- components/float-button/FloatButtonContent.tsx
+++ components/float-button/FloatButtonContent.tsx
@@ -5,18 +5,20 @@ import type { ContentProps } from './interface';
 
 const FloatButtonContent: React.FC<ContentProps> = props => {
   const { icon, description, prefixCls, shape, type, CSSMotionClassName } = props;
+  const defaultElement = (
+    <div className={`${prefixCls}-icon ${prefixCls}-${type}-icon`}>
+      <FileTextOutlined />
+    </div>
+  );
   return (
     <div className={classNames(CSSMotionClassName, `${prefixCls}-content ${prefixCls}-${shape}`)}>
-      {(icon || description) && (
+      {icon || description ? (
         <>
-          <div className={`${prefixCls}-icon ${prefixCls}-${type}-icon`}>{icon}</div>
+          {icon && <div className={`${prefixCls}-icon ${prefixCls}-${type}-icon`}>{icon}</div>}
           {description && <span>{description}</span>}
         </>
-      )}
-      {!icon && !description && (
-        <div className={`${prefixCls}-icon ${prefixCls}-${type}-icon`}>
-          <FileTextOutlined />
-        </div>
+      ) : (
+        defaultElement
       )}
     </div>
   );
