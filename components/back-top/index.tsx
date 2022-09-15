a38f8bd0fe76ca584c59cf9150eb7ed9ab181fbf
“konghai” <574980606@qq.com>
2022-09-15 11:56:58 +0800

    chore: add deprecated warning message

--- components/back-top/index.tsx
+++ components/back-top/index.tsx
@@ -10,6 +10,7 @@ import getScroll from '../_util/getScroll';
 import { cloneElement } from '../_util/reactNode';
 import scrollTo from '../_util/scrollTo';
 import { throttleByAnimationFrame } from '../_util/throttleByAnimationFrame';
+import warning from '../_util/warning';
 
 import useStyle from './style';
 
@@ -80,6 +81,10 @@ const BackTop: React.FC<BackTopProps> = props => {
     });
     handleScroll({ target: container });
   };
+
+  if (process.env.NODE_ENV !== 'production') {
+    warning(false, 'BackTop', '`BackTop` is deprecated, please use `FloatButton.BackTop` instead.');
+  }
 
   React.useEffect(() => {
     bindScrollEvent();
