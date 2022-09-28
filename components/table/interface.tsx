e06107b8d83b8f593398eda9c4fdda8361e86e79
zonghao.jiang <1587315093@qq.com>
2022-09-28 17:20:04 +0800

    feat:table自定义筛选增加关闭函数

--- components/table/interface.tsx
+++ components/table/interface.tsx
@@ -85,6 +85,7 @@ export interface FilterDropdownProps {
   clearFilters?: () => void;
   filters?: ColumnFilterItem[];
   visible: boolean;
+  closeDropDown: () => void;
 }
 
 export interface ColumnType<RecordType> extends Omit<RcColumnType<RecordType>, 'title'> {
