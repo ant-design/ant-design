678c50c8be87b03ea7bb388bda8dcaf83dbd40ad
0x219 <lzm0x219@gmail.com>
2022-10-01 16:22:10 +0800

    test: replace Popconfirm part test with testing lib

--- components/popconfirm/__tests__/index.test.tsx
+++ components/popconfirm/__tests__/index.test.tsx
@@ -1,10 +1,9 @@
-import { mount } from 'enzyme';
 import { spyElementPrototype } from 'rc-util/lib/test/domHook';
 import React from 'react';
 import Popconfirm from '..';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
-import { fireEvent, render, sleep, act } from '../../../tests/utils';
+import { render, fireEvent, act, waitFakeTimer } from '../../../tests/utils';
 import Button from '../../button';
 
 describe('Popconfirm', () => {
@@ -22,10 +21,20 @@ describe('Popconfirm', () => {
     });
   });
 
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+    jest.useRealTimers();
+    jest.runOnlyPendingTimers();
+  });
+
   it('should popup Popconfirm dialog', () => {
     const onOpenChange = jest.fn();
 
-    const wrapper = mount(
+    const wrapper = render(
       <Popconfirm
         title={<span className="popconfirm-test">Are you sure delete this task?</span>}
         okText="Yes"
@@ -38,94 +47,109 @@ describe('Popconfirm', () => {
       </Popconfirm>,
     );
 
-    const triggerNode = wrapper.find('span').at(0);
-    triggerNode.simulate('click');
+    const triggerNode = wrapper.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
     expect(onOpenChange).toHaveBeenLastCalledWith(true, undefined);
-    expect(wrapper.find('.popconfirm-test').length).toBe(1);
+    expect(wrapper.container.querySelectorAll('.popconfirm-test').length).toBe(1);
 
-    triggerNode.simulate('click');
+    fireEvent.click(triggerNode);
     expect(onOpenChange).toHaveBeenLastCalledWith(false, undefined);
   });
 
   it('should show overlay when trigger is clicked', async () => {
-    const ref = React.createRef();
-    const popconfirm = mount(
-      <Popconfirm ref={ref} title="code">
+    const popconfirm = render(
+      <Popconfirm title="code">
         <span>show me your code</span>
       </Popconfirm>,
     );
 
-    expect(ref.current.getPopupDomNode()).toBe(null);
+    expect(popconfirm.container.querySelector('.ant-popover')).toBe(null);
 
-    popconfirm.find('span').simulate('click');
-    await sleep(100);
+    const triggerNode = popconfirm.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
 
-    const popup = ref.current.getPopupDomNode();
-    expect(popup).not.toBe(null);
-    expect(popup.className).toContain('ant-popover-placement-top');
-    expect(popup.innerHTML).toMatchSnapshot();
-    expect(popup.innerHTML).toMatchSnapshot();
+    await waitFakeTimer(100);
+
+    expect(popconfirm.container.querySelector('.ant-popover')).not.toBeNull();
+    expect(popconfirm.container.querySelector('.ant-popover')?.className).toContain(
+      'ant-popover-placement-top',
+    );
+    expect(popconfirm.container.querySelector('.ant-popover')?.innerHTML).toMatchSnapshot();
+    expect(popconfirm.container.querySelector('.ant-popover')?.innerHTML).toMatchSnapshot();
   });
 
   it('shows content for render functions', async () => {
-    const makeRenderFunction = content => () => content;
-    const ref = React.createRef();
+    const makeRenderFunction = (content: string) => () => content;
 
-    const popconfirm = mount(
-      <Popconfirm ref={ref} title={makeRenderFunction('some-title')}>
+    const popconfirm = render(
+      <Popconfirm title={makeRenderFunction('some-title')}>
         <span>show me your code</span>
       </Popconfirm>,
     );
 
-    expect(ref.current.getPopupDomNode()).toBe(null);
+    expect(popconfirm.container.querySelector('.ant-popover')).toBe(null);
 
-    popconfirm.find('span').simulate('click');
-    await sleep(100);
+    const triggerNode = popconfirm.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
+    await waitFakeTimer(100);
 
-    const popup = ref.current.getPopupDomNode();
-    expect(popup).not.toBe(null);
-    expect(popup.innerHTML).toContain('some-title');
-    expect(popup.innerHTML).toMatchSnapshot();
+    expect(popconfirm.container.querySelector('.ant-popover')).not.toBe(null);
+    expect(popconfirm.container.querySelector('.ant-popover')?.innerHTML).toContain('some-title');
+    expect(popconfirm.container.querySelector('.ant-popover')?.innerHTML).toMatchSnapshot();
   });
 
   it('should be controlled by open', () => {
-    const ref = React.createRef();
     jest.useFakeTimers();
-    const popconfirm = mount(
-      <Popconfirm ref={ref} title="code">
+    const popconfirm = render(
+      <Popconfirm title="code">
         <span>show me your code</span>
       </Popconfirm>,
     );
-    expect(ref.current.getPopupDomNode()).toBeFalsy();
-    popconfirm.setProps({ open: true });
-    expect(ref.current.getPopupDomNode()).toBeTruthy();
-    expect(ref.current.getPopupDomNode().className).not.toContain('ant-popover-hidden');
-    popconfirm.setProps({ open: false });
-    popconfirm.update(); // https://github.com/enzymejs/enzyme/issues/2305
+
+    expect(popconfirm.container.querySelector('.ant-popover')).toBe(null);
+    popconfirm.rerender(
+      <Popconfirm title="code" open>
+        <span>show me your code</span>
+      </Popconfirm>,
+    );
+
+    expect(popconfirm.container.querySelector('.ant-popover')).not.toBe(null);
+    expect(popconfirm.container.querySelector('.ant-popover')?.className).not.toContain(
+      'ant-popover-hidden',
+    );
+
+    popconfirm.rerender(
+      <Popconfirm title="code" open={false}>
+        <span>show me your code</span>
+      </Popconfirm>,
+    );
     act(() => {
       jest.runAllTimers();
     });
-    expect(popconfirm.find('Trigger').props().popupVisible).toBe(false);
+    expect(popconfirm.container.querySelector('.ant-popover')).not.toBe(null);
     jest.useRealTimers();
   });
 
-  it('should trigger onConfirm and onCancel', () => {
+  it('should trigger onConfirm and onCancel', async () => {
     const confirm = jest.fn();
     const cancel = jest.fn();
     const onOpenChange = jest.fn();
-    const popconfirm = mount(
+    const popconfirm = render(
       <Popconfirm title="code" onConfirm={confirm} onCancel={cancel} onOpenChange={onOpenChange}>
         <span>show me your code</span>
       </Popconfirm>,
     );
-    const triggerNode = popconfirm.find('span').at(0);
-    triggerNode.simulate('click');
-    popconfirm.find('.ant-btn-primary').simulate('click');
+    const triggerNode = popconfirm.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
+    fireEvent.click(popconfirm.container.querySelector('.ant-btn-primary')!);
     expect(confirm).toHaveBeenCalled();
+    await waitFakeTimer();
     expect(onOpenChange).toHaveBeenLastCalledWith(false, eventObject);
-    triggerNode.simulate('click');
-    popconfirm.find('.ant-btn').at(0).simulate('click');
+
+    fireEvent.click(triggerNode);
+    fireEvent.click(popconfirm.container.querySelectorAll('.ant-btn')[0]);
     expect(cancel).toHaveBeenCalled();
+    await waitFakeTimer();
     expect(onOpenChange).toHaveBeenLastCalledWith(false, eventObject);
   });
 
@@ -135,36 +159,36 @@ describe('Popconfirm', () => {
         setTimeout(res, 300);
       });
     const onOpenChange = jest.fn();
-    const popconfirm = mount(
+    const popconfirm = render(
       <Popconfirm title="code" onConfirm={confirm} onOpenChange={onOpenChange}>
         <span>show me your code</span>
       </Popconfirm>,
     );
 
-    const triggerNode = popconfirm.find('span').at(0);
-    triggerNode.simulate('click');
+    const triggerNode = popconfirm.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
     expect(onOpenChange).toHaveBeenCalledTimes(1);
 
-    popconfirm.find('.ant-btn').at(0).simulate('click');
-    await sleep(400);
+    fireEvent.click(popconfirm.container.querySelectorAll('.ant-btn')[0]);
+    await waitFakeTimer(400);
     expect(onOpenChange).toHaveBeenCalledWith(false, eventObject);
   });
 
   it('should support customize icon', () => {
-    const wrapper = mount(
+    const popconfirm = render(
       <Popconfirm title="code" icon={<span className="customize-icon">custom-icon</span>}>
         <span>show me your code</span>
       </Popconfirm>,
     );
 
-    const triggerNode = wrapper.find('span').at(0);
-    triggerNode.simulate('click');
-    expect(wrapper.find('.customize-icon').length).toBe(1);
+    const triggerNode = popconfirm.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
+    expect(popconfirm.container.querySelectorAll('.customize-icon').length).toBe(1);
   });
 
   it('should prefixCls correctly', () => {
     const btnPrefixCls = 'custom-btn';
-    const wrapper = mount(
+    const wrapper = render(
       <Popconfirm
         open
         title="x"
@@ -176,49 +200,46 @@ describe('Popconfirm', () => {
       </Popconfirm>,
     );
 
-    expect(wrapper.find('.custom-popconfirm').length).toBeGreaterThan(0);
-    expect(wrapper.find('.custom-btn').length).toBeGreaterThan(0);
+    expect(wrapper.container.querySelectorAll('.custom-popconfirm').length).toBeGreaterThan(0);
+    expect(wrapper.container.querySelectorAll('.custom-btn').length).toBeGreaterThan(0);
   });
 
   it('should support defaultOpen', () => {
-    const ref = React.createRef();
-    mount(
-      <Popconfirm ref={ref} title="code" defaultOpen>
+    const wrapper = render(
+      <Popconfirm title="code" defaultOpen>
         <span>show me your code</span>
       </Popconfirm>,
     );
-    expect(ref.current.getPopupDomNode()).toBeTruthy();
+    expect(wrapper.container.querySelector('.ant-popover')).toBeTruthy();
   });
 
   it('should not open in disabled', () => {
-    const ref = React.createRef();
-
-    const popconfirm = mount(
-      <Popconfirm ref={ref} title="code" disabled>
+    const wrapper = render(
+      <Popconfirm title="code" disabled>
         <span>click me</span>
       </Popconfirm>,
     );
-    const triggerNode = popconfirm.find('span').at(0);
-    triggerNode.simulate('click');
-    expect(ref.current.getPopupDomNode()).toBeFalsy();
+    const triggerNode = wrapper.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
+    expect(wrapper.container.querySelector('.ant-popover')).toBeFalsy();
   });
 
   it('should be closed by pressing ESC', () => {
     const onOpenChange = jest.fn();
-    const wrapper = mount(
+    const wrapper = render(
       <Popconfirm title="title" mouseEnterDelay={0} mouseLeaveDelay={0} onOpenChange={onOpenChange}>
         <span>Delete</span>
       </Popconfirm>,
     );
-    const triggerNode = wrapper.find('span').at(0);
-    triggerNode.simulate('click');
+    const triggerNode = wrapper.container.querySelectorAll('span')[0];
+    fireEvent.click(triggerNode);
     expect(onOpenChange).toHaveBeenLastCalledWith(true, undefined);
-    triggerNode.simulate('keydown', { key: 'Escape', keyCode: 27 });
+    fireEvent.keyDown(triggerNode, { key: 'Escape', keyCode: 27 });
     expect(onOpenChange).toHaveBeenLastCalledWith(false, eventObject);
   });
 
   it('should not warn memory leaking if setState in async callback', async () => {
-    const error = jest.spyOn(console, 'error');
+    const error = jest.spyOn(console, 'error').mockImplementation(() => {});
 
     const Test = () => {
       const [show, setShow] = React.useState(true);
@@ -231,7 +252,7 @@ describe('Popconfirm', () => {
               new Promise(resolve => {
                 setTimeout(() => {
                   setShow(false);
-                  resolve();
+                  resolve(true);
                 }, 300);
               })
             }
@@ -251,10 +272,10 @@ describe('Popconfirm', () => {
 
     expect(container.textContent).toEqual('Test');
 
-    fireEvent.click(container.querySelector('.clickTarget'));
-    fireEvent.click(container.querySelector('.ant-btn-primary'));
+    fireEvent.click(container.querySelector('.clickTarget')!);
+    fireEvent.click(container.querySelector('.ant-btn-primary')!);
 
-    await sleep(500);
+    await waitFakeTimer(500);
     // expect(container.textContent).toEqual('Unmounted');
     expect(error).not.toHaveBeenCalled();
   });
