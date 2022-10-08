963594981af937b98b437697fdaaae9caa5ca5f7
二货机器人 <smith3816@gmail.com>
2022-10-08 21:23:39 +0800

    docs: home page init

--- .dumi/pages/index/index/Banner.tsx
+++ .dumi/pages/index/index/Banner.tsx
@@ -1,9 +1,40 @@
 import * as React from 'react';
-import { theme } from 'antd';
+import { theme, Space, Button } from 'antd';
+import useLocale from '../../../locales';
+
+const locales = {
+  cn: {
+    sloganIntro: '一个企业级产品设计体系',
+    sloganDesc: '助力设计开发者「更灵活」的搭建出「更美」的产品，让用户「快乐工作」～',
+    start: '开始使用',
+    designLanguage: '设计语言',
+  },
+  default: {
+    sloganIntro: '一个企业级产品设计体系',
+    sloganDesc: '助力设计开发者「更灵活」的搭建出「更美」的产品，让用户「快乐工作」～',
+    start: '开始使用',
+    designLanguage: '设计语言',
+  },
+};
 
 export default function Banner() {
   const token = theme.useToken();
-  console.log('>>>>', token);
+  const locale = useLocale(locales);
 
-  return <div>Banner!!!</div>;
+  return (
+    <div>
+      <h1>Ant Design 5.0</h1>
+      <div>
+        <p>{locale.sloganIntro}</p>
+        <p>{locale.sloganDesc}</p>
+      </div>
+
+      <Space>
+        <Button shape="round" type="primary">
+          {locale.start}
+        </Button>
+        <Button shape="round">{locale.designLanguage}</Button>
+      </Space>
+    </div>
+  );
 }
---
+++ .dumi/pages/index/index/Group.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export interface GroupProps {}
+
+export default function Group(props: GroupProps) {}
