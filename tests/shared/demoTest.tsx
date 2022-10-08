c216e4c79f9078f2573190bd3e0a8a495666cc05
二货机器人 <smith3816@gmail.com>
2022-10-08 15:27:55 +0800

    chore: fix lint

--- tests/shared/demoTest.tsx
+++ tests/shared/demoTest.tsx
@@ -3,46 +3,46 @@ import * as React from 'react';
 import { renderToString } from 'react-dom/server';
 import glob from 'glob';
 import { excludeWarning } from './excludeWarning';
-import { render } from '../utils';
+// import { render } from '../utils';
 import { TriggerMockContext } from './demoTestContext';
 
 require('isomorphic-fetch');
 
-function normalizeAriaValue(value: string | null): string {
-  const defaultValue = value || '';
+// function normalizeAriaValue(value: string | null): string {
+//   const defaultValue = value || '';
 
-  return defaultValue
-    .replace(/\d+/g, 'test')
-    .replace(/TEST_OR_SSR/g, 'test')
-    .replace(/-test-test/g, '-test');
-}
+//   return defaultValue
+//     .replace(/\d+/g, 'test')
+//     .replace(/TEST_OR_SSR/g, 'test')
+//     .replace(/-test-test/g, '-test');
+// }
 
-function normalizeAria(element: Element, ariaName: string) {
-  if (element.hasAttribute(ariaName)) {
-    element.setAttribute(ariaName, normalizeAriaValue(element.getAttribute(ariaName)));
-  }
-}
+// function normalizeAria(element: Element, ariaName: string) {
+//   if (element.hasAttribute(ariaName)) {
+//     element.setAttribute(ariaName, normalizeAriaValue(element.getAttribute(ariaName)));
+//   }
+// }
 
-/**
- * Rc component will generate id for aria usage. It's created as `test-uuid` when env === 'test'. Or
- * `f7fa7a3c-a675-47bc-912e-0c45fb6a74d9`(randomly) when not test env. So we need hack of this to
- * modify the `aria-controls`.
- */
-function ariaConvert(element: Element) {
-  normalizeAria(element, 'aria-owns');
-  normalizeAria(element, 'aria-controls');
-  normalizeAria(element, 'aria-labelledby');
-  normalizeAria(element, 'aria-activedescendant');
-  normalizeAria(element, 'data-menu-id');
-  normalizeAria(element, 'stroke');
-  if (element.id) {
-    element.id = normalizeAriaValue(element.id);
-  }
+// /**
+//  * Rc component will generate id for aria usage. It's created as `test-uuid` when env === 'test'. Or
+//  * `f7fa7a3c-a675-47bc-912e-0c45fb6a74d9`(randomly) when not test env. So we need hack of this to
+//  * modify the `aria-controls`.
+//  */
+// function ariaConvert(element: Element) {
+//   normalizeAria(element, 'aria-owns');
+//   normalizeAria(element, 'aria-controls');
+//   normalizeAria(element, 'aria-labelledby');
+//   normalizeAria(element, 'aria-activedescendant');
+//   normalizeAria(element, 'data-menu-id');
+//   normalizeAria(element, 'stroke');
+//   if (element.id) {
+//     element.id = normalizeAriaValue(element.id);
+//   }
 
-  Array.from(element.children).forEach(child => {
-    ariaConvert(child);
-  });
-}
+//   Array.from(element.children).forEach(child => {
+//     ariaConvert(child);
+//   });
+// }
 
 export type Options = {
   skip?: boolean | string[];
