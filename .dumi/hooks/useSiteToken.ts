ae8b96fd38ca597fe98d18e01586e9673a7eee0f
MadCcc <1075746765@qq.com>
2022-10-09 10:39:36 +0800

    docs: migrate header (#37896)
    * docs: header
    
    * docs: update

---
+++ .dumi/hooks/useSiteToken.ts
@@ -0,0 +1,27 @@
+import { theme } from 'antd';
+import { useContext } from 'react';
+import { ConfigContext } from 'antd/es/config-provider';
+
+const { useToken } = theme;
+
+const useSiteToken = () => {
+  const result = useToken();
+  const { getPrefixCls, iconPrefixCls } = useContext(ConfigContext);
+  const rootPrefixCls = getPrefixCls();
+  const { token } = result;
+  return {
+    ...result,
+    token: {
+      ...token,
+      headerHeight: 64,
+      menuItemBorder: 2,
+      mobileMaxWidth: 767.99,
+      siteHeaderBoxShadow: '0 2px 8px rgba(240, 241, 242, 65',
+      siteMarkdownCodeBg: '#f2f4f5',
+      antCls: `.${rootPrefixCls}`,
+      iconCls: `.${iconPrefixCls}`,
+    },
+  };
+};
+
+export default useSiteToken;
