e084de38e81855874eae9ac66974f4746445af31
lijianan <574980606@qq.com>
2022-10-10 14:15:08 +0800

    refactor: CC => FC (#37886)
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * test: fix test
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * test: add test
    
    * fix: fix test

--- components/locale-provider/__tests__/cached-context.test.tsx
+++ components/locale-provider/__tests__/cached-context.test.tsx
@@ -1,51 +1,38 @@
-import React, { memo, useContext, useRef, useState } from 'react';
-import { render, fireEvent } from '../../../tests/utils';
-import LocaleProvider, { ANT_MARK } from '..';
+import React, { memo, useContext } from 'react';
+import { fireEvent, pureRender } from '../../../tests/utils';
+import LocaleProvider from '..';
 import LocaleContext from '../context';
 
-const defaultLocale = { locale: 'locale' };
-// we use'memo' here in order to only render inner component while context changed.
-const CacheInner = memo(() => {
-  const countRef = useRef(0);
-  countRef.current++;
-  // subscribe locale context
-  useContext(LocaleContext);
-  return (
-    <div>
-      Child Rendering Count: <span id="child_count">{countRef.current}</span>
-    </div>
-  );
-});
+let innerCount = 0;
+let outerCount = 0;
 
-const CacheOuter: React.FC = () => {
-  // We use 'useState' here in order to trigger parent component rendering.
-  const [count, setCount] = useState(1);
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-  // During each rendering phase, the cached context value returned from method 'LocaleProvider#getMemoizedContextValue' will take effect.
-  // So 'CacheInner' component won't rerender.
-  return (
-    <div>
-      <button type="button" onClick={handleClick} id="parent_btn">
-        Click
-      </button>
-      Parent Rendering Count: <span id="parent_count">{count}</span>
-      <LocaleProvider locale={defaultLocale} _ANT_MARK__={ANT_MARK}>
-        <CacheInner />
-      </LocaleProvider>
-    </div>
-  );
+const handleClick = () => {
+  outerCount++;
 };
 
+// we use'memo' here in order to only render inner component while context changed.
+const CacheInner: React.FC = memo(() => {
+  innerCount++;
+  // subscribe locale context
+  useContext(LocaleContext);
+  return null;
+});
+
+const CacheOuter: React.FC = memo(() => (
+  <>
+    <button type="button" onClick={handleClick} id="parent_btn">
+      Click
+    </button>
+    <LocaleProvider locale={{ locale: 'locale' }}>
+      <CacheInner />
+    </LocaleProvider>
+  </>
+));
 it("Rendering on LocaleProvider won't trigger rendering on child component.", () => {
-  const { container } = render(<CacheOuter />);
+  const { container } = pureRender(<CacheOuter />);
+  expect(outerCount).toBe(0);
+  expect(innerCount).toBe(1);
   fireEvent.click(container.querySelector('#parent_btn')!);
-  expect(container.querySelector('#parent_count')?.innerHTML).toBe('2');
-  // child component won't rerender
-  expect(container.querySelector('#child_count')?.innerHTML).toBe('1');
-  fireEvent.click(container.querySelector('#parent_btn')!);
-  expect(container.querySelector('#parent_count')?.innerHTML).toBe('3');
-  // child component won't rerender
-  expect(container.querySelector('#child_count')?.innerHTML).toBe('1');
+  expect(outerCount).toBe(1);
+  expect(innerCount).toBe(1);
 });
