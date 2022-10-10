75b78d74c0dd933633f14e3a8a6fca564a494783
“konghai” <574980606@qq.com>
2022-10-10 14:49:01 +0800

    refactor: delete defaultprops

--- site/theme/template/Color/ColorPicker.tsx
+++ site/theme/template/Color/ColorPicker.tsx
@@ -1,7 +1,34 @@
-import React, { Component } from 'react';
+/* eslint-disable @typescript-eslint/no-shadow */
+import React, { useMemo, useState } from 'react';
 import { SketchPicker } from 'react-color';
 
 const noop = () => {};
+
+const baseStyle: Record<PropertyKey, React.CSSProperties> = {
+  swatch: {
+    padding: '4px',
+    background: '#fff',
+    borderRadius: '2px',
+    boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+    display: 'inline-block',
+    cursor: 'pointer',
+  },
+  popover: {
+    position: 'absolute',
+    zIndex: 10,
+  },
+  cover: {
+    position: 'fixed',
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
+  },
+  wrapper: {
+    position: 'inherit',
+    zIndex: 100,
+  },
+};
 
 interface ColorPickerProps {
   color?: string;
@@ -12,121 +39,87 @@ interface ColorPickerProps {
   onChangeComplete: (hex: string) => void;
 }
 
-export default class ColorPicker extends Component<ColorPickerProps> {
-  static defaultProps = {
-    onChange: noop,
-    onChangeComplete: noop,
-    position: 'bottom',
+const ColorPicker: React.FC<ColorPickerProps> = props => {
+  const {
+    small,
+    presetColors,
+    position = 'bottom',
+    onChange = noop,
+    onChangeComplete = noop,
+  } = props;
+
+  const [color, setColor] = useState<ColorPickerProps['color']>(props.color);
+  const [displayColorPicker, setDisplayColorPicker] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setDisplayColorPicker(prev => !prev);
   };
 
-  static getDerivedStateFromProps(props: ColorPickerProps) {
-    if ('color' in props) {
-      return {
-        color: props.color,
-      };
-    }
-    return null;
-  }
-
-  state = {
-    displayColorPicker: false,
-    color: undefined,
+  const handleClose = () => {
+    setDisplayColorPicker(false);
   };
 
-  handleClick = () => {
-    const { displayColorPicker } = this.state;
-    this.setState({ displayColorPicker: !displayColorPicker });
-  };
-
-  handleClose = () => {
-    this.setState({ displayColorPicker: false });
-  };
-
-  handleChange = (color: { hex: string }) => {
-    const { onChange } = this.props;
-    this.setState({ color: color.hex });
+  const handleChange = (color: { hex: string }) => {
+    setColor(color.hex);
     onChange(color.hex, color);
   };
 
-  handleChangeComplete = (color: { hex: string }) => {
-    const { onChangeComplete } = this.props;
-    this.setState({ color: color.hex });
+  const handleChangeComplete = (color: { hex: string }) => {
+    setColor(color.hex);
     onChangeComplete(color.hex);
   };
 
-  render() {
-    const { small, position, presetColors } = this.props;
-    const { color, displayColorPicker } = this.state;
-    const width = small ? 80 : 120;
-    const styles = {
+  const width = small ? 80 : 120;
+
+  const styles = useMemo<Record<PropertyKey, React.CSSProperties>>(
+    () => ({
+      ...baseStyle,
       color: {
         width: `${width}px`,
         height: small ? '16px' : '24px',
         borderRadius: '2px',
-        background: color,
-      } as React.CSSProperties,
-      swatch: {
-        padding: '4px',
-        background: '#fff',
-        borderRadius: '2px',
-        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-        display: 'inline-block',
-        cursor: 'pointer',
-      } as React.CSSProperties,
-      popover: {
-        position: 'absolute',
-        zIndex: 10,
-      } as React.CSSProperties,
-      cover: {
-        position: 'fixed',
-        top: '0px',
-        right: '0px',
-        bottom: '0px',
-        left: '0px',
-      } as React.CSSProperties,
-      wrapper: {
-        position: 'inherit',
-        zIndex: 100,
-      } as React.CSSProperties,
-    };
+        backgroundColor: color,
+      },
+    }),
+    [width, small, color],
+  );
 
-    if (position === 'top') {
-      styles.wrapper.transform = `translate(calc(-100% + ${width + 8}px), -100%)`;
-      styles.wrapper.paddingBottom = 8;
-    }
-
-    const swatch = (
-      <div style={styles.swatch} onClick={this.handleClick}>
-        <div style={styles.color} />
-      </div>
-    );
-    const picker = displayColorPicker ? (
-      <div style={styles.popover}>
-        <div style={styles.cover} onClick={this.handleClose} />
-        <div style={styles.wrapper}>
-          <SketchPicker
-            presetColors={presetColors}
-            color={color}
-            onChange={this.handleChange}
-            onChangeComplete={this.handleChangeComplete}
-          />
-        </div>
-      </div>
-    ) : null;
-
-    if (position === 'top') {
-      return (
-        <div>
-          {picker}
-          {swatch}
-        </div>
-      );
-    }
-    return (
-      <div>
-        {swatch}
-        {picker}
-      </div>
-    );
+  if (position === 'top') {
+    styles.wrapper.transform = `translate(calc(-100% + ${width + 8}px), -100%)`;
+    styles.wrapper.paddingBottom = 8;
   }
-}
+
+  const swatch = (
+    <div style={styles.swatch} onClick={handleClick}>
+      <div style={styles.color} />
+    </div>
+  );
+
+  const picker = displayColorPicker ? (
+    <div style={styles.popover}>
+      <div style={styles.cover} onClick={handleClose} />
+      <div style={styles.wrapper}>
+        <SketchPicker
+          presetColors={presetColors}
+          color={color}
+          onChange={handleChange}
+          onChangeComplete={handleChangeComplete}
+        />
+      </div>
+    </div>
+  ) : null;
+
+  return position === 'top' ? (
+    <div>
+      {picker}
+      {swatch}
+    </div>
+  ) : (
+    <div>
+      {swatch}
+      {picker}
+    </div>
+  );
+};
+
+export default ColorPicker;
