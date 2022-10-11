8f43b967866b5cdad286420bbf38c66d7f1244ff
MadCcc <1075746765@qq.com>
2022-10-11 17:22:22 +0800

    chore: code clean

--- .dumi/theme/static/style.js
+++ .dumi/theme/static/style.js
@@ -1,4 +1,4 @@
 import 'react-github-button/assets/style.css';
 import 'rc-drawer/assets/index.css';
 import 'docsearch.js/dist/cdn/docsearch.css';
-// import './index.less';
+import './index.less';
