29af8066ecdb2a776bfe3b5b4fb4c62dede52bbf
MadCcc <1075746765@qq.com>
2022-10-11 15:31:36 +0800

    chore: code clean

--- .dumi/theme/slots/Sidebar/index.tsx
+++ .dumi/theme/slots/Sidebar/index.tsx
@@ -13,68 +13,72 @@ const Sidebar: FC = () => {
   const menuItems = useMemo<MenuProps['items']>(() => {
     // 将设计文档未分类的放在最后
     if (pathname.startsWith('/docs/spec')) {
-      const notGrouped = sidebarData.splice(0 ,1);
+      const notGrouped = sidebarData.splice(0, 1);
       sidebarData.push(...notGrouped);
     }
 
-    const items = sidebarData?.reduce<Exclude<MenuProps['items'], undefined>>((result, group) => {
-      if (group.title) {
-        // 设计文档特殊处理二级分组
-        if (pathname.startsWith('/docs/spec')) {
-          const childrenGroup = group.children.reduce<Record<string, ReturnType<typeof useSidebarData>[number]['children']>>((childrenResult, child) => {
-            const type = (child.frontmatter as any).type ?? 'default';
-            if (!childrenResult[type]) {
-              childrenResult[type] = [];
-            }
-            childrenResult[type].push(child);
-            return childrenResult;
-          }, {});
-          const childItems = [];
-          childItems.push(...childrenGroup.default.map(item => ({
-            label: <Link to={item.link}>{item.title}</Link>,
-            key: item.link.replace(/(-cn$)/g, ''),
-          })));
-          Object.entries(childrenGroup).forEach(([type, children]) => {
-            if (type !== 'default') {
-              childItems.push({
-                type: 'group',
-                label: type,
-                key: type,
-                children: children?.map((item) => ({
-                  label: <Link to={item.link}>{item.title}</Link>,
-                  key: item.link.replace(/(-cn$)/g, ''),
-                })),
-              });
-            }
-          })
-          result.push({
-            label: group.title,
-            key: group.title,
-            children: childItems,
-          });
+    return (
+      sidebarData?.reduce<Exclude<MenuProps['items'], undefined>>((result, group) => {
+        if (group.title) {
+          // 设计文档特殊处理二级分组
+          if (pathname.startsWith('/docs/spec')) {
+            const childrenGroup = group.children.reduce<
+              Record<string, ReturnType<typeof useSidebarData>[number]['children']>
+            >((childrenResult, child) => {
+              const type = (child.frontmatter as any).type ?? 'default';
+              if (!childrenResult[type]) {
+                childrenResult[type] = [];
+              }
+              childrenResult[type].push(child);
+              return childrenResult;
+            }, {});
+            const childItems = [];
+            childItems.push(
+              ...childrenGroup.default.map(item => ({
+                label: <Link to={item.link}>{item.title}</Link>,
+                key: item.link.replace(/(-cn$)/g, ''),
+              })),
+            );
+            Object.entries(childrenGroup).forEach(([type, children]) => {
+              if (type !== 'default') {
+                childItems.push({
+                  type: 'group',
+                  label: type,
+                  key: type,
+                  children: children?.map(item => ({
+                    label: <Link to={item.link}>{item.title}</Link>,
+                    key: item.link.replace(/(-cn$)/g, ''),
+                  })),
+                });
+              }
+            });
+            result.push({
+              label: group.title,
+              key: group.title,
+              children: childItems,
+            });
+          } else {
+            result.push({
+              type: 'group',
+              label: group.title,
+              key: group.title,
+              children: group.children?.map(item => ({
+                label: <Link to={item.link}>{item.title}</Link>,
+                key: item.link.replace(/(-cn$)/g, ''),
+              })),
+            });
+          }
         } else {
-          result.push({
-            type: 'group',
-            label: group.title,
-            key: group.title,
-            children: group.children?.map(item => ({
+          result.push(
+            ...group.children?.map(item => ({
               label: <Link to={item.link}>{item.title}</Link>,
               key: item.link.replace(/(-cn$)/g, ''),
             })),
-          });
+          );
         }
-      } else {
-        result.push(...group.children?.map(item => ({
-          label: <Link to={item.link}>{item.title}</Link>,
-          key: item.link.replace(/(-cn$)/g, ''),
-        })));
-      }
-      return result;
-    }, []) ?? [];
-
-
-
-    return items;
+        return result;
+      }, []) ?? []
+    );
   }, [sidebarData]);
 
   const selectedKeys = useMemo(() => [pathname], [pathname]);
@@ -97,7 +101,9 @@ const Sidebar: FC = () => {
   ) : (
     <Col xxl={4} xl={5} lg={6} md={6} sm={24} xs={24} className="main-menu">
       <Affix>
-        <section style={{ width: '100%' }} className="main-menu-inner">{menuChild}</section>
+        <section style={{ width: '100%' }} className="main-menu-inner">
+          {menuChild}
+        </section>
       </Affix>
     </Col>
   );
