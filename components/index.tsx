39f4c149bab75b185bbffd87921c357e613f8096
MadCcc <1075746765@qq.com>
2022-10-12 17:49:47 +0800

    chore: remove babel-plugin-import warning

--- components/index.tsx
+++ components/index.tsx
@@ -1,21 +1,3 @@
-/* @remove-on-es-build-begin */
-// this file is not used if use https://github.com/ant-design/babel-plugin-import
-const ENV = process.env.NODE_ENV;
-if (
-  ENV !== 'production' &&
-  ENV !== 'test' &&
-  typeof console !== 'undefined' &&
-  console.warn && // eslint-disable-line no-console
-  typeof window !== 'undefined'
-) {
-  // eslint-disable-next-line no-console
-  console.warn(
-    'You are using a whole package of antd, ' +
-      'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.',
-  );
-}
-/* @remove-on-es-build-end */
-
 export { default as Affix } from './affix';
 export type { AffixProps } from './affix';
 export { default as Alert } from './alert';
