7016f0baf58257924e921404aa49febd9349385e
lijianan <574980606@qq.com>
2022-10-21 15:05:38 +0800

    feat: v5 site overview page (#38131)
    * feat: site
    
    * fix: fix
    
    * feat: v5 site overview page
    
    * fix: fix path
    
    * fix: fix
    
    * fix: fix

---
+++ .dumi/theme/builtins/ComponentOverview/ProComponentsList.ts
@@ -0,0 +1,70 @@
+const cnProComponentsList = [
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/4n5H%24UX%24j/bianzu%2525204.svg',
+      path: 'https://procomponents.ant.design/components/layout',
+      subtitle: '高级布局',
+      title: 'ProLayout',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/mStei5BFC/bianzu%2525207.svg',
+      path: 'https://procomponents.ant.design/components/form',
+      subtitle: '高级表单',
+      title: 'ProForm',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/AwU0Cv%26Ju/bianzu%2525208.svg',
+      path: 'https://procomponents.ant.design/components/table',
+      subtitle: '高级表格',
+      title: 'ProTable',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/H0%26LSYYfh/bianzu%2525209.svg',
+      path: 'https://procomponents.ant.design/components/descriptions',
+      subtitle: '高级定义列表',
+      title: 'ProDescriptions',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/uZUmLtne5/bianzu%2525209.svg',
+      path: 'https://procomponents.ant.design/components/list',
+      subtitle: '高级列表',
+      title: 'ProList',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+  {
+    meta: {
+      category: 'Components',
+      cover: 'https://gw.alipayobjects.com/zos/antfincdn/N3eU432oA/bianzu%2525209.svg',
+      path: 'https://procomponents.ant.design/components/editable-table',
+      subtitle: '可编辑表格',
+      title: 'EditableProTable',
+      type: '重型组件',
+      tag: 'https://gw.alipayobjects.com/zos/antfincdn/OG4ajVYzh/bianzu%2525202.svg',
+    },
+  },
+];
+
+export default cnProComponentsList;
