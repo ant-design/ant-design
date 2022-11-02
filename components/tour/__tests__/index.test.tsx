a23659431b324bea5710ad0eb05dd16adaa8fafd
MadCcc <1075746765@qq.com>
2022-11-02 20:26:54 +0800

    test: update test case

--- components/tour/__tests__/index.test.tsx
+++ components/tour/__tests__/index.test.tsx
@@ -251,7 +251,7 @@ describe('Tour', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Next' }));
     expect(getByText('Adjust Placement')).toBeTruthy();
     fireEvent.click(screen.getByRole('button', { name: 'Finish' }));
-    expect(document.querySelector('.rc-tour')).toBeFalsy();
+    expect(document.querySelector('.ant-tour')).toBeFalsy();
     expect(container.firstChild).toMatchSnapshot();
   });
 });
