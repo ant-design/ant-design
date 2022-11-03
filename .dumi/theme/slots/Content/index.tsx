f3853d6178ce53a6bd00bf783082b7e2bf6a697b
MadCcc <1075746765@qq.com>
2022-11-03 16:37:04 +0800

    docs: content

--- .dumi/theme/slots/Content/index.tsx
+++ .dumi/theme/slots/Content/index.tsx
@@ -1,25 +1,92 @@
-import React, { ReactNode, type FC } from 'react';
-import { useRouteMeta } from 'dumi';
+import React, { ReactNode, type FC, useMemo } from 'react';
+import { useIntl, useRouteMeta } from 'dumi';
 import Footer from 'dumi/theme/slots/Footer';
-import { Col, Typography } from 'antd';
+import { Col, Typography, Avatar, Tooltip, Affix, Anchor } from 'antd';
 import EditButton from '../../common/EditButton';
 import { FormattedMessage } from 'react-intl';
 import useLocation from '../../../hooks/useLocation';
+import ContributorsList from '@qixian.cs/github-contributors-list';
+import useSiteToken from '../../../hooks/useSiteToken';
+import { css } from '@emotion/react';
+import PrevAndNext from '../../common/PrevAndNext';
+
+const useStyle = () => {
+  const { token } = useSiteToken();
+
+  const { antCls } = token;
+
+  return {
+    contributorsList: css`
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 120px !important;
+
+      a,
+      ${antCls}-avatar + ${antCls}-avatar {
+        margin-bottom: 8px;
+        margin-inline-end: 8px;
+      }
+    `,
+    toc: css`
+      position: absolute;
+      top: 8px;
+      right: 20px;
+      margin-block: 20px;
+      width: 120px;
+
+      ${antCls}-anchor-link-title {
+        font-size: 12px;
+      }
+    `,
+  };
+};
+
+type AnchorItem = {
+  id: string;
+  title: string;
+  children?: AnchorItem[];
+};
 
 const Content: FC<{ children: ReactNode }> = ({ children }) => {
   // TODO: implement content
   // from: https://github.com/ant-design/ant-design/blob/2a754bd5cad7fd4892a065a8e044fb402f51f426/site/theme/template/Content/MainContent.jsx
-  //  1. Title & edit link --- Done
-  //  2. TOC
-  //  3. Contributors list
-  //  4. Prev & next page
+  //  1. Title & edit link --- Need filename
+  //  2. TOC --- Done
+  //  3. Contributors list --- Need filename
+  //  4. Prev & next page --- Done
 
   console.log('route meta', useRouteMeta());
   const meta = useRouteMeta();
   const { pathname } = useLocation();
+  const { formatMessage } = useIntl();
+  const styles = useStyle();
+
+  const anchorItems = useMemo(() => {
+    return meta.toc.reduce<AnchorItem[]>((result, item) => {
+      if (item.depth === 2) {
+        result.push(item);
+      } else if (item.depth === 3) {
+        const parent = result[result.length - 1];
+        if (parent) {
+          parent.children = parent.children || [];
+          parent.children.push(item);
+        }
+      }
+      return result;
+    }, []);
+  }, [meta.toc]);
 
   return (
     <Col xxl={20} xl={19} lg={18} md={18} sm={24} xs={24}>
+      <Anchor css={styles.toc}>
+        {anchorItems.map(item => (
+          <Anchor.Link href={`#${item.id}`} title={item.title} key={item.id}>
+            {item.children?.map(child => (
+              <Anchor.Link href={`#${child.id}`} title={child.title} key={child.id} />
+            ))}
+          </Anchor.Link>
+        ))}
+      </Anchor>
       <div style={{ padding: '0 170px 32px 64px' }}>
         <Typography.Title level={2}>
           {meta.frontmatter.title}
@@ -34,7 +101,34 @@ const Content: FC<{ children: ReactNode }> = ({ children }) => {
           )}
         </Typography.Title>
         {children}
+        <ContributorsList
+          css={styles.contributorsList}
+          fileName="components/button/index.zh-CN.md"
+          renderItem={(item, loading) =>
+            loading ? (
+              <Avatar style={{ opacity: 0.3 }} />
+            ) : (
+              item && (
+                <Tooltip
+                  title={`${formatMessage({ id: 'app.content.contributors' })}: ${item.username}`}
+                  key={item.username}
+                >
+                  <a
+                    href={`https://github.com/${item.username}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Avatar src={item.url}>{item.username}</Avatar>
+                  </a>
+                </Tooltip>
+              )
+            )
+          }
+          repo="ant-design"
+          owner="ant-design"
+        />
       </div>
+      <PrevAndNext />
       <Footer />
     </Col>
   );
--- .dumi/theme/slots/Sidebar/index.tsx
+++ .dumi/theme/slots/Sidebar/index.tsx
@@ -1,116 +1,15 @@
-import React, { type FC, useContext, useMemo } from 'react';
-import { useSidebarData, Link } from 'dumi';
-import { Affix, Col, Menu, MenuProps } from 'antd';
+import React, { type FC, useContext } from 'react';
+import { useSidebarData } from 'dumi';
+import { Affix, Col, Menu } from 'antd';
 import MobileMenu from 'rc-drawer';
 import SiteContext from '../SiteContext';
-import useLocation from '../../../hooks/useLocation';
-import { useFullSidebarData } from 'dumi';
+import useMenu from '../../../hooks/useMenu';
 
 const Sidebar: FC = () => {
   const sidebarData = useSidebarData();
   const { isMobile } = useContext(SiteContext);
-  const { pathname } = useLocation();
-  const fullData = useFullSidebarData();
 
-  const menuItems = useMemo<MenuProps['items']>(() => {
-    const sidebarItems = [...(sidebarData ?? [])];
-
-    // 将设计文档未分类的放在最后
-    if (pathname.startsWith('/docs/spec')) {
-      const notGrouped = sidebarItems.splice(0, 1);
-      sidebarItems.push(...notGrouped);
-    }
-
-    // 把 /changelog 拼到开发文档中
-    if (pathname.startsWith('/docs/react')) {
-      const changelogData = Object.entries(fullData).find(([key]) =>
-        key.startsWith('/changelog'),
-      )?.[1];
-      if (changelogData) {
-        sidebarItems.push(...changelogData);
-      }
-    }
-    if (pathname.startsWith('/changelog')) {
-      const reactDocData = Object.entries(fullData).find(([key]) =>
-        key.startsWith('/docs/react'),
-      )?.[1];
-      if (reactDocData) {
-        sidebarItems.unshift(...reactDocData);
-      }
-    }
-
-    return (
-      sidebarItems?.reduce<Exclude<MenuProps['items'], undefined>>((result, group) => {
-        if (group.title) {
-          // 设计文档特殊处理二级分组
-          if (pathname.startsWith('/docs/spec')) {
-            const childrenGroup = group.children.reduce<
-              Record<string, ReturnType<typeof useSidebarData>[number]['children']>
-            >((childrenResult, child) => {
-              const type = (child.frontmatter as any).type ?? 'default';
-              if (!childrenResult[type]) {
-                childrenResult[type] = [];
-              }
-              childrenResult[type].push(child);
-              return childrenResult;
-            }, {});
-            const childItems = [];
-            childItems.push(
-              ...childrenGroup.default.map(item => ({
-                label: <Link to={item.link}>{item.title}</Link>,
-                key: item.link.replace(/(-cn$)/g, ''),
-              })),
-            );
-            Object.entries(childrenGroup).forEach(([type, children]) => {
-              if (type !== 'default') {
-                childItems.push({
-                  type: 'group',
-                  label: type,
-                  key: type,
-                  children: children?.map(item => ({
-                    label: <Link to={item.link}>{item.title}</Link>,
-                    key: item.link.replace(/(-cn$)/g, ''),
-                  })),
-                });
-              }
-            });
-            result.push({
-              label: group.title,
-              key: group.title,
-              children: childItems,
-            });
-          } else {
-            result.push({
-              type: 'group',
-              label: group.title,
-              key: group.title,
-              children: group.children?.map(item => ({
-                label: (
-                  <Link to={item.link}>
-                    <span key="english">{item.title}</span>
-                    <span className="chinese" key="chinese">
-                      {(item.frontmatter as any).subtitle}
-                    </span>
-                  </Link>
-                ),
-                key: item.link.replace(/(-cn$)/g, ''),
-              })),
-            });
-          }
-        } else {
-          result.push(
-            ...group.children?.map(item => ({
-              label: <Link to={item.link}>{item.title}</Link>,
-              key: item.link.replace(/(-cn$)/g, ''),
-            })),
-          );
-        }
-        return result;
-      }, []) ?? []
-    );
-  }, [sidebarData]);
-
-  const selectedKeys = useMemo(() => [pathname], [pathname]);
+  const [menuItems, selectedKey] = useMenu();
 
   const menuChild = (
     <Menu
@@ -118,7 +17,7 @@ const Sidebar: FC = () => {
       inlineIndent={30}
       className="aside-container menu-site"
       mode="inline"
-      selectedKeys={selectedKeys}
+      selectedKeys={[selectedKey]}
       defaultOpenKeys={sidebarData?.map(({ title }) => title).filter(item => item) as string[]}
     />
   );
