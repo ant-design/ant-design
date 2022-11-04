74bc37647aeda6e974351b43b338cf31a7ff723d
afc163 <afc163@gmail.com>
2022-11-04 18:43:13 +0800

    Apply suggestions from code review

--- components/form/FormItem/index.tsx
+++ components/form/FormItem/index.tsx
@@ -282,7 +282,7 @@ function InternalFormItem<Values = any>(props: FormItemProps<Values>): React.Rea
           warning(
             false,
             'Form.Item',
-            'A `Form.Item` with a `name` prop must have a single child element. For information on how to render more complex form items, see https://u.ant.design/#complex-form-item.'
+            'A `Form.Item` with a `name` prop must have a single child element. For information on how to render more complex form items, see https://u.ant.design/#complex-form-item.',
           );
           childNode = children;
         } else if (isRenderProps && (!(shouldUpdate || dependencies) || hasName)) {
