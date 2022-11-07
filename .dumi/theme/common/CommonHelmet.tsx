e002e4ca5e442b2467a6bf21f37203944003c965
MadCcc <1075746765@qq.com>
2022-11-07 17:47:10 +0800

    feat: helmet

--- .dumi/theme/common/CommonHelmet.tsx
+++ .dumi/theme/common/CommonHelmet.tsx
@@ -1,15 +1,24 @@
 import { useRouteMeta } from 'dumi';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 const CommonHelmet = () => {
   const meta = useRouteMeta();
-  const helmetTitle = `${meta.frontmatter.subtitle || ''} ${meta.frontmatter.title} - Ant Design`;
+
+  const [title] = useMemo(() => {
+    const helmetTitle = `${meta.frontmatter.subtitle || ''} ${meta.frontmatter.title} - Ant Design`;
+    const helmetDescription =
+      meta.texts
+        ?.filter(item => item.paraId === 0)
+        .map(item => item.value)
+        .join('') || '';
+    return [helmetTitle, helmetDescription];
+  }, [meta]);
 
   return (
     <Helmet>
-      <title>{helmetTitle}</title>
-      <meta property="og:title" content={helmetTitle} />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
     </Helmet>
   );
 };
