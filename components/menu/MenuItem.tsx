3a6f607538250f4dc7ec97addcf9adcf4e8ff707
Zheeeng <hi@zheeeng.me>
2022-11-08 12:41:12 +0800

    refactor: change MenuItem to FC c component

--- components/menu/MenuItem.tsx
+++ components/menu/MenuItem.tsx
@@ -3,12 +3,9 @@ import type { MenuItemProps as RcMenuItemProps } from 'rc-menu';
 import { Item } from 'rc-menu';
 import toArray from 'rc-util/lib/Children/toArray';
 import * as React from 'react';
-import type { SiderContextProps } from '../layout/Sider';
 import { SiderContext } from '../layout/Sider';
-import type { TooltipProps } from '../tooltip';
 import Tooltip from '../tooltip';
 import { cloneElement, isValidElement } from '../_util/reactNode';
-import type { MenuContextProps } from './MenuContext';
 import MenuContext from './MenuContext';
 
 export interface MenuItemProps extends Omit<RcMenuItemProps, 'title'> {
@@ -17,88 +14,94 @@ export interface MenuItemProps extends Omit<RcMenuItemProps, 'title'> {
   title?: React.ReactNode;
 }
 
-export default class MenuItem extends React.Component<MenuItemProps> {
-  static contextType = MenuContext;
+const MenuItem: React.FC<MenuItemProps> = ({
+  className,
+  children,
+  title,
+  icon,
+  danger,
+  ...rest
+}) => {
+  const { prefixCls, firstLevel, inlineCollapsed, direction, disableMenuItemTitleTooltip } =
+    React.useContext(MenuContext);
+  const { siderCollapsed } = React.useContext(SiderContext);
 
-  context: MenuContextProps;
-
-  renderItemChildren(inlineCollapsed: boolean) {
-    const { prefixCls, firstLevel } = this.context;
-    const { icon, children } = this.props;
-
-    const wrapNode = <span className={`${prefixCls}-title-content`}>{children}</span>;
-    // inline-collapsed.md demo 依赖 span 来隐藏文字,有 icon 属性，则内部包裹一个 span
-    // ref: https://github.com/ant-design/ant-design/pull/23456
-    if (!icon || (isValidElement(children) && children.type === 'span')) {
-      if (children && inlineCollapsed && firstLevel && typeof children === 'string') {
-        return <div className={`${prefixCls}-inline-collapsed-noicon`}>{children.charAt(0)}</div>;
-      }
+  const tooltipTitle = React.useMemo(() => {
+    if (title === undefined) {
+      return firstLevel ? children : '';
     }
-    return wrapNode;
-  }
 
-  renderItem = ({ siderCollapsed }: SiderContextProps) => {
-    const { prefixCls, firstLevel, inlineCollapsed, direction, disableMenuItemTitleTooltip } =
-      this.context;
-    const { className, children } = this.props;
-    const { title, icon, danger, ...rest } = this.props;
-
-    let tooltipTitle = title;
-    if (typeof title === 'undefined') {
-      tooltipTitle = firstLevel ? children : '';
-    } else if (title === false) {
-      tooltipTitle = '';
+    if (title === false) {
+      return '';
     }
-    const tooltipProps: TooltipProps = {
-      title: tooltipTitle,
-    };
 
-    if (!siderCollapsed && !inlineCollapsed) {
-      tooltipProps.title = null;
-      // Reset `open` to fix control mode tooltip display not correct
-      // ref: https://github.com/ant-design/ant-design/issues/16742
-      tooltipProps.open = false;
-    }
-    const childrenLength = toArray(children).length;
+    return title;
+  }, []);
 
-    let returnNode = (
-      <Item
-        {...rest}
-        className={classNames(
-          {
-            [`${prefixCls}-item-danger`]: danger,
-            [`${prefixCls}-item-only-child`]: (icon ? childrenLength + 1 : childrenLength) === 1,
+  const tooltipProps = React.useMemo(
+    () =>
+      !siderCollapsed && !inlineCollapsed
+        ? {
+            title: null,
+            // Reset `open` to fix control mode tooltip display not correct
+            // ref: https://github.com/ant-design/ant-design/issues/16742
+            open: false,
+          }
+        : {
+            title: tooltipTitle,
           },
-          className,
-        )}
-        title={typeof title === 'string' ? title : undefined}
+    [],
+  );
+
+  const childrenLength = React.useMemo(() => toArray(children).length, []);
+
+  const returnNode = (
+    <Item
+      {...rest}
+      className={classNames(
+        {
+          [`${prefixCls}-item-danger`]: danger,
+          [`${prefixCls}-item-only-child`]: (icon ? childrenLength + 1 : childrenLength) === 1,
+        },
+        className,
+      )}
+      title={typeof title === 'string' ? title : undefined}
+    >
+      {cloneElement(icon, {
+        className: classNames(
+          isValidElement(icon) ? icon.props?.className : '',
+          `${prefixCls}-item-icon`,
+        ),
+      })}
+      {
+        // inline-collapsed.md demo 依赖 span 来隐藏文字,有 icon 属性，则内部包裹一个 span
+        // ref: https://github.com/ant-design/ant-design/pull/23456
+        (!icon || (isValidElement(children) && children.type === 'span')) &&
+        children &&
+        inlineCollapsed &&
+        firstLevel &&
+        typeof children === 'string' ? (
+          <div className={`${prefixCls}-inline-collapsed-noicon`}>{children.charAt(0)}</div>
+        ) : (
+          <span className={`${prefixCls}-title-content`}>{children}</span>
+        )
+      }
+    </Item>
+  );
+
+  if (!disableMenuItemTitleTooltip) {
+    return (
+      <Tooltip
+        {...tooltipProps}
+        placement={direction === 'rtl' ? 'left' : 'right'}
+        overlayClassName={`${prefixCls}-inline-collapsed-tooltip`}
       >
-        {cloneElement(icon, {
-          className: classNames(
-            isValidElement(icon) ? icon.props?.className : '',
-            `${prefixCls}-item-icon`,
-          ),
-        })}
-        {this.renderItemChildren(inlineCollapsed)}
-      </Item>
+        {returnNode}
+      </Tooltip>
     );
-
-    if (!disableMenuItemTitleTooltip) {
-      returnNode = (
-        <Tooltip
-          {...tooltipProps}
-          placement={direction === 'rtl' ? 'left' : 'right'}
-          overlayClassName={`${prefixCls}-inline-collapsed-tooltip`}
-        >
-          {returnNode}
-        </Tooltip>
-      );
-    }
-
-    return returnNode;
-  };
-
-  render() {
-    return <SiderContext.Consumer>{this.renderItem}</SiderContext.Consumer>;
   }
-}
+
+  return returnNode;
+};
+
+export default MenuItem;
