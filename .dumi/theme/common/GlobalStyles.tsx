9f7e2ee239cfdaaa9744f1095b4df05b054608ac
二货机器人 <smith3816@gmail.com>
2022-11-15 15:31:28 +0800

    docs: enable puhui bold on logo

--- .dumi/theme/common/GlobalStyles.tsx
+++ .dumi/theme/common/GlobalStyles.tsx
@@ -72,25 +72,25 @@ const GlobalStyles = () => {
       {/* common */}
       <Global
         styles={css`
-          // @font-face {
-          //   font-weight: normal;
-          //   font-family: PuHuiTi;
-          //   src: url('//at.alicdn.com/t/webfont_6e11e43nfj.woff2') format('woff2'),
-          //     url('//at.alicdn.com/t/webfont_6e11e43nfj.woff') format('woff'),
-          //     /* chrome、firefox */ url('//at.alicdn.com/t/webfont_6e11e43nfj.ttf')
-          //       format('truetype'); /* chrome、firefox、opera、Safari, Android, iOS 4.2+ */
-          //   font-display: swap;
-          // }
+          @font-face {
+            font-weight: normal;
+            font-family: PuHuiTi;
+            src: url('//at.alicdn.com/t/webfont_6e11e43nfj.woff2') format('woff2'),
+              url('//at.alicdn.com/t/webfont_6e11e43nfj.woff') format('woff'),
+              /* chrome、firefox */ url('//at.alicdn.com/t/webfont_6e11e43nfj.ttf')
+                format('truetype'); /* chrome、firefox、opera、Safari, Android, iOS 4.2+ */
+            font-display: swap;
+          }
 
-          // @font-face {
-          //   font-weight: bold;
-          //   font-family: PuHuiTi;
-          //   src: url('//at.alicdn.com/t/webfont_exesdog9toj.woff2') format('woff2'),
-          //     url('//at.alicdn.com/t/webfont_exesdog9toj.woff') format('woff'),
-          //     /* chrome、firefox */ url('//at.alicdn.com/t/webfont_exesdog9toj.ttf')
-          //       format('truetype'); /* chrome、firefox、opera、Safari, Android, iOS 4.2+ */
-          //   font-display: swap;
-          // }
+          @font-face {
+            font-weight: bold;
+            font-family: PuHuiTi;
+            src: url('//at.alicdn.com/t/webfont_exesdog9toj.woff2') format('woff2'),
+              url('//at.alicdn.com/t/webfont_exesdog9toj.woff') format('woff'),
+              /* chrome、firefox */ url('//at.alicdn.com/t/webfont_exesdog9toj.ttf')
+                format('truetype'); /* chrome、firefox、opera、Safari, Android, iOS 4.2+ */
+            font-display: swap;
+          }
 
           // 组件丰富，选用自如定制主题随心所欲设计语言与研发框架1234567890 QWERTYUIOPLKJHGFDSAZXCVBNM,.mnbvcxzasdfghjklpoiuytrewq
           /* CDN 服务仅供平台体验和调试使用，平台不承诺服务的稳定性，企业客户需下载字体包自行发布使用并做好备份。 */
