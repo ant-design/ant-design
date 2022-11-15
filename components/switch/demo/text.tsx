0ecf1e19cfdbc97e6050ede36335dae8f8b217a8
MadCcc <1075746765@qq.com>
2022-11-15 22:14:09 +0800

    chore: update demo

--- components/switch/demo/text.tsx
+++ components/switch/demo/text.tsx
@@ -5,9 +5,7 @@ import { Switch, Space } from 'antd';
 const App: React.FC = () => (
   <Space direction="vertical">
     <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked />
-    <br />
     <Switch checkedChildren="1" unCheckedChildren="0" />
-    <br />
     <Switch
       checkedChildren={<CheckOutlined />}
       unCheckedChildren={<CloseOutlined />}
