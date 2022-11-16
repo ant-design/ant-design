55f60b6e4d2b4b213ca08391afb70757e8a25af9
wuxh <wxh1220@gmail.com>
2022-11-16 18:31:20 +0800

    Revert "chore(space): optimize documents"
    This reverts commit 88a4378f16cede5b628f885a48a341d507fd53e3.

--- components/space/demo/align.tsx
+++ components/space/demo/align.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Button, Space } from 'antd';
 
-const aligns = ['start', 'end', 'center', 'baseline'] as const;
-
 const App: React.FC = () => (
   <div className="space-align-container">
-    {aligns.map((align) => (
-      <div className="space-align-block" key={align}>
-        <Space align={align}>
-          {align}
-          <Button type="primary">Primary</Button>
-          <span className="mock-block">Block</span>
-        </Space>
-      </div>
-    ))}
+    <div className="space-align-block">
+      <Space align="center">
+        center
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+    <div className="space-align-block">
+      <Space align="start">
+        start
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+    <div className="space-align-block">
+      <Space align="end">
+        end
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+    <div className="space-align-block">
+      <Space align="baseline">
+        baseline
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
   </div>
 );
 
