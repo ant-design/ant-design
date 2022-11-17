473e793a19f172d50ccae83d3292f9920741aa24
afc163 <afc163@gmail.com>
2022-11-17 19:39:43 +0800

    docs: fix edit button

--- .dumi/theme/common/GlobalStyles.tsx
+++ .dumi/theme/common/GlobalStyles.tsx
@@ -1067,7 +1067,7 @@ const GlobalStyles = () => {
               width: 16px;
               height: 16px;
               margin-left: 16px;
-              color: ${token.colorText}-secondary;
+              color: ${token.colorTextSecondary};
               cursor: pointer;
               transition: all 0.24s;
 
@@ -1099,7 +1099,7 @@ const GlobalStyles = () => {
               transition: transform 0.24s;
 
               &:hover {
-                transform: scale(1.2);
+                transform: scale(1.1);
               }
 
               &${iconCls}-check {
