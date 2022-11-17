52cd822610d79e1a52c2bdbd5e4d53bd81cc6a43
MadCcc <1075746765@qq.com>
2022-11-17 14:39:25 +0800

    chore: update snapshot

--- components/anchor/demo/static.tsx
+++ components/anchor/demo/static.tsx
@@ -7,9 +7,9 @@ const App: React.FC = () => (
   <Anchor affix={false}>
     <Link href="#components-anchor-demo-basic" title="Basic demo" />
     <Link href="#components-anchor-demo-static" title="Static demo" />
-    <Link href="#API" title="API">
-      <Link href="#Anchor-Props" title="Anchor Props" />
-      <Link href="#Link-Props" title="Link Props" />
+    <Link href="#api" title="API">
+      <Link href="#anchor-props" title="Anchor Props" />
+      <Link href="#link-props" title="Link Props" />
     </Link>
   </Anchor>
 );
