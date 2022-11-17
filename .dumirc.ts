af139e9d13c80643f6634ad8eda3d589708f1cb7
afc163 <afc163@gmail.com>
2022-11-18 00:01:31 +0800

    docs: remove hotjar (#38654)

--- .dumirc.ts
+++ .dumirc.ts
@@ -95,25 +95,5 @@ export default defineConfig({
       document.documentElement.className += isZhCN(pathname) ? 'zh-cn' : 'en-us';
     })();
     `,
-    // Hotjar Tracking Code for ant.design
-    `
-    (function (h, o, t, j, a, r) {
-      if (location.hostname === 'localhost') {
-        return;
-      }
-
-      h.hj =
-        h.hj ||
-        function () {
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      h._hjSettings = { hjid: 473408, hjsv: 5 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script');
-      r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
-    `,
   ],
 });
