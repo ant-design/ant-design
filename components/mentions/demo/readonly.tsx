1ccfa744b3e6f79b03400d4b0a0cbba7848d61c6
wangning <wangning4567@163.com>
2022-11-19 12:11:46 +0800

    feat: reset and force update

--- components/mentions/demo/readonly.tsx
+++ components/mentions/demo/readonly.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Mentions } from 'antd';
 
-const { Option } = Mentions;
-
-const getOptions = () =>
-  ['afc163', 'zombiej', 'yesmeck'].map(value => (
-    <Option key={value} value={value}>
-      {value}
-    </Option>
-  ));
+const options = ['afc163', 'zombiej', 'yesmeck'].map((value) => ({
+  value,
+  key: value,
+  label: value,
+}));
 
 const App: React.FC = () => (
   <>
     <div style={{ marginBottom: 10 }}>
-      <Mentions style={{ width: '100%' }} placeholder="this is disabled Mentions" disabled>
-        {getOptions()}
-      </Mentions>
+      <Mentions
+        style={{ width: '100%' }}
+        placeholder="this is disabled Mentions"
+        disabled
+        options={options}
+      />
     </div>
-    <Mentions style={{ width: '100%' }} placeholder="this is readOnly Mentions" readOnly>
-      {getOptions()}
-    </Mentions>
+    <Mentions
+      style={{ width: '100%' }}
+      placeholder="this is readOnly Mentions"
+      readOnly
+      options={options}
+    />
   </>
 );
 
