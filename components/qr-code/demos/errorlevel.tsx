c92ffc21394929b62f77f35d875bf8675523a98d
MadCcc <1075746765@qq.com>
2022-11-22 16:56:27 +0800

    feat: qrcode

---
+++ components/qr-code/demos/errorlevel.tsx
@@ -0,0 +1,28 @@
+/**
+ * uuid: f54dc6af
+ * title: errorLevel例子
+ */
+
+import React, { useState } from 'react';
+import { QrCode } from '@alipay/tech-ui';
+import { Radio } from 'antd';
+
+export default () => {
+  const [level, setLevel] = useState<string>('L');
+
+  return (
+    <div>
+      <Radio.Group value={level} onChange={(e) => setLevel(e.target.value)}>
+        <Radio.Button value="L">L</Radio.Button>
+        <Radio.Button value="M">M</Radio.Button>
+        <Radio.Button value="Q">Q</Radio.Button>
+        <Radio.Button value="H">H</Radio.Button>
+      </Radio.Group>
+      <QrCode
+        errorLevel={level}
+        value="https://hitu.alipay.com/packages/@alipay/tech-ui/home"
+        logo="https://gw-office.alipayobjects.com/basement_prod/c83c53ab-515e-43e2-85d0-4d0da16f11ef.svg"
+      />
+    </div>
+  );
+};
---
+++ components/qr-code/demos/withoutlogo.tsx
@@ -0,0 +1,9 @@
+/**
+ * uuid: 0a9254ef
+ * title: 不带 logo 例子
+ */
+
+import React from 'react';
+import { QrCode } from '@alipay/tech-ui';
+
+export default () => <QrCode value="http://www.tmall.com" />;
