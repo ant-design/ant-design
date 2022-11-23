26e014b28e7f1e9565444d52609ed857fd46ec7f
“konghai” <574980606@qq.com>
2022-11-23 10:38:02 +0800

    fix: fix bug

--- components/qr-code/demo/download.tsx
+++ components/qr-code/demo/download.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { QrCode, Button } from 'antd';
 
-const App: React.FC = () => {
-  const downloadQrCode = () => {
-    const canvas = document.getElementById('myqrcode')?.getElementsByTagName('canvas')[0];
-    const url = canvas?.toDataURL() || '';
-    const a = document.createElement('a');
-    a.download = 'qr-code.png';
-    a.href = url;
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  };
-
-  return (
-    <div id="myqrcode">
-      <QrCode value="https://ant.design/" />
-      <Button style={{ marginBlockStart: 16 }} onClick={downloadQrCode}>
-        下载二维码
-      </Button>
-    </div>
-  );
+const downloadQrCode = () => {
+  const canvas = document.getElementById('myqrcode')?.querySelector('canvas');
+  const url = canvas?.toDataURL() || '';
+  const a = document.createElement('a');
+  a.download = 'qr-code.png';
+  a.href = url;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 };
+
+const App: React.FC = () => (
+  <div id="myqrcode">
+    <QrCode value="https://ant.design/" />
+    <Button style={{ marginBlockStart: 16 }} onClick={downloadQrCode}>
+      下载二维码
+    </Button>
+  </div>
+);
 
 export default App;
