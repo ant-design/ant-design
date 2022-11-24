0c3ba124b48117cff7c3d5910095a2f4b810faec
lijianan <574980606@qq.com>
2022-11-24 14:59:17 +0800

    fix[BackTop]: delete visible & use visibilityHeight=0 replace visible (#38763)
    * type: delete visible & code optimization
    
    * add PureBackTop
    
    * fix: update snap
    
    * fix: cov
    
    * fix: add istanbul ignore
    
    * fix
    
    * feat: use visibilityHeight=0 replace visible=true
    
    * snap
    
    * cov
    
    * test case
    
    * test case
    
    * fix
    
    * fix cov
    
    * fix test
    
    * simplify code
    
    * rename function

--- components/float-button/__tests__/back-top.test.tsx
+++ components/float-button/__tests__/back-top.test.tsx
@@ -2,52 +2,53 @@ import React from 'react';
 import FloatButton from '..';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
-import { fireEvent, render, sleep } from '../../../tests/utils';
+import { fireEvent, render, waitFakeTimer } from '../../../tests/utils';
 
 const { BackTop } = FloatButton;
 describe('BackTop', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
   mountTest(BackTop);
   rtlTest(BackTop);
 
   it('should scroll to top after click it', async () => {
-    const { container } = render(<BackTop visible visibilityHeight={-1} />);
+    const { container } = render(<BackTop />);
     const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((_, y) => {
       window.scrollY = y;
       window.pageYOffset = y;
       document.documentElement.scrollTop = y;
     });
     window.scrollTo(0, 400);
+    await waitFakeTimer();
     expect(document.documentElement.scrollTop).toBe(400);
-    fireEvent.click(container.querySelector('.ant-float-btn')!);
-    await sleep(500);
+    fireEvent.click(container.querySelector<HTMLButtonElement>('.ant-float-btn')!);
+    await waitFakeTimer();
     expect(document.documentElement.scrollTop).toBe(0);
     scrollToSpy.mockRestore();
   });
 
   it('support onClick', () => {
     const onClick = jest.fn();
-    const { container } = render(<BackTop visible visibilityHeight={-1} onClick={onClick} />);
-    const scrollToSpy = jest.spyOn(window, 'scrollTo').mockImplementation((_, y) => {
-      window.scrollY = y;
-      window.pageYOffset = y;
-    });
-    document.dispatchEvent(new Event('scroll'));
-    window.scrollTo(0, 400);
-    fireEvent.click(container.querySelector('.ant-float-btn')!);
+    const { container } = render(<BackTop onClick={onClick} visibilityHeight={0} />);
+    fireEvent.click(container.querySelector<HTMLButtonElement>('.ant-float-btn')!);
     expect(onClick).toHaveBeenCalled();
-    scrollToSpy.mockRestore();
   });
 
-  it('invalid target', () => {
+  it('support invalid target', () => {
     const onClick = jest.fn();
-    const { container } = render(<BackTop onClick={onClick} visible target={undefined} />);
-    fireEvent.click(container.querySelector('.ant-float-btn')!);
+    const { container } = render(
+      <BackTop onClick={onClick} visibilityHeight={0} target={undefined} />,
+    );
+    fireEvent.click(container.querySelector<HTMLButtonElement>('.ant-float-btn')!);
     expect(onClick).toHaveBeenCalled();
   });
 
   it('pass style to float button', () => {
-    const { container } = render(<BackTop style={{ color: 'red' }} visible target={undefined} />);
-    const btn = container.querySelector('.ant-float-btn')!;
-    expect(btn).toHaveAttribute('style', 'color: red;');
+    const { container } = render(<BackTop style={{ color: 'red' }} visibilityHeight={0} />);
+    expect(container.querySelector<HTMLButtonElement>('.ant-float-btn')?.style.color).toBe('red');
   });
 });
