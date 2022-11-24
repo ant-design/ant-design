0721f1a51fbb16f4f3f4485d47300dabe9af7743
MadCcc <1075746765@qq.com>
2022-11-24 21:01:20 +0800

    docs: token meta

--- scripts/generate-token-meta.js
+++ scripts/generate-token-meta.js
@@ -1,6 +1,18 @@
 const TypeDoc = require('typedoc');
 const fs = require('fs-extra');
 
+const getTokenList = (list, source) =>
+  list
+    .filter((item) => !item.comment?.blockTags.some((tag) => tag.tag === '@internal'))
+    .map((item) => ({
+      source,
+      name: item.name,
+      type: item.type.toString(),
+      desc: item.comment?.blockTags?.find((tag) => tag.tag === '@desc')?.content[0]?.text || '-',
+      descEn:
+        item.comment?.blockTags?.find((tag) => tag.tag === '@descEn')?.content[0]?.text || '-',
+    }));
+
 function main() {
   const app = new TypeDoc.Application();
 
@@ -17,22 +29,43 @@ function main() {
 
   if (project) {
     // Project may not have converted correctly
-    const output = 'components/version/token-meta-test.json';
+    const output = 'components/version/token-meta.json';
     const tokenMeta = {};
+    let presetColors = [];
     project.children.forEach((type) => {
       if (type.name === 'SeedToken') {
-        tokenMeta.seedToken = type.children.map((item) => ({
-          source: 'seed',
-          desc: item.comment?.blockTags?.find((tag) => tag.tag === '@desc')?.text || '',
-          descEn: item.comment?.blockTags?.find((tag) => tag.tag === '@descEn')?.text || '',
-        }));
+        tokenMeta.seed = getTokenList(type.children, 'seed');
+      } else if (type.name === 'MapToken') {
+        tokenMeta.map = getTokenList(type.children, 'map');
+      } else if (type.name === 'AliasToken') {
+        tokenMeta.alias = getTokenList(type.children, 'alias');
+      } else if (type.name === 'PresetColors') {
+        presetColors = type.type.target.elements.map((item) => item.value);
       }
     });
 
+    // Exclude preset colors
+    tokenMeta.seed = tokenMeta.seed.filter(
+      (item) => !presetColors.some((color) => item.name.startsWith(color)),
+    );
+    tokenMeta.map = tokenMeta.map.filter(
+      (item) => !presetColors.some((color) => item.name.startsWith(color)),
+    );
+    tokenMeta.alias = tokenMeta.alias.filter(
+      (item) => !presetColors.some((color) => item.name.startsWith(color)),
+    );
+
+    tokenMeta.alias = tokenMeta.alias.filter(
+      (item) => !tokenMeta.map.some((mapItem) => mapItem.name === item.name),
+    );
+    tokenMeta.map = tokenMeta.map.filter(
+      (item) => !tokenMeta.seed.some((seedItem) => seedItem.name === item.name),
+    );
+
     fs.writeJsonSync(output, tokenMeta, 'utf8');
-    consoloe.log(`✅ Token Meta has been written to ${output}`);
+    // eslint-disable-next-line no-console
+    console.log(`✅  Token Meta has been written to ${output}`);
   }
 }
 
-// eslint-disable-next-line no-console
 main();
