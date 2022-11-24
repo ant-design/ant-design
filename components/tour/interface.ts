5097f3a8cd2795adf9bcba5ca195b4a7626e74ee
Cat-XHS <1349021570@qq.com>
2022-11-24 15:38:42 +0800

    feat(tour): TourStepProps ButtonProps ts definition update (#38939)
    * feat: tour ts define update
    
    * test: add more unit test

--- components/tour/interface.ts
+++ components/tour/interface.ts
@@ -15,8 +15,18 @@ export type TourProps = Omit<RCTourProps, 'renderPanel'> & {
 
 export interface TourStepProps extends RCTourStepProps {
   cover?: ReactNode; // 展示的图片或者视频
-  nextButtonProps?: { children?: ReactNode; onClick?: () => void };
-  prevButtonProps?: { children?: ReactNode; onClick?: () => void };
+  nextButtonProps?: {
+    children?: ReactNode;
+    onClick?: () => void;
+    className?: string;
+    style?: React.CSSProperties;
+  };
+  prevButtonProps?: {
+    children?: ReactNode;
+    onClick?: () => void;
+    className?: string;
+    style?: React.CSSProperties;
+  };
   stepRender?: (current: number, total: number) => ReactNode;
   type?: 'default' | 'primary'; //	default	类型，影响底色与文字颜色
 }
