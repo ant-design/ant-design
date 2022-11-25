4b178b1ebf824ff761fbf73bbf9d2a655e75a7c5
栗嘉男 <574980606@qq.com>
2022-11-25 19:05:49 +0800

    docs: update demo use Space

--- components/progress/demo/linecap.tsx
+++ components/progress/demo/linecap.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Progress } from 'antd';
+import { Progress, Space } from 'antd';
 
 const App: React.FC = () => (
   <>
     <Progress strokeLinecap="butt" percent={75} />
-    <Progress strokeLinecap="butt" type="circle" percent={75} />
-    <Progress strokeLinecap="butt" type="dashboard" percent={75} style={{ marginLeft: 8 }} />
+    <Space wrap>
+      <Progress strokeLinecap="butt" type="circle" percent={75} />
+      <Progress strokeLinecap="butt" type="dashboard" percent={75} />
+    </Space>
   </>
 );
 
