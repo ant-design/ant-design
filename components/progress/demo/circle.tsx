4ae1870eacfd0d15c3c4f9219883f66655485782
栗嘉男 <574980606@qq.com>
2022-11-25 17:43:09 +0800

    docs: update demo

--- components/progress/demo/circle.tsx
+++ components/progress/demo/circle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Progress, Space } from 'antd';
 
 const App: React.FC = () => (
-  <Space>
+  <Space wrap>
     <Progress type="circle" percent={75} />
     <Progress type="circle" percent={70} status="exception" />
     <Progress type="circle" percent={100} />
--- components/progress/demo/format.tsx
+++ components/progress/demo/format.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Progress, Space } from 'antd';
 
 const App: React.FC = () => (
-  <Space>
+  <Space wrap>
     <Progress type="circle" percent={75} format={(percent) => `${percent} Days`} />
     <Progress type="circle" percent={100} format={() => 'Done'} />
   </Space>
