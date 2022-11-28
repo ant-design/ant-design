b8c296f452ecace09314455b1e8543a681327379
黑雨 <wangning4567@163.com>
2022-11-28 19:38:04 +0800

    feat: update app

---
+++ components/app/demo/modal.tsx
@@ -0,0 +1,19 @@
+import React, { useRef, useState } from 'react';
+import { App, Button } from 'antd';
+
+export default () => {
+  const { Modal } = App.useApp();
+
+  const showModal = () => {
+    Modal.warning({
+      title: 'This is a warning message',
+      content: 'some messages...some messages...',
+    });
+  };
+
+  return (
+    <Button type="primary" onClick={showModal}>
+      Open Modal
+    </Button>
+  );
+};
