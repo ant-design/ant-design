f0dcb20a9522945e95046e0a6922b1f096432ae4
栗嘉男 <574980606@qq.com>
2022-11-30 14:04:23 +0800

    update test case

--- components/qrcode/__tests__/index.test.tsx
+++ components/qrcode/__tests__/index.test.tsx
@@ -7,23 +7,36 @@ import { render } from '../../../tests/utils';
 describe('QRCode test', () => {
   mountTest(QRCode);
   rtlTest(QRCode);
-  it('should correct render ', () => {
+
+  it('should correct render', () => {
     const { container } = render(<QRCode value="test" />);
-    expect(container.firstChild).toMatchSnapshot();
+    expect(
+      container
+        ?.querySelector<HTMLDivElement>('.ant-qrcode')
+        ?.querySelector<HTMLCanvasElement>('canvas'),
+    ).toBeTruthy();
+    expect(container).toMatchSnapshot();
   });
+
   it('should render `null` and console Error when value not exist', () => {
     const errSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
     const { container } = render(<QRCode value={undefined as unknown as string} />);
     expect(container.firstChild).toBe(null);
+    expect(container.firstChild).toMatchSnapshot();
     expect(errSpy).toHaveBeenCalledWith('Warning: [antd: QRCode] need to receive `value` props');
     errSpy.mockRestore();
   });
 
-  it('support icon', () => {
+  it('support custom icon', () => {
     const { container } = render(<QRCode value="test" icon="test" />);
-    expect(container.querySelector<HTMLImageElement>('img')).toBeTruthy();
+    expect(
+      container
+        ?.querySelector<HTMLDivElement>('.ant-qrcode')
+        ?.querySelector<HTMLImageElement>('img'),
+    ).toBeTruthy();
   });
-  it('support size', () => {
+
+  it('support custom size', () => {
     const { container } = render(<QRCode value="test" size={100} />);
     const wapper = container.querySelector<HTMLDivElement>('.ant-qrcode');
     expect(wapper?.style?.width).toBe('100px');
