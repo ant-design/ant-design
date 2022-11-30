46b8dd82a6deb875b7ca71b441abd90e56105bfa
栗嘉男 <574980606@qq.com>
2022-11-30 14:16:24 +0800

    update demo

--- components/qrcode/demo/errorlevel.tsx
+++ components/qrcode/demo/errorlevel.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import type { QRCodeProps } from 'antd';
 import { Segmented, QRCode } from 'antd';
 
 const App: React.FC = () => {
   const [level, setLevel] = useState<string | number>('L');
   return (
     <>
-      <QRCode style={{ marginBottom: 16 }} errorLevel={level} value="https://ant.design/" />
+      <QRCode
+        value="https://ant.design/"
+        style={{ marginBottom: 16 }}
+        errorLevel={level as QRCodeProps['errorLevel']}
+      />
       <Segmented options={['L', 'M', 'Q', 'H']} value={level} onChange={setLevel} />
     </>
   );
