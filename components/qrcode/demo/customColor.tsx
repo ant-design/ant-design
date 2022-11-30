df523cf28ff0a503492168790e9f5952f858c9d2
栗嘉男 <574980606@qq.com>
2022-11-30 22:39:18 +0800

    fix: fix demo

--- components/qrcode/demo/customColor.tsx
+++ components/qrcode/demo/customColor.tsx
@@ -1,44 +1,25 @@
 import React, { useState } from 'react';
-import { QRCode, Button, Popover, Space } from 'antd';
+import { QRCode, Button, Popover } from 'antd';
 import type { ColorResult } from 'react-color';
 import { SketchPicker } from 'react-color';
 
 const App: React.FC = () => {
-  const [bgColor, setBgColor] = useState<string>('#fff');
-  const [fgColor, setFgColor] = useState<string>('#000');
+  const [color, setColor] = useState<string>('#000');
 
-  const onBgColorChange = ({ hex }: ColorResult) => {
-    setBgColor(hex);
-  };
-
-  const onFgColorChange = ({ hex }: ColorResult) => {
-    setFgColor(hex);
+  const onColorChange = ({ hex }: ColorResult) => {
+    setColor(hex);
   };
 
   return (
     <>
-      <QRCode
-        value="https://ant.design/"
-        bgColor={bgColor}
-        fgColor={fgColor}
-        style={{ marginBottom: 16 }}
-      />
-      <Space>
-        <Popover
-          trigger="click"
-          placement="bottom"
-          content={<SketchPicker color={bgColor} onChange={onBgColorChange} />}
-        >
-          <Button>Background Color</Button>
-        </Popover>
-        <Popover
-          trigger="click"
-          placement="bottom"
-          content={<SketchPicker color={fgColor} onChange={onFgColorChange} />}
-        >
-          <Button>Foreground Color</Button>
-        </Popover>
-      </Space>
+      <QRCode value="https://ant.design/" color={color} style={{ marginBottom: 12 }} />
+      <Popover
+        trigger="click"
+        placement="bottom"
+        content={<SketchPicker color={color} onChange={onColorChange} />}
+      >
+        <Button>Change Color</Button>
+      </Popover>
     </>
   );
 };
