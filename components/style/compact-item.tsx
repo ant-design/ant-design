cccbab7c6d26686c378ccb95239913e2507d2a54
Yuki Zhang <foryuki@outlook.com>
2022-12-07 17:11:25 +0800

    fix: disable button focus style in Space.Compact (#39157)
    * fix: button hover style in Space.Compact
    
    * fix: button hover style in Space.Compact
    
    * style: disable button focus effect when in compact-item
    
    * chore: update comments
    
    * fix: improve key name
    
    * fix: remove focus style

--- components/style/compact-item.tsx
+++ components/style/compact-item.tsx
@@ -2,26 +2,40 @@
 import type { CSSObject } from '@ant-design/cssinjs';
 import type { DerivativeToken } from '../theme/internal';
 
+interface CompactItemOptions {
+  focus?: boolean;
+  /**
+   * Some component borders are implemented on child elements
+   * like `Select`
+   */
+  borderElCls?: string;
+  /**
+   * Some components have special `focus` className especially with popovers
+   * like `Select` and `DatePicker`
+   */
+  focusElCls?: string;
+}
+
 // handle border collapse
-function compactItemBorder(
-  token: DerivativeToken,
-  borderedItemCls?: string,
-  popoverFocusedCls?: string,
-): CSSObject {
-  const childCombinator = borderedItemCls ? '> *' : '';
+function compactItemBorder(token: DerivativeToken, options: CompactItemOptions): CSSObject {
+  const childCombinator = options.borderElCls ? '> *' : '';
+  const hoverEffects = ['hover', options.focus ? 'focus' : null, 'active']
+    .filter(Boolean)
+    .map((n) => `&:${n} ${childCombinator}`)
+    .join(',');
   return {
     '&-item:not(&-last-item)': {
       marginInlineEnd: -token.lineWidth,
     },
 
     '&-item': {
-      [`&:hover ${childCombinator}, &:focus ${childCombinator}, &:active ${childCombinator}`]: {
+      [hoverEffects]: {
         zIndex: 2,
       },
 
-      ...(popoverFocusedCls
+      ...(options.focusElCls
         ? {
-            [`&${popoverFocusedCls}`]: {
+            [`&${options.focusElCls}`]: {
               zIndex: 2,
             },
           }
@@ -35,8 +49,8 @@ function compactItemBorder(
 }
 
 // handle border-radius
-function compactItemBorderRadius(prefixCls: string, borderedElementCls?: string): CSSObject {
-  const childCombinator = borderedElementCls ? `> ${borderedElementCls}` : '';
+function compactItemBorderRadius(prefixCls: string, options: CompactItemOptions): CSSObject {
+  const childCombinator = options.borderElCls ? `> ${options.borderElCls}` : '';
 
   return {
     [`&-item:not(&-first-item):not(&-last-item) ${childCombinator}`]: {
@@ -64,18 +78,12 @@ function compactItemBorderRadius(prefixCls: string, borderedElementCls?: string)
 export function genCompactItemStyle(
   token: DerivativeToken,
   prefixCls: string,
-  /** Some component borders are implemented on child elements like `Select` */
-  borderedElementCls?: string,
-  /**
-   * Some components have special `focus` className especially with popovers like `Select` and
-   * `DatePicker`
-   */
-  popoverFocusedCls?: string,
+  options: CompactItemOptions = { focus: true },
 ): CSSObject {
   return {
     '&-compact': {
-      ...compactItemBorder(token, borderedElementCls, popoverFocusedCls),
-      ...compactItemBorderRadius(prefixCls, borderedElementCls),
+      ...compactItemBorder(token, options),
+      ...compactItemBorderRadius(prefixCls, options),
     },
   };
 }
