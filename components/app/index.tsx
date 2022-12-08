4d000592e48debdb066bfd451bff5de1eea55aa3
lijianan <574980606@qq.com>
2022-12-08 17:06:21 +0800

    Update index.tsx

--- components/app/index.tsx
+++ components/app/index.tsx
@@ -18,7 +18,7 @@ export type AppProps = {
 
 const useApp = () => React.useContext<useAppProps>(AppContext);
 
-const App: React.FC<AppProps> & { useApp: () => useAppProps } = (props) => {
+const App: React.FC<AppProps> & { useApp: () => useAppProps; __ANT_APP: boolean } = (props) => {
   const { prefixCls: customizePrefixCls, children, className } = props;
   const { getPrefixCls } = useContext<ConfigConsumerProps>(ConfigContext);
   const prefixCls = getPrefixCls('app', customizePrefixCls);
