c7acf9db77e9fcf3b1fa99d23c618526422cec9a
二货机器人 <smith3816@gmail.com>
2022-12-08 16:27:47 +0800

    chore: rm console

--- .dumi/hooks/useMenu.tsx
+++ .dumi/hooks/useMenu.tsx
@@ -15,8 +15,6 @@ const useMenu = (options: UseMenuOptions = {}): [MenuProps['items'], string] => 
   const sidebarData = useSidebarData();
   const { before, after } = options;
 
-  console.log('sidebarData:', sidebarData);
-
   const menuItems = useMemo<MenuProps['items']>(() => {
     const sidebarItems = [...(sidebarData ?? [])];
 
@@ -118,7 +116,7 @@ const useMenu = (options: UseMenuOptions = {}): [MenuProps['items'], string] => 
           }
         } else {
           const list = group.children || [];
-          // 如果有 date 字段，我们就对齐进行排序
+          // 如果有 date 字段，我们就对其进行排序
           if (list.every((info) => info?.frontmatter?.date)) {
             list.sort((a, b) => (a.frontmatter.date > b.frontmatter.date ? -1 : 1));
           }
