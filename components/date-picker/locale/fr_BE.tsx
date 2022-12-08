ab1ebf3574f7ede4cd473cf8e21a8a03c6f21fb9
azro <raphi.352@hotmail.fr>
2022-12-08 20:49:49 +0100

    Add missing fr_BE internationalization

--- components/date-picker/locale/fr_BE.tsx
+++ components/date-picker/locale/fr_BE.tsx
@@ -6,7 +6,14 @@ import type { PickerLocale } from '../generatePicker';
 const locale: PickerLocale = {
   lang: {
     placeholder: 'Sélectionner une date',
+    yearPlaceholder: 'Sélectionner une année',
+    quarterPlaceholder: 'Sélectionner un trimestre',
+    monthPlaceholder: 'Sélectionner un mois',
+    weekPlaceholder: 'Sélectionner une semaine',
     rangePlaceholder: ['Date de début', 'Date de fin'],
+    rangeYearPlaceholder: ['Année de début', 'Année de fin'],
+    rangeMonthPlaceholder: ['Mois de début', 'Mois de fin'],
+    rangeWeekPlaceholder: ['Semaine de début', 'Semaine de fin'],
     ...CalendarLocale,
   },
   timePickerLocale: {
