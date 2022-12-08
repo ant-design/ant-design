3a8fb47d510b4bfe70e9f5197d8dd4d463d8c69a
yoyo837 <yoyo837@hotmail.com>
2022-12-08 14:27:41 +0800

    chore: 阶段性兼容

---
+++ components/locale-provider/LocaleReceiver.tsx
@@ -0,0 +1,6 @@
+// locale-provider 文件夹的移除需要修改 @ant-design/tools 和 antd-img-crop
+import LocaleReceiver from '../locale/LocaleReceiver';
+
+export * from '../locale/LocaleReceiver';
+
+export default LocaleReceiver;
