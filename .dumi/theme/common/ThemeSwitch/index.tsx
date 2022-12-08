4b78a513d5c1f35661283392d6474cc22351f098
afc163 <afc163@gmail.com>
2022-12-08 15:30:49 +0800

    docs: theme should be in searchParams

--- .dumi/theme/common/ThemeSwitch/index.tsx
+++ .dumi/theme/common/ThemeSwitch/index.tsx
@@ -1,68 +1,34 @@
 import React from 'react';
-import { FloatButton, theme } from 'antd';
+import { FloatButton } from 'antd';
 import { DarkTheme, Light, CompactTheme } from 'antd-token-previewer/es/icons';
 import ThemeIcon from './ThemeIcon';
 
-const { defaultAlgorithm, darkAlgorithm, compactAlgorithm } = theme;
-
 export type ThemeSwitchProps = {
-  value: typeof defaultAlgorithm[];
-  onChange: (value: typeof defaultAlgorithm[]) => void;
+  value?: 'light' | 'dark' | 'compact';
+  onChange: (value: 'light' | 'dark' | 'compact') => void;
 };
 
-const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ value, onChange }) => {
-  const handleLightSwitch = () => {
-    let newValue = [...value];
-    if (value.includes(darkAlgorithm)) {
-      newValue = newValue.filter((item) => item !== darkAlgorithm);
-    }
-    if (!value.includes(defaultAlgorithm)) {
-      newValue.unshift(defaultAlgorithm);
-    }
-    onChange(newValue);
-  };
-
-  const handleDarkSwitch = () => {
-    let newValue = [...value];
-    if (value.includes(defaultAlgorithm)) {
-      newValue = newValue.filter((item) => item !== defaultAlgorithm);
-    }
-    if (!value.includes(darkAlgorithm)) {
-      newValue.push(darkAlgorithm);
-    }
-    onChange(newValue);
-  };
-
-  const handleCompactSwitch = () => {
-    if (value.includes(compactAlgorithm)) {
-      onChange(value.filter((item) => item !== compactAlgorithm));
-    } else {
-      onChange([...value, compactAlgorithm]);
-    }
-  };
-
-  return (
-    <FloatButton.Group trigger="click" icon={<ThemeIcon />}>
-      <FloatButton
-        icon={<Light />}
-        type={value.includes(defaultAlgorithm) ? 'primary' : 'default'}
-        onClick={handleLightSwitch}
-        tooltip="Light"
-      />
-      <FloatButton
-        icon={<DarkTheme />}
-        type={value.includes(darkAlgorithm) ? 'primary' : 'default'}
-        onClick={handleDarkSwitch}
-        tooltip="Dark"
-      />
-      <FloatButton
-        icon={<CompactTheme />}
-        type={value.includes(compactAlgorithm) ? 'primary' : 'default'}
-        onClick={handleCompactSwitch}
-        tooltip="Compact"
-      />
-    </FloatButton.Group>
-  );
-};
+const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ value, onChange }) => (
+  <FloatButton.Group trigger="click" icon={<ThemeIcon />}>
+    <FloatButton
+      icon={<Light />}
+      type={value === 'light' || !value ? 'primary' : 'default'}
+      onClick={() => onChange('light')}
+      tooltip="Light"
+    />
+    <FloatButton
+      icon={<DarkTheme />}
+      type={value === 'dark' ? 'primary' : 'default'}
+      onClick={() => onChange('dark')}
+      tooltip="Dark"
+    />
+    <FloatButton
+      icon={<CompactTheme />}
+      type={value === 'compact' ? 'primary' : 'default'}
+      onClick={() => onChange('compact')}
+      tooltip="Compact"
+    />
+  </FloatButton.Group>
+);
 
 export default ThemeSwitch;
--- .dumi/theme/layouts/GlobalLayout.tsx
+++ .dumi/theme/layouts/GlobalLayout.tsx
@@ -1,7 +1,6 @@
-import React, { startTransition, useLayoutEffect } from 'react';
-import { useOutlet } from 'dumi';
+import React from 'react';
+import { useOutlet, useSearchParams } from 'dumi';
 import { ConfigProvider, theme as antdTheme } from 'antd';
-import type { ThemeConfig } from 'antd/es/config-provider/context';
 import { createCache, StyleProvider } from '@ant-design/cssinjs';
 import ThemeSwitch from '../common/ThemeSwitch';
 import useLocation from '../../hooks/useLocation';
@@ -11,9 +10,7 @@ if (typeof global !== 'undefined') {
   (global as any).styleCache = styleCache;
 }
 
-const ANT_DESIGN_SITE_THEME = 'antd-site-theme';
-
-const getAlgorithm = (theme: string) => {
+const getAlgorithm = (theme: 'light' | 'dark' | 'compact') => {
   if (theme === 'dark') {
     return antdTheme.darkAlgorithm;
   }
@@ -23,65 +20,28 @@ const getAlgorithm = (theme: string) => {
   return antdTheme.defaultAlgorithm;
 };
 
-const getThemeString = (algorithm: typeof antdTheme.defaultAlgorithm) => {
-  if (algorithm === antdTheme.darkAlgorithm) {
-    return 'dark';
-  }
-  if (algorithm === antdTheme.compactAlgorithm) {
-    return 'compact';
-  }
-  return 'light';
-};
-
 const GlobalLayout: React.FC = () => {
   const outlet = useOutlet();
   const { pathname } = useLocation();
-
-  const [theme, setTheme] = React.useState<ThemeConfig>({
-    algorithm: [antdTheme.defaultAlgorithm],
-  });
-
-  const handleThemeChange = (newTheme: ThemeConfig, ignoreAlgorithm: boolean = true) => {
-    const nextTheme = { ...newTheme };
-    if (ignoreAlgorithm) {
-      nextTheme.algorithm = theme.algorithm;
-    }
-    setTheme(nextTheme);
-    localStorage.setItem(
-      ANT_DESIGN_SITE_THEME,
-      JSON.stringify(nextTheme, (key, value) => {
-        if (key === 'algorithm') {
-          return Array.isArray(value) ? value.map((item) => getThemeString(item)) : ['light'];
-        }
-        return value;
-      }),
-    );
+  const [searchParams, setSearchParams] = useSearchParams();
+  const theme = searchParams.get('theme') as 'light' | 'dark' | 'compact';
+  const handleThemeChange = (value: 'light' | 'dark' | 'compact') => {
+    setSearchParams({
+      ...searchParams,
+      theme: value === 'light' ? undefined : value,
+    });
   };
-
-  useLayoutEffect(() => {
-    const localTheme = localStorage.getItem(ANT_DESIGN_SITE_THEME);
-    if (localTheme) {
-      const themeConfig = JSON.parse(localTheme);
-      if (themeConfig.algorithm) {
-        themeConfig.algorithm = themeConfig.algorithm.map((item: string) => getAlgorithm(item));
-      } else {
-        themeConfig.algorithm = [antdTheme.defaultAlgorithm];
-      }
-      startTransition(() => {
-        setTheme(themeConfig);
-      });
-    }
-  }, []);
 
   return (
     <StyleProvider cache={styleCache}>
-      <ConfigProvider theme={theme}>
+      <ConfigProvider
+        theme={{
+          algorithm: [getAlgorithm(theme)],
+        }}
+      >
         {outlet}
         {!pathname.startsWith('/~demos') && (
-          <ThemeSwitch
-            value={theme.algorithm as []}
-            onChange={(value) => handleThemeChange({ ...theme, algorithm: value }, false)}
-          />
+          <ThemeSwitch value={theme} onChange={handleThemeChange} />
         )}
       </ConfigProvider>
     </StyleProvider>
