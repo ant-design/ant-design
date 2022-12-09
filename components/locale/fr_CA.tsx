221e8bd8040552bc4e827b31c6732983b31dcbe2
azro352 <35503478+azro352@users.noreply.github.com>
2022-12-09 05:47:44 +0100

    feat: add missing fr_CA internationalization (#39416)

--- components/locale/fr_CA.tsx
+++ components/locale/fr_CA.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-template-curly-in-string */
 import Pagination from 'rc-pagination/lib/locale/fr_CA';
 import Calendar from '../calendar/locale/fr_CA';
 import DatePicker from '../date-picker/locale/fr_CA';
 import type { Locale } from '../locale-provider';
 import TimePicker from '../time-picker/locale/fr_CA';
+
+const typeTemplate = "La valeur du champ ${label} n'est pas valide pour le type ${type}";
 
 const localeValues: Locale = {
   locale: 'fr',
@@ -14,9 +17,13 @@ const localeValues: Locale = {
     filterTitle: 'Filtrer',
     filterConfirm: 'OK',
     filterReset: 'Réinitialiser',
-    filterCheckall: 'Sélectionner la page actuelle',
+    filterEmptyText: 'Aucun filtre',
+    filterCheckall: 'Tout sélectionner',
+    filterSearchPlaceholder: 'Chercher dans les filtres',
+    emptyText: 'Aucune donnée',
     selectAll: 'Sélectionner la page actuelle',
     selectInvert: 'Inverser la sélection de la page actuelle',
+    selectNone: 'Désélectionner toutes les données',
     selectionAll: 'Sélectionner toutes les données',
     sortTitle: 'Trier',
     expand: 'Développer la ligne',
@@ -39,9 +46,12 @@ const localeValues: Locale = {
     searchPlaceholder: 'Rechercher',
     itemUnit: 'élément',
     itemsUnit: 'éléments',
-  },
-  Empty: {
-    description: 'Aucune donnée',
+    remove: 'Désélectionner',
+    selectCurrent: 'Sélectionner la page actuelle',
+    removeCurrent: 'Désélectionner la page actuelle',
+    selectAll: 'Sélectionner toutes les données',
+    removeAll: 'Désélectionner toutes les données',
+    selectInvert: 'Inverser la sélection de la page actuelle',
   },
   Upload: {
     uploading: 'Téléchargement...',
@@ -49,6 +59,12 @@ const localeValues: Locale = {
     uploadError: 'Erreur de téléchargement',
     previewFile: 'Fichier de prévisualisation',
     downloadFile: 'Télécharger un fichier',
+  },
+  Empty: {
+    description: 'Aucune donnée',
+  },
+  Icon: {
+    icon: 'icône',
   },
   Text: {
     edit: 'Éditer',
@@ -58,6 +74,59 @@ const localeValues: Locale = {
   },
   PageHeader: {
     back: 'Retour',
+  },
+  Form: {
+    optional: '(optionnel)',
+    defaultValidateMessages: {
+      default: 'Erreur de validation pour le champ ${label}',
+      required: 'Le champ ${label} est obligatoire',
+      enum: 'La valeur du champ ${label} doit être parmi [${enum}]',
+      whitespace: 'La valeur du champ ${label} ne peut pas être vide',
+      date: {
+        format: "La valeur du champ ${label} n'est pas au format date",
+        parse: 'La valeur du champ ${label} ne peut pas être convertie vers une date',
+        invalid: "La valeur du champ ${label} n'est pas une date valide",
+      },
+      types: {
+        string: typeTemplate,
+        method: typeTemplate,
+        array: typeTemplate,
+        object: typeTemplate,
+        number: typeTemplate,
+        date: typeTemplate,
+        boolean: typeTemplate,
+        integer: typeTemplate,
+        float: typeTemplate,
+        regexp: typeTemplate,
+        email: typeTemplate,
+        url: typeTemplate,
+        hex: typeTemplate,
+      },
+      string: {
+        len: 'La taille du champ ${label} doit être de ${len} caractères',
+        min: 'La taille du champ ${label} doit être au minimum de ${min} caractères',
+        max: 'La taille du champ ${label} doit être au maximum de ${max} caractères',
+        range: 'La taille du champ ${label} doit être entre ${min} et ${max} caractères',
+      },
+      number: {
+        len: 'La valeur du champ ${label} doit être égale à ${len}',
+        min: 'La valeur du champ ${label} doit être plus grande que ${min}',
+        max: 'La valeur du champ ${label} doit être plus petit que ${max}',
+        range: 'La valeur du champ ${label} doit être entre ${min} et ${max}',
+      },
+      array: {
+        len: 'La taille du tableau ${label} doit être de ${len}',
+        min: 'La taille du tableau ${label} doit être au minimum de ${min}',
+        max: 'La taille du tableau ${label} doit être au maximum de ${max}',
+        range: 'La taille du tableau ${label} doit être entre ${min}-${max}',
+      },
+      pattern: {
+        mismatch: 'La valeur du champ ${label} ne correspond pas au modèle ${pattern}',
+      },
+    },
+  },
+  Image: {
+    preview: 'Aperçu',
   },
 };
 
