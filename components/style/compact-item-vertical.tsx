86f6de01ebb391cedd1af543bcd778a870dfda13
foryuki <foryuki@outlook.com>
2022-12-11 22:18:54 +0800

    fix: lint

--- components/style/compact-item-vertical.tsx
+++ components/style/compact-item-vertical.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import type { CSSInterpolation, CSSObject } from '@ant-design/cssinjs';
 import type { DerivativeToken, FullToken } from '../theme/internal';
-import { OverrideComponent } from '../theme/util/genComponentStyleHook';
+import type { OverrideComponent } from '../theme/util/genComponentStyleHook';
 
 function compactItemVerticalBorder(token: DerivativeToken): CSSObject {
   return {
