a2fa1166af37494ac0ca999a8a2880f61aaa5ecc
王清山 <576222103@qq.com>
2022-12-11 19:30:38 +0800

    fix(antd/transfer): transfer performance optimization while dataSource is large

---
+++ components/_util/transKeys.ts
@@ -0,0 +1,25 @@
+export type mapItem = {
+  k: string;
+  i: number;
+};
+export type MapType = Record<string, mapItem>;
+
+export const groupKeysMap = (keys: string[]) => {
+  const map: MapType = {};
+  keys.forEach((k, i) => {
+    map[k] = { k, i };
+  });
+  return map;
+};
+export function groupDisabledKeysMap<RecordType extends any[]>(dataSource: RecordType) {
+  const map: MapType = {};
+  dataSource.forEach((d, i) => {
+    if (d.disabled) {
+      map[d.key] = {
+        k: d.key,
+        i,
+      };
+    }
+  });
+  return map;
+}
