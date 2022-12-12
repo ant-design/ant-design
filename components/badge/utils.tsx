e68a8495b99776fc089d7635f4b701ebc2e7c9af
栗嘉男 <574980606@qq.com>
2022-12-12 12:30:44 +0800

    type: remove tuple

--- components/badge/utils.tsx
+++ components/badge/utils.tsx
@@ -1,6 +1,6 @@
-import { PresetColorTypes } from '../_util/colors';
+import { PresetColorType, PresetColorTypes } from '../_util/colors';
 
 // eslint-disable-next-line import/prefer-default-export
 export function isPresetColor(color?: string): boolean {
-  return (PresetColorTypes as any[]).includes(color);
+  return PresetColorTypes.includes(color as PresetColorType);
 }
