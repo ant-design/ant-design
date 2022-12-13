821b98febc4ebe4cb5802fd61cafa5638895d909
栗嘉男 <574980606@qq.com>
2022-12-13 10:40:39 +0800

    fix type

--- components/badge/utils.tsx
+++ components/badge/utils.tsx
@@ -1,6 +1,6 @@
 import { PresetColorTypes } from '../_util/colors';
 
 // eslint-disable-next-line import/prefer-default-export
-export function isPresetColor(color?: string): boolean {
-  return (PresetColorTypes as unknown as string[]).includes(color!);
+export function isPresetColor(color?: any): boolean {
+  return PresetColorTypes.includes(color);
 }
