1a43b5c805b4b9ebeb8a02d639933e7c350c293b
栗嘉男 <574980606@qq.com>
2022-12-13 21:02:32 +0800

    use href

--- .dumi/theme/common/ThemeSwitch/index.tsx
+++ .dumi/theme/common/ThemeSwitch/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FloatButton } from 'antd';
-import { FormattedMessage, useNavigate } from 'dumi';
+import { FormattedMessage } from 'dumi';
 import { DarkTheme, CompactTheme } from 'antd-token-previewer/es/icons';
 import { BgColorsOutlined } from '@ant-design/icons';
 import ThemeIcon from './ThemeIcon';
@@ -12,43 +12,38 @@ export type ThemeSwitchProps = {
   onChange: (value: ThemeName[]) => void;
 };
 
-const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ value, onChange }) => {
-  const navigate = useNavigate();
-  return (
-    <FloatButton.Group trigger="click" icon={<ThemeIcon />}>
-      <FloatButton
-        icon={<BgColorsOutlined />}
-        tooltip={<FormattedMessage id="app.footer.theme" />}
-        onClick={() => {
-          navigate('/theme-editor');
-        }}
-      />
-      <FloatButton
-        icon={<DarkTheme />}
-        type={value.includes('dark') ? 'primary' : 'default'}
-        onClick={() => {
-          if (value.includes('dark')) {
-            onChange(value.filter((theme) => theme !== 'dark'));
-          } else {
-            onChange([...value, 'dark']);
-          }
-        }}
-        tooltip={<FormattedMessage id="app.theme.switch.dark" />}
-      />
-      <FloatButton
-        icon={<CompactTheme />}
-        type={value.includes('compact') ? 'primary' : 'default'}
-        onClick={() => {
-          if (value.includes('compact')) {
-            onChange(value.filter((theme) => theme !== 'compact'));
-          } else {
-            onChange([...value, 'compact']);
-          }
-        }}
-        tooltip={<FormattedMessage id="app.theme.switch.compact" />}
-      />
-    </FloatButton.Group>
-  );
-};
+const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ value, onChange }) => (
+  <FloatButton.Group trigger="click" icon={<ThemeIcon />}>
+    <FloatButton
+      icon={<BgColorsOutlined />}
+      tooltip={<FormattedMessage id="app.footer.theme" />}
+      href="/theme-editor"
+    />
+    <FloatButton
+      icon={<DarkTheme />}
+      type={value.includes('dark') ? 'primary' : 'default'}
+      onClick={() => {
+        if (value.includes('dark')) {
+          onChange(value.filter((theme) => theme !== 'dark'));
+        } else {
+          onChange([...value, 'dark']);
+        }
+      }}
+      tooltip={<FormattedMessage id="app.theme.switch.dark" />}
+    />
+    <FloatButton
+      icon={<CompactTheme />}
+      type={value.includes('compact') ? 'primary' : 'default'}
+      onClick={() => {
+        if (value.includes('compact')) {
+          onChange(value.filter((theme) => theme !== 'compact'));
+        } else {
+          onChange([...value, 'compact']);
+        }
+      }}
+      tooltip={<FormattedMessage id="app.theme.switch.compact" />}
+    />
+  </FloatButton.Group>
+);
 
 export default ThemeSwitch;
