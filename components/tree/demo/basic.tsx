80934da38f50640edc01db3959ebbf0f321e79c3
二货机器人 <smith3816@gmail.com>
2022-12-14 23:28:42 +0800

    docs: Tree condction

--- components/tree/demo/basic.tsx
+++ components/tree/demo/basic.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tree } from 'antd';
 import type { DataNode, TreeProps } from 'antd/es/tree';
 
-const treeData: DataNode[] = [
+const treeData1: DataNode[] = [
   {
     title: 'parent 1',
     key: '0-0',
@@ -13,12 +13,12 @@ const treeData: DataNode[] = [
         disabled: true,
         children: [
           {
-            title: 'leaf',
+            title: 'leaf 1',
             key: '0-0-0-0',
             disableCheckbox: true,
           },
           {
-            title: 'leaf',
+            title: 'leaf 2',
             key: '0-0-0-1',
           },
         ],
@@ -26,7 +26,76 @@ const treeData: DataNode[] = [
       {
         title: 'parent 1-1',
         key: '0-0-1',
-        children: [{ title: <span style={{ color: '#1890ff' }}>sss</span>, key: '0-0-1-0' }],
+        children: [{ title: 'leaf 3', key: '0-0-1-0' }],
+      },
+    ],
+  },
+];
+
+const treeData: DataNode[] = [
+  {
+    title: 'parent 1',
+    key: '0',
+    children: [
+      {
+        title: 'parent 1-0',
+        key: '0-0',
+        disableCheckbox: true,
+        children: [
+          {
+            title: 'parent 1-0-0',
+            key: '0-0-0',
+            disableCheckbox: true,
+            children: [
+              {
+                title: 'parent 1-0-0-0',
+                key: '0-0-0-0',
+                disableCheckbox: true,
+                children: [
+                  {
+                    title: 'parent 1-0-0-0-0',
+                    key: '0-0-0-0-0',
+                    disableCheckbox: true,
+                    children: [
+                      {
+                        title: 'parent 1-0-0-0-0-0',
+                        key: '0-0-0-0-0-0',
+                        disableCheckbox: true,
+                      },
+                      {
+                        title: 'parent 1-0-0-0-0-1',
+                        key: '0-0-0-0-0-1',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const treeData2: DataNode[] = [
+  {
+    title: '0',
+    key: '0',
+    children: [
+      {
+        title: '0-0',
+        key: '0-0',
+        children: [
+          {
+            title: '0-0-0',
+            key: '0-0-0',
+          },
+          {
+            title: '0-0-1',
+            key: '0-0-1',
+          },
+        ],
       },
     ],
   },
@@ -42,15 +111,31 @@ const App: React.FC = () => {
   };
 
   return (
-    <Tree
-      checkable
-      defaultExpandedKeys={['0-0-0', '0-0-1']}
-      defaultSelectedKeys={['0-0-0', '0-0-1']}
-      defaultCheckedKeys={['0-0-0', '0-0-1']}
-      onSelect={onSelect}
-      onCheck={onCheck}
-      treeData={treeData}
-    />
+    <div
+      style={{
+        padding: 8,
+        width: 200,
+        border: '1px solid #f4f4f4',
+        // whiteSpace: 'nowrap',
+        // overflow: 'auto',
+        // height: 200,
+      }}
+    >
+      <Tree
+        checkable
+        defaultExpandAll
+        defaultSelectedKeys={['0-0-0', '0-0-1']}
+        selectable={false}
+        // defaultCheckedKeys={['0-0', '0-0-0-1', '0-0-1-0']}
+        // defaultCheckedKeys={['0-0-0-1']}
+        // defaultCheckedKeys={['0-0-0-0-0-1']}
+        defaultCheckedKeys={['0-0', '0-0-0', '0-0-1']}
+        onSelect={onSelect}
+        onCheck={onCheck}
+        treeData={treeData2}
+        checkStrictly
+      />
+    </div>
   );
 };
 
