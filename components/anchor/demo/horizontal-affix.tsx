a8c17cc649a7f5cfdca8dd03367b0c40c0c9ba38
foryuki <foryuki@outlook.com>
2022-12-15 21:56:58 +0800

    doc: update demos again

--- components/anchor/demo/horizontal-affix.tsx
+++ components/anchor/demo/horizontal-affix.tsx
@@ -2,28 +2,36 @@ import React from 'react';
 import { Anchor } from 'antd';
 
 const App: React.FC = () => (
-  <div style={{ padding: '20px' }}>
-    <Anchor
-      direction="horizontal"
-      items={[
-        {
-          key: '1',
-          href: '#components-anchor-demo-basic',
-          title: 'Basic demo',
-        },
-        {
-          key: '2',
-          href: '#components-anchor-demo-static',
-          title: 'Static demo',
-        },
-        {
-          key: '3',
-          href: '#api',
-          title: 'API',
-        },
-      ]}
-    />
-  </div>
+  <Anchor
+    direction="horizontal"
+    items={[
+      {
+        key: '1',
+        href: '#components-anchor-demo-basic',
+        title: 'Basic demo',
+      },
+      {
+        key: '2',
+        href: '#components-anchor-demo-basic-affix',
+        title: 'Basic fixed Anchor',
+      },
+      {
+        key: '3',
+        href: '#components-anchor-demo-static',
+        title: '静态位置',
+      },
+      {
+        key: '4',
+        href: '#components-anchor-demo-customizehighlight',
+        title: '自定义锚点高亮',
+      },
+      {
+        key: '5',
+        href: '#api',
+        title: 'API',
+      },
+    ]}
+  />
 );
 
 export default App;
