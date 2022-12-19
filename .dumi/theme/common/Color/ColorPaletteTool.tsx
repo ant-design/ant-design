a0b713c98b5cdd772e722c692b18fec60e0b1c5f
lijianan <574980606@qq.com>
2022-12-19 11:25:21 +0800

    site: cc => fc (#39635)

--- .dumi/theme/common/Color/ColorPaletteTool.tsx
+++ .dumi/theme/common/Color/ColorPaletteTool.tsx
@@ -1,5 +1,5 @@
 import type { ChangeEvent } from 'react';
-import React, { Component } from 'react';
+import React, { useMemo, useState } from 'react';
 import { FormattedMessage } from 'dumi';
 import ColorPicker from './ColorPicker';
 import ColorPatterns from './ColorPatterns';
@@ -7,21 +7,17 @@ import ColorPatterns from './ColorPatterns';
 const primaryMinSaturation = 70; // 主色推荐最小饱和度
 const primaryMinBrightness = 70; // 主色推荐最小亮度
 
-class ColorPaletteTool extends Component {
-  state = {
-    primaryColor: '#1890ff',
-    primaryColorInstance: null,
-  };
-
-  handleChangeColor = (e: string | ChangeEvent, color: { hex: string }) => {
+const ColorPaletteTool: React.FC = () => {
+  const [primaryColor, setPrimaryColor] = useState<string>('#1890ff');
+  const [primaryColorInstance, setPrimaryColorInstance] = useState(null);
+  const handleChangeColor = (e: string | ChangeEvent, color: { hex: string }) => {
     const value = (e as ChangeEvent<HTMLInputElement>).target
       ? (e as ChangeEvent<HTMLInputElement>).target.value
       : e;
-    this.setState({ primaryColor: value, primaryColorInstance: color });
+    setPrimaryColor(value as string);
+    setPrimaryColorInstance(color);
   };
-
-  renderColorValidation() {
-    const { primaryColorInstance } = this.state;
+  const colorValidation = useMemo<React.ReactNode>(() => {
     let text = '';
     if (primaryColorInstance) {
       if (primaryColorInstance.hsv.s * 100 < primaryMinSaturation) {
@@ -36,26 +32,22 @@ class ColorPaletteTool extends Component {
       }
     }
     return <span className="color-palette-picker-validation">{text.trim()}</span>;
-  }
-
-  render() {
-    const { primaryColor } = this.state;
-    return (
-      <div className="color-palette-horizontal">
-        <div className="color-palette-pick">
-          <FormattedMessage id="app.docs.color.pick-primary" />
-        </div>
-        <div className="main-color">{ColorPatterns({ color: primaryColor })}</div>
-        <div className="color-palette-picker">
-          <span style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-            <ColorPicker color={primaryColor} onChange={this.handleChangeColor} />
-          </span>
-          <span className="color-palette-picker-value">{primaryColor}</span>
-          {this.renderColorValidation()}
-        </div>
+  }, [primaryColorInstance, primaryMinSaturation, primaryMinBrightness]);
+  return (
+    <div className="color-palette-horizontal">
+      <div className="color-palette-pick">
+        <FormattedMessage id="app.docs.color.pick-primary" />
       </div>
-    );
-  }
-}
+      <div className="main-color">{ColorPatterns({ color: primaryColor })}</div>
+      <div className="color-palette-picker">
+        <span style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+          <ColorPicker color={primaryColor} onChange={handleChangeColor} />
+        </span>
+        <span className="color-palette-picker-value">{primaryColor}</span>
+        {colorValidation}
+      </div>
+    </div>
+  );
+};
 
 export default ColorPaletteTool;
