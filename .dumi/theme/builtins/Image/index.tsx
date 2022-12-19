95bcf0f2b498c1c1deb4febf890caec9f31ff5bc
MadCcc <1075746765@qq.com>
2022-12-19 11:19:38 +0800

    Revert "docs: img preview (#39633)"
    This reverts commit 1c1d84e08eafd16be5713561ac39c1058be6d334.

--- .dumi/theme/builtins/Image/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { ImageProps } from 'antd';
-import { Image } from 'antd';
-import type { FC } from 'react';
-import React from 'react';
-
-const MyImage: FC<ImageProps> = ({ style, ...props }) => <Image {...props} />;
-
-export default MyImage;
