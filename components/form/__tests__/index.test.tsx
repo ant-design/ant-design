386588dd2b709655c28e727d4f48106388751182
MadCcc <1075746765@qq.com>
2022-12-27 11:49:09 +0800

    chore: update test (#39824)
    * chore: update test
    
    * chore: code clean

--- components/form/__tests__/index.test.tsx
+++ components/form/__tests__/index.test.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import scrollIntoView from 'scroll-into-view-if-needed';
 import classNames from 'classnames';
 import type { ColProps } from 'antd/es/grid';
-import type { FormInstance } from '..';
 import Form from '..';
 import * as Util from '../util';
 import Button from '../../button';
@@ -19,7 +18,7 @@ import Switch from '../../switch';
 import TreeSelect from '../../tree-select';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
-import { fireEvent, render, screen, pureRender, waitFakeTimer } from '../../../tests/utils';
+import { fireEvent, pureRender, render, screen, waitFakeTimer } from '../../../tests/utils';
 import ConfigProvider from '../../config-provider';
 import Drawer from '../../drawer';
 import zhCN from '../../locale/zh_CN';
@@ -643,18 +642,25 @@ describe('Form', () => {
       return <input id={id} value={value} />;
     };
 
-    const formRef = React.createRef<FormInstance>();
+    const Demo = () => {
+      const [form] = Form.useForm();
 
-    const { container } = pureRender(
-      <Form ref={formRef}>
-        <Form.Item>
-          <StaticInput />
-        </Form.Item>
-        <Form.Item name="light">
-          <DynamicInput id="changed" />
-        </Form.Item>
-      </Form>,
-    );
+      return (
+        <Form form={form}>
+          <Form.Item>
+            <StaticInput />
+          </Form.Item>
+          <Form.Item name="light">
+            <DynamicInput id="changed" />
+          </Form.Item>
+          <Button id="fill-btn" onClick={() => form.setFieldValue('light', 'bamboo')}>
+            fill
+          </Button>
+        </Form>
+      );
+    };
+
+    const { container } = pureRender(<Demo />);
 
     await waitFakeTimer();
 
@@ -662,11 +668,7 @@ describe('Form', () => {
     expect(shouldNotRender).toHaveBeenCalledTimes(1);
     expect(shouldRender).toHaveBeenCalledTimes(1);
 
-    formRef.current!.setFieldsValue({ light: 'bamboo' });
-    await waitFakeTimer(100, 100);
-
-    expect(formRef.current!.getFieldsValue()).toEqual({ light: 'bamboo' });
-
+    fireEvent.click(container.querySelector('#fill-btn')!);
     await waitFakeTimer();
 
     expect(shouldNotRender).toHaveBeenCalledTimes(1);
