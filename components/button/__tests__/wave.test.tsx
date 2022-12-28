bd0605a645014aa7d7d8df68c1047912aa92292c
二货机器人 <smith3816@gmail.com>
2022-12-28 21:38:20 +0800

    test: fix test case

--- components/button/__tests__/wave.test.tsx
+++ components/button/__tests__/wave.test.tsx
@@ -3,14 +3,9 @@ import React from 'react';
 import Button from '..';
 import { fireEvent, render } from '../../../tests/utils';
 
-jest.mock('../../_util/wave', () => {
-  const Wave: typeof import('../../_util/wave') = jest.requireActual('../../_util/wave');
-  const WaveComponent = Wave.default;
-  return {
-    ...Wave,
-    __esModule: true,
-    default: (props: import('../../_util/wave').WaveProps) => <WaveComponent {...props} />,
-  };
+jest.mock('rc-util/lib/Dom/isVisible', () => {
+  const mockFn = () => true;
+  return mockFn;
 });
 
 describe('click wave effect', () => {
@@ -21,6 +16,7 @@ describe('click wave effect', () => {
   afterEach(() => {
     jest.clearAllTimers();
     jest.useRealTimers();
+    document.body.innerHTML = '';
   });
 
   async function clickButton(container: HTMLElement) {
@@ -34,44 +30,24 @@ describe('click wave effect', () => {
   it('should have click wave effect for primary button', async () => {
     const { container } = render(<Button type="primary">button</Button>);
     await clickButton(container);
-    expect(container.querySelector('.ant-btn')).toHaveAttribute(
-      'ant-click-animating-without-extra-node',
-    );
+    expect(document.querySelector('.ant-wave')).toBeTruthy();
   });
 
   it('should have click wave effect for default button', async () => {
     const { container } = render(<Button>button</Button>);
     await clickButton(container);
-    expect(container.querySelector('.ant-btn')).toHaveAttribute(
-      'ant-click-animating-without-extra-node',
-    );
+    expect(document.querySelector('.ant-wave')).toBeTruthy();
   });
 
   it('should not have click wave effect for link type button', async () => {
     const { container } = render(<Button type="link">button</Button>);
     await clickButton(container);
-    expect(container.querySelector('.ant-btn')).not.toHaveAttribute(
-      'ant-click-animating-without-extra-node',
-    );
+    expect(document.querySelector('.ant-wave')).toBeFalsy();
   });
 
   it('should not have click wave effect for text type button', async () => {
     const { container } = render(<Button type="text">button</Button>);
     await clickButton(container);
-    expect(container.querySelector('.ant-btn')).not.toHaveAttribute(
-      'ant-click-animating-without-extra-node',
-    );
-  });
-
-  it('should handle transitionstart', async () => {
-    const { container, unmount } = render(<Button type="primary">button</Button>);
-    await clickButton(container);
-    const buttonNode = container.querySelector('.ant-btn')!;
-    fireEvent(buttonNode, new Event('transitionstart'));
-    expect(container.querySelector('.ant-btn')).toHaveAttribute(
-      'ant-click-animating-without-extra-node',
-    );
-    unmount();
-    fireEvent(buttonNode, new Event('transitionstart'));
+    expect(document.querySelector('.ant-wave')).toBeFalsy();
   });
 });
