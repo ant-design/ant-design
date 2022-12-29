49e726e3a393a1426731b7cd5da143801ce27961
栗嘉男 <574980606@qq.com>
2022-12-29 17:52:03 +0800

    update locale entry

--- index-with-locales.js
+++ index-with-locales.js
@@ -5,7 +5,7 @@ const req = require.context('./components', true, /^\.\/locale\/.+_.+\.ts$/);
 antd.locales = {};
 
 req.keys().forEach((mod) => {
-  const matches = mod.match(/\/([^/]+).tsx$/);
+  const matches = mod.match(/\/([^/]+).ts$/);
   antd.locales[matches[1]] = req(mod).default;
 });
 
