e9b5805fc2b9813351d58e6a70a6ba19260dfb0b
Samyek <39209724+IcedMonk@users.noreply.github.com>
2023-01-01 18:43:29 +0530

    docs: Updated the Copyright Year (#39952)

--- components/layout/demo/fixed.tsx
+++ components/layout/demo/fixed.tsx
@@ -38,7 +38,7 @@ const App: React.FC = () => {
         </Breadcrumb>
         <div style={{ padding: 24, minHeight: 380, background: colorBgContainer }}>Content</div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
     </Layout>
   );
 };
