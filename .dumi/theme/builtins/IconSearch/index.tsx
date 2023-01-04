878f5ff5ea6dd3be9dead4ee72fc865d1cc1603f
afc163 <afc163@gmail.com>
2023-01-04 18:11:12 +0800

    style: change shadow style

--- .dumi/theme/builtins/IconSearch/index.tsx
+++ .dumi/theme/builtins/IconSearch/index.tsx
@@ -82,10 +82,10 @@ const IconSearch: React.FC = () => {
 
   const [searchBarAffixed, setSearchBarAffixed] = React.useState(false);
   const { token } = useSiteToken();
-  const { boxShadowSecondary, borderRadius, colorBgContainer } = token;
+  const { borderRadius, colorBgContainer } = token;
   const affixedStyle = searchBarAffixed
     ? {
-        boxShadow: boxShadowSecondary,
+        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
         padding: 8,
         margin: -8,
         borderRadius,
