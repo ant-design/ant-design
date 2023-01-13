0128018a17cbf9a71bdb2e65404fbe22f400166b
smallbun <30397655+leshalv@users.noreply.github.com>
2023-01-13 10:38:29 +0800

    getContainer 增加 ShadowRoot 类型

--- components/notification/interface.ts
+++ components/notification/interface.ts
@@ -45,7 +45,7 @@ export interface GlobalConfigProps {
   bottom?: number;
   duration?: number;
   prefixCls?: string;
-  getContainer?: () => HTMLElement;
+  getContainer?: () => HTMLElement | ShadowRoot;
   placement?: NotificationPlacement;
   closeIcon?: React.ReactNode;
   rtl?: boolean;
