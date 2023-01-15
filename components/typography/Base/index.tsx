12179dfafb294f759f850a2058e851fd47dbba34
栗嘉男 <574980606@qq.com>
2023-01-15 23:33:41 +0800

    revert

--- components/typography/Base/index.tsx
+++ components/typography/Base/index.tsx
@@ -84,7 +84,7 @@ function wrapperDecorations(
 ) {
   let currentContent = content;
 
-  function wrap(tag: string, needed?: boolean) {
+  function wrap(needed: boolean | undefined, tag: string) {
     if (!needed) {
       return;
     }
@@ -92,13 +92,13 @@ function wrapperDecorations(
     currentContent = React.createElement(tag, {}, currentContent);
   }
 
-  wrap('strong', strong);
-  wrap('u', underline);
-  wrap('del', del);
-  wrap('code', code);
-  wrap('mark', mark);
-  wrap('kbd', keyboard);
-  wrap('i', italic);
+  wrap(strong, 'strong');
+  wrap(underline, 'u');
+  wrap(del, 'del');
+  wrap(code, 'code');
+  wrap(mark, 'mark');
+  wrap(keyboard, 'kbd');
+  wrap(italic, 'i');
 
   return currentContent;
 }
