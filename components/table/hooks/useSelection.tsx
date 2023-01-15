4ca4356b3738e5e26a40643ea38969687e30c7bb
栗嘉男 <574980606@qq.com>
2023-01-15 21:25:43 +0800

    fix type

--- components/table/hooks/useSelection.tsx
+++ components/table/hooks/useSelection.tsx
@@ -75,7 +75,7 @@ function flattenData<RecordType>(
 
 export default function useSelection<RecordType>(
   config: UseSelectionConfig<RecordType>,
-  rowSelection: TableRowSelection<RecordType> = {},
+  rowSelection?: TableRowSelection<RecordType>,
 ): [TransformColumns<RecordType>, Set<Key>] {
   const {
     preserveSelectedRowKeys,
@@ -95,7 +95,7 @@ export default function useSelection<RecordType>(
     renderCell: customizeRenderCell,
     hideSelectAll,
     checkStrictly = true,
-  } = rowSelection;
+  } = rowSelection || {};
 
   const {
     prefixCls,
