5840e5bef893b9ffd5c0d71a5912bfb76e35ee89
栗嘉男 <574980606@qq.com>
2023-01-19 16:53:51 +0800

    fix

--- components/config-provider/demo/locale.tsx
+++ components/config-provider/demo/locale.tsx
@@ -116,7 +116,7 @@ const App: React.FC = () => {
   return (
     <div>
       <div style={{ marginBottom: 16 }}>
-        <span style={{ marginRight: 16 }}>Change locale of components: </span>
+        <span style={{ marginRight: 16 }}>Change locale of components:</span>
         <Radio.Group value={locale} onChange={changeLocale}>
           <Radio.Button key="en" value={enUS}>
             English
