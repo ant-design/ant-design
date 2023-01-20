662f78ac010a83ccd51bcd95f82a3c4df8186b26
MadCcc <1075746765@qq.com>
2023-01-20 10:48:33 +0800

    fix: motionEaseInQuint value (#40133)

--- components/theme/themes/seed.ts
+++ components/theme/themes/seed.ts
@@ -49,7 +49,7 @@ const seedToken: SeedToken = {
   motionEaseInOut: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
   motionEaseOutBack: 'cubic-bezier(0.12, 0.4, 0.29, 1.46)',
   motionEaseInBack: 'cubic-bezier(0.71, -0.46, 0.88, 0.6)',
-  motionEaseInQuint: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  motionEaseInQuint: 'cubic-bezier(0.755, 0.05, 0.855, 0.06)',
   motionEaseOutQuint: 'cubic-bezier(0.23, 1, 0.32, 1)',
 
   // Radius
