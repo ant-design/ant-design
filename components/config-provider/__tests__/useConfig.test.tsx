e7acb5b5cfb55e2077d4d1046ba34071c4c297c6
xliez <xliez@foxmail.com>
2023-01-25 23:02:52 +0800

    test: add useConfig test

---
+++ components/config-provider/__tests__/useConfig.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ConfigProvider from '..';
+import Form from '../../form';
+import { render } from '../../../tests/utils';
+
+describe('ConfigProvider.useConfig', () => {
+  it('useConfig - componentSize', () => {
+    let size: string | undefined = undefined;
+
+    const App: React.FC = () => {
+      const { componentSize } = ConfigProvider.useConfig();
+      size = componentSize;
+
+      return null;
+    };
+
+    render(
+      <ConfigProvider componentSize='small'>
+        <App />
+      </ConfigProvider>,
+    );
+
+    expect(size).toBe('small');
+  });
+
+  it('useConfig - componentDisabled', () => {
+    let disabled: boolean | undefined = undefined;
+    const App: React.FC = () => {
+      const { componentDisabled } = ConfigProvider.useConfig();
+      disabled = componentDisabled;
+      return null;
+    };
+
+    render(
+      <Form disabled>
+        <App />
+      </Form>,
+    );
+
+    expect(disabled).toBe(true);
+  });
+});
