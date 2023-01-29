1f7d059c04afc940d32f662d705f5a12414c4889
Weite Dai <weitedai.dev@gmail.com>
2023-01-29 12:15:52 +1100

    fix: address mock data city spelling error

--- components/table/__tests__/Table.filter.test.tsx
+++ components/table/__tests__/Table.filter.test.tsx
@@ -1687,7 +1687,7 @@ describe('Table.filter', () => {
         key: '2',
         name: 'Joe Black',
         age: 32,
-        address: 'Sidney No. 1 Lake Park',
+        address: 'Sydney No. 1 Lake Park',
       },
     ];
 
@@ -1800,7 +1800,7 @@ describe('Table.filter', () => {
               key: '3',
               name: 'Joe Black',
               age: 66,
-              address: 'Sidney No. 1 Lake Park',
+              address: 'Sydney No. 1 Lake Park',
             },
             {
               key: '4',
@@ -1910,7 +1910,7 @@ describe('Table.filter', () => {
           key: '3',
           name: 'Joe Black',
           age: 32,
-          address: 'Sidney No. 1 Lake Park',
+          address: 'Sydney No. 1 Lake Park',
         },
         {
           key: '4',
@@ -1957,7 +1957,7 @@ describe('Table.filter', () => {
           {
             key: '3',
             name1: 'Joe Black',
-            address: 'Sidney No. 1 Lake Park',
+            address: 'Sydney No. 1 Lake Park',
           },
           {
             key: '4',
