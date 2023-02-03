3e71b63989b00cbd672f8cf46c6bd56fc91eed86
黑雨 <wangning4567@163.com>
2023-02-03 22:06:40 +0800

    doc: update doc

--- components/timeline/demo/color.tsx
+++ components/timeline/demo/color.tsx
@@ -10,10 +10,6 @@ const App: React.FC = () => (
         content: 'Create a services site 2015-09-01',
       },
       {
-        color: 'green',
-        content: 'Create a services site 2015-09-01',
-      },
-      {
         color: 'red',
         content: (
           <>
