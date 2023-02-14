fcbe10a7525476cfa1d89c610cd90930934748f7
afc163 <afc163@gmail.com>
2023-02-14 13:58:56 +0800

    fix demo argos ci

--- components/float-button/demo/group.tsx
+++ components/float-button/demo/group.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { FloatButton } from 'antd';
-import { QuestionCircleOutlined, SyncOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined, SyncOutlined, FileTextOutlined } from '@ant-design/icons';
 
 const App: React.FC = () => (
   <>
     <FloatButton.Group shape="circle" style={{ right: 24 }}>
       <FloatButton icon={<QuestionCircleOutlined />} />
-      <FloatButton />
+      <FloatButton icon={<FileTextOutlined />} />
       <FloatButton.BackTop visibilityHeight={0} />
     </FloatButton.Group>
     <FloatButton.Group shape="square" style={{ right: 94 }}>
       <FloatButton icon={<QuestionCircleOutlined />} />
-      <FloatButton />
+      <FloatButton icon={<FileTextOutlined />} />
       <FloatButton icon={<SyncOutlined />} />
       <FloatButton.BackTop visibilityHeight={0} />
     </FloatButton.Group>
