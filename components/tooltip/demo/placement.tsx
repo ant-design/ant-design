46423ad34f99d23a1f5fda25192f09be167a079e
二货机器人 <smith3816@gmail.com>
2023-02-14 09:41:11 +0800

    chore: update deps

--- components/tooltip/demo/placement.tsx
+++ components/tooltip/demo/placement.tsx
@@ -6,7 +6,7 @@ const text = <span>prompt text</span>;
 const buttonWidth = 70;
 
 const App: React.FC = () => (
-  <div style={{ padding: 80 }}>
+  <div>
     <div style={{ marginLeft: buttonWidth, whiteSpace: 'nowrap' }}>
       <Tooltip placement="topLeft" title={text}>
         <Button>TL</Button>
