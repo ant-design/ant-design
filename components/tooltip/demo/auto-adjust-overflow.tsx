5c1162a1402656412ef6bf9a6c56f106acaf7a4e
二货爱吃白萝卜 <smith3816@gmail.com>
2023-02-15 14:56:22 +0800

    docs: update demo (#40732)
    * docs: update demo
    
    * test: update snapshot
    
    * test: update snapshot
    
    * chore: fix lint

--- components/tooltip/demo/auto-adjust-overflow.tsx
+++ components/tooltip/demo/auto-adjust-overflow.tsx
@@ -1,47 +1,52 @@
-import { Button, Tooltip } from 'antd';
+import type { TooltipProps } from 'antd';
+import { Button, Tooltip, Typography } from 'antd';
 import React from 'react';
 
-const wrapStyles: React.CSSProperties = {
-  overflow: 'auto',
-  position: 'relative',
-  padding: '24px',
-  border: '1px solid #e9e9e9',
-};
+const Block = React.forwardRef<HTMLDivElement, Partial<TooltipProps>>((props, ref) => (
+  <div
+    style={{
+      overflow: 'auto',
+      position: 'relative',
+      padding: '24px',
+      border: '1px solid #e9e9e9',
+    }}
+    ref={ref}
+  >
+    <div
+      style={{
+        width: '200%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        rowGap: 16,
+      }}
+    >
+      <Tooltip {...props} placement="left" title="Prompt Text">
+        <Button>Adjust automatically / 自动调整</Button>
+      </Tooltip>
+      <Tooltip {...props} placement="left" title="Prompt Text" autoAdjustOverflow={false}>
+        <Button>Ignore / 不处理</Button>
+      </Tooltip>
+    </div>
+  </div>
+));
 
 const App: React.FC = () => {
-  const containerRef = React.useRef<HTMLDivElement>(null);
+  const containerRef1 = React.useRef<HTMLDivElement>(null);
+  const containerRef2 = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
-    containerRef.current!.scrollLeft = containerRef.current!.clientWidth * 0.5;
+    containerRef1.current!.scrollLeft = containerRef1.current!.clientWidth * 0.5;
+    containerRef2.current!.scrollLeft = containerRef2.current!.clientWidth * 0.5;
   }, []);
 
   return (
-    <div style={wrapStyles} ref={containerRef}>
-      <div
-        style={{
-          width: '200%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          rowGap: 16,
-        }}
-      >
-        <Tooltip
-          placement="left"
-          title="Prompt Text"
-          getPopupContainer={(trigger) => trigger.parentElement!}
-        >
-          <Button>Adjust automatically / 自动调整</Button>
-        </Tooltip>
-        <Tooltip
-          placement="left"
-          title="Prompt Text"
-          getPopupContainer={(trigger) => trigger.parentElement!}
-          autoAdjustOverflow={false}
-        >
-          <Button>Ignore / 不处理</Button>
-        </Tooltip>
-      </div>
+    <div style={{ display: 'flex', flexDirection: 'column', rowGap: 16 }}>
+      <Typography.Title level={5}>With `getPopupContainer`</Typography.Title>
+      <Block ref={containerRef1} getPopupContainer={(trigger) => trigger.parentElement!} />
+
+      <Typography.Title level={5}>Without `getPopupContainer`</Typography.Title>
+      <Block ref={containerRef2} />
     </div>
   );
 };
