db07315673d923536e9768b5c9adea67e40de9dd
Wuxh <wxh16144@qq.com>
2023-02-27 23:06:16 +0800

    Revert "type: TimePicker added hourStep type (#40947)"
    This reverts commit c89640d843c81bc9bd156798d8b26616e169bd74.

--- components/time-picker/demo/interval-options.tsx
+++ components/time-picker/demo/interval-options.tsx
@@ -1,6 +1,6 @@
-import { TimePicker } from 'antd';
 import React from 'react';
+import { TimePicker } from 'antd';
 
-const App: React.FC = () => <TimePicker minuteStep={15} secondStep={10} hourStep={1} />;
+const App: React.FC = () => <TimePicker minuteStep={15} secondStep={10} />;
 
 export default App;
