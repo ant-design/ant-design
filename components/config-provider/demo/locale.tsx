199f039fea4cf52f1e92fc1de03e26a4258edb20
yisakomi <yisakomi@163.com>
2023-03-07 21:26:56 +0800

    docs(ConfigProvider): update ConfigProvider demo

--- components/config-provider/demo/locale.tsx
+++ components/config-provider/demo/locale.tsx
@@ -130,7 +130,7 @@ const Page: React.FC = () => {
         </Select>
         <DatePicker />
         <TimePicker />
-        <RangePicker style={{ width: 220 }} />
+        <RangePicker />
       </Space>
       <Space wrap>
         <Button type="primary" onClick={showModal}>
