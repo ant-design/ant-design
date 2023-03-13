31e806593564a1799fbadbdd90c746e3048c1f7f
栗嘉男 <574980606@qq.com>
2023-03-13 11:49:08 +0800

    test: update snap

--- components/float-button/demo/badge.tsx
+++ components/float-button/demo/badge.tsx
@@ -4,7 +4,7 @@ import { random } from 'lodash';
 import React, { useState } from 'react';
 
 const App: React.FC = () => {
-  const [number, setNumber] = useState<number>(random(0, 100));
+  const [number, setNumber] = useState<number>(5);
   return (
     <>
       <Button type="primary" style={{ margin: 16 }} onClick={() => setNumber(random(0, 100))}>
