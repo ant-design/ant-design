343dbd8dd99deed6923bc92466450a32f0ba5bd2
栗嘉男 <574980606@qq.com>
2023-03-13 20:22:48 +0800

    update rotationSlider prop

--- components/upload/demo/crop-image.tsx
+++ components/upload/demo/crop-image.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <ImgCrop zoomSlider>
+    <ImgCrop rotationSlider>
       <Upload
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
