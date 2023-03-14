c1d9848ed34f672c2bff716a0b8097126a9f9bad
MadCcc <1075746765@qq.com>
2023-03-14 17:31:11 +0800

    chore: remove try catch

--- .dumi/remarkAntd.ts
+++ .dumi/remarkAntd.ts
@@ -4,12 +4,8 @@ export default function remarkMeta() {
   return (tree, vFile) => {
     // read frontmatter
     unistUtilVisit.visit(tree, 'yaml', (node) => {
-      try {
-        if (!/(^|[\n\r])description:/.test(node.value)) {
-          vFile.data.frontmatter.__autoDescription = true;
-        }
-      } catch {
-        /* empty */
+      if (!/(^|[\n\r])description:/.test(node.value)) {
+        vFile.data.frontmatter.__autoDescription = true;
       }
     });
   };
