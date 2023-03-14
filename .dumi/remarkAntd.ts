16492a7640a5cf81f2fecdb28f06b8994a059e5f
MadCcc <1075746765@qq.com>
2023-03-14 17:17:47 +0800

    chore: use regexp

--- .dumi/remarkAntd.ts
+++ .dumi/remarkAntd.ts
@@ -1,13 +1,11 @@
 import { unistUtilVisit } from 'dumi';
-import yaml from 'js-yaml';
 
 export default function remarkMeta() {
   return (tree, vFile) => {
     // read frontmatter
     unistUtilVisit.visit(tree, 'yaml', (node) => {
       try {
-        const frontmatter = yaml.load(node.value) as any;
-        if (!('description' in frontmatter)) {
+        if (!/(^|[\n\r])description:/.test(node.value)) {
           vFile.data.frontmatter.__autoDescription = true;
         }
       } catch {
