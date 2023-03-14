53fdeac7bb4db5ee955755f7eec3440aefea71bb
MadCcc <1075746765@qq.com>
2023-03-14 16:35:51 +0800

    chore: fix ghost deps

--- .dumi/remarkAntd.ts
+++ .dumi/remarkAntd.ts
@@ -1,16 +1,10 @@
+import { unistUtilVisit } from 'dumi';
 import yaml from 'js-yaml';
-
-let visit: typeof import('unist-util-visit').visit;
-
-// workaround to import pure esm module
-(async () => {
-  ({ visit } = await import('unist-util-visit'));
-})();
 
 export default function remarkMeta() {
   return (tree, vFile) => {
     // read frontmatter
-    visit(tree, 'yaml', (node) => {
+    unistUtilVisit.visit(tree, 'yaml', (node) => {
       try {
         const frontmatter = yaml.load(node.value) as any;
         if (!('description' in frontmatter)) {
