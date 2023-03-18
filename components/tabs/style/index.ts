3d41f3f8dff0f287fff640d932c49732a2a692c0
PhosphorusP <1309396166@qq.com>
2023-03-18 19:24:41 +0800

    fix: <Tabs /> shows wrong more icon color in #41312 (#41313)
    Co-authored-by: David <oncwnuI2Sf77R0KOdSUCh_ieKUzM@git.weixin.qq.com>

--- components/tabs/style/index.ts
+++ components/tabs/style/index.ts
@@ -772,6 +772,23 @@ const genTabsStyle: GenerateStyle<TabsToken> = (token: TabsToken): CSSObject => 
           padding: tabsCardHorizontalPadding,
           background: 'transparent',
           border: 0,
+          color: token.colorText,
+
+          '&::after': {
+            position: 'absolute',
+            right: {
+              _skip_check_: true,
+              value: 0,
+            },
+            bottom: 0,
+            left: {
+              _skip_check_: true,
+              value: 0,
+            },
+            height: token.controlHeightLG / 8,
+            transform: 'translateY(100%)',
+            content: "''",
+          },
         },
 
         [`${componentCls}-nav-add`]: {
