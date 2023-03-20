0032714158a0cfa6499098d7d181d4f341f88809
Duy Pham <duy.pham@salto.vn>
2023-03-20 11:02:13 +0700

    fix: wrong vietname translate text

--- components/date-picker/locale/vi_VN.ts
+++ components/date-picker/locale/vi_VN.ts
@@ -11,7 +11,7 @@ const locale: PickerLocale = {
     monthPlaceholder: 'Chọn tháng',
     weekPlaceholder: 'Chọn tuần',
     rangePlaceholder: ['Ngày bắt đầu', 'Ngày kết thúc'],
-    rangeYearPlaceholder: ['Năm bắt đầy', 'Năm kết thúc'],
+    rangeYearPlaceholder: ['Năm bắt đầu', 'Năm kết thúc'],
     rangeQuarterPlaceholder: ['Quý bắt đầu', 'Quý kết thúc'],
     rangeMonthPlaceholder: ['Tháng bắt đầu', 'Tháng kết thúc'],
     rangeWeekPlaceholder: ['Tuần bắt đầu', 'Tuần kết thúc'],
