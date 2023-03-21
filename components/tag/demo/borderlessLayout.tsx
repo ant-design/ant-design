c00cce9b3285eec5b8502f65d0d6dcd9dc33bdb2
栗嘉男 <574980606@qq.com>
2023-03-21 12:30:22 +0800

    update demo size

--- components/tag/demo/borderlessLayout.tsx
+++ components/tag/demo/borderlessLayout.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   const { token } = theme.useToken();
   return (
     <div style={{ backgroundColor: token.colorBgLayout, padding: 16 }}>
-      <Space wrap>
+      <Space size="small" wrap>
         <Tag bordered={false}>Tag 1</Tag>
         <Tag bordered={false}>Tag 2</Tag>
         <Tag bordered={false} closable>
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         </Tag>
       </Space>
       <Divider />
-      <Space wrap>
+      <Space size="small" wrap>
         <Tag bordered={false} color="magenta">
           magenta
         </Tag>
