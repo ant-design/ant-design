5078f89b165b0449eb4c5b2ab304b5b1f9b4c07b
CCherry07 <2405693142@qq.com>
2023-03-25 18:20:06 +0800

    fix(line-demo): update line demo

--- components/progress/demo/line.tsx
+++ components/progress/demo/line.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Progress } from 'antd';
 
 const App: React.FC = () => (
-  <>
+  <div>
     <Progress percent={30} />
     <Progress percent={50} status="active" />
     <Progress percent={70} status="exception" />
     <Progress percent={100} />
     <Progress percent={50} showInfo={false} />
-  </>
+  </div>
 );
 
 export default App;
