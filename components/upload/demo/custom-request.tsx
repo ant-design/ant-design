f71fb6f197243a6db009bc9b26100476d851879c
wuxh <wxh1220@gmail.com>
2023-03-27 18:52:45 +0800

    chore: update

--- components/upload/demo/custom-request.tsx
+++ components/upload/demo/custom-request.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { Upload } from 'antd';
+import useFetch from './useFetch';
 
 const App: React.FC = () => {
-  return <div>自定义上传</div>;
+  const { data, loading, error } = useFetch('https://www.mocky.io/v2/5cc8019d300000980a055e76');
+
+  return (
+    <Upload>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error</div>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </Upload>
+  );
 };
 
 export default App;
