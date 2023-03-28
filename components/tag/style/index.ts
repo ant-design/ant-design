4f8051cb41677517db260515222629cb7b7ed7e3
MadCcc <1075746765@qq.com>
2023-03-28 10:26:43 +0800

    fix: fix cycle-deps

--- components/tag/style/index.ts
+++ components/tag/style/index.ts
@@ -1,9 +1,9 @@
 import type { CSSInterpolation } from '@ant-design/cssinjs';
 import type React from 'react';
 import type { FullToken } from '../../theme/internal';
-import { genComponentStyleHook, mergeToken } from '../../theme/internal';
+import { genComponentStyleHook, mergeToken, genPresetColor } from '../../theme/internal';
 import capitalize from '../../_util/capitalize';
-import { genPresetColor, resetComponent } from '../../style';
+import { resetComponent } from '../../style';
 
 export interface ComponentToken {}
 
