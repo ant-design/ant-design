b75776d8e27c15d2688c8895bf4d15b009c6c8e5
栗嘉男 <574980606@qq.com>
2023-03-29 13:50:54 +0800

    fix style

--- components/_util/selectDefaultRender.tsx
+++ components/_util/selectDefaultRender.tsx
@@ -2,11 +2,13 @@ import type { CustomTagProps } from 'rc-select/lib/BaseSelect';
 import React from 'react';
 import Tag from '../tag';
 
-const genDefaultRender = (rootCls: string) => ({ label, ...rest }: CustomTagProps) =>
-  (
+const genDefaultRender = (rootCls: string) => (props: CustomTagProps) => {
+  const { label, ...rest } = props;
+  return (
     <Tag {...rest} className={`${rootCls}-select-tag`} bordered={false}>
-      {label}
+      {props.closable ? label : <span className={`${rootCls}-tag-content`}>{label}</span>}
     </Tag>
   );
+};
 
 export default genDefaultRender;
