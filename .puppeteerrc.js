d66d7945dd96fbac9af4037c68a806145fb5e4fe
MadCcc <1075746765@qq.com>
2023-03-29 18:07:31 +0800

    chore: modify cache path

---
+++ .puppeteerrc.js
@@ -0,0 +1,9 @@
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
