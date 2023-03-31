6d7277c3ef7c93be4cab3b9f59ec0e0212737969
二货机器人 <smith3816@gmail.com>
2023-03-31 10:23:34 +0800

    chore: fix missing selector

--- components/space/style/index.tsx
+++ components/space/style/index.tsx
@@ -43,7 +43,7 @@ const genSpaceStyle: GenerateStyle<SpaceToken> = (token) => {
           display: 'none',
         },
 
-        '*': {
+        '> *': {
           verticalAlign: 'top',
         },
       },
