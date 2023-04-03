dd3aee930ecc44d9e38ba22bef88b875f7d7a3fa
二货机器人 <smith3816@gmail.com>
2023-04-03 21:44:31 +0800

    docs: Update qr code demo

--- components/qrcode/demo/base.tsx
+++ components/qrcode/demo/base.tsx
@@ -1,6 +1,19 @@
+import { Col, Input, QRCode, Row } from 'antd';
 import React from 'react';
-import { QRCode } from 'antd';
 
-const App: React.FC = () => <QRCode value="https://ant.design/" />;
+const App: React.FC = () => {
+  const [text, setText] = React.useState('https://ant.design/');
+
+  return (
+    <Row>
+      <Col span={24}>
+        <QRCode value={text} />
+      </Col>
+      <Col span={24}>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+      </Col>
+    </Row>
+  );
+};
 
 export default App;
