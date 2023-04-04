3399dd358d529ac19d53b9f9c97e7708b58a59ba
二货机器人 <smith3816@gmail.com>
2023-04-04 15:38:44 +0800

    test: update test case

--- components/config-provider/__tests__/popup.test.tsx
+++ components/config-provider/__tests__/popup.test.tsx
@@ -12,22 +12,49 @@ dayjs.extend(customParseFormat);
 jest.mock('rc-util/lib/Portal');
 
 describe('ConfigProvider.Popup', () => {
+  const selectLikeNodes = (
+    <>
+      <Select
+        open
+        options={new Array(20).fill(null).map((_, index) => ({ value: index, label: index }))}
+      />
+      <TreeSelect
+        open
+        treeData={new Array(20).fill(null).map((_, index) => ({ value: index, title: index }))}
+      />
+      <Cascader
+        open
+        options={new Array(20).fill(null).map((_, index) => ({ value: index, label: index }))}
+      />
+    </>
+  );
+
   it('disable virtual if is false', () => {
     const { container } = render(
-      <ConfigProvider virtual={false}>
-        <Select
-          open
-          options={new Array(20).fill(null).map((_, index) => ({ value: index, label: index }))}
-        />
-        <TreeSelect
-          open
-          treeData={new Array(20).fill(null).map((_, index) => ({ value: index, title: index }))}
-        />
-        <Cascader
-          open
-          options={new Array(20).fill(null).map((_, index) => ({ value: index, label: index }))}
-        />
-      </ConfigProvider>,
+      <ConfigProvider virtual={false}>{selectLikeNodes}</ConfigProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('disable virtual if dropdownMatchSelectWidth is false', () => {
+    const errSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const { container } = render(
+      <ConfigProvider dropdownMatchSelectWidth={false}>{selectLikeNodes}</ConfigProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+
+    expect(errSpy).toHaveBeenCalledWith(
+      'Warning: [antd: ConfigProvider] `dropdownMatchSelectWidth` is deprecated. Please use `popupMatchSelectWidth` instead.',
+    );
+    errSpy.mockRestore();
+  });
+
+  it('disable virtual if popupMatchSelectWidth is false', () => {
+    const { container } = render(
+      <ConfigProvider popupMatchSelectWidth={false}>{selectLikeNodes}</ConfigProvider>,
     );
 
     expect(container).toMatchSnapshot();
