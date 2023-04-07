1566e7497b99aba31a14a4065990ac29f50664d6
therealrinku <rinkunited2032@gmail.com>
2023-04-07 17:13:40 +0545

    scripts change revert

--- scripts/check-version-md.js
+++ scripts/check-version-md.js
@@ -8,7 +8,7 @@ const getChangelogByVersion = (content, version) => {
   const lines = content.split('\n');
   const changeLog = [];
   const startPattern = new RegExp(`^## ${version}`);
-  const stopPattern = /^## /; // previous version
+  const stopPattern = /^## /; // 前一个版本
   let begin = false;
   for (let i = 0; i < lines.length; i += 1) {
     const line = lines[i];
