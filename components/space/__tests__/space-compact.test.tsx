0950a5064d72c8b202203c785ca2517db7cf0bf7
foryuki <foryuki@outlook.com>
2023-04-08 17:08:32 +0800

    fix: styling issues with the Tooltip based  component within the Space.Compact wrapper

--- components/space/__tests__/space-compact.test.tsx
+++ components/space/__tests__/space-compact.test.tsx
@@ -4,17 +4,18 @@ import Space from '..';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import { render } from '../../../tests/utils';
-import Input from '../../input';
-import Button from '../../button';
 import AutoComplete from '../../auto-complete';
+import Button from '../../button';
 import Cascader from '../../cascader';
 import DatePicker from '../../date-picker';
+import Drawer from '../../drawer';
+import Dropdown from '../../dropdown';
+import Input from '../../input';
+import Modal from '../../modal';
+import Popover from '../../popover';
 import Select from '../../select';
 import TimePicker from '../../time-picker';
 import TreeSelect from '../../tree-select';
-import Modal from '../../modal';
-import Dropdown from '../../dropdown';
-import Drawer from '../../drawer';
 
 describe('Space.Compact', () => {
   mountTest(Space.Compact);
@@ -245,6 +246,38 @@ describe('Space.Compact', () => {
         .querySelector('.ant-drawer')
         ?.querySelector('.ant-btn')
         ?.classList.contains('ant-btn-compact-item'),
+    ).toBe(false);
+  });
+  it('context for Tooltip', () => {
+    render(
+      <Space.Compact>
+        <Input placeholder="Debug Popover context" />
+        <Popover
+          open
+          content={
+            <>
+              <Input placeholder="Left Border" />
+              <DatePicker />
+            </>
+          }
+          trigger={['click']}
+          placement="bottom"
+        >
+          <Button>Settings</Button>
+        </Popover>
+      </Space.Compact>,
+    );
+    expect(
+      document.body
+        .querySelector('.ant-popover')
+        ?.querySelector('.ant-input')
+        ?.classList.contains('ant-input-compact-item'),
+    ).toBe(false);
+    expect(
+      document.body
+        .querySelector('.ant-popover')
+        ?.querySelector('.ant-picker')
+        ?.classList.contains('ant-picker-compact-item'),
     ).toBe(false);
   });
 });
