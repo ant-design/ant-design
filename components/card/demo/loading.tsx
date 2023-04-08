1d7ca0e158be49e307fff3a1e7ed8c24dcf610ea
JiaQi <112228030+Yuiai01@users.noreply.github.com>
2023-04-08 21:01:51 +0800

    chore: Update random image sources (#41704)
    * chore: update demo
    
    * chore: update test case
    
    * chore: update snap

--- components/card/demo/loading.tsx
+++ components/card/demo/loading.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
       <Switch checked={!loading} onChange={onChange} />
       <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
         <Meta
-          avatar={<Avatar src="https://joesch.moe/api/v1/random?key=1" />}
+          avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />}
           title="Card title"
           description="This is the description"
         />
@@ -31,7 +31,7 @@ const App: React.FC = () => {
       >
         <Skeleton loading={loading} avatar active>
           <Meta
-            avatar={<Avatar src="https://joesch.moe/api/v1/random?key=2" />}
+            avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=2" />}
             title="Card title"
             description="This is the description"
           />
