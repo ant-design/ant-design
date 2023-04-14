2c74340a4c4391186a5e79c77a70a725f27caecc
栗嘉男 <574980606@qq.com>
2023-04-14 17:42:02 +0800

    fix

--- scripts/generate-version.ts
+++ scripts/generate-version.ts
@@ -1,12 +1,11 @@
-import fs from 'fs-extra';
+import fs from 'fs';
 import path from 'path';
+import localPackage from '../package.json';
 
-(async () => {
-  const { default: localPackage } = await import('../package.json');
-  const { version } = localPackage;
-  fs.writeFileSync(
-    path.join(__dirname, '..', 'components', 'version', 'version.ts'),
-    `export default '${version}';`,
-    'utf8',
-  );
-})();
+const { version } = localPackage;
+
+fs.writeFileSync(
+  path.join(__dirname, '..', 'components', 'version', 'version.ts'),
+  `export default '${version}';`,
+  'utf8',
+);
