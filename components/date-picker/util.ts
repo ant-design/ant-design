c20c34e5f7855ed589040a124f0cfb2f9325c7e1
二货机器人 <smith3816@gmail.com>
2023-04-19 22:44:47 +0800

    chore: fix type

--- components/date-picker/util.ts
+++ components/date-picker/util.ts
@@ -1,8 +1,8 @@
+import type { AlignType } from '@rc-component/trigger';
 import type { PickerMode } from 'rc-picker/lib/interface';
-import type { DirectionType } from '../config-provider';
 import type { SelectCommonPlacement } from '../_util/motion';
+import type { DirectionType } from '../config-provider';
 import type { PickerLocale } from './generatePicker';
-import { AlignType } from '@rc-component/trigger';
 
 export function getPlaceholder(
   locale: PickerLocale,
