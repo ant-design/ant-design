ceac9b1e66b1d8b6f2088db1d7b86cff1753ac20
栗嘉男 <574980606@qq.com>
2023-04-23 10:46:10 +0800

    demo: delete div

--- components/button/demo/legacy-group.tsx
+++ components/button/demo/legacy-group.tsx
@@ -4,18 +4,16 @@ import type { ButtonGroupProps } from 'antd/es/button';
 import React from 'react';
 
 const CustomGroup: React.FC<ButtonGroupProps> = (props) => (
-  <div>
-    <Button.Group {...props}>
-      <Button type="primary">Button 1</Button>
-      <Button type="primary">Button 2</Button>
-      <Tooltip title="Tooltip">
-        <Button type="primary" icon={<DownloadOutlined />} disabled />
-      </Tooltip>
-      <Tooltip title="Tooltip">
-        <Button type="primary" icon={<DownloadOutlined />} />
-      </Tooltip>
-    </Button.Group>
-  </div>
+  <Button.Group {...props}>
+    <Button type="primary">Button 1</Button>
+    <Button type="primary">Button 2</Button>
+    <Tooltip title="Tooltip">
+      <Button type="primary" icon={<DownloadOutlined />} disabled />
+    </Tooltip>
+    <Tooltip title="Tooltip">
+      <Button type="primary" icon={<DownloadOutlined />} />
+    </Tooltip>
+  </Button.Group>
 );
 
 const App: React.FC = () => (
