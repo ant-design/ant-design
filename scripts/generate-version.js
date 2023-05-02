b80c31806d03f879409c6661f364fe27b428e1e4
二货机器人 <smith3816@gmail.com>
2023-05-02 20:52:40 +0800

    chore: modify script

--- scripts/generate-version.js
+++ scripts/generate-version.js
@@ -6,7 +6,7 @@ const { version } = require('../package.json');
 fs.ensureDirSync(path.join(__dirname, '..', 'components', 'version'));
 
 fs.writeFileSync(
-  path.join(__dirname, '..', 'components', 'version', 'version.tsx'),
+  path.join(__dirname, '..', 'components', 'version', 'index.ts'),
   `export default '${version}'`,
   'utf8',
 );
