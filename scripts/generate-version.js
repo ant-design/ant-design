801abad9785f38fc80a65f460086eb6478e23207
二货机器人 <smith3816@gmail.com>
2023-05-02 20:40:03 +0800

    chore: clean all

--- scripts/generate-version.js
+++ scripts/generate-version.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const { version } = require('../package.json');
 
+fs.ensureDirSync(path.join(__dirname, '..', 'components', 'version'));
+
 fs.writeFileSync(
   path.join(__dirname, '..', 'components', 'version', 'version.tsx'),
   `export default '${version}'`,
