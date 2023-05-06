81d0cf52f336daf24625cc749660ad2d49d57e6b
Ylg12345 <2624966035@qq.com>
2023-05-06 23:11:51 +0800

    reduce diff

--- components/table/demo/virtual-list.tsx
+++ components/table/demo/virtual-list.tsx
@@ -51,9 +51,9 @@ const VirtualTable = <RecordType extends object>(props: TableProps<RecordType>) 
 
   useEffect(() => resetVirtualGrid, [tableWidth]);
 
-  const renderVirtualList = (data: readonly object[], { scrollbarSize, ref, onScroll }: any) => {
+  const renderVirtualList = (rawData: readonly object[], { scrollbarSize, ref, onScroll }: any) => {
     ref.current = connectObject;
-    const totalHeight = data.length * 54;
+    const totalHeight = rawData.length * 54;
 
     return (
       <Grid
@@ -67,7 +67,7 @@ const VirtualTable = <RecordType extends object>(props: TableProps<RecordType>) 
             : (width as number);
         }}
         height={scroll!.y as number}
-        rowCount={data.length}
+        rowCount={rawData.length}
         rowHeight={() => 54}
         width={tableWidth}
         onScroll={({ scrollLeft }: { scrollLeft: number }) => {
@@ -95,7 +95,7 @@ const VirtualTable = <RecordType extends object>(props: TableProps<RecordType>) 
               background: token.colorBgContainer,
             }}
           >
-            {(data[rowIndex] as any)[(mergedColumns as any)[columnIndex].dataIndex]}
+            {(rawData[rowIndex] as any)[(mergedColumns as any)[columnIndex].dataIndex]}
           </div>
         )}
       </Grid>
