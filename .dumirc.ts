6f42ce85f6329c1ae3ac5309b81df972e7e71149
Peach <scdzwyxst@gmail.com>
2023-05-15 11:29:50 +0800

    chore: update comment

--- .dumirc.ts
+++ .dumirc.ts
@@ -100,6 +100,7 @@ export default defineConfig({
     `,
   ],
   chainWebpack(memo) {
+    // FIXME: disable case-sensitive-paths plugin since it costs more than 1min in HMR, wait for plugin repair then remove
     memo.plugins.delete('case-sensitive-paths');
     return memo;
   },
