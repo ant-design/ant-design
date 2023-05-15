a56d483fbd3deafa423e67723000149c1caafe98
Redjue <pingfj77@gmail.com>
2023-05-15 11:19:37 +0800

    fix: color-picker review issues

--- components/color-picker/demo/format.tsx
+++ components/color-picker/demo/format.tsx
@@ -1,11 +1,14 @@
 import { Col, ColorPicker, Row, Space } from 'antd';
-import type { Color } from 'antd/es/color-picker';
+import type { Color, ColorPickerProps } from 'antd/es/color-picker';
 import React, { useMemo, useState } from 'react';
 
 export default () => {
   const [colorHex, setColorHex] = useState<Color | string>('#1677ff');
   const [colorHsb, setColorHsb] = useState<Color | string>('hsb(215, 91%, 100%)');
   const [colorRgb, setColorRgb] = useState<Color | string>('rgb(22, 119, 255)');
+  const [formatHex, setFormatHex] = useState<ColorPickerProps['format']>('hex');
+  const [formatHsb, setFormatHsb] = useState<ColorPickerProps['format']>('hsb');
+  const [formatRgb, setFormatRgb] = useState<ColorPickerProps['format']>('rgb');
 
   const hexString = useMemo(
     () => (typeof colorHex === 'string' ? colorHex : colorHex.toHexString()),
@@ -23,31 +26,46 @@ export default () => {
   );
 
   return (
-    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-      <Row align="middle">
+    <Space direction='vertical' size='middle' style={{ display: 'flex' }}>
+      <Row align='middle'>
         <Space>
           <Col>
-            <ColorPicker format="hex" value={colorHex} onChange={setColorHex} />
+            <ColorPicker
+              format={formatHex}
+              value={colorHex}
+              onChange={setColorHex}
+              onFormatChange={setFormatHex}
+            />
           </Col>
           <Col>
             HEX: <span>{hexString}</span>
           </Col>
         </Space>
       </Row>
-      <Row align="middle">
+      <Row align='middle'>
         <Space>
           <Col>
-            <ColorPicker format="hsb" value={colorHsb} onChange={setColorHsb} />
+            <ColorPicker
+              format={formatHsb}
+              value={colorHsb}
+              onChange={setColorHsb}
+              onFormatChange={setFormatHsb}
+            />
           </Col>
           <Col>
             HSB: <span>{hsbString}</span>
           </Col>
         </Space>
       </Row>
-      <Row align="middle">
+      <Row align='middle'>
         <Space>
           <Col>
-            <ColorPicker format="rgb" value={colorRgb} onChange={setColorRgb} />
+            <ColorPicker
+              format={formatRgb}
+              value={colorRgb}
+              onChange={setColorRgb}
+              onFormatChange={setFormatRgb}
+            />
           </Col>
           <Col>
             RGB: <span>{rgbString}</span>
