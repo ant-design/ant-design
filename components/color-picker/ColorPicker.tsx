26135e8bb23c6b681fea58e5abbf310454856e45
kiner-tang(文辉) <1127031143@qq.com>
2023-05-16 18:20:03 +0800

    chore: test

--- components/color-picker/ColorPicker.tsx
+++ components/color-picker/ColorPicker.tsx
@@ -7,6 +7,7 @@ import classNames from 'classnames';
 import useMergedState from 'rc-util/lib/hooks/useMergedState';
 import type { CSSProperties } from 'react';
 import React, { useContext, useState } from 'react';
+import genPurePanel from '../_util/PurePanel';
 import type { ConfigConsumerProps } from '../config-provider/context';
 import { ConfigContext } from '../config-provider/context';
 import type { PopoverProps } from '../popover';
@@ -19,7 +20,6 @@ import useColorState from './hooks/useColorState';
 import type { ColorFormat, ColorPickerBaseProps, PresetsItem } from './interface';
 import useStyle from './style/index';
 import { customizePrefixCls, generateColor } from './util';
-import genPurePanel from '../_util/PurePanel';
 
 export interface ColorPickerProps
   extends Omit<
@@ -164,16 +164,11 @@ if (process.env.NODE_ENV !== 'production') {
   ColorPicker.displayName = 'ColorPicker';
 }
 
-const PurePanel = genPurePanel(
-  ColorPicker,
-  'color-picker',
-  (prefixCls) => prefixCls,
-  (props: ColorPickerProps) => ({
-    ...props,
-    placement: 'bottom' as TriggerPlacement,
-    autoAdjustOverflow: false,
-  }),
-);
+const PurePanel = genPurePanel(ColorPicker, 'color-picker', null, (props: ColorPickerProps) => ({
+  ...props,
+  placement: 'bottom' as TriggerPlacement,
+  autoAdjustOverflow: false,
+}));
 
 ColorPicker._InternalPanelDoNotUseOrYouWillBeFired = PurePanel;
 
