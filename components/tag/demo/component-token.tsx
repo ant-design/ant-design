0cc416403f0b7f0ae64d7af0b4dccbc0e13943d9
黑雨 <wangning4567@163.com>
2023-05-19 13:59:57 +0800

    ✨ feat: update

--- components/tag/demo/component-token.tsx
+++ components/tag/demo/component-token.tsx
@@ -1,3 +1,4 @@
+import { CloseCircleOutlined, SyncOutlined } from '@ant-design/icons';
 import { ConfigProvider, Space, Tag } from 'antd';
 import React from 'react';
 
@@ -15,12 +16,22 @@ export default () => (
     }}
   >
     <Space size={[0, 8]} wrap>
-      <Tag>Tag 1</Tag>
       <Tag>
         <a href="https://github.com/ant-design/ant-design/issues/1862">Link</a>
       </Tag>
-      <Tag closable>Tag 2</Tag>
-      <Tag closable>Prevent Default</Tag>
+      <Tag closable color="magenta">
+        Tag 2
+      </Tag>
+      <Tag icon={<CloseCircleOutlined />} color="error">
+        error
+      </Tag>
+      <Tag color="red-inverse">red</Tag>
+      <Tag bordered={false} color="magenta">
+        magenta
+      </Tag>
+      <Tag icon={<SyncOutlined spin />} color="processing">
+        processing
+      </Tag>
     </Space>
   </ConfigProvider>
 );
