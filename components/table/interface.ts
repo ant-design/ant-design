fa687885085918b7b2b1aab6b614aa2d61873d25
sawadyecma <ikuma.sawada@japan-d2.com>
2023-05-19 19:04:16 +0900

    implement sortIcon on Table

--- components/table/interface.ts
+++ components/table/interface.ts
@@ -119,6 +119,7 @@ export interface ColumnType<RecordType> extends Omit<RcColumnType<RecordType>, '
   sortOrder?: SortOrder;
   defaultSortOrder?: SortOrder;
   sortDirections?: SortOrder[];
+  sortIcon?: React.ReactNode | ((sortOrder: SortOrder) => React.ReactNode);
   showSorterTooltip?: boolean | TooltipProps;
 
   // Filter
