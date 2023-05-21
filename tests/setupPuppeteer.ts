abb4433c2b017ed14aeca7a664062102c5e15f83
Dunqing <dengqing0821@gmail.com>
2023-05-21 12:17:02 +0800

    fix: should close browser at the ended

--- tests/setupPuppeteer.ts
+++ tests/setupPuppeteer.ts
@@ -1,36 +1,44 @@
 import puppeteer from 'puppeteer';
+import { afterAll, beforeAll } from 'vitest';
 
-const browser = await puppeteer.launch({
-  headless: 'new',
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+beforeAll(async () => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+  });
+  const handlePageError = (error: Error) => {
+    process.emit('uncaughtException', error);
+  };
+
+  const openPage = async () => {
+    if (globalThis.page) {
+      throw new Error('Cannot open page before closing previous page.');
+    }
+    const page = await browser.newPage();
+    page.on('pageerror', handlePageError);
+    globalThis.page = page;
+  };
+
+  const closePage = async () => {
+    if (!globalThis.page) return;
+    globalThis.page.off('pageerror', handlePageError);
+    await globalThis.page.close({
+      runBeforeUnload: true,
+    });
+    globalThis.page = undefined as any;
+  };
+
+  const resetPage = async () => {
+    await closePage();
+    await openPage();
+  };
+
+  globalThis.resetPage = resetPage;
+  globalThis.browser = browser;
 });
 
-const handlePageError = (error: Error) => {
-  process.emit('uncaughtException', error);
-};
-
-const openPage = async () => {
-  if (globalThis.page) {
-    throw new Error('Cannot open page before closing previous page.');
+afterAll(() => {
+  if (globalThis.browser) {
+    globalThis.browser.close();
   }
-  const page = await browser.newPage();
-  page.on('pageerror', handlePageError);
-  globalThis.page = page;
-};
-
-const closePage = async () => {
-  if (!globalThis.page) return;
-  globalThis.page.off('pageerror', handlePageError);
-  await globalThis.page.close({
-    runBeforeUnload: true,
-  });
-  globalThis.page = undefined as any;
-};
-
-const resetPage = async () => {
-  await closePage();
-  await openPage();
-};
-
-globalThis.resetPage = resetPage;
-globalThis.browser = browser;
+});
