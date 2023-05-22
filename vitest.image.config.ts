17c4a6a121bcca919df47ef05921be080f348433
Dunqing <dengqing0821@gmail.com>
2023-05-22 02:19:52 +0000

    chore: update image test

--- vitest.image.config.ts
+++ vitest.image.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vitest/config';
 import { commonConfig } from './vitest.config';
 
 export default defineConfig({
-  ...commonConfig,
   test: {
     ...commonConfig.test,
-    include: ['components/*/__tests__/image.test.{ts,tsx}'],
+    include: ['components/config-provider/__tests__/image.test.{ts,tsx}'],
     globals: true,
-    setupFiles: ['./tests/setup.ts', './tests/setupPuppeteer.ts'],
+    setupFiles: ['./tests/setupPuppeteer.ts'],
     environment: 'jsdom',
   },
 });
