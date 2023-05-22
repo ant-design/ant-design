33d94f3c3e33edccd06b4f5a4323ac4c03e57fae
sawadyecma <ikuma.sawada@japan-d2.com>
2023-05-22 10:05:35 +0900

    add sortIcon test

--- components/table/__tests__/Table.sorter.test.tsx
+++ components/table/__tests__/Table.sorter.test.tsx
@@ -355,6 +355,39 @@ describe('Table.sorter', () => {
     expect(container.querySelector('.ant-tooltip-open')).toBeFalsy();
   });
 
+  it('renders custome sort icon correctly', () => {
+    const sortIcon = (sortOrder?: SortOrder): React.ReactNode => {
+      let text: string;
+      if (sortOrder === undefined) {
+        text = 'unsorted';
+      } else if (sortOrder === 'descend') {
+        text = 'sortDescend';
+      } else {
+        text = 'sortAscend';
+      }
+
+      return <span className="customize-icon">{text}</span>;
+    };
+
+    const { container } = render(
+      createTable({
+        columns: [
+          {
+            ...column,
+            sortIcon,
+          },
+        ],
+      }),
+    );
+
+    fireEvent.click(container.querySelector('.customize-icon')!);
+    expect(container.querySelector('.customize-icon')).toMatchSnapshot();
+    fireEvent.click(container.querySelector('.customize-icon')!);
+    expect(container.querySelector('.customize-icon')).toMatchSnapshot();
+    fireEvent.click(container.querySelector('.customize-icon')!);
+    expect(container.querySelector('.customize-icon')).toMatchSnapshot();
+  });
+
   it('works with grouping columns in controlled mode', () => {
     const columns = [
       {
