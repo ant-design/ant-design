1fde1fd32122071491a9802e24ecd6625b44de21
sy296565890 <sy296565890@outlook.com>
2023-05-24 01:44:01 +0800

    fix: QRCode support renderAs

--- components/qrcode/demo/renderAs.tsx
+++ components/qrcode/demo/renderAs.tsx
@@ -1,15 +1,13 @@
 import { QRCode, Space, Tooltip } from 'antd';
 import React from 'react';
 
-const App: React.FC = () => {
-  return (
-    <Tooltip title="二维码上鼠标右键->检查->源码中查看渲染结果">
-      <Space>
-        <QRCode renderAs="canvas" value="https://ant.design/" />
-        <QRCode renderAs="svg" value="https://ant.design/" />
-      </Space>
-    </Tooltip>
-  );
-};
+const App: React.FC = () => (
+  <Tooltip title="二维码上鼠标右键->检查->源码中查看渲染结果">
+    <Space>
+      <QRCode renderAs="canvas" value="https://ant.design/" />
+      <QRCode renderAs="svg" value="https://ant.design/" />
+    </Space>
+  </Tooltip>
+);
 
 export default App;
