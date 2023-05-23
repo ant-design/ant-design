7c367c190572c0de2381f98698f5dfa397c140da
MadCcc <1075746765@qq.com>
2023-05-23 13:51:17 +0800

    chore: use one screenshot per component

--- tests/shared/imageTest.tsx
+++ tests/shared/imageTest.tsx
@@ -18,57 +18,55 @@ expect.extend({ toMatchImageSnapshot });
 
 // eslint-disable-next-line jest/no-export
 export default function imageTest(component: React.ReactElement) {
-  [theme.defaultAlgorithm, theme.darkAlgorithm, theme.compactAlgorithm].forEach(
-    (algorithm, index) => {
-      it(`component image screenshot should correct${index || ''}`, async () => {
-        await jestPuppeteer.resetPage();
-        await page.setRequestInterception(true);
-        const onRequestHandle = (request: any) => {
-          if (['image'].includes(request.resourceType())) {
-            request.abort();
-          } else {
-            request.continue();
-          }
-        };
+  [theme.defaultAlgorithm, theme.darkAlgorithm, theme.compactAlgorithm].forEach((algorithm) => {
+    it(`component image screenshot should correct with ${algorithm.name}`, async () => {
+      await jestPuppeteer.resetPage();
+      await page.setRequestInterception(true);
+      const onRequestHandle = (request: any) => {
+        if (['image'].includes(request.resourceType())) {
+          request.abort();
+        } else {
+          request.continue();
+        }
+      };
 
-        MockDate.set(dayjs('2016-11-22').valueOf());
-        page.on('request', onRequestHandle);
-        await page.goto(`file://${process.cwd()}/tests/index.html`);
-        await page.addStyleTag({ path: `${process.cwd()}/dist/reset.css` });
+      MockDate.set(dayjs('2016-11-22').valueOf());
+      page.on('request', onRequestHandle);
+      await page.goto(`file://${process.cwd()}/tests/index.html`);
+      await page.addStyleTag({ path: `${process.cwd()}/dist/reset.css` });
 
-        const cache = createCache();
+      const cache = createCache();
 
-        const element = (
-          <ConfigProvider theme={{ algorithm }}>
-            <App style={{ background: algorithm === theme.darkAlgorithm ? '#000' : '' }}>
-              <StyleProvider cache={cache}>{component}</StyleProvider>
-            </App>
-          </ConfigProvider>
-        );
+      const element = (
+        <ConfigProvider theme={{ algorithm }}>
+          <App style={{ background: algorithm === theme.darkAlgorithm ? '#000' : '' }}>
+            <StyleProvider cache={cache}>{component}</StyleProvider>
+          </App>
+        </ConfigProvider>
+      );
 
-        const html = ReactDOMServer.renderToString(element);
-        const styleStr = extractStyle(cache);
+      const html = ReactDOMServer.renderToString(element);
+      const styleStr = extractStyle(cache);
 
-        await page.evaluate(
-          (innerHTML, ssrStyle) => {
-            document.querySelector('#root')!.innerHTML = innerHTML;
+      await page.evaluate(
+        (innerHTML, ssrStyle) => {
+          document.querySelector('#root')!.innerHTML = innerHTML;
 
-            const head = document.querySelector('head')!;
-            head.innerHTML += ssrStyle;
-          },
-          html,
-          styleStr,
-        );
+          const head = document.querySelector('head')!;
+          head.innerHTML += ssrStyle;
+        },
+        html,
+        styleStr,
+      );
 
-        const image = await page.screenshot();
+      const image = await page.screenshot();
 
-        expect(image).toMatchImageSnapshot();
+      expect(image).toMatchImageSnapshot();
 
-        MockDate.reset();
-        page.removeListener('request', onRequestHandle);
-      });
-    },
-  );
+      MockDate.reset();
+      page.removeListener('request', onRequestHandle);
+    });
+  });
 }
 
 type Options = {
@@ -80,19 +78,23 @@ export function imageDemoTest(component: string, options: Options = {}) {
   let describeMethod = options.skip === true ? describe.skip : describe;
   const files = globSync(`./components/${component}/demo/*.tsx`);
 
-  files.forEach((file) => {
-    if (Array.isArray(options.skip) && options.skip.some((c) => file.includes(c))) {
-      describeMethod = describe.skip;
-    } else {
-      describeMethod = describe;
-    }
-    describeMethod(`Test ${file} image`, () => {
-      // eslint-disable-next-line global-require,import/no-dynamic-require
-      let Demo = require(`../../${file}`).default;
-      if (typeof Demo === 'function') {
-        Demo = <Demo />;
-      }
-      imageTest(Demo);
-    });
+  describeMethod(`Test ${component} image`, () => {
+    imageTest(
+      <>
+        {files.map((file) => {
+          if (Array.isArray(options.skip) && options.skip.some((c) => file.includes(c))) {
+            describeMethod = describe.skip;
+          } else {
+            describeMethod = describe;
+          }
+          // eslint-disable-next-line global-require,import/no-dynamic-require
+          let Demo = require(`../../${file}`).default;
+          if (typeof Demo === 'function') {
+            Demo = <Demo />;
+          }
+          return Demo;
+        })}
+      </>,
+    );
   });
 }
