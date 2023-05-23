cb92cd1d8bd1eb7c7f48d991169f29029b572ffb
黑雨 <wangning4567@163.com>
2023-05-23 14:20:23 +0800

    ✨ feat: update

--- components/tabs/demo/component-token.tsx
+++ components/tabs/demo/component-token.tsx
@@ -1,15 +1,15 @@
-import { ConfigProvider, Select, Tabs } from 'antd';
+import { AppleOutlined } from '@ant-design/icons';
+import type { RadioChangeEvent } from 'antd';
+import { Button, ConfigProvider, Radio, Tabs } from 'antd';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
 import React, { useState } from 'react';
 
-const { Option } = Select;
-
-const positionList = ['left', 'right', 'top', 'bottom'];
-
 const App: React.FC = () => {
-  const [parentPos, setParentPos] = useState(undefined);
-  const [childPos, setChildPos] = useState(undefined);
-  const [parentType, setParentType] = useState(undefined);
-  const [childType, setChildType] = useState(undefined);
+  const [size, setSize] = useState<SizeType>('small');
+
+  const onChange = (e: RadioChangeEvent) => {
+    setSize(e.target.value);
+  };
 
   return (
     <ConfigProvider
@@ -45,86 +45,57 @@ const App: React.FC = () => {
       }}
     >
       <div>
-        <Select
-          style={{ width: 200 }}
-          onChange={(val) => {
-            setParentPos(val);
-          }}
-        >
-          {positionList.map((pos) => (
-            <Option key={pos} value={pos}>
-              Parent - {pos}
-            </Option>
-          ))}
-        </Select>
-
-        <Select
-          style={{ width: 200 }}
-          onChange={(val) => {
-            setChildPos(val);
-          }}
-        >
-          {positionList.map((pos) => (
-            <Option key={pos} value={pos}>
-              Child - {pos}
-            </Option>
-          ))}
-        </Select>
-
-        <Select
-          style={{ width: 200 }}
-          onChange={(val) => {
-            setParentType(val);
-          }}
-        >
-          <Option value="line">Parent - line</Option>
-          <Option value="card">Parent - card</Option>
-          <Option value="editable-card">Parent - card edit</Option>
-        </Select>
-
-        <Select
-          style={{ width: 200 }}
-          onChange={(val) => {
-            setChildType(val);
-          }}
-        >
-          <Option value="line">Child - line</Option>
-          <Option value="card">Child - card</Option>
-          <Option value="editable-card">Parent - card edit</Option>
-        </Select>
-
+        <Radio.Group value={size} onChange={onChange} style={{ marginBottom: 16 }}>
+          <Radio.Button value="small">Small</Radio.Button>
+          <Radio.Button value="middle">Middle</Radio.Button>
+          <Radio.Button value="large">Large</Radio.Button>
+        </Radio.Group>
         <Tabs
           defaultActiveKey="1"
-          tabPosition={parentPos}
-          type={parentType}
-          items={[
-            {
-              label: 'Tab 1',
-              key: '1',
-              children: (
-                <Tabs
-                  defaultActiveKey="1"
-                  tabPosition={childPos}
-                  type={childType}
-                  style={{ height: 300 }}
-                  items={new Array(20).fill(null).map((_, index) => {
-                    const key = String(index);
-
-                    return {
-                      label: `Tab ${key}`,
-                      key,
-                      children: `TTTT ${key}`,
-                    };
-                  })}
-                />
+          size={size}
+          tabPosition="right"
+          tabBarExtraContent={<Button>Extra Action</Button>}
+          style={{ marginBottom: 32 }}
+          items={new Array(3).fill(null).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          defaultActiveKey="1"
+          type="card"
+          size={size}
+          items={new Array(30).fill(null).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: (
+                <span>
+                  <AppleOutlined />
+                  Card Tab{id}
+                </span>
               ),
-            },
-            {
-              label: 'Tab 2',
-              key: '2',
-              children: 'Content of Tab Pane 2',
-            },
-          ]}
+              key: id,
+              children: `Content of card tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          defaultActiveKey="1"
+          centered
+          type="card"
+          items={new Array(3).fill(null).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              disabled: i === 2,
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of Tab Pane ${id}`,
+            };
+          })}
         />
       </div>
     </ConfigProvider>
