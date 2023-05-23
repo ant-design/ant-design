5e344f8a61e8adf85398d3a445c441b8b117c3c6
kiner-tang(文辉) <1127031143@qq.com>
2023-05-23 18:16:30 +0800

    feat: optimize code

--- components/collapse/demo/extra.tsx
+++ components/collapse/demo/extra.tsx
@@ -37,19 +37,19 @@ const App: React.FC = () => {
     {
       key: '1',
       label: 'This is panel header 1',
-      children: <p>{text}</p>,
+      children: <div>{text}</div>,
       extra: genExtra(),
     },
     {
       key: '2',
       label: 'This is panel header 2',
-      children: <p>{text}</p>,
+      children: <div>{text}</div>,
       extra: genExtra(),
     },
     {
       key: '3',
       label: 'This is panel header 3',
-      children: <p>{text}</p>,
+      children: <div>{text}</div>,
       extra: genExtra(),
     },
   ];
