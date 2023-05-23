a74e0b2d509a1ed9bf9ac0df9946b81e3daabb16
黑雨 <wangning4567@163.com>
2023-05-23 17:23:14 +0800

    ✨ feat: update snap

--- components/slider/demo/component-token.tsx
+++ components/slider/demo/component-token.tsx
@@ -1,5 +1,45 @@
+import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
 import { ConfigProvider, Slider } from 'antd';
-import React from 'react';
+import type { SliderMarks } from 'antd/es/slider';
+import React, { useState } from 'react';
+
+const style: React.CSSProperties = {
+  display: 'inline-block',
+  height: 300,
+  marginLeft: 70,
+};
+
+const marks: SliderMarks = {
+  0: '0°C',
+  26: '26°C',
+  37: '37°C',
+  100: {
+    style: { color: '#f50' },
+    label: <strong>100°C</strong>,
+  },
+};
+
+interface IconSliderProps {
+  max: number;
+  min: number;
+}
+
+const IconSlider: React.FC<IconSliderProps> = (props) => {
+  const { max, min } = props;
+  const [value, setValue] = useState(0);
+
+  const mid = Number(((max - min) / 2).toFixed(5));
+  const preColorCls = value >= mid ? '' : 'icon-wrapper-active';
+  const nextColorCls = value >= mid ? 'icon-wrapper-active' : '';
+
+  return (
+    <div className="icon-wrapper">
+      <FrownOutlined className={preColorCls} />
+      <Slider {...props} onChange={setValue} value={value} />
+      <SmileOutlined className={nextColorCls} />
+    </div>
+  );
+};
 
 const App: React.FC = () => (
   <ConfigProvider
@@ -29,7 +69,25 @@ const App: React.FC = () => (
       },
     }}
   >
+    <div style={style}>
+      <Slider vertical defaultValue={30} />
+    </div>
+    <div style={style}>
+      <Slider vertical range step={10} defaultValue={[20, 50]} />
+    </div>
+    <div style={style}>
+      <Slider vertical range marks={marks} defaultValue={[26, 37]} />
+    </div>
+
     <Slider range defaultValue={[20, 50]} />
+
+    <Slider range={{ draggableTrack: true }} defaultValue={[20, 50]} />
+
+    <Slider defaultValue={30} tooltip={{ open: true }} />
+
+    <IconSlider min={0} max={20} />
+
+    <Slider range marks={marks} defaultValue={[26, 37]} />
   </ConfigProvider>
 );
 
