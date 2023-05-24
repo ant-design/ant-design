3a3f6c669ffe0ae490e0897df0874be3d071e642
sy296565890 <sy296565890@outlook.com>
2023-05-24 17:10:09 +0800

    fix: QRCode demo

--- components/qrcode/demo/type.tsx
+++ components/qrcode/demo/type.tsx
@@ -1,13 +1,11 @@
-import { QRCode, Space, Tooltip } from 'antd';
+import { QRCode, Space } from 'antd';
 import React from 'react';
 
 const App: React.FC = () => (
-  <Tooltip title="二维码上鼠标右键->检查->源码中查看渲染结果">
-    <Space>
-      <QRCode type="canvas" value="https://ant.design/" />
-      <QRCode type="svg" value="https://ant.design/" />
-    </Space>
-  </Tooltip>
+  <Space>
+    <QRCode type="canvas" value="https://ant.design/" />
+    <QRCode type="svg" value="https://ant.design/" />
+  </Space>
 );
 
 export default App;
