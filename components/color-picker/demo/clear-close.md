cf24e3dd575f9fa89d8131a5098f2eccbd10931a
linxianxi <904492381@qq.com>
2023-05-26 16:32:44 +0800

    demo: remove

--- components/color-picker/demo/clear-close.md
+++
@@ -1,7 +0,0 @@
-## zh-CN
-
-清除时关闭弹窗。
-
-## en-US
-
-close popover when clearing.
--- components/color-picker/demo/clear-close.tsx
+++
@@ -1,9 +0,0 @@
-import { ColorPicker } from 'antd';
-import React, { useState } from 'react';
-
-export default () => {
-  const [open, setOpen] = useState(false);
-  return (
-    <ColorPicker open={open} allowClear onOpenChange={setOpen} onClear={() => setOpen(false)} />
-  );
-};
