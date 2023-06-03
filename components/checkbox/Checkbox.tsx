b80d7ab08f41e982ac17e78d6665b6e659a1430c
jrr997 <997571692@qq.com>
2023-06-03 15:55:21 +0800

    refactor: resolve circular reference of Checkbox

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -2,11 +2,11 @@ import classNames from 'classnames';
 import type { CheckboxRef } from 'rc-checkbox';
 import RcCheckbox from 'rc-checkbox';
 import * as React from 'react';
-import { ConfigContext } from '../config-provider';
-import { FormItemInputContext } from '../form/context';
 import warning from '../_util/warning';
-import { GroupContext } from './Group';
+import { ConfigContext } from '../config-provider';
 import DisabledContext from '../config-provider/DisabledContext';
+import { FormItemInputContext } from '../form/context';
+import GroupContext from './GroupContext';
 
 import useStyle from './style';
 
