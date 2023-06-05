3629ad5859c76ad81cc182e3327692a6610c4a1a
MuxinFeng <434980373@qq.com>
2023-06-05 18:15:11 +0800

    test: update test

--- components/input-number/__tests__/prefix.test.tsx
+++ components/input-number/__tests__/prefix.test.tsx
@@ -11,10 +11,12 @@ describe('prefix', () => {
   it('should support className when has prefix', () => {
     const { container } = render(<InputNumber prefix="suffix" className="my-class-name" />);
     expect((container.firstChild as HTMLElement)?.className.includes('my-class-name')).toBe(true);
+    expect(container.querySelector('input')?.className.includes('my-class-name')).toBe(false);
   });
 
   it('should trigger focus when prefix is clicked', () => {
     const { container } = render(<InputNumber prefix={<i>123</i>} />);
+
     const mockFocus = jest.spyOn(container.querySelector('input')!, 'focus');
     fireEvent.click(container.querySelector('i')!);
     expect(mockFocus).toHaveBeenCalled();
