3c9b00bb119f868825916f3f7bb339fb4f2dd43e
MuxinFeng <434980373@qq.com>
2023-06-05 18:06:54 +0800

    test: update test

--- components/input-number/__tests__/prefix.test.tsx
+++ components/input-number/__tests__/prefix.test.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import InputNumber from '..';
+import focusTest from '../../../tests/shared/focusTest';
 import { fireEvent, render } from '../../../tests/utils';
 
 describe('prefix', () => {
+  focusTest(
+    forwardRef((props, ref) => <InputNumber {...props} prefix="A" ref={ref} />),
+    { refFocus: true },
+  );
   it('should support className when has prefix', () => {
     const { container } = render(<InputNumber prefix="suffix" className="my-class-name" />);
     expect((container.firstChild as HTMLElement)?.className.includes('my-class-name')).toBe(true);
-    expect(container.querySelector('input')?.className.includes('my-class-name')).toBe(false);
   });
 
   it('should trigger focus when prefix is clicked', () => {
