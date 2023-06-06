9e6ce373a8af99fa8a12f8fdb49027dbd191b807
黑雨 <wangning4567@163.com>
2023-06-06 17:16:51 +0800

    ✨ feat: add demo

--- components/slider/demo/component-token.tsx
+++ components/slider/demo/component-token.tsx
@@ -1,6 +1,22 @@
 import { ConfigProvider, Slider } from 'antd';
 import React from 'react';
 
+const style: React.CSSProperties = {
+  display: 'inline-block',
+  height: 300,
+  marginLeft: 70,
+};
+
+const marks = {
+  0: '0°C',
+  26: '26°C',
+  37: '37°C',
+  100: {
+    style: { color: '#f50' },
+    label: <strong>100°C</strong>,
+  },
+};
+
 const App: React.FC = () => (
   <ConfigProvider
     theme={{
@@ -29,8 +45,16 @@ const App: React.FC = () => (
       },
     }}
   >
-    <Slider range defaultValue={[20, 50]} />
     <Slider range={{ draggableTrack: true }} defaultValue={[20, 50]} />
+    <div style={style}>
+      <Slider vertical defaultValue={30} />
+    </div>
+    <div style={style}>
+      <Slider vertical range step={10} defaultValue={[20, 50]} />
+    </div>
+    <div style={style}>
+      <Slider vertical range marks={marks} defaultValue={[26, 37]} />
+    </div>
   </ConfigProvider>
 );
 
