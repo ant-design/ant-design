458e9bf6b738a00bd01b7ee1954982c58c9cedd9
poyiding <dingkang0458@gmail.com>
2023-06-06 16:44:55 +0800

    feat: add warning

--- components/float-button/demo/controlled.tsx
+++ components/float-button/demo/controlled.tsx
@@ -3,10 +3,11 @@ import { FloatButton, Switch } from 'antd';
 import React, { useState } from 'react';
 
 const App: React.FC = () => {
+  const [show, setShow] = useState(true);
   const [open, setOpen] = useState(true);
 
   const onChange = (checked: boolean) => {
-    setOpen(checked);
+    setShow(checked);
   };
 
   return (
@@ -14,13 +15,24 @@ const App: React.FC = () => {
       <FloatButton.Group
         open={open}
         trigger="click"
+        type="primary"
         style={{ right: 24 }}
+        onOpenChange={(opened) => setOpen(!opened)}
         icon={<CustomerServiceOutlined />}
       >
         <FloatButton />
         <FloatButton icon={<CommentOutlined />} />
       </FloatButton.Group>
-      <Switch onChange={onChange} checked={open} />
+      <FloatButton.Group
+        open={show}
+        trigger="click"
+        style={{ right: 94 }}
+        icon={<CustomerServiceOutlined />}
+      >
+        <FloatButton />
+        <FloatButton icon={<CommentOutlined />} />
+      </FloatButton.Group>
+      <Switch onChange={onChange} checked={show} />
     </>
   );
 };
