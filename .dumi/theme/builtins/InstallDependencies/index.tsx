0754e4c29fb259036fe764c7aa59ca7c54e1ce43
kiner-tang(文辉) <1127031143@qq.com>
2023-06-15 19:27:11 +0800

    site: hide pkgManager tab when the command is empty

--- .dumi/theme/builtins/InstallDependencies/index.tsx
+++ .dumi/theme/builtins/InstallDependencies/index.tsx
@@ -11,45 +11,37 @@ interface InstallProps {
   pnpm?: string;
 }
 
-const InstallDependencies: React.FC<InstallProps> = (props) => {
-  const { npm, yarn, pnpm } = props;
+const InstallDependencies: React.FC<InstallProps> = ({ npm, yarn, pnpm }) => {
+  const options = [
+    { key: 'npm', value: npm, label: 'npm', icon: <NpmLogo /> },
+    { key: 'yarn', value: yarn, label: 'yarn', icon: <YarnLogo /> },
+    { key: 'pnpm', value: pnpm, label: 'pnpm', icon: <PnpmLogo /> },
+  ];
+
+  const filteredOptions = options.filter((option) => option.value);
+
+  if (filteredOptions.length === 0) return null;
+
+  const renderTabPane = (option) => (
+    <Tabs.TabPane
+      key={option.key}
+      tab={
+        <span className="snippet-label">
+          {option.icon}
+          {option.label}
+        </span>
+      }
+    >
+      <SourceCode lang="bash">{option.value}</SourceCode>
+    </Tabs.TabPane>
+  );
+
+  const tabPanes = filteredOptions.map(renderTabPane);
+
   return (
-    <Tabs
-      className="antd-site-snippet"
-      defaultActiveKey="npm"
-      items={[
-        {
-          key: 'npm',
-          children: <SourceCode lang="bash">{npm}</SourceCode>,
-          label: (
-            <span className="snippet-label">
-              <NpmLogo />
-              npm
-            </span>
-          ),
-        },
-        {
-          key: 'yarn',
-          children: <SourceCode lang="bash">{yarn}</SourceCode>,
-          label: (
-            <span className="snippet-label">
-              <YarnLogo />
-              yarn
-            </span>
-          ),
-        },
-        {
-          key: 'pnpm',
-          children: <SourceCode lang="bash">{pnpm}</SourceCode>,
-          label: (
-            <span className="snippet-label">
-              <PnpmLogo />
-              pnpm
-            </span>
-          ),
-        },
-      ]}
-    />
+    <Tabs className="antd-site-snippet" defaultActiveKey={filteredOptions[0].key}>
+      {tabPanes}
+    </Tabs>
   );
 };
 
