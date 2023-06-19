53fd1db960b043991f69da43e0b42f07d352808e
Redjue <pingfj77@gmail.com>
2023-06-19 17:06:49 +0800

    fix: lint issue

--- components/color-picker/demo/text-render.tsx
+++ components/color-picker/demo/text-render.tsx
@@ -1,13 +1,11 @@
 import { ColorPicker, Space } from 'antd';
 import React from 'react';
 
-const Demo = () => {
-  return (
-    <Space direction="vertical">
-      <ColorPicker showText />
-      <ColorPicker textRender={(color) => <span>Custom Text ({color.toHexString()})</span>} />
-    </Space>
-  );
-};
+const Demo = () => (
+  <Space direction="vertical">
+    <ColorPicker showText />
+    <ColorPicker textRender={(color) => <span>Custom Text ({color.toHexString()})</span>} />
+  </Space>
+);
 
 export default Demo;
