7aecef8d3b8fd91d71c1e0eba62b0ac558cfca0c
栗嘉男 <574980606@qq.com>
2023-06-20 17:39:16 +0800

    fix

--- .dumi/theme/slots/Content/ZhihuCard.tsx
+++ .dumi/theme/slots/Content/ZhihuCard.tsx
@@ -2,67 +2,71 @@ import { css } from '@emotion/react';
 import { Button, Card } from 'antd';
 import React from 'react';
 import useLocale from '../../../hooks/useLocale';
+import useSiteToken from '../../../hooks/useSiteToken';
 
 const ANTD_IMG_URL =
   'https://picx.zhimg.com/v2-3b2bca09c2771e7a82a81562e806be4d_200x0.jpg?source=d16d100b';
 
-const useStyle = () => ({
-  card: css`
-    width: 100%;
-    margin: 40px 0;
-    transition: all 0.2s;
-    background-color: rgba(0, 0, 0, 0.01);
-    &:hover {
-      border-color: transparent;
-      box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12),
-        0 5px 12px 4px rgba(0, 0, 0, 0.1);
-    }
-  `,
-  bigTitle: css`
-    font-size: 16px;
-    color: #121212;
-    margin-bottom: 24px;
-    font-weight: 500;
-  `,
-  cardBody: css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  `,
-  left: css`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    img {
-      width: 200px;
-      margin-right: 24px;
-      overflow: hidden;
-      border-radius: 8px;
-    }
-  `,
-  title: css`
-    color: #444;
-    font-size: 15px;
-    font-weight: 600;
-  `,
-  subTitle: css`
-    color: #646464;
-    font-size: 14px;
-    font-weight: 400;
-    margin-top: 8px;
-  `,
-});
+const useStyle = () => {
+  const { token } = useSiteToken();
+  return {
+    card: css`
+      width: 100%;
+      margin: 40px 0;
+      transition: all 0.2s;
+      background-color: ${token.siteMarkdownCodeBg};
+      &:hover {
+        border-color: transparent;
+        box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12),
+          0 5px 12px 4px rgba(0, 0, 0, 0.1);
+      }
+    `,
+    bigTitle: css`
+      font-size: 16px;
+      color: #121212;
+      margin-bottom: 24px;
+      font-weight: 500;
+    `,
+    cardBody: css`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    `,
+    left: css`
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      img {
+        width: 200px;
+        margin-right: 24px;
+        overflow: hidden;
+        border-radius: 8px;
+      }
+    `,
+    title: css`
+      color: #444;
+      font-size: 16px;
+      font-weight: 600;
+    `,
+    subTitle: css`
+      color: #646464;
+      font-size: 14px;
+      font-weight: 400;
+      margin-top: 8px;
+    `,
+  };
+};
 
 const locales = {
   cn: {
     bigTitle: '文章被收录于专栏：',
     subTitle: '一个 UI 设计体系',
-    buttonText: '去专栏参与讨论',
+    buttonText: '参与讨论',
   },
   en: {
     bigTitle: 'Articles are included in the column:',
     subTitle: 'A UI design system',
-    buttonText: 'go to the column to discuss',
+    buttonText: 'go to discuss',
   },
 };
 
