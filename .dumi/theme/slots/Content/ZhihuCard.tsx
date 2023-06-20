3280a0b70ecc4d581cdd71fd842b355162bb62b6
栗嘉男 <574980606@qq.com>
2023-06-20 22:31:04 +0800

    fix

--- .dumi/theme/slots/Content/ZhihuCard.tsx
+++ .dumi/theme/slots/Content/ZhihuCard.tsx
@@ -14,11 +14,9 @@ const useStyle = () => {
       width: 100%;
       margin: 40px 0;
       transition: all 0.2s;
-      background-color: ${token.siteMarkdownCodeBg};
+      background-color: ${token.colorFillQuaternary};
       &:hover {
-        border-color: transparent;
-        box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12),
-          0 5px 12px 4px rgba(0, 0, 0, 0.1);
+        border-color: rgba(0, 0, 0, 0.1);
       }
     `,
     bigTitle: css`
@@ -59,7 +57,7 @@ const useStyle = () => {
 
 const locales = {
   cn: {
-    bigTitle: '文章被收录于专栏：',
+    bigTitle: '文章被收录于知乎专栏：',
     subTitle: '一个 UI 设计体系',
     buttonText: '参与讨论',
   },
