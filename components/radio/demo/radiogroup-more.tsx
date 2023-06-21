79407a78f210544681123e1d0868ea7d79ef1509
Trieu010 <91833984+Trieu010@users.noreply.github.com>
2023-06-21 09:40:48 +0700

    Update radiogroup-more.tsx

--- components/radio/demo/radiogroup-more.tsx
+++ components/radio/demo/radiogroup-more.tsx
@@ -15,9 +15,6 @@ const App: React.FC = () => {
       <Space direction="vertical">
         <Radio value={1}>Option A</Radio>
         <Radio value={2}>Option B</Radio>
-        <Radio value={3}>Option C</Radio>
-        <Radio value={4}>
-          More...
           {value === 4 ? <Input style={{ width: 100, marginLeft: 10 }} /> : null}
         </Radio>
       </Space>
