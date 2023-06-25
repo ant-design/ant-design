869fe7c82f988b61df05c9d0697ac27487db7c8f
Redjue <pingfj77@gmail.com>
2023-06-25 16:29:58 +0800

    docs: showText add select demo

--- components/color-picker/demo/text-render.tsx
+++ components/color-picker/demo/text-render.tsx
@@ -1,11 +1,55 @@
-import { ColorPicker, Space } from 'antd';
-import React from 'react';
+import { ColorPicker, Select, Space } from 'antd';
+import type { ColorPickerProps } from 'antd/es/color-picker';
+import React, { useRef, useState } from 'react';
 
-const Demo = () => (
-  <Space direction="vertical">
-    <ColorPicker showText />
-    <ColorPicker showText={(color) => <span>Custom Text ({color.toHexString()})</span>} />
-  </Space>
-);
+const { Option } = Select;
+const COLOR_PRESETS = ['#1677ff', '#f1d76f', '#b4ff16', '#ea16ff'];
+
+const Demo = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<ColorPickerProps['value']>('#1677ff');
+  const popupOpenRef = useRef(true);
+
+  return (
+    <Space direction="vertical">
+      <ColorPicker showText />
+      <ColorPicker showText={(color) => <span>Custom Text ({color.toHexString()})</span>} />
+      <ColorPicker
+        open={open}
+        value={value}
+        onChange={setValue}
+        onOpenChange={(visible) => {
+          if (popupOpenRef.current) {
+            setOpen(visible);
+          }
+        }}
+        showText={(color) => (
+          <Select
+            value={color.toHexString()}
+            bordered={false}
+            onMouseEnter={() => {
+              popupOpenRef.current = false;
+            }}
+            onMouseLeave={() => {
+              popupOpenRef.current = true;
+            }}
+            onDropdownVisibleChange={() => {
+              if (open) {
+                setOpen(false);
+              }
+            }}
+            onChange={setValue}
+          >
+            {COLOR_PRESETS.map((preset) => (
+              <Option value={preset.toUpperCase()} key={preset}>
+                <div style={{ backgroundColor: preset, height: 20, width: 100 }} />
+              </Option>
+            ))}
+          </Select>
+        )}
+      />
+    </Space>
+  );
+};
 
 export default Demo;
