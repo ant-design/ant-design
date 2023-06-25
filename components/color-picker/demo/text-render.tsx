d9457d98e3dc534d45d2f68b6bf511495a95ef35
Redjue <pingfj77@gmail.com>
2023-06-25 19:18:53 +0800

    refactor: optimize code

--- components/color-picker/demo/text-render.tsx
+++ components/color-picker/demo/text-render.tsx
@@ -1,14 +1,9 @@
-import { ColorPicker, Select, Space } from 'antd';
-import type { ColorPickerProps } from 'antd/es/color-picker';
-import React, { useRef, useState } from 'react';
-
-const { Option } = Select;
-const COLOR_PRESETS = ['#1677ff', '#f1d76f', '#b4ff16', '#ea16ff'];
+import { DownOutlined } from '@ant-design/icons';
+import { ColorPicker, Space } from 'antd';
+import React, { useState } from 'react';
 
 const Demo = () => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState<ColorPickerProps['value']>('#1677ff');
-  const popupOpenRef = useRef(true);
 
   return (
     <Space direction="vertical">
@@ -16,36 +11,14 @@ const Demo = () => {
       <ColorPicker showText={(color) => <span>Custom Text ({color.toHexString()})</span>} />
       <ColorPicker
         open={open}
-        value={value}
-        onChange={setValue}
-        onOpenChange={(visible) => {
-          if (popupOpenRef.current) {
-            setOpen(visible);
-          }
-        }}
-        showText={(color) => (
-          <Select
-            value={color.toHexString()}
-            bordered={false}
-            onMouseEnter={() => {
-              popupOpenRef.current = false;
+        onOpenChange={setOpen}
+        showText={() => (
+          <DownOutlined
+            rotate={open ? 180 : 0}
+            style={{
+              color: 'rgba(0, 0, 0, 0.25)',
             }}
-            onMouseLeave={() => {
-              popupOpenRef.current = true;
-            }}
-            onDropdownVisibleChange={() => {
-              if (open) {
-                setOpen(false);
-              }
-            }}
-            onChange={setValue}
-          >
-            {COLOR_PRESETS.map((preset) => (
-              <Option value={preset.toUpperCase()} key={preset}>
-                <div style={{ backgroundColor: preset, height: 20, width: 100 }} />
-              </Option>
-            ))}
-          </Select>
+          />
         )}
       />
     </Space>
