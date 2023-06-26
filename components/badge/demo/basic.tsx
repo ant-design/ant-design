46687b95c92c5b397e0c29928e6e572196adfa1d
lijianan <574980606@qq.com>
2023-06-27 02:04:16 +0800

    Update basic.tsx

--- components/badge/demo/basic.tsx
+++ components/badge/demo/basic.tsx
@@ -1,18 +1,19 @@
-import { Badge, ConfigProvider } from 'antd';
 import React from 'react';
+import { ClockCircleOutlined } from '@ant-design/icons';
+import { Avatar, Badge, Space } from 'antd';
 
 const App: React.FC = () => (
-  <ConfigProvider
-    badge={{
-      className: 'aaaaaaa',
-      style: { color: 'blue' },
-      classNames: { count: 'lllllll' },
-    }}
-  >
-    <Badge className="kkkkkkk" style={{ backgroundColor: 'yellow' }} count={10}>
-      test
+  <Space size="middle">
+    <Badge count={5}>
+      <Avatar shape="square" size="large" />
     </Badge>
-  </ConfigProvider>
+    <Badge count={0} showZero>
+      <Avatar shape="square" size="large" />
+    </Badge>
+    <Badge count={<ClockCircleOutlined style={{ color: '#f5222d' }} />}>
+      <Avatar shape="square" size="large" />
+    </Badge>
+  </Space>
 );
 
 export default App;
