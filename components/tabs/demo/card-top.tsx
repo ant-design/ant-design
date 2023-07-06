d5c3807670aca0e0ce3fb930948fc6793a2e4af9
thinkasany <480968828@qq.com>
2023-07-06 19:18:46 +0800

    fix: remove useless code

--- components/tabs/demo/card-top.tsx
+++ components/tabs/demo/card-top.tsx
@@ -1,38 +1,5 @@
-import { css } from '@emotion/css';
-import { Tabs, theme } from 'antd';
+import { Tabs } from 'antd';
 import React from 'react';
-
-const useStyle = () => {
-  const { token } = theme.useToken();
-  const antdTabsCls = '.ant-tabs';
-
-  return css`
-    ${antdTabsCls}${antdTabsCls}-card {
-      ${antdTabsCls}-content {
-        padding: ${token.padding}px;
-        background: ${token.colorBgContainer};
-      }
-
-      ${antdTabsCls}-nav {
-        margin: 0;
-
-        ${antdTabsCls}-nav-wrap > ${antdTabsCls}-nav-list > ${antdTabsCls}-tab {
-          background: transparent;
-          border-color: transparent;
-
-          &-active {
-            background: ${token.colorBgContainer};
-            border-color: ${token.colorBorderBg};
-          }
-        }
-
-        &::before {
-          display: none;
-        }
-      }
-    }
-  `;
-};
 
 const items = new Array(3).fill(null).map((_, i) => {
   const id = String(i + 1);
@@ -49,14 +16,10 @@ const items = new Array(3).fill(null).map((_, i) => {
   };
 });
 
-const App: React.FC = () => {
-  const style = useStyle();
-
-  return (
-    <div className={style}>
-      <Tabs type="card" items={items} />
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div>
+    <Tabs type="card" items={items} />
+  </div>
+);
 
 export default App;
