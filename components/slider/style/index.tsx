6e55e8dac8df83d1f4e3552a75cf33592d299585
jomorxhu <jomorxhu@tencent.com>
2023-07-07 18:10:37 +0800

    fix: formatting code

--- components/slider/style/index.tsx
+++ components/slider/style/index.tsx
@@ -57,7 +57,7 @@ interface SliderToken extends FullToken<'Slider'> {
 
 // =============================== Base ===============================
 const genBaseStyle: GenerateStyle<SliderToken> = (token) => {
-  const { componentCls, controlSize, dotSize, marginFull, marginPart, colorFillContentHover ,antCls } =
+  const { componentCls, antCls, controlSize, dotSize, marginFull, marginPart, colorFillContentHover } =
     token;
 
   return {
@@ -256,10 +256,9 @@ const genBaseStyle: GenerateStyle<SliderToken> = (token) => {
           cursor: `not-allowed !important`,
         },
       },
-      [`&-tooltip`]: {
-        [`${antCls}-tooltip-inner`]:{
-          minWidth: "unset",
-        },
+
+      [`&-tooltip ${antCls}-tooltip-inner`]:{
+        minWidth: "unset",
       },
     },
   };
