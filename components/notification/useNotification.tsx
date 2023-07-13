eca6f1ca19f7af7e356295e5252487452004aea0
Rajil Bajracharya <imrb1213@gmail.com>
2023-07-13 15:04:50 +0545

    style: remove extra new lines in import block

--- components/notification/useNotification.tsx
+++ components/notification/useNotification.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-
 import classNames from 'classnames';
 import { useNotification as useRcNotification } from 'rc-notification';
 import type { NotificationAPI } from 'rc-notification/lib';
-
 import warning from '../_util/warning';
 import { ConfigContext } from '../config-provider';
 import type { ComponentStyleConfig } from '../config-provider/context';
