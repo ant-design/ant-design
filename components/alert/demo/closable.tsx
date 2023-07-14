860e58ee6f4f4b45ebdc880699ea6d11f31fe845
Albert E. Hidalgo Taveras <35310226+itsalb3rt@users.noreply.github.com>
2023-07-14 01:09:54 -0400

    demo: use closable prop (#43542)
    * Update closable.tsx
    
    Added `closable` prop
    
    Signed-off-by: Albert E. Hidalgo Taveras <35310226+itsalb3rt@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: afc163 <afc163@gmail.com>
    
    ---------
    
    Signed-off-by: Albert E. Hidalgo Taveras <35310226+itsalb3rt@users.noreply.github.com>
    Signed-off-by: afc163 <afc163@gmail.com>
    Co-authored-by: afc163 <afc163@gmail.com>

--- components/alert/demo/closable.tsx
+++ components/alert/demo/closable.tsx
@@ -10,14 +10,14 @@ const App: React.FC = () => (
     <Alert
       message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
       type="warning"
-      closeIcon
+      closable
       onClose={onClose}
     />
     <Alert
       message="Error Text"
       description="Error Description Error Description Error Description Error Description Error Description Error Description"
       type="error"
-      closeIcon
+      closable
       onClose={onClose}
     />
   </Space>
