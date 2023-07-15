30355fa8ce480ba889623606e4af4e0ccc058b10
lijianan <574980606@qq.com>
2023-07-15 11:59:57 +0800

    Update components/list/index.tsx
    Signed-off-by: lijianan <574980606@qq.com>

--- components/list/index.tsx
+++ components/list/index.tsx
@@ -270,7 +270,7 @@ function List<T>({
     childrenContent = (
       <div className={`${prefixCls}-empty-text`}>
         {(locale && locale.emptyText) || renderEmpty?.('List') || (
-          <DefaultRenderEmpty componentName='List' />
+          <DefaultRenderEmpty componentName="List" />
         )}
       </div>
     );
