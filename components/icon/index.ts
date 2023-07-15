94541eda67eec294dd6b1cf170f31b42a1017946
lijianan <574980606@qq.com>
2023-07-15 16:03:53 +0800

    fix: add NODE_ENV for Icon warning (#43574)

--- components/icon/index.ts
+++ components/icon/index.ts
@@ -1,7 +1,9 @@
 import warning from '../_util/warning';
 
 const Icon: React.FC = () => {
-  warning(false, 'Icon', 'Empty Icon');
+  if (process.env.NODE_ENV !== 'production') {
+    warning(false, 'Icon', 'Empty Icon');
+  }
   return null;
 };
 
