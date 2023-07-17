9ddad8662a68e40d28a8f3ca5ad45ea7ec5d2ed3
kiner-tang(文辉) <1127031143@qq.com>
2023-07-17 12:18:41 +0800

    feat: update demo about cell-render in date-picker (#43597)

--- components/date-picker/demo/cell-render.tsx
+++ components/date-picker/demo/cell-render.tsx
@@ -6,7 +6,8 @@ const { RangePicker } = DatePicker;
 const App: React.FC = () => (
   <Space direction="vertical" size={12}>
     <DatePicker
-      cellRender={(current) => {
+      cellRender={(current, info) => {
+        if (info.type !== 'date') return info.originNode;
         const style: React.CSSProperties = {};
         if (current.date() === 1) {
           style.border = '1px solid #1677ff';
@@ -20,7 +21,8 @@ const App: React.FC = () => (
       }}
     />
     <RangePicker
-      cellRender={(current) => {
+      cellRender={(current, info) => {
+        if (info.type !== 'date') return info.originNode;
         const style: React.CSSProperties = {};
         if (current.date() === 1) {
           style.border = '1px solid #1677ff';
