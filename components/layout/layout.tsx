5e42e5a94f798dbecdd9a9368b53ce31af8e26ae
Khalifa Lame <khalibloo@gmail.com>
2023-07-23 23:40:55 +0400

    fix: type mismatch

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -45,7 +45,7 @@ function generator({ suffixCls, tagName, displayName }: GeneratorProps) {
   };
 }
 
-const Basic = React.forwardRef<HTMLElement, BasicPropsWithTagName>((props, ref) => {
+const Basic = React.forwardRef<HTMLDivElement, BasicPropsWithTagName>((props, ref) => {
   const {
     prefixCls: customizePrefixCls,
     suffixCls,
@@ -70,7 +70,7 @@ const Basic = React.forwardRef<HTMLElement, BasicPropsWithTagName>((props, ref) 
   );
 });
 
-const BasicLayout = React.forwardRef<HTMLElement, BasicPropsWithTagName>((props, ref) => {
+const BasicLayout = React.forwardRef<HTMLDivElement, BasicPropsWithTagName>((props, ref) => {
   const { direction } = React.useContext(ConfigContext);
 
   const [siders, setSiders] = React.useState<string[]>([]);
