87a1004ff49ab450f6712d1d3b12a5ffef2e5d62
二货爱吃白萝卜 <smith3816@gmail.com>
2023-07-25 21:44:30 +0800

    docs: adjust home image load logic (#43798)
    * docs: add webp format
    
    * docs: img loaded keys
    
    * docs: auto color
    
    * docs: fix lint
    
    * chore: fix theme switch
    
    * fix: logo missing

--- .dumi/pages/index/components/Theme/index.tsx
+++ .dumi/pages/index/components/Theme/index.tsx
@@ -1,3 +1,4 @@
+import { TinyColor } from '@ctrl/tinycolor';
 import {
   BellOutlined,
   FolderOutlined,
@@ -87,12 +88,12 @@ const useStyle = createStyles(({ token, cx }) => {
   const { carousel } = getCarouselStyle();
 
   const demo = css`
-      overflow: hidden;
-      background: rgba(240, 242, 245, 0.25);
-      backdrop-filter: blur(50px);
-      box-shadow: 0 2px 10px 2px rgba(0, 0, 0, 0.1);
-      transition: all ${token.motionDurationSlow};
-    `;
+    overflow: hidden;
+    background: rgba(240, 242, 245, 0.25);
+    backdrop-filter: blur(50px);
+    box-shadow: 0 2px 10px 2px rgba(0, 0, 0, 0.1);
+    transition: all ${token.motionDurationSlow};
+  `;
 
   return {
     demo,
@@ -177,12 +178,6 @@ const useStyle = createStyles(({ token, cx }) => {
       }
     `,
 
-    logoImgPureColor: css`
-      img {
-        transform: translate3d(-30px, 0, 0);
-      }
-    `,
-
     transBg: css`
       background: transparent !important;
     `,
@@ -245,6 +240,10 @@ function getTitleColor(colorPrimary: string | Color, isLight?: boolean) {
     case '#F2BD27':
       return undefined;
 
+    case '#5A54F9':
+    case '#E0282E':
+      return '#FFF';
+
     default:
       return color.toHsb().b < 0.7 ? '#FFF' : undefined;
   }
@@ -278,6 +277,27 @@ const ThemesInfo: Record<THEME, Partial<ThemeData>> = {
     borderRadius: 16,
   },
 };
+
+function rgbToColorMatrix(color: string) {
+  const rgb = new TinyColor(color).toRgb();
+  const { r, g, b } = rgb;
+
+  const normalize = (value) => value / 255;
+  const invertValue = normalize(r) * 100;
+  const sepiaValue = 100;
+  const saturateValue = Math.max(normalize(r), normalize(g), normalize(b)) * 10000;
+  const hueRotateValue =
+    ((Math.atan2(
+      Math.sqrt(3) * (normalize(g) - normalize(b)),
+      2 * normalize(r) - normalize(g) - normalize(b),
+    ) *
+      180) /
+      Math.PI +
+      360) %
+    360;
+
+  return `invert(${invertValue}%) sepia(${sepiaValue}%) saturate(${saturateValue}%) hue-rotate(${hueRotateValue}deg)`;
+}
 
 export default function Theme() {
   const { styles } = useStyle();
@@ -399,19 +419,15 @@ export default function Theme() {
           >
             {/* Logo */}
             <div className={styles.logo}>
-              <div
-                className={classNames(
-                  styles.logoImg,
-                  closestColor !== DEFAULT_COLOR && styles.logoImgPureColor,
-                )}
-              >
+              <div className={styles.logoImg}>
                 <img
                   src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
                   style={{
                     filter:
                       closestColor === DEFAULT_COLOR
                         ? undefined
-                        : `drop-shadow(30px 0 0 ${logoColor})`,
+                        : // : `drop-shadow(30px 0 0 ${logoColor})`,
+                          rgbToColorMatrix(logoColor),
                   }}
                   alt=""
                 />
