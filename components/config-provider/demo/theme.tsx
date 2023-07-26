5121402e1c6b74ee87251e83d0de7f9f06f55865
MadCcc <1075746765@qq.com>
2023-07-26 12:01:30 +0800

    feat: component token support algorith,

--- components/config-provider/demo/theme.tsx
+++ components/config-provider/demo/theme.tsx
@@ -1,15 +1,33 @@
-import { Button, ColorPicker, ConfigProvider, Form, InputNumber, theme } from 'antd';
+import {
+  Button,
+  ColorPicker,
+  ConfigProvider,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  Space,
+  Switch,
+  theme,
+} from 'antd';
 import type { Color } from 'antd/es/color-picker';
 import React from 'react';
 
 type ThemeData = {
   borderRadius: number;
   colorPrimary: string;
+  Button?: {
+    colorPrimary: string;
+    algorithm?: boolean;
+  };
 };
 
 const defaultData: ThemeData = {
   borderRadius: 6,
   colorPrimary: '#1677ff',
+  Button: {
+    colorPrimary: '#00B96B',
+  },
 };
 
 export default () => {
@@ -17,28 +35,36 @@ export default () => {
 
   const [data, setData] = React.useState<ThemeData>(defaultData);
 
+  console.log(data);
+
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: data.colorPrimary,
-          borderRadius: data.borderRadius,
-        },
-        components: {
-          Button: {
-            colorPrimary: 'red',
-            borderRadius: 0,
-            algorithm: true,
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: data.colorPrimary,
+            borderRadius: data.borderRadius,
           },
-          InputNumber: {
-            colorPrimary: 'green',
-            colorBgBase: '#000',
-            colorTextBase: '#fff',
-            algorithm: theme.darkAlgorithm,
+          components: {
+            Button: {
+              colorPrimary: data.Button?.colorPrimary,
+              algorithm: data.Button?.algorithm,
+            },
+            InputNumber: {
+              colorPrimary: 'green',
+              colorBgBase: '#000',
+              colorTextBase: '#fff',
+              algorithm: theme.darkAlgorithm,
+            },
           },
-        },
-      }}
-    >
+        }}
+      >
+        <Space>
+          <Input />
+          <Button type="primary">Button</Button>
+        </Space>
+      </ConfigProvider>
+      <Divider />
       <Form
         form={form}
         onValuesChange={(changedValues, allValues) => {
@@ -58,15 +84,21 @@ export default () => {
         <Form.Item valuePropName="color" name="colorPrimary" label="Primary Color">
           <ColorPicker />
         </Form.Item>
-        <div style={{ background: 'rgb(20, 20, 20)', padding: 24 }}>
-          <Form.Item name="borderRadius" label="Border Radius">
-            <InputNumber />
+        <Form.Item name="borderRadius" label="Border Radius">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="Button">
+          <Form.Item name={['Button', 'algorithm']} valuePropName="checked" label="algorithm">
+            <Switch />
           </Form.Item>
-        </div>
+          <Form.Item name={['Button', 'colorPrimary']} label="Primary Color">
+            <ColorPicker />
+          </Form.Item>
+        </Form.Item>
         <Form.Item name="submit" wrapperCol={{ offset: 4, span: 20 }}>
           <Button type="primary">Submit</Button>
         </Form.Item>
       </Form>
-    </ConfigProvider>
+    </div>
   );
 };
