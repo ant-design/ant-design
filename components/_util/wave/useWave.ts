e8de38efa3ef3d55da137c406f9746098ca07ce0
二货机器人 <smith3816@gmail.com>
2023-08-03 22:43:16 +0800

    chore: fix checkbox

--- components/_util/wave/useWave.ts
+++ components/_util/wave/useWave.ts
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import useEvent from 'rc-util/lib/hooks/useEvent';
+import raf from 'rc-util/lib/raf';
 import showWaveEffect from './WaveEffect';
 import { ConfigContext } from '../../config-provider';
 import useToken from '../../theme/useToken';
 import type { GlobalToken } from '../../theme';
+
+export const TARGET_CLS = 'ant-wave-target';
 
 export type ShowWaveEffect = (
   element: HTMLElement,
@@ -15,6 +18,8 @@ export type ShowWaveEffect = (
   },
 ) => void;
 
+type ShowWave = (event: MouseEvent) => void;
+
 export default function useWave(
   nodeRef: React.RefObject<HTMLElement>,
   className: string,
@@ -23,18 +28,31 @@ export default function useWave(
   const { wave } = React.useContext(ConfigContext);
   const [, token] = useToken();
 
-  const showWave = useEvent((event: MouseEvent) => {
+  const showWave = useEvent<ShowWave>((event) => {
     const node = nodeRef.current!;
 
     if (wave?.disabled || !node) {
       return;
     }
 
+    const targetNode = node.querySelector<HTMLElement>(`.${TARGET_CLS}`) || node;
+
     const { showEffect } = wave || {};
 
     // Customize wave effect
-    (showEffect || showWaveEffect)(node, { className, token, component, event });
+    (showEffect || showWaveEffect)(targetNode, { className, token, component, event });
   });
 
-  return showWave;
+  const rafId = React.useRef<number>();
+
+  // Merge trigger event into one for each frame
+  const showDebounceWave: ShowWave = (event) => {
+    raf.cancel(rafId.current!);
+
+    rafId.current = raf(() => {
+      showWave(event);
+    });
+  };
+
+  return showDebounceWave;
 }
