8d37d4faef4b9c37f02db885c6c0377b1868cdc1
栗嘉男 <574980606@qq.com>
2023-08-04 16:55:09 +0800

    fiux

--- components/config-provider/demo/wave.tsx
+++ components/config-provider/demo/wave.tsx
@@ -112,9 +112,7 @@ const showShakeEffect: WaveConfig['showEffect'] = (node, { component }) => {
 // Component
 const Wrapper = ({ name, ...wave }: WaveConfig & { name: string }) => (
   <ConfigProvider wave={wave}>
-    <Button style={{ width: 90 }} type="primary">
-      {name}
-    </Button>
+    <Button type="primary">{name}</Button>
   </ConfigProvider>
 );
 
