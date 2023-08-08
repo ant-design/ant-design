c22e31ed22a39cededbc6486a76135e9af59d872
MadCcc <1075746765@qq.com>
2023-08-08 17:16:47 +0800

    chore: code clean

--- components/float-button/demo/group-menu.tsx
+++ components/float-button/demo/group-menu.tsx
@@ -9,7 +9,6 @@ const App: React.FC = () => (
       type="primary"
       style={{ right: 24 }}
       icon={<CustomerServiceOutlined />}
-      badge={{ dot: true }}
     >
       <FloatButton />
       <FloatButton icon={<CommentOutlined />} />
