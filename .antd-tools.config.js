58cd7de11a79749c26e288062a3fa1e5b3668f56
二货机器人 <smith3816@gmail.com>
2023-08-09 21:55:12 +0800

    chore: build all

--- .antd-tools.config.js
+++ .antd-tools.config.js
@@ -247,10 +247,10 @@ function transformTSFile(file) {
 
   if (matched) {
     const content = [`import r from '../../style/sheet';`, ...parsedLines].join('\n');
-    if (file.path.includes('/alert/')) {
-      console.log(content);
-      process.exit(1);
-    }
+    // if (file.path.includes('/alert/')) {
+    //   console.log(content);
+    //   process.exit(1);
+    // }
     cloneFile.contents = Buffer.from(content);
     return cloneFile;
   }
