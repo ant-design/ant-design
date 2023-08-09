c217e093fcda04e7d021e097204fafc89eaaaafc
二货机器人 <smith3816@gmail.com>
2023-08-09 20:04:23 +0800

    chore: tmp record

--- .antd-tools.config.js
+++ .antd-tools.config.js
@@ -1,6 +1,169 @@
 const fs = require('fs');
 const path = require('path');
 
+// When style name length <= 5, we just ignore it since it's smaller.
+const KEY_LIST = [
+  'WebkitBackfaceVisibility',
+  'WebkitBoxOrient',
+  'WebkitLineClamp',
+  'WebkitTapHighlightColor',
+  'WebkitTransformStyle',
+  'alignItems',
+  'alignSelf',
+  'animationDelay',
+  'animationDirection',
+  'animationDuration',
+  'animationFillMode',
+  'animationIterationCount',
+  'animationName',
+  'animationPlayState',
+  'animationTimingFunction',
+  'appearance',
+  'background',
+  'backgroundClip',
+  'backgroundColor',
+  'backgroundImage',
+  'backgroundPosition',
+  'backgroundRepeat',
+  'backgroundSize',
+  'border',
+  'borderBlockEnd',
+  'borderBlockEndColor',
+  'borderBlockEndWidth',
+  'borderBlockStart',
+  'borderBlockStartColor',
+  'borderBlockStartWidth',
+  'borderBottom',
+  'borderBottomColor',
+  'borderBottomWidth',
+  'borderCollapse',
+  'borderColor',
+  'borderEndEndRadius',
+  'borderEndStartRadius',
+  'borderInlineEnd',
+  'borderInlineEndColor',
+  'borderInlineEndWidth',
+  'borderInlineStart',
+  'borderInlineStartColor',
+  'borderInlineStartWidth',
+  'borderLeft',
+  'borderLeftColor',
+  'borderRadius',
+  'borderRight',
+  'borderRightColor',
+  'borderSpacing',
+  'borderStartEndRadius',
+  'borderStartStartRadius',
+  'borderStyle',
+  'borderTop',
+  'borderTopColor',
+  'borderTopWidth',
+  'borderWidth',
+  'bottom',
+  'boxShadow',
+  'boxSizing',
+  'columnGap',
+  'container',
+  'content',
+  'cursor',
+  'direction',
+  'display',
+  'filter',
+  'flexBasis',
+  'flexDirection',
+  'flexFlow',
+  'flexGrow',
+  'flexShrink',
+  'flexWrap',
+  'fontFamily',
+  'fontSize',
+  'fontStretch',
+  'fontStyle',
+  'fontVariant',
+  'fontWeight',
+  'height',
+  'insetBlock',
+  'insetBlockEnd',
+  'insetBlockStart',
+  'insetInline',
+  'insetInlineEnd',
+  'insetInlineStart',
+  'justifyContent',
+  'justifyItems',
+  'letterSpacing',
+  'lineHeight',
+  'listStyle',
+  'listStylePosition',
+  'listStyleType',
+  'margin',
+  'marginBlock',
+  'marginBlockEnd',
+  'marginBlockStart',
+  'marginBottom',
+  'marginInline',
+  'marginInlineEnd',
+  'marginInlineStart',
+  'marginLeft',
+  'marginRight',
+  'marginTop',
+  'maxHeight',
+  'maxWidth',
+  'minHeight',
+  'minWidth',
+  'objectFit',
+  'opacity',
+  'outline',
+  'outlineColor',
+  'overflow',
+  'overflowWrap',
+  'overflowX',
+  'overflowY',
+  'padding',
+  'paddingBlock',
+  'paddingBlockStart',
+  'paddingBottom',
+  'paddingInline',
+  'paddingInlineEnd',
+  'paddingInlineStart',
+  'paddingLeft',
+  'paddingRight',
+  'paddingTop',
+  'pointerEvents',
+  'position',
+  'resize',
+  'rowGap',
+  'stroke',
+  'tabSize',
+  'tableLayout',
+  'textAlign',
+  'textDecoration',
+  'textDecorationSkipInk',
+  'textIndent',
+  'textOverflow',
+  'textRendering',
+  'textShadow',
+  'textTransform',
+  'touchAction',
+  'transform',
+  'transformOrigin',
+  'transition',
+  'transitionDuration',
+  'transitionTimingFunction',
+  'userSelect',
+  'verticalAlign',
+  'visibility',
+  'whiteSpace',
+  'width',
+  'willChange',
+  'wordBreak',
+  'wordWrap',
+  'writingMode',
+  'zIndex',
+];
+
+// =================================================================
+// ==                            Style                            ==
+// =================================================================
 const restCssPath = path.join(process.cwd(), 'components', 'style', 'reset.css');
 const tokenStatisticPath = path.join(process.cwd(), 'components', 'version', 'token.json');
 const tokenMetaPath = path.join(process.cwd(), 'components', 'version', 'token-meta.json');
@@ -25,6 +188,20 @@ function finalizeDist() {
   }
 }
 
+// =================================================================
+// ==                         Mini Bundle                         ==
+// =================================================================
+const sheet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const styleMap = {};
+KEY_LIST.forEach((key, index) => {
+  const id =
+    (index >= sheet.length ? sheet[Math.floor(index / sheet.length) - 1] : '') +
+    sheet[index % sheet.length];
+
+  styleMap[key] = id;
+});
+
 // Convert `style/xxx.ts` file to hashed map to min bundle size
 function transformTSFile(file) {
   if (!/components\/[^/]+\/style/.test(file.path)) {
@@ -34,18 +211,29 @@ function transformTSFile(file) {
   const cloneFile = file.clone();
 
   // Replacement
-  const content = file.contents.toString();
-  cloneFile.contents = Buffer.from(content);
+  let matched = false;
+  const lines = file.contents.toString().split('\n');
+  const parsedLines = lines.map((line) => {
+    let newLine = line;
+    KEY_LIST.forEach((key) => {
+      const keyMatch = ` ${key}: `;
+      if (newLine.includes(keyMatch)) {
+        matched = true;
+        newLine = newLine.replace(keyMatch, ` [r.${styleMap[key]}]: `);
+      }
+    });
 
-  content.split(/\n/).forEach((line) => {
-    const cell = line.match(/(\w+):/)?.[1];
-    if (cell) {
-      const styleTxt = fs.readFileSync('./~tmp.txt', 'utf-8');
-      fs.writeFileSync('./~tmp.txt', `${styleTxt}\n${cell}`, 'utf-8');
-    }
+    return newLine;
   });
 
-  return cloneFile;
+  if (matched) {
+    const content = [`import r from '../../style/sheet';`, ...parsedLines].join('\n');
+    if (file.path.includes('/alert/')) {
+      console.log(content);
+    }
+    cloneFile.contents = Buffer.from(content);
+    return cloneFile;
+  }
 }
 
 module.exports = {
