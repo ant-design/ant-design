c33c4215d7cc0f5e96491c3793051da88f6a8c52
叶枫 <645381995@qq.com>
2023-08-18 14:18:45 +0800

    style: sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -205,6 +205,7 @@ module.exports = {
     '@typescript-eslint/no-shadow': [2, { ignoreTypeValueShadow: true }],
     // https://github.com/typescript-eslint/typescript-eslint/issues/2528#issuecomment-689369395
     'no-undef': 0,
+    'import/order': 0,
   },
   globals: {
     gtag: true,
