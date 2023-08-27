b1a57387f71a6409ad731b15877ea3f04ed2ce4f
Stefano Magrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2023-08-27 05:06:04 +0200

    type: fix notification config typings (#44163)
    * add notification.config typing
    
    * add .node-version file
    
    * remove .node-version
    
    * ignore .node-version file from git
    
    ---------
    
    Co-authored-by: lijianan <574980606@qq.com>

--- components/notification/__tests__/config.test.tsx
+++ components/notification/__tests__/config.test.tsx
@@ -17,8 +17,8 @@ describe('notification.config', () => {
     await triggerMotionEnd();
 
     notification.config({
-      prefixCls: null,
-      getContainer: null,
+      prefixCls: undefined,
+      getContainer: undefined,
     });
 
     jest.useRealTimers();
