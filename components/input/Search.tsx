ad62e71e8cc9050d23aace1a97275814e742bae1
kiner-tang(文辉) <1127031143@qq.com>
2023-08-28 14:13:10 +0800

    feat: optimize code

--- components/input/Search.tsx
+++ components/input/Search.tsx
@@ -18,7 +18,9 @@ export interface SearchProps extends InputProps {
       | React.ChangeEvent<HTMLInputElement>
       | React.MouseEvent<HTMLElement>
       | React.KeyboardEvent<HTMLInputElement>,
-    source?: 'clear' | 'input',
+    info?: {
+      source?: 'clear' | 'input';
+    },
   ) => void;
   enterButton?: React.ReactNode;
   loading?: boolean;
@@ -56,7 +58,9 @@ const Search = React.forwardRef<InputRef, SearchProps>((props, ref) => {
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e && e.target && e.type === 'click' && customOnSearch) {
-      customOnSearch((e as React.ChangeEvent<HTMLInputElement>).target.value, e, 'clear');
+      customOnSearch((e as React.ChangeEvent<HTMLInputElement>).target.value, e, {
+        source: 'clear',
+      });
     }
     if (customOnChange) {
       customOnChange(e);
@@ -71,7 +75,9 @@ const Search = React.forwardRef<InputRef, SearchProps>((props, ref) => {
 
   const onSearch = (e: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLInputElement>) => {
     if (customOnSearch) {
-      customOnSearch(inputRef.current?.input?.value!, e, 'input');
+      customOnSearch(inputRef.current?.input?.value!, e, {
+        source: 'input',
+      });
     }
   };
 
