cd7f0609b18172d61f0bb8ab54a8110c1164c455
二货机器人 <smith3816@gmail.com>
2023-08-28 15:21:34 +0800

    chore: fix demo

--- components/tooltip/demo/basic.tsx
+++ components/tooltip/demo/basic.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Tooltip } from 'antd';
 
 const App: React.FC = () => (
-  <Tooltip title={'prompt text '.repeat(300)} placement="bottom">
+  <Tooltip title="prompt text">
     <span>Tooltip will show on mouse enter.</span>
   </Tooltip>
 );
