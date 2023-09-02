84cc472b38a77b3c97b2954804b74cbcdabd221f
栗嘉男 <574980606@qq.com>
2023-09-02 20:09:19 +0800

    test: fix test case

--- components/config-provider/__tests__/style.test.tsx
+++ components/config-provider/__tests__/style.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ConfigProvider from '..';
 import { fireEvent, render } from '../../../tests/utils';
 import Alert from '../../alert';
@@ -104,9 +105,7 @@ describe('ConfigProvider support style and className props', () => {
         </Space>
       </ConfigProvider>,
     );
-    expect(container.querySelector('.ant-space-item')).toHaveStyle(
-      'margin-right: 8px; color: red;',
-    );
+    expect(container.querySelector('.ant-space-item')).toHaveStyle('color: red;');
   });
 
   it('Should Space style works', () => {
