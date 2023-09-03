fd07e07358a6e0e8d26684a32772190e9a9f8025
栗嘉男 <574980606@qq.com>
2023-09-03 10:37:38 +0800

    chore: add comment

--- components/space/utils.ts
+++ components/space/utils.ts
@@ -3,6 +3,7 @@ import type { SizeType } from '../config-provider/SizeContext';
 
 export const isPresetSize = (size: SpaceSize): size is SizeType => {
   if (!size) {
+    // 这里故意排除 size = 0 的情况，因为 gap 属性在 CSS 中的默认值就是 0，所以如果用户传0进来的话，我们可以直接忽略
     return false;
   }
   return typeof size === 'string' && ['small', 'middle', 'large'].includes(size);
