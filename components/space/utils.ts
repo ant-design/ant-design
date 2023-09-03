d0c049723395bfc8ff3dc07a2b5e319cbc0e645a
栗嘉男 <574980606@qq.com>
2023-09-03 14:34:46 +0800

    fix: fix

--- components/space/utils.ts
+++ components/space/utils.ts
@@ -1,17 +1,14 @@
 import type { SpaceSize } from '.';
 import type { SizeType } from '../config-provider/SizeContext';
 
-export const isPresetSize = (size: SpaceSize): size is SizeType => {
-  if (!size) {
-    return false;
-  }
+export function isPresetSize(size: SpaceSize): size is SizeType {
   return typeof size === 'string' && ['small', 'middle', 'large'].includes(size);
-};
+}
 
-export const isValidGapNumber = (size: SpaceSize): size is number => {
+export function isValidGapNumber(size: SpaceSize): size is number {
   if (!size) {
     // The case of size = 0 is deliberately excluded here, because the default value of the gap attribute in CSS is 0, so if the user passes 0 in, we can directly ignore it.
     return false;
   }
   return typeof size === 'number' && !Number.isNaN(size);
-};
+}
