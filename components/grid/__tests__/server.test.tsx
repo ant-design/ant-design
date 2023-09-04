3e616812aa0f2f1eb3d5bcf9c2eefc84a75dcf4e
栗嘉男 <574980606@qq.com>
2023-09-04 23:25:46 +0800

    fix: fix

--- components/grid/__tests__/server.test.tsx
+++ components/grid/__tests__/server.test.tsx
@@ -15,8 +15,8 @@ describe('Grid.Server', () => {
 
     expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginLeft).toBe('-4px');
     expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginRight).toBe('-4px');
-    expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginTop).toBe('-8px');
-    expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginBottom).toBe('-8px');
+    expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginTop).toBe('');
+    expect((container.querySelector('.ant-row') as HTMLElement)?.style.marginBottom).toBe('');
 
     expect((container.querySelector('.ant-col') as HTMLElement)?.style.paddingLeft).toBe('4px');
     expect((container.querySelector('.ant-col') as HTMLElement)?.style.paddingRight).toBe('4px');
