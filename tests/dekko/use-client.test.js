b73013c74b2fbbbe90ac71315f2600b96d8e4aaa
Dunqing <dengqing0821@gmail.com>
2023-09-04 12:36:59 +0800

    test: update

---
+++ tests/dekko/use-client.test.js
@@ -0,0 +1,21 @@
+const $ = require('dekko');
+const chalk = require('chalk');
+const fs = require('fs');
+
+const includeUseClient = (filename) =>
+  fs.readFileSync(filename).toString().includes('"use client"');
+
+$('dist/*')
+  .isFile()
+  .assert("doesn't contain use client", (filename) => !includeUseClient(filename));
+
+$('{es,lib}/index.js')
+  .isFile()
+  .assert('contain use client', (filename) => includeUseClient(filename));
+
+$('{es,lib}/*/index.js')
+  .isFile()
+  .assert('contain use client', (filename) => includeUseClient(filename));
+
+// eslint-disable-next-line no-console
+console.log(chalk.green('✨ use client passed!'));
