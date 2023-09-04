2d8f854fe9d0950abcd8777cb5fd5321af31eba3
二货机器人 <smith3816@gmail.com>
2023-09-04 11:14:09 +0800

    docs: demo back

--- components/button/demo/basic.tsx
+++ components/button/demo/basic.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
-import { SmileOutlined } from '@ant-design/icons';
-import { Button, ConfigProvider, Space } from 'antd';
-import { createStyles } from 'antd-style';
-
-const useButtonStyle = () => {
-  const { getPrefixCls } = React.useContext(ConfigProvider.ConfigContext);
-
-  const btnPrefixCls = getPrefixCls('btn');
-
-  return createStyles(({ css }) => ({
-    btn: css`
-      background: red;
-
-      .${btnPrefixCls}-icon {
-        color: green;
-      }
-    `,
-  }))();
-};
-
-function GeekProvider(props: { children?: React.ReactNode }) {
-  const { styles } = useButtonStyle();
-
-  return <ConfigProvider button={{ className: styles.btn }}>{props.children}</ConfigProvider>;
-}
+import { Button, Space } from 'antd';
 
 const App: React.FC = () => (
-  <GeekProvider>
-    <Space wrap>
-      <Button type="primary" icon={<SmileOutlined />}>
-        Primary Button
-      </Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
-    </Space>
-  </GeekProvider>
+  <Space wrap>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </Space>
 );
 
 export default App;
