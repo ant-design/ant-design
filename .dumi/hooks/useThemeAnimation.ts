d874c389f20d72f39eacc1d7463b96ebe7601430
Redjue <pingfj77@gmail.com>
2023-09-08 14:04:58 +0800

    fix: scroll bar theme color

--- .dumi/hooks/useThemeAnimation.ts
+++ .dumi/hooks/useThemeAnimation.ts
@@ -47,6 +47,7 @@ const useThemeAnimation = () => {
     `,
       'disable-transition',
     );
+
     document.documentElement
       .animate(
         {
@@ -69,7 +70,18 @@ const useThemeAnimation = () => {
     const x = event.clientX;
     const y = event.clientY;
     const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
-
+    updateCSS(
+      `
+    [data-prefers-color='dark'] {
+      color-scheme: light !important;
+    }
+    
+    [data-prefers-color='light'] {
+      color-scheme: dark !important;
+    }
+    `,
+      'color-scheme',
+    );
     document
       // @ts-ignore
       .startViewTransition(async () => {
@@ -89,6 +101,7 @@ const useThemeAnimation = () => {
           `circle(0px at ${x}px ${y}px)`,
           `circle(${endRadius}px at ${x}px ${y}px)`,
         ];
+        removeCSS('color-scheme');
         startAnimationTheme(clipPath, isDark);
       });
   };
