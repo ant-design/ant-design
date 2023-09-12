411764e1d5cfb1e8df825698fad62f5c2d398dcd
Mamaev <lexiosma@gmail.com>
2023-09-12 04:53:54 +0300

    fix: Checkbox reference type test

--- components/checkbox/__tests__/type.test.tsx
+++ components/checkbox/__tests__/type.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import Checkbox from '..';
+import Checkbox, { CheckboxRef } from '..';
 import Input from '../../input';
 
 describe('Checkbox.typescript', () => {
   it('Checkbox', () => {
-    const ref = React.createRef<HTMLInputElement>();
+    const ref = React.createRef<CheckboxRef>();
     const checkbox = <Checkbox value ref={ref} />;
 
     expect(checkbox).toBeTruthy();
