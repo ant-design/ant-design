c9e94c26f6a1c859113ecdba71b1d017dcb515f0
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2023-09-12 09:23:11 +0545

    refactor: convertLegacyProps function moved to buttonHelpers (#44777)

--- components/button/buttonHelpers.tsx
+++ components/button/buttonHelpers.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { cloneElement, isFragment } from '../_util/reactNode';
+import type { BaseButtonProps, LegacyButtonType } from './button';
 
 const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
 export const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
+
+export function convertLegacyProps(
+  type?: LegacyButtonType,
+): Pick<BaseButtonProps, 'danger' | 'type'> {
+  if (type === 'danger') {
+    return { danger: true };
+  }
+  return { type };
+}
 
 export function isString(str: any): str is string {
   return typeof str === 'string';
