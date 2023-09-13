d749eb2e577d215d168f5819aabc0951465ef0d5
Dunqing <dengqing0821@gmail.com>
2023-09-13 17:00:21 +0800

    test: refactor

--- tests/shared/demoTest.tsx
+++ tests/shared/demoTest.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import path from 'path';
+import * as React from 'react';
 import { createCache, StyleProvider } from '@ant-design/cssinjs';
 import { globSync } from 'glob';
-import * as React from 'react';
-import { renderToString } from 'react-dom/server';
 import { kebabCase } from 'lodash';
+import { renderToString } from 'react-dom/server';
+import { test } from 'vitest';
+
+import { resetWarned } from '../../components/_util/warning';
 import { render } from '../utils';
 import { TriggerMockContext } from './demoTestContext';
 import { excludeWarning, isSafeWarning } from './excludeWarning';
 import rootPropsTest from './rootPropsTest';
-import { resetWarned } from '../../components/_util/warning';
 
 import 'isomorphic-fetch';
 
@@ -27,68 +29,65 @@ export type Options = {
 
 function baseText(doInject: boolean, component: string, options: Options = {}) {
   const files = globSync(`./components/${component}/demo/*.tsx`);
-  files.forEach((file) => {
-    // to compatible windows path
-    file = file.split(path.sep).join('/');
-    const testMethod =
-      options.skip === true ||
-      (Array.isArray(options.skip) && options.skip.some((c) => file.includes(c)))
-        ? test.skip
-        : test;
+  if (options.skip === true) {
+    return;
+  }
+  test.each(files)(
+    doInject ? `renders %s extend context correctly` : `renders %s correctly`,
+    async (file) => {
+      file = file.split(path.sep).join('/');
+      if (options.skip && (options.skip as string[]).some((f) => file.includes(f))) {
+        return;
+      }
 
-    // function doTest(name: string, openTrigger = false) {
-    testMethod(
-      doInject ? `renders ${file} extend context correctly` : `renders ${file} correctly`,
-      async () => {
-        resetWarned();
+      resetWarned();
 
-        const errSpy = excludeWarning();
+      const errSpy = excludeWarning();
 
-        Date.now = vi.fn(() => new Date('2016-11-22').getTime());
-        vi.useFakeTimers().setSystemTime(new Date('2016-11-22'));
+      Date.now = vi.fn(() => new Date('2016-11-22').getTime());
+      vi.useFakeTimers().setSystemTime(new Date('2016-11-22'));
 
-        let { default: Demo } = await import(`../../${file}`);
-        // Inject Trigger status unless skipped
-        Demo = typeof Demo === 'function' ? <Demo /> : Demo;
-        if (doInject) {
-          Demo = (
-            <TriggerMockContext.Provider value={{ popupVisible: true }}>
-              {Demo}
-            </TriggerMockContext.Provider>
-          );
-        }
+      let { default: Demo } = await import(`../../${file}`);
+      // Inject Trigger status unless skipped
+      Demo = typeof Demo === 'function' ? <Demo /> : Demo;
+      if (doInject) {
+        Demo = (
+          <TriggerMockContext.Provider value={{ popupVisible: true }}>
+            {Demo}
+          </TriggerMockContext.Provider>
+        );
+      }
 
-        // Inject cssinjs cache to avoid create <style /> element
-        Demo = <StyleProvider cache={createCache()}>{Demo}</StyleProvider>;
+      // Inject cssinjs cache to avoid create <style /> element
+      Demo = <StyleProvider cache={createCache()}>{Demo}</StyleProvider>;
 
-        // Demo Test also include `dist` test which is already uglified.
-        // We need test this as SSR instead.
-        if (doInject) {
-          const { container } = render(Demo);
-          expect({ type: 'demo', html: container.innerHTML }).toMatchSnapshot();
-        } else {
-          const html = renderToString(Demo);
-          expect({ type: 'demo', html }).toMatchSnapshot();
-        }
+      // Demo Test also include `dist` test which is already uglified.
+      // We need test this as SSR instead.
+      if (doInject) {
+        const { container } = render(Demo);
+        expect({ type: 'demo', html: container.innerHTML }).toMatchSnapshot();
+      } else {
+        const html = renderToString(Demo);
+        expect({ type: 'demo', html }).toMatchSnapshot();
+      }
 
-        vi.clearAllTimers();
+      vi.clearAllTimers();
 
-        // Snapshot of warning info
-        if (doInject) {
-          const errorMessageSet = new Set(errSpy.mock.calls.map((args) => args[0]));
-          const errorMessages = Array.from(errorMessageSet)
-            .filter((msg) => !isSafeWarning(msg, true))
-            .sort();
+      // Snapshot of warning info
+      if (doInject) {
+        const errorMessageSet = new Set(errSpy.mock.calls.map((args) => args[0]));
+        const errorMessages = Array.from(errorMessageSet)
+          .filter((msg) => !isSafeWarning(msg, true))
+          .sort();
 
-          expect(errorMessages).toMatchSnapshot();
-        }
+        expect(errorMessages).toMatchSnapshot();
+      }
 
-        errSpy.mockRestore();
-      },
-      120000,
-    );
-    vi.useRealTimers();
-  });
+      errSpy.mockRestore();
+      vi.useRealTimers();
+    },
+    120000,
+  );
 }
 
 /**
