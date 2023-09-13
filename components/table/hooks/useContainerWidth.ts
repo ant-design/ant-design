316fce3c152f8ebf6973fff214127ba47ef42b28
二货机器人 <smith3816@gmail.com>
2023-09-13 17:20:33 +0800

    chore: cov

--- components/table/hooks/useContainerWidth.ts
+++ components/table/hooks/useContainerWidth.ts
@@ -3,16 +3,17 @@ import type { TableProps } from 'rc-table';
 export default function useContainerWidth(prefixCls: string) {
   const getContainerWidth: TableProps['getContainerWidth'] = (ele, width) => {
     const container = ele.querySelector(`.${prefixCls}-container`);
+    let returnWidth = width;
 
     if (container) {
       const style = getComputedStyle(container);
       const borderLeft = parseInt(style.borderLeftWidth, 10);
       const borderRight = parseInt(style.borderRightWidth, 10);
 
-      return width - borderLeft - borderRight;
+      returnWidth = width - borderLeft - borderRight;
     }
 
-    return width;
+    return returnWidth;
   };
 
   return getContainerWidth;
