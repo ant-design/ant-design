feca20980fe32c52cc3ca358b75403bff86fa625
MadCcc <1075746765@qq.com>
2023-09-14 15:11:22 +0800

    chore: config demos

--- components/select/__tests__/image.test.ts
+++ components/select/__tests__/image.test.ts
@@ -1,5 +1,8 @@
 import { imageDemoTest } from '../../../tests/shared/imageTest';
 
 describe('Select image', () => {
-  imageDemoTest('select', { skip: ['basic.tsx'] });
+  imageDemoTest('select', {
+    skip: ['basic.tsx'],
+    onlyViewPort: ['debug-flip-shift.tsx'],
+  });
 });
