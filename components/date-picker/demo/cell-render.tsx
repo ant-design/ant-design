dbb18d063a7fe960b7f3a1039e8e26eb82c4efd2
栗嘉男 <574980606@qq.com>
2023-09-14 18:22:18 +0800

    demo: update demo

--- components/date-picker/demo/cell-render.tsx
+++ components/date-picker/demo/cell-render.tsx
@@ -1,42 +1,30 @@
 import React from 'react';
 import { DatePicker, Space } from 'antd';
+import type { DatePickerProps } from 'antd';
 
-const { RangePicker } = DatePicker;
+const style: React.CSSProperties = {
+  borderRadius: '50%',
+  border: '1px solid #1677ff',
+};
+
+const cellRender: DatePickerProps['cellRender'] = (current, info) => {
+  if (info.type !== 'date') {
+    return info.originNode;
+  }
+  if (typeof current === 'number') {
+    return <div className="ant-picker-cell-inner">{current}</div>;
+  }
+  return (
+    <div className="ant-picker-cell-inner" style={current.date() === 1 ? style : {}}>
+      {current.date()}
+    </div>
+  );
+};
 
 const App: React.FC = () => (
   <Space direction="vertical" size={12}>
-    <DatePicker
-      cellRender={(current, info) => {
-        if (info.type !== 'date') return info.originNode;
-        if (typeof current === 'number') {
-          return <div className="ant-picker-cell-inner">{current}</div>;
-        }
-        return (
-          <div
-            className="ant-picker-cell-inner"
-            style={current.date() === 1 ? { border: '1px solid #1677ff', borderRadius: '50%' } : {}}
-          >
-            {current.date()}
-          </div>
-        );
-      }}
-    />
-    <RangePicker
-      cellRender={(current, info) => {
-        if (info.type !== 'date') return info.originNode;
-        if (typeof current === 'number') {
-          return <div className="ant-picker-cell-inner">{current}</div>;
-        }
-        return (
-          <div
-            className="ant-picker-cell-inner"
-            style={current.date() === 1 ? { border: '1px solid #1677ff', borderRadius: '50%' } : {}}
-          >
-            {current.date()}
-          </div>
-        );
-      }}
-    />
+    <DatePicker cellRender={cellRender} />
+    <DatePicker.RangePicker cellRender={cellRender} />
   </Space>
 );
 
