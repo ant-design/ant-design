5ac472d0426899c2959ef70f5da8fd6cd5633949
cwg <1227646458@qq.com>
2023-09-16 23:42:12 +0800

    site: add LocalStorage caching for bannerVisible (#44910)
    * feat: add LocalStorage caching for banner visible
    
    * feat: add cache expiration time

--- .dumi/theme/layouts/GlobalLayout.tsx
+++ .dumi/theme/layouts/GlobalLayout.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useMemo, startTransition } from 'react';
+import dayjs from 'dayjs';
 import {
   createCache,
   extractStyle,
@@ -32,6 +33,7 @@ type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T][];
 type SiteState = Partial<Omit<SiteContextProps, 'updateSiteContext'>>;
 
 const RESPONSIVE_MOBILE = 768;
+export const ANT_DESIGN_NOT_SHOW_BANNER = 'ANT_DESIGN_NOT_SHOW_BANNER';
 
 // const styleCache = createCache();
 // if (typeof global !== 'undefined') {
@@ -56,12 +58,12 @@ const GlobalLayout: React.FC = () => {
   const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const [, , setPrefersColor] = usePrefersColor();
-  const [{ theme = [], direction, isMobile, bannerVisible = true }, setSiteState] =
+  const [{ theme = [], direction, isMobile, bannerVisible = false }, setSiteState] =
     useLayoutState<SiteState>({
       isMobile: false,
       direction: 'ltr',
       theme: [],
-      bannerVisible: true,
+      bannerVisible: false,
     });
 
   const updateSiteConfig = useCallback(
@@ -106,8 +108,16 @@ const GlobalLayout: React.FC = () => {
   useEffect(() => {
     const _theme = searchParams.getAll('theme') as ThemeName[];
     const _direction = searchParams.get('direction') as DirectionType;
+    const storedBannerVisibleLastTime =
+      localStorage && localStorage.getItem(ANT_DESIGN_NOT_SHOW_BANNER);
+    const storedBannerVisible =
+      storedBannerVisibleLastTime && dayjs().diff(dayjs(storedBannerVisibleLastTime), 'day') >= 1;
 
-    setSiteState({ theme: _theme, direction: _direction === 'rtl' ? 'rtl' : 'ltr' });
+    setSiteState({
+      theme: _theme,
+      direction: _direction === 'rtl' ? 'rtl' : 'ltr',
+      bannerVisible: storedBannerVisibleLastTime ? storedBannerVisible : true,
+    });
     // Handle isMobile
     updateMobileMode();
 
