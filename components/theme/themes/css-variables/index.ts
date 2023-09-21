0bd84155ec9cb74c60d888e2bd56bb451b1d8529
MadCcc <1075746765@qq.com>
2023-09-21 19:31:07 +0800

    feat: css variables theme

---
+++ components/theme/themes/css-variables/index.ts
@@ -0,0 +1,6 @@
+import type { DerivativeFunc } from '@ant-design/cssinjs';
+import type { MapToken, SeedToken } from 'antd/es/theme/interface';
+
+const derivative: DerivativeFunc<SeedToken, MapToken> = (token, mapToken) => ({});
+
+export default derivative;
