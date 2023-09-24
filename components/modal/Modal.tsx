a26cb7bae4d712293eb851e5e672ad0cbb8943bd
栗嘉男 <574980606@qq.com>
2023-09-24 21:42:06 +0800

    rerun CI

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -102,7 +102,6 @@ const Modal: React.FC<ModalProps> = (props) => {
   );
 
   // ============================ Refs ============================
-  // Select `ant-modal-content` by `panelRef`
   const panelRef = usePanelRef(`.${prefixCls}-content`);
 
   // =========================== Render ===========================
