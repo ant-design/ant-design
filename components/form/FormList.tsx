57ede2b6a11542aaaa77c3fc1efbbc867f0dc5dc
Wasim <mwasimakram0@gmail.com>
2023-09-25 11:31:58 +0500

    fix:add in form list

--- components/form/FormList.tsx
+++ components/form/FormList.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { List } from 'rc-field-form';
 import type { StoreValue, ValidatorRule } from 'rc-field-form/lib/interface';
+import { getNamePath } from 'rc-field-form/lib/utils/valueUtil';
 
 import { devUseWarning } from '../_util/warning';
 import { ConfigContext } from '../config-provider';
-import { FormItemPrefixContext } from './context';
+import { FormContext, FormItemPrefixContext } from './context';
+import { getFieldId } from './util';
 
 export interface FormListFieldData {
   name: number;
@@ -56,22 +58,26 @@ const FormList: React.FC<FormListProps> = ({
     }),
     [prefixCls],
   );
+  const { name: formName } = React.useContext(FormContext);
+  const fieldId = getFieldId(getNamePath(props.name), formName);
 
   return (
-    <List {...props}>
-      {(fields, operation, meta) => (
-        <FormItemPrefixContext.Provider value={contextValue}>
-          {children(
-            fields.map((field) => ({ ...field, fieldKey: field.key })),
-            operation,
-            {
-              errors: meta.errors,
-              warnings: meta.warnings,
-            },
-          )}
-        </FormItemPrefixContext.Provider>
-      )}
-    </List>
+    <div id={fieldId}>
+      <List {...props}>
+        {(fields, operation, meta) => (
+          <FormItemPrefixContext.Provider value={contextValue}>
+            {children(
+              fields.map((field) => ({ ...field, fieldKey: field.key })),
+              operation,
+              {
+                errors: meta.errors,
+                warnings: meta.warnings,
+              },
+            )}
+          </FormItemPrefixContext.Provider>
+        )}
+      </List>
+    </div>
   );
 };
 
