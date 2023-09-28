6e9f9b248e9ccddae6268ac1b981545122b18a80
二货机器人 <smith3816@gmail.com>
2023-09-28 16:28:15 +0800

    test: update test

--- components/input/demo/advance-count.tsx
+++ components/input/demo/advance-count.tsx
@@ -3,35 +3,41 @@ import { Flex, Input, Typography } from 'antd';
 import { runes } from 'runes2';
 
 const App: React.FC = () => (
-  <Flex vertical>
-    <Typography.Title level={5}>Exceed Style</Typography.Title>
-    <Input
-      count={{
-        show: true,
-        max: 10,
-      }}
-      defaultValue="Hello, antd!"
-    />
+  <Flex vertical gap={16}>
+    <div>
+      <Typography.Title level={5}>Exceed Max</Typography.Title>
+      <Input
+        count={{
+          show: true,
+          max: 10,
+        }}
+        defaultValue="Hello, antd!"
+      />
+    </div>
 
-    <Typography.Title level={5}>Emoji count as length 1</Typography.Title>
-    <Input
-      count={{
-        show: true,
-        strategy: (txt) => runes(txt).length,
-      }}
-      defaultValue="🔥🔥🔥"
-    />
+    <div>
+      <Typography.Title level={5}>Emoji count as length 1</Typography.Title>
+      <Input
+        count={{
+          show: true,
+          strategy: (txt) => runes(txt).length,
+        }}
+        defaultValue="🔥🔥🔥"
+      />
+    </div>
 
-    <Typography.Title level={5}>Not exceed max</Typography.Title>
-    <Input
-      count={{
-        show: true,
-        max: 5,
-        strategy: (txt) => runes(txt).length,
-        exceedFormatter: (txt, { max }) => runes(txt).slice(0, max).join(''),
-      }}
-      defaultValue="Type"
-    />
+    <div>
+      <Typography.Title level={5}>Not exceed max</Typography.Title>
+      <Input
+        count={{
+          show: true,
+          max: 6,
+          strategy: (txt) => runes(txt).length,
+          exceedFormatter: (txt, { max }) => runes(txt).slice(0, max).join(''),
+        }}
+        defaultValue="🔥 antd"
+      />
+    </div>
   </Flex>
 );
 
