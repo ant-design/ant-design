2142bb3b836a9844fefed3a0de82a30b0a56cbfd
二货机器人 <smith3816@gmail.com>
2023-10-12 14:31:32 +0800

    chore: clean up

--- components/tree/demo/virtual-scroll.tsx
+++ components/tree/demo/virtual-scroll.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tree, Tooltip } from 'antd';
+import { Tree } from 'antd';
 import type { DataNode } from 'antd/es/tree';
 
 const dig = (path = '0', level = 3) => {
@@ -22,13 +22,6 @@ const dig = (path = '0', level = 3) => {
 
 const treeData = dig();
 
-const App: React.FC = () => (
-  <Tree
-    treeData={treeData}
-    height={233}
-    defaultExpandAll
-    titleRender={(item) => <Tooltip title={item.title as any}>{item.title as any}</Tooltip>}
-  />
-);
+const App: React.FC = () => <Tree treeData={treeData} height={233} defaultExpandAll />;
 
 export default App;
