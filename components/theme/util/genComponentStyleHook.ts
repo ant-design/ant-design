02ba7c25b626ae181f8521248fb5d0082624a7b6
MadCcc <1075746765@qq.com>
2023-11-08 15:52:59 +0800

    fix: component token should work

--- components/theme/util/genComponentStyleHook.ts
+++ components/theme/util/genComponentStyleHook.ts
@@ -170,7 +170,7 @@ export default function genComponentStyleHook<ComponentName extends OverrideComp
           });
           flush(component, mergedComponentToken);
           return [
-            options.resetStyle === false ? null : genCommonStyle(token, prefixCls),
+            options.resetStyle === false ? null : genCommonStyle(mergedToken, prefixCls),
             styleInterpolation,
           ];
         },
