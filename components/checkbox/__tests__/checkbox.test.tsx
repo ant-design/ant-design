2fce0f6927c0074d1004e981958d569899f3033e
wuxh <wxh1220@gmail.com>
2023-11-09 13:01:48 +0800

    test: add unit case

--- components/checkbox/__tests__/checkbox.test.tsx
+++ components/checkbox/__tests__/checkbox.test.tsx
@@ -24,4 +24,38 @@ describe('Checkbox', () => {
     fireEvent.mouseLeave(container.querySelector('label')!);
     expect(onMouseLeave).toHaveBeenCalled();
   });
+
+  it('should be controlled by value', () => {
+    const mockChangeHandler = jest.fn();
+
+    const { getByRole } = render(<Checkbox value onChange={mockChangeHandler} />);
+
+    const checkboxNode = getByRole('checkbox');
+    expect(checkboxNode).toBeTruthy();
+    expect(checkboxNode).toBeChecked();
+
+    fireEvent.click(checkboxNode);
+
+    expect(mockChangeHandler).toHaveBeenCalledWith(expect.any(Object));
+    expect(mockChangeHandler.mock.calls[0][0].target.checked).toBeFalsy();
+    // controlled component, so still true after click
+    expect(checkboxNode).toBeChecked();
+  });
+
+  it('should be uncontrolled by defaultValue', () => {
+    const mockChangeHandler = jest.fn();
+
+    const { getByRole } = render(<Checkbox defaultValue onChange={mockChangeHandler} />);
+
+    const checkboxNode = getByRole('checkbox');
+    expect(checkboxNode).toBeTruthy();
+    expect(checkboxNode).toBeChecked();
+
+    fireEvent.click(checkboxNode);
+
+    expect(mockChangeHandler).toHaveBeenCalledWith(expect.any(Object));
+    expect(mockChangeHandler.mock.calls[0][0].target.checked).toBeFalsy();
+    // uncontrolled component, so false after click
+    expect(checkboxNode).not.toBeChecked();
+  });
 });
