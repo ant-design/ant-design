fb91232b936ef825e211234ec081a9da5bedbf68
wuxh <wxh1220@gmail.com>
2023-11-09 12:45:36 +0800

    chore: remove value warn

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -3,7 +3,6 @@ import classNames from 'classnames';
 import type { CheckboxRef } from 'rc-checkbox';
 import RcCheckbox from 'rc-checkbox';
 
-import { devUseWarning } from '../_util/warning';
 import Wave from '../_util/wave';
 import { TARGET_CLS } from '../_util/wave/interface';
 import { ConfigContext } from '../config-provider';
@@ -76,16 +75,6 @@ const InternalCheckbox: React.ForwardRefRenderFunction<CheckboxRef, CheckboxProp
   const mergedDisabled = (checkboxGroup?.disabled || disabled) ?? contextDisabled;
 
   const prevValue = React.useRef(restProps.value);
-
-  if (process.env.NODE_ENV !== 'production') {
-    const warning = devUseWarning('Checkbox');
-
-    warning(
-      'checked' in restProps || !!checkboxGroup || !('value' in restProps),
-      'usage',
-      '`value` is not a valid prop, do you mean `checked`?',
-    );
-  }
 
   React.useEffect(() => {
     checkboxGroup?.registerValue(restProps.value);
--- components/checkbox/__tests__/checkbox.test.tsx
+++ components/checkbox/__tests__/checkbox.test.tsx
@@ -4,7 +4,6 @@ import focusTest from '../../../tests/shared/focusTest';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import { fireEvent, render } from '../../../tests/utils';
-import { resetWarned } from '../../_util/warning';
 
 describe('Checkbox', () => {
   focusTest(Checkbox, { refFocus: true });
@@ -24,16 +23,5 @@ describe('Checkbox', () => {
 
     fireEvent.mouseLeave(container.querySelector('label')!);
     expect(onMouseLeave).toHaveBeenCalled();
-  });
-
-  it('warning if set `value`', () => {
-    resetWarned();
-
-    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-    render(<Checkbox value />);
-    expect(errorSpy).toHaveBeenCalledWith(
-      'Warning: [antd: Checkbox] `value` is not a valid prop, do you mean `checked`?',
-    );
-    errorSpy.mockRestore();
   });
 });
