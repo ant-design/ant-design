8cefdebd7ee3f7b98be8ea17b5f1e1247917ecf8
栗嘉男 <574980606@qq.com>
2023-11-11 23:33:38 +0800

    chore: add jsDoc for cssVar calculator

--- components/theme/util/calc/calculator.ts
+++ components/theme/util/calc/calculator.ts
@@ -1,11 +1,28 @@
-export default abstract class AbstractCalculator {
+abstract class AbstractCalculator {
+  /**
+   * 加
+   */
   abstract add(num: number | string | AbstractCalculator): this;
 
+  /**
+   * 减
+   */
   abstract sub(num: number | string | AbstractCalculator): this;
 
+  /**
+   * 乘
+   */
   abstract mul(num: number | string | AbstractCalculator): this;
 
+  /**
+   * 除
+   */
   abstract div(num: number | string | AbstractCalculator): this;
 
+  /**
+   * 等于
+   */
   abstract equal(options?: { unit?: boolean }): string | number;
 }
+
+export default AbstractCalculator;
