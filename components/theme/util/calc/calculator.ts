833fc37f1509364b13d9344d9027007774d79440
lijianan <574980606@qq.com>
2023-11-12 13:59:06 +0800

    fix: add prefix

--- components/theme/util/calc/calculator.ts
+++ components/theme/util/calc/calculator.ts
@@ -1,26 +1,31 @@
 abstract class AbstractCalculator {
   /**
-   * 加
+   * @desc 计算两数的和，例如：1 + 2
+   * @descEN Calculate the sum of two numbers, for example: 1 + 2
    */
   abstract add(num: number | string | AbstractCalculator): this;
 
   /**
-   * 减
+   * @desc 计算两数的差，例如：1 - 2
+   * @descEN Calculate the difference between two numbers, for example: 1 - 2
    */
   abstract sub(num: number | string | AbstractCalculator): this;
 
   /**
-   * 乘
+   * @desc 计算两数的积，例如：1 * 2
+   * @descEN Calculate the product of two numbers, for example: 1 * 2
    */
   abstract mul(num: number | string | AbstractCalculator): this;
 
   /**
-   * 除
+   * @desc 计算两数的商，例如：1 / 2
+   * @descEN Calculate the quotient of two numbers, for example: 1 / 2
    */
   abstract div(num: number | string | AbstractCalculator): this;
 
   /**
-   * 等于
+   * @desc 获取计算结果
+   * @descEN Get the calculation result
    */
   abstract equal(options?: { unit?: boolean }): string | number;
 }
