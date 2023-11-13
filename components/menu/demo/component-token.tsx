496ac3326f21d9be82d1d6ee713f4659b7a2ddcc
陈帅 <qixian.cs@outlook.com>
2023-11-12 20:15:07 -0600

    feat: support popupBg and darkPopupBg (#45826)
    * feat: support popupBg and darkPopupBg
    
    * fix test

--- components/menu/demo/component-token.tsx
+++ components/menu/demo/component-token.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   AppstoreOutlined,
   ContainerOutlined,
@@ -6,7 +7,6 @@ import {
   PieChartOutlined,
   SettingOutlined,
 } from '@ant-design/icons';
-import React, { useState } from 'react';
 import type { MenuProps } from 'antd';
 import { ConfigProvider, Menu, Space } from 'antd';
 
@@ -120,6 +120,7 @@ const App: React.FC = () => {
           components: {
             Menu: {
               horizontalItemBorderRadius: 6,
+              popupBg: 'red',
               horizontalItemHoverBg: '#f5f5f5',
             },
           },
@@ -136,6 +137,7 @@ const App: React.FC = () => {
               darkSubMenuItemBg: '#faad14',
               darkItemSelectedColor: '#ffccc7',
               darkItemSelectedBg: '#52c41a',
+              darkPopupBg: 'red',
             },
           },
         }}
@@ -149,6 +151,24 @@ const App: React.FC = () => {
           style={{
             width: 256,
           }}
+        />
+      </ConfigProvider>
+      <ConfigProvider
+        theme={{
+          components: {
+            Menu: {
+              darkPopupBg: 'red',
+            },
+          },
+        }}
+      >
+        <Menu
+          inlineCollapsed
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          items={items2}
         />
       </ConfigProvider>
     </Space>
--- components/menu/style/index.tsx
+++ components/menu/style/index.tsx
@@ -1,6 +1,7 @@
+import type { CSSProperties } from 'react';
 import type { CSSObject } from '@ant-design/cssinjs';
 import { TinyColor } from '@ctrl/tinycolor';
-import type { CSSProperties } from 'react';
+
 import { clearFix, resetComponent, resetIcon } from '../../style';
 import { genCollapseMotion, initSlideMotion, initZoomMotion } from '../../style/motion';
 import type { FullToken, GenerateStyle, UseComponentStyleResult } from '../../theme/internal';
@@ -287,6 +288,12 @@ export interface ComponentToken {
   collapsedIconSize: number;
 
   // Dark
+  /**
+   * @desc 暗色模式下的浮层菜单的背景颜色
+   * @descEN The background color of the overlay menu in dark mode.
+   */
+  darkPopupBg: string;
+
   /**
    * @desc 暗色模式下的菜单项文字颜色
    * @descEN Color of menu item text in dark mode
@@ -814,6 +821,7 @@ export default (prefixCls: string, injectStyle: boolean): UseComponentStyleResul
         darkItemColor,
         darkDangerItemColor,
         darkItemBg,
+        darkPopupBg,
         darkSubMenuItemBg,
         darkItemSelectedColor,
         darkItemSelectedBg,
@@ -844,7 +852,7 @@ export default (prefixCls: string, injectStyle: boolean): UseComponentStyleResul
         groupTitleColor: darkGroupTitleColor,
         itemSelectedColor: darkItemSelectedColor,
         itemBg: darkItemBg,
-        popupBg: darkItemBg,
+        popupBg: darkPopupBg || darkItemBg,
         subMenuItemBg: darkSubMenuItemBg,
         itemActiveBg: 'transparent',
         itemSelectedBg: darkItemSelectedBg,
@@ -1000,6 +1008,7 @@ export default (prefixCls: string, injectStyle: boolean): UseComponentStyleResul
         darkItemColor: colorTextDark,
         darkDangerItemColor: colorError,
         darkItemBg: '#001529',
+        darkPopupBg: '#001529',
         darkSubMenuItemBg: '#000c17',
         darkItemSelectedColor: colorTextLightSolid,
         darkItemSelectedBg: colorPrimary,
