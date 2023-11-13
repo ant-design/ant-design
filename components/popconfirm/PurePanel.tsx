b080f83865f6e27b8977dff5a9d903edd01923ab
MadCcc <1075746765@qq.com>
2023-11-13 21:26:33 +0800

    Apply suggestions from code review
    Signed-off-by: MadCcc <1075746765@qq.com>

--- components/popconfirm/PurePanel.tsx
+++ components/popconfirm/PurePanel.tsx
@@ -10,6 +10,7 @@ import { ConfigContext } from '../config-provider';
 import { useLocale } from '../locale';
 import defaultLocale from '../locale/en_US';
 import PopoverPurePanel from '../popover/PurePanel';
+import useStyle from './style';
 import useCSSVar from './style/cssVar';
 
 export interface PopconfirmLocale {
