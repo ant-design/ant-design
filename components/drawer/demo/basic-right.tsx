2bc4285eb750ebc950bd72e0a0891904e0edf3cc
kiner-tang(文辉) <1127031143@qq.com>
2023-11-14 15:17:07 +0800

    feat: optimize code

--- components/drawer/demo/basic-right.tsx
+++ components/drawer/demo/basic-right.tsx
@@ -1,48 +1,28 @@
 import React, { useState } from 'react';
-import { Button, ColorPicker, Drawer, Popconfirm, Popover, Space, Tooltip } from 'antd';
+import { Button, Drawer } from 'antd';
 
-const App = () => {
+const App: React.FC = () => {
   const [open, setOpen] = useState(false);
-  const [open2, setOpen2] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div className="App">
-      <Button type="primary" onClick={() => setOpen(true)}>
-        Open Drawer1
+    <>
+      <Button type="primary" onClick={showDrawer}>
+        Open
       </Button>
-      <Drawer
-        title="Basic Drawer"
-        width="80%"
-        placement="right"
-        onClose={() => setOpen(false)}
-        open={open}
-      >
-        <Button type="primary" onClick={() => setOpen2(true)}>
-          Open Drawer2
-        </Button>
-
-        <Drawer
-          title="Second Drawer"
-          width="50%"
-          placement="right"
-          onClose={() => setOpen2(false)}
-          open={open2}
-        >
-          <Space>
-            <Tooltip title="Delete the task">
-              <Button danger>Tooltip</Button>
-            </Tooltip>
-            <Popover title="Delete the task">
-              <Button danger>Popover</Button>
-            </Popover>
-            <Popconfirm title="Delete the task">
-              <Button danger>popconfirm</Button>
-            </Popconfirm>
-            <ColorPicker />
-          </Space>
-        </Drawer>
+      <Drawer title="Basic Drawer" placement="right" onClose={onClose} open={open}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
       </Drawer>
-    </div>
+    </>
   );
 };
 
