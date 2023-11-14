5dc8c96867dbf152701a8e4f14a84c6fc831fc44
MadCcc <1075746765@qq.com>
2023-11-14 09:46:34 +0800

    chore: use lineHeight

--- components/form/style/index.ts
+++ components/form/style/index.ts
@@ -224,7 +224,7 @@ const genFormItemStyle: GenerateStyle<FormToken> = (token) => {
 
         '&-wrap': {
           overflow: 'unset',
-          lineHeight: `calc(${unit(token.fontHeight)} - 0.25em)`,
+          lineHeight: token.lineHeight,
           whiteSpace: 'unset',
         },
 
