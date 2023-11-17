0f9620b5fe5ee58d108c90c3cec2ae2efed7764a
MadCcc <1075746765@qq.com>
2023-11-17 11:55:05 +0800

    chore: update

--- scripts/check-cssinjs.tsx
+++ scripts/check-cssinjs.tsx
@@ -29,7 +29,7 @@ console.error = (msg: any) => {
 async function checkCSSVar() {
   await generateCssinjs({
     key: 'check',
-    ignore: ['grid', 'pagination', 'steps'],
+    ignore: ['grid', 'pagination', 'steps', 'calendar'],
     render(Component: any) {
       ReactDOMServer.renderToString(
         <StyleProvider linters={[NaNLinter]}>
