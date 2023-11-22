9e10a9a36c9a7aab34a83d8aeaf248e754717f03
lijianan <574980606@qq.com>
2023-11-22 20:56:42 +0800

    chore: remove useless parameter

--- scripts/generate-cssinjs.ts
+++ scripts/generate-cssinjs.ts
@@ -9,7 +9,6 @@ interface GenCssinjsOptions {
   key: string;
   render: (component: React.FC) => void;
   beforeRender?: (componentName: string) => void;
-  ignore?: string[];
 }
 
 export const styleFiles = globSync(
@@ -22,16 +21,13 @@ export const styleFiles = globSync(
     .join('/'),
 );
 
-export const generateCssinjs = ({ key, beforeRender, render, ignore }: GenCssinjsOptions) =>
+export const generateCssinjs = ({ key, beforeRender, render }: GenCssinjsOptions) =>
   Promise.all(
     styleFiles.map(async (file) => {
       const absPath = url.pathToFileURL(file).href;
       const pathArr = file.split('/');
       const styleIndex = pathArr.lastIndexOf('style');
       const componentName = pathArr[styleIndex - 1];
-      if (ignore?.includes(componentName)) {
-        return;
-      }
       let useStyle: StyleFn = () => {};
       if (file.includes('grid')) {
         const { useColStyle, useRowStyle } = await import(absPath);
