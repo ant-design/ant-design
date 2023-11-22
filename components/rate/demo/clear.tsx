c7b788539a3d8b3698045b246deb56f19eec8f80
MadCcc <1075746765@qq.com>
2023-11-22 16:10:03 +0800

    fix: Rate style

--- components/rate/demo/clear.tsx
+++ components/rate/demo/clear.tsx
@@ -4,10 +4,10 @@ import { Rate } from 'antd';
 const App: React.FC = () => (
   <>
     <Rate defaultValue={3} />
-    <span className="ant-rate-text">allowClear: true</span>
+    <span style={{ fontSize: 14, marginLeft: 8 }}>allowClear: true</span>
     <br />
     <Rate allowClear={false} defaultValue={3} />
-    <span className="ant-rate-text">allowClear: false</span>
+    <span style={{ fontSize: 14, marginLeft: 8 }}>allowClear: false</span>
   </>
 );
 
