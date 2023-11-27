eb45f2486205b0816836b76526895dbcecf537af
vagusX <vagusxl@gmail.com>
2023-11-27 09:08:03 +0000

    chore: update

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,9 @@
 // jest-puppeteer.config.js
 module.exports = {
   launch: {
+    headless: process.env.CI === 'true' ? 'new' : false,
+    ignoreDefaultArgs: ['--disable-extensions'],
+    executablePath: 'chrome.exe',
     args: [
       // Required for Docker version of Puppeteer
       '--no-sandbox',
