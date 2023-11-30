e1b88a778b7cf5c009712032315161d196034eb0
lijianan <574980606@qq.com>
2023-11-30 10:45:36 +0800

    test: add test

--- components/float-button/demo/basic.tsx
+++ components/float-button/demo/basic.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { FloatButton } from 'antd';
 
-const App: React.FC = () => (
-  <FloatButton
-    onClick={() => console.log('click')}
-    onMouseEnter={() => console.log('mouseEnter')}
-    onMouseLeave={() => console.log('mouseLeave')}
-  />
-);
+const App: React.FC = () => <FloatButton onClick={() => console.log('onClick')} />;
 
 export default App;
