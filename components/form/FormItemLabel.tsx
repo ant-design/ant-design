2bd6c07c334de493c4778ccc0d9595888144b4fb
wuxh <wxh1220@gmail.com>
2023-12-01 14:15:53 +0800

    chore: fix spelling error

--- components/form/FormItemLabel.tsx
+++ components/form/FormItemLabel.tsx
@@ -103,7 +103,7 @@ const FormItemLabel: React.FC<FormItemLabelProps & { required?: boolean; prefixC
                   // https://github.com/ant-design/ant-design/issues/46154
                   e.preventDefault();
                 },
-                tabindex: null,
+                tabIndex: null,
               })}
             </Tooltip>
           );
