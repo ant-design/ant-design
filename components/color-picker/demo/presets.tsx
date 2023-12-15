635e05b7d81985522943ae02b31c33b7de70e378
红 <wxh1220@gmail.com>
2023-12-15 16:29:51 +0800

    docs(color-picker): improve example (#45527)
    * docs: improved color-picker demo #45527
    
    chore: improve customization panel cls
    
    chore: update demo
    
    chore: update snapshots
    
    test: add case
    
    chore: update
    
    chore: update snapshots
    
    chore: to simplify the complex things
    
    chore: update snapshots
    
    * revert files
    
    * chore: revert change
    
    * chore: update snapshot

--- components/color-picker/demo/presets.tsx
+++ components/color-picker/demo/presets.tsx
@@ -1,48 +1,24 @@
 import React from 'react';
-import { ColorPicker } from 'antd';
+import { ColorPicker, theme } from 'antd';
+import type { ColorPickerProps } from 'antd';
+import { generate, green, presetPalettes, red } from '@ant-design/colors';
 
-export default () => (
-  <ColorPicker
-    presets={[
-      {
-        label: 'Recommended',
-        colors: [
-          '#000000',
-          '#000000E0',
-          '#000000A6',
-          '#00000073',
-          '#00000040',
-          '#00000026',
-          '#0000001A',
-          '#00000012',
-          '#0000000A',
-          '#00000005',
-          '#F5222D',
-          '#FA8C16',
-          '#FADB14',
-          '#8BBB11',
-          '#52C41A',
-          '#13A8A8',
-          '#1677FF',
-          '#2F54EB',
-          '#722ED1',
-          '#EB2F96',
-          '#F5222D4D',
-          '#FA8C164D',
-          '#FADB144D',
-          '#8BBB114D',
-          '#52C41A4D',
-          '#13A8A84D',
-          '#1677FF4D',
-          '#2F54EB4D',
-          '#722ED14D',
-          '#EB2F964D',
-        ],
-      },
-      {
-        label: 'Recent',
-        colors: [],
-      },
-    ]}
-  />
-);
+type Presets = Required<ColorPickerProps>['presets'][number];
+
+const genPresets = (presets = presetPalettes) =>
+  Object.entries(presets).map<Presets>(([label, colors]) => ({
+    label,
+    colors,
+  }));
+
+export default () => {
+  const { token } = theme.useToken();
+
+  const presets = genPresets({
+    primary: generate(token.colorPrimary),
+    red,
+    green,
+  });
+
+  return <ColorPicker presets={presets} />;
+};
