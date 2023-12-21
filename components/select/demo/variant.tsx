72c97f594633397f73b6ea48864469253d39049f
MadCcc <1075746765@qq.com>
2023-12-21 11:46:25 +0800

    chore: code clean

--- components/select/demo/variant.tsx
+++ components/select/demo/variant.tsx
@@ -27,7 +27,6 @@ const App: React.FC = () => (
     </Flex>
     <Flex gap={8}>
       <Select
-        allowClear
         placeholder="Filled"
         variant="filled"
         style={{ flex: 1 }}
