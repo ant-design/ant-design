6b2862b5c793ee4998dc249dcf250e4312735b36
叶枫 <645381995@qq.com>
2023-12-25 09:06:09 +0800

    feat: reset demo

--- components/message/demo/info.tsx
+++ components/message/demo/info.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { StyleProvider } from '@ant-design/cssinjs';
-import { Button, ConfigProvider, message } from 'antd';
+import { Button, message } from 'antd';
 
 const info = () => {
-  message.info('This is a normal message', 50);
+  message.info('This is a normal message');
 };
-
-ConfigProvider.config({
-  container: (children) => <StyleProvider hashPriority="high">{children}</StyleProvider>,
-});
 
 const App: React.FC = () => (
   <Button type="primary" onClick={info}>
