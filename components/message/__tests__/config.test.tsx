8fb6bbe3f6f0131b2a36b2d0a8cc98f7f3a57dba
叶枫 <645381995@qq.com>
2023-12-26 09:30:03 +0800

    feat: test

--- components/message/__tests__/config.test.tsx
+++ components/message/__tests__/config.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import message, { actWrapper } from '..';
+import message, { actDestroy, actWrapper } from '..';
 import { act } from '../../../tests/utils';
 import ConfigProvider from '../../config-provider';
 import { awaitPromise, triggerMotionEnd } from './util';
@@ -28,10 +28,8 @@ describe('message.config', () => {
     message.config({
       top: 100,
     });
-
     message.info('whatever');
     await awaitPromise();
-
     expect(document.querySelector('.ant-message')).toHaveStyle({
       top: '100px',
     });
@@ -222,14 +220,22 @@ describe('message.config', () => {
     message.config({ getContainer: undefined });
   });
   it('should be able to config container', async () => {
+    actDestroy();
     ConfigProvider.config({
-      container: (children) => <ConfigProvider prefixCls="test">{children}</ConfigProvider>,
+      container: (children) => (
+        <ConfigProvider iconPrefixCls="aaa" prefixCls="test">
+          {children}
+        </ConfigProvider>
+      ),
     });
 
     message.info('last');
     await awaitPromise();
 
-    expect(document.querySelector('.test-message')).toBeTruthy();
-    ConfigProvider.config({ container: (children) => children });
+    expect(document.querySelectorAll('.ant-message')).toHaveLength(0);
+    expect(document.querySelectorAll('.anticon-info-circle')).toHaveLength(0);
+    expect(document.querySelectorAll('.test-message')).toHaveLength(1);
+    expect(document.querySelectorAll('.aaa-info-circle')).toHaveLength(1);
+    ConfigProvider.config({ container: undefined });
   });
 });
