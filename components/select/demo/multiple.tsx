1b0e15ad8d4db00c97340c180b4c5e66230d3f3a
MadCcc <1075746765@qq.com>
2023-12-29 15:13:57 +0800

    chore: code clean

--- components/select/demo/multiple.tsx
+++ components/select/demo/multiple.tsx
@@ -4,7 +4,7 @@ import type { SelectProps } from 'antd';
 
 const options: SelectProps['options'] = [];
 
-for (let i = 10; i < 20; i++) {
+for (let i = 10; i < 36; i++) {
   options.push({
     label: i.toString(36) + i,
     value: i.toString(36) + i,
