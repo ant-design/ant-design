e7809bdb4a326ee97c6afdf4452a13d2a48bfe41
wuxh <wxh1220@gmail.com>
2023-12-29 16:07:31 +0800

    demo: 复现 bug

--- components/select/demo/option-label-center.tsx
+++ components/select/demo/option-label-center.tsx
@@ -1,76 +1,103 @@
 import React from 'react';
-import { Select, Space, Cascader, Typography, TreeSelect } from 'antd';
+import { Select, Space, Cascader, Typography, TreeSelect, Segmented } from 'antd';
 
 const options = [
   { value: 'long', label: <Typography>long, long, long piece of text</Typography> },
   { value: 'short', label: <Typography>short</Typography> },
   { value: 'normal', label: <div>normal</div> },
+  {
+    value: 'issue-46636',
+    label: (
+      <Typography.Text
+        ellipsis={{
+          tooltip: {
+            overlay: 'https://github.com/ant-design/ant-design/issues/46636',
+            overlayInnerStyle: {
+              maxHeight: '50vh',
+              maxWidth: '40vw',
+            },
+            mouseEnterDelay: 0.5,
+          },
+        }}
+      >
+        {String('Issue #46636 ').repeat(10)}
+      </Typography.Text>
+    ),
+  },
 ];
 
-const App: React.FC = () => (
-  <Space wrap>
-    <Select
-      defaultValue="long, long, long piece of text"
-      style={{ width: 120 }}
-      allowClear
-      options={options}
-    />
+const App: React.FC = () => {
+  const values = options.map((item) => item.value);
+  const [value, setValue] = React.useState(values.at(0));
 
-    <Select
-      placeholder="Select a option"
-      style={{ width: 120, height: 60 }}
-      allowClear
-      options={options}
-    />
+  return (
+    <>
+      <Segmented options={values} onChange={setValue as any} />
+      <br />
+      <Space wrap key={value}>
+        <Select defaultValue={value} style={{ width: 120 }} allowClear options={options} />
 
-    <Select
-      defaultValue="normal"
-      placeholder="Select a option"
-      style={{ width: 120 }}
-      allowClear
-      options={options}
-    />
+        <Select
+          defaultValue={value}
+          placeholder="Select a option"
+          style={{ width: 120, height: 60 }}
+          allowClear
+          options={options}
+        />
 
-    <Select
-      defaultValue={['normal']}
-      mode="multiple"
-      placeholder="Select a option"
-      style={{ width: 120 }}
-      allowClear
-      options={options}
-    />
+        <Select
+          defaultValue={value}
+          placeholder="Select a option"
+          style={{ width: 120 }}
+          allowClear
+          options={options}
+        />
 
-    <Select
-      mode="multiple"
-      placeholder="Select a option"
-      style={{ width: 120, height: 60 }}
-      allowClear
-      options={options}
-    />
+        <Select
+          defaultValue={[value]}
+          mode="multiple"
+          placeholder="Select a option"
+          style={{ width: 120 }}
+          allowClear
+          options={options}
+        />
 
-    <Cascader
-      placeholder="Select a option"
-      style={{ width: 120, height: 60 }}
-      allowClear
-      options={options}
-    />
+        <Select
+          mode="multiple"
+          defaultValue={[value]}
+          placeholder="Select a option"
+          style={{ width: 120, height: 60 }}
+          allowClear
+          options={options}
+        />
 
-    <TreeSelect
-      showSearch
-      style={{ width: 120, height: 60 }}
-      placeholder="Please select"
-      allowClear
-      popupMatchSelectWidth={false}
-      treeDefaultExpandAll
-      treeData={[
-        {
-          value: 'parent 1',
-          title: 'parent 1',
-          children: options,
-        },
-      ]}
-    />
-  </Space>
-);
+        <Cascader
+          placeholder="Select a option"
+          style={{ width: 120, height: 60 }}
+          allowClear
+          defaultValue={value as any}
+          options={options}
+        />
+
+        <TreeSelect
+          showSearch
+          style={{ width: 120, height: 60 }}
+          placeholder="Please select"
+          allowClear
+          popupMatchSelectWidth={false}
+          treeDefaultExpandAll
+          defaultValue={value}
+          treeData={[
+            {
+              value: 'parent 1',
+              title: 'parent 1',
+              children: options,
+            },
+          ]}
+        />
+      </Space>
+    </>
+  );
+};
 
 export default App;
