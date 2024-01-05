ca39a4b1eed70ff945745d4dc2735cd99f059201
lijianan <574980606@qq.com>
2024-01-05 10:56:11 +0800

    refactor: refactor indicator props (#46786)
    * refactor: refactor indicator props
    
    * Update components/tabs/index.tsx
    
    Co-authored-by: afc163 <afc163@gmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * fix: update
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * test: add test case
    
    * docs: update docs
    
    * Update components/config-provider/index.en-US.md
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/config-provider/index.zh-CN.md
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/tabs/index.tsx
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/tabs/index.tsx
    
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/tabs/index.zh-CN.md
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/tabs/index.en-US.md
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    * Update components/tabs/index.zh-CN.md
    
    Co-authored-by: MadCcc <madccc@foxmail.com>
    Signed-off-by: lijianan <574980606@qq.com>
    
    ---------
    
    Signed-off-by: lijianan <574980606@qq.com>
    Co-authored-by: afc163 <afc163@gmail.com>
    Co-authored-by: MadCcc <madccc@foxmail.com>

--- components/tabs/demo/custom-indicator.tsx
+++ components/tabs/demo/custom-indicator.tsx
@@ -7,39 +7,28 @@ const onChange = (key: string) => {
 };
 
 const items: TabsProps['items'] = [
-  {
-    key: '1',
-    label: 'Tab 1',
-    children: 'Content of Tab Pane 1',
-  },
-  {
-    key: '2',
-    label: 'Tab 2',
-    children: 'Content of Tab Pane 2',
-  },
-  {
-    key: '3',
-    label: 'Tab 3',
-    children: 'Content of Tab Pane 3',
-  },
+  { key: '1', label: 'Tab 1', children: 'Content of Tab Pane 1' },
+  { key: '2', label: 'Tab 2', children: 'Content of Tab Pane 2' },
+  { key: '3', label: 'Tab 3', children: 'Content of Tab Pane 3' },
 ];
 
+type Align = 'start' | 'center' | 'end';
+
 const App: React.FC = () => {
-  const [align, setAlign] = React.useState<TabsProps['indicatorAlign']>('center');
+  const [alignValue, setAlignValue] = React.useState<Align>('center');
   return (
     <>
       <Segmented
         defaultValue="center"
         style={{ marginBottom: 8 }}
-        onChange={(value) => setAlign(value as TabsProps['indicatorAlign'])}
+        onChange={(value) => setAlignValue(value as Align)}
         options={['start', 'center', 'end']}
       />
       <Tabs
         defaultActiveKey="1"
         items={items}
         onChange={onChange}
-        indicatorSize={(origin) => origin - 20}
-        indicatorAlign={align}
+        indicator={{ size: (origin) => origin - 20, align: alignValue }}
       />
     </>
   );
--- components/tabs/index.zh-CN.md
+++ components/tabs/index.zh-CN.md
@@ -55,8 +55,7 @@ Ant Design 依次提供了三级选项卡，分别用于不同的场景。
 | centered | 标签居中展示 | boolean | false | 4.4.0 |
 | defaultActiveKey | 初始化选中面板的 key，如果没有设置 activeKey | string | `第一个面板` |  |
 | hideAdd | 是否隐藏加号图标，在 `type="editable-card"` 时有效 | boolean | false |  |
-| indicatorAlign | 自定义指示条对齐方式 | `start` \| `center` \| `end` | `center` | 5.13.0 |
-| indicatorSize | 自定义指示条长度，默认与 tab 等宽 | number \| (origin: number) => number | - | 5.9.0 |
+| indicator | 自定义指示条的长度和对齐方式 | { size?: number \| (origin: number) => number; align: `start` \| `center` \| `end`; } | - | 5.13.0 |
 | items | 配置选项卡内容 | [TabItemType](#tabitemtype) | [] | 4.23.0 |
 | moreIcon | 自定义折叠 icon | ReactNode | &lt;EllipsisOutlined /> | 4.14.0 |
 | popupClassName | 更多菜单的 `className` | string | - | 4.21.0 |
