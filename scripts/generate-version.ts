dc25aa145858783ae11b95873a3385655b8e6943
wuxh <wxh1220@gmail.com>
2024-01-05 10:29:37 +0800

    chore: revert commit

--- scripts/generate-version.ts
+++ scripts/generate-version.ts
@@ -1,12 +1,11 @@
-import fs from 'fs-extra';
+import fs from 'fs';
 import path from 'path';
 import localPackage from '../package.json';
-import deprecatedVersions from '../deprecated-versions.json';
 
-const outputBase = path.join(__dirname, '..', 'components', 'version');
 const { version } = localPackage;
 
-fs.writeFileSync(path.join(outputBase, 'version.ts'), `export default '${version}';`, 'utf8');
-
-// Compression formatting and rename to deprecated.json
-fs.writeJsonSync(path.join(outputBase, 'deprecated.json'), deprecatedVersions);
+fs.writeFileSync(
+  path.join(__dirname, '..', 'components', 'version', 'version.ts'),
+  `export default '${version}';`,
+  'utf8',
+);
