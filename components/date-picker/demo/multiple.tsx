76a49bf7a96297976d42d84b14a90c6143375dcd
二货机器人 <smith3816@gmail.com>
2024-01-09 17:34:09 +0800

    chore: size of it

--- components/date-picker/demo/multiple.tsx
+++ components/date-picker/demo/multiple.tsx
@@ -8,14 +8,24 @@ const onChange: DatePickerProps<Dayjs[]>['onChange'] = (date, dateString) => {
   console.log(date, dateString);
 };
 
+const defaultValue = [dayjs(), dayjs().add(1, 'day'), dayjs().add(2, 'day')];
+
 const App: React.FC = () => (
   <Flex vertical gap="small">
     <DatePicker
       multiple
       onChange={onChange}
-      defaultValue={[dayjs('2024-01-01')]}
       maxTagCount="responsive"
-      open
+      defaultValue={defaultValue}
+      size="small"
+    />
+    <DatePicker multiple onChange={onChange} maxTagCount="responsive" defaultValue={defaultValue} />
+    <DatePicker
+      multiple
+      onChange={onChange}
+      maxTagCount="responsive"
+      defaultValue={defaultValue}
+      size="large"
     />
   </Flex>
 );
