23f76378982cb5ba027758d51c4f95ae24e38daa
MadCcc <1075746765@qq.com>
2024-01-09 15:06:28 +0800

    fix: inputnumber padding token

--- components/input-number/style/index.ts
+++ components/input-number/style/index.ts
@@ -53,6 +53,9 @@ const genInputNumberStyles: GenerateStyle<InputNumberToken> = (token: InputNumbe
     controlHeightSM,
     colorError,
     paddingInlineSM,
+    paddingBlockSM,
+    paddingBlockLG,
+    paddingInlineLG,
     colorTextDescription,
     motionDurationMid,
     handleHoverColor,
@@ -67,6 +70,7 @@ const genInputNumberStyles: GenerateStyle<InputNumberToken> = (token: InputNumbe
     handleOpacity,
     handleBorderColor,
     filledHandleBg,
+    lineHeightLG,
     calc,
   } = token;
 
@@ -118,10 +122,12 @@ const genInputNumberStyles: GenerateStyle<InputNumberToken> = (token: InputNumbe
         '&-lg': {
           padding: 0,
           fontSize: fontSizeLG,
+          lineHeight: lineHeightLG,
           borderRadius: borderRadiusLG,
 
           [`input${componentCls}-input`]: {
             height: calc(controlHeightLG).sub(calc(lineWidth).mul(2)).equal(),
+            padding: `${unit(paddingBlockLG)} ${unit(paddingInlineLG)}`,
           },
         },
 
@@ -131,7 +137,7 @@ const genInputNumberStyles: GenerateStyle<InputNumberToken> = (token: InputNumbe
 
           [`input${componentCls}-input`]: {
             height: calc(controlHeightSM).sub(calc(lineWidth).mul(2)).equal(),
-            padding: `0 ${unit(paddingInlineSM)}`,
+            padding: `${unit(paddingBlockSM)} ${unit(paddingInlineSM)}`,
           },
         },
 
@@ -358,11 +364,14 @@ const genAffixWrapperStyles: GenerateStyle<InputNumberToken> = (token: InputNumb
     controlWidth,
     borderRadiusLG,
     borderRadiusSM,
+    paddingInlineLG,
+    paddingInlineSM,
+    paddingBlockLG,
+    paddingBlockSM,
   } = token;
 
   return {
     [`${componentCls}-affix-wrapper`]: {
-      ...genBasicInputStyle(token),
       // or number handler will cover form status
       position: 'relative',
       display: 'inline-flex',
@@ -370,12 +379,32 @@ const genAffixWrapperStyles: GenerateStyle<InputNumberToken> = (token: InputNumb
       padding: 0,
       paddingInlineStart: paddingInline,
 
+      [`input${componentCls}-input`]: {
+        paddingBlock,
+        paddingInlineStart: 0,
+        paddingInlineEnd: paddingInline,
+      },
+
+      ...genBasicInputStyle(token),
+
       '&-lg': {
         borderRadius: borderRadiusLG,
+        paddingInlineStart: paddingInlineLG,
+
+        [`input${componentCls}-input`]: {
+          paddingBlock: paddingBlockLG,
+          paddingInlineEnd: paddingInlineLG,
+        },
       },
 
       '&-sm': {
         borderRadius: borderRadiusSM,
+        paddingInlineStart: paddingInlineSM,
+
+        [`input${componentCls}-input`]: {
+          paddingBlock: paddingBlockSM,
+          paddingInlineEnd: paddingInlineSM,
+        },
       },
 
       [`&:not(${componentCls}-disabled):hover`]: {
@@ -398,10 +427,6 @@ const genAffixWrapperStyles: GenerateStyle<InputNumberToken> = (token: InputNumb
         [`&${componentCls}-focused`]: {
           boxShadow: 'none !important',
         },
-      },
-
-      [`input${componentCls}-input`]: {
-        padding: `${unit(paddingBlock)} 0`,
       },
 
       '&::before': {
