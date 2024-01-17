b1dde311efceda6cee9ce3daeaf85ddb15224fa9
MadCcc <1075746765@qq.com>
2024-01-17 17:42:30 +0800

    chore: correct lint

--- components/progress/design/behavior-pattern.tsx
+++ components/progress/design/behavior-pattern.tsx
@@ -16,12 +16,12 @@ const BehaviorPattern: React.FC = () => (
             {
               id: '707000085',
               label: '了解任务进度',
-              link: 'components-date-picker-index-tab-design-zh-cn-demo-pick-date',
+              link: 'components-progress-index-tab-design-demo-progress',
             },
             {
               id: '707000086',
               label: '了解任务状态',
-              link: 'components-date-picker-index-tab-design-zh-cn-demo-pick-week',
+              link: 'components-progress-index-tab-design-demo-status',
             },
           ],
         },
@@ -29,7 +29,7 @@ const BehaviorPattern: React.FC = () => (
           id: '200000005',
           label: '查看进度相关描述',
           targetType: 'extension',
-          link: '',
+          link: 'components-progress-index-tab-design-demo-info',
         },
       ],
     }}
