916d63e2bb9ee8e9249c156fa95a30031889d8ae
madocto <hi.madocto@gmail.com>
2024-01-22 15:37:30 +0800

    docs: update demo

--- components/segmented/demo/basic.tsx
+++ components/segmented/demo/basic.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Segmented } from 'antd';
 
 const Demo: React.FC = () => (
-  <Segmented options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']} />
+  <Segmented<string | number | boolean>
+    options={['Daily', 'Weekly', 2021, 2022, true, false]}
+    onChange={(value) => {
+      console.log(value);
+    }}
+  />
 );
 
 export default Demo;
