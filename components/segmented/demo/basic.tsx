9ceb44a14efd25b8e32a8083ea7866fa9245b44b
afc163 <afc163@gmail.com>
2024-01-26 23:11:40 +0800

    Update components/segmented/demo/basic.tsx
    Signed-off-by: afc163 <afc163@gmail.com>

--- components/segmented/demo/basic.tsx
+++ components/segmented/demo/basic.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Segmented } from 'antd';
 
 const Demo: React.FC = () => (
-  <Segmented<string | number | boolean>
-    options={['Daily', 'Weekly', 2021, 2022, true, false]}
+  <Segmented<string>
+    options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']}
     onChange={(value) => {
-      console.log(value);
+      console.log(value); // string
     }}
   />
 );
