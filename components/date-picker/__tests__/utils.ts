6e9a9249b68d86b80067ca4b3456332ae9b4fef0
二货机器人 <smith3816@gmail.com>
2024-02-01 15:45:21 +0800

    chore: cleanup

--- components/date-picker/__tests__/utils.ts
+++ components/date-picker/__tests__/utils.ts
@@ -1,5 +1,5 @@
 import type { render } from '../../../tests/utils';
-import { fireEvent, screen } from '../../../tests/utils';
+import { fireEvent } from '../../../tests/utils';
 
 export function openPicker(wrapper: ReturnType<typeof render>, index = 0) {
   const inputEle = wrapper.container?.querySelectorAll<HTMLInputElement>('input')?.[index]!;
