ebb6feb81abdaaa6d5f655e0511cf5c6d40404a1
栗嘉男 <574980606@qq.com>
2024-02-02 09:58:47 +0800

    fix: fix

--- components/upload/utils.ts
+++ components/upload/utils.ts
@@ -39,3 +39,96 @@ export function removeFileItem(file: UploadFile, fileList: (UploadFile | Readonl
   }
   return removed;
 }
+
+// ==================== Default Image Preview ====================
+const extname = (url: string = '') => {
+  const temp = url.split('/');
+  const filename = temp[temp.length - 1];
+  const filenameWithoutSuffix = filename.split(/#|\?/)[0];
+  return (/\.[^./\\]*$/.exec(filenameWithoutSuffix) || [''])[0];
+};
+
+const isImageFileType = (type: string): boolean => type.indexOf('image/') === 0;
+
+export const isImageUrl = (file: UploadFile): boolean => {
+  if (file.type && !file.thumbUrl) {
+    return isImageFileType(file.type);
+  }
+  const url = file.thumbUrl || file.url || '';
+  const extension = extname(url);
+  if (
+    /^data:image\//.test(url) ||
+    /(webp|svg|png|gif|jpg|jpeg|jfif|bmp|dpg|ico|heic|heif)$/i.test(extension)
+  ) {
+    return true;
+  }
+  if (/^data:/.test(url)) {
+    // other file types of base64
+    return false;
+  }
+  if (extension) {
+    // other file types which have extension
+    return false;
+  }
+  return true;
+};
+
+const MEASURE_SIZE = 200;
+
+export function previewImage(file: File | Blob): Promise<string> {
+  return new Promise<string>((resolve) => {
+    if (!file.type || !isImageFileType(file.type)) {
+      resolve('');
+      return;
+    }
+    const canvas = document.createElement('canvas');
+    canvas.width = MEASURE_SIZE;
+    canvas.height = MEASURE_SIZE;
+    canvas.style.cssText = `position: fixed; left: 0; top: 0; width: ${MEASURE_SIZE}px; height: ${MEASURE_SIZE}px; z-index: 9999; display: none;`;
+    document.body.appendChild<HTMLCanvasElement>(canvas);
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.onload = () => {
+      const { width, height } = img;
+
+      let drawWidth = MEASURE_SIZE;
+      let drawHeight = MEASURE_SIZE;
+      let offsetX = 0;
+      let offsetY = 0;
+
+      if (width > height) {
+        drawHeight = height * (MEASURE_SIZE / width);
+        offsetY = -(drawHeight - drawWidth) / 2;
+      } else {
+        drawWidth = width * (MEASURE_SIZE / height);
+        offsetX = -(drawWidth - drawHeight) / 2;
+      }
+
+      ctx!.drawImage(img, offsetX, offsetY, drawWidth, drawHeight);
+      const dataURL = canvas.toDataURL();
+      document.body.removeChild(canvas);
+      window.URL.revokeObjectURL(img.src);
+      resolve(dataURL);
+    };
+    img.crossOrigin = 'anonymous';
+    if (file.type.startsWith('image/svg+xml')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result && typeof reader.result === 'string') {
+          img.src = reader.result;
+        }
+      };
+      reader.readAsDataURL(file);
+    } else if (file.type.startsWith('image/gif')) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result) {
+          resolve(reader.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    } else {
+      img.src = window.URL.createObjectURL(file);
+    }
+  });
+}
