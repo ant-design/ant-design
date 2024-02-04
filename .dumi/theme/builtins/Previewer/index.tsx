7b5a7e54ef69eccfbfd74ee81cbf897c4a4b49b9
栗嘉男 <574980606@qq.com>
2024-02-04 09:43:32 +0800

    fix: fix

--- .dumi/theme/builtins/Previewer/index.tsx
+++ .dumi/theme/builtins/Previewer/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
-import type { IPreviewerProps } from 'dumi';
-import { Skeleton, Alert } from 'antd';
+import { Alert, Skeleton } from 'antd';
 import { createStyles } from 'antd-style';
+import type { IPreviewerProps } from 'dumi';
 
 const { ErrorBoundary } = Alert;
 
@@ -10,26 +10,19 @@ const Previewer = React.lazy(() => import('./Previewer'));
 const useStyle = createStyles(({ css }) => ({
   skeletonWrapper: css`
     width: 100% !important;
-    height: 250px;
+    height: 100%;
     margin-bottom: 16px;
     border-radius: 8px;
   `,
 }));
 
-export default (props: IPreviewerProps) => {
+const PreviewerSuspense: React.FC<IPreviewerProps> = (props) => {
   const { styles } = useStyle();
   return (
     <ErrorBoundary>
       <Suspense
         fallback={
-          <Skeleton.Node
-            active
-            className={styles.skeletonWrapper}
-            style={{
-              width: '100%',
-              height: '100%',
-            }}
-          >
+          <Skeleton.Node active className={styles.skeletonWrapper}>
             {' '}
           </Skeleton.Node>
         }
@@ -39,3 +32,5 @@ export default (props: IPreviewerProps) => {
     </ErrorBoundary>
   );
 };
+
+export default PreviewerSuspense;
