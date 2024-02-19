dd0d914d8207d53bd95af47662d0090e79043b7b
kiner-tang <1127031143@qq.com>
2024-02-19 11:21:10 +0800

    feat: Alert support aria-* in closable

--- components/config-provider/__tests__/style.test.tsx
+++ components/config-provider/__tests__/style.test.tsx
@@ -708,7 +708,21 @@ describe('ConfigProvider support style and className props', () => {
   });
 
   it('Should Alert className works', () => {
-    const { container } = render(
+    const { container, rerender } = render(
+      <ConfigProvider
+        alert={{
+          className: 'test-class',
+          closeIcon: <span className="cp-test-icon">cp-test-icon</span>,
+          closable: { 'aria-label': 'close' },
+        }}
+      >
+        <Alert message="Test Message" />
+      </ConfigProvider>,
+    );
+    expect(container.querySelector<HTMLDivElement>('.ant-alert')).toHaveClass('test-class');
+    expect(container.querySelector<HTMLSpanElement>('.ant-alert .cp-test-icon')).toBeTruthy();
+    expect(container.querySelectorAll('*[aria-label="close"]')).toBeTruthy();
+    rerender(
       <ConfigProvider
         alert={{
           className: 'test-class',
@@ -721,6 +735,7 @@ describe('ConfigProvider support style and className props', () => {
         <Alert message="Test Message" />
       </ConfigProvider>,
     );
+
     expect(container.querySelector<HTMLDivElement>('.ant-alert')).toHaveClass('test-class');
     expect(container.querySelector<HTMLSpanElement>('.ant-alert .cp-test-icon')).toBeTruthy();
     expect(container.querySelectorAll('*[aria-label="close"]')).toBeTruthy();
