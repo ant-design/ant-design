7ceee1c28ec9bef5e9b99afbb073acc5ad4a7327
叶枫 <645381995@qq.com>
2024-02-23 18:54:25 +0800

    feat: demo

--- components/form/demo/layout-multiple.tsx
+++ components/form/demo/layout-multiple.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import { Divider, Form, Input } from 'antd';
+import { Button, Col, Flex, Form, Input, Radio, Row, Space } from 'antd';
 
-const App: React.FC = () => (
-  <Form name="layout-multiple" style={{ maxWidth: 600 }} autoComplete="off" noLayout>
-    <Divider orientation="left" plain>
-      horizontal
-    </Divider>
-    <Form.Layout layout="horizontal" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-      <Form.Item label="horizontal" name="horizontal" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-    </Form.Layout>
-    <Divider orientation="left" plain>
-      vertical
-    </Divider>
-    <Form.Layout layout="vertical">
-      <Form.Item label="vertical" name="vertical" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-    </Form.Layout>
-    <Divider orientation="left" plain>
-      inline
-    </Divider>
-    <Form.Layout layout="inline">
-      <Form.Item label="inline" name="inline" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="inline2" name="inline2" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-    </Form.Layout>
-  </Form>
-);
+const getFields = () => {
+  const count = 3;
+  const children = [];
+  for (let i = 0; i < count; i++) {
+    children.push(
+      <Form.Item key={i} name={`field-${i}`} label={`Field ${i}`} style={{ marginBottom: 0 }}>
+        <Input placeholder="placeholder" />
+      </Form.Item>,
+    );
+  }
+  return children;
+};
+
+const App: React.FC = () => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} name="layout-multiple" noLayout>
+      <Row gutter={24} style={{ marginBottom: 8 }}>
+        <Col span={16}>
+          <Form.Layout layout="vertical">
+            <Flex gap={8}>{getFields()}</Flex>
+          </Form.Layout>
+        </Col>
+        <Col
+          span={8}
+          style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end' }}
+        >
+          <Space>
+            <Button type="primary" htmlType="submit">
+              Search
+            </Button>
+            <Button onClick={() => form.resetFields()}>Clear</Button>
+          </Space>
+        </Col>
+      </Row>
+      <Form.Layout layout="inline">
+        <Flex gap={8}>
+          <Form.Item name="fruit" label="fruit">
+            <Radio.Group optionType="button" options={['Apple', 'Orange']} />
+          </Form.Item>
+          <Form.Item name="type" label="type">
+            <Radio.Group optionType="button" options={['Option A', 'Option B']} />
+          </Form.Item>
+        </Flex>
+      </Form.Layout>
+    </Form>
+  );
+};
 
 export default App;
