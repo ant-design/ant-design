4d6ac1a6b06e5d38cd5f39abc93baf518d0c7a09
米家-iOS-张文锋 <zhangwfyoupu@126.com>
2024-03-01 09:17:46 +0800

    Update options.tsx
    Remove the unused param:index
    
    Signed-off-by: 米家-iOS-张文锋 <zhangwfyoupu@126.com>

--- components/auto-complete/demo/options.tsx
+++ components/auto-complete/demo/options.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
     if (!value || value.indexOf('@') >= 0) {
       res = [];
     } else {
-      res = ['gmail.com', '163.com', 'qq.com'].map((domain,index) => ({
+      res = ['gmail.com', '163.com', 'qq.com'].map((domain) => ({
         value: `${value}@${domain}`,
         label: `${value}@${domain}`,
       }));
