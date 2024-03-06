2f4dc018ba358825a0d6b79419c4a886008ae61f
kiner-tang <1127031143@qq.com>
2024-03-06 11:53:05 +0800

    feat: Notification support aria-* in closable (#47710)
    * feat: Notification support aria-* in closable
    
    * feat: optimize code

--- components/notification/interface.ts
+++ components/notification/interface.ts
@@ -1,4 +1,5 @@
 import type * as React from 'react';
+import type { ClosableType } from '../_util/hooks/useClosable';
 
 interface DivProps extends React.HTMLProps<HTMLDivElement> {
   'data-testid'?: string;
@@ -30,6 +31,7 @@ export interface ArgsProps {
   readonly type?: IconType;
   onClick?: () => void;
   closeIcon?: React.ReactNode;
+  closable?: ClosableType;
   props?: DivProps;
   role?: 'alert' | 'status';
 }
@@ -53,6 +55,7 @@ export interface GlobalConfigProps {
   getContainer?: () => HTMLElement | ShadowRoot;
   placement?: NotificationPlacement;
   closeIcon?: React.ReactNode;
+  closable?: ClosableType;
   rtl?: boolean;
   maxCount?: number;
   props?: DivProps;
