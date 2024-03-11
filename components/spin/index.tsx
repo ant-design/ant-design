401477db56128c7a5d9158d4e6e0762385ffb7b4
mr-chenguang lcgash <lcgash@163.com>
2024-03-11 09:49:29 +0000

    fix: 调整透明度的实现方式

--- components/spin/index.tsx
+++ components/spin/index.tsx
@@ -14,11 +14,7 @@ export type SpinSize = (typeof SpinSizes)[number];
 
 const SpinVisibilityies = ['soft', 'default', 'heavy'] as const;
 export type SpinVisibility = (typeof SpinVisibilityies)[number];
-const SpinVisibilityOpacity: Record<SpinVisibility, number> = {
-  soft: 0.9,
-  default: 0.5,
-  heavy: 0.2,
-};
+
 export type SpinIndicator = React.ReactElement<HTMLElement>;
 
 export interface SpinProps {
@@ -157,13 +153,9 @@ const Spin: SpinType = (props) => {
 
   const containerClassName = classNames(`${prefixCls}-container`, {
     [`${prefixCls}-blur`]: spinning,
+    [`${prefixCls}-blur-heavy`]: visibility === 'heavy',
+    [`${prefixCls}-blur-soft`]: visibility === 'soft',
   });
-
-  const containerStyle: React.CSSProperties = visibility
-    ? {
-        opacity: SpinVisibilityOpacity[visibility],
-      }
-    : {};
 
   // fix https://fb.me/react-unknown-prop
   const divProps = omit(restProps, ['indicator']);
@@ -192,7 +184,7 @@ const Spin: SpinType = (props) => {
         className={classNames(`${prefixCls}-nested-loading`, wrapperClassName, hashId, cssVarCls)}
       >
         {spinning && <div key="loading">{spinElement}</div>}
-        <div className={containerClassName} style={containerStyle} key="container">
+        <div className={containerClassName} key="container">
           {children}
         </div>
       </div>,
