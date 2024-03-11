b3605ba9f2f527138dba480edb25f7e4c93cd237
MadCcc <1075746765@qq.com>
2024-03-11 11:41:04 +0800

    chore: code clean

--- components/form/demo/basic.tsx
+++ components/form/demo/basic.tsx
@@ -7,11 +7,11 @@ type FieldType = {
   remember?: string;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
   console.log('Success:', values);
 };
 
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
   console.log('Failed:', errorInfo);
 };
 
