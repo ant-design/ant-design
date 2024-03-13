66469e7ede49b6316e4bb926d55ed39e9c5d89f3
叶晓龙 <evermore.ye@quectel.com>
2024-03-13 10:20:27 +0800

    perf: 代码优化: 1.Skeleton组件children null判断; 2.Paragraph组件内部getWidth方法抽离;

--- components/skeleton/Paragraph.tsx
+++ components/skeleton/Paragraph.tsx
@@ -11,18 +11,19 @@ export interface SkeletonParagraphProps {
   rows?: number;
 }
 
+const getWidth = (index: number, props: SkeletonParagraphProps) => {
+  const { width, rows = 2 } = props;
+  if (Array.isArray(width)) {
+    return width[index];
+  }
+  // last paragraph
+  if (rows - 1 === index) {
+    return width;
+  }
+  return undefined;
+};
+
 const Paragraph: React.FC<SkeletonParagraphProps> = (props) => {
-  const getWidth = (index: number) => {
-    const { width, rows = 2 } = props;
-    if (Array.isArray(width)) {
-      return width[index];
-    }
-    // last paragraph
-    if (rows - 1 === index) {
-      return width;
-    }
-    return undefined;
-  };
   const { prefixCls, className, style, rows } = props;
   const rowList = [...Array(rows)].map((_, index) => (
     // eslint-disable-next-line react/no-array-index-key
