a7a2979bff99f7791583b7e310a5e0041e4fe147
CooperHash <2597905281@qq.com>
2024-03-18 13:43:11 +0800

    fix

--- components/popconfirm/index.tsx
+++ components/popconfirm/index.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import ExclamationCircleFilled from '@ant-design/icons/ExclamationCircleFilled';
 import classNames from 'classnames';
 import useMergedState from 'rc-util/lib/hooks/useMergedState';
-import KeyCode from 'rc-util/lib/KeyCode';
 import omit from 'rc-util/lib/omit';
 
 import type { RenderFunction } from '../_util/getRenderPropValue';
-import { cloneElement } from '../_util/reactNode';
 import type { ButtonProps, LegacyButtonType } from '../button/button';
 import { ConfigContext } from '../config-provider';
 import Popover from '../popover';
