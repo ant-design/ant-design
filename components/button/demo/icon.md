d9f48d1f3a03c2a835e15396aa9e800d4b00bc2a
GeorgeH <default_hyn@163.com>
2024-03-25 04:29:26 +0800

    restore icon demo

--- components/button/demo/icon.md
+++ components/button/demo/icon.md
@@ -1,15 +1,11 @@
 ## zh-CN
 
-当需要在 `Button` 内嵌入 `Icon` 时，可以设置 `icon` 属性，或者直接在 `Button` 内使用 `Icon` 组件。
+当需要在 `Button` 内嵌入 `Icon` 时，可以设置 `icon` 属性，或者直接在`Button`内使用`Icon`组件。
 
-`5.16.0`开始支持使用`iconPosition`属性，通过设置其值为`start`,`end`设置icon的位置。
-
-推荐使用`icon`属性加`iconPosition`的方式设置图标的位置。
+如果想控制`Icon`具体的位置，只能直接使用 `Icon `组件，而非` icon` 属性。
 
 ## en-US
 
 `Button` components can contain an `Icon`. This is done by setting the `icon` property or placing an `Icon` component within the `Button`.
 
-`5.16.0` starts to support the use of `iconPosition` attribute, and sets the position of icon by setting its value to `start` and `end`.
-
-It is recommended to use the `icon` attribute plus `iconPosition` to set the position of the icon.
+If you want specific control over the positioning and placement of the `Icon`, then that should be done by placing the `Icon` component within the `Button` rather than using the `icon` property.
--- components/button/demo/icon.tsx
+++ components/button/demo/icon.tsx
@@ -1,58 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { SearchOutlined } from '@ant-design/icons';
-import { Button, Divider, Flex, Radio, Space, Tooltip } from 'antd';
+import { Button, Flex, Tooltip } from 'antd';
 
-const App: React.FC = () => {
-  const [position, setPosition] = useState<'start' | 'end'>('start');
-
-  return (
-    <>
-      <Space>
-        iconPosition
-        <Radio.Group value={position} onChange={(e) => setPosition(e.target.value)}>
-          <Radio.Button value="start">start</Radio.Button>
-          <Radio.Button value="end">end</Radio.Button>
-        </Radio.Group>
-      </Space>
-      <Divider orientation="left" plain>
-        Preview
-      </Divider>
-      <Flex gap="small" vertical>
-        <Flex wrap="wrap" gap="small">
-          <Tooltip title="search">
-            <Button type="primary" shape="circle" icon={<SearchOutlined />} />
-          </Tooltip>
-          <Button type="primary" shape="circle">
-            A
-          </Button>
-          <Button type="primary" icon={<SearchOutlined />} iconPosition={position}>
-            Search
-          </Button>
-          <Tooltip title="search">
-            <Button shape="circle" icon={<SearchOutlined />} />
-          </Tooltip>
-          <Button icon={<SearchOutlined />} iconPosition={position}>
-            Search
-          </Button>
-        </Flex>
-        <Flex wrap="wrap" gap="small">
-          <Tooltip title="search">
-            <Button shape="circle" icon={<SearchOutlined />} />
-          </Tooltip>
-          <Button icon={<SearchOutlined />} iconPosition={position}>
-            Search
-          </Button>
-          <Tooltip title="search">
-            <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
-          </Tooltip>
-          <Button type="dashed" icon={<SearchOutlined />} iconPosition={position}>
-            Search
-          </Button>
-          <Button icon={<SearchOutlined />} href="https://www.google.com" iconPosition={position} />
-        </Flex>
-      </Flex>
-    </>
-  );
-};
+const App: React.FC = () => (
+  <Flex gap="small" vertical>
+    <Flex wrap="wrap" gap="small">
+      <Tooltip title="search">
+        <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+      </Tooltip>
+      <Button type="primary" shape="circle">
+        A
+      </Button>
+      <Button type="primary" icon={<SearchOutlined />}>
+        Search
+      </Button>
+      <Tooltip title="search">
+        <Button shape="circle" icon={<SearchOutlined />} />
+      </Tooltip>
+      <Button icon={<SearchOutlined />}>Search</Button>
+    </Flex>
+    <Flex wrap="wrap" gap="small">
+      <Tooltip title="search">
+        <Button shape="circle" icon={<SearchOutlined />} />
+      </Tooltip>
+      <Button icon={<SearchOutlined />}>Search</Button>
+      <Tooltip title="search">
+        <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
+      </Tooltip>
+      <Button type="dashed" icon={<SearchOutlined />}>
+        Search
+      </Button>
+      <Button icon={<SearchOutlined />} href="https://www.google.com" />
+    </Flex>
+  </Flex>
+);
 
 export default App;
