14bdbf8e79edf52c5e4dd81e6f2bb7a6d062c87e
二货机器人 <smith3816@gmail.com>
2024-03-25 16:52:35 +0800

    docs: update demo

--- components/input/OTP/index.tsx
+++ components/input/OTP/index.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import { useEvent, useMergedState } from 'rc-util';
+import { useEvent } from 'rc-util';
+import pickAttrs from 'rc-util/lib/pickAttrs';
 
+import type { InputStatus } from '../../_util/statusUtils';
 import { ConfigContext } from '../../config-provider';
 import useCSSVarCls from '../../config-provider/hooks/useCSSVarCls';
 import useSize from '../../config-provider/hooks/useSize';
 import { type SizeType } from '../../config-provider/SizeContext';
+import type { Variant } from '../../form/hooks/useVariants';
 import { type InputRef } from '../Input';
 import useStyle from '../style/otp';
 import OTPInput, { type OTPInputProps } from './OTPInput';
 
-export interface OTPProps {
+export interface OTPRef {
+  focus?: VoidFunction;
+  blur?: VoidFunction;
+  nativeElement: HTMLDivElement;
+}
+
+export interface OTPProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
   prefixCls?: string;
-  length?: number;
+  count?: number;
+
+  // Style
+  variant?: Variant;
   rootClassName?: string;
   className?: string;
   style?: React.CSSProperties;
@@ -22,27 +34,51 @@ export interface OTPProps {
   defaultValue?: string;
   value?: string;
   onChange?: (value: string) => void;
+  formatter?: (value: string) => string;
+
+  // Status
+  disabled?: boolean;
+  status?: InputStatus;
 }
 
 /**
  * 需求：
- * - 输入框长度
  * - 验证颜色
- * - 格式化输入（大小写）
  */
 
-const OTP = (props: OTPProps) => {
+function strToArr(str: string) {
+  return str.split('');
+}
+
+const OTP = React.forwardRef<OTPRef, OTPProps>((props, ref) => {
   const {
     prefixCls: customizePrefixCls,
-    length = 6,
+    count = 6,
     size: customSize,
     defaultValue,
     value,
     onChange,
+    formatter,
+    variant,
+    disabled,
+    status,
+    ...restProps
   } = props;
 
-  const { getPrefixCls, direction, input } = React.useContext(ConfigContext);
+  const { getPrefixCls, direction } = React.useContext(ConfigContext);
   const prefixCls = getPrefixCls('otp', customizePrefixCls);
+
+  const domAttrs = pickAttrs(restProps, {
+    aria: true,
+    data: true,
+    attr: true,
+  });
+
+  const inputSharedProps = {
+    variant,
+    disabled,
+    status,
+  };
 
   // ========================= Root =========================
   // Style
@@ -53,11 +89,42 @@ const OTP = (props: OTPProps) => {
   const mergedSize = useSize((ctx) => customSize ?? ctx);
 
   // ========================= Refs =========================
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
   const refs = React.useRef<Record<number, InputRef | null>>({});
 
+  React.useImperativeHandle(ref, () => ({
+    focus: () => {
+      refs.current[0]?.focus();
+    },
+    blur: () => {
+      refs.current[0]?.blur();
+    },
+    nativeElement: containerRef.current!,
+  }));
+
   // ======================== Values ========================
-  const [valueCells, setValueCells] = React.useState<string[]>([]);
-  const valueCellsStr = valueCells.join('');
+  const [valueCells, setValueCells] = React.useState<string[]>(strToArr(defaultValue || ''));
+
+  React.useEffect(() => {
+    if (value) {
+      setValueCells(strToArr(value));
+    }
+  }, [value]);
+
+  const triggerValueCellsChange = useEvent((nextValueCells: string[]) => {
+    setValueCells(nextValueCells);
+
+    // Trigger if all cells are filled
+    if (
+      onChange &&
+      nextValueCells.length === count &&
+      nextValueCells.every((c) => c) &&
+      nextValueCells.some((c, index) => valueCells[index] !== c)
+    ) {
+      onChange(nextValueCells.join(''));
+    }
+  });
 
   const patchValue = useEvent((index: number, txt: string) => {
     let nextCells = [...valueCells];
@@ -65,28 +132,42 @@ const OTP = (props: OTPProps) => {
     if (txt.length <= 1) {
       nextCells[index] = txt;
     } else {
-      nextCells = nextCells.slice(0, index).concat(txt.split(''));
+      nextCells = nextCells.slice(0, index).concat(strToArr(txt));
+    }
+    nextCells = nextCells.slice(0, count);
+
+    // Clean the last empty cell
+    for (let i = nextCells.length - 1; i >= 0; i -= 1) {
+      if (nextCells[i]) {
+        break;
+      }
+      nextCells.pop();
     }
 
-    return nextCells.slice(0, length).filter((cell) => cell);
+    // Format if needed
+    if (formatter) {
+      const formattedValue = formatter(nextCells.map((c) => c || ' ').join(''));
+      nextCells = strToArr(formattedValue).map((c, i) => {
+        if (c === ' ' && !nextCells[i]) {
+          return nextCells[i];
+        }
+        return c;
+      });
+    }
+
+    return nextCells;
   });
-
-  React.useEffect(() => {
-    if (value) {
-      setValueCells(value.split(''));
-    }
-  }, [value]);
 
   // ======================== Change ========================
   const onInputChange: OTPInputProps['onChange'] = (index, txt) => {
     const nextCells = patchValue(index, txt);
 
-    const nextIndex = Math.min(index + txt.length, length - 1);
+    const nextIndex = Math.min(index + txt.length, count - 1);
     if (nextIndex !== index) {
       refs.current[nextIndex]?.focus();
     }
 
-    setValueCells(nextCells);
+    triggerValueCellsChange(nextCells);
   };
 
   const onInputBack: OTPInputProps['onBack'] = (index) => {
@@ -95,8 +176,10 @@ const OTP = (props: OTPProps) => {
   };
 
   // ======================== Render ========================
-  return (
+  return wrapCSSVar(
     <div
+      {...domAttrs}
+      ref={containerRef}
       className={classNames(
         prefixCls,
         {
@@ -108,7 +191,7 @@ const OTP = (props: OTPProps) => {
         hashId,
       )}
     >
-      {new Array(length).fill(0).map((_, index) => {
+      {new Array(count).fill(0).map((_, index) => {
         const key = `otp-${index}`;
         const singleValue = valueCells[index] || '';
 
@@ -125,11 +208,12 @@ const OTP = (props: OTPProps) => {
             onChange={onInputChange}
             value={singleValue}
             onBack={onInputBack}
+            {...inputSharedProps}
           />
         );
       })}
-    </div>
+    </div>,
   );
-};
+});
 
 export default OTP;
--- components/input/demo/otp.tsx
+++ components/input/demo/otp.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Input, Space } from 'antd';
+import { Input, Segmented, Space, Typography, type GetProp } from 'antd';
 
-const App: React.FC = () => (
-  <Space direction="vertical">
-    <Input.OTP size="small" />
-    <Input.OTP />
-    <Input.OTP size="large" />
-  </Space>
-);
+const { Title } = Typography;
+
+type Status = 'default' | 'disabled' | 'warning' | 'error';
+
+const App: React.FC = () => {
+  const [status, setStatus] = React.useState<Status>('default');
+
+  const onChange: GetProp<typeof Input.OTP, 'onChange'> = (text) => {
+    console.log('onChange:', text);
+  };
+
+  const sharedProps = {
+    disabled: status === 'disabled',
+    onChange,
+    status: status === 'error' || status === 'warning' ? status : undefined,
+  };
+
+  return (
+    <Space direction="vertical">
+      <Space>
+        <Segmented<Status>
+          options={['default', 'disabled', 'warning', 'error']}
+          value={status}
+          onChange={setStatus}
+        />
+      </Space>
+
+      <Title level={5}>With formatter (Upcase)</Title>
+      <Input.OTP formatter={(str) => str.toUpperCase()} {...sharedProps} />
+      <Title level={5}>With count</Title>
+      <Input.OTP count={8} {...sharedProps} />
+      <Title level={5}>With variant</Title>
+      <Input.OTP variant="filled" {...sharedProps} />
+    </Space>
+  );
+};
 
 export default App;
