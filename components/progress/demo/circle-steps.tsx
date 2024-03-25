9b81b4f1835deee05dddad24b30e92a6ea01ea35
栗嘉男 <574980606@qq.com>
2024-03-25 12:00:56 +0800

    fix: fix

--- components/progress/demo/circle-steps.tsx
+++ components/progress/demo/circle-steps.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import { Flex, Progress, Slider } from 'antd';
 
-const DEFAULT_COUNT = 5;
-const DEFAULT_GAP = 8;
-
 const App: React.FC = () => {
-  const [stepsCount, setStepsCount] = React.useState<number>(DEFAULT_COUNT);
-  const [stepsGap, setStepsGap] = React.useState<number>(DEFAULT_GAP);
+  const [stepsCount, setStepsCount] = React.useState<number>(5);
+  const [stepsGap, setStepsGap] = React.useState<number>(7);
   return (
     <>
       <p>Custom count:</p>
-      <Slider
-        defaultValue={DEFAULT_COUNT}
-        min={2}
-        max={10}
-        value={stepsCount}
-        onChange={setStepsCount}
-      />
+      <Slider min={2} max={10} value={stepsCount} onChange={setStepsCount} />
       <p>Custom gap:</p>
-      <Slider
-        defaultValue={DEFAULT_GAP}
-        step={4}
-        min={0}
-        max={40}
-        value={stepsGap}
-        onChange={setStepsGap}
-      />
+      <Slider step={4} min={0} max={40} value={stepsGap} onChange={setStepsGap} />
       <Flex wrap="wrap" gap="middle" style={{ marginTop: 16 }}>
         <Progress
           type="dashboard"
