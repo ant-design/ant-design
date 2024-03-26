4a97108dae9bc701073d9258e1ac3fac227fc679
栗嘉男 <574980606@qq.com>
2024-03-26 10:07:34 +0800

    fix: update snap

--- components/progress/demo/circle-steps.tsx
+++ components/progress/demo/circle-steps.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Flex, Progress, Slider } from 'antd';
+import { Flex, Progress, Slider, Typography } from 'antd';
 
 const App: React.FC = () => {
   const [stepsCount, setStepsCount] = React.useState<number>(5);
   const [stepsGap, setStepsGap] = React.useState<number>(7);
   return (
     <>
-      <p>Custom count:</p>
+      <Typography.Title level={5}>Custom count:</Typography.Title>
       <Slider min={2} max={10} value={stepsCount} onChange={setStepsCount} />
-      <p>Custom gap:</p>
+      <Typography.Title level={5}>Custom gap:</Typography.Title>
       <Slider step={4} min={0} max={40} value={stepsGap} onChange={setStepsGap} />
       <Flex wrap="wrap" gap="middle" style={{ marginTop: 16 }}>
         <Progress
