880d75e46ce7fd3d11a5bb33d074c84a876ea84d
konka <1094918@gmail.com>
2024-03-26 14:05:43 +0800

    fix(review): fix code  review

--- .dumi/theme/slots/Content/DocAnchor.tsx
+++ .dumi/theme/slots/Content/DocAnchor.tsx
@@ -27,7 +27,7 @@ const useStyle = createStyles(({ token, css }) => {
       border-radius: ${token.borderRadius}px;
       box-sizing: border-box;
       margin-inline-end: calc(16px - 100vw + 100%);
-      z-index: 999;
+      z-index: 10;
 
       .toc-debug {
         color: ${token.purple6};
