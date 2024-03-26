e5849fc1b9db68fa95f674110592480c33608821
lijianan <574980606@qq.com>
2024-03-26 10:43:02 +0800

    demo: update Progress demo (#48052)
    * demo: update Progress demo
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: update snap

--- components/progress/demo/circle-steps.tsx
+++ components/progress/demo/circle-steps.tsx
@@ -1,23 +1,33 @@
-import { Progress, Space } from 'antd';
 import React from 'react';
+import { Flex, Progress, Slider, Typography } from 'antd';
 
-const App: React.FC = () => (
-  <Space wrap>
-    <Progress
-      type="dashboard"
-      steps={8}
-      percent={50}
-      trailColor="rgba(0, 0, 0, 0.06)"
-      strokeWidth={20}
-    />
-    <Progress
-      type="circle"
-      percent={100}
-      steps={{ count: 5, gap: 12 }}
-      trailColor="rgba(0, 0, 0, 0.06)"
-      strokeWidth={20}
-    />
-  </Space>
-);
+const App: React.FC = () => {
+  const [stepsCount, setStepsCount] = React.useState<number>(5);
+  const [stepsGap, setStepsGap] = React.useState<number>(7);
+  return (
+    <>
+      <Typography.Title level={5}>Custom count:</Typography.Title>
+      <Slider min={2} max={10} value={stepsCount} onChange={setStepsCount} />
+      <Typography.Title level={5}>Custom gap:</Typography.Title>
+      <Slider step={4} min={0} max={40} value={stepsGap} onChange={setStepsGap} />
+      <Flex wrap="wrap" gap="middle" style={{ marginTop: 16 }}>
+        <Progress
+          type="dashboard"
+          steps={8}
+          percent={50}
+          trailColor="rgba(0, 0, 0, 0.06)"
+          strokeWidth={20}
+        />
+        <Progress
+          type="circle"
+          percent={100}
+          steps={{ count: stepsCount, gap: stepsGap }}
+          trailColor="rgba(0, 0, 0, 0.06)"
+          strokeWidth={20}
+        />
+      </Flex>
+    </>
+  );
+};
 
 export default App;
