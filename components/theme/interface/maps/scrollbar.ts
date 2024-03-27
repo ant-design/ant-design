cd23f86fe77a7fcd29b05e7061a50c3023aebc99
栗嘉男 <574980606@qq.com>
2024-03-27 10:23:21 +0800

    feat: add Scrollbar css token

---
+++ components/theme/interface/maps/scrollbar.ts
@@ -0,0 +1,23 @@
+export interface ScrollBarToken {
+  /**
+   * @desc 滚动条颜色
+   * @descEN Scrollbar color
+   * @default auto
+   * @since 5.16.0
+   */
+  scrollbarColor: string;
+  /**
+   * @desc 滚动条宽度
+   * @descEN Scrollbar width
+   * @default auto
+   * @since 5.16.0
+   */
+  scrollbarWidth: string;
+  /**
+   * @desc 设置滚动条出现的时候内容不晃动
+   * @descEN Set the content not to shake when the scrollbar appears
+   * @default auto
+   * @since 5.16.0
+   */
+  scrollbarGutter: string;
+}
