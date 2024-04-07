ede502ed3b59826c7e705d3b400e1cac00780df7
afc163 <afc163@gmail.com>
2024-04-07 18:32:02 +0800

    fix active colors

--- components/button/demo/linear-gradient.tsx
+++ components/button/demo/linear-gradient.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Button, ConfigProvider, Space } from 'antd';
 import { TinyColor } from '@ctrl/tinycolor';
 
-const colors = ['#6253E1', '#04BEFE'];
-const hovers = colors.map((color) => new TinyColor(color).lighten(5).toString());
+const colors1 = ['#6253E1', '#04BEFE'];
 const colors2 = ['#fc6076', '#ff9a44', '#ef9d43', '#e75516'];
-const hovers2 = colors2.map((color) => new TinyColor(color).lighten(5).toString());
 const colors3 = ['#40e495', '#30dd8a', '#2bb673'];
-const hovers3 = colors3.map((color) => new TinyColor(color).lighten(5).toString());
+const getHoverColors = (colors: string[]) =>
+  colors.map((color) => new TinyColor(color).lighten(5).toString());
+const getActiveColors = (colors: string[]) =>
+  colors.map((color) => new TinyColor(color).darken(5).toString());
 
 const App: React.FC = () => (
   <Space>
@@ -15,8 +16,9 @@ const App: React.FC = () => (
       theme={{
         components: {
           Button: {
-            colorPrimary: `linear-gradient(135deg, ${colors.join(', ')})`,
-            colorPrimaryHover: `linear-gradient(135deg, ${hovers.join(', ')})`,
+            colorPrimary: `linear-gradient(135deg, ${colors1.join(', ')})`,
+            colorPrimaryHover: `linear-gradient(135deg, ${getHoverColors(colors1).join(', ')})`,
+            colorPrimaryActive: `linear-gradient(135deg, ${getActiveColors(colors1).join(', ')})`,
             lineWidth: 0,
           },
         },
@@ -31,7 +33,8 @@ const App: React.FC = () => (
         components: {
           Button: {
             colorPrimary: `linear-gradient(90deg,  ${colors2.join(', ')})`,
-            colorPrimaryHover: `linear-gradient(90deg, ${hovers2.join(', ')})`,
+            colorPrimaryHover: `linear-gradient(90deg, ${getHoverColors(colors2).join(', ')})`,
+            colorPrimaryActive: `linear-gradient(90deg, ${getActiveColors(colors2).join(', ')})`,
             lineWidth: 0,
           },
         },
@@ -46,7 +49,8 @@ const App: React.FC = () => (
         components: {
           Button: {
             colorPrimary: `linear-gradient(116deg,  ${colors3.join(', ')})`,
-            colorPrimaryHover: `linear-gradient(116deg, ${hovers3.join(', ')})`,
+            colorPrimaryHover: `linear-gradient(116deg, ${getHoverColors(colors3).join(', ')})`,
+            colorPrimaryActive: `linear-gradient(116deg, ${getActiveColors(colors3).join(', ')})`,
             lineWidth: 0,
           },
         },
