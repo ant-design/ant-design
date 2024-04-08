9747496394d2735182f6b17ef3d28f363914883d
二货机器人 <smith3816@gmail.com>
2024-04-08 23:08:01 +0800

    chore: adjust ts def

--- components/alert/index.ts
+++ components/alert/index.ts
@@ -1,14 +1,9 @@
-import type React from 'react';
-
-import type { AlertProps, AlertRef } from './Alert';
 import InternalAlert from './Alert';
 import ErrorBoundary from './ErrorBoundary';
 
 export type { AlertProps } from './Alert';
 
-type CompoundedComponent = React.ForwardRefExoticComponent<
-  AlertProps & React.RefAttributes<AlertRef>
-> & {
+type CompoundedComponent = typeof InternalAlert & {
   ErrorBoundary: typeof ErrorBoundary;
 };
 
