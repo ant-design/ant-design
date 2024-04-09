1c0127ce0e7910f3b788677bab4d7bc9fc791e2f
wuxh <wxh1220@gmail.com>
2024-04-09 16:33:02 +0800

    chore: update

--- components/form/demo/validate-scroll-to-field.tsx
+++ components/form/demo/validate-scroll-to-field.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Button, Flex, Form, Input } from 'antd';
+import { Button, Flex, Form, Input, Select } from 'antd';
 
 const App = () => {
   const [form] = Form.useForm();
 
   return (
-    <Form form={form} scrollToFirstError style={{ padding: '2rem 4rem' }} layout="vertical">
-      <Form.Item>
+    <Form
+      form={form}
+      scrollToFirstError
+      style={{ padding: '2rem 4rem' }}
+      labelCol={{ span: 2 }}
+      wrapperCol={{ span: 8 }}
+    >
+      <Form.Item wrapperCol={{ offset: 2 }}>
         <Button onClick={() => form.scrollToField('bio')}>Scroll to Bio</Button>
       </Form.Item>
 
       <Form.Item name="username" label="UserName" rules={[{ required: true }]}>
         <Input />
+      </Form.Item>
+
+      <Form.Item label="Occupation" name="occupation">
+        <Select
+          options={[
+            { label: 'Designer', value: 'designer' },
+            { label: 'Developer', value: 'developer' },
+            { label: 'Product Manager', value: 'product-manager' },
+          ]}
+        />
       </Form.Item>
 
       <Form.Item name="motto" label="Motto">
@@ -22,7 +38,7 @@ const App = () => {
         <Input.TextArea rows={6} />
       </Form.Item>
 
-      <Form.Item>
+      <Form.Item wrapperCol={{ offset: 2 }}>
         <Flex gap="small">
           <Button type="primary" htmlType="submit">
             Submit
