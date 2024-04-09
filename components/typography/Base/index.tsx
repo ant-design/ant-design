5feb2da3f013033d524f8edb1e2051bbf3890f35
二货机器人 <smith3816@gmail.com>
2024-04-09 11:18:45 +0800

    fix: editable should show

--- components/typography/Base/index.tsx
+++ components/typography/Base/index.tsx
@@ -487,7 +487,7 @@ const Base = React.forwardRef<HTMLElement, BlockProps>((props, ref) => {
               width={ellipsisWidth}
               onEllipsis={onJsEllipsis}
               expanded={expanded}
-              miscDeps={[copied, expanded, copyLoading]}
+              miscDeps={[copied, expanded, copyLoading, editable]}
             >
               {(node, canEllipsis) => {
                 let renderNode: React.ReactNode = node;
