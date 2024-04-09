a99faf1cdbbbeb704cb09e8b7e71b66fbff1c48e
wuxh <wxh1220@gmail.com>
2024-04-09 15:57:43 +0800

    chore: update demo

--- components/form/demo/validate-scroll-to-field.tsx
+++ components/form/demo/validate-scroll-to-field.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Button, Flex, Form, Input, Upload } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
+import { Button, Flex, Form, Input } from 'antd';
 
 const App = () => {
   const [form] = Form.useForm();
 
   return (
-    <Form
-      form={form}
-      scrollToFirstError
-      style={{ padding: '2rem 4rem' }}
-      layout="vertical"
-    >
+    <Form form={form} scrollToFirstError style={{ padding: '2rem 4rem' }} layout="vertical">
       <Form.Item>
-        <Button onClick={() => form.scrollToField('demo-form_dragger')}>Scroll to Upload</Button>
+        <Button onClick={() => form.scrollToField('demo-form_baz')}>Scroll to Baz</Button>
       </Form.Item>
 
       <Form.Item name="demo-form_foo" label="Foo" rules={[{ required: true }]}>
@@ -26,23 +20,6 @@ const App = () => {
 
       <Form.Item name="demo-form_baz" label="Baz" rules={[{ required: true }]}>
         <Input.TextArea placeholder="Please input baz" rows={6} />
-      </Form.Item>
-
-      <Form.Item label="Dragger">
-        <Form.Item
-          name="demo-form_dragger"
-          valuePropName="fileList"
-          getValueFromEvent={(e) => (Array.isArray(e) ? e : e?.fileList)}
-          noStyle
-        >
-          <Upload.Dragger name="files" action="/upload.do">
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">Support for a single or bulk upload.</p>
-          </Upload.Dragger>
-        </Form.Item>
       </Form.Item>
 
       <Form.Item>
