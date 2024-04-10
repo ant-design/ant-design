2b80fe83b42299bed47706667a0300e7abd74ddf
CooperHash <2597905281@qq.com>
2024-04-10 13:09:59 +0800

    fix

--- components/form/demo/component-token.tsx
+++ components/form/demo/component-token.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => (
           labelColonMarginInlineStart: 4,
           labelColonMarginInlineEnd: 12,
           itemMarginBottom: 18,
-          inlineItemMargin: 18,
+          inlineItemMarginBottom: 18,
         },
       },
     }}
