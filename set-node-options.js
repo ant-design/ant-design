0248c217dc650045a629ec1676b2381f43369c3c
GeorgeH <default_hyn@163.com>
2024-04-13 19:09:34 +0800

    update

--- set-node-options.js
+++ set-node-options.js
@@ -3,20 +3,11 @@ const childProcess = require('child_process');
 
 const totalMemory = Math.floor(os.totalmem() / (1024 * 1024));
 
-let maxOldSpaceSize;
 if (totalMemory <= 8192) {
-  maxOldSpaceSize = 4096;
-} else if (totalMemory <= 16384) {
-  maxOldSpaceSize = 4096;
-} else {
-  maxOldSpaceSize = 8192;
+  // setting NODE_OPTIONS
+  process.env.NODE_OPTIONS = '--max-old-space-size=4096';
+  // Execute project startup command
+  const args = process.argv.slice(2);
+
+  childProcess.execSync(` ${args.join(' ')}`, { stdio: 'inherit' });
 }
-
-// setting NODE_OPTIONS
-const nodeArgs = `--max-old-space-size=${maxOldSpaceSize}`;
-process.env.NODE_OPTIONS = nodeArgs;
-
-// Execute project startup command
-const args = process.argv.slice(2);
-
-childProcess.execSync(` ${args.join(' ')}`, { stdio: 'inherit' });
