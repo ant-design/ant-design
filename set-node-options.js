28c6eb5a0b8b948200b55a6b86a67877a6117f73
GeorgeH <default_hyn@163.com>
2024-04-13 17:17:40 +0800

    update

--- set-node-options.js
+++ set-node-options.js
@@ -12,11 +12,11 @@ if (totalMemory <= 8192) {
   maxOldSpaceSize = 8192;
 }
 
-// 设置 NODE_OPTIONS
+// setting NODE_OPTIONS
 const nodeArgs = `--max-old-space-size=${maxOldSpaceSize}`;
 process.env.NODE_OPTIONS = nodeArgs;
 
-// 执行项目启动命令
+// Execute project startup command
 const args = process.argv.slice(2);
 
 childProcess.execSync(` ${args.join(' ')}`, { stdio: 'inherit' });
