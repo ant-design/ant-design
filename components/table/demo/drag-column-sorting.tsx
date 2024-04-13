3dc4d0c5ced19dd5c80db14328c0d4090ae95ee6
GeorgeH <default_hyn@163.com>
2024-04-13 16:43:08 +0800

    docs(table): add drag column sorting demo

---
+++ components/table/demo/drag-column-sorting.tsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import type { DragEndEvent } from '@dnd-kit/core';
+import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { restrictToHorizontalAxis } from '@dnd-kit/modifiers';
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  SortableContext,
+  useSortable,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { Table } from 'antd';
+import type { TableColumnsType } from 'antd';
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const dataSource: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address:
+      'Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text Long text',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
+
+interface CellProps extends React.HTMLAttributes<HTMLTableCellElement> {
+  id: string;
+}
+
+const TableHeaderCell = (props: CellProps) => {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+    id: props.id,
+  });
+
+  const style: React.CSSProperties = {
+    ...props.style,
+    transform: CSS.Transform.toString(transform && { ...transform, scaleX: 1 }),
+    transition,
+    cursor: 'move',
+    ...(isDragging ? { position: 'relative', zIndex: 9999, userSelect: 'none' } : {}),
+  };
+
+  return <th {...props} ref={setNodeRef} style={style} {...attributes} {...listeners} />;
+};
+const baseColumns: TableColumnsType<DataType> = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+  },
+];
+const columnInit = baseColumns.map((column, i) => ({
+  ...column,
+  key: `${i}`,
+  onHeaderCell: () => ({
+    id: `${i}`,
+  }),
+}));
+
+const App: React.FC = () => {
+  const [columns, setColumns] = useState(columnInit);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        // https://docs.dndkit.com/api-documentation/sensors/pointer#activation-constraints
+        distance: 1,
+      },
+    }),
+  );
+
+  const onDragEnd = ({ active, over }: DragEndEvent) => {
+    if (active.id !== over?.id) {
+      setColumns((prev) => {
+        const activeIndex = prev.findIndex((i) => i.key === active.id);
+        const overIndex = prev.findIndex((i) => i.key === over?.id);
+        return arrayMove(prev, activeIndex, overIndex);
+      });
+    }
+  };
+
+  return (
+    <DndContext sensors={sensors} modifiers={[restrictToHorizontalAxis]} onDragEnd={onDragEnd}>
+      <SortableContext items={columns.map((i) => i.key)} strategy={horizontalListSortingStrategy}>
+        <Table
+          components={{
+            header: {
+              cell: TableHeaderCell,
+            },
+          }}
+          rowKey="key"
+          columns={columns}
+          dataSource={dataSource}
+        />
+      </SortableContext>
+    </DndContext>
+  );
+};
+
+export default App;
