374276d94cdd333d87f1e5e86ab3606f5fb4bd53
闲夕 <1075746765@qq.com>
2024-04-15 11:38:30 +0800

    fix: ColorPicker default clear value should change if set

--- components/color-picker/demo/base.tsx
+++ components/color-picker/demo/base.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { ColorPicker } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { ColorPicker, Form } from 'antd';
 
-const Demo = () => <ColorPicker defaultValue="#1677ff" />;
+const App = () => {
+  const [color, setColor] = useState<string>('');
+  useEffect(() => {
+    setTimeout(() => {
+      setColor('#1677ff');
+    }, 500);
+  }, []);
+  return <ColorPicker value={color} allowClear />;
+};
 
-export default Demo;
+export default App;
