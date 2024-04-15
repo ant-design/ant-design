a1a08f3b88b6c19df98a7a536fecf2c08d0dfe78
栗嘉男 <574980606@qq.com>
2024-04-15 15:37:32 +0800

    test: add test case

--- components/chat-box/__tests__/index.test.tsx
+++ components/chat-box/__tests__/index.test.tsx
@@ -14,4 +14,52 @@ describe('chat-box', () => {
     expect(element).toBeTruthy();
     expect(element).toMatchSnapshot();
   });
+
+  it('ChatBox support content', () => {
+    const { container } = render(<ChatBox content="hello world" />);
+    const element = container.querySelector<HTMLDivElement>('.ant-chat-box .ant-chat-box-content');
+    expect(element?.textContent).toBe('hello world');
+  });
+
+  it('ChatBox support contentRender', () => {
+    const { container } = render(
+      <ChatBox
+        content="test-contentRender"
+        contentRender={(content) => <span className="test-contentRender">{content}</span>}
+      />,
+    );
+    const element = container.querySelector<HTMLSpanElement>('.ant-chat-box .test-contentRender');
+    expect(element).toBeTruthy();
+    expect(element?.textContent).toBe('test-contentRender');
+  });
+
+  it('ChatBox support step', () => {
+    const { container } = render(<ChatBox step content="test" />);
+    const selectors = '.ant-chat-box .ant-chat-box-content';
+    expect(container.querySelector<HTMLDivElement>(selectors)).toHaveClass(
+      'ant-chat-box-content-cursorBlink',
+    );
+  });
+
+  it('ChatBox support avatar', () => {
+    const { container } = render(
+      <ChatBox avatar={<span className="test-avatar">avatar</span>} content="" />,
+    );
+    expect(container.querySelector<HTMLSpanElement>('.ant-chat-box .test-avatar')).toBeTruthy();
+  });
+
+  it('ChatBox support loading', () => {
+    const { container } = render(<ChatBox content="" loading />);
+    expect(
+      container.querySelector<HTMLSpanElement>('.ant-chat-box .ant-spin.ant-spin-spinning'),
+    ).toBeTruthy();
+  });
+
+  it('ChatBox support placement', () => {
+    const { container, rerender } = render(<ChatBox placement="start" content="" />);
+    const element = container.querySelector<HTMLDivElement>('.ant-chat-box');
+    expect(element).toHaveClass('ant-chat-box-start');
+    rerender(<ChatBox placement="end" content="" />);
+    expect(element).toHaveClass('ant-chat-box-end');
+  });
 });
