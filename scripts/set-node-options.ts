9ba577f3dfc0df3ec7d9efce814cdf468b2861a1
George H <default_hyn@163.com>
2024-04-15 16:27:47 +0800

    fix :fix npm start when memory more than 8192 (#48462)
    Co-authored-by: lijianan <574980606@qq.com>

--- scripts/set-node-options.ts
+++ scripts/set-node-options.ts
@@ -7,8 +7,8 @@ const totalMemory = Math.floor(os.totalmem() / (1024 * 1024));
 if (totalMemory <= 8192) {
   // setting NODE_OPTIONS
   process.env.NODE_OPTIONS = '--max-old-space-size=4096';
-  // Execute project startup command
-  const args: string[] = process.argv.slice(2);
-
-  childProcess.execSync(` ${args.join(' ')}`, { stdio: 'inherit' });
 }
+// Execute project startup command
+const args: string[] = process.argv.slice(2);
+
+childProcess.execSync(` ${args.join(' ')}`, { stdio: 'inherit' });
