582dabd6382fc44b60b7a6e0fa6fd489d3b41eeb
canhe077 <hecan077@gmail.com>
2024-05-10 11:55:15 +0800

    Update fields-name.tsx
    Signed-off-by: canhe077 <hecan077@gmail.com>

--- components/cascader/demo/fields-name.tsx
+++ components/cascader/demo/fields-name.tsx
@@ -10,8 +10,8 @@ interface Option {
 
 const options: Option[] = [
   {
-    code: 'zhejiang',
-    name: 'Zhejiang',
+    code: 'zhejiangzhejiangzhejiangzhejiang',
+    name: 'Zhejiangzhejiangzhejiangzhejiang',
     items: [
       {
         code: 'hangzhou',
