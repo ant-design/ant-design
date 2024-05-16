ff029b2d780db7cb66485bec7cf0dab455d4b4c9
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-05-16 09:10:15 +0900

    docs: add onchange event to catch time changed

--- components/time-picker/demo/needConfirm.tsx
+++ components/time-picker/demo/needConfirm.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import type { TimePickerProps } from 'antd';
 import { TimePicker, Space } from 'antd';
+
+const onChange: TimePickerProps['onChange'] = (time, timeString) => {
+  console.log(time, timeString);
+};
 
 const App: React.FC = () => (
   <Space direction="vertical" size={12}>
-    <TimePicker needConfirm />
-    <TimePicker needConfirm={false} />
+    <TimePicker onChange={onChange} needConfirm />
+    <TimePicker onChange={onChange} needConfirm={false} />
   </Space>
 );
 
