a59ac094aacb9d9d193a1203a856ab56bc28b943
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-05-17 16:41:46 +0900

    docs: delete needConfirm true demo in datePicker

--- components/time-picker/demo/need-confirm.tsx
+++ components/time-picker/demo/need-confirm.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import type { TimePickerProps } from 'antd';
-import { TimePicker, Space } from 'antd';
+import { TimePicker } from 'antd';
 
 const onChange: TimePickerProps['onChange'] = (time, timeString) => {
   console.log(time, timeString);
 };
 
-const App: React.FC = () => (
-  <Space direction="vertical" size={12}>
-    <TimePicker onChange={onChange} needConfirm />
-    <TimePicker onChange={onChange} needConfirm={false} />
-  </Space>
-);
+const App: React.FC = () => <TimePicker onChange={onChange} needConfirm={false} />;
 
 export default App;
