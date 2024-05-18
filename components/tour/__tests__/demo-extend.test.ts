6663911f237eb298ab1e185660a8fc14ea667649
二货机器人 <smith3816@gmail.com>
2024-05-18 14:10:34 +0800

    test: update snapshot

--- components/tour/__tests__/demo-extend.test.ts
+++ components/tour/__tests__/demo-extend.test.ts
@@ -1,3 +1,14 @@
 import { extendTest } from '../../../tests/shared/demoTest';
 
-extendTest('tour');
+jest.mock('../.', () => {
+  const OriReact = jest.requireActual('react');
+  const OriTour = jest.requireActual('../.').default;
+
+  const ProxyTour = OriReact.forwardRef((props: any, ref: any) =>
+    OriReact.createElement(OriTour, { ...props, open: true, ref }),
+  );
+
+  return ProxyTour;
+});
+
+extendTest('tour', { skip: ['render-panel.tsx'] });
