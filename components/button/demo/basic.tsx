1961eed8e17ee5db099cbb8b686228170a9c34d8
二货机器人 <smith3816@gmail.com>
2024-05-20 21:27:22 +0800

    test: for CI approve

--- components/button/demo/basic.tsx
+++ components/button/demo/basic.tsx
@@ -4,7 +4,7 @@ import { Button, Flex } from 'antd';
 const App: React.FC = () => (
   <Flex gap="small" wrap>
     <Button type="primary">Approve Test</Button>
-    <Button>Default Button</Button>
+    <Button>Failed again</Button>
     <Button type="dashed">Dashed Button</Button>
     <Button type="text">Text Button</Button>
     <Button type="link">Link Button</Button>
