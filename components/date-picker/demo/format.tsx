bfb6df066e070c4fd5c3978191644d4de27f3ec1
叶枫 <645381995@qq.com>
2024-05-28 14:00:56 +0800

    feat: test

--- components/date-picker/demo/format.tsx
+++ components/date-picker/demo/format.tsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
+import type { FormProps } from 'antd';
+import { Button, DatePicker, Form } from 'antd';
 import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-dayjs.extend(customParseFormat);
+type FieldType = {
+  date?: string;
+};
 
-const { RangePicker } = DatePicker;
-
-const dateFormat = 'YYYY/MM/DD';
-const weekFormat = 'MM/DD';
-const monthFormat = 'YYYY/MM';
-
-/** Manually entering any of the following formats will perform date parsing */
-const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY', 'DD-MM-YYYY', 'DD-MM-YY'];
-
-const customFormat: DatePickerProps['format'] = (value) =>
-  `custom format: ${value.format(dateFormat)}`;
-
-const customWeekStartEndFormat: DatePickerProps['format'] = (value) =>
-  `${dayjs(value).startOf('week').format(weekFormat)} ~ ${dayjs(value)
-    .endOf('week')
-    .format(weekFormat)}`;
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
 
 const App: React.FC = () => (
-  <Space direction="vertical" size={12}>
-    <DatePicker defaultValue={dayjs('2024/05/11', dateFormat)} format={dateFormat} />
-    <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} />
-    <DatePicker defaultValue={dayjs('2015/01', monthFormat)} format={monthFormat} picker="month" />
-    <DatePicker defaultValue={dayjs()} format={customWeekStartEndFormat} picker="week" />
-    <RangePicker
-      defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
-      format={dateFormat}
-    />
-    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={customFormat} />
-  </Space>
+  <Form
+    name="getValueProps"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600 }}
+    initialValues={{ date: '1715356800000' }}
+    onFinish={onFinish}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="Date"
+      name="date"
+      rules={[{ required: true }]}
+      getValueProps={(value) => ({ value: value && dayjs(Number(value)) })}
+      normalize={(value) => value && `${dayjs(value).valueOf()}`}
+    >
+      <DatePicker />
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
 );
 
 export default App;
