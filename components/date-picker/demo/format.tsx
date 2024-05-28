010671202e206ab846322e7e5ffb742954f0f2e7
叶枫 <645381995@qq.com>
2024-05-28 09:11:50 +0800

    feat: test

--- components/date-picker/demo/format.tsx
+++ components/date-picker/demo/format.tsx
@@ -25,7 +25,7 @@ const customWeekStartEndFormat: DatePickerProps['format'] = (value) =>
 
 const App: React.FC = () => (
   <Space direction="vertical" size={12}>
-    <DatePicker defaultValue={dayjs('2015/01/01', dateFormat)} format={dateFormat} />
+    <DatePicker defaultValue={dayjs('2024/05/11', dateFormat)} format={dateFormat} />
     <DatePicker defaultValue={dayjs('01/01/2015', dateFormatList[0])} format={dateFormatList} />
     <DatePicker defaultValue={dayjs('2015/01', monthFormat)} format={monthFormat} picker="month" />
     <DatePicker defaultValue={dayjs()} format={customWeekStartEndFormat} picker="week" />
