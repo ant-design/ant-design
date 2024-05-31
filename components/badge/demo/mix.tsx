760b1176aff0ac95446730264e7c6a9c6284bbdf
wuxh <wxh1220@gmail.com>
2024-05-31 11:31:45 +0800

    chore: update debug demo

--- components/badge/demo/mix.tsx
+++ components/badge/demo/mix.tsx
@@ -1,50 +1,78 @@
 import React from 'react';
-import { Avatar, Badge, Space } from 'antd';
+import { Avatar, Badge, Flex, Popover, Typography } from 'antd';
 
-const App: React.FC = () => (
-  <Space size="middle" wrap>
-    <Space size="middle" wrap>
-      <Badge count={5} status="success">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge count={5} status="warning">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge count={5} color="blue">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge count={5} color="#fa541c">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge dot status="success">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge dot status="warning">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge dot status="processing">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge dot color="blue">
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge dot color="#fa541c">
-        <Avatar shape="square" size="large" />
-      </Badge>
-    </Space>
+function getCombinations(...arrays: any[]) {
+  return arrays.reduce(
+    (combinations, array) =>
+      combinations.flatMap((combination: any) =>
+        array.map((element: any) => [...combination, element]),
+      ),
+    [[]],
+  );
+}
 
-    <Space size="middle" wrap>
-      <Badge count={0} showZero />
-      <Badge count={0} showZero color="blue" />
-      <Badge count={0} showZero color="#f0f" />
-      <Badge count={0} showZero>
-        <Avatar shape="square" size="large" />
-      </Badge>
-      <Badge count={0} showZero color="blue">
-        <Avatar shape="square" size="large" />
-      </Badge>
-    </Space>
-  </Space>
-);
+function genBadgeProps(obj: Record<string, any>) {
+  const keys: any = [];
+  const values: any = [];
+
+  Object.entries(obj).forEach(([key, value]) => {
+    keys.push(key);
+    values.push(value);
+  });
+
+  const combinations = getCombinations(...values);
+
+  return combinations.map((combination: any) => {
+    const props: Record<string, any> = {};
+    combination.forEach((value: any, index: number) => {
+      props[keys[index]] = value;
+    });
+    return props;
+  });
+}
+
+const App: React.FC = () => {
+  const [examplesProps] = React.useState(() =>
+    genBadgeProps({
+      children: [undefined, <Avatar key="avatar" shape="square" />],
+      dot: [true, false],
+      status: ['default', 'success', 'processing'],
+      count: [0, 5, 100],
+      color: ['#e7d', 'yellow'],
+      showZero: [true, false],
+    }),
+  );
+
+  return (
+    <Flex gap={64} wrap>
+      {examplesProps.map((props: any, index: number) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { children, ...restProps } = props;
+        return (
+          <Popover
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            content={
+              <Typography.Paragraph>
+                <pre>
+                  {Object.entries(restProps)
+                    .map(([key, value]) =>
+                      [key, typeof value === 'string' ? `"${value}"` : `{${value}}`].join('='),
+                    )
+                    .join('\n')}
+                </pre>
+              </Typography.Paragraph>
+            }
+            trigger={['hover']}
+          >
+            <div style={{ border: '1px solid #0ee', padding: 6 }}>
+              <Badge {...props} />
+            </div>
+          </Popover>
+        );
+      })}
+    </Flex>
+  );
+};
 
 export default App;
