df4f7e07eb3d2a9c7a990f9b746c7572c2095653
CodeCompilerConduct <1376397133guoli@gmail.com>
2024-06-11 19:07:29 +0930

    Add missing translations for Malay language (ms_MY)
    Signed-off-by: CodeCompilerConduct <1376397133guoli@gmail.com>

--- components/locale/ms_MY.ts
+++ components/locale/ms_MY.ts
@@ -5,6 +5,8 @@ import Calendar from '../calendar/locale/ms_MY';
 import DatePicker from '../date-picker/locale/ms_MY';
 import TimePicker from '../time-picker/locale/ms_MY';
 
+const typeTemplate = '${label} bukan ${type} jenis yang sah';
+
 const localeValues: Locale = {
   locale: 'ms-my',
   Pagination,
@@ -13,6 +15,9 @@ const localeValues: Locale = {
   Calendar,
   global: {
     placeholder: 'Sila pilih',
+  },
+  PageHeader: {
+    back: 'Kembali',
   },
   Text: {
     edit: 'Sunting',
@@ -24,12 +29,23 @@ const localeValues: Locale = {
     description: 'Tiada data',
   },
   Table: {
-    filterTitle: 'Cari dengan tajuk',
+    filterTitle: 'Saring',
     filterConfirm: 'OK',
-    filterReset: 'Menetapkan semula',
-    emptyText: 'Tiada data',
-    selectAll: 'Pilih semua',
-    selectInvert: 'Terbalikkan',
+    filterReset: 'Reset',
+    emptyText: 'Kosong',
+    selectAll: 'Pilih Semua',
+    selectInvert: 'Balikkan',
+    filterEmptyText: 'Tiada Saringan',
+    filterCheckall: 'Semak Semua',
+    filterSearchPlaceholder: 'Cari',
+    selectNone: 'Kosong Semua',
+    selectionAll: 'Semua Data',
+    sortTitle: 'Urutkan',
+    expand: 'Buka',
+    collapse: 'Tutup',
+    triggerDesc: 'Turun',
+    triggerAsc: 'Naik',
+    cancelSort: 'Batal Urut',
   },
   Modal: {
     okText: 'OK',
@@ -43,15 +59,18 @@ const localeValues: Locale = {
   Transfer: {
     titles: ['', ''],
     notFoundContent: 'Tidak dijumpai',
-    searchPlaceholder: 'Carian di sini',
+    searchPlaceholder: 'Carian',
     itemUnit: 'item',
     itemsUnit: 'item',
+    remove: 'Buang',
+    selectCurrent: 'Pilih Halaman Ini',
+    removeCurrent: 'Buang Dari Halaman Ini',
+    selectAll: 'Pilih Semua',
+    removeAll: 'Buang Semua',
+    selectInvert: 'Balik Pilihan',
   },
   Icon: {
     icon: 'ikon',
-  },
-  Select: {
-    notFoundContent: 'Tidak Dijumpai',
   },
   Upload: {
     uploading: 'Sedang memuat naik...',
@@ -59,6 +78,71 @@ const localeValues: Locale = {
     uploadError: 'Masalah muat naik',
     previewFile: 'Tengok fail',
     downloadFile: 'Muat turun fail',
+  },
+  Tour: {
+    Next: 'Seterusnya',
+    Previous: 'Sebelumnya',
+    Finish: 'Tamat',
+  },  
+  Form: {
+    optional: '(Opsional)',
+    defaultValidateMessages: {
+      default: 'Ralat pengesahan untuk ${label}',
+      required: 'Isi ${label}',
+      enum: '${label} mesti salah satu dari [${enum}]',
+      whitespace: '${label} tidak boleh kosong',
+      date: {
+        format: 'Format tarikh ${label} salah',
+        parse: '${label} tidak boleh jadi tarikh',
+        invalid: '${label} adalah tarikh tidak sah',
+      },
+      types: {
+        string: typeTemplate,
+        method: typeTemplate,
+        array: typeTemplate,
+        object: typeTemplate,
+        number: typeTemplate,
+        date: typeTemplate,
+        boolean: typeTemplate,
+        integer: typeTemplate,
+        float: typeTemplate,
+        regexp: typeTemplate,
+        email: typeTemplate,
+        url: typeTemplate,
+        hex: typeTemplate,
+      },
+      string: {
+        len: '${label} mesti ${len} aksara',
+        min: 'Min ${min} aksara',
+        max: 'Max ${max} aksara',
+        range: '${label} antara ${min}-${max} aksara',
+      },
+      number: {
+        len: '${label} sama dengan ${len}',
+        min: 'Min ${min}',
+        max: 'Max ${max}',
+        range: '${label} antara ${min}-${max}',
+      },
+      array: {
+        len: '${len} ${label}',
+        min: 'Min ${min} ${label}',
+        max: 'Max ${max} ${label}',
+        range: '${label} antara ${min}-${max}',
+      },
+      pattern: {
+        mismatch: '${label} tidak sesuai ${pattern}',
+      },
+    },
+  },
+  Image: {
+    preview: 'Pratonton',
+  },
+  QRCode: {
+    expired: 'Kod QR luput',
+    refresh: 'Segar Semula',
+  },
+  ColorPicker: {
+    presetEmpty: 'Tiada',
   },
 };
 
