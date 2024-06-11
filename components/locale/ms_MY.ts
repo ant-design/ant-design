90af9627360c0f40b19c773bbac6139a391df0be
CodeCompilerConduct <1376397133guoli@gmail.com>
2024-06-11 20:45:48 +0930

    Update ms_MY.ts, remove PageHeader
    Signed-off-by: CodeCompilerConduct <1376397133guoli@gmail.com>

--- components/locale/ms_MY.ts
+++ components/locale/ms_MY.ts
@@ -81,9 +81,6 @@ const localeValues: Locale = {
     copied: 'Berjaya menyalin',
     expand: 'Kembang',
   },
-  PageHeader: {
-    back: 'Kembali',
-  },
   Form: {
     optional: '(Opsional)',
     defaultValidateMessages: {
