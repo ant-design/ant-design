36b6be983194d88187944fc063919ca849c01dbf
栗嘉男 <574980606@qq.com>
2024-06-16 15:22:54 +0800

    fix: fix lint

--- components/_util/InputAddon.tsx
+++ components/_util/InputAddon.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { NoFormStyle } from '../form/context';
 import { NoCompactStyle } from '../space/Compact';
 
-const getInputAddon = (addon: React.ReactNode): React.ReactNode => addon ? (
-  <NoCompactStyle>
-    <NoFormStyle override status>
-      {addon}
-    </NoFormStyle>
-  </NoCompactStyle>
-) : null;
+const getInputAddon = (addon: React.ReactNode): React.ReactNode =>
+  addon ? (
+    <NoCompactStyle>
+      <NoFormStyle override status>
+        {addon}
+      </NoFormStyle>
+    </NoCompactStyle>
+  ) : null;
 
 export default getInputAddon;
