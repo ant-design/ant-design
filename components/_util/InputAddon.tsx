1badf9ef3e52ffa25063c6973eada2d0b6885cca
afc163 <afc163@gmail.com>
2024-06-16 15:18:10 +0800

    Update components/_util/InputAddon.tsx
    Signed-off-by: afc163 <afc163@gmail.com>

--- components/_util/InputAddon.tsx
+++ components/_util/InputAddon.tsx
@@ -3,17 +3,12 @@ import React from 'react';
 import { NoFormStyle } from '../form/context';
 import { NoCompactStyle } from '../space/Compact';
 
-const getInputAddon = (addon: React.ReactNode): React.ReactNode => {
-  if (!addon) {
-    return null;
-  }
-  return (
-    <NoCompactStyle>
-      <NoFormStyle override status>
-        {addon}
-      </NoFormStyle>
-    </NoCompactStyle>
-  );
-};
+const getInputAddon = (addon: React.ReactNode): React.ReactNode => addon ? (
+  <NoCompactStyle>
+    <NoFormStyle override status>
+      {addon}
+    </NoFormStyle>
+  </NoCompactStyle>
+) : null;
 
 export default getInputAddon;
