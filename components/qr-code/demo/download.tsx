452dbb093b19926db00b92e53e532d9417a38f9a
kiner-tang(文辉) <1127031143@qq.com>
2024-06-22 16:51:11 +0800

    feat: add download qrcode when render type is svg

--- components/qr-code/demo/download.tsx
+++ components/qr-code/demo/download.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, QRCode } from 'antd';
+import { Button, QRCode, Segmented, Space } from 'antd';
+import type { QRCodeProps } from 'antd';
 
-const downloadQRCode = () => {
+const downloadCanvasQRCode = () => {
   const canvas = document.getElementById('myqrcode')?.querySelector<HTMLCanvasElement>('canvas');
   if (canvas) {
     const url = canvas.toDataURL();
@@ -14,18 +15,53 @@ const downloadQRCode = () => {
   }
 };
 
-const App: React.FC = () => (
-  <div id="myqrcode">
-    <QRCode
-      value="https://ant.design/"
-      bgColor="#fff"
-      style={{ marginBottom: 16 }}
-      icon="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-    />
-    <Button type="primary" onClick={downloadQRCode}>
-      Download
-    </Button>
-  </div>
-);
+function downloadSVG(svgElement: SVGElement) {
+  // 获取SVG的XML字符串
+  const svgData = new XMLSerializer().serializeToString(svgElement);
+  // 创建Blob对象
+  const blob = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
+
+  // 创建可下载链接
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.download = 'QRCode.svg';
+  a.href = url;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+const downloadSvgQRCode = () => {
+  const svg = document.getElementById('myqrcode')?.querySelector<SVGElement>('svg');
+  downloadSVG(svg!);
+};
+
+const App: React.FC = () => {
+  const [renderType, setRenderType] = React.useState<QRCodeProps['type']>('canvas');
+  return (
+    <Space id="myqrcode" direction="vertical">
+      <Segmented
+        options={['canvas', 'svg']}
+        onChange={(val) => setRenderType(val as QRCodeProps['type'])}
+      />
+      <div>
+        <QRCode
+          type={renderType}
+          value="https://ant.design/"
+          bgColor="#fff"
+          style={{ marginBottom: 16 }}
+          icon="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+        />
+        <Button
+          type="primary"
+          onClick={renderType === 'canvas' ? downloadCanvasQRCode : downloadSvgQRCode}
+        >
+          Download
+        </Button>
+      </div>
+    </Space>
+  );
+};
 
 export default App;
