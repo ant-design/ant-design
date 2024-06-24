540812a77b31230b65e657ba25f1ea86819593ab
duqigit <916475145@qq.com>
2024-06-24 11:07:03 +0800

    update Table.filter.test.tsx

--- components/table/__tests__/Table.filter.test.tsx
+++ components/table/__tests__/Table.filter.test.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react';
 import type { ColumnGroupType, ColumnType, TableProps } from '..';
 import Table from '..';
 import { resetWarned } from '../../_util/warning';
-import { act, fireEvent, render, waitFor } from '../../../tests/utils';
+import { act, fireEvent, render, waitFakeTimer, waitFor } from '../../../tests/utils';
 import Button from '../../button';
 import ConfigProvider from '../../config-provider';
 import Input from '../../input';
@@ -2944,59 +2944,41 @@ describe('Table.filter', () => {
   });
 
   it('Fixed the problem that the custom global component did not take effect in the filter', async () => {
-    const customizeRenderEmpty = () => (
-      <div
-        style={{
-          textAlign: 'center',
-          color: '#ccc',
-        }}
-        className="renderEmpty"
-      >
-        <p>Data Not Found</p>
-      </div>
-    );
-
-    interface DataType {
-      key: string;
-      name: string;
-      age: number;
-    }
-
-    const columns: TableProps<DataType>['columns'] = [
-      {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        filters: [],
-      },
-      {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-      },
-    ];
-
+    const customizeRenderEmpty = jest.fn<string, any, any>(() => 'Data Not Found');
     const { container } = render(
       <ConfigProvider renderEmpty={customizeRenderEmpty}>
-        <Table columns={columns} />
+        <Table
+          columns={[
+            {
+              title: 'Name',
+              dataIndex: 'name',
+              key: 'name',
+              filters: [], // empty filters
+              onFilter: filterFn,
+            },
+            {
+              title: 'Age',
+              dataIndex: 'age',
+              key: 'age',
+            },
+          ]}
+        />
       </ConfigProvider>,
     );
 
-    // Ensure element exists
-    const dropdownTrigger = container.querySelector('.ant-dropdown-trigger');
-    expect(dropdownTrigger).toBeInTheDocument(); // Assert that dropdownTrigger exists
-
-    if (dropdownTrigger) {
-      fireEvent.click(dropdownTrigger);
-    }
-
-    // Wait for DOM updates after clicking
-    await waitFor(() => {
-      const emptyElement = document.querySelector('.ant-table-filter-dropdown .renderEmpty p');
-      expect(emptyElement).toBeInTheDocument();
-
-      // Verify the contents of the emptyElement
-      expect(emptyElement).toHaveTextContent('Data Not Found');
+    // Open Filter
+    fireEvent.click(container.querySelector('span.ant-dropdown-trigger')!);
+    act(() => {
+      jest.runAllTimers();
     });
+
+    await waitFakeTimer();
+
+    expect(container.querySelector('.ant-table-filter-dropdown')).toHaveTextContent(
+      'Data Not Found',
+    );
+
+    expect(customizeRenderEmpty).toHaveBeenCalled();
+    expect(customizeRenderEmpty.mock.calls[0][0]).toEqual('Table');
   });
 });
