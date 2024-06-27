b81c5cda34befb718bf716a822b8386a4e6487b4
二货爱吃白萝卜 <smith3816@gmail.com>
2024-06-27 16:54:57 +0800

    fix: typography ellipsis button style (#49620)

--- components/typography/Base/index.tsx
+++ components/typography/Base/index.tsx
@@ -384,15 +384,14 @@ const Base = React.forwardRef<HTMLElement, BlockProps>((props, ref) => {
     }
 
     return (
-      <button
-        type="button"
+      <TransButton
         key="expand"
         className={`${prefixCls}-${expanded ? 'collapse' : 'expand'}`}
-        onClick={(e) => onExpandClick(e, { expanded: !expanded })}
+        onClick={(e) => onExpandClick(e!, { expanded: !expanded })}
         aria-label={expanded ? textLocale.collapse : textLocale?.expand}
       >
         {typeof symbol === 'function' ? symbol(expanded) : symbol}
-      </button>
+      </TransButton>
     );
   };
 
