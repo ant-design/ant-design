5f17c447d697fa3a7db90a56006bdf7144eca156
二货爱吃白萝卜 <smith3816@gmail.com>
2024-07-01 15:13:05 +0800

    Revert "feat: Add `okButtonProps` to time picker (#49657)"
    This reverts commit 6c656c85f7a64cc1e4e70e511a9e8efed973cdce.

--- components/date-picker/generatePicker/generateSinglePicker.tsx
+++ components/date-picker/generatePicker/generateSinglePicker.tsx
@@ -3,8 +3,8 @@ import { forwardRef, useContext, useImperativeHandle } from 'react';
 import CalendarOutlined from '@ant-design/icons/CalendarOutlined';
 import ClockCircleOutlined from '@ant-design/icons/ClockCircleOutlined';
 import classNames from 'classnames';
-import type { PickerRef } from 'rc-picker';
 import RCPicker from 'rc-picker';
+import type { PickerRef } from 'rc-picker';
 import type { GenerateConfig } from 'rc-picker/lib/generate/index';
 import type { PickerMode } from 'rc-picker/lib/interface';
 
@@ -56,7 +56,6 @@ export default function generatePicker<DateType extends AnyObject>(
         status: customStatus,
         variant: customVariant,
         onCalendarChange,
-        okButtonProps,
         ...restProps
       } = props;
 
@@ -120,7 +119,7 @@ export default function generatePicker<DateType extends AnyObject>(
       const [mergedAllowClear, removeIcon] = useIcons(props, prefixCls);
 
       // ================== components ==================
-      const mergedComponents = useComponents(components, okButtonProps);
+      const mergedComponents = useComponents(components);
 
       // ===================== Size =====================
       const mergedSize = useSize((ctx) => customizeSize ?? compactSize ?? ctx);
