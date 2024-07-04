ca6badf3a5d8cabf9dcfdde4cc0ffa713e6d2d57
二货机器人 <smith3816@gmail.com>
2024-07-04 15:18:16 +0800

    docs: back of demo

--- components/drawer/demo/basic-right.tsx
+++ components/drawer/demo/basic-right.tsx
@@ -1,39 +1,28 @@
 import React, { useState } from 'react';
-import { ConfigProvider, Drawer, Popover } from 'antd';
+import { Button, Drawer } from 'antd';
 
 const App: React.FC = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
   const onClose = () => {
     setOpen(false);
   };
+
   return (
-    <div>
-      <ConfigProvider
-        theme={{
-          components: {
-            Select: {
-              zIndexPopup: 2147483698,
-            },
-            Popover: {
-              zIndexPopup: 2147483698,
-            },
-          },
-        }}
-      >
-        <Drawer
-          mask={false}
-          onClose={onClose}
-          open={open}
-          zIndex={2147483647}
-          width={428}
-          push={false}
-        >
-          <Popover title="Popover内容" open>
-            Popover内容
-          </Popover>
-        </Drawer>
-      </ConfigProvider>
-    </div>
+    <>
+      <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
+      <Drawer title="Basic Drawer" onClose={onClose} open={open}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
   );
 };
 
