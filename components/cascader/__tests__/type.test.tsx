3a4837af2f3bcac2705c061625c67efda6a7bc97
Lim Yang Zhi <36231658+YangZhi1@users.noreply.github.com>
2024-07-05 23:02:45 +0800

    Revert "fix: eslint type issue"
    This reverts commit 03146a9d692f07c726c81c2e061faef5a70ccd7f.

--- components/cascader/__tests__/type.test.tsx
+++ components/cascader/__tests__/type.test.tsx
@@ -86,15 +86,13 @@ describe('Cascader.typescript', () => {
   });
 
   it('multiple onChange', () => {
-    const { container } = render(
-      <Cascader multiple onChange={(values: (string | number | null)[][]) => values} />,
-    );
+    const { container } = render(<Cascader multiple onChange={(values) => values} />);
     expect(container).toBeTruthy();
   });
 
   it('props', () => {
     const list: { props?: CascaderAutoProps }[] = [
-      { props: { multiple: true, onChange: (value: (string | number | null)[][]) => value } },
+      { props: { multiple: true, onChange: (value) => value } },
       { props: { multiple: false, onChange: (value) => value } },
     ];
     expect(list).toBeTruthy();
