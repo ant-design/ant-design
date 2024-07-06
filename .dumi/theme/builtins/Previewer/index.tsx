a0d86aa28d72e709e03b80e743b2f93e2911efa3
afc163 <afc163@gmail.com>
2024-07-06 23:25:08 +0800

    chore: update code previewer

--- .dumi/theme/builtins/Previewer/index.tsx
+++ .dumi/theme/builtins/Previewer/index.tsx
@@ -1,11 +1,10 @@
 import React, { Suspense } from 'react';
 import { Alert, Skeleton } from 'antd';
 import { createStyles } from 'antd-style';
+import Previewer from './Previewer';
 import type { IPreviewerProps } from 'dumi';
 
 const { ErrorBoundary } = Alert;
-
-const Previewer = React.lazy(() => import('./Previewer'));
 
 const useStyle = createStyles(({ token, css }) => ({
   skeletonWrapper: css`
