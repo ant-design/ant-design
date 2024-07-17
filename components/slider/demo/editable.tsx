653df32ae0e917f009c5e8e178abb91bfbf7a1bf
二货机器人 <smith3816@gmail.com>
2024-07-17 19:26:37 +0800

    test: update snapshot

--- components/slider/demo/editable.tsx
+++ components/slider/demo/editable.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Slider } from 'antd';
 
 const App: React.FC = () => {
-  const [value, setValue] = React.useState([0, 100]);
+  const [value, setValue] = React.useState([20, 80]);
 
   return <Slider range={{ editable: true }} value={value} onChange={setValue} />;
 };
