ad70505a109016e1970bcdd45cff327982913fa4
afc163 <afc163@gmail.com>
2024-07-18 10:56:57 +0800

    fix: Pagination token.itemBg not working (#49933)

--- components/pagination/demo/component-token.tsx
+++ components/pagination/demo/component-token.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => (
           itemActiveBgDisabled: '#9c1515',
           itemInputBg: '#9c1515',
           miniOptionsSizeChangerTop: 0,
-          itemBg: '#333',
+          itemBg: '#b5f5ec',
         },
       },
     }}
