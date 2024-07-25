daff879c2dd95b2448686b6255f63767e50211d3
叶枫 <645381995@qq.com>
2024-07-25 17:46:23 +0800

    chore: update form+date demo

--- components/form/demo/getValueProps-normalize.tsx
+++ components/form/demo/getValueProps-normalize.tsx
@@ -13,32 +13,38 @@ const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
   console.log('Success:', values);
 };
 
-const App: React.FC = () => (
-  <Form
-    name="getValueProps"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ date: dateTimestamp }}
-    onFinish={onFinish}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Date"
-      name="date"
-      rules={[{ required: true }]}
-      getValueProps={(value) => ({ value: value && dayjs(Number(value)) })}
-      normalize={(value) => value && `${dayjs(value).valueOf()}`}
+const App: React.FC = () => {
+  const [form] = Form.useForm();
+  const date = Form.useWatch('date', form);
+  return (
+    <Form
+      form={form}
+      name="getValueProps"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ date: dateTimestamp }}
+      onFinish={onFinish}
+      autoComplete="off"
     >
-      <DatePicker />
-    </Form.Item>
+      <Form.Item<FieldType>
+        label="Date"
+        name="date"
+        rules={[{ required: true }]}
+        getValueProps={(value) => ({ value: value && dayjs(Number(value)) })}
+        normalize={(value) => value && `${dayjs(value).valueOf()}`}
+        extra={`date: ${date}`}
+      >
+        <DatePicker />
+      </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default App;
