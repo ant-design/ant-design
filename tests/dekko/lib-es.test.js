fbfeccb5bbf5aba14ab235b93e68817a83fc79d6
afc163 <afc163@gmail.com>
2024-08-11 11:13:13 +0800

    fix: lib es test

--- tests/dekko/lib-es.test.js
+++ tests/dekko/lib-es.test.js
@@ -4,9 +4,9 @@ const chalk = require('chalk');
 $('lib').isDirectory().hasFile('index.js').hasFile('index.d.ts');
 
 $('lib/*')
-  .filter((filename) => ['index.js', 'index.d.ts', '.map'].every((ext) => !filename.endsWith(ext)))
+  .filter((filename) => !['index.js', 'index.d.ts', '.map'].some((ext) => filename.endsWith(ext)))
   .isDirectory()
-  .filter((filename) => ['style', '_util', 'locale'].every((ext) => !filename.endsWith(ext)))
+  .filter((filename) => !['style', '_util', 'locale'].some((ext) => filename.endsWith(ext)))
   .hasFile('index.js')
   .hasFile('index.d.ts');
 
@@ -16,9 +16,9 @@ console.log(chalk.green('✨ `lib` directory is valid.'));
 $('es').isDirectory().hasFile('index.js').hasFile('index.d.ts');
 
 $('es/*')
-  .filter((filename) => ['index.js', 'index.d.ts', '.map'].every((ext) => !filename.endsWith(ext)))
+  .filter((filename) => !['index.js', 'index.d.ts', '.map'].some((ext) => filename.endsWith(ext)))
   .isDirectory()
-  .filter((filename) => ['style', '_util', 'locale'].every((ext) => !filename.endsWith(ext)))
+  .filter((filename) => !['style', '_util', 'locale'].some((ext) => filename.endsWith(ext)))
   .hasFile('index.js')
   .hasFile('index.d.ts');
 
