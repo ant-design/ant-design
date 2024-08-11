0fef8981fc8b8cb85b362daae7b69da284433fe1
lijianan <574980606@qq.com>
2024-08-11 10:58:13 +0800

    type(Table): TypeScript improvement (#50351)
    * type: Table TypeScript improvement
    
    * type: Table TypeScript improvement
    
    * type: Table TypeScript improvement
    
    * fix: fix demo
    
    * fix: fix demo
    
    * fix: fix type
    
    * fix: fix type
    
    * fix: fix type
    
    * fix: fix type

--- components/table/RcTable/index.tsx
+++ components/table/RcTable/index.tsx
@@ -1,13 +1,15 @@
 import { genTable } from 'rc-table';
 
+import type { AnyObject } from '../../_util/type';
 import type { InternalTableProps } from '../InternalTable';
 
 /**
  * Same as `rc-table` but we modify trigger children update logic instead.
  */
-export default genTable((prev, next) => {
-  const { _renderTimes: prevRenderTimes } = prev as InternalTableProps<any>;
-  const { _renderTimes: nextRenderTimes } = next as InternalTableProps<any>;
-
+const RcTable = genTable((prev, next) => {
+  const { _renderTimes: prevRenderTimes } = prev as Readonly<InternalTableProps<AnyObject>>;
+  const { _renderTimes: nextRenderTimes } = next as Readonly<InternalTableProps<AnyObject>>;
   return prevRenderTimes !== nextRenderTimes;
 });
+
+export default RcTable;
