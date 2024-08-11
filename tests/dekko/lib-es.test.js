9aa2cde4b0526950604ae8617116a0c6b837603a
afc163 <afc163@gmail.com>
2024-08-11 11:09:49 +0800

    fix: lib es test

--- tests/dekko/lib-es.test.js
+++ tests/dekko/lib-es.test.js
@@ -10,6 +10,9 @@ $('lib/*')
   .hasFile('index.js')
   .hasFile('index.d.ts');
 
+// eslint-disable-next-line no-console
+console.log(chalk.green('✨ `lib` directory is valid.'));
+
 $('es').isDirectory().hasFile('index.js').hasFile('index.d.ts');
 
 $('es/*')
@@ -20,4 +23,4 @@ $('es/*')
   .hasFile('index.d.ts');
 
 // eslint-disable-next-line no-console
-console.log(chalk.green('✨ `lib` directory is valid.'));
+console.log(chalk.green('✨ `es` directory is valid.'));
