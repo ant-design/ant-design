e789f56a42b9c431a81aa8f9cd9c147fbc053682
ice <1597834867@qq.com>
2024-08-29 16:38:42 +0800

    docs: responsive

--- components/button/demo/color-variant.tsx
+++ components/button/demo/color-variant.tsx
@@ -1,71 +1,76 @@
 import React from 'react';
 import { Button, ConfigProvider, Flex } from 'antd';
+import { useResponsive } from 'antd-style';
 
-const App: React.FC = () => (
-  <ConfigProvider componentSize="small">
-    <Flex vertical gap={18}>
-      <Flex gap={18} wrap>
-        <Button color="default" variant="solid">
-          Solid
-        </Button>
-        <Button color="default" variant="outlined">
-          Outlined
-        </Button>
-        <Button color="default" variant="dashed">
-          Dashed
-        </Button>
-        <Button color="default" variant="filled">
-          Filled
-        </Button>
-        <Button color="default" variant="text">
-          Text
-        </Button>
-        <Button color="default" variant="link">
-          Link
-        </Button>
+const App: React.FC = () => {
+  const { xxl } = useResponsive();
+
+  return (
+    <ConfigProvider componentSize={xxl ? 'middle' : 'small'}>
+      <Flex vertical gap={18}>
+        <Flex gap={18} wrap>
+          <Button color="default" variant="solid">
+            Solid
+          </Button>
+          <Button color="default" variant="outlined">
+            Outlined
+          </Button>
+          <Button color="default" variant="dashed">
+            Dashed
+          </Button>
+          <Button color="default" variant="filled">
+            Filled
+          </Button>
+          <Button color="default" variant="text">
+            Text
+          </Button>
+          <Button color="default" variant="link">
+            Link
+          </Button>
+        </Flex>
+        <Flex gap={18} wrap>
+          <Button color="primary" variant="solid">
+            Solid
+          </Button>
+          <Button color="primary" variant="outlined">
+            Outlined
+          </Button>
+          <Button color="primary" variant="dashed">
+            Dashed
+          </Button>
+          <Button color="primary" variant="filled">
+            Filled
+          </Button>
+          <Button color="primary" variant="text">
+            Text
+          </Button>
+          <Button color="primary" variant="link">
+            Link
+          </Button>
+        </Flex>
+        <Flex gap={18} wrap>
+          <Button color="danger" variant="solid">
+            Solid
+          </Button>
+          <Button color="danger" variant="outlined">
+            Outlined
+          </Button>
+          <Button color="danger" variant="dashed">
+            Dashed
+          </Button>
+          <Button color="danger" variant="filled">
+            Filled
+          </Button>
+          <Button color="danger" variant="text">
+            Text
+          </Button>
+          <Button color="danger" variant="link">
+            Link
+          </Button>
+        </Flex>
       </Flex>
-      <Flex gap={18} wrap>
-        <Button color="primary" variant="solid">
-          Solid
-        </Button>
-        <Button color="primary" variant="outlined">
-          Outlined
-        </Button>
-        <Button color="primary" variant="dashed">
-          Dashed
-        </Button>
-        <Button color="primary" variant="filled">
-          Filled
-        </Button>
-        <Button color="primary" variant="text">
-          Text
-        </Button>
-        <Button color="primary" variant="link">
-          Link
-        </Button>
-      </Flex>
-      <Flex gap={18} wrap>
-        <Button color="danger" variant="solid">
-          Solid
-        </Button>
-        <Button color="danger" variant="outlined">
-          Outlined
-        </Button>
-        <Button color="danger" variant="dashed">
-          Dashed
-        </Button>
-        <Button color="danger" variant="filled">
-          Filled
-        </Button>
-        <Button color="danger" variant="text">
-          Text
-        </Button>
-        <Button color="danger" variant="link">
-          Link
-        </Button>
-      </Flex>
-    </Flex>
-  </ConfigProvider>
-);
+    </ConfigProvider>
+  );
+};
 
 export default App;
