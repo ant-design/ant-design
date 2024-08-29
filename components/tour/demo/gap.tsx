046c54f7eed8cee6b1fff118236a973fa2eaa372
karos <chenwenfan@harmonycloud.cn>
2024-08-29 17:20:41 +0800

    docs:simplify demo of tour.gap

--- components/tour/demo/gap.tsx
+++ components/tour/demo/gap.tsx
@@ -2,26 +2,15 @@ import React, { useRef, useState } from 'react';
 import { Button, InputNumber, Space, Tour } from 'antd';
 import type { TourProps } from 'antd';
 
-type Refs = 'ref1' | 'ref2' | 'ref3' | 'ref4';
 const App: React.FC = () => {
   const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const ref3 = useRef(null);
-  const ref4 = useRef(null);
   const [radius, setRadius] = useState(8);
-
-  const target = useRef<Refs>('ref1');
   const [offsetX, setOffsetX] = useState(2);
   const [offsetY, setOffsetY] = useState(2);
   const [offset, setOffset] = useState(2);
   const [open, setOpen] = useState(false);
+  const [offsetDirection, setOffsetDirection] = useState<'both' | 'individual'>('individual');
 
-  const refMap = {
-    ref1: ref1.current,
-    ref2: ref2.current,
-    ref3: ref3.current,
-    ref4: ref4.current,
-  };
   const steps: TourProps['steps'] = [
     {
       title: 'Upload File',
@@ -32,54 +21,51 @@ const App: React.FC = () => {
           src="https://user-images.githubusercontent.com/5378891/197385811-55df8480-7ff4-44bd-9d43-a7dade598d70.png"
         />
       ),
-      target: () => refMap[target.current],
+      target: () => ref1.current,
     },
   ];
-  const clickOffsetBtn = (ref: Refs) => {
-    setOpen(true);
 
-    target.current = ref;
-  };
   const offsetGap =
-    target.current === 'ref2'
+    offsetDirection === 'both'
       ? { offset }
       : {
           offset: [offsetX, offsetY] as [number, number],
         };
   return (
-    <>
+    <div ref={ref1}>
       <Space>
-        <Button type="primary" onClick={() => clickOffsetBtn('ref1')}>
-          Begin Tour with radius
-        </Button>
-        <Button type="primary" onClick={() => clickOffsetBtn('ref2')}>
-          Begin Tour with offset
+        <Button type="primary" onClick={() => setOpen(true)}>
+          Begin Tour
         </Button>
       </Space>
-      <div style={{ marginTop: 12 }}>
-        <Button type="primary" style={{ marginRight: 12 }} onClick={() => clickOffsetBtn('ref3')}>
-          Begin Tour with offsetX
-        </Button>
-        <Button type="primary" onClick={() => clickOffsetBtn('ref4')}>
-          Begin Tour with offsetY
-        </Button>
-      </div>
       <Space style={{ display: 'flex', marginTop: 12 }} direction="vertical">
         <div>
           Radius:
-          <InputNumber value={radius} onChange={(val) => val && setRadius(val)} ref={ref1} />
+          <InputNumber value={radius} onChange={(val) => val && setRadius(val)} />
         </div>
         <div>
           offset:
-          <InputNumber value={offset} ref={ref2} onChange={(val) => val && setOffset(val)} />
+          <InputNumber
+            value={offset}
+            onChange={(val) => val && setOffset(val)}
+            onFocus={() => setOffsetDirection('both')}
+          />
         </div>
         <div>
           Horizontal offset:
-          <InputNumber value={offsetX} ref={ref3} onChange={(val) => val && setOffsetX(val)} />
+          <InputNumber
+            value={offsetX}
+            onChange={(val) => val && setOffsetX(val)}
+            onFocus={() => setOffsetDirection('individual')}
+          />
         </div>
         <div>
           Vertical offset:
-          <InputNumber value={offsetY} ref={ref4} onChange={(val) => val && setOffsetY(val)} />
+          <InputNumber
+            value={offsetY}
+            onChange={(val) => val && setOffsetY(val)}
+            onFocus={() => setOffsetDirection('individual')}
+          />
         </div>
       </Space>
       <Tour
@@ -88,7 +74,7 @@ const App: React.FC = () => {
         steps={steps}
         gap={{ ...offsetGap, radius }}
       />
-    </>
+    </div>
   );
 };
 
