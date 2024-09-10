44b2d10a60853f39aa2570245edf1ef5960212aa
栗嘉男 <574980606@qq.com>
2024-09-10 14:01:41 +0800

    fix: fix snap

--- components/splitter/demo/multiple.tsx
+++ components/splitter/demo/multiple.tsx
@@ -4,7 +4,7 @@ import { Flex, Splitter, Typography } from 'antd';
 const Desc: React.FC<Readonly<{ text?: string | number }>> = (props) => (
   <Flex justify="center" align="center" style={{ height: '100%' }}>
     <Typography.Title type="secondary" level={5} style={{ whiteSpace: 'nowrap' }}>
-      {props.text}
+      Panel {props.text}
     </Typography.Title>
   </Flex>
 );
