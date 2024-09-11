3c306b9c077e2fb31175e974f49b5c07a1310bcf
cuikaipeng <767184612@qq.com>
2024-09-11 17:22:02 +0800

    chore: Form export ValidateErrorEntity type

--- components/form/interface.ts
+++ components/form/interface.ts
@@ -1,3 +1,9 @@
-export type { InternalNamePath, NamePath, Store, StoreValue } from 'rc-field-form/lib/interface';
+export type {
+  InternalNamePath,
+  NamePath,
+  Store,
+  StoreValue,
+  ValidateErrorEntity,
+} from 'rc-field-form/lib/interface';
 export type { Options as ScrollOptions } from 'scroll-into-view-if-needed';
 export type FormLabelAlign = 'left' | 'right';
