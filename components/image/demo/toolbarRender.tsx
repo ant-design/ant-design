ff6d0d4b2f289bfa911c25ae391ec5aa97c421e0
madocto <hi.madocto@gmail.com>
2024-09-11 15:29:33 +0800

    feat(Image): add `onActive` to `toolbarRender`

--- components/image/demo/toolbarRender.tsx
+++ components/image/demo/toolbarRender.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
   DownloadOutlined,
+  LeftOutlined,
+  RightOutlined,
   RotateLeftOutlined,
   RotateRightOutlined,
   SwapOutlined,
@@ -10,51 +12,79 @@ import {
 } from '@ant-design/icons';
 import { Image, Space } from 'antd';
 
-const src = 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png';
+const imageList = [
+  'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg',
+];
 
 // you can download flipped and rotated image
 // https://codesandbox.io/s/zi-ding-yi-gong-ju-lan-antd-5-7-0-forked-c9jvmp
-const onDownload = (imgUrl: string) => {
-  fetch(imgUrl)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const url = URL.createObjectURL(new Blob([blob]));
-      const link = document.createElement<'a'>('a');
-      link.href = url;
-      link.download = 'image.png';
-      document.body.appendChild(link);
-      link.click();
-      URL.revokeObjectURL(url);
-      link.remove();
-    });
-};
+const App: React.FC = () => {
+  const [current, setCurrent] = React.useState(0);
 
-const App: React.FC = () => (
-  <Image
-    width={200}
-    src={src}
-    preview={{
-      toolbarRender: (
-        _,
-        {
-          image: { url },
-          transform: { scale },
-          actions: { onFlipY, onFlipX, onRotateLeft, onRotateRight, onZoomOut, onZoomIn, onReset },
+  // or you can download flipped and rotated image
+  // https://codesandbox.io/s/zi-ding-yi-gong-ju-lan-antd-5-7-0-forked-c9jvmp
+  const onDownload = () => {
+    const url = imageList[current];
+    const suffix = url.slice(url.lastIndexOf('.'));
+    const filename = Date.now() + suffix;
+
+    fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const blobUrl = URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        URL.revokeObjectURL(blobUrl);
+        link.remove();
+      });
+  };
+
+  return (
+    <Image.PreviewGroup
+      preview={{
+        toolbarRender: (
+          _,
+          {
+            transform: { scale },
+            actions: {
+              onActive,
+              onFlipY,
+              onFlipX,
+              onRotateLeft,
+              onRotateRight,
+              onZoomOut,
+              onZoomIn,
+              onReset,
+            },
+          },
+        ) => (
+          <Space size={12} className="toolbar-wrapper">
+            <LeftOutlined onClick={() => onActive?.(-1)} />
+            <RightOutlined onClick={() => onActive?.(1)} />
+            <DownloadOutlined onClick={onDownload} />
+            <SwapOutlined rotate={90} onClick={onFlipY} />
+            <SwapOutlined onClick={onFlipX} />
+            <RotateLeftOutlined onClick={onRotateLeft} />
+            <RotateRightOutlined onClick={onRotateRight} />
+            <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
+            <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
+            <UndoOutlined onClick={onReset} />
+          </Space>
+        ),
+        onChange: (index) => {
+          setCurrent(index);
         },
-      ) => (
-        <Space size={12} className="toolbar-wrapper">
-          <DownloadOutlined onClick={() => onDownload(url)} />
-          <SwapOutlined rotate={90} onClick={onFlipY} />
-          <SwapOutlined onClick={onFlipX} />
-          <RotateLeftOutlined onClick={onRotateLeft} />
-          <RotateRightOutlined onClick={onRotateRight} />
-          <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
-          <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
-          <UndoOutlined onClick={onReset} />
-        </Space>
-      ),
-    }}
-  />
-);
+      }}
+    >
+      {imageList.map((item) => (
+        <Image key={item} src={item} width={200} />
+      ))}
+    </Image.PreviewGroup>
+  );
+};
 
 export default App;
