bf3ff9fe3e5a4b1bae09427638887e75b0880e33
二货机器人 <smith3816@gmail.com>
2024-09-12 11:32:14 +0800

    docs: back of demo

--- components/app/demo/basic.tsx
+++ components/app/demo/basic.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
-import { App, Button, Select } from 'antd';
+import { App, Button, Space } from 'antd';
 
-const TheModal = () => {
-  const { modal } = App.useApp();
+// Sub page
+const MyPage = () => {
+  const { message, modal, notification } = App.useApp();
+
+  const showMessage = () => {
+    message.success('Success!');
+  };
+
+  const showModal = () => {
+    modal.warning({
+      title: 'This is a warning message',
+      content: 'some messages...some messages...',
+    });
+  };
+
+  const showNotification = () => {
+    notification.info({
+      message: 'Notification topLeft',
+      description: 'Hello, Ant Design!!',
+      placement: 'topLeft',
+    });
+  };
 
   return (
-    <Button type="primary" onClick={() => modal.confirm({ content: <Select /> })}>
-      Open Modal
-    </Button>
+    <Space wrap>
+      <Button type="primary" onClick={showMessage}>
+        Open message
+      </Button>
+      <Button type="primary" onClick={showModal}>
+        Open modal
+      </Button>
+      <Button type="primary" onClick={showNotification}>
+        Open notification
+      </Button>
+    </Space>
   );
 };
 
-const ReactApp = () => (
+// Entry component
+export default () => (
   <App>
-    <TheModal />
+    <MyPage />
   </App>
 );
-
-export default ReactApp;
