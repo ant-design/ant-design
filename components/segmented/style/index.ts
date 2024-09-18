2855ea6826ac44adcde14f28d245ea2feb138401
thinkasany <480968828@qq.com>
2024-09-18 17:13:01 +0800

    fix: token

--- components/segmented/style/index.ts
+++ components/segmented/style/index.ts
@@ -126,7 +126,7 @@ const genSegmentedStyle: GenerateStyle<SegmentedToken> = (token: SegmentedToken)
         [`${componentCls}-thumb`]: {
           width: '100%',
           height: 0,
-          padding: '0 4px',
+          padding: `0 ${unit(token.paddingXXS)}`,
         },
       },
 
