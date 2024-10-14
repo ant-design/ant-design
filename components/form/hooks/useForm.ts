015a655ad43e65116e1d099628ac1c295ee5d12d
nathanlao <gla93@sfu.ca>
2024-10-13 22:26:57 -0700

    feat: support focus when scrollToFirstError in form

--- components/form/hooks/useForm.ts
+++ components/form/hooks/useForm.ts
@@ -9,6 +9,7 @@ import { getFieldId, toArray } from '../util';
 
 export interface FormInstance<Values = any> extends RcFormInstance<Values> {
   scrollToField: (name: NamePath, options?: ScrollOptions) => void;
+  focusField: (name: NamePath) => void;
   /** @internal: This is an internal usage. Do not use in your prod */
   __INTERNAL__: {
     /** No! Do not use this in your code! */
@@ -65,6 +66,13 @@ export default function useForm<Values = any>(form?: FormInstance<Values>): [For
               block: 'nearest',
               ...options,
             } as any);
+          }
+        },
+        focusField: (name: NamePath) => {
+          const node = getFieldDOMNode(name, wrapForm);
+
+          if (node) {
+            node.focus();
           }
         },
         getFieldInstance: (name: NamePath) => {
