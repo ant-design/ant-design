a7770fc2f53eaa21abf795a2ac69dbf1aceeb9c0
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-10-18 12:21:35 +0800

    test: add unit test

--- components/table/__tests__/Table.filter.test.tsx
+++ components/table/__tests__/Table.filter.test.tsx
@@ -3099,4 +3099,53 @@ describe('Table.filter', () => {
       expect(container.querySelector('.ant-table-filter-dropdown')!.childNodes).toHaveLength(1);
     });
   });
+
+  // https://github.com/ant-design/ant-design/issues/51151#issuecomment-2419116749
+  describe('should support filterDropdownProps', () => {
+    it('dropdownRender', () => {
+      const dropdownRender = jest.fn((node) => (
+        <>
+          {node}
+          <span>Foo</span>
+        </>
+      ));
+
+      const { container, getByText } = render(
+        createTable({
+          columns: [
+            {
+              ...column,
+              filterDropdownProps: {
+                dropdownRender,
+              },
+            },
+          ],
+        }),
+      );
+
+      fireEvent.click(container.querySelector('.ant-dropdown-trigger')!);
+      expect(dropdownRender).toHaveBeenCalled();
+      expect(dropdownRender.mock.calls[0][0]).toMatchSnapshot();
+      expect(getByText('Foo')).toBeTruthy();
+    });
+
+    // https://github.com/ant-design/ant-design/issues/51151
+    it('placement', () => {
+      const { container } = render(
+        createTable({
+          columns: [
+            {
+              ...column,
+              filterDropdownProps: {
+                placement: 'topLeft',
+              },
+            },
+          ],
+        }),
+      );
+
+      fireEvent.click(container.querySelector('.ant-dropdown-trigger')!);
+      expect(container.querySelector('.ant-dropdown-placement-topLeft')).toBeTruthy();
+    });
+  });
 });
