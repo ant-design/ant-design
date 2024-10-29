6aaae7bb75cb54e673b001d83f61453c749664df
yoyo837 <yoyo837@hotmail.com>
2024-10-29 15:59:12 +0800

    event should be optional

--- components/upload/interface.ts
+++ components/upload/interface.ts
@@ -77,7 +77,7 @@ export type ItemRender<T = any> = (
   fileList: Array<UploadFile<T>>,
   actions: {
     download: () => void;
-    preview: () => void;
+    preview: (e?: React.SyntheticEvent<HTMLElement>) => void;
     remove: () => void;
   },
 ) => React.ReactNode;
