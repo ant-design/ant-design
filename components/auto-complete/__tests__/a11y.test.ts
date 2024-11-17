8cffe0958a4310b393f38416ae9802b444eb84b5
小豪 <1844749591@qq.com>
2024-11-17 23:04:48 +0800

    test: add a11y test for autocomplete demo

--- components/auto-complete/__tests__/a11y.test.ts
+++ components/auto-complete/__tests__/a11y.test.ts
@@ -1,6 +1,8 @@
 import accessibilityDemoTest from '../../../tests/shared/accessibilityTest';
 
 describe('auto-complete demo a11y', () => {
-  // wait for fix
-  accessibilityDemoTest('auto-complete', { skip: ['custom.tsx'] });
+  accessibilityDemoTest('auto-complete', {
+    disabledRules: ['label'],
+    skip: ['render-panel.tsx', 'custom.tsx'],
+  });
 });
