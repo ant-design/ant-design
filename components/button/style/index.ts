63bbf0a36ebd096190605dcff8d3b2ee7fcfc121
Oyster Lee <oysterd3@gmail.com>
2024-11-18 21:35:59 +0800

    refactor(button): use light color as box shadow

--- components/button/style/index.ts
+++ components/button/style/index.ts
@@ -250,7 +250,7 @@ const genPresetColorStyle: GenerateStyle<ButtonToken, CSSObject> = (token) => {
     const lightHoverColor = token[`${colorKey}2`];
     const lightBorderColor = token[`${colorKey}3`];
     const activeColor = token[`${colorKey}7`];
-    const boxShadow = `0 ${token.controlOutlineWidth} 0 ${token.controlOutline}`;
+    const boxShadow = `0 ${token.controlOutlineWidth} 0 ${token[`${colorKey}1`]}`;
 
     return {
       ...prev,
