26f9922ed5507b98781135c0340afd368d8141fd
Oyster Lee <oysterd3@gmail.com>
2024-11-23 17:16:16 +0800

    refactor(masonry): update props and use transform

--- components/masonry/demo/basic.tsx
+++ components/masonry/demo/basic.tsx
@@ -5,19 +5,13 @@ import DemoItem from './DemoItem';
 
 const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
 
-const Demo: React.FC = () => {
-  return (
-    <Masonry columns={4} gutter={16}>
-      {heights.map((height, index) => {
-        const key = `item-${index}`;
-        return (
-          <DemoItem key={key} height={height}>
-            {index + 1}
-          </DemoItem>
-        );
-      })}
-    </Masonry>
-  );
+const items = heights.map((height, index) => ({
+  key: `item-${index}`,
+  render: () => <DemoItem height={height}>{index + 1}</DemoItem>,
+}));
+
+const Basic: React.FC = () => {
+  return <Masonry columns={4} gutter={16} items={items} />;
 };
 
-export default Demo;
+export default Basic;
--- components/masonry/demo/responsive.tsx
+++ components/masonry/demo/responsive.tsx
@@ -5,19 +5,15 @@ import DemoItem from './DemoItem';
 
 const heights = [120, 45, 85, 160, 95, 140, 75, 110, 65, 130, 90, 145, 55, 100, 80];
 
-const Demo: React.FC = () => {
+const Responsive: React.FC = () => {
+  const items = heights.map((height, index) => ({
+    key: `item-${index}`,
+    render: () => <DemoItem height={height}>{index + 1}</DemoItem>,
+  }));
+
   return (
-    <Masonry columns={{ xs: 1, sm: 2, md: 3 }} gutter={{ xs: 8, sm: 12, md: 16 }}>
-      {heights.map((height, index) => {
-        const key = `item-${index}`;
-        return (
-          <DemoItem key={key} height={height}>
-            {index + 1}
-          </DemoItem>
-        );
-      })}
-    </Masonry>
+    <Masonry columns={{ xs: 1, sm: 2, md: 3 }} gutter={{ xs: 8, sm: 12, md: 16 }} items={items} />
   );
 };
 
-export default Demo;
+export default Responsive;
