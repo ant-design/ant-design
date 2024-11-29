4f90d1cb5f96d772e385eff3018722f5fe7cee1d
Oyster Lee <oysterd3@gmail.com>
2024-11-29 15:06:14 +0800

    refactor(button): remove svg

--- components/button/demo/debug-icon.tsx
+++ components/button/demo/debug-icon.tsx
@@ -56,24 +56,6 @@ const App: React.FC = () => {
             <Button>One</Button>
             <Button>Two</Button>
             <Button icon={<SearchOutlined />}>Search</Button>
-            <Button
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-square"
-                >
-                  <rect width="18" height="18" x="3" y="3" rx="2" />
-                </svg>
-              }
-            />
             <Button icon={<SearchOutlined />} />
           </div>
         </Flex>
