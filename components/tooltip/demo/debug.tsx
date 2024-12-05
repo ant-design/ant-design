58a10401dc4aa9caa5c46ee57da1c38f3ed94074
二货机器人 <smith3816@gmail.com>
2024-12-05 12:02:11 +0800

    test: update snapshot

--- components/tooltip/demo/debug.tsx
+++ components/tooltip/demo/debug.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Button, Tooltip } from 'antd';
+import { Button, Flex, Tooltip } from 'antd';
 
 const App: React.FC = () => (
-  <Tooltip
-    open
-    title="Thanks for using antd. Have a nice day !"
-    arrow={{ pointAtCenter: true }}
-    placement="topLeft"
-  >
-    <Button>Point at center</Button>
-  </Tooltip>
+  <Flex vertical gap={72} align="flex-start">
+    <span />
+    <Tooltip
+      open
+      title="Thanks for using antd. Have a nice day !"
+      arrow={{ pointAtCenter: true }}
+      placement="topLeft"
+    >
+      <Button>Point at center</Button>
+    </Tooltip>
+    <Tooltip open title="." placement="topLeft">
+      <Button>Min Width</Button>
+    </Tooltip>
+  </Flex>
 );
 
 export default App;
