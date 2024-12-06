ca5be165a41ab554d5b9b10f5223c77142414973
afc163 <afc163@gmail.com>
2024-12-06 14:39:26 +0800

    chore: fix link button style and behavior

--- .dumi/theme/common/Link.tsx
+++ .dumi/theme/common/Link.tsx
@@ -1,5 +1,5 @@
 import type { MouseEvent, MouseEventHandler } from 'react';
-import React, { useMemo } from 'react';
+import React, { useMemo, forwardRef } from 'react';
 import { Link as DumiLink, useLocation, useAppData, useNavigate } from 'dumi';
 
 export interface LinkProps {
@@ -11,43 +11,46 @@ export interface LinkProps {
   children?: React.ReactNode;
 }
 
-const Link: React.FC<LinkProps> = ({ component, children, to, ...rest }) => {
-  const { pathname } = useLocation();
-  const { preloadRoute } = useAppData();
-  const navigate = useNavigate();
-  const href = useMemo<string>(() => {
-    if (typeof to === 'object') {
-      return `${to.pathname || pathname}${to.search || ''}${to.hash || ''}`;
-    }
-    return to;
-  }, [to]);
-  const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
-    rest.onClick?.(e);
-    if (!href?.startsWith('http')) {
-      // Should support open in new tab
-      if (!e.metaKey && !e.ctrlKey && !e.shiftKey) {
-        e.preventDefault();
-        navigate(href);
+const Link = forwardRef<HTMLAnchorElement, React.PropsWithChildren<LinkProps>>(
+  ({ component, children, to, ...rest }, ref) => {
+    const { pathname } = useLocation();
+    const { preloadRoute } = useAppData();
+    const navigate = useNavigate();
+    const href = useMemo<string>(() => {
+      if (typeof to === 'object') {
+        return `${to.pathname || pathname}${to.search || ''}${to.hash || ''}`;
       }
+      return to;
+    }, [to]);
+    const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
+      rest.onClick?.(e);
+      if (!href?.startsWith('http')) {
+        // Should support open in new tab
+        if (!e.metaKey && !e.ctrlKey && !e.shiftKey) {
+          e.preventDefault();
+          navigate(href);
+        }
+      }
+    };
+    if (component) {
+      return React.createElement(
+        component,
+        {
+          ...rest,
+          ref,
+          href,
+          onClick,
+          onMouseEnter: () => preloadRoute?.(href),
+        },
+        children,
+      );
     }
-  };
-  if (component) {
-    return React.createElement(
-      component,
-      {
-        ...rest,
-        href,
-        onClick,
-        onMouseEnter: () => preloadRoute?.(href),
-      },
-      children,
+    return (
+      <DumiLink ref={ref} {...rest} to={href} prefetch>
+        {children}
+      </DumiLink>
     );
-  }
-  return (
-    <DumiLink {...rest} to={to} prefetch>
-      {children}
-    </DumiLink>
-  );
-};
+  },
+);
 
 export default Link;
