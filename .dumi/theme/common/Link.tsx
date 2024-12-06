6e6bdc8a972c64e1383b47029e2388eca3e82c00
afc163 <afc163@gmail.com>
2024-12-06 14:32:36 +0800

    chore: fix link button style and preload behavior

--- .dumi/theme/common/Link.tsx
+++ .dumi/theme/common/Link.tsx
@@ -1,6 +1,6 @@
-import type { MouseEventHandler } from 'react';
-import React from 'react';
-import { Link as DumiLink } from 'dumi';
+import type { MouseEvent, MouseEventHandler } from 'react';
+import React, { useMemo } from 'react';
+import { Link as DumiLink, useLocation, useAppData, useNavigate } from 'dumi';
 
 export interface LinkProps {
   to: string | { pathname?: string; search?: string; hash?: string };
@@ -11,6 +11,43 @@ export interface LinkProps {
   children?: React.ReactNode;
 }
 
-const Link: React.FC<LinkProps> = (props) => <DumiLink {...props} prefetch />;
+const Link: React.FC<LinkProps> = ({ component, children, to, ...rest }) => {
+  const { pathname } = useLocation();
+  const { preloadRoute } = useAppData();
+  const navigate = useNavigate();
+  const href = useMemo<string>(() => {
+    if (typeof to === 'object') {
+      return `${to.pathname || pathname}${to.search || ''}${to.hash || ''}`;
+    }
+    return to;
+  }, [to]);
+  const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    rest.onClick?.(e);
+    if (!href?.startsWith('http')) {
+      // Should support open in new tab
+      if (!e.metaKey && !e.ctrlKey && !e.shiftKey) {
+        e.preventDefault();
+        navigate(href);
+      }
+    }
+  };
+  if (component) {
+    return React.createElement(
+      component,
+      {
+        ...rest,
+        href,
+        onClick,
+        onMouseEnter: () => preloadRoute?.(href),
+      },
+      children,
+    );
+  }
+  return (
+    <DumiLink {...rest} to={to} prefetch>
+      {children}
+    </DumiLink>
+  );
+};
 
 export default Link;
