9dd55f5f4b65d8207db66891db80bd2657fadd01
小豪 <1844749591@qq.com>
2024-12-07 17:54:28 +0800

    fix: lint fix

--- components/tabs/style/index.ts
+++ components/tabs/style/index.ts
@@ -944,7 +944,7 @@ const genTabsStyle: GenerateStyle<TabsToken> = (token: TabsToken): CSSObject => 
             _skip_check_: true,
             value: cardGutter,
           },
-          padding: `${unit(token.paddingXXS * 1.5)} ${unit(token.paddingXS)}`,
+          padding: `${token.paddingXXS * 1.5}px ${token.paddingXS}px`,
           background: 'transparent',
           border: `${unit(token.lineWidth)} ${token.lineType} ${colorBorderSecondary}`,
           borderRadius: `${unit(token.borderRadiusLG)} ${unit(token.borderRadiusLG)} 0 0`,
