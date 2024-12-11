75670b9ca88c8bc1b650a1b88225e2ec62109694
二货机器人 <smith3816@gmail.com>
2024-12-11 18:30:42 +0800

    chore: rest of it

--- components/config-provider/UnstableContext.tsx
+++ components/config-provider/UnstableContext.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { render, unmount } from 'rc-util/lib/React/render';
 
 export type UnmountType = () => Promise<void>;
-export type RenderType = (node: React.ReactElement, container: HTMLElement) => UnmountType;
+export type RenderType = (
+  node: React.ReactElement,
+  container: Element | DocumentFragment,
+) => UnmountType;
 
 const defaultReactRender: RenderType = (node, container) => {
   render(node, container);
@@ -15,7 +18,8 @@ let unstableRender: RenderType = defaultReactRender;
 
 /**
  * @deprecated Set React render function for compatible usage.
- * This is internal usage only. And will be removed in next major version.
+ * This is internal usage only compatible with React 19.
+ * And will be removed in next major version.
  */
 export function unstableSetRender(render: RenderType) {
   unstableRender = render;
