f3fc7738c176ac2e590cc8ce6797756144d1e7a6
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-01-02 19:32:33 +0800

    Revert "fix(layout): enhance Sider zero-width-trigger selector specificity (#51936)"
    This reverts commit a5dc4069b125fdc9ca9b180bc312e9d779d3d3ef.

--- components/layout/style/sider.ts
+++ components/layout/style/sider.ts
@@ -69,7 +69,7 @@ const genSiderStyle: GenerateStyle<LayoutToken, CSSObject> = (token) => {
         transition: `all ${motionDurationMid}`,
       },
 
-      [`${antCls}-layout &-zero-width`]: {
+      '&-zero-width': {
         '> *': {
           overflow: 'hidden',
         },
