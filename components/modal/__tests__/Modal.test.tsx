8087523e1de838f7f69d2783db7dbd0dc8dc8599
kiner-tang（星河） <1127031143@qq.com>
2025-01-07 14:20:33 +0800

    feat: Retire deprecated api for modal (#52237)

--- components/modal/__tests__/Modal.test.tsx
+++ components/modal/__tests__/Modal.test.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 
 import type { ModalProps } from '..';
 import Modal from '..';
-import { resetWarned } from '../../_util/warning';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import { createEvent, fireEvent, render } from '../../../tests/utils';
@@ -119,20 +118,6 @@ describe('Modal', () => {
     expect(
       (container.querySelectorAll('.ant-modal')[0] as HTMLDivElement).style.transformOrigin,
     ).toBe('100px 100px');
-  });
-
-  it('deprecated warning', () => {
-    resetWarned();
-    const errSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-
-    render(<Modal visible />);
-    expect(errSpy).toHaveBeenCalledWith(
-      'Warning: [antd: Modal] `visible` is deprecated. Please use `open` instead.',
-    );
-
-    expect(document.querySelector('.ant-modal')).toBeTruthy();
-
-    errSpy.mockRestore();
   });
 
   it('should not render footer if null', () => {
