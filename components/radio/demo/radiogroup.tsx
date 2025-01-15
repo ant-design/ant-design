ecf0e67d329b948369cf5fd677bf9fa68a0ed933
栗嘉男 <574980606@qq.com>
2025-01-15 23:39:54 +0800

    demo: update Radio demo

--- components/radio/demo/radiogroup.tsx
+++ components/radio/demo/radiogroup.tsx
@@ -6,17 +6,20 @@ const App: React.FC = () => {
   const [value, setValue] = useState(1);
 
   const onChange = (e: RadioChangeEvent) => {
-    console.log('radio checked', e.target.value);
     setValue(e.target.value);
   };
 
   return (
-    <Radio.Group onChange={onChange} value={value}>
-      <Radio value={1}>A</Radio>
-      <Radio value={2}>B</Radio>
-      <Radio value={3}>C</Radio>
-      <Radio value={4}>D</Radio>
-    </Radio.Group>
+    <Radio.Group
+      onChange={onChange}
+      value={value}
+      options={[
+        { value: 1, label: <span>A</span> },
+        { value: 2, label: <span>B</span> },
+        { value: 3, label: <span>C</span> },
+        { value: 4, label: <span>D</span> },
+      ]}
+    />
   );
 };
 
