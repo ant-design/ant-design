a6db88a0081ffaf25a5630489b56f6c18966cb47
栗嘉男 <574980606@qq.com>
2025-01-16 15:27:32 +0800

    fix: fix

--- components/radio/demo/radiogroup.tsx
+++ components/radio/demo/radiogroup.tsx
@@ -6,7 +6,7 @@ import {
   PieChartOutlined,
 } from '@ant-design/icons';
 import type { RadioChangeEvent } from 'antd';
-import { Radio } from 'antd';
+import { Flex, Radio } from 'antd';
 
 const App: React.FC = () => {
   const [value, setValue] = useState(1);
@@ -20,10 +20,42 @@ const App: React.FC = () => {
       onChange={onChange}
       value={value}
       options={[
-        { value: 1, label: <LineChartOutlined style={{ fontSize: 18 }} /> },
-        { value: 2, label: <DotChartOutlined style={{ fontSize: 18 }} /> },
-        { value: 3, label: <BarChartOutlined style={{ fontSize: 18 }} /> },
-        { value: 4, label: <PieChartOutlined style={{ fontSize: 18 }} /> },
+        {
+          value: 1,
+          label: (
+            <Flex gap="small" justify="center" align="center" vertical>
+              <LineChartOutlined style={{ fontSize: 18 }} />
+              LineChart
+            </Flex>
+          ),
+        },
+        {
+          value: 2,
+          label: (
+            <Flex gap="small" justify="center" align="center" vertical>
+              <DotChartOutlined style={{ fontSize: 18 }} />
+              DotChart
+            </Flex>
+          ),
+        },
+        {
+          value: 3,
+          label: (
+            <Flex gap="small" justify="center" align="center" vertical>
+              <BarChartOutlined style={{ fontSize: 18 }} />
+              BarChart
+            </Flex>
+          ),
+        },
+        {
+          value: 4,
+          label: (
+            <Flex gap="small" justify="center" align="center" vertical>
+              <PieChartOutlined style={{ fontSize: 18 }} />
+              PieChart
+            </Flex>
+          ),
+        },
       ]}
     />
   );
