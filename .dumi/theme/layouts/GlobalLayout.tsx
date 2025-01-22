370b3e900368087fc8423fbc89b3fa76d6550834
二货机器人 <smith3816@gmail.com>
2025-01-22 12:01:26 +0800

    docs: update desc

--- .dumi/theme/layouts/GlobalLayout.tsx
+++ .dumi/theme/layouts/GlobalLayout.tsx
@@ -230,7 +230,6 @@ const GlobalLayout: React.FC = () => {
       <StyleProvider
         cache={styleCache}
         linters={[legacyNotSelectorLinter, parentSelectorLinter, NaNLinter]}
-        layer
       >
         <SiteContext.Provider value={siteContextValue}>
           <SiteThemeProvider theme={themeConfig}>
