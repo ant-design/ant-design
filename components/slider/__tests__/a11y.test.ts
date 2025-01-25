cb65d3f74708c56e13ecc372fb97c67b0a7f4751
小豪 <1844749591@qq.com>
2025-01-25 21:32:42 +0800

    test: add a11y test

--- components/slider/__tests__/a11y.test.ts
+++ components/slider/__tests__/a11y.test.ts
@@ -1,3 +1,6 @@
 import accessibilityDemoTest from '../../../tests/shared/accessibilityTest';
 
-accessibilityDemoTest('slider');
+accessibilityDemoTest('slider', {
+  // we can set aria attribute to fix it
+  disabledRules: ['aria-input-field-name', 'label', 'button-name'],
+});
