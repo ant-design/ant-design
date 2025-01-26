26234bcd029090ad0b0c834806c4313676daac9d
kiner-tang（星河） <1127031143@qq.com>
2025-01-26 11:00:05 +0800

    feat: [v6] Retire deprecated api for table (#52460)
    * feat: [v6] Retire deprecated api for table
    
    * feat: [v6] Retire deprecated api for table
    
    * feat: [v6] Retire deprecated api for table
    
    * feat: [v6] Retire deprecated api for table
    
    * feat: opt code
    
    * feat: opt code
    
    * feat: opt code

--- components/transfer/demo/component-token.tsx
+++ components/transfer/demo/component-token.tsx
@@ -44,14 +44,15 @@ const TableTransfer = ({ leftColumns, rightColumns, ...restProps }: TableTransfe
 
       const rowSelection: TableRowSelection<TransferItem> = {
         getCheckboxProps: (item) => ({ disabled: listDisabled || item.disabled }),
-        onSelectAll(selected, selectedRows) {
+        onChange(_selectedKeys, selectedRows, info) {
           const treeSelectedKeys = selectedRows
             .filter((item) => !item.disabled)
             .map(({ key }) => key);
-          const diffKeys = selected
-            ? difference(treeSelectedKeys, listSelectedKeys)
-            : difference(listSelectedKeys, treeSelectedKeys);
-          onItemSelectAll(diffKeys as string[], selected);
+          const diffKeys =
+            info.type === 'all'
+              ? difference(treeSelectedKeys, listSelectedKeys)
+              : difference(listSelectedKeys, treeSelectedKeys);
+          onItemSelectAll(diffKeys as string[], info.type === 'all');
         },
         onSelect({ key }, selected) {
           onItemSelect(key as string, selected);
