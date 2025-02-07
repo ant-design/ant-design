22dcf5f94e8d012e9b116e6e8002f59562974827
栗嘉男 <574980606@qq.com>
2025-02-07 14:51:41 +0800

    chore: update

--- components/pagination/__tests__/a11y.test.ts
+++ components/pagination/__tests__/a11y.test.ts
@@ -1,6 +1,6 @@
 import accessibilityDemoTest from '../../../tests/shared/accessibilityTest';
 
 accessibilityDemoTest('pagination', {
-  // waiting for rc-component/pagination fix
+  // waiting for @rc-component/pagination fix
   skip: ['simple.tsx'],
 });
