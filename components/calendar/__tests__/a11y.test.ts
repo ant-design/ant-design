d0324d024dc0e7fe22bc3083df396083a166a4a7
Jony J <1844749591@qq.com>
2025-02-08 09:59:27 +0800

    fix(a11y): fix empty table header for a11y and add localization support (#52689)
    * test: update snapshot
    
    * feat: add 'week' localization
    
    * test: update snapshot

--- components/calendar/__tests__/a11y.test.ts
+++ components/calendar/__tests__/a11y.test.ts
@@ -1,8 +1,6 @@
 import accessibilityDemoTest from '../../../tests/shared/accessibilityTest';
 
 accessibilityDemoTest('calendar', {
-  // waiting for rc-picker fix
-  skip: ['week.tsx'],
   // fix in another pr
   disabledRules: ['label'],
 });
