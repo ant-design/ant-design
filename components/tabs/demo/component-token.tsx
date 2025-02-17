37b62d43dfb5866555e3a0e83d1d505ff3f9f94a
二货机器人 <smith3816@gmail.com>
2025-02-17 17:12:51 +0800

    docs: update demo for diff

--- components/tabs/demo/component-token.tsx
+++ components/tabs/demo/component-token.tsx
@@ -2,138 +2,157 @@ import React from 'react';
 import { Button, ConfigProvider, Tabs } from 'antd';
 
 const App: React.FC = () => (
-  <ConfigProvider
-    theme={{
-      components: {
-        Tabs: {
-          cardBg: '#f6ffed',
-          cardHeight: 60,
-          cardPadding: `20px`,
-          cardPaddingSM: `20px`,
-          cardPaddingLG: `20px`,
-          titleFontSize: 20,
-          titleFontSizeLG: 20,
-          titleFontSizeSM: 20,
-          inkBarColor: '#52C41A',
-          horizontalMargin: `0 0 12px 0`,
-          horizontalItemGutter: 12, // Fixed Value
-          horizontalItemPadding: `20px`,
-          horizontalItemPaddingSM: `20px`,
-          horizontalItemPaddingLG: `20px`,
-          verticalItemPadding: `8px`,
-          verticalItemMargin: `4px 0 0 0`,
-          itemColor: 'rgba(0,0,0,0.85)',
-          itemSelectedColor: '#389e0d',
-          itemHoverColor: '#d9f7be',
-          itemActiveColor: '#b7eb8f',
-          cardGutter: 12,
+  <>
+    <ConfigProvider
+      theme={{
+        components: {
+          Tabs: {
+            cardBg: '#f6ffed',
+            cardHeight: 60,
+            cardPadding: `20px`,
+            cardPaddingSM: `20px`,
+            cardPaddingLG: `20px`,
+            titleFontSize: 20,
+            titleFontSizeLG: 20,
+            titleFontSizeSM: 20,
+            inkBarColor: '#52C41A',
+            horizontalMargin: `0 0 12px 0`,
+            horizontalItemGutter: 12, // Fixed Value
+            horizontalItemPadding: `20px`,
+            horizontalItemPaddingSM: `20px`,
+            horizontalItemPaddingLG: `20px`,
+            verticalItemPadding: `8px`,
+            verticalItemMargin: `4px 0 0 0`,
+            itemColor: 'rgba(0,0,0,0.85)',
+            itemSelectedColor: '#389e0d',
+            itemHoverColor: '#d9f7be',
+            itemActiveColor: '#b7eb8f',
+            cardGutter: 12,
+          },
         },
-      },
-    }}
-  >
-    <div>
-      <Tabs
-        defaultActiveKey="1"
-        tabBarExtraContent={<Button>Extra Action</Button>}
-        style={{ marginBottom: 32 }}
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of tab ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        tabPosition="left"
-        defaultActiveKey="1"
-        tabBarExtraContent={<Button>Extra Action</Button>}
-        style={{ marginBottom: 32 }}
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of tab ${id}`,
-          };
-        })}
-      />
+      }}
+    >
+      <div>
+        <Tabs
+          defaultActiveKey="1"
+          tabBarExtraContent={<Button>Extra Action</Button>}
+          style={{ marginBottom: 32 }}
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          tabPosition="left"
+          defaultActiveKey="1"
+          tabBarExtraContent={<Button>Extra Action</Button>}
+          style={{ marginBottom: 32 }}
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          size="small"
+          defaultActiveKey="1"
+          tabBarExtraContent={<Button>Extra Action</Button>}
+          style={{ marginBottom: 32 }}
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          size="large"
+          defaultActiveKey="1"
+          tabBarExtraContent={<Button>Extra Action</Button>}
+          style={{ marginBottom: 32 }}
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of tab ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          defaultActiveKey="1"
+          centered
+          type="card"
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              disabled: i === 2,
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of Tab Pane ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          size="small"
+          defaultActiveKey="1"
+          centered
+          type="card"
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              disabled: i === 2,
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of Tab Pane ${id}`,
+            };
+          })}
+        />
+        <Tabs
+          size="large"
+          defaultActiveKey="1"
+          centered
+          type="card"
+          items={Array.from({ length: 3 }).map((_, i) => {
+            const id = String(i + 1);
+            return {
+              disabled: i === 2,
+              label: `Tab ${id}`,
+              key: id,
+              children: `Content of Tab Pane ${id}`,
+            };
+          })}
+        />
+      </div>
+    </ConfigProvider>
+    <ConfigProvider
+      theme={{
+        components: {
+          Tabs: {
+            cardHeight: 180,
+            cardPadding: '0px 0px 0px 0px',
+            cardPaddingSM: '0px 0px 0px 0px',
+            verticalItemPadding: '0px 0px',
+            borderRadiusLG: 0,
+            borderRadius: 0,
+            horizontalItemPadding: '0px 0px 0px 0px',
+            horizontalMargin: '0 0 0 0',
+            inkBarColor: '#ffa940',
+          },
+        },
+      }}
+    >
       <Tabs
         size="small"
-        defaultActiveKey="1"
-        tabBarExtraContent={<Button>Extra Action</Button>}
-        style={{ marginBottom: 32 }}
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of tab ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        size="large"
-        defaultActiveKey="1"
-        tabBarExtraContent={<Button>Extra Action</Button>}
-        style={{ marginBottom: 32 }}
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of tab ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        defaultActiveKey="1"
-        centered
-        type="card"
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            disabled: i === 2,
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of Tab Pane ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        size="small"
-        defaultActiveKey="1"
-        centered
-        type="card"
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            disabled: i === 2,
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of Tab Pane ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        size="large"
-        defaultActiveKey="1"
-        centered
-        type="card"
-        items={Array.from({ length: 3 }).map((_, i) => {
-          const id = String(i + 1);
-          return {
-            disabled: i === 2,
-            label: `Tab ${id}`,
-            key: id,
-            children: `Content of Tab Pane ${id}`,
-          };
-        })}
-      />
-      <Tabs
-        defaultActiveKey="1"
-        centered
         type="editable-card"
         items={Array.from({ length: 3 }).map((_, i) => {
           const id = String(i + 1);
@@ -145,8 +164,8 @@ const App: React.FC = () => (
           };
         })}
       />
-    </div>
-  </ConfigProvider>
+    </ConfigProvider>
+  </>
 );
 
 export default App;
