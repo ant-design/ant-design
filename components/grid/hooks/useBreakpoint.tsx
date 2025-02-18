bd710b5b939107dfaca6dd2250137f8f969b2346
二货机器人 <smith3816@gmail.com>
2025-02-18 19:31:26 +0800

    chore: fix ts

--- components/grid/hooks/useBreakpoint.tsx
+++ components/grid/hooks/useBreakpoint.tsx
@@ -5,17 +5,20 @@ import useForceUpdate from '../../_util/hooks/useForceUpdate';
 import type { ScreenMap } from '../../_util/responsiveObserver';
 import useResponsiveObserver from '../../_util/responsiveObserver';
 
-function useBreakpoint<T extends ScreenMap | null = ScreenMap>(
+function useBreakpoint(refreshOnChange: boolean, defaultScreens: null): ScreenMap | null;
+function useBreakpoint(refreshOnChange?: boolean, defaultScreens?: ScreenMap): ScreenMap;
+
+function useBreakpoint(
   refreshOnChange = true,
-  defaultScreens: T = {} as T,
-): T {
-  const screensRef = useRef<T>(defaultScreens);
+  defaultScreens: ScreenMap | null = {} as ScreenMap,
+): ScreenMap | null {
+  const screensRef = useRef<ScreenMap | null>(defaultScreens);
   const forceUpdate = useForceUpdate();
   const responsiveObserver = useResponsiveObserver();
 
   useLayoutEffect(() => {
     const token = responsiveObserver.subscribe((supportScreens) => {
-      screensRef.current = supportScreens as T;
+      screensRef.current = supportScreens;
       if (refreshOnChange) {
         forceUpdate();
       }
