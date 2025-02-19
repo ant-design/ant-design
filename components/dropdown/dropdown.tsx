565cb02a8e7b783356d44f7207910f2cf25d5b80
yellworyan <877520264@qq.com>
2025-02-19 10:15:17 +0800

    fix: dropdown submenu arrow wrong direction in rtl

--- components/dropdown/dropdown.tsx
+++ components/dropdown/dropdown.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import RightOutlined from '@ant-design/icons/RightOutlined';
+import { RightOutlined, LeftOutlined } from '@ant-design/icons';
 import type { AlignType } from '@rc-component/trigger';
 import classNames from 'classnames';
 import RcDropdown from 'rc-dropdown';
@@ -252,14 +252,17 @@ const Dropdown: CompoundedComponent = (props) => {
     overlayNode = React.Children.only(
       typeof overlayNode === 'string' ? <span>{overlayNode}</span> : overlayNode,
     );
-
     return (
       <OverrideProvider
         prefixCls={`${prefixCls}-menu`}
         rootClassName={classNames(cssVarCls, rootCls)}
         expandIcon={
           <span className={`${prefixCls}-menu-submenu-arrow`}>
-            <RightOutlined className={`${prefixCls}-menu-submenu-arrow-icon`} />
+            {direction === 'rtl' ? (
+              <LeftOutlined className={`${prefixCls}-menu-submenu-arrow-icon`} />
+            ) : (
+              <RightOutlined className={`${prefixCls}-menu-submenu-arrow-icon`} />
+            )}
           </span>
         }
         mode="vertical"
