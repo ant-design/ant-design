e123ec16e586a4aff239ff4adab41c95c50a12ae
lijianan <574980606@qq.com>
2025-02-24 13:41:19 +0800

    feat: Radio.Group & Checkbox.Group Option support className (#52917)

--- components/checkbox/Group.tsx
+++ components/checkbox/Group.tsx
@@ -14,6 +14,7 @@ export interface CheckboxOptionType<T = any> {
   label: React.ReactNode;
   value: T;
   style?: React.CSSProperties;
+  className?: string; // 👈 5.25.0+
   disabled?: boolean;
   title?: string;
   id?: string;
@@ -125,7 +126,7 @@ const CheckboxGroup = React.forwardRef(
             value={option.value}
             checked={value.includes(option.value)}
             onChange={option.onChange}
-            className={`${groupPrefixCls}-item`}
+            className={classNames(`${groupPrefixCls}-item`, option.className)}
             style={option.style}
             title={option.title}
             id={option.id}
@@ -145,6 +146,7 @@ const CheckboxGroup = React.forwardRef(
       registerValue,
       cancelValue,
     };
+
     const classString = classNames(
       groupPrefixCls,
       {
@@ -156,6 +158,7 @@ const CheckboxGroup = React.forwardRef(
       rootCls,
       hashId,
     );
+
     return wrapCSSVar(
       <div className={classString} style={style} {...domProps} ref={ref}>
         <GroupContext.Provider value={context}>{childrenNode}</GroupContext.Provider>
