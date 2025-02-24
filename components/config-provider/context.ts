879704454f9b9cc6d0798af9c4cc854758fa642e
thinkasany <480968828@qq.com>
2025-02-24 19:12:23 +0800

    feat: ConfigProvider support tour

--- components/config-provider/context.ts
+++ components/config-provider/context.ts
@@ -151,7 +151,8 @@ export type CheckboxConfig = ComponentStyleConfig & Pick<CheckboxProps, 'classNa
 
 export type MenuConfig = ComponentStyleConfig & Pick<MenuProps, 'expandIcon'>;
 
-export type TourConfig = Pick<TourProps, 'closeIcon'>;
+export type TourConfig = ComponentStyleConfig &
+  Pick<TourProps, 'closeIcon' | 'classNames' | 'styles'>;
 
 export type DescriptionsConfig = ComponentStyleConfig &
   Pick<DescriptionsProps, 'classNames' | 'styles'>;
