c1390c7ef9e435a58a17044d1c644f238f7df316
刘欢 <lh01217311@antgroup.com>
2025-02-28 17:46:58 +0800

    没有hover状态的移动端，设置清除按钮和后缀图标透明度

--- components/select/style/index.ts
+++ components/select/style/index.ts
@@ -198,6 +198,17 @@ const genBaseStyle: GenerateStyle<SelectToken> = (token) => {
         },
       },
 
+      '@media(hover:none)': {
+        [`${componentCls}-clear`]: {
+          opacity: 1,
+        },
+        [`&:has(${componentCls}-clear)`]: {
+          [`${componentCls}-arrow`]: {
+            opacity: 0,
+          },
+        },
+      },
+
       [`&:hover ${componentCls}-clear`]: {
         opacity: 1,
         background: token.colorBgBase,
