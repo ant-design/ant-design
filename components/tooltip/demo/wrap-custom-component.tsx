4d6d5d72317019299e3bbab3f3d024fd6797a8e4
Dan M. <danspamable@gmail.com>
2025-03-03 10:28:05 -0800

    Replace custom props with HTMLSpanElement props
    Co-authored-by: afc163 <afc163@gmail.com>
    Signed-off-by: Dan M. <danspamable@gmail.com>

--- components/tooltip/demo/wrap-custom-component.tsx
+++ components/tooltip/demo/wrap-custom-component.tsx
@@ -1,16 +1,7 @@
 import { Tooltip } from 'antd';
 import React, { MouseEventHandler, PointerEventHandler, FocusEventHandler } from "react";
 
-interface ComponentWithEventsProps {
-    onMouseEnter?: MouseEventHandler | undefined;
-    onMouseLeave?: MouseEventHandler | undefined;
-    onPointerEnter?: PointerEventHandler | undefined;
-    onPointerLeave?: PointerEventHandler | undefined;
-    onFocus?: FocusEventHandler | undefined;
-    onClick?: MouseEventHandler | undefined;
-}
-
-const ComponentWithEvents: React.FC = (props: ComponentWithEventsProps) => {
+const ComponentWithEvents: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => {
     return (
         <span {...props}>
             This text is inside a component with the necessary events exposed.
