ce98374dc7c6d0058b5bd7679de882af9847572f
Dan M. <danspamable@gmail.com>
2025-03-04 11:18:31 -0800

    Update components/tooltip/demo/wrap-custom-component.tsx
    Co-authored-by: afc163 <afc163@gmail.com>
    Signed-off-by: Dan M. <danspamable@gmail.com>

--- components/tooltip/demo/wrap-custom-component.tsx
+++ components/tooltip/demo/wrap-custom-component.tsx
@@ -1,13 +1,11 @@
 import { Tooltip } from 'antd';
 import React, type { MouseEventHandler, PointerEventHandler, FocusEventHandler } from "react";
 
-const ComponentWithEvents: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => {
-    return (
-      <span {...props}>
-        This text is inside a component with the necessary events exposed.
-      </span>
-    );
-}
+const ComponentWithEvents: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => (
+  <span {...props}>
+    This text is inside a component with the necessary events exposed.
+  </span>
+);
 
 const App: React.FC = () => (
   <Tooltip title="prompt text">
