b2e0c53751cfa5d7c209504f1d570a1f722901e4
thinkasany <480968828@qq.com>
2025-03-05 10:34:43 +0800

    test: update snap

--- components/tooltip/demo/wrap-custom-component.tsx
+++ components/tooltip/demo/wrap-custom-component.tsx
@@ -1,11 +1,8 @@
 import { Tooltip } from 'antd';
 import React from 'react';
-import type { MouseEventHandler, PointerEventHandler, FocusEventHandler } from 'react';
 
 const ComponentWithEvents: React.FC<React.HTMLAttributes<HTMLSpanElement>> = (props) => (
-  <span {...props}>
-    This text is inside a component with the necessary events exposed.
-  </span>
+  <span {...props}>This text is inside a component with the necessary events exposed.</span>
 );
 
 const App: React.FC = () => (
