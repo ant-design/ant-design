8ee8c50fab7d1c4911c8ebbaf2e9fd6b7d3a9ed6
二货机器人 <smith3816@gmail.com>
2025-03-05 11:12:07 +0800

    chore: re-trigger

--- components/mentions/__tests__/index.test.tsx
+++ components/mentions/__tests__/index.test.tsx
@@ -7,6 +7,7 @@ import rtlTest from '../../../tests/shared/rtlTest';
 import { act, fireEvent, render } from '../../../tests/utils';
 
 const { getMentions } = Mentions;
+
 function simulateInput(wrapper: ReturnType<typeof render>, text: string, keyEvent?: Event): void {
   const lastChar = text[text.length - 1];
   const myKeyEvent = keyEvent || {
