394ee850fe14172f93a1e96deff861010afeb4be
二货机器人 <smith3816@gmail.com>
2025-03-07 18:20:19 +0800

    chore: back of style

--- components/table/style/fixed.ts
+++ components/table/style/fixed.ts
@@ -17,124 +17,156 @@ const genFixedStyle: GenerateStyle<TableToken, CSSObject> = (token) => {
 
   const shadowColor = colorSplit;
 
+  const cellCls = `${componentCls}-cell`;
+  const fixCellCls = `${cellCls}-fix`;
+
   // Follow style is magic of shadow which should not follow token:
-  return {
+  const style = {
     [`${componentCls}-wrapper`]: {
-      [`
-        ${componentCls}-cell-fix-left,
-        ${componentCls}-cell-fix-right
-      `]: {
-        position: 'sticky !important' as 'sticky',
-        zIndex: zIndexTableFixed,
+      [`${cellCls}${fixCellCls}`]: {
+        position: 'sticky',
+      },
+
+      [fixCellCls]: {
+        zIndex: `calc(var(--z-offset) + ${zIndexTableFixed})`,
         background: tableBg,
-      },
 
-      [`
-        ${componentCls}-cell-fix-left-first::after,
-        ${componentCls}-cell-fix-left-last::after
-      `]: {
-        position: 'absolute',
-        top: 0,
-        right: {
-          _skip_check_: true,
-          value: 0,
-        },
-        bottom: calc(lineWidth).mul(-1).equal(),
-        width: 30,
-        transform: 'translateX(100%)',
-        transition: `box-shadow ${motionDurationSlow}`,
-        content: '""',
-        pointerEvents: 'none',
-      },
-
-      [`${componentCls}-cell-fix-left-all::after`]: {
-        display: 'none',
-      },
-
-      [`
-        ${componentCls}-cell-fix-right-first::after,
-        ${componentCls}-cell-fix-right-last::after
-      `]: {
-        position: 'absolute',
-        top: 0,
-        bottom: calc(lineWidth).mul(-1).equal(),
-        left: {
-          _skip_check_: true,
-          value: 0,
-        },
-        width: 30,
-        transform: 'translateX(-100%)',
-        transition: `box-shadow ${motionDurationSlow}`,
-        content: '""',
-        pointerEvents: 'none',
-      },
-
-      [`${componentCls}-container`]: {
-        position: 'relative',
-
-        '&::before, &::after': {
+        '&:after': {
           position: 'absolute',
           top: 0,
-          bottom: 0,
-          zIndex: calc(zIndexTableSticky).add(1).equal({ unit: false }),
+          bottom: calc(lineWidth).mul(-1).equal(),
           width: 30,
           transition: `box-shadow ${motionDurationSlow}`,
           content: '""',
           pointerEvents: 'none',
         },
 
-        '&::before': {
+        // Position
+        '&-start:after': {
+          transform: 'translateX(100%)',
+          insetInlineEnd: 0,
+        },
+
+        '&-end:after': {
+          transform: 'translateX(-100%)',
           insetInlineStart: 0,
         },
 
-        '&::after': {
-          insetInlineEnd: 0,
-        },
-      },
-
-      [`${componentCls}-ping-left`]: {
-        [`&:not(${componentCls}-has-fix-left) ${componentCls}-container::before`]: {
+        // visible
+        '&-start-shadow-show:after': {
           boxShadow: `inset 10px 0 8px -8px ${shadowColor}`,
         },
-
-        [`
-          ${componentCls}-cell-fix-left-first::after,
-          ${componentCls}-cell-fix-left-last::after
-        `]: {
-          boxShadow: `inset 10px 0 8px -8px ${shadowColor}`,
-        },
-
-        [`${componentCls}-cell-fix-left-last::before`]: {
-          backgroundColor: 'transparent !important',
-        },
-      },
-
-      [`${componentCls}-ping-right`]: {
-        [`&:not(${componentCls}-has-fix-right) ${componentCls}-container::after`]: {
-          boxShadow: `inset -10px 0 8px -8px ${shadowColor}`,
-        },
-
-        [`
-          ${componentCls}-cell-fix-right-first::after,
-          ${componentCls}-cell-fix-right-last::after
-        `]: {
+        '&-end-shadow-show:after': {
           boxShadow: `inset -10px 0 8px -8px ${shadowColor}`,
         },
       },
 
-      // Gapped fixed Columns do not show the shadow
-      [`${componentCls}-fixed-column-gapped`]: {
-        [`
-        ${componentCls}-cell-fix-left-first::after,
-        ${componentCls}-cell-fix-left-last::after,
-        ${componentCls}-cell-fix-right-first::after,
-        ${componentCls}-cell-fix-right-last::after
-      `]: {
-          boxShadow: 'none',
-        },
-      },
+      // [`
+      //   ${componentCls}-cell-fix-left,
+      //   ${componentCls}-cell-fix-right
+      // `]: {
+      //   position: 'sticky !important' as 'sticky',
+      //   zIndex: zIndexTableFixed,
+      //   background: tableBg,
+      // },
+      // [`
+      //   ${componentCls}-cell-fix-left-first::after,
+      //   ${componentCls}-cell-fix-left-last::after
+      // `]: {
+      //   position: 'absolute',
+      //   top: 0,
+      //   right: {
+      //     _skip_check_: true,
+      //     value: 0,
+      //   },
+      //   bottom: calc(lineWidth).mul(-1).equal(),
+      //   width: 30,
+      //   transform: 'translateX(100%)',
+      //   transition: `box-shadow ${motionDurationSlow}`,
+      //   content: '""',
+      //   pointerEvents: 'none',
+      // },
+      // [`${componentCls}-cell-fix-left-all::after`]: {
+      //   display: 'none',
+      // },
+      // [`
+      //   ${componentCls}-cell-fix-right-first::after,
+      //   ${componentCls}-cell-fix-right-last::after
+      // `]: {
+      //   position: 'absolute',
+      //   top: 0,
+      //   bottom: calc(lineWidth).mul(-1).equal(),
+      //   left: {
+      //     _skip_check_: true,
+      //     value: 0,
+      //   },
+      //   width: 30,
+      //   transform: 'translateX(-100%)',
+      //   transition: `box-shadow ${motionDurationSlow}`,
+      //   content: '""',
+      //   pointerEvents: 'none',
+      // },
+      // [`${componentCls}-container`]: {
+      //   position: 'relative',
+      //   '&::before, &::after': {
+      //     position: 'absolute',
+      //     top: 0,
+      //     bottom: 0,
+      //     zIndex: calc(zIndexTableSticky).add(1).equal({ unit: false }),
+      //     width: 30,
+      //     transition: `box-shadow ${motionDurationSlow}`,
+      //     content: '""',
+      //     pointerEvents: 'none',
+      //   },
+      //   '&::before': {
+      //     insetInlineStart: 0,
+      //   },
+      //   '&::after': {
+      //     insetInlineEnd: 0,
+      //   },
+      // },
+      // [`${componentCls}-ping-left`]: {
+      //   [`&:not(${componentCls}-has-fix-left) ${componentCls}-container::before`]: {
+      //     boxShadow: `inset 10px 0 8px -8px ${shadowColor}`,
+      //   },
+      //   [`
+      //     ${componentCls}-cell-fix-left-first::after,
+      //     ${componentCls}-cell-fix-left-last::after
+      //   `]: {
+      //     boxShadow: `inset 10px 0 8px -8px ${shadowColor}`,
+      //   },
+      //   [`${componentCls}-cell-fix-left-last::before`]: {
+      //     backgroundColor: 'transparent !important',
+      //   },
+      // },
+      // [`${componentCls}-ping-right`]: {
+      //   [`&:not(${componentCls}-has-fix-right) ${componentCls}-container::after`]: {
+      //     boxShadow: `inset -10px 0 8px -8px ${shadowColor}`,
+      //   },
+      //   [`
+      //     ${componentCls}-cell-fix-right-first::after,
+      //     ${componentCls}-cell-fix-right-last::after
+      //   `]: {
+      //     boxShadow: `inset -10px 0 8px -8px ${shadowColor}`,
+      //   },
+      // },
+      // // Gapped fixed Columns do not show the shadow
+      // [`${componentCls}-fixed-column-gapped`]: {
+      //   [`
+      //   ${componentCls}-cell-fix-left-first::after,
+      //   ${componentCls}-cell-fix-left-last::after,
+      //   ${componentCls}-cell-fix-right-first::after,
+      //   ${componentCls}-cell-fix-right-last::after
+      // `]: {
+      //     boxShadow: 'none',
+      //   },
+      // },
     },
   };
+
+  console.log('>>>', style);
+
+  return style;
 };
 
 export default genFixedStyle;
