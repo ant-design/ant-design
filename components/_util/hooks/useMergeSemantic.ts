01324eab136833b67a1c6a2cd26e9bfffaa89930
二货机器人 <smith3816@gmail.com>
2025-03-14 17:11:44 +0800

    feat: add useSemanticClassNames hook

---
+++ components/_util/hooks/useMergeSemantic.ts
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import classnames from 'classnames';
+
+type SemanticClassNames<T extends string> = Partial<Record<T, string>>;
+type SemanticStyles<T extends string> = Partial<Record<T, React.CSSProperties>>;
+
+function useSemanticClassNames<T extends string>(...classNames: SemanticClassNames<T>[]) {
+  return React.useMemo(() => {
+    return classNames.reduce(
+      (acc, cur) => {
+        Object.keys(cur).forEach((key) => {
+          acc[key] = classnames(acc[key], (cur as Record<string, string>)[key]);
+        });
+        return acc;
+      },
+      {} as Record<string, string>,
+    ) as SemanticClassNames<T>;
+  }, [classNames]);
+}
+
+function useSemanticStyles<T extends string>(...styles: SemanticStyles<T>[]) {}
+
+export function useMergeSemantic<T extends string>(
+  classNamesList: SemanticClassNames<T>[],
+  stylesList: SemanticStyles<T>[],
+) {
+  const mergedClassNames = useSemanticClassNames(...classNamesList);
+  const mergedStyles = useSemanticStyles(...stylesList);
+
+  return [mergedClassNames, mergedStyles] as const;
+}
