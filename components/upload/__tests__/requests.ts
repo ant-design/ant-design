bab363325bbe4d29f67e4a55b81356ab73a7ede1
lijianan <574980606@qq.com>
2025-03-15 12:38:25 +0800

    test: remove ts-ignore

--- components/upload/__tests__/requests.ts
+++ components/upload/__tests__/requests.ts
@@ -1,15 +1,9 @@
 import type { UploadProps } from '../interface';
 
 export const successRequest: UploadProps['customRequest'] = ({ onSuccess, file }) => {
-  setTimeout(() => {
-    // @ts-ignore
-    onSuccess?.(null, file);
-  });
+  setTimeout(() => onSuccess?.(null, file));
 };
 
 export const errorRequest: UploadProps['customRequest'] = ({ onError }) => {
-  setTimeout(() => {
-    // @ts-ignore
-    onError?.();
-  });
+  setTimeout(() => onError?.(new Error('test error')));
 };
