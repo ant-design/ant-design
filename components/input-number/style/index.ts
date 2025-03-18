db54f6d1ecb0b5f1ec2acb53f9a96e738ed42781
yellworyan <877520264@qq.com>
2025-03-18 14:11:19 +0800

    chore: style update

--- components/input-number/style/index.ts
+++ components/input-number/style/index.ts
@@ -480,12 +480,9 @@ const genAffixWrapperStyles: GenerateStyle<InputNumberToken> = (token: InputNumb
         width: token.handleWidth,
         opacity: 1,
       },
-      [`&:not(${componentCls}-affix-wrapper-without-controls):hover ${componentCls}-suffix`]: {
-        marginInlineEnd: token.calc(token.handleWidth).add(paddingInline).equal(),
-      },
-      [`&:not(${componentCls}-affix-wrapper-without-controls)&-disabled:hover ${componentCls}-suffix`]:
+      [`&:not(${componentCls}-affix-wrapper-without-controls):not(${componentCls}-affix-wrapper-disabled):hover ${componentCls}-suffix`]:
         {
-          marginInlineEnd: paddingInline,
+          marginInlineEnd: token.calc(token.handleWidth).add(paddingInline).equal(),
         },
     },
   };
