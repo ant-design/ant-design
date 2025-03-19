35d5f1b89b52eecdba0fb93dba85b87cc0fed732
刘欢 <lh01217311@antgroup.com>
2025-03-19 11:06:43 +0800

    恢复到master相同代码

--- components/select/style/index.ts
+++ components/select/style/index.ts
@@ -198,15 +198,6 @@ const genBaseStyle: GenerateStyle<SelectToken> = (token) => {
         },
       },
 
-      '@media(hover:none)': {
-        [`${componentCls}-clear`]: {
-          opacity: 1,
-        },
-        [`&:has(${componentCls}-clear) ${componentCls}-arrow`]: {
-          opacity: 0,
-        },
-      },
-
       [`&:hover ${componentCls}-clear`]: {
         opacity: 1,
         background: token.colorBgBase,
