4728cd79f8a237d71bf69147792e4d03b4a74dab
WB01676250 <jl01676250@antgroup.com>
2025-03-25 11:56:09 +0800

    fix: test

--- components/modal/__tests__/Modal.test.tsx
+++ components/modal/__tests__/Modal.test.tsx
@@ -233,9 +233,9 @@ describe('Modal', () => {
     expect(document.querySelector('.ant-modal-centered')).toBeFalsy();
   });
 
-  it('Should not close modal when confirmLoading is loading', async () => {
+  it('Should not close modal when confirmLoading is loading', () => {
     const onCancel = jest.fn();
-    const onOk = jest.fn(() => new Promise((resolve) => setTimeout(resolve, 1000)));
+    const onOk = jest.fn();
 
     render(<Modal open onCancel={onCancel} onOk={onOk} />);
 
@@ -243,8 +243,8 @@ describe('Modal', () => {
     fireEvent.click(okButton);
     expect(okButton).not.toHaveClass('ant-btn-loading');
 
-    fireEvent.click(document.body.querySelector('.ant-modal-close')!);
-    fireEvent.click(document.body.querySelector('.ant-modal-wrap')!);
+    fireEvent.click(document.body.querySelectorAll('.ant-modal-close')[0]);
+    fireEvent.click(document.body.querySelectorAll('.ant-modal-wrap')[0]);
     expect(onCancel).toHaveBeenCalled();
 
     onOk.mockReset();
