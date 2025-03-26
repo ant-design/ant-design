0e00c4202e6fcb4c4aebd9964a7a602f25450641
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-03-26 17:25:57 +0800

    chore(site): color preview

--- .dumi/theme/builtins/TokenTable/index.tsx
+++ .dumi/theme/builtins/TokenTable/index.tsx
@@ -79,7 +79,11 @@ export function useColumns(): Exclude<TableProps<TokenData>['columns'], undefine
           typeof record.value === 'string' &&
           (record.value.startsWith('#') || record.value.startsWith('rgb'));
         if (isColor) {
-          return <ColorChunk value={record.value}>{record.value}</ColorChunk>;
+          return (
+            <ColorChunk value={record.value} enablePopover>
+              {record.value}
+            </ColorChunk>
+          );
         }
         return typeof record.value !== 'string' ? JSON.stringify(record.value) : record.value;
       },
