10234b7f3c3f003bd244b761b89ef6e5c8f20e6c
EmilyyyLiu <100924403+EmilyyyLiu@users.noreply.github.com>
2025-04-03 12:33:31 +0800

    Update .dumi/theme/builtins/Previewer/CodePreviewer.tsx
    Co-authored-by: afc163 <afc163@gmail.com>
    Signed-off-by: EmilyyyLiu <100924403+EmilyyyLiu@users.noreply.github.com>

--- .dumi/theme/builtins/Previewer/CodePreviewer.tsx
+++ .dumi/theme/builtins/Previewer/CodePreviewer.tsx
@@ -325,9 +325,9 @@ createRoot(document.getElementById('container')).render(<Demo />);
       ...dependencies,
       react: '^19.0.0',
       'react-dom': '^19.0.0',
-      '@types/react' :'^19.0.0',
-      '@types/react-dom' : '^19.0.0',
-      '@ant-design/v5-patch-for-react-19':'latest'
+      '@types/react': '^19.0.0',
+      '@types/react-dom': '^19.0.0',
+      '@ant-design/v5-patch-for-react-19': '^1.0.3'
     },
     description: '',
     files: {
