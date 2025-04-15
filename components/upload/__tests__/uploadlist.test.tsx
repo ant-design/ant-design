c7169aea66e4b14bc5585863cee03fa348d2596d
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-04-15 13:09:25 +0800

    chore: add unit test

--- components/upload/__tests__/uploadlist.test.tsx
+++ components/upload/__tests__/uploadlist.test.tsx
@@ -608,7 +608,7 @@ describe('Upload List', () => {
     unmount();
   });
 
-  it('disabled should not affect preview and download icon', () => {
+  describe('disabled should not affect preview and download icon', () => {
     const list = [
       {
         name: 'image',
@@ -618,7 +618,24 @@ describe('Upload List', () => {
       },
     ];
 
-    const { container: wrapper, unmount } = render(
+    const check = (wrapper: HTMLElement) => {
+      // preview icon
+      expect(
+        wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[0].hasAttribute('disabled'),
+      ).toBeFalsy();
+
+      // download icon
+      expect(
+        wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[1].hasAttribute('disabled'),
+      ).toBeFalsy();
+
+      // delete icon
+      expect(
+        wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[2].hasAttribute('disabled'),
+      ).toBeTruthy();
+    };
+
+    const InnerUploadList = (props: Partial<UploadProps>) => (
       <Upload
         listType="picture-card"
         defaultFileList={list as UploadProps['defaultFileList']}
@@ -627,25 +644,29 @@ describe('Upload List', () => {
           showDownloadIcon: true,
           showRemoveIcon: true,
         }}
-        disabled
+        {...props}
       >
         <button type="button">upload</button>
-      </Upload>,
+      </Upload>
     );
-    // preview icon
-    expect(
-      wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[0].hasAttribute('disabled'),
-    ).toBeFalsy();
-    // download icon
 
-    expect(
-      wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[1].hasAttribute('disabled'),
-    ).toBeFalsy();
-    // delete icon
-    expect(
-      wrapper.querySelectorAll('.ant-upload-list-item-actions > *')[2].hasAttribute('disabled'),
-    ).toBeTruthy();
-    unmount();
+    // https://github.com/ant-design/ant-design/issues/46171
+    it('normal', () => {
+      const { container: wrapper } = render(<InnerUploadList disabled />);
+      check(wrapper);
+    });
+
+    // https://github.com/ant-design/ant-design/issues/53503
+    it('with Form', () => {
+      const { container: wrapper } = render(
+        <Form disabled>
+          <Form.Item>
+            <InnerUploadList />
+          </Form.Item>
+        </Form>,
+      );
+      check(wrapper);
+    });
   });
 
   it('should support custom onClick in custom icon', async () => {
