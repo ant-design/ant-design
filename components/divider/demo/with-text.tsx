cd04f5061c9dfa3a973884e16356ed24b5afb0d4
刘欢 <lh01217311@antgroup.com>
2025-04-25 12:11:45 +0800

    feat[Divider]: Unified use of orientation attribute

--- components/divider/demo/with-text.tsx
+++ components/divider/demo/with-text.tsx
@@ -12,24 +12,24 @@ const App: React.FC = () => (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider orientation="left">Left Text</Divider>
+    <Divider titlePlacement="left">Left Text</Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider orientation="right">Right Text</Divider>
+    <Divider titlePlacement="right">Right Text</Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider orientation="left" orientationMargin="0">
+    <Divider titlePlacement="left" placementMargin="0">
       Left Text with 0 orientationMargin
     </Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider orientation="right" orientationMargin={50}>
+    <Divider titlePlacement="right" placementMargin={50}>
       Right Text with 50px orientationMargin
     </Divider>
     <p>
