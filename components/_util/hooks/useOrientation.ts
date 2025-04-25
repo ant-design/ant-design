0507c892bbb5e481b1722fc72875c3e898d29adb
刘欢 <lh01217311@antgroup.com>
2025-04-25 10:57:16 +0800

    test:useOrientation add test

---
+++ components/_util/hooks/useOrientation.ts
@@ -0,0 +1,39 @@
+import { useMemo } from 'react';
+
+export type Orientation = 'horizontal' | 'vertical';
+interface OrientationProps {
+  orientation?: Orientation;
+  direction?: any;
+  vertical?: boolean;
+  ctxVertical?: boolean;
+}
+
+export const useOrientation = (
+  { orientation, vertical, ctxVertical }: OrientationProps,
+  type?: Orientation,
+) => {
+  return useMemo(() => {
+    const haveOrientation = ['horizontal', 'vertical'].includes(orientation || '');
+    if (haveOrientation) {
+      return orientation;
+    }
+    if (vertical ?? ctxVertical) {
+      return 'vertical';
+    }
+    return type ?? 'horizontal';
+  }, [type, orientation, vertical, ctxVertical]);
+};
+
+interface VerticalProps {
+  orientation?: Orientation;
+  vertical?: boolean;
+  ctxVertical?: boolean;
+}
+export const useVertical = ({ orientation, vertical, ctxVertical }: VerticalProps) => {
+  return useMemo(() => {
+    if (orientation) {
+      return orientation === 'vertical';
+    }
+    return vertical ?? ctxVertical ?? false;
+  }, [vertical, ctxVertical, orientation]);
+};
