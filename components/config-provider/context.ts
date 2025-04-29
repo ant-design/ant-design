4669c037351a4f9081bfce8d1a345fe69854831c
thinkasany <480968828@qq.com>
2025-04-29 17:17:04 +0800

    feat: ConfigProvider support classNames and styles for inputNumber

--- components/config-provider/context.ts
+++ components/config-provider/context.ts
@@ -298,7 +298,8 @@ export type ResultConfig = ComponentStyleConfig & Pick<ResultProps, 'classNames'
 
 export type RadioConfig = ComponentStyleConfig & Pick<RadioProps, 'classNames' | 'styles'>;
 
-export type InputNumberConfig = ComponentStyleConfig & Pick<InputNumberProps, 'variant'>;
+export type InputNumberConfig = ComponentStyleConfig &
+  Pick<InputNumberProps, 'variant' | 'classNames' | 'styles'>;
 
 export type CascaderConfig = ComponentStyleConfig &
   Pick<CascaderProps, 'variant' | 'styles' | 'classNames'>;
---
+++ components/input-number/demo/_semantic.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { InputNumber } from 'antd';
+
+import SemanticPreview from '../../../.dumi/components/SemanticPreview';
+import useLocale from '../../../.dumi/hooks/useLocale';
+
+const locales = {
+  cn: {
+    root: '根元素',
+    input: '输入框元素',
+    prefix: '前缀的包裹元素',
+    suffix: '后缀的包裹元素',
+    handle: '操作元素',
+  },
+  en: {
+    root: 'root element',
+    input: 'input element',
+    prefix: 'prefix element',
+    suffix: 'suffix element',
+    handle: 'handle element',
+  },
+};
+
+const App: React.FC = () => {
+  const [locale] = useLocale(locales);
+  return (
+    <SemanticPreview
+      componentName="InputNumber"
+      semantics={[
+        { name: 'root', desc: locale.root },
+        { name: 'prefix', desc: locale.prefix },
+        { name: 'input', desc: locale.input },
+        { name: 'suffix', desc: locale.suffix },
+        { name: 'handle', desc: locale.handle },
+      ]}
+    >
+      <InputNumber prefix="￥" suffix="RMB" defaultValue={100} style={{ width: 200 }} />
+    </SemanticPreview>
+  );
+};
+
+export default App;
