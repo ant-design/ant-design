c8390debb24faafeac39c534c351c61f73f66956
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-04-30 13:52:26 +0800

    chore: clean similar function methods (#53699)

--- components/message/index.tsx
+++ components/message/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 
 import { AppConfigContext } from '../app/context';
 import ConfigProvider, { ConfigContext, globalConfig, warnContext } from '../config-provider';
-import { getReactRender } from '../config-provider/UnstableContext';
+import { unstableSetRender } from '../config-provider/UnstableContext';
 import type {
   ArgsProps,
   ConfigOptions,
@@ -132,7 +132,7 @@ function flushNotice() {
 
     // Delay render to avoid sync issue
     act(() => {
-      const reactRender = getReactRender();
+      const reactRender = unstableSetRender();
 
       reactRender(
         <GlobalHolderWrapper
