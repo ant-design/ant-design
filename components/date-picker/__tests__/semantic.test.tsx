bb119755329722c8539d4b6bab1c60686f64c448
aojunhao123 <1844749591@qq.com>
2025-05-07 13:40:17 +0800

    fix: test coverage

--- components/date-picker/__tests__/semantic.test.tsx
+++ components/date-picker/__tests__/semantic.test.tsx
@@ -4,7 +4,7 @@ import type { DatePickerProps } from '..';
 import DatePicker from '..';
 import { render } from '../../../tests/utils';
 
-describe('Progress.Semantic', () => {
+describe('DatePicker.Semantic', () => {
   describe('inline', () => {
     function test(name: string, renderFn: (props: any) => React.ReactElement) {
       it(name, () => {
@@ -36,5 +36,49 @@ describe('Progress.Semantic', () => {
     test('DatePicker - Multiple', (props) => <DatePicker.RangePicker {...props} />);
     test('TimePicker - Single', (props) => <DatePicker {...props} picker="time" />);
     test('TimePicker - Multiple', (props) => <DatePicker.RangePicker {...props} picker="time" />);
+
+    it('DatePicker - Single - with popup className as string', () => {
+      const classNamesConfig = {
+        root: 'my-custom-root-str-popup',
+        popup: 'my-custom-popup-flat-string',
+      } as any;
+      const stylesConfig = {
+        root: { color: 'rgb(255, 0, 0)' },
+        popup: { root: { color: 'rgb(0, 0, 255)' } },
+      };
+
+      render(<DatePicker classNames={classNamesConfig} styles={stylesConfig} open />);
+
+      const pickerElement = document.body.querySelector('.ant-picker');
+      const dropdownElement = document.body.querySelector('.ant-picker-dropdown');
+
+      expect(pickerElement).toHaveClass(classNamesConfig.root);
+      expect(dropdownElement).toHaveClass(classNamesConfig.popup);
+
+      expect(pickerElement).toHaveStyle(stylesConfig.root);
+      expect(dropdownElement).toHaveStyle(stylesConfig.popup.root);
+    });
+
+    it('DatePicker.RangePicker - with popup className as string', () => {
+      const classNamesConfig = {
+        root: 'my-custom-range-root-str-popup',
+        popup: 'my-custom-range-popup-flat-string',
+      } as any;
+      const stylesConfig = {
+        root: { borderColor: 'rgb(0, 255, 0)' }, // green
+        popup: { root: { borderColor: 'rgb(255, 255, 0)' } }, // yellow
+      };
+
+      render(<DatePicker.RangePicker classNames={classNamesConfig} styles={stylesConfig} open />);
+
+      const pickerElement = document.body.querySelector('.ant-picker');
+      const dropdownElement = document.body.querySelector('.ant-picker-dropdown');
+
+      expect(pickerElement).toHaveClass(classNamesConfig.root);
+      expect(dropdownElement).toHaveClass(classNamesConfig.popup);
+
+      expect(pickerElement).toHaveStyle(stylesConfig.root);
+      expect(dropdownElement).toHaveStyle(stylesConfig.popup.root);
+    });
   });
 });
