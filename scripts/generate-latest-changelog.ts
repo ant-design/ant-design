8f5973fe29bfc6606c7180aa8d5fd7adad206e92
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-05-08 18:42:33 +0800

    feat: Add script to generate last changelog files for English and Chinese versions

---
+++ scripts/generate-latest-changelog.ts
@@ -0,0 +1,56 @@
+import path from 'path';
+import fse from 'fs-extra';
+import assert from 'assert';
+
+import { version } from '../package.json';
+
+const ROOT = path.resolve(__dirname, '../');
+const OUTPUT_DIR = path.join(ROOT, '.dumi/preset');
+
+const originENChangelog = fse.readFileSync(path.join(ROOT, 'CHANGELOG.en-US.md'), 'utf-8');
+const originCNChangelog = fse.readFileSync(path.join(ROOT, 'CHANGELOG.zh-CN.md'), 'utf-8');
+
+const getChangelog = (content: string, v = version) => {
+  const lines = content.split('\n');
+  const changeLog = [];
+  const startPattern = new RegExp(`^## ${v}`);
+  const stopPattern = /^## /; // 前一个版本
+  // const skipPattern = /^`/; // 日期
+  let begin = false;
+  for (let i = 0; i < lines.length; i += 1) {
+    const line = lines[i];
+    if (begin && stopPattern.test(line)) {
+      break;
+    }
+    if (begin && line) {
+      changeLog.push(line);
+    }
+    if (!begin) {
+      begin = startPattern.test(line);
+    }
+  }
+  return changeLog.join('\n');
+};
+
+const main = async () => {
+  const lastENChangelog = getChangelog(originENChangelog);
+  const lastCNChangelog = getChangelog(originCNChangelog);
+
+  assert(lastCNChangelog, 'lastCNChangelog is empty');
+  fse.writeFileSync(path.join(OUTPUT_DIR, 'last-changelog.en-US.md'), lastENChangelog, 'utf-8');
+
+  assert(lastENChangelog, 'lastENChangelog is empty');
+  fse.writeFileSync(path.join(OUTPUT_DIR, 'last-changelog.zh-CN.md'), lastCNChangelog, 'utf-8');
+};
+
+// \\\\\\\\\\\
+// \\\ run \\\
+// \\\\\\\\\\\
+main()
+  .then(() => {
+    console.log('✅ last changelog generated');
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
