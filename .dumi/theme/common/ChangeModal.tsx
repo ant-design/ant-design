6c5d03f1ef64b5704de475c8a3298e17c8fe75de
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-05-09 00:19:53 +0800

    feat: Enhance ChangeModal with new features and styling improvements

--- .dumi/theme/common/ChangeModal.tsx
+++ .dumi/theme/common/ChangeModal.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Button, version as currentVersion, Flex, Modal } from 'antd';
+import { FastColor } from '@ant-design/fast-color';
+import { Button, version as currentVersion, Flex, Modal, Tooltip } from 'antd';
+import { createStyles } from 'antd-style';
 import { useNavigate } from 'dumi';
+import debounce from 'lodash/debounce';
 
 import useLocale from '../../hooks/useLocale';
+// @ts-ignore
+import EN from '../../preset/last-changelog.en-US.md?type=dumi-component';
+// @ts-ignore
+import CN from '../../preset/last-changelog.zh-CN.md?type=dumi-component';
+
+const changeLog = { cn: CN, en: EN };
 
 const STORAGE_KEY = 'ANT_DESIGN_LAST_VISITED_VERSION';
 const VER_REGEX = /^\d+\.\d+\.\d+$/;
@@ -29,13 +38,82 @@ const locales = {
   cn: {
     title: `v${currentVersion} 现已发布`,
     gettingStarted: '开始使用',
-    fullChangeLog: '完整更新日志',
+    fullChangeLog: '查看全部日志',
+    mirror: '国内镜像',
+    mirrorDesc: '前往国内镜像',
   },
   en: {
     title: `v${currentVersion} is released`,
     gettingStarted: 'Getting Started',
-    fullChangeLog: 'Full Change Log',
+    fullChangeLog: 'View Full Change Log',
+    mirror: 'Chinese Mirror',
+    mirrorDesc: 'Go to mainland China to mirror',
   },
+};
+
+const useStyle = createStyles(({ css, token }, { isOverflowing }: any) => ({
+  container: css`
+    max-height: max(62vh, 500px);
+    overflow: hidden;
+    position: relative;
+
+    ::after {
+      opacity: ${isOverflowing ? 1 : 0};
+      content: '';
+      position: absolute;
+      inset-block-end: 0;
+      inset-inline-start: 0;
+      width: 100%;
+      height: 30%;
+      pointer-events: none;
+      background: linear-gradient(
+        to top,
+        ${token.colorBgElevated} 0%,
+        ${new FastColor(token.colorBgElevated).setA(0.25).toHexString()} 100%
+      );
+      backdrop-filter: blur(0.1px);
+    }
+  `,
+}));
+
+const ChangeLog = () => {
+  const [, lang] = useLocale();
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const [isOverflowing, setIsOverflowing] = React.useState(false);
+  const { styles } = useStyle({ isOverflowing });
+
+  const validatedLanguage = Object.keys(changeLog).includes(lang) ? lang : 'en';
+  const C = changeLog[validatedLanguage];
+
+  const checkOverflow = React.useMemo(
+    () =>
+      debounce(() => {
+        const hasOverflow =
+          containerRef.current?.scrollHeight! > containerRef.current?.clientHeight!;
+        setIsOverflowing(hasOverflow);
+      }),
+    [],
+  );
+
+  // 检测溢出状态
+  React.useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    checkOverflow();
+
+    const resizeObserver = new ResizeObserver(checkOverflow);
+    resizeObserver.observe(container);
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
+  return (
+    <div className={styles.container} ref={containerRef}>
+      <C />
+    </div>
+  );
 };
 
 const ChangeModal = () => {
@@ -49,7 +127,7 @@ const ChangeModal = () => {
     return currentWeight > lastVisitedWeight;
   }, [currentVersion, lastVisitedVersion]);
 
-  const [open, updateOpen] = React.useState(hasNewVersion);
+  const [open, updateOpen] = React.useState(false);
 
   function handleClose() {
     localStorage.setItem(STORAGE_KEY, currentVersion);
@@ -57,10 +135,32 @@ const ChangeModal = () => {
     updateOpen(false);
   }
 
+  const hasChinesePreference = navigator.languages.some((lang) => lang.startsWith('zh'));
+  const isChineseMirror = ['ant-design.gitee.io', 'ant-design.antgroup.com'].includes(
+    window.location.hostname,
+  );
+  const showMirror = hasChinesePreference && !isChineseMirror;
+
   const fullChangeLog = () => {
     handleClose();
     navigate(`/changelog${lang === 'cn' ? '-cn' : ''}`);
   };
+
+  React.useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+    function showModal() {
+      // 避免 modal 内容样式未加载完成时的闪烁
+      timer = setTimeout(() => {
+        updateOpen(true);
+      }, 1000);
+    }
+
+    window.addEventListener('load', showModal);
+    return function cleanup() {
+      window.removeEventListener('load', showModal);
+      timer && clearTimeout(timer);
+    };
+  }, [hasNewVersion]);
 
   // ========== render ==========
   if (!hasNewVersion) return null;
@@ -69,23 +169,46 @@ const ChangeModal = () => {
     <Modal
       title={[locale.title, ' 🎉']}
       open={open}
+      width={`min(90vw, 800px)`}
       centered
       onCancel={handleClose}
       onOk={handleClose}
       styles={{
         mask: {
-          backdropFilter: 'blur(4px)',
+          backdropFilter: 'blur(2px)',
+        },
+        body: {
+          padding: 0,
         },
       }}
       okText={locale.gettingStarted}
       footer={(_, { OkBtn }) => (
-        <Flex align="center" gap={8} justify="end">
-          <Button onClick={fullChangeLog}>{locale.fullChangeLog}</Button>
-          <OkBtn />
+        <Flex align="center" gap="middle" justify="space-between">
+          {showMirror ? (
+            <Tooltip title={locale.mirrorDesc}>
+              <Button
+                icon={'🇨🇳'}
+                onClick={() => {
+                  window.location.href = window.location.href.replace(
+                    window.location.host,
+                    'ant-design.antgroup.com',
+                  );
+                }}
+              >
+                {locale.mirror}
+              </Button>
+            </Tooltip>
+          ) : (
+            <span style={{ visibility: 'hidden' }} />
+          )}
+          <Flex gap="middle">
+            <Button onClick={fullChangeLog}>{locale.fullChangeLog}</Button>
+            <OkBtn />
+          </Flex>
         </Flex>
       )}
     >
-      TODO: changelog
+      <ChangeLog />
     </Modal>
   );
 };
