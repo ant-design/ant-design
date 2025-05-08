7c9014f3e580d064092494d340910b16f2909157
二货机器人 <smith3816@gmail.com>
2025-05-08 15:02:21 +0800

    chore: status text

--- components/steps/style/status.ts
+++ components/steps/style/status.ts
@@ -113,7 +113,7 @@ const getStatusStyle = (
   };
 };
 
-const genStatusStyle: GenerateStyle<StepsToken, CSSObject> = (token) => {
+const genStatusStyle1: GenerateStyle<StepsToken, CSSObject> = (token) => {
   const {
     componentCls,
     colorTextDisabled,
@@ -318,6 +318,158 @@ const genStatusStyle: GenerateStyle<StepsToken, CSSObject> = (token) => {
         token.colorErrorHover,
       ),
     ],
+  };
+};
+
+const genStatusStyle: GenerateStyle<StepsToken, CSSObject> = (token) => {
+  const {
+    componentCls,
+    colorTextDisabled,
+    colorTextLightSolid,
+    colorPrimary,
+    colorTextLabel,
+    colorError,
+    colorText,
+    colorTextDescription,
+    colorFillContent,
+    controlItemBgActive,
+    colorBgContainer,
+    colorPrimaryHover,
+  } = token;
+
+  const itemCls = `${componentCls}-item`;
+
+  return {
+    [componentCls]: {
+      // ========================= Template =========================
+      '--steps-item-solid-line-color': '#000',
+      '--steps-item-title-color': '#000',
+      '--steps-item-description-color': '#000',
+      '--steps-item-description-active-color': '#000',
+      '--steps-item-text-hover-color': '#000',
+      '--steps-item-icon-bg-color': '#000',
+      '--steps-item-icon-border-color': '#000',
+      '--steps-item-icon-text-color': '#fff',
+      '--steps-item-icon-bg-hover-color': '#000',
+      '--steps-item-icon-border-hover-color': '#000',
+      '--steps-item-icon-text-hover-color': '#000',
+      '--steps-item-icon-dot-bg-color': '#000',
+      '--steps-item-icon-dot-border-color': '#000',
+
+      [`${itemCls}-custom ${itemCls}-icon`]: {
+        // color: solidLineColor,
+        color: `var(--steps-item-solid-line-color)`,
+      },
+
+      [`${itemCls}-title`]: {
+        // color: titleColor,
+        color: `var(--steps-item-title-color)`,
+      },
+
+      [`${itemCls}-description`]: {
+        // color: descriptionColor,
+        color: `var(--steps-item-description-color)`,
+      },
+
+      [`${itemCls}-active ${itemCls}-description`]: {
+        // color: activeDescriptionColor,
+        color: `var(--steps-item-description-active-color)`,
+      },
+
+      // Hover
+      [`${itemCls}:not(${itemCls}-active) ${itemCls}-wrapper[role='button']:hover`]: {
+        [`${itemCls}-title, ${itemCls}-description, &${itemCls}-active ${itemCls}-description`]: {
+          // color: hoverTitleColor,
+          color: `var(--steps-item-text-hover-color)`,
+        },
+      },
+
+      // [`${itemCls}-rail-${status}`]: {
+      [`${itemCls}-rail`]: {
+        // background: solidLineColor,
+        background: `var(--steps-item-solid-line-color)`,
+      },
+
+      // Not dot
+      [`&:not(${componentCls}-dot)`]: {
+        // [`${itemCls}-${status}:not(${itemCls}-custom)`]: {
+        [`${itemCls}:not(${itemCls}-custom)`]: {
+          [`${itemCls}-icon`]: {
+            // background: iconBgColor,
+            background: `var(--steps-item-icon-bg-color)`,
+            // borderColor: iconBorderColor,
+            borderColor: `var(--steps-item-icon-border-color)`,
+            // color: iconTextColor,
+            color: `var(--steps-item-icon-text-color)`,
+          },
+
+          // Hover
+          [`&:not(${itemCls}-active) ${itemCls}-wrapper[role='button']:hover`]: {
+            [`${itemCls}-icon`]: {
+              // background: hoverIconBgColor,
+              background: `var(--steps-item-icon-bg-hover-color)`,
+              // borderColor: hoverIconBorderColor,
+              borderColor: `var(--steps-item-icon-border-hover-color)`,
+              // color: hoverIconTextColor,
+              color: `var(--steps-item-icon-text-hover-color)`,
+            },
+          },
+        },
+      },
+
+      // Dot
+      [`&${componentCls}-dot`]: {
+        // [`${itemCls}-${status}`]: {
+        [itemCls]: {
+          [`${itemCls}-icon-dot`]: {
+            // background: dotIconBgColor,
+            background: `var(--steps-item-icon-dot-bg-color)`,
+            // borderColor: dotIconBorderColor,
+            borderColor: `var(--steps-item-icon-dot-border-color)`,
+          },
+        },
+      },
+
+      // ========================== Shared ==========================
+      // Wait
+      [`${itemCls}-${STATUS_WAIT}`]: {
+        '--steps-item-solid-line-color': colorTextDisabled,
+        '--steps-item-title-color': colorText,
+        '--steps-item-description-color': colorTextDescription,
+        '--steps-item-description-active-color': colorText,
+        '--steps-item-text-hover-color': colorPrimaryHover,
+      },
+
+      // Process
+      [`${itemCls}-${STATUS_PROCESS}`]: {
+        '--steps-item-solid-line-color': colorPrimary,
+        '--steps-item-title-color': colorText,
+        '--steps-item-description-color': colorTextDescription,
+        '--steps-item-description-active-color': colorText,
+        '--steps-item-text-hover-color': colorPrimaryHover,
+      },
+
+      // Finish
+      [`${itemCls}-${STATUS_FINISH}`]: {
+        '--steps-item-solid-line-color': colorPrimary,
+        '--steps-item-title-color': colorText,
+        '--steps-item-description-color': colorTextDescription,
+        '--steps-item-description-active-color': colorText,
+        '--steps-item-text-hover-color': colorPrimaryHover,
+      },
+
+      // Error
+      [`${itemCls}-${STATUS_ERROR}`]: {
+        '--steps-item-solid-line-color': colorError,
+        '--steps-item-title-color': colorError,
+        '--steps-item-description-color': colorError,
+        '--steps-item-description-active-color': colorError,
+        '--steps-item-text-hover-color': token.colorErrorHover,
+      },
+
+      // ========================== Filled ==========================
+      // ========================= Outlined =========================
+    },
   };
 };
 
