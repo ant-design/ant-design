a02c6ac2c9d2641964fb0ea986cf46e568060603
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-05-08 16:13:31 +0800

    chore: update

--- .dumi/scripts/mirror-modal.js
+++ .dumi/scripts/mirror-modal.js
@@ -1,6 +1,4 @@
 (function createMirrorModal() {
-  const isDebug = window.localStorage.getItem('DEBUG') === 'antd';
-  const ANTD_DOT_NOT_SHOW_MIRROR_MODAL = 'ANT_DESIGN_DO_NOT_OPEN_MIRROR_MODAL';
   /**
    * If you do not wish to be prompted by the modal again,
    * you can set the next display timestamp manually to a future date.
@@ -8,37 +6,63 @@
    * For example, the line below sets it to 6 months from now.
    */
   // window.localStorage.setItem("ANT_DESIGN_DO_NOT_OPEN_MIRROR_MODAL", Date.now() + 6 * 30 * 24 * 60 * 60 * 1000);
-  let nextShowTimestamp = window.localStorage.getItem(ANTD_DOT_NOT_SHOW_MIRROR_MODAL);
+  const ANTD_DOT_NOT_SHOW_MIRROR_MODAL = 'ANT_DESIGN_DO_NOT_OPEN_MIRROR_MODAL';
+  const isDebug = window.localStorage.getItem('DEBUG') === 'antd';
 
-  if (nextShowTimestamp === null) {
-    nextShowTimestamp = Date.now() - 0.5 * 60 * 1000; // Set to 30 seconds ago
-    window.localStorage.setItem(ANTD_DOT_NOT_SHOW_MIRROR_MODAL, nextShowTimestamp);
+  function onReady() {
+    window.removeEventListener('load', onReady);
+    if (check()) createModal();
+  }
+  window.addEventListener('load', onReady);
+
+  function check() {
+    let nextShowTimestamp = window.localStorage.getItem(ANTD_DOT_NOT_SHOW_MIRROR_MODAL);
+
+    if (nextShowTimestamp === null) {
+      nextShowTimestamp = Date.now() - 0.5 * 60 * 1000; // Set to 30 seconds ago
+      window.localStorage.setItem(ANTD_DOT_NOT_SHOW_MIRROR_MODAL, nextShowTimestamp);
+    }
+
+    // Normalize
+    if (/^\d+$/.test(nextShowTimestamp)) {
+      nextShowTimestamp = parseInt(nextShowTimestamp, 10);
+    } else {
+      nextShowTimestamp = new Date(nextShowTimestamp).getTime();
+    }
+
+    const navEntry = performance.getEntriesByType('navigation')[0];
+    let domReadyTime = Infinity;
+    if (navEntry) {
+      domReadyTime = navEntry.startTime - navEntry.domContentLoadedEventEnd;
+    }
+
+    const condition = [
+      // Check the DOM ready time is greater than 3 seconds.
+      domReadyTime < -3000,
+      // Check if the current time is greater than the next show timestamp
+      Date.now() > nextShowTimestamp,
+      // Check if the browser language is Chinese
+      navigator.languages.includes('zh') || navigator.languages.includes('zh-CN'),
+      // Check if the URL path ends with -cn
+      /-cn\/?$/.test(window.location.pathname),
+      // chinese mirror URL
+      !['ant-design.gitee.io', 'ant-design.antgroup.com'].includes(window.location.hostname),
+      // PR review URL
+      !window.location.host.includes('surge'),
+      // development mode
+      isDebug ? true : !['127.0.0.1', 'localhost'].includes(window.location.hostname),
+    ];
+
+    if (isDebug) {
+      console.log('🚀 [mirror-modal]', { nextShowTimestamp, domReadyTime });
+      console.log('🚀 [mirror-modal] condition:', condition);
+    }
+
+    // Check if all conditions are met
+    return condition.every(Boolean);
   }
 
-  const condition = [
-    // Check if the current time is greater than the next show timestamp
-    Date.now() > new Date(nextShowTimestamp).getTime(),
-    // Check if the browser language is Chinese
-    navigator.languages.includes('zh') || navigator.languages.includes('zh-CN'),
-    // Check if the URL path ends with -cn
-    /-cn\/?$/.test(window.location.pathname),
-    // chinese mirror URL
-    !['ant-design.gitee.io', 'ant-design.antgroup.com'].includes(window.location.hostname),
-    // PR review URL
-    !window.location.host.includes('surge'),
-    // development mode
-    isDebug ? true : !['127.0.0.1', 'localhost'].includes(window.location.hostname),
-  ];
-
-  if (isDebug) {
-    console.log('🚀 [mirror-modal] nextShowTimestamp:', nextShowTimestamp);
-    console.log('🚀 [mirror-modal] condition:', condition);
-  }
-
-  // Check if all conditions are met
-  const shouldShowMirrorModal = condition.every(Boolean);
-
-  if (shouldShowMirrorModal) {
+  function createModal() {
     const style = document.createElement('style');
     style.innerHTML = `
   @keyframes mirror-fade-in {
