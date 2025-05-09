eb95cce2cb9fb7ac042f6fbd7aacb958fdf44b99
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-05-09 13:22:32 +0800

    chore: add version meta

--- .dumirc.ts
+++ .dumirc.ts
@@ -59,6 +59,7 @@ export default defineConfig({
     { name: 'build-time', content: Date.now().toString() },
     // https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
     { name: 'build-hash', content: process.env.GITHUB_SHA ?? 'unknown' },
+    { name: 'antd-version', content: version },
   ],
   analytics: {
     ga_v2: 'UA-72788897-1',
