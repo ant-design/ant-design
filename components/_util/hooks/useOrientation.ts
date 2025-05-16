5b46957b95aded2a1165b7e09ce0a3d2aaf4d15f
刘欢 <lh01217311@antgroup.com>
2025-05-16 13:34:00 +0800

    feat:  useOrientation ,change params name

--- components/_util/hooks/useOrientation.ts
+++ components/_util/hooks/useOrientation.ts
@@ -4,20 +4,20 @@ export type Orientation = 'horizontal' | 'vertical';
 
 export default function useOrientation(
   orientation?: Orientation,
-  defaultVertical?: boolean,
-  oldOrientation?: Orientation,
+  vertical?: boolean,
+  legacyDirection?: Orientation,
 ): [Orientation, boolean] {
   return useMemo(() => {
     const validOrientation = orientation === 'horizontal' || orientation === 'vertical';
     let mergedOrientation: Orientation = 'horizontal';
     if (validOrientation) {
       mergedOrientation = orientation;
-    } else if (typeof defaultVertical === 'boolean') {
-      mergedOrientation = defaultVertical ? 'vertical' : 'horizontal';
+    } else if (typeof vertical === 'boolean') {
+      mergedOrientation = vertical ? 'vertical' : 'horizontal';
     } else {
-      mergedOrientation = oldOrientation ?? 'horizontal';
+      mergedOrientation = legacyDirection ?? 'horizontal';
     }
 
     return [mergedOrientation, mergedOrientation === 'vertical'];
-  }, [oldOrientation, orientation, defaultVertical]);
+  }, [legacyDirection, orientation, vertical]);
 }
