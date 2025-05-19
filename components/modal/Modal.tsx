3b9f6bf938b2bae82d3487af963e3ecbe5447b3f
lijianan <574980606@qq.com>
2025-05-19 16:13:58 +0800

    refactor(modal): update destroyOnHidden API (#53865)
    * refactor(modal): update destroyOnHidden API
    
    * refactor(modal): update destroyOnHidden API
    
    ---------
    
    Co-authored-by: thinkasany <480968828@qq.com>

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -234,8 +234,7 @@ const Modal: React.FC<ModalProps> = (props) => {
             footer: { ...contextStyles.footer, ...modalStyles?.footer },
           }}
           panelRef={panelRef}
-          // TODO: In the future, destroyOnClose in rc-dialog needs to be upgrade to destroyOnHidden
-          destroyOnClose={destroyOnHidden ?? destroyOnClose}
+          destroyOnHidden={destroyOnHidden ?? destroyOnClose}
         >
           {loading ? (
             <Skeleton
