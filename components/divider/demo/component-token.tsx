099f59f90e2a5b6e36e331a4e2aef5186c087271
刘欢 <lh01217311@antgroup.com>
2025-05-30 14:59:50 +0800

    feat: change demos orientationMargin-> styles.content.margin

--- components/divider/demo/component-token.tsx
+++ components/divider/demo/component-token.tsx
@@ -38,15 +38,15 @@ const App: React.FC = () => (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider titlePlacement="start" orientationMargin="0">
-      Left Text with 0 orientationMargin
+    <Divider titlePlacement="start" styles={{ content: { margin: '0' } }}>
+      Left Text margin with 0
     </Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
       probare, quae sunt a te dicta? Refert tamen, quo modo.
     </p>
-    <Divider titlePlacement="end" orientationMargin={50}>
-      Right Text with 50px orientationMargin
+    <Divider titlePlacement="end" styles={{ content: { margin: '0 50px' } }}>
+      Right Text margin with 50px
     </Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
