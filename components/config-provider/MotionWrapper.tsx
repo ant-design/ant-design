d3f0e4faa7bf1f95c8645ac1fd85df8f02dbbde2
Wanpan <wanpan96@163.com>
2025-06-03 15:07:12 +0800

    feat: add MotionCacheContext

--- components/config-provider/MotionWrapper.tsx
+++ components/config-provider/MotionWrapper.tsx
@@ -3,14 +3,30 @@ import { Provider as MotionProvider } from 'rc-motion';
 
 import { useToken } from '../theme/internal';
 
+const MotionCacheContext = React.createContext({ parentMotion: true });
+if (process.env.NODE_ENV !== 'production') {
+  MotionCacheContext.displayName = 'MotionCacheContext';
+}
+
 export interface MotionWrapperProps {
   children?: React.ReactNode;
 }
 
 export default function MotionWrapper(props: MotionWrapperProps): React.ReactElement {
+  const { parentMotion } = React.use(MotionCacheContext);
+
   const { children } = props;
   const [, token] = useToken();
   const { motion } = token;
 
-  return <MotionProvider motion={motion}>{children}</MotionProvider>;
+  let node = children as React.ReactElement;
+  if (parentMotion !== motion) {
+    node = <MotionProvider motion={motion}>{children}</MotionProvider>;
+  }
+
+  return (
+    <MotionCacheContext.Provider value={{ parentMotion: motion }}>
+      {node}
+    </MotionCacheContext.Provider>
+  );
 }
