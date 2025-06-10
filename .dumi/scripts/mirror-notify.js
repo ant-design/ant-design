ae038de3fe17197998cff5de3fc2aac2925280c8
afc163 <afc163@gmail.com>
2025-06-10 18:17:26 +0800

    update

--- .dumi/scripts/mirror-notify.js
+++ .dumi/scripts/mirror-notify.js
@@ -142,7 +142,7 @@
       <div class="${prefixCls}-title">🇨🇳 访问不畅？试试国内镜像</div>
       <div class="${prefixCls}-message">
         国内镜像站点可以帮助您更快地访问文档和资源。<br>
-        请尝试访问 <a id="antd-mirror-notify-link" href="${officialChinaMirror}">国内镜像站点</a>，以获得更好的体验。
+        请尝试访问 <a id="${prefixCls}-action" href="${officialChinaMirror}">国内镜像站点</a>，以获得更好的体验。
       </div>
       <div class="${prefixCls}-footer">
         <button class="${prefixCls}-action">🚀 立即前往</button>
@@ -169,9 +169,7 @@
       removeNotify();
     };
 
-    notify
-      .querySelector(`.${prefixCls}-action, #antd-mirror-notify-link`)
-      .addEventListener('click', goToChinaMirror);
+    notify.querySelector(`.${prefixCls}-action`).addEventListener('click', goToChinaMirror);
 
     const refreshRate = 50; // ms
     const duration = 10; // s
