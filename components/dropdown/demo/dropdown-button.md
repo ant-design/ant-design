4667d2228e4e394992e0a67fbc560e08e9409351
Meet-student <1875694521@qq.com>
2025-06-10 23:28:26 +0800

    chore: [v6] Change DropDown icon-debug demo

---
+++ components/dropdown/demo/dropdown-button.md
@@ -0,0 +1,7 @@
+## zh-CN
+
+左边是按钮，右边是额外的相关功能菜单。可设置 `icon` 属性来修改右边的图标。
+
+## en-US
+
+A button is on the left, and a related functional menu is on the right. You can set the icon property to modify the icon of right.
--- components/dropdown/index.tsx
+++ components/dropdown/index.tsx
@@ -1,7 +1,14 @@
 import InternalDropdown from './dropdown';
+import DropdownButton from './dropdown-button';
 
 export type { DropdownProps as DropDownProps, DropdownProps } from './dropdown';
+export type { DropdownButtonProps, DropdownButtonType } from './dropdown-button';
 
-const Dropdown = InternalDropdown as typeof InternalDropdown;
+const Dropdown = InternalDropdown as typeof InternalDropdown & {
+  Button: typeof DropdownButton;
+};
+
+/** @deprecated Please use Space.Compact + Dropdown + Button instead */
+Dropdown.Button = DropdownButton;
 
 export default Dropdown;
