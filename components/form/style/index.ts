af666b45eb6d54d483dcd0f5149fd699b973eb44
tellt@126.com <zhujunjie@sundear.com>
2025-06-21 12:43:32 +0800

    fix-54025: Fixed height issue of .ant-form-item-control.

--- components/form/style/index.ts
+++ components/form/style/index.ts
@@ -603,6 +603,7 @@ const genItemVerticalStyle: GenerateStyle<FormToken> = (token) => {
 
       [`${formItemCls}-control`]: {
         width: '100%',
+        minHeight: 'auto',
       },
       [`${formItemCls}-control-input`]: {
         minHeight: 'auto',
