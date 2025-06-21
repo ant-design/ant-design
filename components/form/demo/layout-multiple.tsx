cd79054114383c833f7825870734e77a0d128cba
tellt@126.com <zhujunjie@sundear.com>
2025-06-21 09:20:53 +0800

    fix-54026: add test case

--- components/form/demo/layout-multiple.tsx
+++ components/form/demo/layout-multiple.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Divider, Form, Input } from 'antd';
 
 const App: React.FC = () => (
   <>
+    <Divider plain>input</Divider>
     <Form
       name="layout-multiple-horizontal"
       layout="horizontal"
@@ -40,6 +41,46 @@ const App: React.FC = () => (
         rules={[{ required: true }]}
       >
         <Input />
+      </Form.Item>
+    </Form>
+    <Divider plain>input.TextArea</Divider>
+    <Form
+      name="layout-multiple-horizontal-textarea"
+      layout="horizontal"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 20 }}
+    >
+      <Form.Item label="horizontal" name="horizontalTextArea" rules={[{ required: true }]}>
+        <Input.TextArea rows={5} />
+      </Form.Item>
+      <Form.Item
+        layout="vertical"
+        label="vertical"
+        name="verticalTextArea"
+        rules={[{ required: true }]}
+        labelCol={{ span: 24 }}
+        wrapperCol={{ span: 24 }}
+      >
+        <Input.TextArea rows={5} />
+      </Form.Item>
+    </Form>
+    <br />
+    <Form
+      name="layout-multiple-vertical-textarea"
+      layout="vertical"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 20 }}
+    >
+      <Form.Item label="vertical" name="verticalTextArea" rules={[{ required: true }]}>
+        <Input.TextArea rows={5} />
+      </Form.Item>
+      <Form.Item
+        layout="horizontal"
+        label="horizontal"
+        name="horizontalTextArea"
+        rules={[{ required: true }]}
+      >
+        <Input.TextArea rows={5} />
       </Form.Item>
     </Form>
   </>
