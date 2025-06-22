52ecf2fd564226161cd9e2c4ed1be60764af95e9
遇见 <1875694521@qq.com>
2025-06-22 10:22:15 +0800

    chore: update demo

--- components/badge/demo/mix.tsx
+++ components/badge/demo/mix.tsx
@@ -36,7 +36,6 @@ const App: React.FC = () => (
     <Space size="middle" wrap>
       <Badge count={0} showZero />
       <Badge count={0} showZero color="blue" />
-      <Badge count={0} color="#f0f" />
       <Badge count={0} showZero color="#f0f" />
       <Badge count={0} showZero>
         <Avatar shape="square" size="large" />
