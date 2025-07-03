d4a3495c5d109f65b837765748562a9522fe01c1
afc163 <afc163@gmail.com>
2025-07-03 11:50:22 +0800

    style

--- .dumi/theme/common/styles/Reset.tsx
+++ .dumi/theme/common/styles/Reset.tsx
@@ -8,17 +8,87 @@ export default () => {
   return (
     <Global
       styles={css`
-        @font-face { font-family: "Inter"; font-style: normal; font-weight: 300; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*pnhuS6LeI6IAAAAARuAAAAgAegCCAQ/Inter-Light.woff2") format("woff2"); }
-@font-face { font-family: "Inter"; font-style: normal; font-weight: 400; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*dkVaS5m9cUwAAAAARtAAAAgAegCCAQ/Inter-Regular.woff2") format("woff2"); }
-@font-face { font-family: "Inter"; font-style: normal; font-weight: 500; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*SujlQqXwMsEAAAAARwAAAAgAegCCAQ/Inter-Medium.woff2") format("woff2"); }
-@font-face { font-family: "Inter"; font-style: normal; font-weight: 600; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*P80xR7K7mdYAAAAARwAAAAgAegCCAQ/Inter-SemiBold.woff2") format("woff2"); }
-@font-face { font-family: 'Inter'; font-style: normal; font-weight: 700; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*C3NVT5SPeXcAAAAARwAAAAgAegCCAQ/Inter-Bold.woff2") format("woff2"); }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 300;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*pnhuS6LeI6IAAAAARuAAAAgAegCCAQ/Inter-Light.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*dkVaS5m9cUwAAAAARtAAAAgAegCCAQ/Inter-Regular.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*SujlQqXwMsEAAAAARwAAAAgAegCCAQ/Inter-Medium.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 600;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*P80xR7K7mdYAAAAARwAAAAgAegCCAQ/Inter-SemiBold.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 700;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*C3NVT5SPeXcAAAAARwAAAAgAegCCAQ/Inter-Bold.woff2")
+            format("woff2");
+        }
 
-@font-face { font-family: "AlibabaSans"; font-style: normal; font-weight: 300; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*1GSgSYDD_aIAAAAAQsAAAAgAegCCAQ/AlibabaSans-Light.woff2") format("woff2"); }
-@font-face { font-family: "AlibabaSans"; font-style: normal; font-weight: 400; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*2zEUQqnPNesAAAAAQtAAAAgAegCCAQ/AlibabaSans-Regular.woff2") format("woff2"); }
-@font-face { font-family: "AlibabaSans"; font-style: normal; font-weight: 500; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Medium.woff2") format("woff2"); }
-@font-face { font-family: "AlibabaSans"; font-style: normal; font-weight: 600; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Bold.woff2") format("woff2"); }
-@font-face { font-family: "AlibabaSans"; font-style: normal; font-weight: 600; font-display: swap; src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Heavy.woff2") format("woff2"); }
+        @font-face {
+          font-family: "AlibabaSans";
+          font-style: normal;
+          font-weight: 300;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*1GSgSYDD_aIAAAAAQsAAAAgAegCCAQ/AlibabaSans-Light.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "AlibabaSans";
+          font-style: normal;
+          font-weight: 400;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*2zEUQqnPNesAAAAAQtAAAAgAegCCAQ/AlibabaSans-Regular.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "AlibabaSans";
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Medium.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "AlibabaSans";
+          font-style: normal;
+          font-weight: 600;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Bold.woff2")
+            format("woff2");
+        }
+        @font-face {
+          font-family: "AlibabaSans";
+          font-style: normal;
+          font-weight: 600;
+          font-display: swap;
+          src: url("//mdn.alipayobjects.com/huamei_iwk9zp/afts/file/A*E_cxRbMlZqUAAAAAQuAAAAgAegCCAQ/AlibabaSans-Heavy.woff2")
+            format("woff2");
+        }
 
         html {
           direction: initial;
