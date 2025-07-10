745da0d42e81154f0e490bbbd718dca1ac8a76e7
刘欢 <lh01217311@antgroup.com>
2025-07-10 17:11:48 +0800

    test: change demo

--- components/progress/demo/dashboard.tsx
+++ components/progress/demo/dashboard.tsx
@@ -7,42 +7,37 @@ const App: React.FC = () => {
   const [gapPlacement, setGapPlacement] = useState<GapPlacement>('bottom');
   const [gapDegree, setGapDegree] = useState<number>(50);
   return (
-    <Flex vertical gap="middle">
-      <Flex vertical gap="small">
-        <div>
-          gapDegree:
-          <Segmented
-            options={[
-              { label: 50, value: 50 },
+    <Flex vertical gap="large">
+      <div>
+        gapDegree:
+        <Segmented
+          options={[
+            { label: 50, value: 50 },
 
-              { label: 100, value: 100 },
-            ]}
-            defaultValue={50}
-            onChange={(value: number) => {
-              setGapDegree(value);
-            }}
-          />
-        </div>
-        <Progress type="dashboard" percent={75} gapDegree={gapDegree} />
-      </Flex>
-      <Flex vertical gap="small">
-        <div>
-          gapPlacement:
-          <Segmented
-            options={[
-              { label: 'start', value: 'start' },
-              { label: 'end', value: 'end' },
-              { label: 'top', value: 'top' },
-              { label: 'bottom', value: 'bottom' },
-            ]}
-            defaultValue="bottom"
-            onChange={(value: GapPlacement) => {
-              setGapPlacement(value);
-            }}
-          />
-        </div>
-        <Progress type="dashboard" gapDegree={gapDegree} percent={30} gapPlacement={gapPlacement} />
-      </Flex>
+            { label: 100, value: 100 },
+          ]}
+          defaultValue={50}
+          onChange={(value: number) => {
+            setGapDegree(value);
+          }}
+        />
+      </div>
+      <div>
+        gapPlacement:
+        <Segmented
+          options={[
+            { label: 'start', value: 'start' },
+            { label: 'end', value: 'end' },
+            { label: 'top', value: 'top' },
+            { label: 'bottom', value: 'bottom' },
+          ]}
+          defaultValue="bottom"
+          onChange={(value: GapPlacement) => {
+            setGapPlacement(value);
+          }}
+        />
+      </div>
+      <Progress type="dashboard" gapDegree={gapDegree} percent={30} gapPlacement={gapPlacement} />
     </Flex>
   );
 };
