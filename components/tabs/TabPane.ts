253d9caeb6d573f4b2b1065938f6d6dab9b0933e
smallbun <30397655+leshalv@users.noreply.github.com>
2025-07-26 17:49:45 +0800

    Update TabPane.ts
    Signed-off-by: smallbun <30397655+leshalv@users.noreply.github.com>

--- components/tabs/TabPane.ts
+++ components/tabs/TabPane.ts
@@ -1,11 +1,20 @@
 import type * as React from 'react';
-import type { TabPaneProps } from 'rc-tabs/lib/TabPanelList/TabPane';
+import type { TabPaneProps as RcTabPaneProps } from 'rc-tabs/lib/TabPanelList/TabPane';
 
 const TabPane: React.FC<TabPaneProps> = () => null;
 
 if (process.env.NODE_ENV !== 'production') {
   TabPane.displayName = 'DeprecatedTabPane';
 }
+
+type TabPaneProps = {
+  /** @deprecated Please use `destroyOnHidden` instead */
+  destroyInactiveTabPane?: boolean;
+  /**
+   * @since 5.25.0
+   */
+  destroyOnHidden?: boolean;
+} & RcTabPaneProps;
 
 export type { TabPaneProps };
 
