c1067d83ed81acab766f5664f17cb90fc48888d7
Guo Yunhe <guoyunhe.gyh@alibaba-inc.com>
2025-07-28 15:40:21 +0800

    docs: support hide layout param (#54398)

--- .dumi/theme/layouts/SidebarLayout/index.tsx
+++ .dumi/theme/layouts/SidebarLayout/index.tsx
@@ -5,6 +5,7 @@ import { createStyles } from 'antd-style';
 import CommonHelmet from '../../common/CommonHelmet';
 import Content from '../../slots/Content';
 import Sidebar from '../../slots/Sidebar';
+import { useSearchParams } from 'dumi';
 
 const useStyle = createStyles(({ css, cssVar }) => ({
   main: css`
@@ -16,6 +17,9 @@ const useStyle = createStyles(({ css, cssVar }) => ({
 }));
 
 const SidebarLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  const [searchParams] = useSearchParams();
+  const hideLayout = searchParams.get('layout') === 'false';
+
   const { styles } = useStyle();
   return (
     <main className={styles.main}>
