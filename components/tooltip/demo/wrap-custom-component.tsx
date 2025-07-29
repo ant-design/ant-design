f28b933d54eac9b7c80a05b480209facf370eebe
lijianan <574980606@qq.com>
2025-07-30 01:40:41 +0800

    update demo

--- components/tooltip/demo/wrap-custom-component.tsx
+++ components/tooltip/demo/wrap-custom-component.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Tooltip } from 'antd';
 
-const ComponentWithEvents = React.forwardRef<
-  HTMLSpanElement,
-  React.HTMLAttributes<HTMLSpanElement>
->((props, ref) => (
-  <span ref={ref} {...props}>
-    This text is inside a component with the necessary events exposed.
-  </span>
-));
+const ComponentWithEvents = React.forwardRef<HTMLSpanElement, React.HTMLAttributes<HTMLElement>>(
+  (props, ref) => (
+    <span ref={ref} {...props}>
+      This text is inside a component with the necessary events exposed.
+    </span>
+  ),
+);
 
 const App: React.FC = () => (
   <Tooltip title="prompt text">
