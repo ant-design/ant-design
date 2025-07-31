6b8ec4238af5d9aedd16d89209ed6dd21489712a
thinkasany <480968828@qq.com>
2025-07-31 15:55:37 +0800

    Update components/menu/interface.ts
    Signed-off-by: thinkasany <480968828@qq.com>

--- components/menu/interface.ts
+++ components/menu/interface.ts
@@ -6,7 +6,7 @@ import type {
 } from 'rc-menu/lib/interface';
 
 export type DataAttributes = {
-  [Key in `data-${string}`]: unknown; // data- attributes can be anything but we avoid using `any`. As we don't use the attributes anywhere ourselves, we don't care what type they actually have, hence `unknown`
+  [Key in `data-${string}`]: unknown;
 };
 
 export interface MenuItemType extends RcMenuItemType, DataAttributes {
