e82a7fae8c27983a9bedeadcae89775100486561
afc163 <afc163@gmail.com>
2025-08-04 14:17:54 +0800

    chore: fix Split eslint error (#54578)
    * style: 添加 eslint 忽略注释避免 no-leaked-event-listener 警告
    
    * docs: 补充中文文档句号并同步英文格式

--- components/splitter/SplitBar.tsx
+++ components/splitter/SplitBar.tsx
@@ -154,6 +154,7 @@ const SplitBar: React.FC<SplitBarProps> = (props) => {
     };
 
     for (const [event, handler] of Object.entries(eventHandlerMap)) {
+      // eslint-disable-next-line react-web-api/no-leaked-event-listener
       window.addEventListener(event, handler);
     }
 
