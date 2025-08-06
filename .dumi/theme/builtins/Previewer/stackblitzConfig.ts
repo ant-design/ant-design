792f30e2d51edba0b7fb14fbeeed7f2d5e0ab3d9
crazyair <645381995@qq.com>
2025-08-06 12:12:21 +0800

    feat: add stackblitz node

--- .dumi/theme/builtins/Previewer/stackblitzConfig.ts
+++ .dumi/theme/builtins/Previewer/stackblitzConfig.ts
@@ -64,11 +64,11 @@ const getStackblitzConfig = ({
     [`src/main.${suffix}`]: `import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@ant-design/v5-patch-for-react-19';
-import App from './App.${suffix}';
+import Demo from './demo.${suffix}';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Demo />
   </StrictMode>
 );`,
 
