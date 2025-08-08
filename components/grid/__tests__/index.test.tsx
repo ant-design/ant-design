9d05375bfda1ba27d8f8b1562212554486d60b70
liuqiang <qiang.liu@xinjifamily.com>
2025-08-08 14:26:41 +0800

    update

--- components/grid/__tests__/index.test.tsx
+++ components/grid/__tests__/index.test.tsx
@@ -116,6 +116,14 @@ describe('Grid', () => {
     expect((global as any).unsubscribeCnt).toEqual(called + 1);
   });
 
+  it('should work correct when gutter is string', () => {
+    const { container } = render(<Row gutter={'2rem'} />);
+    expect(container.querySelector('div')!).toHaveStyle({
+      'margin-left': 'calc(2rem / -2)',
+      'margin-right': 'calc(2rem / -2)',
+    });
+  });
+
   it('should work correct when gutter is object', () => {
     const { container } = render(<Row gutter={{ xs: 20 }} />);
     expect(container.querySelector('div')!.style.marginLeft).toEqual('-10px');
--- components/grid/col.tsx
+++ components/grid/col.tsx
@@ -118,8 +118,9 @@ const Col = React.forwardRef<HTMLDivElement, ColProps>((props, ref) => {
 
   const mergedStyle: React.CSSProperties = {};
   // Horizontal gutter use padding
-  if (gutter && gutter[0] > 0) {
-    const horizontalGutter = gutter[0] / 2;
+  if (gutter && gutter[0] != null && gutter[0] !== 0) {
+    const horizontalGutter =
+      typeof gutter[0] === 'number' ? `${gutter[0] / 2}px` : `calc(${gutter[0]} / 2)`;
     mergedStyle.paddingLeft = horizontalGutter;
     mergedStyle.paddingRight = horizontalGutter;
   }
