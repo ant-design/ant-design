4690c9eeaf9c00c48e80709cb4bdc98bb59b174b
遇见 <1875694521@qq.com>
2025-08-13 00:11:44 +0800

    update test

--- components/_util/__tests__/isPrimitive.test.ts
+++ components/_util/__tests__/isPrimitive.test.ts
@@ -1,17 +1,38 @@
+/* eslint-disable unicorn/new-for-builtins */
+/* eslint-disable no-new-wrappers */
 import isPrimitive from '../isPrimitive';
 
 describe('isPrimitive', () => {
-  it.each(['string', '', 123, 0, true, false, null, undefined, Symbol('test'), BigInt(123)])(
-    'should return true for primitive type: %p',
-    (primitive) => {
-      expect(isPrimitive(primitive)).toBe(true);
-    },
-  );
+  it('should return true for primitive types', () => {
+    expect(isPrimitive('string')).toBe(true);
+    expect(isPrimitive('')).toBe(true);
+    expect(isPrimitive(123)).toBe(true);
+    expect(isPrimitive(0)).toBe(true);
+    expect(isPrimitive(true)).toBe(true);
+    expect(isPrimitive(false)).toBe(true);
+    expect(isPrimitive(null)).toBe(true);
+    expect(isPrimitive(undefined)).toBe(true);
+    expect(isPrimitive(NaN)).toBe(true);
+    expect(isPrimitive(Infinity)).toBe(true);
+    expect(isPrimitive(Symbol('test'))).toBe(true);
+    expect(isPrimitive(BigInt(123))).toBe(true);
+    expect(isPrimitive(Symbol.for('test'))).toBe(true);
+  });
 
-  it.each([{}, [], new Date(), /regex/, () => {}, new Map(), new Set()])(
-    'should return false for non-primitive type: %p',
-    (nonPrimitive) => {
-      expect(isPrimitive(nonPrimitive)).toBe(false);
-    },
-  );
+  it('should return false for non-primitive types', () => {
+    expect(isPrimitive({})).toBe(false);
+    expect(isPrimitive([])).toBe(false);
+    expect(isPrimitive(new Date())).toBe(false);
+    expect(isPrimitive(/regex/)).toBe(false);
+    expect(isPrimitive(() => {})).toBe(false);
+    expect(isPrimitive(new Map())).toBe(false);
+    expect(isPrimitive(new Set())).toBe(false);
+    expect(isPrimitive(new WeakMap())).toBe(false);
+    expect(isPrimitive(new WeakSet())).toBe(false);
+    expect(isPrimitive(new String())).toBe(false);
+    expect(isPrimitive(new Number())).toBe(false);
+    expect(isPrimitive(new Boolean())).toBe(false);
+    expect(isPrimitive(class A {})).toBe(false);
+    expect(isPrimitive(Intl)).toBe(false);
+  });
 });
