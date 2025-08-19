19d5666baf983660c7fbc065d035a5a6cf4519b9
刘欢 <lh01217311@antgroup.com>
2025-08-19 16:45:42 +0800

    feat: demo basic

--- components/image/demo/basic.tsx
+++ components/image/demo/basic.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { ConfigProvider, Image } from 'antd';
+import { Image } from 'antd';
 
 const App: React.FC = () => (
-  <ConfigProvider
-    image={{
-      preview: {
-        previewMask: { enabled: true },
-      },
-    }}
-  >
-    <Image
-      width={200}
-      alt="basic"
-      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-      preview={{
-        previewMask: {
-          blur: false,
-        },
-      }}
-    />
-  </ConfigProvider>
+  <Image
+    width={200}
+    alt="basic"
+    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+  />
 );
 
 export default App;
