efe70297ff13eb7542ebcb8a2810be667c9e0df0
thinkasany <480968828@qq.com>
2025-08-27 14:23:12 +0800

    clean

--- components/image/demo/mask.tsx
+++ components/image/demo/mask.tsx
@@ -3,49 +3,47 @@ import { Image, Space } from 'antd';
 
 const App: React.FC = () => {
   return (
-    <>
-      <Space>
-        <Image
-          width={100}
-          alt="Default blur"
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-          preview={{
-            mask: true,
-            cover: (
-              <Space vertical align="center">
-                Default blur
-              </Space>
-            ),
-          }}
-        />
-        <Image
-          alt="Dimmed mask"
-          width={100}
-          src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-          preview={{
-            mask: { blur: false },
-            cover: (
-              <Space vertical align="center">
-                Dimmed mask
-              </Space>
-            ),
-          }}
-        />
-        <Image
-          width={100}
-          alt="No mask"
-          src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
-          preview={{
-            mask: false,
-            cover: (
-              <Space vertical align="center">
-                No mask
-              </Space>
-            ),
-          }}
-        />
-      </Space>
-    </>
+    <Space>
+      <Image
+        width={100}
+        alt="Default blur"
+        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        preview={{
+          mask: true,
+          cover: (
+            <Space vertical align="center">
+              Default blur
+            </Space>
+          ),
+        }}
+      />
+      <Image
+        alt="Dimmed mask"
+        width={100}
+        src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+        preview={{
+          mask: { blur: false },
+          cover: (
+            <Space vertical align="center">
+              Dimmed mask
+            </Space>
+          ),
+        }}
+      />
+      <Image
+        width={100}
+        alt="No mask"
+        src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+        preview={{
+          mask: false,
+          cover: (
+            <Space vertical align="center">
+              No mask
+            </Space>
+          ),
+        }}
+      />
+    </Space>
   );
 };
 
