8b40d84703dda65df9eb536425c070641e8154cc
Kenma2 <79778848+Kenma2@users.noreply.github.com>
2025-09-04 13:17:13 +0300

    Update customColor.tsx
    Signed-off-by: Kenma2 <79778848+Kenma2@users.noreply.github.com>

--- components/qr-code/demo/customColor.tsx
+++ components/qr-code/demo/customColor.tsx
@@ -7,9 +7,9 @@ const App: React.FC = () => {
   const { token } = useToken();
   return (
     <Space>
-      <QRCode value="https://ant.design/" color={token.colorSuccessText} />
+      <QRCode value="https://www.figma.com/proto/d02NfxgLpwYmlGDF4dvoai/UX-Analyze?page-id=0%3A1&node-id=304-1065&viewport=2522%2C-2234%2C0.35&t=1x5rHFPv8ch3tzBe-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=404%3A2139" color={token.colorSuccessText} />
       <QRCode
-        value="https://ant.design/"
+        value="https://www.figma.com/proto/d02NfxgLpwYmlGDF4dvoai/UX-Analyze?page-id=0%3A1&node-id=304-1065&viewport=2522%2C-2234%2C0.35&t=1x5rHFPv8ch3tzBe-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=404%3A2139"
         color={token.colorInfoText}
         bgColor={token.colorBgLayout}
       />
