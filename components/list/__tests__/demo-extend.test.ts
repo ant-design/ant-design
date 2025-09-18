e55ec746358f5e28f3eff4918e65328cd7df6eb5
FatahChan <ahmadfathallah89@gmail.com>
2025-09-18 22:10:36 +0300

    test: skip drag sorting demos in list component tests

--- components/list/__tests__/demo-extend.test.ts
+++ components/list/__tests__/demo-extend.test.ts
@@ -1,3 +1,11 @@
 import { extendTest } from '../../../tests/shared/demoTest';
 
-extendTest('list', { skip: ['virtual-list.tsx'] });
+extendTest('list', {
+  skip: [
+    'virtual-list.tsx',
+    'drag-sorting.tsx',
+    'drag-sorting-handler.tsx',
+    'grid-drag-sorting.tsx',
+    'grid-drag-sorting-handler.tsx',
+  ],
+});
