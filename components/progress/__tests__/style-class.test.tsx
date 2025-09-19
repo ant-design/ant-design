4ac948e1b5fc2c848c41ec97637bdce817102da8
lijianan <574980606@qq.com>
2025-09-19 16:58:22 +0800

    test: update case

--- components/progress/__tests__/style-class.test.tsx
+++ components/progress/__tests__/style-class.test.tsx
@@ -5,29 +5,71 @@ import type { ProgressProps } from '..';
 import { render } from '../../../tests/utils';
 
 const classNames: ProgressProps['classNames'] = (info) => {
-  if (info.props.type === 'circle') {
-    return { root: 'progress-circle' };
-  }
-  return { root: 'progress-line' };
+  return info.props.percent === 100
+    ? {
+        root: 'progress-root-full',
+        body: 'progress-body-full',
+        rail: 'progress-rail-full',
+        track: 'progress-track-full',
+        indicator: 'progress-indicator-full',
+      }
+    : {
+        root: 'progress-root-not-full',
+        body: 'progress-body-not-full',
+        rail: 'progress-rail-not-full',
+        track: 'progress-track-not-full',
+        indicator: 'progress-indicator-not-full',
+      };
 };
 
 const styles: ProgressProps['styles'] = (info) => {
-  if (info.props.type === 'circle') {
-    return { root: { padding: 12 } };
-  }
-  return { root: { padding: 8 } };
+  return info.props.percent === 100
+    ? {
+        root: { padding: 12 },
+        body: { padding: 12 },
+        rail: { padding: 12 },
+        track: { padding: 12 },
+        indicator: { padding: 12 },
+      }
+    : {
+        root: { padding: 8 },
+        body: { padding: 8 },
+        rail: { padding: 8 },
+        track: { padding: 8 },
+        indicator: { padding: 8 },
+      };
 };
 
 describe('Progress classNames & styles function', () => {
   it('should apply dynamic classNames and styles from props function', () => {
     const { container, rerender } = render(
-      <Progress type="circle" percent={30} classNames={classNames} styles={styles} />,
+      <Progress percent={100} classNames={classNames} styles={styles} />,
     );
     const root = container.querySelector<HTMLDivElement>('.ant-progress');
-    expect(root).toHaveClass('progress-circle');
+    const body = root?.querySelector<HTMLDivElement>('.ant-progress-body');
+    const rail = body?.querySelector<HTMLDivElement>('.ant-progress-rail');
+    const track = body?.querySelector<HTMLDivElement>('.ant-progress-track');
+    const indicator = body?.querySelector<HTMLDivElement>('.ant-progress-indicator');
+    expect(root).toHaveClass('progress-root-full');
+    expect(body).toHaveClass('progress-body-full');
+    expect(rail).toHaveClass('progress-rail-full');
+    expect(track).toHaveClass('progress-track-full');
+    expect(indicator).toHaveClass('progress-indicator-full');
     expect(root).toHaveStyle({ padding: '12px' });
-    rerender(<Progress type="line" percent={30} classNames={classNames} styles={styles} />);
-    expect(root).toHaveClass('progress-line');
+    expect(body).toHaveStyle({ padding: '12px' });
+    expect(rail).toHaveStyle({ padding: '12px' });
+    expect(track).toHaveStyle({ padding: '12px' });
+    expect(indicator).toHaveStyle({ padding: '12px' });
+    rerender(<Progress percent={50} classNames={classNames} styles={styles} />);
+    expect(root).toHaveClass('progress-root-not-full');
+    expect(body).toHaveClass('progress-body-not-full');
+    expect(rail).toHaveClass('progress-rail-not-full');
+    expect(track).toHaveClass('progress-track-not-full');
+    expect(indicator).toHaveClass('progress-indicator-not-full');
+    expect(body).toHaveStyle({ padding: '8px' });
+    expect(rail).toHaveStyle({ padding: '8px' });
+    expect(track).toHaveStyle({ padding: '8px' });
+    expect(indicator).toHaveStyle({ padding: '8px' });
     expect(root).toHaveStyle({ padding: '8px' });
   });
 });
