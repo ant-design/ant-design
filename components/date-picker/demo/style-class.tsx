b2451f01598bbbbbc692010bc69ecb7c0c527ad0
遇见同学 <1875694521@qq.com>
2025-09-25 12:49:21 +0800

    update demo snap

--- components/date-picker/demo/style-class.tsx
+++ components/date-picker/demo/style-class.tsx
@@ -1,50 +1,40 @@
 import React from 'react';
 import { DatePicker, Flex, Space } from 'antd';
 import type { DatePickerProps } from 'antd';
+import { createStyles } from 'antd-style';
 
-const classNamesObject: DatePickerProps['classNames'] = {
-  root: 'demo-datepicker-root',
-  input: 'demo-datepicker-input',
-  suffix: 'demo-datepicker-suffix',
-  popup: {
-    header: 'demo-datepicker-popup-header',
+const useStyles = createStyles(({ token }) => ({
+  root: {
+    border: `2px dashed ${token.colorPrimary}`,
   },
-};
-
-const classNamesFn: DatePickerProps['classNames'] = (info) => {
-  return {
-    root:
-      info.props.size === 'large' ? 'demo-datepicker-root--large' : 'demo-datepicker-root--small',
-  };
-};
+}));
 
 const stylesObject: DatePickerProps['styles'] = {
-  root: { borderWidth: 2, borderStyle: 'dashed' },
   input: { fontStyle: 'italic' },
   suffix: { opacity: 0.85 },
-  popup: {
-    root: {
-      border: '2px solid #fffbe6',
-    },
-  },
 };
 
 const stylesFn: DatePickerProps['styles'] = (info) => {
   if (info.props.size === 'large') {
-    return { root: { backgroundColor: '#fffbe6', borderColor: '#ffe58f' } };
+    return {
+      root: { borderColor: '#ffe58f' },
+    };
   }
   return {};
 };
 
-const App: React.FC = () => (
-  <Space size={[8, 16]} wrap>
-    <Flex gap="small">
-      <DatePicker classNames={classNamesObject} styles={stylesObject} placeholder="Object" />
-    </Flex>
-    <Flex gap="small">
-      <DatePicker size="large" styles={stylesFn} classNames={classNamesFn} placeholder="Function" />
-    </Flex>
-  </Space>
-);
+const App: React.FC = () => {
+  const { styles: classNames } = useStyles();
+  return (
+    <Space size={[8, 16]} wrap>
+      <Flex gap="small">
+        <DatePicker classNames={classNames} styles={stylesObject} placeholder="Object" />
+      </Flex>
+      <Flex gap="small">
+        <DatePicker size="large" classNames={classNames} styles={stylesFn} placeholder="Function" />
+      </Flex>
+    </Space>
+  );
+};
 
 export default App;
--- components/time-picker/demo/style-class.tsx
+++ components/time-picker/demo/style-class.tsx
@@ -1,90 +1,43 @@
 import React from 'react';
-import { Space, TimePicker } from 'antd';
+import { Flex, TimePicker } from 'antd';
 import type { TimePickerProps } from 'antd';
+import { createStyles } from 'antd-style';
+
+const useStyles = createStyles(({ token }) => ({
+  root: {
+    border: `1px dashed ${token.colorPrimary}`,
+    width: 300,
+  },
+}));
 
 const App: React.FC = () => {
-  const classNamesObject: TimePickerProps['classNames'] = {
-    root: 'custom-time-picker-root',
-    prefix: 'custom-prefix',
-    input: 'custom-input',
-    suffix: 'custom-suffix',
-    popup: 'custom-popup-root',
-  };
-
+  const { styles: classNames } = useStyles();
   const stylesObject: TimePickerProps['styles'] = {
     root: {
-      borderColor: '#1890ff',
-      borderRadius: 8,
-      boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
+      border: '2px solid #d9d9d9',
     },
-    prefix: {
-      color: '#1890ff',
-      marginRight: 8,
-    },
-    input: {
-      color: '#262626',
-    },
-    suffix: {
-      color: '#8c8c8c',
-    },
-    popup: {
-      root: {
-        borderRadius: 8,
-        boxShadow: '0 6px 16px rgba(0,0,0,0.12)',
-        border: '2px solid #fffbe6',
-      },
-    },
-  };
-
-  const classNamesFn: TimePickerProps['classNames'] = (info) => {
-    return {
-      root: info.props.size === 'large' ? 'large-time-picker-root' : 'custom-time-picker-root',
-      prefix: info.props.size === 'large' ? 'large-prefix' : 'custom-prefix',
-      input: info.props.size === 'large' ? 'large-input' : 'custom-input',
-    };
   };
 
   const stylesFn: TimePickerProps['styles'] = (info) => {
-    return {
-      root: {
-        borderColor: '#1890ff',
-        borderRadius: 8,
-        boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
-      },
-      prefix: {
-        color: '#1890ff',
-        marginRight: 8,
-      },
-      input: {
-        color: '#262626',
-        fontWeight: info.props.size === 'large' ? 500 : 300,
-      },
-      suffix: {
-        color: '#8c8c8c',
-      },
-      popup: {
+    if (info.props.size === 'large') {
+      return {
         root: {
-          borderRadius: 8,
-          boxShadow: '0 6px 16px rgba(0,0,0,0.12)',
-          border: info.props.size === 'large' ? '2px solid #1890ff' : 'none',
+          border: '2px solid #faad14',
         },
-      },
-    };
+        suffix: {
+          color: '#faad14',
+        },
+      };
+    }
+    return {};
   };
 
   return (
-    <Space size="middle" style={{ display: 'flex' }}>
-      <Space wrap>
-        <TimePicker
-          size="small"
-          placeholder="small"
-          classNames={classNamesObject}
-          styles={stylesObject}
-        />
+    <Flex vertical gap="middle">
+      <TimePicker placeholder="Object" classNames={classNames} styles={stylesObject} />
 
-        <TimePicker size="large" placeholder="large" classNames={classNamesFn} styles={stylesFn} />
-      </Space>
-    </Space>
+      <TimePicker size="large" placeholder="Function" classNames={classNames} styles={stylesFn} />
+    </Flex>
   );
 };
 
