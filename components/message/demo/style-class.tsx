3c762956bafe2aff9a87bfffec56a90edade051a
nmsn <136696700@qq.com>
2025-09-25 20:19:32 +0800

    refactor(message): update style-class demo

--- components/message/demo/style-class.tsx
+++ components/message/demo/style-class.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
-import { Button, message } from 'antd';
+import { Button, Space, message } from 'antd';
 import type { MessageArgsProps } from 'antd';
+import { createStyles } from 'antd-style';
 
-const classNamesObject: MessageArgsProps['classNames'] = {
-  content: 'demo-message-content',
-  icon: 'demo-message-icon',
-};
-
-const classNamesFn: MessageArgsProps['classNames'] = (info) => {
-  if (info.props.type === 'success') {
-    return { content: 'demo-message-content--success' };
-  }
-  return { content: 'demo-message-content--info' };
-};
+const useStyle = createStyles(({ css }) => ({
+  root: css`
+    margin-top: 20vh;
+  `,
+}));
 
 const stylesObject: MessageArgsProps['styles'] = {
-  content: { fontWeight: 'bold', fontSize: '16px' },
   icon: { fontSize: '20px' },
 };
 
-const stylesFn: MessageArgsProps['styles'] = (info) => {
-  if (info.props.type === 'error') {
-    return { content: { backgroundColor: '#fff1f0', borderColor: '#ffccc7' } };
+const stylesFn: MessageArgsProps['styles'] = ({ props: { type } }) => {
+  if (type === 'success') {
+    return { content: { backgroundColor: '#F5F5F5' } };
   }
-  return { content: { backgroundColor: '#f6ffed', borderColor: '#b7eb8f' } };
+
+  return {};
 };
 
 const App: React.FC = () => {
+  const { styles } = useStyle();
   const [messageApi, contextHolder] = message.useMessage();
 
   const showObjectStyle = () => {
     messageApi.open({
       type: 'info',
       content: 'This is a message with object classNames and styles',
-      classNames: classNamesObject,
+      classNames: {
+        root: styles.root,
+      },
       styles: stylesObject,
     });
   };
@@ -42,16 +40,9 @@ const App: React.FC = () => {
     messageApi.open({
       type: 'success',
       content: 'This is a message with function classNames and styles',
-      classNames: classNamesFn,
-      styles: stylesFn,
-    });
-  };
-
-  const showErrorStyle = () => {
-    messageApi.open({
-      type: 'error',
-      content: 'This is an error message with function styles',
-      classNames: classNamesFn,
+      classNames: {
+        root: styles.root,
+      },
       styles: stylesFn,
     });
   };
@@ -59,13 +50,10 @@ const App: React.FC = () => {
   return (
     <>
       {contextHolder}
-      <Button onClick={showObjectStyle} style={{ marginRight: 8 }}>
-        Object Style
-      </Button>
-      <Button onClick={showFunctionStyle} style={{ marginRight: 8 }}>
-        Function Style
-      </Button>
-      <Button onClick={showErrorStyle}>Error Style</Button>
+      <Space>
+        <Button onClick={showObjectStyle}>Object style</Button>
+        <Button onClick={showFunctionStyle}>Function style</Button>
+      </Space>
     </>
   );
 };
