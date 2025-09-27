dab6dbfa4711199499559b8bf2946a2440de97f9
遇见同学 <1875694521@qq.com>
2025-09-27 12:10:32 +0800

    update demo snap

--- components/tag/demo/style-class.tsx
+++ components/tag/demo/style-class.tsx
@@ -1,108 +1,56 @@
 import React from 'react';
-import { Tag, Space, Divider } from 'antd';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { Flex, Tag } from 'antd';
 import type { TagProps } from 'antd';
-import {
-  CheckCircleOutlined,
-  CloseCircleOutlined,
-  SyncOutlined,
-  ExclamationCircleOutlined,
-} from '@ant-design/icons';
+import { createStyles } from 'antd-style';
+
+const useStyles = createStyles(() => ({
+  root: {
+    padding: '2px 6px',
+    borderRadius: 4,
+  },
+}));
 
 const App: React.FC = () => {
-  const classNamesObject: TagProps['classNames'] = {
-    root: 'custom-tag-root',
-    icon: 'custom-tag-icon',
-    content: 'custom-tag-content',
-  };
-  const stylesObject: TagProps['styles'] = {
+  const { styles: classNames } = useStyles();
+  const styles: TagProps['styles'] = {
     root: {
-      backgroundColor: '#f0f8ff',
-      border: '2px solid #1890ff',
-      borderRadius: '8px',
-      padding: '4px 12px',
+      backgroundColor: '#e6f7ff',
     },
     icon: {
       color: '#52c41a',
-      fontSize: '16px',
-    },
-    content: {
-      fontWeight: 'bold',
-      color: '#1890ff',
     },
   };
-  const classNamesFunction: TagProps['classNames'] = (info) => ({
-    root: info.props.variant === 'filled' ? 'filled-tag' : 'outlined-tag',
-    icon: `icon-${info.props.color}`,
-    content: `content-${info.props.variant}`,
-  });
-  const stylesFunction: TagProps['styles'] = (info) => ({
-    root: {
-      backgroundColor: info.props.color === 'blue' ? '#e6f7ff' : '#f6ffed',
-      border: `2px solid ${info.props.color === 'blue' ? '#1890ff' : '#52c41a'}`,
-      borderRadius: info.props.variant === 'filled' ? '16px' : '4px',
-      padding: '8px 16px',
-      transform: 'scale(1.05)',
-    },
-    icon: {
-      color: info.props.color === 'blue' ? '#1890ff' : '#52c41a',
-      fontSize: '18px',
-      marginRight: '8px',
-    },
-    content: {
-      color: info.props.color === 'blue' ? '#1890ff' : '#52c41a',
-      fontWeight: info.props.variant === 'filled' ? 'bold' : 'normal',
-    },
-  });
+
+  const stylesFn: TagProps['styles'] = (info) => {
+    if (info.props.variant === 'filled') {
+      return {
+        root: {
+          backgroundColor: '#F5EFFF',
+        },
+        icon: {
+          color: '#8F87F1',
+        },
+        content: {
+          color: '#8F87F1',
+        },
+      };
+    }
+  };
   return (
-    <Space size="large" style={{ width: '100%' }}>
-      <div>
-        <h4> classNames and styles object</h4>
-        <Space wrap>
-          <Tag classNames={classNamesObject} styles={stylesObject} icon={<CheckCircleOutlined />}>
-            成功标签
-          </Tag>
-
-          <Tag
-            color="red"
-            classNames={classNamesObject}
-            styles={stylesObject}
-            icon={<CloseCircleOutlined />}
-          >
-            错误标签
-          </Tag>
-        </Space>
-      </div>
-
-      <Divider />
-
-      <div>
-        <h4>classNames and styles function</h4>
-        <Space wrap>
-          <Tag
-            variant="filled"
-            color="blue"
-            icon={<SyncOutlined />}
-            classNames={classNamesFunction}
-            styles={stylesFunction}
-          >
-            动态样式标签
-          </Tag>
-
-          <Tag
-            variant="outlined"
-            color="orange"
-            disabled={false}
-            icon={<ExclamationCircleOutlined />}
-            classNames={classNamesFunction}
-            styles={stylesFunction}
-          >
-            警告标签
-          </Tag>
-        </Space>
-      </div>
-
-      <Divider />
-    </Space>
+    <Flex gap="middle">
+      <Tag classNames={classNames} styles={styles} icon={<CheckCircleOutlined />}>
+        Object
+      </Tag>
+      <Tag
+        variant="filled"
+        classNames={classNames}
+        styles={stylesFn}
+        icon={<CloseCircleOutlined />}
+      >
+        Function
+      </Tag>
+    </Flex>
   );
 };
 
