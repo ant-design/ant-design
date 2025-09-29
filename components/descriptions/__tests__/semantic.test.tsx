9338e46e64c44d1c645e6b7c6d47ee20976f79f3
thinkasany <480968828@qq.com>
2025-09-29 21:50:09 +0800

    add test

--- components/descriptions/__tests__/semantic.test.tsx
+++ components/descriptions/__tests__/semantic.test.tsx
@@ -74,4 +74,79 @@ describe('Descriptions.Semantic', () => {
       background: 'blue',
     });
   });
+
+  it('should apply semantic classNames and styles from Descriptions.Item', () => {
+    const customStyles: DescriptionsProps['styles'] = {
+      label: { color: 'rgb(255, 0, 0)' },
+      content: { color: 'rgb(0, 0, 255)' },
+    };
+    const customClassNames: DescriptionsProps['classNames'] = {
+      label: 'custom-label',
+      content: 'custom-content',
+    };
+    const rootClassNames: DescriptionsProps['classNames'] = {
+      label: 'root-label',
+      content: 'root-content',
+    };
+    const rootStyles: DescriptionsProps['styles'] = {
+      label: { fontSize: '20px' },
+      content: { fontSize: '18px' },
+    };
+    const Items: DescriptionsProps['items'] = [
+      {
+        key: '1',
+        label: 'Product',
+        children: 'Cloud Database',
+        classNames: customClassNames,
+        styles: customStyles,
+      },
+      {
+        key: '2',
+        label: 'Billing Mode',
+        children: 'Prepaid',
+      },
+    ];
+
+    const { container } = render(
+      <Descriptions
+        classNames={rootClassNames}
+        styles={rootStyles}
+        title="User Info"
+        items={Items}
+      />,
+    );
+    const items = container.querySelectorAll('.ant-descriptions-item');
+    const firstChild = items[0];
+    const secondChild = items[1];
+    expect(firstChild.querySelector('.ant-descriptions-item-label')).toHaveStyle({
+      color: 'rgb(255, 0, 0)',
+      fontSize: '20px',
+    });
+    expect(firstChild.querySelector('.ant-descriptions-item-content')).toHaveStyle({
+      color: 'rgb(0, 0, 255)',
+      fontSize: '18px',
+    });
+    expect(secondChild.querySelector('.ant-descriptions-item-label')).not.toHaveStyle({
+      color: 'rgb(255, 0, 0)',
+    });
+    expect(secondChild.querySelector('.ant-descriptions-item-content')).not.toHaveStyle({
+      color: 'rgb(0, 0, 255)',
+    });
+
+    expect(firstChild.querySelector('.ant-descriptions-item-label')).toHaveClass('custom-label');
+    expect(firstChild.querySelector('.ant-descriptions-item-content')).toHaveClass(
+      'custom-content',
+    );
+    expect(secondChild.querySelector('.ant-descriptions-item-label')).toHaveClass('root-label');
+    expect(secondChild.querySelector('.ant-descriptions-item-content')).toHaveClass('root-content');
+
+    expect(firstChild.querySelector('.ant-descriptions-item-label')).toHaveClass('root-label');
+    expect(firstChild.querySelector('.ant-descriptions-item-content')).toHaveClass('root-content');
+    expect(secondChild.querySelector('.ant-descriptions-item-label')).not.toHaveClass(
+      'custom-label',
+    );
+    expect(secondChild.querySelector('.ant-descriptions-item-content')).not.toHaveClass(
+      'custom-content',
+    );
+  });
 });
