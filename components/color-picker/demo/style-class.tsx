8808f24c8a47c0530ccb00384b7fa35aaadb371c
遇见同学 <1875694521@qq.com>
2025-09-30 21:02:26 +0800

    update demo

--- components/color-picker/demo/style-class.tsx
+++ components/color-picker/demo/style-class.tsx
@@ -42,6 +42,7 @@ const App: React.FC = () => {
           defaultValue="#722ed1"
           size="large"
           styles={stylesFn}
+          arrow={false}
           classNames={classNames}
         />
       </Flex>
