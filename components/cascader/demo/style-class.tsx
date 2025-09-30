e7148d33000560770bc96dfce4b2ae5ed4f32dca
遇见同学 <1875694521@qq.com>
2025-10-01 00:21:38 +0800

    feat(cascader): Support better customization with semantic classNames/styles as function (#54960)
    * feat(cascader): Support better customization with semantic classNames/styles as function
    
    * update demo
    
    * chore: update demo
    
    ---------
    
    Co-authored-by: thinkasany <480968828@qq.com>

---
+++ components/cascader/demo/style-class.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Cascader, Flex } from 'antd';
+import type { CascaderProps } from 'antd';
+import { createStyles } from 'antd-style';
+
+const useStyles = createStyles(({ token }) => {
+  return {
+    root: {
+      borderRadius: token.borderRadiusLG,
+    },
+  };
+});
+
+interface Option {
+  value: string;
+  label: string;
+  children?: Option[];
+}
+
+const options: Option[] = [
+  {
+    value: 'Meet-student',
+    label: 'Meet-student',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const stylesObject: CascaderProps['styles'] = {
+  prefix: {
+    color: '#ccc',
+  },
+  suffix: {
+    color: '#ccc',
+  },
+};
+
+const stylesFn: CascaderProps['styles'] = (info) => {
+  if (info.props.variant === 'filled') {
+    return {
+      prefix: {
+        color: '#1890ff',
+      },
+      suffix: {
+        color: '#1890ff',
+      },
+      popup: {
+        listItem: {
+          color: '#1890ff',
+        },
+      },
+    };
+  }
+  return {};
+};
+
+const onChange: CascaderProps<Option>['onChange'] = (value) => {
+  console.log(value);
+};
+
+const App: React.FC = () => {
+  const { styles: classNames } = useStyles();
+  return (
+    <Flex vertical gap="middle">
+      <Cascader
+        options={options}
+        onChange={onChange}
+        placeholder="Object styles"
+        classNames={classNames}
+        styles={stylesObject}
+        prefix="🏠"
+      />
+
+      <Cascader
+        options={options}
+        onChange={onChange}
+        placeholder="Function  styles"
+        variant="filled"
+        classNames={classNames}
+        styles={stylesFn}
+        prefix="✅"
+      />
+    </Flex>
+  );
+};
+
+export default App;
