87e4837bd835f1576f105ae336729bd823b153ce
thinkasany <480968828@qq.com>
2025-10-01 10:41:55 +0800

    chore: improve AutoComplete semantic code (#55208)

--- components/auto-complete/AutoComplete.tsx
+++ components/auto-complete/AutoComplete.tsx
@@ -195,8 +195,8 @@ const AutoComplete: React.ForwardRefRenderFunction<RefSelectProps, AutoCompleteP
     AutoCompleteStylesType,
     AutoCompleteProps
   >(
-    [undefined, classNames],
-    [undefined, styles],
+    [classNames],
+    [styles],
     {
       popup: {
         _default: 'root',
