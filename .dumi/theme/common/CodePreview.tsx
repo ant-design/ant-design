d1c1e620afbb048ba3c9180f5d13a94699f90848
lijianan <574980606@qq.com>
2025-10-02 17:40:54 +0800

    update

--- .dumi/theme/common/CodePreview.tsx
+++ .dumi/theme/common/CodePreview.tsx
@@ -153,6 +153,8 @@ const CodePreview: React.FC<CodePreviewProps> = ({
             ) : (
               toReactComponent(['pre', { lang, highlighted: highlightedCodes[lang] }])
             )}
+            {/* 这里用 div 标签，不能用 button */}
+            {/* button 嵌套 button 会导致水合失败 */}
             <div className={styles.copyButton}>
               <Typography.Text className={styles.copyIcon} copyable={{ text: sourceCodes[lang] }} />
             </div>
