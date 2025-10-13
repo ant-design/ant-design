1b6d5527c6c3a112f9ea28fc4e6e70070dcf3889
lijianan <574980606@qq.com>
2025-10-13 08:10:19 +0800

    perf: keep LoadingIcon in memo (#55285)

--- components/cascader/hooks/useColumnIcons.tsx
+++ components/cascader/hooks/useColumnIcons.tsx
@@ -9,10 +9,13 @@ const useColumnIcons = (prefixCls: string, rtl: boolean, expandIcon?: React.Reac
     mergedExpandIcon = rtl ? <LeftOutlined /> : <RightOutlined />;
   }
 
-  const loadingIcon = (
-    <span className={`${prefixCls}-menu-item-loading-icon`}>
-      <LoadingOutlined spin />
-    </span>
+  const loadingIcon = React.useMemo(
+    () => (
+      <span className={`${prefixCls}-menu-item-loading-icon`}>
+        <LoadingOutlined spin />
+      </span>
+    ),
+    [prefixCls],
   );
 
   return React.useMemo<Readonly<[React.ReactNode, React.ReactNode]>>(
