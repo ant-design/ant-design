49072cbfae8a6fbb3bd6ad2164950a244ff39e8a
thinkasany <480968828@qq.com>
2025-10-19 19:56:21 +0800

    Revert "refactor(modal): use modalRef replace createRef (#55368)"
    This reverts commit 3b47ac3630192451bd64093c57e07781b36f7f0c.

--- components/modal/useModal/index.tsx
+++ components/modal/useModal/index.tsx
@@ -37,7 +37,6 @@ const ElementsHolder = React.memo(
 
 function useModal(): readonly [instance: HookAPI, contextHolder: React.ReactElement] {
   const holderRef = React.useRef<ElementsHolderRef>(null);
-  const modalRef = React.useRef<HookModalRef>(null);
 
   // ========================== Effect ==========================
   const [actionQueue, setActionQueue] = React.useState<VoidFunction[]>([]);
@@ -59,6 +58,8 @@ function useModal(): readonly [instance: HookAPI, contextHolder: React.ReactElem
     (withFunc: (config: ModalFuncProps) => ModalFuncProps) =>
       function hookConfirm(config: ModalFuncProps) {
         uuid += 1;
+
+        const modalRef = React.createRef<HookModalRef>();
 
         // Proxy to promise with `onClose`
         let resolvePromise: (confirmed: boolean) => void;
