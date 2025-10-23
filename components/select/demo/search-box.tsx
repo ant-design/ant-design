cc4e31b9302414b0ca782d403b9f16dad062f15e
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-10-23 14:38:21 +0800

    chore(demo): remove usage of internal utility methods and types from component demos

--- components/select/demo/search-box.tsx
+++ components/select/demo/search-box.tsx
@@ -2,12 +2,11 @@
 import React, { useState } from 'react';
 import { Select } from 'antd';
 import type { SelectProps } from 'antd';
-import type { AnyObject } from 'antd/es/_util/type';
 
 let timeout: ReturnType<typeof setTimeout> | null;
 let currentValue: string;
 
-const toURLSearchParams = <T extends AnyObject>(record: T) => {
+const toURLSearchParams = <T extends Record<string, any>>(record: T) => {
   const params = new URLSearchParams();
   for (const [key, value] of Object.entries(record)) {
     params.append(key, value);
