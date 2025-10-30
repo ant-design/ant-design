1f668b459a57a368afd079049842523e724de01d
二货机器人 <smith3816@gmail.com>
2025-10-30 14:55:08 +0800

    chore: fix search

--- components/input/Search.tsx
+++ components/input/Search.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import SearchOutlined from '@ant-design/icons/SearchOutlined';
 import classNames from 'classnames';
+import omit from 'rc-util/lib/omit';
+import pickAttrs from 'rc-util/lib/pickAttrs';
 import { composeRef } from 'rc-util/lib/ref';
 
 import { cloneElement } from '../_util/reactNode';
@@ -47,6 +49,7 @@ const Search = React.forwardRef<InputRef, SearchProps>((props, ref) => {
     onCompositionEnd,
     variant,
     onPressEnter: customOnPressEnter,
+    hidden,
     ...restProps
   } = props;
 
@@ -163,21 +166,30 @@ const Search = React.forwardRef<InputRef, SearchProps>((props, ref) => {
     onCompositionEnd?.(e);
   };
 
-  const inputProps: InputProps = {
-    ...restProps,
-    prefixCls: inputPrefixCls,
-    type: 'search',
-    size,
-    variant,
-    onPressEnter,
-    onCompositionStart: handleOnCompositionStart,
-    onCompositionEnd: handleOnCompositionEnd,
-    onChange,
-    disabled,
-  };
+  // ========================== Render ==========================
+  // >>> Root Props
+  const rootProps = pickAttrs(restProps, {
+    data: true,
+  });
+
+  const inputProps: InputProps = omit(
+    {
+      ...restProps,
+      prefixCls: inputPrefixCls,
+      type: 'search',
+      size,
+      variant,
+      onPressEnter,
+      onCompositionStart: handleOnCompositionStart,
+      onCompositionEnd: handleOnCompositionEnd,
+      onChange,
+      disabled,
+    },
+    Object.keys(rootProps) as any[],
+  );
 
   return (
-    <Space.Compact className={cls} style={style}>
+    <Space.Compact className={cls} style={style} {...rootProps} hidden={hidden}>
       <Input ref={composeRef<InputRef>(inputRef, ref)} {...inputProps} />
       {button}
     </Space.Compact>
