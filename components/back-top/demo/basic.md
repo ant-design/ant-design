31ef3230382fee506f2bc1b98972306863d6893e
lijianan <574980606@qq.com>
2025-11-11 00:52:06 +0800

    update

---
+++ components/back-top/demo/basic.md
@@ -0,0 +1,7 @@
+## zh-CN
+
+最简单的用法。（已废弃）
+
+## en-US
+
+The most basic usage. (Deprecated)
---
+++ components/back-top/demo/basic.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BackTop } from 'antd';
+
+const Demo: React.FC = () => (
+  <>
+    <BackTop />
+    Scroll down to see the bottom-right
+    <strong className="site-back-top-basic">gray</strong>
+    button.
+  </>
+);
+
+export default Demo;
