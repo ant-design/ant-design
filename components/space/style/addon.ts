3111b158fc06a23d7365e596d0c67695f19dd743
刘欢 <lh01217311@antgroup.com>
2025-11-11 14:56:54 +0800

    refactor: Refactoring Space.Addon style and class name

---
+++ components/space/style/addon.ts
@@ -0,0 +1,83 @@
+import { genCompactItemStyle } from '../../style/compact-item';
+import { genStyleHooks } from '../../theme/internal';
+import type { FullToken, GenerateStyle } from '../../theme/internal';
+
+/** Component only token. Which will handle additional calculation of alias token */
+// biome-ignore lint/suspicious/noEmptyInterface: ComponentToken need to be empty by default
+export interface ComponentToken {}
+
+interface SpaceToken extends FullToken<'Space'> {
+  // Custom token here
+}
+
+const genSpaceAddonStyle: GenerateStyle<SpaceToken> = (token) => {
+  const {
+    componentCls,
+    borderRadius,
+    paddingSM,
+    colorBorder,
+    paddingXS,
+    fontSizeLG,
+    fontSizeSM,
+    borderRadiusLG,
+    borderRadiusSM,
+    colorBgContainerDisabled,
+    lineWidth,
+  } = token;
+
+  return {
+    [componentCls]: [
+      {
+        display: 'inline-flex',
+        alignItems: 'center',
+        gap: 0,
+        paddingInline: paddingSM,
+        margin: 0,
+        background: colorBgContainerDisabled,
+        borderWidth: lineWidth,
+        borderStyle: 'solid',
+        borderColor: colorBorder,
+        borderRadius,
+
+        '&-large': {
+          fontSize: fontSizeLG,
+          borderRadius: borderRadiusLG,
+        },
+        '&-small': {
+          paddingInline: paddingXS,
+          borderRadius: borderRadiusSM,
+          fontSize: fontSizeSM,
+        },
+        '&-compact-last-item': {
+          borderEndStartRadius: 0,
+          borderStartStartRadius: 0,
+        },
+        '&-compact-first-item': {
+          borderEndEndRadius: 0,
+          borderStartEndRadius: 0,
+        },
+        '&-compact-item:not(:first-child):not(:last-child)': {
+          borderRadius: 0,
+        },
+        '&-compact-item:not(:last-child)': {
+          borderInlineEndWidth: 0,
+        },
+      },
+      genCompactItemStyle(token, {
+        focus: false,
+      }),
+    ],
+  };
+};
+
+// ============================== Export ==============================
+export default genStyleHooks(
+  ['Space', 'Addon'],
+  (token) => [genSpaceAddonStyle(token)],
+  () => ({}),
+  {
+    // Space component don't apply extra font style
+    // https://github.com/ant-design/ant-design/issues/40315
+    resetStyle: false,
+  },
+);
