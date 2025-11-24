ant-design/ant-design

commit da2ddb99553bb942865078d9b902dc5afc9992d3

fix(Anchor): regression introduced in #55814 (#55865)

components/anchor/Anchor.tsx
@@ -279,7 +279,10 @@ const Anchor: React.FC<AnchorProps> = (props) => {
         return;
       }
 
-      if (animating.current && activeLinkRef.current !== link) {
+      if (animating.current) {
+        if (activeLinkRef.current === link) {
+          return;
+        }
         scrollRequestId.current?.();
       }
 

commit b36fc936a0efea5435d346748bfdf21d84d4c993

fix(anchor): regression introduced in #55865

components/anchor/Anchor.tsx
@@ -269,6 +269,7 @@ const Anchor: React.FC<AnchorProps> = (props) => {
 
   const handleScrollTo = React.useCallback<(link: string) => void>(
     (link) => {
+      const previousActiveLink = activeLinkRef.current;
       setCurrentActiveLink(link);
       const sharpLinkMatch = sharpMatcherRegex.exec(link);
       if (!sharpLinkMatch) {
@@ -280,7 +281,7 @@ const Anchor: React.FC<AnchorProps> = (props) => {
       }
 
       if (animating.current) {
-        if (activeLinkRef.current === link) {
+        if (previousActiveLink === link) {
           return;
         }
         scrollRequestId.current?.();