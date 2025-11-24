b48cf933ddaf3e6b6406a621166d74599e9d5d95
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-11-24 16:17:39 +0800

    chore: update

--- .dumi/theme/common/VersionUpgrade/ChangeLog.tsx
+++ .dumi/theme/common/VersionUpgrade/ChangeLog.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { FastColor } from '@ant-design/fast-color';
 import { createStyles } from 'antd-style';
-import debounce from 'lodash/debounce';
 
 import useLocale from '../../../hooks/useLocale';
 import EN from './en-US.md';
@@ -9,27 +7,10 @@ import CN from './zh-CN.md';
 
 const changeLog = { cn: CN, en: EN };
 
-const useStyle = createStyles(({ css, token }, { isOverflowing }: any) => ({
+const useStyle = createStyles(({ css }) => ({
   container: css`
     max-height: max(62vh, 500px);
-    overflow: hidden;
-    position: relative;
-
-    ::after {
-      opacity: ${isOverflowing ? 1 : 0};
-      content: '';
-      position: absolute;
-      inset-block-end: 0;
-      inset-inline-start: 0;
-      width: 100%;
-      height: 30%;
-      pointer-events: none;
-      background: linear-gradient(
-        to top,
-        ${token.colorBgElevated} 0%,
-        ${new FastColor(token.colorBgElevated).setA(0.25).toHexString()} 100%
-      );
-    }
+    overflow: scroll;
 
     /* 图片铺满 */
     && img {
@@ -41,43 +22,14 @@ const useStyle = createStyles(({ css, token }, { isOverflowing }: any) => ({
 
 const ChangeLog = () => {
   const [, lang] = useLocale();
-  const containerRef = React.useRef<HTMLDivElement>(null);
 
-  const [isOverflowing, setIsOverflowing] = React.useState(false);
-  const { styles } = useStyle({ isOverflowing });
+  const { styles } = useStyle();
 
   const validatedLanguage = Object.keys(changeLog).includes(lang) ? lang : 'en';
   const C = changeLog[validatedLanguage];
 
-  const checkOverflow = React.useMemo(
-    () =>
-      debounce(() => {
-        const hasOverflow =
-          containerRef.current?.scrollHeight! > containerRef.current?.clientHeight!;
-        setIsOverflowing(hasOverflow);
-      }),
-    [],
-  );
-
-  // 检测溢出状态
-  React.useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    checkOverflow();
-
-    // eslint-disable-next-line compat/compat
-    const resizeObserver = new ResizeObserver(checkOverflow);
-    resizeObserver.observe(container);
-
-    return function cleanup() {
-      resizeObserver.disconnect();
-      checkOverflow.cancel();
-    };
-  }, []);
-
   return (
-    <div className={styles.container} ref={containerRef}>
+    <div className={styles.container}>
       <C />
     </div>
   );
