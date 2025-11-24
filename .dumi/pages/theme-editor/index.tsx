5b9b36a743082e2a6ea12b05cce07176f463262e
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-11-24 18:26:42 +0800

    chore: remove useEffect

--- .dumi/pages/theme-editor/index.tsx
+++ .dumi/pages/theme-editor/index.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, useEffect } from 'react';
-import { App, Button, ConfigProvider, Skeleton } from 'antd';
+import React, { Suspense } from 'react';
+import { App, Button, ConfigProvider, Skeleton, version } from 'antd';
 import { enUS, zhCN } from 'antd-token-previewer';
 import type { ThemeConfig } from 'antd/es/config-provider/context';
 import { Helmet } from 'dumi';
@@ -33,21 +33,21 @@ const locales = {
   },
 };
 
-const ANT_DESIGN_V5_THEME_EDITOR_THEME = 'ant-design-v5-theme-editor-theme';
+const [antdMajor] = version.split('.');
+const ANT_DESIGN_V5_THEME_EDITOR_THEME = `ant-design-v${antdMajor}-theme-editor-theme`;
 
 const CustomTheme: React.FC = () => {
   const { message } = App.useApp();
   const [locale, lang] = useLocale(locales);
 
-  const [theme, setTheme] = React.useState<ThemeConfig>({});
-
-  useEffect(() => {
-    const storedConfig = localStorage.getItem(ANT_DESIGN_V5_THEME_EDITOR_THEME);
-    if (storedConfig) {
-      const themeConfig = JSON.parse(storedConfig);
-      setTheme(themeConfig);
+  const [theme, setTheme] = React.useState<ThemeConfig>(() => {
+    try {
+      const storedConfig = localStorage.getItem(ANT_DESIGN_V5_THEME_EDITOR_THEME);
+      return storedConfig ? JSON.parse(storedConfig) : {};
+    } catch {
+      return {};
     }
-  }, []);
+  });
 
   const handleSave = () => {
     localStorage.setItem(ANT_DESIGN_V5_THEME_EDITOR_THEME, JSON.stringify(theme));
