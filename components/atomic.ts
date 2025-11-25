d8d0e99f120a9b140eee664dd35317f1f47a16c5
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-11-25 11:57:37 +0800

    feat(atomic): atomic exports

---
+++ components/atomic.ts
@@ -0,0 +1,9 @@
+/**
+ * Atomic Exports for Ant Design
+ *
+ * This file exports the atomic components of Ant Design, which is good for tree shaking.
+ */
+
+export { default as Affix, AffixProps } from './affix';
+export { default as Alert, AlertProps } from './alert/Alert';
+export { default as ErrorBoundary, ErrorBoundaryProps } from './alert/ErrorBoundary';
