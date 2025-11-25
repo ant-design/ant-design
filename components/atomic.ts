facd0adb09b3f14ff9dd31bb7c94872429725673
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-11-25 17:07:13 +0800

    feat(atomic): add Breadcrumb and Button exports

--- components/atomic.ts
+++ components/atomic.ts
@@ -16,3 +16,11 @@ export { default as Avatar, AvatarProps } from './avatar/Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './avatar/AvatarGroup';
 // TODO: https://github.com/ant-design/ant-design/pull/55883
 // TODO: https://github.com/ant-design/ant-design/pull/55892
+export { default as Breadcrumb, BreadcrumbProps } from './breadcrumb/Breadcrumb';
+export { default as BreadcrumbItem, BreadcrumbItemProps } from './breadcrumb/BreadcrumbItem';
+export {
+  default as BreadcrumbSeparator,
+  BreadcrumbSeparatorProps,
+} from './breadcrumb/BreadcrumbSeparator';
+export { default as Button, ButtonProps } from './button/button';
+export { default as ButtonGroup, ButtonGroupProps } from './button/button-group';
