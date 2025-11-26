a1075a0c9fb9b55abdc154d996d945b798d92ab5
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-11-26 17:43:23 +0800

    feat: add atomic exports for color picker

--- components/atomic.ts
+++ components/atomic.ts
@@ -15,6 +15,8 @@ export { default as AutoComplete, AutoCompleteProps } from './auto-complete/Auto
 export { default as Avatar, AvatarProps } from './avatar/Avatar';
 export { default as AvatarGroup, AvatarGroupProps } from './avatar/AvatarGroup';
 // TODO: https://github.com/ant-design/ant-design/pull/55883
+export { default as Badge, BadgeProps } from './badge';
+export { default as Ribbon, RibbonProps } from './badge/Ribbon';
 // TODO: https://github.com/ant-design/ant-design/pull/55892
 export { default as Breadcrumb, BreadcrumbProps } from './breadcrumb/Breadcrumb';
 export { default as BreadcrumbItem, BreadcrumbItemProps } from './breadcrumb/BreadcrumbItem';
@@ -22,3 +24,20 @@ export { default as BreadcrumbSeparator } from './breadcrumb/BreadcrumbSeparator
 // TODO: https://github.com/ant-design/ant-design/pull/55893
 export { default as Button, ButtonProps } from './button/button';
 export { default as ButtonGroup, ButtonGroupProps } from './button/button-group';
+export { default as Calendar, CalendarProps } from './calendar';
+export { default as generateCalendar } from './calendar/generateCalendar';
+export { default as Card, CardProps } from './card/Card';
+export { default as CardGrid, CardGridProps } from './card/Grid';
+export { default as CardMeta, CardMetaProps } from './card/Meta';
+export { default as Carousel, CarouselProps } from './carousel';
+export { default as Cascader, CascaderProps } from './cascader';
+export { default as Checkbox, CheckboxProps } from './checkbox/Checkbox';
+export { default as CheckboxGroup, CheckboxGroupProps } from './checkbox/Group';
+export { default as Col, ColProps } from './col';
+export { default as Collapse, CollapseProps } from './collapse/Collapse';
+export { default as CollapsePanel, CollapsePanelProps } from './collapse/CollapsePanel';
+export { default as ColorPicker, ColorPickerProps } from './color-picker/ColorPicker';
+export {
+  default as ColorPickerPanel,
+  ColorPickerPanelProps,
+} from './color-picker/ColorPickerPanel';
