7a2c5facb05d0431c166d04cf9bd3ebef465afe4
二货机器人 <smith3816@gmail.com>
2025-11-27 16:00:13 +0800

    test: fix ci

--- tests/setup.ts
+++ tests/setup.ts
@@ -96,13 +96,34 @@ global.requestAnimationFrame = global.requestAnimationFrame || global.setTimeout
 global.cancelAnimationFrame = global.cancelAnimationFrame || global.clearTimeout;
 
 if (typeof MessageChannel === 'undefined') {
-  (global as any).MessageChannel = function MockMessageChannel() {
-    const port1: any = {};
-    const port2: any = {};
-    port1.postMessage = port2.onmessage = () => {};
-    port2.postMessage = port1.onmessage = () => {};
-    return { port1, port2 };
-  };
+  (global as any).MessageChannel = class {
+    port1: any;
+    port2: any;
+
+    constructor() {
+      const createPort = (): any => {
+        const port: any = {
+          onmessage: null,
+          postMessage: (message: any) => {
+            setTimeout(() => {
+              if (port._target && typeof port._target.onmessage === 'function') {
+                port._target.onmessage({ data: message });
+              }
+            }, 0);
+          },
+          _target: null,
+        };
+        return port;
+      };
+
+      const port1 = createPort();
+      const port2 = createPort();
+      port1._target = port2;
+      port2._target = port1;
+      this.port1 = port1;
+      this.port2 = port2;
+    }
+  } as any;
 }
 
 // Mock useId to return a stable id for snapshot testing
