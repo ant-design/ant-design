f37c89cccff995b8314a9a8e7e2b902f70e6fc75
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-11-28 17:38:37 +0800

    refactor(atomic): update exports to include type definitions for components

--- components/atomic.ts
+++ components/atomic.ts
@@ -4,42 +4,67 @@
  * This file exports the atomic components of Ant Design, which is good for tree shaking.
  */
 
-export { default as Affix, AffixProps } from './affix';
-export { default as Alert, AlertProps } from './alert/Alert';
-// TODO: https://github.com/ant-design/ant-design/pull/55974
-export { default as ErrorBoundary, ErrorBoundaryProps } from './alert/ErrorBoundary';
-export { default as Anchor, AnchorProps } from './anchor/Anchor';
-export { default as AnchorLink, AnchorLinkProps } from './anchor/AnchorLink';
-export { default as App, AppProps } from './app/App';
+export { default as Affix } from './affix';
+export type { AffixProps } from './affix';
+export { default as Alert } from './alert/Alert';
+export type { AlertProps } from './alert/Alert';
+export { default as ErrorBoundary } from './alert/ErrorBoundary';
+export type { ErrorBoundaryProps } from './alert/ErrorBoundary';
+export { default as Anchor } from './anchor/Anchor';
+export type { AnchorProps } from './anchor/Anchor';
+export { default as AnchorLink } from './anchor/AnchorLink';
+export type { AnchorLinkProps } from './anchor/AnchorLink';
+export { default as App } from './app/App';
+export type { AppProps } from './app/App';
 export { default as useApp } from './app/useApp';
-export { default as AutoComplete, AutoCompleteProps } from './auto-complete/AutoComplete';
-export { default as Avatar, AvatarProps } from './avatar/Avatar';
-export { default as AvatarGroup, AvatarGroupProps } from './avatar/AvatarGroup';
-export { default as Badge, BadgeProps } from './badge/Badge';
-export { default as Ribbon, RibbonProps } from './badge/Ribbon';
+export { default as AutoComplete } from './auto-complete/AutoComplete';
+export type { AutoCompleteProps } from './auto-complete/AutoComplete';
+export { default as Avatar } from './avatar/Avatar';
+export type { AvatarProps } from './avatar/Avatar';
+export { default as AvatarGroup } from './avatar/AvatarGroup';
+export type { AvatarGroupProps } from './avatar/AvatarGroup';
+export { default as Badge } from './badge/Badge';
+export type { BadgeProps } from './badge/Badge';
+export { default as Ribbon } from './badge/Ribbon';
+export type { RibbonProps } from './badge/Ribbon';
 // TODO: https://github.com/ant-design/ant-design/pull/55892
-export { default as Breadcrumb, BreadcrumbProps } from './breadcrumb/Breadcrumb';
-export { default as BreadcrumbItem, BreadcrumbItemProps } from './breadcrumb/BreadcrumbItem';
+export { default as Breadcrumb } from './breadcrumb/Breadcrumb';
+export type { BreadcrumbProps } from './breadcrumb/Breadcrumb';
+export { default as BreadcrumbItem } from './breadcrumb/BreadcrumbItem';
+export type { BreadcrumbItemProps } from './breadcrumb/BreadcrumbItem';
 export { default as BreadcrumbSeparator } from './breadcrumb/BreadcrumbSeparator';
 // TODO: https://github.com/ant-design/ant-design/pull/55893
-export { default as Button, ButtonProps } from './button/button';
-export { default as ButtonGroup, ButtonGroupProps } from './button/button-group';
-export { default as Calendar, CalendarProps } from './calendar';
+export { default as Button } from './button/button';
+export type { ButtonProps } from './button/button';
+export { default as ButtonGroup } from './button/button-group';
+export type { ButtonGroupProps } from './button/button-group';
+export { default as Calendar } from './calendar';
+export type { CalendarProps } from './calendar';
 export { default as generateCalendar } from './calendar/generateCalendar';
-export { default as Card, CardProps } from './card/Card';
-export { default as CardGrid, CardGridProps } from './card/Grid';
-export { default as CardMeta, CardMetaProps } from './card/Meta';
-export { default as Carousel, CarouselProps } from './carousel';
-export { default as Cascader, CascaderProps } from './cascader';
-export { default as Checkbox, CheckboxProps } from './checkbox/Checkbox';
-export { default as CheckboxGroup, CheckboxGroupProps } from './checkbox/Group';
-export { default as Col, ColProps } from './col';
-export { default as Collapse, CollapseProps } from './collapse/Collapse';
-export { default as CollapsePanel, CollapsePanelProps } from './collapse/CollapsePanel';
-export { default as ColorPicker, ColorPickerProps } from './color-picker/ColorPicker';
-export {
-  default as ColorPickerPanel,
-  ColorPickerPanelProps,
-} from './color-picker/ColorPickerPanel';
-export { default as ConfigProvider, ConfigProviderProps } from './config-provider';
+export { default as Card } from './card/Card';
+export type { CardProps } from './card/Card';
+export { default as CardGrid } from './card/Grid';
+export type { CardGridProps } from './card/Grid';
+export { default as CardMeta } from './card/Meta';
+export type { CardMetaProps } from './card/Meta';
+export { default as Carousel } from './carousel';
+export type { CarouselProps } from './carousel';
+export { default as Cascader } from './cascader';
+export type { CascaderProps } from './cascader';
+export { default as Checkbox } from './checkbox/Checkbox';
+export type { CheckboxProps } from './checkbox/Checkbox';
+export { default as CheckboxGroup } from './checkbox/Group';
+export type { CheckboxGroupProps } from './checkbox/Group';
+export { default as Col } from './col';
+export type { ColProps } from './col';
+export { default as Collapse } from './collapse/Collapse';
+export type { CollapseProps } from './collapse/Collapse';
+export { default as CollapsePanel } from './collapse/CollapsePanel';
+export type { CollapsePanelProps } from './collapse/CollapsePanel';
+export { default as ColorPicker } from './color-picker/ColorPicker';
+export type { ColorPickerProps } from './color-picker/ColorPicker';
+export { default as ColorPickerPanel } from './color-picker/ColorPickerPanel';
+export type { ColorPickerPanelProps } from './color-picker/ColorPickerPanel';
+export { default as ConfigProvider } from './config-provider';
+export type { ConfigProviderProps } from './config-provider';
 export { default as useConfig } from './config-provider/hooks/useConfig';
