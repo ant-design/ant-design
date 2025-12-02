f50b31a0deb19306ab61ea8115442c2cd547c83f
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-12-02 10:15:54 +0800

    feat(Pagination): three size

--- components/pagination/demo/mini.tsx
+++ components/pagination/demo/mini.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import type { PaginationProps } from 'antd';
-import { Pagination } from 'antd';
+import { Flex, Pagination } from 'antd';
 
 const showTotal: PaginationProps['showTotal'] = (total) => `Total ${total} items`;
 
 const App: React.FC = () => (
-  <>
+  <Flex vertical gap="middle">
     <Pagination size="small" total={50} />
     <Pagination size="small" total={50} showSizeChanger showQuickJumper />
     <Pagination size="small" total={50} showTotal={showTotal} />
@@ -17,7 +17,19 @@ const App: React.FC = () => (
       showSizeChanger
       showQuickJumper
     />
-  </>
+
+    <Pagination size="large" total={50} />
+    <Pagination size="large" total={50} showSizeChanger showQuickJumper />
+    <Pagination size="large" total={50} showTotal={showTotal} />
+    <Pagination
+      size="large"
+      total={50}
+      disabled
+      showTotal={showTotal}
+      showSizeChanger
+      showQuickJumper
+    />
+  </Flex>
 );
 
 export default App;
