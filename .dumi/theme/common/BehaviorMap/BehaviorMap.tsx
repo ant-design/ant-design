0da2c5bf73376aa7844df4719f75ee5c148dcd60
liuqiang <qiang.liu@xinjifamily.com>
2025-12-15 13:04:55 +0800

    update

--- .dumi/theme/common/BehaviorMap/BehaviorMap.tsx
+++ .dumi/theme/common/BehaviorMap/BehaviorMap.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createStyles, css } from 'antd-style';
 import { useRouteMeta } from 'dumi';
 
 import useLocale from '../../../hooks/useLocale';
+import { useMermaidCode } from './useMermaidCode';
 
-interface BehaviorMapItem {
+export interface BehaviorMapItem {
   id: string;
   label: string;
   targetType?: 'mvp' | 'extension';
@@ -97,55 +98,13 @@ const locales = {
   },
 };
 
-const generateMermaidCode = (root: BehaviorMapItem): string => {
-  const lines: string[] = [];
-
-  lines.push('graph LR');
-
-  lines.push(`classDef baseNode fill:#fff,stroke:none,stroke-width:0px,rx:5,ry:5,font-size:14px`);
-
-  const traverse = (node: BehaviorMapItem, parentId?: string) => {
-    const safeId = `node_${node.id.replace(/[^a-z0-9]/gi, '_')}`;
-    let labelText = node.label.replace(/"/g, "'");
-
-    if (!parentId) {
-      lines.push(`style ${safeId} font-size:16px`);
-      labelText = `**${labelText}**`;
-    } else if (node.targetType === 'mvp') {
-      const blueDot = `<span style="display:inline-block;width:8px;height:8px;background-color:rgb(22, 119, 255);border-radius:50%;margin-right:8px;vertical-align:middle;"></span>`;
-      labelText = `${blueDot}${labelText}`;
-    } else if (node.targetType === 'extension') {
-      const grayDot = `<span style="display:inline-block;width:8px;height:8px;background-color:rgb(160, 160, 160);border-radius:50%;margin-right:8px;vertical-align:middle;"></span>`;
-      labelText = `${grayDot}${labelText}`;
-    }
-    lines.push(`${safeId}["${labelText}"]:::baseNode`);
-
-    if (node.link) {
-      lines.push(`click ${safeId} "#${node.link}"`);
-    }
-
-    if (parentId) {
-      lines.push(`${parentId} --> ${safeId}`);
-    }
-
-    if (node.children && node.children.length > 0) {
-      node.children.forEach((child) => traverse(child, safeId));
-    }
-  };
-
-  traverse(root);
-  return lines.join('\n');
-};
-
 const BehaviorMap: React.FC<BehaviorMapProps> = ({ data }) => {
   const chartRef = useRef<HTMLDivElement>(null);
   const { styles } = useStyle();
   const [locale] = useLocale(locales);
   const meta = useRouteMeta();
 
-  const mermaidCode = useMemo(() => {
-    return generateMermaidCode(data);
-  }, [data]);
+  const mermaidCode = useMermaidCode(data);
 
   useEffect(() => {
     let isCancelled = false;
