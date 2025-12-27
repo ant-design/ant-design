fa02713e80f377ac4b6f3282aa23e89224fc4f01
thinkasany <480968828@qq.com>
2025-12-27 18:16:24 +0800

    test: preview-build use ubuntu-latest

--- .dumi/theme/builtins/ColorPaletteToolDark/index.ts
+++ .dumi/theme/builtins/ColorPaletteToolDark/index.ts
@@ -1,5 +1,3 @@
 import ColorPaletteToolDark from '../../common/Color/ColorPaletteToolDark';
 
-// Ensure SSR compatibility
-export { ColorPaletteToolDark };
 export default ColorPaletteToolDark;
--- .dumi/theme/builtins/ColorPalettes/index.ts
+++ .dumi/theme/builtins/ColorPalettes/index.ts
@@ -1,5 +1,3 @@
 import ColorPalettes from '../../common/Color/ColorPalettes';
 
-// Ensure SSR compatibility
-export { ColorPalettes };
 export default ColorPalettes;
