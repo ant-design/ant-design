64818a40686df081a16c5811ab0d1b64b69cd754
thinkasany <480968828@qq.com>
2025-12-27 15:06:23 +0800

    fix

--- .jest.site.js
+++ .jest.site.js
@@ -1,24 +1,6 @@
-const { moduleNameMapper } = require('./.jest');
+const { moduleNameMapper, transformIgnorePatterns } = require('./.jest');
 
 // jest config for server render environment
-const compileModules = [
-  // jsdom 27+ depends on ESM parse5, need transform
-  'parse5',
-];
-
-// cnpm use `_` as prefix
-const ignoreList = ['', '_'].reduce(
-  (acc, prefix) => [...acc, ...compileModules.map((module) => `${prefix}${module}`)],
-  [],
-);
-
-const transformIgnorePatterns = [
-  // 忽略无需转译的 node_modules，仅转译 compileModules 中的包
-  `[/\\\\]node_modules[/\\\\](?!${ignoreList.join('|')})[^/\\\\]+?[/\\\\](?!(es)[/\\\\])`,
-  // 忽略 antd 的 UMD 构建文件
-  '[/\\\\]dist[/\\\\]antd.*\\.js$',
-];
-
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'md'],
   moduleNameMapper,
