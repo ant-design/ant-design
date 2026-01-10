e072dcf553db294500b687491d110a60ffea358e
Oyster Lee <oysterd3@gmail.com>
2026-01-10 19:29:16 +0800

    Revert "fix: type assertion"
    This reverts commit b084ca7ece1998d9722b441fa66a56911c8b19d7.

--- components/masonry/Masonry.tsx
+++ components/masonry/Masonry.tsx
@@ -198,7 +198,7 @@ const Masonry = React.forwardRef<MasonryRef, MasonryProps>((props, ref) => {
     const nextItemsHeight: ItemHeightData[] = [];
 
     mergedItems.forEach((item, index) => {
-      const itemKey = item.key ?? index;
+      const itemKey = (item.key ?? index) as Key;
       const itemEle = getItemRef(itemKey);
       const rect = itemEle?.getBoundingClientRect();
 
@@ -231,7 +231,7 @@ const Masonry = React.forwardRef<MasonryRef, MasonryProps>((props, ref) => {
   const itemWithPositions = React.useMemo(
     () =>
       mergedItems.map((item, index) => {
-        const key = item.key ?? index;
+        const key = (item.key ?? index) as Key;
         return {
           item,
           itemIndex: index,
@@ -447,7 +447,7 @@ const Masonry = React.forwardRef<MasonryRef, MasonryProps>((props, ref) => {
               style: motionStyle,
               ...restMotionInfo
             } = motionInfo;
-            return renderItem(restMotionInfo as Parameters<typeof renderItem>[0], motionRef, motionClassName, motionStyle);
+            return renderItem(restMotionInfo as any, motionRef, motionClassName, motionStyle);
           }}
         </CSSMotionList>
       </div>
