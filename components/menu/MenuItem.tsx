5b073b4dafba80a2d69a05f529dc0ea6d8c909c3
gayanMatch <smithginger037@gmail.com>
2026-01-11 13:40:42 -0600

    menu

--- components/menu/MenuItem.tsx
+++ components/menu/MenuItem.tsx
@@ -7,7 +7,6 @@ import { clsx } from 'clsx';
 import { cloneElement } from '../_util/reactNode';
 import type { SiderContextProps } from '../layout/Sider';
 import { SiderContext } from '../layout/Sider';
-import type { TooltipProps } from '../tooltip';
 import Tooltip from '../tooltip';
 import type { MenuContextProps } from './MenuContext';
 import MenuContext from './MenuContext';
@@ -45,6 +44,14 @@ const MenuItem: GenericComponent = (props) => {
     styles,
     classNames,
   } = React.useContext<MenuContextProps>(MenuContext);
+  const { siderCollapsed } = React.useContext<SiderContextProps>(SiderContext);
+
+  const mergedCollapsed = siderCollapsed || isInlineCollapsed;
+
+  // Controlled tooltip state to prevent flash during collapse/expand transitions
+  // ref: https://github.com/ant-design/ant-design/issues/56528
+  const [tooltipOpen, setTooltipOpen] = React.useState(false);
+
   const renderItemChildren = (inlineCollapsed: boolean) => {
     const label = (children as React.ReactNode[])?.[0];
     const wrapNode = (
@@ -71,23 +78,12 @@ const MenuItem: GenericComponent = (props) => {
     return wrapNode;
   };
 
-  const { siderCollapsed } = React.useContext<SiderContextProps>(SiderContext);
-
+  // Compute tooltip title
   let tooltipTitle = title;
-
   if (typeof title === 'undefined') {
     tooltipTitle = firstLevel ? children : '';
   } else if (title === false) {
     tooltipTitle = '';
-  }
-
-  const tooltipProps: TooltipProps = { title: tooltipTitle };
-
-  if (!siderCollapsed && !isInlineCollapsed) {
-    tooltipProps.title = null;
-    // Reset `open` to fix control mode tooltip display not correct
-    // ref: https://github.com/ant-design/ant-design/issues/16742
-    tooltipProps.open = false;
   }
 
   const childrenLength = toArray(children).length;
@@ -124,10 +120,14 @@ const MenuItem: GenericComponent = (props) => {
     </Item>
   );
 
-  if (!disableMenuItemTitleTooltip) {
+  // Only render Tooltip when menu is collapsed to prevent flash during transitions
+  // ref: https://github.com/ant-design/ant-design/issues/56528
+  if (!disableMenuItemTitleTooltip && mergedCollapsed) {
     returnNode = (
       <Tooltip
-        {...tooltipProps}
+        title={tooltipTitle}
+        open={tooltipOpen}
+        onOpenChange={setTooltipOpen}
         placement={direction === 'rtl' ? 'left' : 'right'}
         classNames={{ root: `${prefixCls}-inline-collapsed-tooltip` }}
       >
