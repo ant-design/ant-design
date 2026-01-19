214bc2ced4196401c65a10f5af612de7cc55d6d5
zhengjihao <2714499297@qq.com>
2026-01-19 16:46:55 +0800

    fix: link styles to avoid conflicts with Button component

--- components/typography/style/mixins.ts
+++ components/typography/style/mixins.ts
@@ -54,12 +54,13 @@ export const getTitleStyles: GenerateStyle<TypographyToken, CSSObject> = (token)
 };
 
 export const getLinkStyles: GenerateStyle<TypographyToken, CSSObject> = (token) => {
-  const { componentCls } = token;
+  const { componentCls, antCls } = token;
+  const btnCls = `${antCls}-btn`;
 
   return {
     // fix issue: https://github.com/ant-design/ant-design/issues/56606
     // exclude ant-btn to avoid style conflicts with Button component when it renders as <a></a> tag (variant='link' with href)
-    'a&:not(.ant-btn), a:not(.ant-btn)': {
+    [`a&:not(${btnCls}), a:not(${btnCls})`]: {
       ...operationUnit(token),
 
       [`&[disabled], &${componentCls}-disabled`]: {
