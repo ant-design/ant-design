40afd4c35331e68bcc501b69111a6164fb7cdd9a
二货机器人 <smith3816@gmail.com>
2026-01-19 14:57:31 +0800

    chore: of it

--- .dumi/pages/index/components/ThemePreview/previewThemes/muiTheme.ts
+++ .dumi/pages/index/components/ThemePreview/previewThemes/muiTheme.ts
@@ -1,10 +1,72 @@
 import { useMemo } from 'react';
-import { theme } from 'antd';
-import type { ConfigProviderProps } from 'antd';
+import raf from '@rc-component/util/lib/raf';
+import type { ConfigProviderProps, GetProp } from 'antd';
 import { createStyles } from 'antd-style';
 import clsx from 'clsx';
 
 import type { UseTheme } from '.';
+
+type WaveConfig = GetProp<ConfigProviderProps, 'wave'>;
+
+// Prepare effect holder
+const createHolder = (node: HTMLElement) => {
+  const { borderWidth } = getComputedStyle(node);
+  const borderWidthNum = Number.parseInt(borderWidth, 10);
+
+  const div = document.createElement('div');
+  div.style.position = 'absolute';
+  div.style.inset = `-${borderWidthNum}px`;
+  div.style.borderRadius = 'inherit';
+  div.style.background = 'transparent';
+  div.style.zIndex = '999';
+  div.style.pointerEvents = 'none';
+  div.style.overflow = 'hidden';
+  node.appendChild(div);
+
+  return div;
+};
+
+const createDot = (holder: HTMLElement, color: string, left: number, top: number, size = 0) => {
+  const dot = document.createElement('div');
+  dot.style.position = 'absolute';
+  dot.style.left = `${left}px`;
+  dot.style.top = `${top}px`;
+  dot.style.width = `${size}px`;
+  dot.style.height = `${size}px`;
+  dot.style.borderRadius = '50%';
+  dot.style.background = color;
+  dot.style.transform = 'translate3d(-50%, -50%, 0)';
+  dot.style.transition = 'all 1s ease-out';
+  holder.appendChild(dot);
+  return dot;
+};
+
+// Inset Effect
+const showInsetEffect: WaveConfig['showEffect'] = (node, { event, component }) => {
+  if (component !== 'Button') {
+    return;
+  }
+
+  const holder = createHolder(node);
+
+  const rect = holder.getBoundingClientRect();
+
+  const left = event.clientX - rect.left;
+  const top = event.clientY - rect.top;
+
+  const dot = createDot(holder, 'rgba(255, 255, 255, 0.65)', left, top);
+
+  // Motion
+  raf(() => {
+    dot.ontransitionend = () => {
+      holder.remove();
+    };
+
+    dot.style.width = '200px';
+    dot.style.height = '200px';
+    dot.style.opacity = '0';
+  });
+};
 
 const useStyles = createStyles(({ css }) => {
   return {
@@ -67,7 +129,6 @@ const useMuiTheme: UseTheme = () => {
   return useMemo<ConfigProviderProps>(
     () => ({
       theme: {
-        algorithm: theme.defaultAlgorithm,
         token: {
           colorPrimary: '#1976d2',
           colorSuccess: '#2e7d32',
@@ -194,6 +255,9 @@ const useMuiTheme: UseTheme = () => {
             borderRadius: 4,
           },
         },
+      },
+      wave: {
+        showEffect: showInsetEffect,
       },
       button: {
         classNames: ({ props }) => ({
