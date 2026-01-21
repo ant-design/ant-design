3c829496b21cd3b94578ed7616fa4b921afe0511
Clayton <claytonlin1110@gmail.com>
2026-01-21 10:17:02 -0600

    Revert "chore: remove unnecessary unit in style (#56677)"
    This reverts commit 7a058ab00599e7f581490d1f843abb94a0bc65e7.

--- components/card/style/index.ts
+++ components/card/style/index.ts
@@ -343,9 +343,10 @@ const genCardStyle: GenerateStyle<CardToken> = (token): CSSObject => {
         padding: bodyPadding,
         borderRadius: `0 0 ${unit(token.borderRadiusLG)} ${unit(token.borderRadiusLG)}`,
         '&:first-child': {
-          borderStartStartRadius: token.borderRadiusLG,
-          borderStartEndRadius: token.borderRadiusLG,
+          borderStartStartRadius: unit(token.borderRadiusLG),
+          borderStartEndRadius: unit(token.borderRadiusLG),
         },
+
         '&:not(:last-child)': {
           borderEndStartRadius: 0,
           borderEndEndRadius: 0,
