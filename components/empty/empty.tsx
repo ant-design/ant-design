2c6b805af0d5a561c93b3025d9fb27283e03a3a5
liuqiang <qiang.liu@xinjifamily.com>
2026-01-22 15:23:35 +0800

    update

--- components/empty/empty.tsx
+++ components/empty/empty.tsx
@@ -13,42 +13,17 @@ const Empty: React.FC = () => {
   // Dark Theme need more dark of this
   const themeStyle: React.CSSProperties = bgColor.toHsl().l < 0.5 ? { opacity: 0.65 } : {};
 
-  return (
-    <svg
-      style={themeStyle}
-      width="184"
-      height="152"
-      viewBox="0 0 184 152"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>{locale?.description || 'Empty'}</title>
-      <g fill="none" fillRule="evenodd">
-        <g transform="translate(24 31.7)">
-          <ellipse fillOpacity=".8" fill="#F5F5F7" cx="67.8" cy="106.9" rx="67.8" ry="12.7" />
-          <path
-            fill="#aeb8c2"
-            d="M122 69.7 98.1 40.2a6 6 0 0 0-4.6-2.2H42.1a6 6 0 0 0-4.6 2.2l-24 29.5V85H122z"
-          />
-          <path
-            fill="#f5f5f7"
-            d="M33.8 0h68a4 4 0 0 1 4 4v93.3a4 4 0 0 1-4 4h-68a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4"
-          />
-          <path
-            fill="#dce0e6"
-            d="M42.7 10h50.2a2 2 0 0 1 2 2v25a2 2 0 0 1-2 2H42.7a2 2 0 0 1-2-2V12a2 2 0 0 1 2-2m.2 39.8h49.8a2.3 2.3 0 1 1 0 4.5H42.9a2.3 2.3 0 0 1 0-4.5m0 11.7h49.8a2.3 2.3 0 1 1 0 4.6H42.9a2.3 2.3 0 0 1 0-4.6m79 43.5a7 7 0 0 1-6.8 5.4H20.5a7 7 0 0 1-6.7-5.4l-.2-1.8V69.7h26.3c2.9 0 5.2 2.4 5.2 5.4s2.4 5.4 5.3 5.4h34.8c2.9 0 5.3-2.4 5.3-5.4s2.3-5.4 5.2-5.4H122v33.5q0 1-.2 1.8"
-          />
-        </g>
-        <path
-          fill="#dce0e6"
-          d="m149.1 33.3-6.8 2.6a1 1 0 0 1-1.3-1.2l2-6.2q-4.1-4.5-4.2-10.4c0-10 10.1-18.1 22.6-18.1S184 8.1 184 18.1s-10.1 18-22.6 18q-6.8 0-12.3-2.8"
-        />
-        <g fill="#fff" transform="translate(149.7 15.4)">
-          <circle cx="20.7" cy="3.2" r="2.8" />
-          <path d="M5.7 5.6H0L2.9.7zM9.3.7h5v5h-5z" />
-        </g>
-      </g>
-    </svg>
-  );
+  return /*#__PURE__*/ React.createElement('svg', {
+    style: themeStyle,
+    width: '184',
+    height: '152',
+    viewBox: '0 0 184 152',
+    xmlns: 'http://www.w3.org/2000/svg',
+    // biome-ignore lint/security/noDangerouslySetInnerHtml: SVG content is static and safe
+    dangerouslySetInnerHTML: {
+      __html: `<title>${locale?.description || 'Empty'}</title><g fill="none" fill-rule="evenodd"><g transform="translate(24 31.7)"><ellipse fill-opacity=".8" fill="#F5F5F7" cx="67.8" cy="106.9" rx="67.8" ry="12.7"></ellipse><path fill="#aeb8c2" d="M122 69.7 98.1 40.2a6 6 0 0 0-4.6-2.2H42.1a6 6 0 0 0-4.6 2.2l-24 29.5V85H122z"></path><path fill="#f5f5f7" d="M33.8 0h68a4 4 0 0 1 4 4v93.3a4 4 0 0 1-4 4h-68a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4"></path><path fill="#dce0e6" d="M42.7 10h50.2a2 2 0 0 1 2 2v25a2 2 0 0 1-2 2H42.7a2 2 0 0 1-2-2V12a2 2 0 0 1 2-2m.2 39.8h49.8a2.3 2.3 0 1 1 0 4.5H42.9a2.3 2.3 0 0 1 0-4.5m0 11.7h49.8a2.3 2.3 0 1 1 0 4.6H42.9a2.3 2.3 0 0 1 0-4.6m79 43.5a7 7 0 0 1-6.8 5.4H20.5a7 7 0 0 1-6.7-5.4l-.2-1.8V69.7h26.3c2.9 0 5.2 2.4 5.2 5.4s2.4 5.4 5.3 5.4h34.8c2.9 0 5.3-2.4 5.3-5.4s2.3-5.4 5.2-5.4H122v33.5q0 1-.2 1.8"></path></g><path fill="#dce0e6" d="m149.1 33.3-6.8 2.6a1 1 0 0 1-1.3-1.2l2-6.2q-4.1-4.5-4.2-10.4c0-10 10.1-18.1 22.6-18.1S184 8.1 184 18.1s-10.1 18-22.6 18q-6.8 0-12.3-2.8"></path><g fill="#fff" transform="translate(149.7 15.4)"><circle cx="20.7" cy="3.2" r="2.8"></circle><path d="M5.7 5.6H0L2.9.7zM9.3.7h5v5h-5z"></path></g></g>`,
+    },
+  });
 };
 
 if (process.env.NODE_ENV !== 'production') {
--- components/empty/simple.tsx
+++ components/empty/simple.tsx
@@ -20,21 +20,16 @@ const Simple: React.FC = () => {
     [colorFill, colorFillTertiary, colorFillQuaternary, colorBgContainer],
   );
 
-  return (
-    <svg width="64" height="41" viewBox="0 0 64 41" xmlns="http://www.w3.org/2000/svg">
-      <title>{locale?.description || 'Empty'}</title>
-      <g transform="translate(0 1)" fill="none" fillRule="evenodd">
-        <ellipse fill={shadowColor} cx="32" cy="33" rx="32" ry="7" />
-        <g fillRule="nonzero" stroke={borderColor}>
-          <path d="M55 12.8 44.9 1.3Q44 0 42.9 0H21.1q-1.2 0-2 1.3L9 12.8V22h46z" />
-          <path
-            d="M41.6 16c0-1.7 1-3 2.2-3H55v18.1c0 2.2-1.3 3.9-3 3.9H12c-1.7 0-3-1.7-3-3.9V13h11.2c1.2 0 2.2 1.3 2.2 3s1 2.9 2.2 2.9h14.8c1.2 0 2.2-1.4 2.2-3"
-            fill={contentColor}
-          />
-        </g>
-      </g>
-    </svg>
-  );
+  return /*#__PURE__*/ React.createElement('svg', {
+    width: '64',
+    height: '41',
+    viewBox: '0 0 64 41',
+    xmlns: 'http://www.w3.org/2000/svg',
+    // biome-ignore lint/security/noDangerouslySetInnerHtml: SVG content is static and safe
+    dangerouslySetInnerHTML: {
+      __html: `<title>${locale?.description || 'Empty'}</title><g transform="translate(0 1)" fill="none" fill-rule="evenodd"><ellipse fill="${shadowColor}" cx="32" cy="33" rx="32" ry="7"></ellipse><g fill-rule="nonzero" stroke="${borderColor}"><path d="M55 12.8 44.9 1.3Q44 0 42.9 0H21.1q-1.2 0-2 1.3L9 12.8V22h46z"></path><path d="M41.6 16c0-1.7 1-3 2.2-3H55v18.1c0 2.2-1.3 3.9-3 3.9H12c-1.7 0-3-1.7-3-3.9V13h11.2c1.2 0 2.2 1.3 2.2 3s1 2.9 2.2 2.9h14.8c1.2 0 2.2-1.4 2.2-3" fill="${contentColor}"></path></g></g>`,
+    },
+  });
 };
 
 if (process.env.NODE_ENV !== 'production') {
