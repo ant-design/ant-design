ead5840d9036dbdd3d06b9a1f3d838924a708f2c
unknowntocka <64572397+unknowntocka@users.noreply.github.com>
2026-01-28 17:57:18 +0800

    fix(Button): default button token (#56719)
    * fix(Button): default button token
    
    * feat(Button): add default button token with custom colors in debug demo
    
    ---------
    
    Co-authored-by: lijianan <574980606@qq.com>

--- components/button/style/variant.ts
+++ components/button/style/variant.ts
@@ -218,9 +218,9 @@ const genVariantStyle: GenerateStyle<ButtonToken> = (token) => {
           [varName('color-light-hover')]: token.colorFillSecondary,
           [varName('color-light-active')]: token.colorFill,
 
-          [varName('text-color')]: token.colorText,
-          [varName('text-color-hover')]: token.defaultHoverBorderColor,
-          [varName('text-color-active')]: token.defaultActiveBorderColor,
+          [varName('text-color')]: token.defaultColor,
+          [varName('text-color-hover')]: token.defaultHoverColor,
+          [varName('text-color-active')]: token.defaultActiveColor,
           [varName('shadow')]: token.defaultShadow,
 
           [`&${componentCls}-variant-solid`]: {
