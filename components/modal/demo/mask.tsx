379b22fadf0d39a20342d657bfe7cf5c2db3e4b9
thinkasany <480968828@qq.com>
2026-01-28 20:31:58 +0800

    Update components/modal/demo/mask.tsx
    Signed-off-by: thinkasany <480968828@qq.com>

--- components/modal/demo/mask.tsx
+++ components/modal/demo/mask.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
         </Button>
         <Button
           onClick={() => {
-            modal.confirm({ ...modalConfig });
+            modal.confirm(modalConfig);
           }}
         >
           Dimmed mask
