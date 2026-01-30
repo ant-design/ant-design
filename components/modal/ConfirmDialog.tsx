38d4ae419ac201bc05dcc17f4772617d1e629ab2
罗忠泽 <victor.luo@spotterio.com>
2026-01-30 15:19:59 +0800

    fix: Preserve the original maskClosable capabilities of ConfigDialog

--- components/modal/ConfirmDialog.tsx
+++ components/modal/ConfirmDialog.tsx
@@ -243,7 +243,7 @@ const ConfirmDialog: React.FC<ConfirmDialogProps> = (props) => {
       footer={null}
       transitionName={getTransitionName(rootPrefixCls || '', 'zoom', props.transitionName)}
       maskTransitionName={getTransitionName(rootPrefixCls || '', 'fade', props.maskTransitionName)}
-      mask={{ closable: maskClosable }}
+      maskClosable={maskClosable}
       style={style}
       styles={{ body: bodyStyle, mask: maskStyle, ...styles }}
       width={width}
