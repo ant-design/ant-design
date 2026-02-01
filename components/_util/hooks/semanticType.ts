6a21f98740bdf72fa96feb1624b0908109274572
crazyair <645381995@qq.com>
2026-02-01 09:54:07 +0800

    feat: change-styles-classnames-ts-v2

--- components/_util/hooks/semanticType.ts
+++ components/_util/hooks/semanticType.ts
@@ -1,7 +1,5 @@
 type RemoveString<T> = T extends string ? never : T;
-type RemoveStringKey<T, K extends keyof T> = string extends T[K]
-  ? RemoveString<T[K]>
-  : T[K];
+type RemoveStringKey<T, K extends keyof T> = string extends T[K] ? RemoveString<T[K]> : T[K];
 export type RemoveClassNamesString<T> = { [K in keyof T]: RemoveStringKey<T, K> };
 
 export type DeepClassNameType<T> = {
@@ -17,43 +15,3 @@ export type GenerateSemantic<T extends { classNames?: any; styles?: any }, Props
   stylesFn: (info: { props: Props }) => DeepStylesType<T['styles']>;
   classNamesNoString: RemoveClassNamesString<DeepClassNameType<T['classNames']>>;
 };
-
-// ------ demo type ------
-// export type MenuSemanticType = {
-//   classNames: {
-//     root?: string;
-//     popup?: string | { root?: string };
-//   };
-//   styles: {
-//     root?: React.CSSProperties;
-//     popup?: { root?: React.CSSProperties };
-//   };
-// };
-
-// export type MenuClassNamesType = GenerateSemantic<MenuSemanticType, MenuProps>;
-// export interface MenuProps {
-//   disabled?: boolean;
-//   classNames?: MenuClassNamesType['classNames'] | MenuClassNamesType['classNamesFn'];
-//   styles?: MenuClassNamesType['styles'] | MenuClassNamesType['stylesFn'];
-//   classNamesNoString?: MenuClassNamesType['classNamesNoString'];
-// }
-// export const menuConfig: MenuProps = {
-//   classNames: { root: 'root-class', popup: { root: 'popup-root-class' } },
-//   styles: { root: { color: 'red' }, popup: { root: { backgroundColor: 'blue' } } },
-//   classNamesNoString: { popup: { root: 'popup-root-class' } },
-// };
-// export const menuConfig3: MenuProps = {
-//   classNames: { root: 'root-class', popup: 'string' },
-//   styles: { root: { color: 'red' }, popup: { root: { backgroundColor: 'blue' } } },
-// };
-
-// export const menuConfig2: MenuProps = {
-//   classNames: ({ props }): MenuClassNamesType['classNames'] => ({
-//     root: props ? 'a' : 'b',
-//     popup: { root: 'c' },
-//   }),
-//   styles: ({ props }): MenuClassNamesType['styles'] => ({
-//     root: { color: props ? 'pink' : 'gray' },
-//     popup: { root: { backgroundColor: 'black' } },
-//   }),
-// };
