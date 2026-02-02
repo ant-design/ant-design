c7ff9751a587f0479554223c3fa7a3c547638702
crazyair <645381995@qq.com>
2026-02-02 17:12:05 +0800

    feat: review

--- components/_util/hooks/useMergeSemantic.ts
+++ components/_util/hooks/useMergeSemantic.ts
@@ -8,11 +8,6 @@ export type SemanticSchema = { _default?: string } & {
   [key: `${ValidChar}${string}`]: SemanticSchema;
 };
 
-export type RemoveStringSemanticType<T extends { classNames?: any; styles?: any }> = {
-  styles?: T['styles'];
-  classNames?: RemoveClassNamesString<NonNullable<T['classNames']>>;
-};
-
 // ========================= ClassNames =========================
 export const mergeClassNames = <SemanticClassNames = any>(
   schema?: SemanticSchema,
