3f298df1ddb7f6fe1bbafb7a065a8379d9f12fb5
crazyair <645381995@qq.com>
2026-02-05 09:05:58 +0800

    feat: add classNamesAndFn

--- components/_util/hooks/semanticType.ts
+++ components/_util/hooks/semanticType.ts
@@ -12,13 +12,14 @@ export type DeepStylesType<T> = {
 };
 
 export type GenerateSemantic<T extends { classNames?: any; styles?: any }, Props> = {
+  // classNames
   classNames: DeepClassNameType<T['classNames']>;
   classNamesNoString: RemoveClassNamesString<DeepClassNameType<T['classNames']>>;
   classNamesFn: (info: { props: Props }) => DeepClassNameType<T['classNames']>;
   classNamesAndFn:
     | DeepClassNameType<T['classNames']>
     | ((info: { props: Props }) => DeepClassNameType<T['classNames']>);
-
+  // styles
   styles: DeepStylesType<T['styles']>;
   stylesFn: (info: { props: Props }) => DeepStylesType<T['styles']>;
   stylesAndFn:
