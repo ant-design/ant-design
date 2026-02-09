54ae4d880253b26e5ce2eebbf30776cb218ba093
二货机器人 <smith3816@gmail.com>
2026-02-09 15:46:17 +0800

    chore: init

--- components/list/demo/basic.tsx
+++ components/list/demo/basic.tsx
@@ -16,6 +16,11 @@ const data = [
   },
 ];
 
+/**
+ * TODO: List 包裹的 Spin 子组件导致了 List 自身带的 css var 注入被 Spin 覆盖的问题，
+ * 需要在 components/list/index.tsx 给 Row or ul 添加一个 css var 注入，来解决这个问题
+ */
+
 const App: React.FC = () => (
   <ConfigProvider
     theme={{
@@ -29,7 +34,7 @@ const App: React.FC = () => (
     <List
       itemLayout="horizontal"
       dataSource={data}
-      loading
+      // loading
       renderItem={(item, index) => (
         <List.Item>
           <List.Item.Meta
