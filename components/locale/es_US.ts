3a422f3a74cc284e378537a4921f1a3a1faad0ae
Yurii Dumych <yurii.dumych@sycle.net>
2026-03-04 19:02:38 +0200

    feat(locale): rework approach

--- components/locale/es_US.ts
+++ components/locale/es_US.ts
@@ -1,24 +1,12 @@
 import esES from './es_ES';
 import type { Locale } from '.';
-import Calendar from '../calendar/locale/es_ES';
-import DatePicker from '../date-picker/locale/es_ES';
+import Calendar from '../calendar/locale/es_US';
+import DatePicker from '../date-picker/locale/es_US';
 
 const localeValues: Locale = {
   ...esES,
-  DatePicker: {
-    ...DatePicker,
-    lang: {
-      ...DatePicker.lang,
-      locale: 'en_US',
-    },
-  },
-  Calendar: {
-    ...Calendar,
-    lang: {
-      ...Calendar.lang,
-      locale: 'en_US',
-    },
-  },
+  DatePicker,
+  Calendar,
 };
 
 export default localeValues;
