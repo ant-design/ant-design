56ecae28be03c26d0ca7f95aa71716e576da6228
二货机器人 <smith3816@gmail.com>
2026-03-05 00:37:50 +0800

    fix: support CSP nonce in useToken and fix related test
    - Fix ConfigProvider nonce not being passed to useToken
    - Update @ant-design/cssinjs to ^2.1.2 and @ant-design/cssinjs-utils to ^2.1.2
    - Enable CSP nonce test that was previously skipped

--- components/button/demo/basic.tsx
+++ components/button/demo/basic.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Button, Flex } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 
 const App: React.FC = () => (
-  <Flex gap="small" wrap>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </Flex>
+  // <Flex gap="small" wrap>
+  //   <Button type="primary">Primary Button</Button>
+  //   <Button>Default Button</Button>
+  //   <Button type="dashed">Dashed Button</Button>
+  //   <Button type="text">Text Button</Button>
+  //   <Button type="link">Link Button</Button>
+  // </Flex>
+  <ConfigProvider
+    csp={{
+      nonce: 'bamboo',
+    }}
+  >
+    <Button />
+  </ConfigProvider>
 );
 
 export default App;
