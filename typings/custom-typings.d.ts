b71351d2354f7fffcbc8e9b2a3fd4e9f11fa3b56
lijianan <574980606@qq.com>
2026-03-24 05:40:59 +0800

    update

--- typings/custom-typings.d.ts
+++ typings/custom-typings.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="jquery" />
+
 // https://github.com/facebook/create-react-app/blob/f09d3d3a52c1b938cecc977c2bbc0942ea0a7e70/packages/react-scripts/lib/react-app.d.ts#L42-L49
 declare module '*.svg' {
   import type * as React from 'react';
@@ -39,4 +41,8 @@ declare module 'csstree-validator';
 declare module '*?raw' {
   const content: string;
   export default content;
+}
+
+interface Window {
+  gtag?: (command: string, action: string, params?: Record<string, unknown>) => void;
 }
