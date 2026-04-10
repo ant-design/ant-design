582c2ad29c3466ce38dbd693e87cd065c58edf86
lijianan <574980606@qq.com>
2026-04-11 06:51:13 +0800

    demo: add font-variant-numeric for demo

--- components/statistic/demo/timer.tsx
+++ components/statistic/demo/timer.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import type { StatisticTimerProps } from 'antd';
 import { Col, Row, Statistic } from 'antd';
+import { createStyles } from 'antd-style';
 
 const { Timer } = Statistic;
+
+const useStyle = createStyles(({ css }) => {
+  return {
+    content: css`
+      font-variant-numeric: tabular-nums;
+    `,
+  };
+});
 
 const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Dayjs is also OK
 const before = Date.now() - 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
@@ -13,42 +22,70 @@ const onFinish: StatisticTimerProps['onFinish'] = () => {
 };
 
 const onChange: StatisticTimerProps['onChange'] = (val) => {
-  if (typeof val === 'number' && 4.95 * 1000 < val && val < 5 * 1000) {
+  if (typeof val === 'number' && !Number.isNaN(val) && 4.95 * 1000 < val && val < 5 * 1000) {
     console.log('changed!');
   }
 };
 
-const App: React.FC = () => (
-  <Row gutter={16}>
-    <Col span={12}>
-      <Timer type="countdown" value={deadline} onFinish={onFinish} />
-    </Col>
-    <Col span={12}>
-      <Timer type="countdown" title="Million Seconds" value={deadline} format="HH:mm:ss:SSS" />
-    </Col>
-    <Col span={12}>
-      <Timer type="countdown" title="Countdown" value={tenSecondsLater} onChange={onChange} />
-    </Col>
-    <Col span={12}>
-      <Timer type="countup" title="Countup" value={before} onChange={onChange} />
-    </Col>
-    <Col span={24} style={{ marginTop: 32 }}>
-      <Timer
-        type="countdown"
-        title="Day Level (Countdown)"
-        value={deadline}
-        format="D 天 H 时 m 分 s 秒"
-      />
-    </Col>
-    <Col span={24} style={{ marginTop: 32 }}>
-      <Timer
-        type="countup"
-        title="Day Level (Countup)"
-        value={before}
-        format="D 天 H 时 m 分 s 秒"
-      />
-    </Col>
-  </Row>
-);
+const Demo: React.FC = () => {
+  const { styles } = useStyle();
+  return (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countdown"
+          value={deadline}
+          onFinish={onFinish}
+        />
+      </Col>
+      <Col span={12}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countdown"
+          title="Million Seconds"
+          value={deadline}
+          format="HH:mm:ss:SSS"
+        />
+      </Col>
+      <Col span={12}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countdown"
+          title="Countdown"
+          value={tenSecondsLater}
+          onChange={onChange}
+        />
+      </Col>
+      <Col span={12}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countup"
+          title="Countup"
+          value={before}
+          onChange={onChange}
+        />
+      </Col>
+      <Col span={24} style={{ marginTop: 32 }}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countdown"
+          title="Day Level (Countdown)"
+          value={deadline}
+          format="D 天 H 时 m 分 s 秒"
+        />
+      </Col>
+      <Col span={24} style={{ marginTop: 32 }}>
+        <Timer
+          classNames={{ content: styles.content }}
+          type="countup"
+          title="Day Level (Countup)"
+          value={before}
+          format="D 天 H 时 m 分 s 秒"
+        />
+      </Col>
+    </Row>
+  );
+};
 
-export default App;
+export default Demo;
