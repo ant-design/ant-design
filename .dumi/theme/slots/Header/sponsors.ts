ddd08e4e0b84d064e007e1c2eb2587d5d9f74a7c
afc163 <afc163@gmail.com>
2026-04-20 16:55:01 +0800

    feat: add sponsors static data file

---
+++ .dumi/theme/slots/Header/sponsors.ts
@@ -0,0 +1,23 @@
+export interface Sponsor {
+  name: string;
+  logo: string;
+  url: string;
+}
+
+export const sponsors: Sponsor[] = [
+  {
+    name: 'TRACTIAN',
+    logo: 'https://images.opencollective.com/tractian/0235da9/logo/256.png',
+    url: 'https://tractian.com',
+  },
+  {
+    name: 'LobeHub',
+    logo: 'https://unpkg.com/@lobehub/icons-static-svg@1.79.0/icons/lobehub-color.svg',
+    url: 'https://lobehub.com',
+  },
+  {
+    name: 'YouMind',
+    logo: 'https://marketing-assets.youmind.com/logo-512.png',
+    url: 'https://youmind.com',
+  },
+];
