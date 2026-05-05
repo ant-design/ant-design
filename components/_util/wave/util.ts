7ccc3fe056d0bcd6dacd306285618dd25dfd2685
lijianan <574980606@qq.com>
2026-05-05 13:31:27 +0800

    update

--- components/_util/wave/util.ts
+++ components/_util/wave/util.ts
@@ -3,13 +3,17 @@ import { isString } from '../is';
 export const isValidWaveColor = (
   color: CSSStyleDeclaration[keyof CSSStyleDeclaration],
 ): color is string => {
+  if (!color) {
+    return false;
+  }
   return (
     isString(color) &&
     color !== '#fff' &&
     color !== '#ffffff' &&
     color !== 'rgb(255, 255, 255)' &&
     color !== 'rgba(255, 255, 255, 1)' &&
-    !/^rgba\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*0(?:\.0+)?\s*\)$/i.test(color) &&
+    !/rgba\((?:\d*, ){3}0\)/i.test(color) && // any transparent rgba color
+    !/^#(?:[0-9a-f]{3}0|[0-9a-f]{6}00)$/i.test(color) && // any transparent hex color
     color !== 'transparent' &&
     color !== 'canvastext'
   );
