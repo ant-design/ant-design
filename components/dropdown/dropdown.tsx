dbae762180a8fe20c04f482d1e349bac5ce959a4
高艳兵 <66234749+QDyanbing@users.noreply.github.com>
2026-05-19 17:33:13 +0800

    fix(Dropdown): avoid React 19 element ref warning (#58056)
    Co-authored-by: 遇见同学 <1875694521@qq.com>

--- components/dropdown/dropdown.tsx
+++ components/dropdown/dropdown.tsx
@@ -4,7 +4,7 @@ import RightOutlined from '@ant-design/icons/RightOutlined';
 import RcDropdown from '@rc-component/dropdown';
 import type { MenuProps as RcMenuProps } from '@rc-component/menu';
 import type { AlignType } from '@rc-component/trigger';
-import { omit, useComposeRef, useControlledState, useEvent } from '@rc-component/util';
+import { getNodeRef, omit, useComposeRef, useControlledState, useEvent } from '@rc-component/util';
 import { clsx } from 'clsx';
 
 import { useZIndex } from '../_util/hooks';
@@ -222,7 +222,7 @@ const Dropdown: CompoundedComponent = React.forwardRef<HTMLElement, DropdownProp
     isPrimitive(children) ? <span>{children}</span> : children,
   ) as React.ReactElement<{ className?: string; disabled?: boolean }>;
 
-  const composedRef = useComposeRef(ref, (child as any).ref);
+  const composedRef = useComposeRef(ref, getNodeRef(child));
 
   const popupTrigger = cloneElement(child, {
     className: clsx(
