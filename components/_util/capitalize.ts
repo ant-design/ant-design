86a3a53bd7d74c690e52b12e82d0d89d48c0df29
lijianan <574980606@qq.com>
2026-06-05 12:24:44 +0800

    revert

--- components/_util/capitalize.ts
+++ components/_util/capitalize.ts
@@ -1,9 +1,7 @@
-const capitalize = <T extends string>(str: T): Capitalize<T> => {
+export default function capitalize<T extends string>(str: T): Capitalize<T> {
   if (typeof str !== 'string') {
     return str;
   }
   const ret = str.charAt(0).toUpperCase() + str.slice(1);
   return ret as Capitalize<T>;
-};
-
-export default capitalize;
+}
