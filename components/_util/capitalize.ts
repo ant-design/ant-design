d3a5574110016821d7083b2f0f83e7df68c6444a
lijianan <574980606@qq.com>
2026-06-05 11:41:34 +0800

    fix: update

--- components/_util/capitalize.ts
+++ components/_util/capitalize.ts
@@ -1,10 +1,11 @@
 import { isString } from './is';
 
-export default function capitalize<T extends string>(str: T): Capitalize<T> {
+const capitalize = <T extends string>(str: T): Capitalize<T> => {
   if (!isString(str)) {
     return str;
   }
-
   const ret = str.charAt(0).toUpperCase() + str.slice(1);
   return ret as Capitalize<T>;
-}
+};
+
+export default capitalize;
