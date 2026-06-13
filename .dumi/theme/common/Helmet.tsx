2c53c85b0f73bf7f6bfb64e5e83c788e39932a2f
lijianan <574980606@qq.com>
2026-06-13 22:50:07 +0800

    update

--- .dumi/theme/common/Helmet.tsx
+++ .dumi/theme/common/Helmet.tsx
@@ -1,8 +1,11 @@
-import * as React from 'react';
+import React from 'react';
 import { Helmet } from 'dumi';
 
-const WrapHelmet: React.FC<React.PropsWithChildren<Helmet['props']>> = (props) => (
-  <Helmet {...props} />
-);
+const WrapHelmet: React.FC<React.PropsWithChildren<React.ComponentProps<typeof Helmet>>> = (
+  props,
+) => {
+  const { children, ...rest } = props;
+  return <Helmet {...rest}>{children}</Helmet>;
+};
 
 export default WrapHelmet;
