fcd531eb668f6610de5743a660e0f5d3b65cf98a
lijianan <574980606@qq.com>
2026-06-16 11:41:21 +0800

    Revert "fix(table): prevent ellipsis from disappearing on cell click (#58234)" (#58394)
    This reverts commit 506a72484077f7b52913be35bdd7eb2f01761eb0.

--- components/table/style/ellipsis.ts
+++ components/table/style/ellipsis.ts
@@ -10,7 +10,6 @@ const genEllipsisStyle: GenerateStyle<TableToken, CSSObject> = (token) => {
     [`${componentCls}-wrapper`]: {
       [`${componentCls}-cell-ellipsis`]: {
         ...textEllipsis,
-        overflow: 'clip',
         wordBreak: 'keep-all',
 
         // Fixed first or last should special process
@@ -21,7 +20,6 @@ const genEllipsisStyle: GenerateStyle<TableToken, CSSObject> = (token) => {
           overflow: 'visible',
           [`${componentCls}-cell-content`]: {
             ...textEllipsis,
-            overflow: 'clip',
             display: 'block',
           },
         },
