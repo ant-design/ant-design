924ac698b9eb4396dbda9ea895b4855a23545530
afc163 <afc163@gmail.com>
2026-06-23 12:26:21 +0800

    demo: reorder Typography shimmer examples

--- components/typography/demo/shimmer.tsx
+++ components/typography/demo/shimmer.tsx
@@ -4,15 +4,15 @@ import { Flex, Typography } from 'antd';
 const App: React.FC = () => (
   <Flex vertical>
     <Typography.Text shimmer>Thinking...</Typography.Text>
-    <Typography.Title level={4} shimmer={{ duration: 1.5 }}>
-      Generating title
-    </Typography.Title>
     <Typography.Paragraph shimmer>
       Ant Design is analyzing your request and preparing the next response.
     </Typography.Paragraph>
     <Typography.Link href="https://ant.design" shimmer>
       Loading link
     </Typography.Link>
+    <Typography.Title level={4} shimmer={{ duration: 1.5 }} style={{ marginTop: 0 }}>
+      Generating title
+    </Typography.Title>
   </Flex>
 );
 
