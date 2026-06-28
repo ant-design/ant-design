7248fe479fa1c5abfb4ca699b725497dcb90a699
lijianan <574980606@qq.com>
2026-06-28 09:25:38 +0800

    demo: fix lint warning

--- components/slider/demo/disabled-handle.tsx
+++ components/slider/demo/disabled-handle.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Checkbox, Slider, Space } from 'antd';
+import { Checkbox, Flex, Slider } from 'antd';
+
+const handleOptions = [
+  { key: 'start', label: 'Disabled Handle 1' },
+  { key: 'middle', label: 'Disabled Handle 2' },
+  { key: 'end', label: 'Disabled Handle 3' },
+];
 
 const App: React.FC = () => {
   const [value, setValue] = React.useState([20, 50, 80]);
@@ -14,26 +20,24 @@ const App: React.FC = () => {
   return (
     <>
       <Slider
-        range={{
-          draggableTrack: true,
-          minCount: 2,
-          maxCount: 5,
-        }}
+        range={{ draggableTrack: true, minCount: 2, maxCount: 5 }}
         value={value}
         onChange={setValue}
         disabled={disabled}
       />
-      <Space>
-        {value.map((_, index) => (
-          <Checkbox
-            key={`handle-${index}`}
-            checked={disabled[index]}
-            onChange={(e) => handleDisabledChange(index, e.target.checked)}
-          >
-            Handle {index + 1}
-          </Checkbox>
-        ))}
-      </Space>
+      <Flex gap="small" align="center" justify="flex-start" style={{ marginTop: 16 }}>
+        {handleOptions.map((handle, index) => {
+          return (
+            <Checkbox
+              key={`item-${handle.key}`}
+              checked={disabled[index]}
+              onChange={(e) => handleDisabledChange(index, e.target.checked)}
+            >
+              {handle.label}
+            </Checkbox>
+          );
+        })}
+      </Flex>
     </>
   );
 };
