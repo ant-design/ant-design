d9ea4b067a05fd47cbb2a2fca59ee55e2158177b
lijianan <574980606@qq.com>
2026-07-13 12:42:08 +0800

    demo(BorderBeam): add Hover over demo

---
+++ components/border-beam/demo/hover.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BorderBeam, Card } from 'antd';
+import { createStyles } from 'antd-style';
+
+const useStyles = createStyles((props) => {
+  const { css, prefixCls, cssVar } = props;
+  return {
+    card: css`
+      width: 360px;
+      .${prefixCls}-border-beam {
+        opacity: 0;
+        transition: opacity ${cssVar.motionDurationMid};
+      }
+      &:hover {
+        .${prefixCls}-border-beam {
+          opacity: 1;
+        }
+      }
+    `,
+  };
+});
+
+const Demo: React.FC = () => {
+  const { styles } = useStyles();
+  return (
+    <BorderBeam>
+      <Card className={styles.card} title="Hover over the card">
+        The border beam appears when the pointer moves over this card.
+      </Card>
+    </BorderBeam>
+  );
+};
+
+export default Demo;
