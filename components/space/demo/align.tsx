94e530fd748572a80afc829f82cc61a5958fa1a9
lijianan <574980606@qq.com>
2026-07-19 16:48:04 +0800

    refactor(demo): migrate styles from markdown to CSS-in-JS (#58734)
    * refactor(demo): migrate styles from markdown to CSS-in-JS
    
    * update
    
    * update
    
    * update
    
    * update

--- components/space/demo/align.tsx
+++ components/space/demo/align.tsx
@@ -1,37 +1,58 @@
 import React from 'react';
-import { Button, Space } from 'antd';
+import { Button, Flex, Space } from 'antd';
+import { createStyles } from 'antd-style';
 
-const App: React.FC = () => (
-  <div className="space-align-container">
-    <div className="space-align-block">
-      <Space align="center">
-        center
-        <Button type="primary">Primary</Button>
-        <span className="mock-block">Block</span>
-      </Space>
-    </div>
-    <div className="space-align-block">
-      <Space align="start">
-        start
-        <Button type="primary">Primary</Button>
-        <span className="mock-block">Block</span>
-      </Space>
-    </div>
-    <div className="space-align-block">
-      <Space align="end">
-        end
-        <Button type="primary">Primary</Button>
-        <span className="mock-block">Block</span>
-      </Space>
-    </div>
-    <div className="space-align-block">
-      <Space align="baseline">
-        baseline
-        <Button type="primary">Primary</Button>
-        <span className="mock-block">Block</span>
-      </Space>
-    </div>
-  </div>
-);
+const useStyles = createStyles((props) => {
+  const { css, cssVar } = props;
+  return {
+    spaceAlignBox: css`
+      flex: none;
+      margin: ${cssVar.marginXXS};
+      padding: ${cssVar.paddingXXS};
+      border: ${cssVar.lineWidth} ${cssVar.lineType} ${cssVar.blue};
+    `,
+    mockBox: css`
+      display: inline-block;
+      padding: ${cssVar.paddingXL} ${cssVar.padding};
+      background-color: rgba(150, 150, 150, 0.2);
+    `,
+  };
+});
+
+const App: React.FC = () => {
+  const { styles } = useStyles();
+  return (
+    <Flex wrap align="flex-start">
+      <div className={styles.spaceAlignBox}>
+        <Space align="center">
+          center
+          <Button type="primary">Primary</Button>
+          <span className={styles.mockBox}>Block</span>
+        </Space>
+      </div>
+      <div className={styles.spaceAlignBox}>
+        <Space align="start">
+          start
+          <Button type="primary">Primary</Button>
+          <span className={styles.mockBox}>Block</span>
+        </Space>
+      </div>
+      <div className={styles.spaceAlignBox}>
+        <Space align="end">
+          end
+          <Button type="primary">Primary</Button>
+          <span className={styles.mockBox}>Block</span>
+        </Space>
+      </div>
+      <div className={styles.spaceAlignBox}>
+        <Space align="baseline">
+          baseline
+          <Button type="primary">Primary</Button>
+          <span className={styles.mockBox}>Block</span>
+        </Space>
+      </div>
+    </Flex>
+  );
+};
 
 export default App;
