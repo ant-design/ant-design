9d7bc1cbd374a2faaf2b92e52878f0cbe92f381e
lazerg <lazerg2@gmail.com>
2026-07-29 21:40:49 +0500

    fix: apply disabled text color to AutoComplete input

--- components/auto-complete/__tests__/index.test.tsx
+++ components/auto-complete/__tests__/index.test.tsx
@@ -6,11 +6,24 @@ import { resetWarned } from '../../_util/warning';
 import mountTest from '../../../tests/shared/mountTest';
 import rtlTest from '../../../tests/shared/rtlTest';
 import { render, screen } from '../../../tests/utils';
+import Form from '../../form';
 import Input from '../../input';
 
 describe('AutoComplete', () => {
   mountTest(AutoComplete);
   rtlTest(AutoComplete);
+
+  it('should use disabled text color when Form is disabled', () => {
+    const { container } = render(
+      <Form disabled>
+        <AutoComplete value="disabled" options={[{ value: 'disabled' }]} />
+      </Form>,
+    );
+    const select = container.querySelector<HTMLElement>('.ant-select-disabled')!;
+    expect(getComputedStyle(select).getPropertyValue('--ant-select-color')).toBe(
+      'var(--ant-color-text-disabled)',
+    );
+  });
 
   it('AutoComplete with custom Input render perfectly', async () => {
     render(
