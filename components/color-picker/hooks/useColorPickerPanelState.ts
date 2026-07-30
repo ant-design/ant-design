42cc05f91b541f2aff053c4a2b48465eb266c24d
高艳兵 <yanbing.gao@aliyun.com>
2026-07-30 10:15:54 +0800

    refactor(ColorPicker): remove redundant disabledAlpha fallback

--- components/color-picker/hooks/useColorPickerPanelState.ts
+++ components/color-picker/hooks/useColorPickerPanelState.ts
@@ -48,7 +48,7 @@ export default function useColorPickerPanelState(
     defaultValue,
     format,
     defaultFormat,
-    disabledAlpha = false,
+    disabledAlpha,
     onFormatChange,
     onChange,
     onChangeComplete,
