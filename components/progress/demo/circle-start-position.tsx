4fac208801c679b1f3b58efd7d7e408871decc87
dogle <15707274049@163.com>
2026-08-18 20:20:48 +0800

    docs(Progress): display startPosition values in circle demo

--- components/progress/demo/circle-start-position.tsx
+++ components/progress/demo/circle-start-position.tsx
@@ -3,8 +3,14 @@ import { Flex, Progress } from 'antd';
 
 const App: React.FC = () => (
   <Flex gap="small" wrap>
-    <Progress type="circle" percent={75} />
-    <Progress type="circle" percent={75} startPosition={25} />
+    <Flex vertical align="center" gap="small">
+      <Progress type="circle" percent={75} />
+      <span>startPosition: 0</span>
+    </Flex>
+    <Flex vertical align="center" gap="small">
+      <Progress type="circle" percent={75} startPosition={25} />
+      <span>startPosition: 25</span>
+    </Flex>
   </Flex>
 );
 
