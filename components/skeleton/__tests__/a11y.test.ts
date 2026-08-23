c4eb744a2e7fb7aa86b1af3b00d2938c25fb2a27
Bao Nguyen <waycaosi1994@gmail.com>
2026-08-23 11:47:22 +0700

    fix(Skeleton): hide decorative title placeholder from assistive technology

--- components/skeleton/__tests__/a11y.test.ts
+++ components/skeleton/__tests__/a11y.test.ts
@@ -1,8 +1,6 @@
 import accessibilityDemoTest from '../../../tests/shared/accessibilityTest';
 
 accessibilityDemoTest('skeleton', {
-  // waiting for fix
-  disabledRules: ['empty-heading'],
   // we can set aria attribute to fix it
   skip: ['list.tsx', 'element.tsx'],
 });
