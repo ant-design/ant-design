f9eb07a7ea60d9d4b6854310f2c9c8e986713260
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-26 07:58:56 +0500

    docs(Select): cover small and large sizes in the line height demo

--- components/select/demo/line-height-debug.tsx
+++ components/select/demo/line-height-debug.tsx
@@ -6,10 +6,28 @@ const options = [{ value: 'light', label: 'Light' }];
 const selectStyle: React.CSSProperties = { width: 180 };
 
 const App: React.FC = () => (
-  <Flex gap="small" align="start">
+  <Flex gap="small" align="start" wrap>
     <ConfigProvider theme={{ token: { lineHeight: 2 } }}>
       <Select style={selectStyle} defaultValue="light" options={options} />
       <Select style={selectStyle} mode="multiple" defaultValue={['light']} options={options} />
+      <Select style={selectStyle} size="small" defaultValue="light" options={options} />
+      <Select
+        style={selectStyle}
+        size="small"
+        mode="multiple"
+        defaultValue={['light']}
+        options={options}
+      />
+    </ConfigProvider>
+    <ConfigProvider theme={{ token: { lineHeightLG: 2 } }}>
+      <Select style={selectStyle} size="large" defaultValue="light" options={options} />
+      <Select
+        style={selectStyle}
+        size="large"
+        mode="multiple"
+        defaultValue={['light']}
+        options={options}
+      />
     </ConfigProvider>
     <ConfigProvider theme={{ components: { Select: { lineHeight: 2 } } }}>
       <Select style={selectStyle} defaultValue="light" options={options} />
