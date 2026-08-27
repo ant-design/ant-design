08967781206ec1e3d7d72487092830ff5d963663
陳廷安 <73953029+nrps9909@users.noreply.github.com>
2026-08-27 18:44:29 +0800

    test(Descriptions): snapshot zero header states

--- components/descriptions/__tests__/content.test.tsx
+++ components/descriptions/__tests__/content.test.tsx
@@ -9,9 +9,53 @@ describe('Descriptions content rendering', () => {
 
     expect(container.querySelector('.ant-descriptions-title')).toHaveTextContent('0');
     expect(container.querySelector('.ant-descriptions-extra')).toHaveTextContent('0');
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="ant-descriptions css-var-root"
+        >
+          <div
+            class="ant-descriptions-header"
+          >
+            <div
+              class="ant-descriptions-title"
+            >
+              0
+            </div>
+            <div
+              class="ant-descriptions-extra"
+            >
+              0
+            </div>
+          </div>
+          <div
+            class="ant-descriptions-view"
+          >
+            <table>
+              <tbody />
+            </table>
+          </div>
+        </div>
+      </div>
+    `);
 
     rerender(<Descriptions title={null} extra={false} />);
 
     expect(container.querySelector('.ant-descriptions-header')).not.toBeInTheDocument();
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div
+          class="ant-descriptions css-var-root"
+        >
+          <div
+            class="ant-descriptions-view"
+          >
+            <table>
+              <tbody />
+            </table>
+          </div>
+        </div>
+      </div>
+    `);
   });
 });
