57756b5bc06ad7bf6c5ddfdfb8aabc8c5a360ce9
dogle <15707274049@163.com>
2026-09-02 23:11:56 +0800

    feat(Radio): support Radio.Group size for default Radio options

--- components/radio/demo/size.tsx
+++ components/radio/demo/size.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Flex, Radio } from 'antd';
+import { Flex, Radio, Typography } from 'antd';
+
+const options = [
+  { label: 'Hangzhou', value: 'a' },
+  { label: 'Shanghai', value: 'b' },
+  { label: 'Beijing', value: 'c' },
+];
 
 const App: React.FC = () => (
-  <Flex vertical gap="medium">
-    <Radio.Group defaultValue="a" size="large">
-      <Radio.Button value="a">Hangzhou</Radio.Button>
-      <Radio.Button value="b">Shanghai</Radio.Button>
-      <Radio.Button value="c">Beijing</Radio.Button>
-      <Radio.Button value="d">Chengdu</Radio.Button>
-    </Radio.Group>
-    <Radio.Group defaultValue="a">
-      <Radio.Button value="a">Hangzhou</Radio.Button>
-      <Radio.Button value="b">Shanghai</Radio.Button>
-      <Radio.Button value="c">Beijing</Radio.Button>
-      <Radio.Button value="d">Chengdu</Radio.Button>
-    </Radio.Group>
-    <Radio.Group defaultValue="a" size="small">
-      <Radio.Button value="a">Hangzhou</Radio.Button>
-      <Radio.Button value="b">Shanghai</Radio.Button>
-      <Radio.Button value="c">Beijing</Radio.Button>
-      <Radio.Button value="d">Chengdu</Radio.Button>
-    </Radio.Group>
+  <Flex gap="large" wrap>
+    <Flex vertical gap="medium" style={{ flex: '1 1 280px' }}>
+      <Typography.Text strong>Radio</Typography.Text>
+      <Radio.Group defaultValue="a" size="large" options={options} />
+      <Radio.Group defaultValue="a" options={options} />
+      <Radio.Group defaultValue="a" size="small" options={options} />
+    </Flex>
+    <Flex vertical gap="medium" style={{ flex: '1 1 280px' }}>
+      <Typography.Text strong>Radio.Button</Typography.Text>
+      <Radio.Group defaultValue="a" size="large" options={options} optionType="button" />
+      <Radio.Group defaultValue="a" options={options} optionType="button" />
+      <Radio.Group defaultValue="a" size="small" options={options} optionType="button" />
+    </Flex>
   </Flex>
 );
 
