59af1789ea08c505348ab36b8f2d111f371cfa62
carl chen <ccheartszzz@gmail.com>
2026-09-04 10:40:41 +0800

    site: drop comment in theme editor icon fix

--- .dumi/pages/theme-editor/index.tsx
+++ .dumi/pages/theme-editor/index.tsx
@@ -16,9 +16,6 @@ const classNames = createStaticStyles(({ css }) => ({
       display: inline;
     }
 
-    /* Zero out the margin-block-end compensation that Segmented applies to
-       bare-svg icons, which lifts the light/dark switcher icons above
-       center. Unlayered here so it wins over the layered antd rule. */
     .ant-segmented-item-icon > svg {
       margin-block-end: 0;
     }
