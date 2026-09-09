1d0f54bb720ef537247a8d373b24a222b62c2a5e
二货机器人 <smith3816@gmail.com>
2026-09-09 11:30:23 +0800

    docs(InputNumber): refine allowClear demo

--- components/input-number/demo/allow-clear.tsx
+++ components/input-number/demo/allow-clear.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { CloseSquareFilled } from '@ant-design/icons';
 import { InputNumber, Space } from 'antd';
 
 const App: React.FC = () => (
   <Space vertical>
-    <InputNumber allowClear defaultValue={100} />
-    <InputNumber allowClear defaultValue={0} suffix="kg" />
-    <InputNumber allowClear mode="spinner" defaultValue={0} />
-    <InputNumber allowClear={{ clearIcon: <CloseSquareFilled /> }} defaultValue={100} />
+    <InputNumber allowClear defaultValue={100} style={{ width: 120 }} />
+    <InputNumber allowClear defaultValue={0} suffix="kg" style={{ width: 120 }} />
+    <InputNumber allowClear mode="spinner" defaultValue={0} style={{ width: 150 }} />
   </Space>
 );
 
