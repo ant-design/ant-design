c2cf508351fee76449ebe398b06d7a7594bc658a
高艳兵 <yanbing.gao@aliyun.com>
2026-09-09 10:06:24 +0800

    fix(Modal): preserve onCancel with custom cancel button click

--- components/modal/components/NormalCancelBtn.tsx
+++ components/modal/components/NormalCancelBtn.tsx
@@ -11,8 +11,14 @@ export interface NormalCancelBtnProps extends Pick<ModalProps, 'cancelButtonProp
 
 const NormalCancelBtn: FC = () => {
   const { cancelButtonProps, cancelTextLocale, onCancel } = useContext(ModalContext);
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    onCancel?.(event);
+    cancelButtonProps?.onClick?.(event);
+  };
+
   return (
-    <Button onClick={onCancel} {...cancelButtonProps}>
+    <Button {...cancelButtonProps} onClick={handleClick}>
       {cancelTextLocale}
     </Button>
   );
