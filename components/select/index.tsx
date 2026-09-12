6a642e342b67bdf022e3a01f02ab3e2dc3103e36
lijianan <574980606@qq.com>
2026-09-12 18:05:59 +0800

    type: deprecate prop types and add aliases (#58949)
    * fix: export missing component props types
    
    * update
    
    * update
    
    * chore: update
    
    * chore: update
    
    ---------
    
    Signed-off-by: lijianan <574980606@qq.com>
    Co-authored-by: thinkasany <480968828@qq.com>

--- components/select/index.tsx
+++ components/select/index.tsx
@@ -45,6 +45,7 @@ type RawValue = string | number;
 export type {
   BaseOptionType,
   DefaultOptionType,
+  /** @deprecated Please use `GetProps<typeof Select.Option>` instead. */
   OptionProps,
   BaseSelectRef as RefSelectProps,
   SearchConfig,
@@ -133,15 +134,15 @@ export interface SelectProps<
   ValueType = any,
   OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType,
 > extends Omit<
-    InternalSelectProps<ValueType, OptionType>,
-    | 'mode'
-    | 'getInputElement'
-    | 'getRawInputElement'
-    | 'backfill'
-    | 'placement'
-    | 'dropdownClassName'
-    | 'dropdownStyle'
-  > {
+  InternalSelectProps<ValueType, OptionType>,
+  | 'mode'
+  | 'getInputElement'
+  | 'getRawInputElement'
+  | 'backfill'
+  | 'placement'
+  | 'dropdownClassName'
+  | 'dropdownStyle'
+> {
   placement?: SelectCommonPlacement;
   mode?: 'multiple' | 'tags';
   status?: InputStatus;
